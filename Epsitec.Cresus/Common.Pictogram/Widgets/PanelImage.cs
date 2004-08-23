f455d28cc479214c62173aebab86cf931752de81
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:41:39 +0000

    ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1537 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Pictogram/Widgets/PanelImage.cs
@@ -0,0 +1,185 @@
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Dialogs;
+using Epsitec.Common.Support;
+using Epsitec.Common.Pictogram.Data;
+
+namespace Epsitec.Common.Pictogram.Widgets
+{
+	/// <summary>
+	/// La classe PanelImage permet de choisir un fichier image bitmap.
+	/// </summary>
+	public class PanelImage : AbstractPanel
+	{
+		public PanelImage()
+		{
+			this.label = new StaticText(this);
+			this.label.Alignment = Drawing.ContentAlignment.MiddleLeft;
+
+			this.fieldFilename = new TextField(this);
+			this.fieldFilename.TextChanged += new EventHandler(this.HandleTextChanged);
+			this.fieldFilename.TabIndex = 1;
+			this.fieldFilename.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+
+			this.buttonBrowse = new Button(this);
+			this.buttonBrowse.Text = "Parcourir...";
+			this.buttonBrowse.Clicked += new MessageEventHandler(this.HandleBrowseClicked);
+			this.buttonBrowse.TabIndex = 2;
+			this.buttonBrowse.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+
+			this.buttonMirrorH = new CheckButton(this);
+			this.buttonMirrorH.Text = "Miroir X";
+			this.buttonMirrorH.ActiveStateChanged += new EventHandler(this.ButtonActiveStateChanged);
+			this.buttonMirrorH.TabIndex = 3;
+			this.buttonMirrorH.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+
+			this.buttonMirrorV = new CheckButton(this);
+			this.buttonMirrorV.Text = "Miroir Y";
+			this.buttonMirrorV.ActiveStateChanged += new EventHandler(this.ButtonActiveStateChanged);
+			this.buttonMirrorV.TabIndex = 4;
+			this.buttonMirrorV.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+
+			this.buttonHomo = new CheckButton(this);
+			this.buttonHomo.Text = "Conserver proportions";
+			this.buttonHomo.ActiveStateChanged += new EventHandler(this.ButtonActiveStateChanged);
+			this.buttonHomo.TabIndex = 5;
+			this.buttonHomo.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+
+			this.isNormalAndExtended = true;
+		}
+		
+		public PanelImage(Widget embedder) : this()
+		{
+			this.SetEmbedder(embedder);
+		}
+		
+		protected override void Dispose(bool disposing)
+		{
+			if ( disposing )
+			{
+				this.fieldFilename.TextChanged -= new EventHandler(this.HandleTextChanged);
+				this.buttonBrowse.Clicked -= new MessageEventHandler(this.HandleBrowseClicked);
+				this.buttonMirrorH.ActiveStateChanged -= new EventHandler(this.ButtonActiveStateChanged);
+				this.buttonMirrorV.ActiveStateChanged -= new EventHandler(this.ButtonActiveStateChanged);
+				this.buttonHomo.ActiveStateChanged -= new EventHandler(this.ButtonActiveStateChanged);
+			}
+			
+			base.Dispose(disposing);
+		}
+
+
+		// Retourne la hauteur standard.
+		public override double DefaultHeight
+		{
+			get
+			{
+				return ( this.extendedSize ? 100 : 55 );
+			}
+		}
+
+		// Propriété -> widget.
+		public override void SetProperty(AbstractProperty property)
+		{
+			base.SetProperty(property);
+			this.label.Text = this.textStyle;
+
+			PropertyImage p = property as PropertyImage;
+			if ( p == null )  return;
+
+			this.fieldFilename.Text = p.Filename;
+			this.fieldFilename.Cursor = p.Filename.Length;
+			this.buttonMirrorH.ActiveState = p.MirrorH ? WidgetState.ActiveYes : WidgetState.ActiveNo;
+			this.buttonMirrorV.ActiveState = p.MirrorV ? WidgetState.ActiveYes : WidgetState.ActiveNo;
+			this.buttonHomo   .ActiveState = p.Homo    ? WidgetState.ActiveYes : WidgetState.ActiveNo;
+		}
+
+		// Widget -> propriété.
+		public override AbstractProperty GetProperty()
+		{
+			PropertyImage p = new PropertyImage();
+			base.GetProperty(p);
+
+			p.Filename = this.fieldFilename.Text;
+			p.MirrorH = ( this.buttonMirrorH.ActiveState == WidgetState.ActiveYes );
+			p.MirrorV = ( this.buttonMirrorV.ActiveState == WidgetState.ActiveYes );
+			p.Homo    = ( this.buttonHomo   .ActiveState == WidgetState.ActiveYes );
+			return p;
+		}
+
+
+		// Met à jour la géométrie.
+		protected override void UpdateClientGeometry()
+		{
+			base.UpdateClientGeometry();
+
+			if ( this.fieldFilename == null )  return;
+
+			Drawing.Rectangle rect = this.Client.Bounds;
+			rect.Deflate(this.extendedZoneWidth, 0);
+			rect.Deflate(5);
+
+			Drawing.Rectangle r = rect;
+			r.Bottom = r.Top-20;
+			r.Left = rect.Left;
+			r.Right = rect.Right-110;
+			this.label.Bounds = r;
+			r.Left = r.Right;
+			r.Right = rect.Right;
+			this.fieldFilename.Bounds = r;
+
+			r.Offset(0, -25);
+			r.Left = rect.Right-110;
+			r.Right = rect.Right;
+			this.buttonBrowse.Bounds = r;
+
+			r.Offset(0, -25);
+			r.Left = rect.Left;
+			r.Width = 70;
+			this.buttonMirrorH.Bounds = r;
+			r.Offset(80, 0);
+			this.buttonMirrorV.Bounds = r;
+
+			r.Offset(0, -20);
+			r.Left = rect.Left;
+			r.Right = rect.Right;
+			this.buttonHomo.Bounds = r;
+
+			this.buttonMirrorH.SetVisible(this.extendedSize);
+			this.buttonMirrorV.SetVisible(this.extendedSize);
+			this.buttonHomo.SetVisible(this.extendedSize);
+		}
+		
+		// Une valeur a été changée.
+		private void HandleTextChanged(object sender)
+		{
+			this.OnChanged();
+		}
+
+		// Une valeur a été changée.
+		private void ButtonActiveStateChanged(object sender)
+		{
+			this.OnChanged();
+		}
+
+		private void HandleBrowseClicked(object sender, MessageEventArgs e)
+		{
+			FileOpen dialog = new FileOpen();
+		
+			dialog.Title = "Ouvrir une image";
+			dialog.FileName = this.fieldFilename.Text;
+			dialog.Filters.Add("tiff", "Images TIFF", "*.tif");
+			dialog.Filters.Add("jpg", "Images JPG", "*.jpg");
+			dialog.Show();
+
+			this.fieldFilename.Text = dialog.FileName;
+			this.fieldFilename.Cursor = dialog.FileName.Length;
+		}
+
+
+		protected StaticText				label;
+		protected TextField					fieldFilename;
+		protected Button					buttonBrowse;
+		protected CheckButton				buttonMirrorH;
+		protected CheckButton				buttonMirrorV;
+		protected CheckButton				buttonHomo;
+	}
+}
