7a4106dea55a21abfb68ceb6859740238067c55f
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:03:06 +0000

    ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1196 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/GlyphButton.cs
@@ -0,0 +1,59 @@
+namespace Epsitec.Common.Widgets
+{
+	/// <summary>
+	/// La classe GlyphButton dessine un bouton avec une icône simple.
+	/// </summary>
+	public class GlyphButton : Button
+	{
+		public GlyphButton()
+		{
+			this.glyphType = GlyphType.None;
+			this.InternalState &= ~InternalState.AutoFocus;
+			this.InternalState &= ~InternalState.Focusable;
+		}
+		
+		public GlyphButton(Widget embedder) : this()
+		{
+			this.SetEmbedder(embedder);
+		}
+		
+		
+		public GlyphType GlyphType
+		{
+			get { return this.glyphType; }
+			set { this.glyphType = value; }
+		}
+		
+		public override double DefaultWidth
+		{
+			get { return 17; }
+		}
+		
+		public override double DefaultHeight
+		{
+			get { return 17; }
+		}
+		
+
+		// Dessine le bouton.
+		protected override void PaintBackgroundImplementation(Drawing.Graphics graphics, Drawing.Rectangle clipRect)
+		{
+			IAdorner adorner = Widgets.Adorner.Factory.Active;
+			Drawing.Rectangle rect = this.Client.Bounds;
+
+			Direction dir = Direction.None;
+			switch ( this.glyphType )
+			{
+				case GlyphType.ArrowUp:     dir = Direction.Up;     break;
+				case GlyphType.ArrowDown:   dir = Direction.Down;   break;
+				case GlyphType.ArrowLeft:   dir = Direction.Left;   break;
+				case GlyphType.ArrowRight:  dir = Direction.Right;  break;
+			}
+			adorner.PaintButtonBackground(graphics, rect, this.PaintState, dir, this.buttonStyle);
+			adorner.PaintGlyph(graphics, rect, this.PaintState, this.glyphType, PaintTextStyle.Button);
+		}
+
+		
+		protected GlyphType				glyphType;
+	}
+}
