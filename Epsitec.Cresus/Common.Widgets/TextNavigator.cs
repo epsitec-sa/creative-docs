da7935e9a6953d4b2dec6e0b5218c1ce3f296534
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:14:13 +0000

    ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1844 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/TextNavigator.cs
@@ -0,0 +1,451 @@
+using Epsitec.Common.Support;
+
+namespace Epsitec.Common.Widgets
+{
+	/// <summary>
+	/// La classe TextNavigator permet de naviguer dans un TextLayout,
+	/// d'insérer/supprimer des caractères, etc.
+	/// </summary>
+	public class TextNavigator
+	{
+		public TextNavigator(TextLayout textLayout)
+		{
+			this.textLayout = textLayout;
+			this.context = new TextLayout.Context();
+		}
+
+		
+		public TextLayout.Context				Context
+		{
+			get { return this.context; }
+		}
+
+		public bool								IsReadOnly
+		{
+			get { return this.isReadOnly; }
+			set { this.isReadOnly = value; }
+		}
+
+		public bool								IsNumeric
+		{
+			get { return this.isNumeric; }
+			set { this.isNumeric = value; }
+		}
+
+		public bool								IsMultiLine
+		{
+			get
+			{
+				if ( (this.textLayout.BreakMode & Drawing.TextBreakMode.SingleLine) == 0 )
+				{
+					return true;
+				}
+				
+				return false;
+			}
+		}
+		
+		public int								MaxChar
+		{
+			get { return this.maxChar; }
+			set { this.maxChar = value; }
+		}
+
+		public string							Selection
+		{
+			get
+			{
+				int cursorFrom = this.textLayout.FindOffsetFromIndex(this.context.CursorFrom);
+				int cursorTo   = this.textLayout.FindOffsetFromIndex(this.context.CursorTo);
+				
+				int from = System.Math.Min(cursorFrom, cursorTo);
+				int to   = System.Math.Max(cursorFrom, cursorTo);
+				
+				string text = this.textLayout.Text;
+				return text.Substring(from, to-from);
+			}
+
+			set
+			{
+				this.textLayout.ReplaceSelection(this.context, value);
+				this.OnTextInserted(true);
+				this.OnCursorScrolled();
+				this.OnCursorChanged(true);
+			}
+		}
+
+		public int								Cursor
+		{
+			get
+			{
+				return this.context.CursorTo;
+			}
+
+			set
+			{
+				value = System.Math.Max(value, 0);
+				value = System.Math.Min(value, this.textLayout.Text.Length);
+
+				if ( value != this.context.CursorFrom && value != this.context.CursorTo )
+				{
+					this.context.CursorFrom = value;
+					this.context.CursorTo   = value;
+					this.OnCursorScrolled();
+					this.OnCursorChanged(true);
+				}
+			}
+		}
+		
+		public int								CursorFrom
+		{
+			get
+			{
+				return this.context.CursorFrom;
+			}
+
+			set
+			{
+				value = System.Math.Max(value, 0);
+				value = System.Math.Min(value, this.textLayout.Text.Length);
+
+				if ( value != this.context.CursorFrom )
+				{
+					this.context.CursorFrom = value;
+					this.OnCursorScrolled();
+					this.OnCursorChanged(true);
+				}
+			}
+		}
+		
+		public int								CursorTo
+		{
+			get
+			{
+				return this.context.CursorTo;
+			}
+
+			set
+			{
+				value = System.Math.Max(value, 0);
+				value = System.Math.Min(value, this.textLayout.Text.Length);
+
+				if ( value != this.context.CursorTo )
+				{
+					this.context.CursorTo = value;
+					this.OnCursorScrolled();
+					this.OnCursorChanged(true);
+				}
+			}
+		}
+
+		
+		public bool ProcessMessage(Message message, Drawing.Point pos)
+		{
+			switch ( message.Type )
+			{
+				case MessageType.KeyDown:
+					if ( this.ProcessKeyDown(message.KeyCode, message.IsShiftPressed, message.IsCtrlPressed) )
+					{
+						message.Swallowed = true;
+						return true;
+					}
+					break;
+				
+				case MessageType.KeyPress:
+					if ( this.ProcessKeyPress(message.KeyChar) )
+					{
+						return true;
+					}
+					break;
+
+				case MessageType.MouseDown:
+					if ( message.ButtonDownCount == 1 )
+					{
+						this.ProcessBeginPress(pos);
+						this.mouseDrag = true;
+					}
+					this.mouseDown = true;
+					break;
+				
+				case MessageType.MouseMove:
+					if ( this.mouseDrag )
+					{
+						this.ProcessMovePress(pos);
+						return true;
+					}
+					break;
+				
+				case MessageType.MouseUp:
+					if ( this.mouseDown )
+					{
+						this.ProcessEndPress(pos, message.ButtonDownCount);
+						this.mouseDown = false;
+						this.mouseDrag = false;
+						return true;
+					}
+					break;
+			}
+
+			return false;
+		}
+
+		public void MouseDownMessage(Drawing.Point pos)
+		{
+			this.ProcessBeginPress(pos);
+			this.mouseDown = true;
+		}
+
+		// Gestion d'une touche pressée avec KeyDown dans le texte.
+		protected bool ProcessKeyDown(KeyCode key, bool isShiftPressed, bool isCtrlPressed)
+		{
+			this.InitialMemorize();
+
+			if ( this.IsMultiLine )
+			{
+				switch ( key )
+				{
+					case KeyCode.Return:
+						this.textLayout.InsertCharacter(this.context, '\n');
+						return true;
+
+					case KeyCode.Home:
+						if ( isCtrlPressed )
+						{
+							this.textLayout.MoveCursor(this.context, -1000000, isShiftPressed, false);
+						}
+						else
+						{
+							this.textLayout.MoveExtremity(this.context, -1, isShiftPressed);
+						}
+						this.OnCursorScrolled();
+						this.OnCursorChanged(false);
+						return true;
+
+					case KeyCode.End:
+						if ( isCtrlPressed )
+						{
+							this.textLayout.MoveCursor(this.context, 1000000, isShiftPressed, false);
+						}
+						else
+						{
+							this.textLayout.MoveExtremity(this.context, 1, isShiftPressed);
+						}
+						this.OnCursorScrolled();
+						this.OnCursorChanged(false);
+						return true;
+
+					case KeyCode.ArrowUp:
+						this.textLayout.MoveLine(this.context, -1, isShiftPressed);
+						this.OnCursorScrolled();
+						this.OnCursorChanged(false);
+						return true;
+
+					case KeyCode.ArrowDown:
+						this.textLayout.MoveLine(this.context, 1, isShiftPressed);
+						this.OnCursorScrolled();
+						this.OnCursorChanged(false);
+						return true;
+				
+					case KeyCode.PageUp:
+					case KeyCode.PageDown:
+						// TODO: gérer PageUp et PageDown...
+						return true;
+				}
+			}
+
+			switch ( key )
+			{
+				case KeyCode.Back:
+					if ( this.isReadOnly )  return false;
+					if ( isShiftPressed || isCtrlPressed )  return false;
+					this.textLayout.DeleteCharacter(this.context, -1);
+					this.OnTextDeleted(false);
+					this.OnCursorScrolled();
+					this.OnCursorChanged(false);
+					return true;
+				
+				case KeyCode.Delete:
+					if ( this.isReadOnly )  return false;
+					if ( isShiftPressed || isCtrlPressed )  return false;
+					this.textLayout.DeleteCharacter(this.context, 1);
+					this.OnTextDeleted(false);
+					this.OnCursorScrolled();
+					this.OnCursorChanged(false);
+					return true;
+				
+				case KeyCode.Home:
+					this.textLayout.MoveCursor(this.context, -1000000, isShiftPressed, false);  // recule beaucoup
+					this.OnCursorScrolled();
+					this.OnCursorChanged(false);
+					return true;
+				
+				case KeyCode.End:
+					this.textLayout.MoveCursor(this.context, 1000000, isShiftPressed, false);  // avance beaucoup
+					this.OnCursorScrolled();
+					this.OnCursorChanged(false);
+					return true;
+				
+				case KeyCode.ArrowLeft:
+					this.textLayout.MoveCursor(this.context, -1, isShiftPressed, isCtrlPressed);
+					this.OnCursorScrolled();
+					this.OnCursorChanged(false);
+					return true;
+				
+				case KeyCode.ArrowRight:
+					this.textLayout.MoveCursor(this.context, 1, isShiftPressed, isCtrlPressed);
+					this.OnCursorScrolled();
+					this.OnCursorChanged(false);
+					return true;
+			}
+			
+			return false;
+		}
+
+		// Gestion d'une touche pressée avec KeyPress dans le texte.
+		protected bool ProcessKeyPress(int key)
+		{
+			if ( this.isReadOnly )  return false;
+			this.InitialMemorize();
+
+			if ( key >= 32 )  // TODO: à vérifier ...
+			{
+				this.textLayout.InsertCharacter(this.context, (char)key);
+				this.OnTextInserted(false);
+				return true;
+			}
+			
+			return false;
+		}
+
+		// Appelé lorsque le bouton de la souris est pressé.
+		protected bool ProcessBeginPress(Drawing.Point pos)
+		{
+			int detect = this.textLayout.DetectIndex(pos);
+			if ( detect != -1 )
+			{
+				this.context.CursorFrom = detect;
+				this.context.CursorTo   = detect;
+				this.OnCursorChanged(true);
+				return true;
+			}
+			
+			return false;
+		}
+
+		// Appelé lorsque la souris est déplacée, bouton pressé.
+		protected void ProcessMovePress(Drawing.Point pos)
+		{
+			int detect = this.textLayout.DetectIndex(pos);
+			if ( detect != -1 )
+			{
+				this.context.CursorTo = detect;
+				this.OnCursorChanged(true);
+			}
+		}
+
+		// Appelé lorsque le bouton de la souris est relâché.
+		protected void ProcessEndPress(Drawing.Point pos, int downCount)
+		{
+			this.InitialMemorize();
+
+			if ( this.IsNumeric )
+			{
+				if ( downCount >= 2 )  downCount = 4;  // double clic -> sélectionne tout
+			}
+
+			if ( downCount >= 4 )  // quadruple clic ?
+			{
+				this.textLayout.SelectAll(this.context);
+			}
+			else if ( downCount >= 3 )  // triple clic ?
+			{
+				this.textLayout.SelectLine(this.context);
+			}
+			else if ( downCount >= 2 )  // double clic ?
+			{
+				this.textLayout.SelectWord(this.context);
+			}
+			else	// simple clic ?
+			{
+				int detect = this.textLayout.DetectIndex(pos);
+				if ( detect != -1 )
+				{
+					this.context.CursorTo = detect;
+				}
+			}
+
+			this.OnCursorChanged(false);
+		}
+
+
+		// Mémorise l'état avant une opération quelconque sur le texte.
+		protected void InitialMemorize()
+		{
+			this.iCursorFrom = this.context.CursorFrom;
+			this.iCursorTo   = this.context.CursorTo;
+			this.iTextLength = this.textLayout.Text.Length;
+		}
+
+		// Génère un événement pour dire que des caractères ont été insérés.
+		protected void OnTextInserted(bool always)
+		{
+			if ( !always && this.iTextLength == this.textLayout.Text.Length )  return;
+
+			if ( this.TextInserted != null )  // qq'un écoute ?
+			{
+				this.TextInserted(this);
+			}
+		}
+
+		public event EventHandler TextInserted;
+
+		// Génère un événement pour dire que des caractères ont été détruits.
+		protected void OnTextDeleted(bool always)
+		{
+			if ( !always && this.iTextLength == this.textLayout.Text.Length )  return;
+
+			if ( this.TextDeleted != null )  // qq'un écoute ?
+			{
+				this.TextDeleted(this);
+			}
+		}
+
+		public event EventHandler TextDeleted;
+
+		// Génère un événement pour dire que le curseur a bougé.
+		protected void OnCursorChanged(bool always)
+		{
+			if ( !always && this.iCursorFrom == this.context.CursorFrom && this.iCursorTo == this.context.CursorTo )  return;
+
+			if ( this.CursorChanged != null )  // qq'un écoute ?
+			{
+				this.CursorChanged(this);
+			}
+		}
+
+		public event EventHandler CursorChanged;
+
+		// Génère un événement pour dire que le curseur a scrollé.
+		protected void OnCursorScrolled()
+		{
+			if ( this.CursorScrolled != null )  // qq'un écoute ?
+			{
+				this.CursorScrolled(this);
+			}
+		}
+
+		public event EventHandler CursorScrolled;
+
+
+		protected TextLayout					textLayout;
+		protected TextLayout.Context			context;
+		protected bool							isReadOnly = false;
+		protected bool							isNumeric = false;
+		protected int							maxChar = 1000;
+		protected int							iCursorFrom;
+		protected int							iCursorTo;
+		protected int							iTextLength;
+		protected bool							mouseDown = false;
+		protected bool							mouseDrag = false;
+	}
+}
