6f11930437b1b61a0c68ed3b02cb1e731a95b263
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:16:58 +0000

    ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1301 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/AbstractToolBar.cs
@@ -0,0 +1,110 @@
+namespace Epsitec.Common.Widgets
+{
+	/// <summary>
+	/// La classe AbstractToolBar implémente ce qui est commun à HToolBar et à VToolBar.
+	/// </summary>
+	public abstract class AbstractToolBar : Widget, Helpers.IWidgetCollectionHost
+	{
+		public AbstractToolBar()
+		{
+			this.items = new Helpers.WidgetCollection (this);
+			
+			using (IconButton button = new IconButton ())
+			{
+				this.defaultButtonWidth  = button.DefaultWidth;
+				this.defaultButtonHeight = button.DefaultHeight;
+			}
+		}
+		
+		
+		public Helpers.WidgetCollection		Items
+		{
+			get { return this.items; }
+		}
+		
+		
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				Widget[] items = new Widget[this.items.Count];
+				
+				this.items.CopyTo (items, 0);
+				this.items.Clear ();
+				
+				for (int i = 0; i < items.Length; i++)
+				{
+					items[i].Dispose();
+				}
+				
+				this.items.Dispose ();
+				this.items = null;
+			}
+			
+			base.Dispose (disposing);
+		}
+		
+		protected override void PaintBackgroundImplementation(Drawing.Graphics graphics, Drawing.Rectangle clipRect)
+		{
+			// Dessine la barre.
+			IAdorner adorner = Widgets.Adorner.Factory.Active;
+			
+			Drawing.Rectangle rect  = this.Client.Bounds;
+			adorner.PaintToolBackground(graphics, rect, WidgetState.None, this.direction);
+		}
+		
+		
+		#region Interface IBundleSupport
+		public override void RestoreFromBundle(Epsitec.Common.Support.ObjectBundler bundler, Epsitec.Common.Support.ResourceBundle bundle)
+		{
+			base.RestoreFromBundle (bundler, bundle);
+			
+			Support.ResourceBundle.FieldList item_list = bundle["items"].AsList;
+			
+			if (item_list != null)
+			{
+				//	Notre bundle contient une liste de sous-bundles contenant les descriptions des
+				//	items composant le menu.
+				
+				foreach (Support.ResourceBundle.Field field in item_list)
+				{
+					Support.ResourceBundle item_bundle = field.AsBundle;
+					Widget                 item_widget = bundler.CreateFromBundle (item_bundle) as Widget;
+					
+					this.Items.Add (item_widget);
+				}
+			}
+		}
+		#endregion
+		
+		#region IWidgetCollectionHost Members
+		Helpers.WidgetCollection Helpers.IWidgetCollectionHost.GetWidgetCollection()
+		{
+			return this.Items;
+		}
+		
+		public void NotifyInsertion(Widget widget)
+		{
+			widget.Dock = this.iconDockStyle;
+			widget.AutoFocus = false;
+			widget.SetEmbedder (this);
+		}
+
+		public void NotifyRemoval(Widget widget)
+		{
+			this.Children.Remove (widget);
+		}
+		
+		public void NotifyPostRemoval(Widget widget)
+		{
+		}
+		#endregion
+		
+		protected DockStyle					iconDockStyle;
+		protected Direction					direction;
+		protected Helpers.WidgetCollection	items;
+		
+		protected double					defaultButtonWidth;
+		protected double					defaultButtonHeight;
+	}
+}
