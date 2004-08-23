644640a53d0042a3d8abca710765149c149622a6
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:30:17 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2015 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/Converter.cs
@@ -0,0 +1,310 @@
+//	Copyright © 2003-2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : en chantier/PA
+
+namespace Epsitec.Common.Types
+{
+	/// <summary>
+	/// La classe Converter permet de convertir des données simples entre
+	/// elles.
+	/// </summary>
+	public class Converter
+	{
+		private Converter()
+		{
+		}
+		
+		
+		public static bool IsNull(object obj)
+		{
+			return (obj == null) || (obj is System.DBNull);
+		}
+		
+		public static bool IsNotNull(object obj)
+		{
+			return (obj != null) && !(obj is System.DBNull);
+		}
+		
+		
+		public static string ToString(object obj)
+		{
+			string value;
+			Converter.Convert (obj, out value);
+			return value;
+		}
+		
+		
+		public static bool Convert(object obj, out string value)
+		{
+			if ((obj == null) || (obj is System.DBNull))
+			{
+				value = null;
+				return false;
+			}
+			if (obj is string)
+			{
+				value = obj as string;
+				return true;
+			}
+			
+			if (obj is System.ValueType)
+			{
+				System.TypeCode code = System.Convert.GetTypeCode (obj);
+				
+				switch (code)
+				{
+					case System.TypeCode.Boolean:	value = ((System.Boolean) obj).ToString (System.Globalization.CultureInfo.InvariantCulture); return true;
+					case System.TypeCode.Decimal:	value = ((System.Decimal) obj).ToString (System.Globalization.CultureInfo.InvariantCulture); return true;
+					case System.TypeCode.Single:	value = ((System.Single)  obj).ToString (System.Globalization.CultureInfo.InvariantCulture); return true;
+					case System.TypeCode.Double:	value = ((System.Double)  obj).ToString (System.Globalization.CultureInfo.InvariantCulture); return true;
+					case System.TypeCode.Byte:		value = ((System.Byte)    obj).ToString (System.Globalization.CultureInfo.InvariantCulture); return true;
+					case System.TypeCode.Int16:		value = ((System.Int16)   obj).ToString (System.Globalization.CultureInfo.InvariantCulture); return true;
+					case System.TypeCode.Int32:		value = ((System.Int32)   obj).ToString (System.Globalization.CultureInfo.InvariantCulture); return true;
+					case System.TypeCode.Int64:		value = ((System.Int64)   obj).ToString (System.Globalization.CultureInfo.InvariantCulture); return true;
+				}
+			}
+			
+			System.ComponentModel.TypeConverter conv = System.ComponentModel.TypeDescriptor.GetConverter (obj);
+			System.Diagnostics.Debug.Assert (conv != null);
+			
+			value = conv.ConvertToInvariantString (obj);
+			
+			System.Diagnostics.Debug.WriteLine (string.Format ("Warning: type {0} used {1} converter. Value={2}.", obj.GetType ().FullName, conv.GetType ().FullName, value));
+			
+			return true;
+		}
+		
+		public static bool Convert(object obj, out bool value)
+		{
+			decimal value_decimal;
+			bool ok = Converter.Convert (obj, out value_decimal);
+			value = (value_decimal == 0) ? false : true;
+			return ok;
+		}
+		
+		public static bool Convert(object obj, out int value)
+		{
+			decimal value_decimal;
+			bool ok = Converter.Convert (obj, out value_decimal);
+			value = (int) value_decimal;
+			return ok;
+		}
+		
+		public static bool Convert(object obj, out long value)
+		{
+			decimal value_decimal;
+			bool ok = Converter.Convert (obj, out value_decimal);
+			value = (long) value_decimal;
+			return ok;
+		}
+		
+		public static bool Convert(object obj, out decimal value)
+		{
+			//	Retourne true si la valeur de 'obj' n'est pas 'null' ou une
+			//	de ses variantes, false sinon. Si le type n'est pas reconnu ou
+			//	que la syntaxe n'est pas correcte, une exception est levée.
+			
+			if ((obj == null) || (obj is System.DBNull))
+			{
+				value = 0;
+				return false;
+			}
+			
+			string text = obj as string;
+			
+			if (text != null)
+			{
+				//	On va devoir faire un "Parse" coûteux... On pourrait bien sûr aussi utiliser TypeConverter,
+				//	mais ça ne nous apporterait rien ici.
+				
+				text = text.Trim ();
+				
+				if (text == "")
+				{
+					value = 0;
+					return false;
+				}
+				
+				switch (text.ToLower (System.Globalization.CultureInfo.InvariantCulture))
+				{
+					case "false":
+					case "off":
+					case "no":
+						value = 0;
+						return true;
+					
+					case "true":
+					case "on":
+					case "yes":
+						value = 1;
+						return true;
+				}
+				
+				value = System.Decimal.Parse (text, System.Globalization.CultureInfo.InvariantCulture);
+				return true;
+			}
+			
+			if (obj is System.ValueType)
+			{
+				System.TypeCode code = System.Convert.GetTypeCode (obj);
+				
+				switch (code)
+				{
+					case System.TypeCode.Boolean:	value = (decimal) ((System.Boolean) obj ? 1 : 0);	return true;
+					
+					case System.TypeCode.Decimal:	value = (decimal) (System.Decimal) obj;				return true;
+					
+					case System.TypeCode.Single:	value = (decimal) (System.Single) obj;				return true;
+					case System.TypeCode.Double:	value = (decimal) (System.Double) obj;				return true;
+					
+					case System.TypeCode.Byte:		value = (decimal) (System.Byte) obj;				return true;
+					case System.TypeCode.Int16:		value = (decimal) (System.Int16) obj;				return true;
+					case System.TypeCode.Int32:		value = (decimal) (System.Int32) obj;				return true;
+					case System.TypeCode.Int64:		value = (decimal) (System.Int64) obj;				return true;
+					
+					case System.TypeCode.DBNull:	value = 0;											return false;
+				}
+			}
+			
+			throw new System.NotSupportedException (string.Format ("Type {0}: conversion not supported.", obj.GetType ().Name));
+		}
+		
+		public static bool Convert(object obj, System.Type type, out System.Enum value)
+		{
+			if ((obj == null) || (obj is System.DBNull))
+			{
+				value = null;
+				return false;
+			}
+			
+			if (obj.GetType () == type)
+			{
+				value = (System.Enum) obj;
+				return true;
+			}
+			
+			string text = obj as string;
+			
+			if (text != null)
+			{
+				try
+				{
+					value = (System.Enum) System.Enum.Parse (type, text);
+					
+					if (System.Enum.IsDefined (type, value))
+					{
+						return true;
+					}
+				}
+				catch
+				{
+				}
+			}
+			
+			int index;
+			
+			if ((Converter.Convert (obj, out index)) &&
+				(System.Enum.IsDefined (type, index)))
+			{
+				value = (System.Enum) System.Enum.Parse (type, index.ToString (System.Globalization.CultureInfo.InvariantCulture));
+				return true;
+			}
+			
+			value = null;
+			return false;
+		}
+		
+		public static bool Convert(object obj, System.Type type, out object value)
+		{
+			if (obj == null)
+			{
+				value = null;
+				
+				if (type.IsValueType)
+				{
+					//	On ne peut pas convertir 'null' en une valeur...
+					
+					return false;
+				}
+				
+				return true;
+			}
+			
+			if (obj.GetType () == type)
+			{
+				//	Le type est déjà correct, on n'a donc rien à faire :
+				
+				value = obj;
+				return true;
+			}
+			
+			if (type.IsValueType)
+			{
+				if (type == typeof (bool))
+				{
+					bool result;
+					
+					if (Converter.Convert (obj, out result))
+					{
+						value = result;
+						return true;
+					}
+				}
+				else if (type == typeof (int))
+				{
+					int result;
+					
+					if (Converter.Convert (obj, out result))
+					{
+						value = result;
+						return true;
+					}
+				}
+				else if (type == typeof (long))
+				{
+					long result;
+					
+					if (Converter.Convert (obj, out result))
+					{
+						value = result;
+						return true;
+					}
+				}
+				else if (type == typeof (decimal))
+				{
+					decimal result;
+					
+					if (Converter.Convert (obj, out result))
+					{
+						value = result;
+						return true;
+					}
+				}
+				else if (type.IsEnum)
+				{
+					System.Enum result;
+					if (Converter.Convert (obj, type, out result))
+					{
+						value = result;
+						return true;
+					}
+				}
+			}
+			else
+			{
+				if (type == typeof (string))
+				{
+					string result;
+					
+					if (Converter.Convert (obj, out result))
+					{
+						value = result;
+						return true;
+					}
+				}
+			}
+			
+			value = null;
+			return false;
+		}
+	}
+}
