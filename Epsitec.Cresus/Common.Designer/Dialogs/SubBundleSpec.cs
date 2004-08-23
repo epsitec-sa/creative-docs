4c6c789043b085e1e7ab9be09c6a53bd62ca2b98
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:14:04 +0000

    ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1275 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Designer/Dialogs/SubBundleSpec.cs
@@ -0,0 +1,172 @@
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+using System.Globalization;
+
+namespace Epsitec.Common.Designer.Dialogs
+{
+	public class SubBundleSpec
+	{
+		public SubBundleSpec()
+		{
+			this.culture = CultureInfo.CurrentUICulture;
+		}
+		
+		public event Support.EventHandler		Changed;
+		
+		protected virtual void OnChanged()
+		{
+			if (this.Changed != null)
+			{
+				this.Changed (this);
+			}
+		}
+		
+		public string							Prefix
+		{
+			get
+			{
+				return this.prefix;
+			}
+		}
+		
+		public ResourceLevel					ResourceLevel
+		{
+			get
+			{
+				return this.level;
+			}
+			set
+			{
+				if (this.level != value)
+				{
+					this.level = value;
+					
+					if (this.container != null)
+					{
+						RadioButton.Activate (this.container, "level", (int) this.level);
+					}
+					
+					this.OnChanged ();
+				}
+			}
+		}
+		
+		public CultureInfo						CultureInfo
+		{
+			get
+			{
+				return this.culture;
+			}
+			set
+			{
+				if (this.culture != value)
+				{
+					this.culture = value;
+					
+					if (this.lang_id != null)
+					{
+						this.lang_id.SelectedName = this.CultureInfo.TwoLetterISOLanguageName;
+					}
+					
+					this.OnChanged ();
+				}
+			}
+		}
+		
+		public double							ExtraHeight
+		{
+			get { return 45; }
+		}
+		
+		
+		public void AddExtraWidgets(Widget container)
+		{
+			System.Diagnostics.Debug.Assert (this.container == null);
+			System.Diagnostics.Debug.Assert (container != null);
+			
+			this.container = container;
+			
+			RadioButton radio;
+			
+			radio = new RadioButton (container);
+			
+			double h = radio.DefaultHeight;
+			
+			radio.Text = "version par <m>d</m>éfaut";
+			radio.Bounds = new Drawing.Rectangle (0, h*2, 140, h);
+			radio.Group  = "level";
+			radio.Index  = (int) ResourceLevel.Default;
+			radio.TabIndex = 10;
+			radio.ActiveStateChanged += new EventHandler (this.HandleRadioActiveStateChanged);
+			
+			radio = new RadioButton (container);
+			radio.Text = "version <m>t</m>raduite, choix de la langue : ";
+			radio.Bounds = new Drawing.Rectangle (0, h*1, 205, h);
+			radio.Group  = "level";
+			radio.Index  = (int) ResourceLevel.Localised;
+			radio.TabIndex = 10;
+			radio.ActiveStateChanged += new EventHandler (this.HandleRadioActiveStateChanged);
+			
+			this.lang_id = new TextFieldCombo (container);
+			this.lang_id.Bounds = new Drawing.Rectangle (radio.Right + 4, radio.Bottom - 4, container.Width - radio.Right - 4, this.lang_id.DefaultHeight);
+			this.lang_id.TabIndex = 11;
+			this.lang_id.Items.Add ("de", "allemand");
+			this.lang_id.Items.Add ("en", "anglais");
+			this.lang_id.Items.Add ("it", "italien");
+			this.lang_id.Items.Add ("fr", "français");
+			this.lang_id.IsReadOnly = true;
+			this.lang_id.SelectedName = this.CultureInfo.TwoLetterISOLanguageName;
+			this.lang_id.SelectedIndexChanged += new Support.EventHandler (this.HandleLangIdSelectedIndexChanged);
+			this.lang_id.Focused += new Support.EventHandler (this.HandleLangIdFocused);
+			
+			radio = new RadioButton (container);
+			radio.Text = "version <m>p</m>ersonnalisée";
+			radio.Bounds = new Drawing.Rectangle (0, h*0, 140, h);
+			radio.Group  = "level";
+			radio.Index  = (int) ResourceLevel.Customised;
+			radio.TabIndex = 10;
+			radio.ActiveStateChanged += new EventHandler (this.HandleRadioActiveStateChanged);
+			
+			RadioButton.Activate (container, "level", (int) this.level);
+		}
+		
+		
+		private void HandleRadioActiveStateChanged(object sender)
+		{
+			RadioButton radio = sender as RadioButton;
+			
+			if ((radio.ActiveState == WidgetState.ActiveYes) &&
+				(radio.Group == "level"))
+			{
+				ResourceLevel level = (ResourceLevel) radio.Index;
+				this.ResourceLevel = level;
+			}
+		}
+		
+		private void HandleLangIdSelectedIndexChanged(object sender)
+		{
+			System.Diagnostics.Debug.Assert (this.lang_id == sender);
+			
+			int    index = this.lang_id.SelectedIndex;
+			string name  = this.lang_id.Items.GetName (index);
+			
+			this.CultureInfo = Resources.FindCultureInfo (name);
+		}
+		
+		private void HandleLangIdFocused(object sender)
+		{
+			System.Diagnostics.Debug.Assert (this.lang_id == sender);
+			
+			RadioButton radio = RadioButton.FindRadio (this.lang_id.Parent, "level", (int) ResourceLevel.Localised);
+			radio.ActiveState = WidgetState.ActiveYes;
+		}
+		
+		
+		protected Widget						container;
+		protected TextFieldCombo				lang_id;
+		
+		protected string						prefix = "file:";
+		protected CultureInfo					culture = null;
+		protected ResourceLevel					level = ResourceLevel.Default;
+	}
+}
