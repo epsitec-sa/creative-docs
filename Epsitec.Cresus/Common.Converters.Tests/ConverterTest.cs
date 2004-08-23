f744e18a031004c49d9920f86af9560ec24bdf17
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:11:17 +0000

    ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1254 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Converters.Tests/ConverterTest.cs
@@ -0,0 +1,95 @@
+using NUnit.Framework;
+
+namespace Epsitec.Common.Converters
+{
+	[TestFixture] public class ConverterTest
+	{
+		[Test] public void CheckIsNull()
+		{
+			object a = null;
+			object b = System.DBNull.Value;
+			
+			Assertion.Assert (Converter.IsNull (a));
+			Assertion.Assert (Converter.IsNull (b));
+			Assertion.Assert (! Converter.IsNotNull (a));
+			Assertion.Assert (! Converter.IsNotNull (b));
+		}
+		
+		[Test] public void CheckToString()
+		{
+			object a = null;
+			object b = System.DBNull.Value;
+			object c = "test";
+			object d = 10;
+			object e = 10.0;
+			object f = 10.00M;
+			object g = true;
+			object h = new Drawing.Color (0.1, 0.2, 0.3, 0.4);
+			object i = new Drawing.Rectangle (10, 20, 30, 40);
+			
+			string result;
+			
+			Assertion.Assert (! Converter.Convert (a, out result));
+			Assertion.Assert (! Converter.Convert (b, out result));
+			
+			Assertion.Assert (Converter.Convert (c, out result));
+			Assertion.AssertEquals ("test", result);
+			Assertion.Assert (Converter.Convert (d, out result));
+			Assertion.AssertEquals ("10", result);
+			Assertion.Assert (Converter.Convert (e, out result));
+			Assertion.AssertEquals ("10", result);
+			Assertion.Assert (Converter.Convert (f, out result));
+			Assertion.AssertEquals ("10.00", result);
+			Assertion.Assert (Converter.Convert (g, out result));
+			Assertion.AssertEquals ("True", result);
+			Assertion.Assert (Converter.Convert (h, out result));
+			Assertion.AssertEquals ("0.1;0.2;0.3;0.4", result);
+			Assertion.Assert (Converter.Convert (i, out result));
+			Assertion.AssertEquals ("10;20;30;40", result);
+		}
+		
+		[Test] public void CheckToDecimal()
+		{
+			object a = null;
+			object b = System.DBNull.Value;
+			object c = "";
+			object d = 10;
+			object e = 10.0;
+			object f = 10.00M;
+			object g = true;
+			object h = "10";
+			object i = "10.00";
+			
+			decimal result;
+			
+			Assertion.Assert (! Converter.Convert (a, out result));
+			Assertion.Assert (! Converter.Convert (b, out result));
+			Assertion.Assert (! Converter.Convert (c, out result));
+			
+			Assertion.Assert (Converter.Convert (d, out result));
+			Assertion.Assert (result == 10);
+			Assertion.Assert (Converter.Convert (e, out result));
+			Assertion.Assert (result == 10);
+			Assertion.Assert (Converter.Convert (f, out result));
+			Assertion.Assert (result == 10);
+			Assertion.Assert (Converter.Convert (g, out result));
+			Assertion.Assert (result == 1);
+			Assertion.Assert (Converter.Convert (h, out result));
+			Assertion.Assert (result == 10);
+			Assertion.Assert (Converter.Convert (i, out result));
+			Assertion.Assert (result == 10);
+		}
+		
+		[Test] [ExpectedException (typeof (System.FormatException))] public void CheckToDecimalEx1()
+		{
+			decimal result;
+			Converter.Convert ("X", out result);
+		}
+		
+		[Test] [ExpectedException (typeof (System.NotSupportedException))] public void CheckToDecimalEx2()
+		{
+			decimal result;
+			Converter.Convert (new Drawing.Rectangle (10, 20, 30, 40), out result);
+		}
+	}
+}
