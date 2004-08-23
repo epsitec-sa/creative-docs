f95b711a7c6710006c33246851b4825fa30edc8d
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:34:49 +0000

    ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:
    **********************
    Label: "Stable-15-03-2004"
    User: Arnaud       Date: 15.03.04   Time: 11:30
    Labeled
    Label comment: EditArray et App.Designer bien avancés.
    
    **********************
    Label: "12-02-2004"
    User: Arnaud       Date: 13.02.04   Time:  9:42
    Labeled
    Label comment: ScrollArray reworked.
    
    **********************
    Label: "11-02-2004"
    User: Arnaud       Date: 12.02.04   Time:  7:47
    Labeled
    Label comment: Stable version.
    
    
    **********************
    Label: "22-01-2004"
    User: Arnaud       Date: 22.01.04   Time: 15:38
    Labeled
    Label comment: Object re-bundling implemented.
    
    **********************
    Label: "21-01-2004"
    User: Arnaud       Date: 21.01.04   Time: 18:14
    Labeled
    Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.
    
    
    **********************
    Label: "19-01-2004"
    User: Arnaud       Date: 19.01.04   Time: 16:24
    Labeled
    Label comment: Before in-depth modification of XML handling in ResourceBundle.
    
    **********************
    Label: "13-01-2004"
    User: Arnaud       Date: 13.01.04   Time: 20:50
    Labeled
    Label comment: Stable, before rework of Layout management.
    
    git-svn-id: https://svn.opac.ch/svn/cr@1476 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Widgets/AssemblyInfo.cs
+++ Epsitec.Cresus/Common.Widgets/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EPSITEC SA")]
 [assembly: AssemblyProduct("Crésus")]
-[assembly: AssemblyCopyright("Copyright © 2003, EPSITEC SA, CH-1092 BELMONT, Switzerland")]
+[assembly: AssemblyCopyright("Copyright © 2003-2004, EPSITEC SA, CH-1092 BELMONT, Switzerland")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -26,7 +26,13 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.*")]
+
+// Function name   : AssemblyVersion
+// Description     : 
+// Return type     : 
+// Argument        : "1.0.*"
+
+[assembly: AssemblyVersion("1.0.0.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
