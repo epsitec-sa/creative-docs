46ea1af96fddd11f03c8cf1b78da8c8dab511937
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 12:30:29 +0000

    git-svn-id: https://svn.opac.ch/svn/cr@273 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Support/Tags.cs
@@ -0,0 +1,15 @@
+//	Copyright © 2003, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : OK/PA, 19/10/2003
+
+namespace Epsitec.Common.Support
+{
+	public class Tags
+	{
+		public const string	Id			= "id";
+		public const string	Name		= "name";
+		public const string	Binding		= "bind";
+		public const string	Data		= "data";
+		public const string	Caption		= "capt";
+		public const string	Description	= "desc";
+	}
+}
