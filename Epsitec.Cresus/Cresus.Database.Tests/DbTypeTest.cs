c3c320cb93b022500397d6f4afd3b473d63f43dd
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:25:32 +0000

    git-svn-id: https://svn.opac.ch/svn/cr@1972 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Database.Tests/DbTypeTest.cs
+++ Epsitec.Cresus/Cresus.Database.Tests/DbTypeTest.cs
@@ -20,29 +20,29 @@ namespace Epsitec.Cresus.Database
 			DbTypeEnum e_type = new DbTypeEnum (list);
 			DbTypeEnum e_copy = e_type.Clone () as DbTypeEnum;
 			
-			Assertion.AssertEquals ("M", e_type.Values[0].Name);
-			Assertion.AssertEquals ("M", e_type[1].Name);
-			Assertion.AssertEquals ("M", e_type["M"].Name);
-			Assertion.AssertEquals (1, e_type.Values[0].Rank);
-			Assertion.AssertEquals (1, e_type["M"].Rank);
+			Assert.AreEqual ("M", e_type.Values[0].Name);
+			Assert.AreEqual ("M", e_type[1].Name);
+			Assert.AreEqual ("M", e_type["M"].Name);
+			Assert.AreEqual (1, e_type.Values[0].Rank);
+			Assert.AreEqual (1, e_type["M"].Rank);
 			
-			Assertion.AssertEquals ("MME", e_copy.Values[1].Name);
-			Assertion.AssertEquals ("MME", e_copy[2].Name);
-			Assertion.AssertEquals ("MME", e_copy["MME"].Name);
-			Assertion.AssertEquals (2, e_copy.Values[1].Rank);
-			Assertion.AssertEquals (2, e_copy["MME"].Rank);
+			Assert.AreEqual ("MME", e_copy.Values[1].Name);
+			Assert.AreEqual ("MME", e_copy[2].Name);
+			Assert.AreEqual ("MME", e_copy["MME"].Name);
+			Assert.AreEqual (2, e_copy.Values[1].Rank);
+			Assert.AreEqual (2, e_copy["MME"].Rank);
 			
-			Assertion.AssertEquals ("MLLE", e_copy.Values[2].Name);
-			Assertion.AssertEquals ("MLLE", e_copy[3].Name);
-			Assertion.AssertEquals ("MLLE", e_copy["MLLE"].Name);
-			Assertion.AssertEquals (3, e_copy.Values[2].Rank);
-			Assertion.AssertEquals (3, e_copy["MLLE"].Rank);
+			Assert.AreEqual ("MLLE", e_copy.Values[2].Name);
+			Assert.AreEqual ("MLLE", e_copy[3].Name);
+			Assert.AreEqual ("MLLE", e_copy["MLLE"].Name);
+			Assert.AreEqual (3, e_copy.Values[2].Rank);
+			Assert.AreEqual (3, e_copy["MLLE"].Rank);
 			
-			Assertion.AssertNull (e_type["X"]);
+			Assert.IsNull (e_type["X"]);
 			
-			Assertion.AssertEquals ("Monsieur", e_type["M"].Caption);
-			Assertion.AssertEquals ("Madame", e_type["MME"].Caption);
-			Assertion.AssertEquals ("Mademoiselle", e_type["MLLE"].Caption);
+			Assert.AreEqual ("Monsieur", e_type["M"].Caption);
+			Assert.AreEqual ("Madame", e_type["MME"].Caption);
+			Assert.AreEqual ("Mademoiselle", e_type["MLLE"].Caption);
 		}
 		
 		[Test] public void CheckDbTypeEnumXml()
@@ -62,14 +62,14 @@ namespace Epsitec.Cresus.Database
 			string xml_p = DbEnumValue.SerializeToXml (ev_1, false);
 			string xml_f = DbEnumValue.SerializeToXml (ev_1, true);
 			
-			Assertion.AssertEquals (@"<enumval rank=""2""/>", xml_p);
-			Assertion.AssertEquals (@"<enumval rank=""2"" attr.capt=""Madame"" attr.name=""MME""/>", xml_f);
+			Assert.AreEqual (@"<enumval rank=""2""/>", xml_p);
+			Assert.AreEqual (@"<enumval rank=""2"" attr.capt=""Madame"" attr.name=""MME""/>", xml_f);
 			
 			xml_p = DbEnumValue.SerializeToXml (ev_2, false);
 			xml_f = DbEnumValue.SerializeToXml (ev_2, true);
 			
-			Assertion.AssertEquals (@"<enumval rank=""3""/>", xml_p);
-			Assertion.AssertEquals (@"<enumval rank=""3"" attr.capt=""&gt;Mademoiselle&lt;"" attr.name=""MLLE""/>", xml_f);
+			Assert.AreEqual (@"<enumval rank=""3""/>", xml_p);
+			Assert.AreEqual (@"<enumval rank=""3"" attr.capt=""&gt;Mademoiselle&lt;"" attr.name=""MLLE""/>", xml_f);
 			
 			string full_xml = DbTypeFactory.SerializeToXml (e_type, true);
 			System.Console.Out.WriteLine ("XML: {0}", full_xml);
@@ -78,37 +78,37 @@ namespace Epsitec.Cresus.Database
 			DbEnumValue ev_2x = DbEnumValue.CreateEnumValue (DbEnumValue.SerializeToXml (ev_2, true));
 			DbEnumValue ev_3x = DbEnumValue.CreateEnumValue (DbEnumValue.SerializeToXml (ev_3, true));
 			
-			Assertion.AssertEquals (ev_1.Name,    ev_1x.Name);
-			Assertion.AssertEquals (ev_1.Caption, ev_1x.Caption);
-			Assertion.AssertEquals (ev_1.Rank,    ev_1x.Rank);
+			Assert.AreEqual (ev_1.Name,    ev_1x.Name);
+			Assert.AreEqual (ev_1.Caption, ev_1x.Caption);
+			Assert.AreEqual (ev_1.Rank,    ev_1x.Rank);
 			
-			Assertion.AssertEquals (ev_2.Name,    ev_2x.Name);
-			Assertion.AssertEquals (ev_2.Caption, ev_2x.Caption);
-			Assertion.AssertEquals (ev_2.Rank,    ev_2x.Rank);
+			Assert.AreEqual (ev_2.Name,    ev_2x.Name);
+			Assert.AreEqual (ev_2.Caption, ev_2x.Caption);
+			Assert.AreEqual (ev_2.Rank,    ev_2x.Rank);
 			
-			Assertion.AssertEquals (ev_3.Name,    ev_3x.Name);
-			Assertion.AssertEquals (ev_3.Caption, ev_3x.Caption);
-			Assertion.AssertEquals (ev_3.Rank,    ev_3x.Rank);
+			Assert.AreEqual (ev_3.Name,    ev_3x.Name);
+			Assert.AreEqual (ev_3.Caption, ev_3x.Caption);
+			Assert.AreEqual (ev_3.Rank,    ev_3x.Rank);
 			
 			DbTypeEnum copy = DbTypeFactory.CreateType (full_xml) as DbTypeEnum;
 			
-			Assertion.AssertNotNull (copy);
-			Assertion.AssertEquals (3, copy.Count);
-			Assertion.AssertEquals (ev_1.Name,    copy["MME"] .Name);
-			Assertion.AssertEquals (ev_1.Rank,    copy["MME"] .Rank);
-			Assertion.AssertEquals (ev_1.Caption, copy["MME"] .Caption);
-			Assertion.AssertEquals (ev_2.Name,    copy["MLLE"].Name);
-			Assertion.AssertEquals (ev_2.Rank,    copy["MLLE"].Rank);
-			Assertion.AssertEquals (ev_2.Caption, copy["MLLE"].Caption);
-			Assertion.AssertEquals (ev_3.Name,    copy["M"]   .Name);
-			Assertion.AssertEquals (ev_3.Rank,    copy["M"]   .Rank);
-			Assertion.AssertEquals (ev_3.Caption, copy["M"]   .Caption);
+			Assert.IsNotNull (copy);
+			Assert.AreEqual (3, copy.Count);
+			Assert.AreEqual (ev_1.Name,    copy["MME"] .Name);
+			Assert.AreEqual (ev_1.Rank,    copy["MME"] .Rank);
+			Assert.AreEqual (ev_1.Caption, copy["MME"] .Caption);
+			Assert.AreEqual (ev_2.Name,    copy["MLLE"].Name);
+			Assert.AreEqual (ev_2.Rank,    copy["MLLE"].Rank);
+			Assert.AreEqual (ev_2.Caption, copy["MLLE"].Caption);
+			Assert.AreEqual (ev_3.Name,    copy["M"]   .Name);
+			Assert.AreEqual (ev_3.Rank,    copy["M"]   .Rank);
+			Assert.AreEqual (ev_3.Caption, copy["M"]   .Caption);
 			
 			for (int i = 0; i < 3; i++)
 			{
-				Assertion.AssertEquals (e_type.Values[i].Name,    copy.Values[i].Name);
-				Assertion.AssertEquals (e_type.Values[i].Rank,    copy.Values[i].Rank);
-				Assertion.AssertEquals (e_type.Values[i].Caption, copy.Values[i].Caption);
+				Assert.AreEqual (e_type.Values[i].Name,    copy.Values[i].Name);
+				Assert.AreEqual (e_type.Values[i].Rank,    copy.Values[i].Rank);
+				Assert.AreEqual (e_type.Values[i].Caption, copy.Values[i].Caption);
 			}
 		}
 		
@@ -138,7 +138,7 @@ namespace Epsitec.Cresus.Database
 				System.Xml.XmlDocument doc = new System.Xml.XmlDocument ();
 				doc.LoadXml (buffer.ToString ());
 				System.Xml.XmlElement  xml = doc.DocumentElement;
-				Assertion.AssertEquals (k0, DbKey.DeserializeFromXmlAttributes (xml));
+				Assert.AreEqual (k0, DbKey.DeserializeFromXmlAttributes (xml));
 			}
 			{
 				System.Text.StringBuilder buffer = new System.Text.StringBuilder ();
@@ -148,7 +148,7 @@ namespace Epsitec.Cresus.Database
 				System.Xml.XmlDocument doc = new System.Xml.XmlDocument ();
 				doc.LoadXml (buffer.ToString ());
 				System.Xml.XmlElement  xml = doc.DocumentElement;
-				Assertion.AssertEquals (k1, DbKey.DeserializeFromXmlAttributes (xml));
+				Assert.AreEqual (k1, DbKey.DeserializeFromXmlAttributes (xml));
 			}
 			{
 				System.Text.StringBuilder buffer = new System.Text.StringBuilder ();
@@ -158,7 +158,7 @@ namespace Epsitec.Cresus.Database
 				System.Xml.XmlDocument doc = new System.Xml.XmlDocument ();
 				doc.LoadXml (buffer.ToString ());
 				System.Xml.XmlElement  xml = doc.DocumentElement;
-				Assertion.AssertEquals (k2, DbKey.DeserializeFromXmlAttributes (xml));
+				Assert.AreEqual (k2, DbKey.DeserializeFromXmlAttributes (xml));
 			}
 			{
 				System.Text.StringBuilder buffer = new System.Text.StringBuilder ();
@@ -168,7 +168,7 @@ namespace Epsitec.Cresus.Database
 				System.Xml.XmlDocument doc = new System.Xml.XmlDocument ();
 				doc.LoadXml (buffer.ToString ());
 				System.Xml.XmlElement  xml = doc.DocumentElement;
-				Assertion.AssertEquals (k3, DbKey.DeserializeFromXmlAttributes (xml));
+				Assert.AreEqual (k3, DbKey.DeserializeFromXmlAttributes (xml));
 			}
 		}
 	}
