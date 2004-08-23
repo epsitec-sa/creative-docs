c8b0af1ecebd333514b67ee9dd0a012d2e9185af
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 12:31:41 +0000

    ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@290 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Database.Tests/DbColumnTest.cs
+++ Epsitec.Cresus/Cresus.Database.Tests/DbColumnTest.cs
@@ -5,20 +5,52 @@ namespace Epsitec.Cresus.Database
 	[TestFixture]
 	public class DbColumnTest
 	{
+		[Test] public void CheckNewDbColumn()
+		{
+			DbColumn column_a = new DbColumn ("A", DbNumDef.FromRawType (DbRawType.SmallDecimal), Nullable.Yes);
+			DbColumn column_b = new DbColumn ("B", DbSimpleType.Guid);
+			DbColumn column_c = new DbColumn ("C", DbSimpleType.String, 100, true);
+			
+			Assertion.AssertEquals ("A", column_a.Name);
+			Assertion.AssertEquals (DbSimpleType.Decimal, column_a.SimpleType);
+			Assertion.AssertEquals (true, column_a.IsNullAllowed);
+			
+			Assertion.AssertEquals ("B", column_b.Name);
+			Assertion.AssertEquals (DbSimpleType.Guid, column_b.SimpleType);
+			
+			Assertion.AssertEquals ("C", column_c.Name);
+			Assertion.AssertEquals (DbSimpleType.String, column_c.SimpleType);
+			Assertion.AssertEquals (100, column_c.Length);
+			Assertion.AssertEquals (true, column_c.IsFixedLength);
+		}
+		
+		[Test] [Ignore ("Not implemented")] public void CheckNewDbColumnByteArray()
+		{
+			DbColumn column_d = new DbColumn ("D", DbSimpleType.ByteArray, 10, false);
+			Assertion.AssertEquals ("D", column_d.Name);
+			Assertion.AssertEquals (DbSimpleType.ByteArray, column_d.SimpleType);
+			Assertion.AssertEquals (10, column_d.Length);
+			Assertion.AssertEquals (false, column_d.IsFixedLength);
+		}
+		
+		[Test] public void CheckTypes()
+		{
+			DbColumn column_a = new DbColumn ("A", DbNumDef.FromRawType (DbRawType.Int32));
+			DbColumn column_b = new DbColumn ("B", DbSimpleType.String, 100, true);
+			DbColumn column_c = new DbColumn ("C", DbSimpleType.Time);
+			
+			Assertion.Assert (column_a.Type.GetType () == typeof (DbType));
+			Assertion.Assert (column_b.Type.GetType () == typeof (DbTypeString));
+			Assertion.Assert (column_c.Type.GetType () == typeof (DbType));
+		}
+		
 		[Test] public void CheckCreateSqlColumn()
 		{
 			IDbAbstraction db_abstraction = DbFactoryTest.CreateDbAbstraction (true);
 			ITypeConverter type_converter = db_abstraction.Factory.TypeConverter;
 			
-			DbColumn column_a = new DbColumn ();
-			DbColumn column_b = new DbColumn ();
-			
-			column_a.Name = "A";
-			column_a.SetType (DbSimpleType.Decimal, DbNumDef.FromRawType (DbRawType.SmallDecimal));
-			column_a.IsNullAllowed = true;
-			
-			column_b.Name = "B";
-			column_b.SetType (DbSimpleType.Guid);
+			DbColumn column_a = new DbColumn ("A", DbNumDef.FromRawType (DbRawType.SmallDecimal), Nullable.Yes);
+			DbColumn column_b = new DbColumn ("B", DbSimpleType.Guid);
 			
 			SqlColumn sql_a = column_a.CreateSqlColumn (type_converter);
 			SqlColumn sql_b = column_b.CreateSqlColumn (type_converter);
