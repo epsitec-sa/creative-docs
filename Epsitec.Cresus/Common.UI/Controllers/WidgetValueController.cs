d5ddaea3c819dc9bbd80abad1d88db107c1830b8
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:21:36 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1923 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.UI/Controllers/WidgetValueController.cs
@@ -0,0 +1,114 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : en chantier/PA, 27/04/2004
+
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+
+namespace Epsitec.Common.UI.Controllers
+{
+	/// <summary>
+	/// La classe WidgetValueController réalise un contrôleur très simple qui
+	/// s'appuie sur un widget existant et interagit avec sa propriété Text.
+	/// </summary>
+	public class WidgetValueController : AbstractConstrainedController
+	{
+		public WidgetValueController()
+		{
+		}
+		
+		public WidgetValueController(Adapters.IAdapter adapter) : this ()
+		{
+			this.Adapter = adapter;
+		}
+		
+		public WidgetValueController(Adapters.IAdapter adapter, Widget widget) : this ()
+		{
+			this.Adapter = adapter;
+			this.CreateUI (widget);
+		}
+		
+		public WidgetValueController(Adapters.IAdapter adapter, Widget widget, Types.IDataConstraint constraint, Types.INum num_type) : this ()
+		{
+			this.Adapter    = adapter;
+			this.Constraint = constraint;
+			this.NumType    = num_type;
+			this.CreateUI (widget);
+		}
+		
+		
+		public Types.INum						NumType
+		{
+			get
+			{
+				return this.num_type;
+			}
+			set
+			{
+				this.num_type = value;
+			}
+		}
+		
+		
+		public override void CreateUI(Widget widget)
+		{
+			Support.Data.INumValue num_value = widget as Support.Data.INumValue;
+			
+			if (num_value == null)
+			{
+				throw new System.ArgumentException ("The specified widget does not conform to INumValue interface.", "widget");
+			}
+			
+			this.widget    = widget;
+			this.num_value = num_value;
+			
+			if (this.num_type != null)
+			{
+				this.num_value.MinValue   = this.NumType.Range.Minimum;
+				this.num_value.MaxValue   = this.NumType.Range.Maximum;
+				this.num_value.Resolution = this.NumType.Range.Resolution;
+			}
+			
+			this.num_value.ValueChanged += new EventHandler (this.HandleWidgetValueChanged);
+			
+			this.SyncFromAdapter ();
+		}
+		
+		public override void SyncFromAdapter()
+		{
+			Adapters.DecimalAdapter adapter = this.Adapter as Adapters.DecimalAdapter;
+			
+			if ((adapter != null) &&
+				(this.num_value != null))
+			{
+				this.num_value.Value = adapter.Value;
+			}
+		}
+		
+		public override void SyncFromUI()
+		{
+			Adapters.DecimalAdapter adapter = this.Adapter as Adapters.DecimalAdapter;
+			
+			if ((adapter != null) &&
+				(this.num_value != null))
+			{
+				decimal value = this.num_value.Value;
+				
+				if (this.CheckConstraint (value))
+				{
+					adapter.Value = value;
+				}
+			}
+		}
+		
+		
+		private void HandleWidgetValueChanged(object sender)
+		{
+			this.SyncFromUI ();
+		}
+		
+		
+		private Widget							widget;
+		private Support.Data.INumValue			num_value;
+		private Types.INum						num_type;
+	}
+}
