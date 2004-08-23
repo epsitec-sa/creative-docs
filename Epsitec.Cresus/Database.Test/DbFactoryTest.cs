276f9863dbab234890d81f7d42344abac9e32f96
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 11:54:03 +0000

    git-svn-id: https://svn.opac.ch/svn/cr@90 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Database.Test/DbFactoryTest.cs
+++ Epsitec.Cresus/Database.Test/DbFactoryTest.cs
@@ -8,19 +8,110 @@ namespace Epsitec.Cresus.Database
 		[SetUp]
 		public void LoadAssemblies()
 		{
-			try
-			{
-				System.Diagnostics.Debug.WriteLine ("");
-			}
-			catch
-			{
-			}
-			
 			DbFactory.Initialise ();
 		}
 		
 		[Test]
-		public void CheckFindDbAbstraction()
+		public void CheckFindDbAbstractionAndOpenClose()
+		{
+			IDbAbstraction db_abstraction = this.CreateDbAbstraction (true);
+			
+			Assertion.AssertNotNull ("Could not instanciate Firebird abstraction", db_abstraction);
+			Assertion.AssertNotNull ("Cannot retrieve the factory", db_abstraction.Factory);
+			
+			Assertion.AssertEquals ("Connection should not be alive", db_abstraction.IsConnectionAlive, false);
+			Assertion.AssertEquals ("Connection should not be open", db_abstraction.IsConnectionOpen, false);
+			
+			db_abstraction.Connection.Open ();
+			
+			Assertion.AssertEquals ("Connection should be alive", db_abstraction.IsConnectionAlive, true);
+			Assertion.AssertEquals ("Connection should be open", db_abstraction.IsConnectionOpen, true);
+			
+			db_abstraction.Connection.Close ();
+		}
+		
+		[Test]
+		public void CheckNewDbCommand()
+		{
+			IDbAbstraction  db_abstraction = this.CreateDbAbstraction (false);
+			System.Data.IDbCommand command = db_abstraction.NewDbCommand ();
+			
+			Assertion.AssertNotNull (command);
+		}
+		
+		[Test]
+		public void CheckNewDataAdapter()
+		{
+			IDbAbstraction  db_abstraction = this.CreateDbAbstraction (false);
+			
+			db_abstraction.Connection.Open ();
+			
+			System.Data.IDbCommand   command = db_abstraction.NewDbCommand ();
+			System.Data.IDataAdapter adapter = db_abstraction.NewDataAdapter (command);
+			
+			db_abstraction.Connection.Close ();
+			
+			Assertion.AssertNotNull (adapter);
+		}
+		
+		[Test]
+		public void CheckExecuteScalar()
+		{
+			IDbAbstraction  db_abstraction = this.CreateDbAbstraction (false);
+			
+			db_abstraction.Connection.Open ();
+			
+			System.Data.IDbCommand   command = db_abstraction.NewDbCommand ();
+			
+			command.CommandType = System.Data.CommandType.Text;
+			command.CommandText = "SELECT CURRENT_TIMESTAMP FROM RDB$DATABASE;";
+			
+			object result = command.ExecuteScalar ();
+			
+			System.Console.Out.WriteLine ("Result: " + result.ToString () + ", type is " + result.GetType ().ToString ());
+			
+			command.Dispose ();
+			db_abstraction.Connection.Close ();
+		}
+		
+		[Test]
+		public void CheckExecuteReader()
+		{
+			IDbAbstraction  db_abstraction = this.CreateDbAbstraction (false);
+			
+			db_abstraction.Connection.Open ();
+			
+			System.Data.IDbCommand   command = db_abstraction.NewDbCommand ();
+			
+			command.CommandType = System.Data.CommandType.Text;
+			command.CommandText = "SELECT * FROM RDB$DATABASE;";
+			
+			System.Data.IDataAdapter adapter = db_abstraction.NewDataAdapter (command);
+//			FbDataAdapter fb_adapter = adapter as FbDataAdapter;
+			
+			System.Data.DataSet data_set = new System.Data.DataSet ();
+			int rows = adapter.Fill (data_set);
+			
+			System.Console.Out.WriteLine ("Rows returned: " + rows.ToString ());
+			
+			foreach (System.Data.DataTable table in data_set.Tables)
+			{
+				foreach (System.Data.DataColumn column in table.Columns)
+				{
+					System.Console.Out.WriteLine ("Col: " + column.ColumnName + ", type " + column.DataType.ToString ());
+				}
+			}
+			
+			db_abstraction.Connection.Close ();
+		}
+		
+		[Test]
+		public void CheckDebugDumpRegisteredDbAbstractions ()
+		{
+			DbFactory.DebugDumpRegisteredDbAbstractions ();
+		}
+		
+		protected DbAccess CreateDbAccess(bool force_db_creation)
 		{
 			DbAccess db_access = new DbAccess ();
 			
@@ -29,17 +120,31 @@ namespace Epsitec.Cresus.Database
 			db_access.login_pwd  = "masterkey";
 			db_access.database   = "test";
 			db_access.server     = "localhost";
-			db_access.create     = true;
+			db_access.create     = force_db_creation;
 			
-			IDbAbstraction db_abstraction = DbFactory.FindDbAbstraction (db_access);
-			
-			Assertion.AssertNotNull ("Could not instanciate Firebird abstraction", db_abstraction);
+			return db_access;
 		}
 		
-		[Test]
-		public void CheckDebugDumpRegisteredDbAbstractions ()
+		protected IDbAbstraction CreateDbAbstraction(bool force_db_creation)
 		{
-			DbFactory.DebugDumpRegisteredDbAbstractions ();
+			IDbAbstraction db_abstraction = null;
+			DbAccess db_access = this.CreateDbAccess (force_db_creation);
+			
+			try
+			{
+				db_abstraction = DbFactory.FindDbAbstraction (db_access);
+			}
+			catch (DbExistsException ex)
+			{
+				Assertion.AssertNotNull (ex);
+				
+				//	OK: la base existait déjà... on ne peut pas la créer !
+				
+				db_access.create = false;
+				db_abstraction = DbFactory.FindDbAbstraction (db_access);
+			}
+			
+			return db_abstraction;
 		}
 	}
 }
