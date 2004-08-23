c8b4912632c00793804d1fab77fa1a18866544eb
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:14:08 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:
    **********************
    Label: "Stable-01-04-2004"
    User: Arnaud       Date:  1.04.04   Time: 12:42
    Labeled
    Label comment: TabIndex edition working in GUI.
    
    **********************
    Label: "Stable-24-03-2004"
    User: Arnaud       Date: 24.03.04   Time: 22:47
    Labeled
    Label comment: With DR contributions. First draft of Common.UI.
    
    git-svn-id: https://svn.opac.ch/svn/cr@2398 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/AbstractController.cs
+++ Epsitec.Cresus/Common.Designer/AbstractController.cs
@@ -1,18 +1,66 @@
 //	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
-//	Statut : en chantier/PA
+//	Responsable: Pierre ARNAUD
 
 using Epsitec.Common.Widgets;
 using Epsitec.Common.Support;
-using System.Globalization;
 
 namespace Epsitec.Common.Designer
 {
 	/// <summary>
 	/// La classe AbstractController sert de base pour tous les contrôleurs.
 	/// </summary>
-	public abstract class AbstractController
+	public abstract class AbstractController : Support.ICommandDispatcherHost, System.IDisposable
 	{
-		protected AbstractController()
+		public AbstractController(Application application)
+		{
+			this.application = application;
+			this.dispatcher  = application.CommandDispatcher;
+			
+			//	Enregistre toutes les commandes exportées par cette classe (en particulier
+			//	celles de la classe dérivée).
+			
+			this.dispatcher.RegisterController (this);
+		}
+		
+		
+		public Application						Application
+		{
+			get
+			{
+				return this.application;
+			}
+		}
+		
+		public Support.CommandDispatcher		CommandDispatcher
+		{
+			get
+			{
+				return this.dispatcher;
+			}
+			set
+			{
+				if (this.dispatcher != value)
+				{
+					throw new System.InvalidOperationException ("CommandDispatcher may not be changed.");
+				}
+			}
+		}
+		
+		
+		public virtual void Initialise()
+		{
+		}
+		
+		
+		#region IDisposable Members
+		public void Dispose()
+		{
+			this.Dispose (true);
+			System.GC.SuppressFinalize (this);
+		}
+		#endregion
+		
+		protected virtual void Dispose(bool disposing)
 		{
 		}
 		
@@ -23,5 +71,9 @@ namespace Epsitec.Common.Designer
 			
 			throw new System.InvalidOperationException (string.Format ("Command {0} requires {1} argument(s), got {2}.", command, num_expected, num_found));
 		}
+		
+		
+		protected Support.CommandDispatcher		dispatcher;
+		protected Application					application;
 	}
 }
