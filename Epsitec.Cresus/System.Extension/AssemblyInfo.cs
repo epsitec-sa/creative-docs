66e2a62c7555e0d0dc9e1d1cfef7f9b013ef2f84
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 12:28:49 +0000

    ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@258 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/System.Extension/AssemblyInfo.cs
+++ Epsitec.Cresus/System.Extension/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("More System Types")]
+[assembly: AssemblyTitle("More System Types and Utilities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EPSITEC SA")]
