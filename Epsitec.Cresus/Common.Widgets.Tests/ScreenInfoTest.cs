f4c632be9ca5f17843053772331101b5e3e39de5
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:56:34 +0000

    ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.
    **********************
    Label: "Stable-24-03-2004"
    User: Arnaud       Date: 24.03.04   Time: 22:47
    Labeled
    Label comment: With DR contributions. First draft of Common.UI.
    
    **********************
    Label: "Stable-20-03-2004"
    User: Arnaud       Date: 20.03.04   Time: 12:18
    Labeled
    Label comment: 
    
    **********************
    Label: "Stable-15-03-2004"
    User: Arnaud       Date: 15.03.04   Time: 11:30
    Labeled
    Label comment: EditArray et App.Designer bien avancés.
    
    **********************
    Label: "12-02-2004"
    User: Arnaud       Date: 13.02.04   Time:  9:42
    Labeled
    Label comment: ScrollArray reworked.
    
    **********************
    Label: "11-02-2004"
    User: Arnaud       Date: 12.02.04   Time:  7:47
    Labeled
    Label comment: Stable version.
    
    
    **********************
    Label: "22-01-2004"
    User: Arnaud       Date: 22.01.04   Time: 15:38
    Labeled
    Label comment: Object re-bundling implemented.
    
    **********************
    Label: "21-01-2004"
    User: Arnaud       Date: 21.01.04   Time: 18:14
    Labeled
    Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.
    
    
    **********************
    Label: "19-01-2004"
    User: Arnaud       Date: 19.01.04   Time: 16:24
    Labeled
    Label comment: Before in-depth modification of XML handling in ResourceBundle.
    
    **********************
    Label: "13-01-2004"
    User: Arnaud       Date: 13.01.04   Time: 20:50
    Labeled
    Label comment: Stable, before rework of Layout management.
    
    **********************
    Label: "12.12.2003"
    User: Arnaud       Date: 13.12.03   Time: 18:39
    Labeled
    Label comment: 
    
    **********************
    Label: "08-12-2003"
    User: Arnaud       Date:  8.12.03   Time: 15:25
    Labeled
    Label comment: Stable version.
    
    git-svn-id: https://svn.opac.ch/svn/cr@1652 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Widgets.Tests/ScreenInfoTest.cs
+++ Epsitec.Cresus/Common.Widgets.Tests/ScreenInfoTest.cs
@@ -54,5 +54,335 @@ namespace Epsitec.Common.Widgets
 				System.Console.Out.WriteLine ();
 			}
 		}
+		
+		[Test] public void CheckGrabScreen()
+		{
+			Window window = new Window ();
+			
+			window.MakeTopLevelWindow ();
+			window.MakeFramelessWindow ();
+			window.MakeLayeredWindow ();
+			
+			window.ClientSize = new Drawing.Size (110, 110);
+			window.Root.BackColor = Drawing.Color.Transparent;
+			
+			Glass glass = new Glass ();
+			
+			glass.Dock   = DockStyle.Fill;
+			glass.Parent = window.Root;
+			glass.SetFocused (true);
+			
+			glass.HotColorChanged += new Support.EventHandler (this.HandleGlassHotColorChanged);
+			
+			window.Show ();
+		}
+		
+		
+		
+		private class Glass : Widget
+		{
+			public Glass()
+			{
+				this.bitmap = Drawing.Bitmap.FromNativeBitmap (11, 11);
+				this.scale  = 11;
+			}
+			
+			
+			public Drawing.Color				HotColor
+			{
+				get
+				{
+					return this.hot_color;
+				}
+				
+				set
+				{
+					if (this.hot_color != value)
+					{
+						this.hot_color = value;
+						this.OnHotColorChanged ();
+					}
+				}
+			}
+			
+			
+			protected override void ProcessMessage(Message message, Point pos)
+			{
+				switch (message.Type)
+				{
+					case MessageType.MouseDown:
+						if (message.IsLeftButton)
+						{
+							this.is_dragging = true;
+							this.origin      = this.MapClientToScreen (pos);
+							MouseCursor.Hide ();
+							message.Captured = true;
+						}
+						break;
+					
+					case MessageType.MouseMove:
+						if (this.is_dragging)
+						{
+							pos = this.MapClientToScreen (pos);
+							
+							this.Window.WindowLocation += pos - this.origin;
+							this.Invalidate ();
+							
+							this.origin = pos;
+						}
+						break;
+				
+					case MessageType.MouseUp:
+						MouseCursor.Show ();
+						this.is_dragging = false;
+						break;
+					
+					case MessageType.MouseWheel:
+						int wheel = message.Wheel < 0 ? -2 : 2;
+						int scale = this.scale + wheel;
+						
+						if (scale < 5)  scale = 5;
+						if (scale > 21) scale = 21;
+						
+						double dx = this.Client.Width;
+						double dy = this.Client.Height;
+						
+						this.bitmap = Drawing.Bitmap.FromNativeBitmap (scale, scale);
+						this.scale  = scale;
+						this.Invalidate ();
+						break;
+					
+					case MessageType.KeyDown:
+						switch (message.KeyCode)
+						{
+							case KeyCode.ArrowUp:
+								this.Window.WindowLocation += new Drawing.Point (0, 1);
+								this.Invalidate ();
+								break;
+							
+							case KeyCode.ArrowDown:
+								this.Window.WindowLocation += new Drawing.Point (0, -1);
+								this.Invalidate ();
+								break;
+							
+							case KeyCode.ArrowLeft:
+								this.Window.WindowLocation += new Drawing.Point (-1, 0);
+								this.Invalidate ();
+								break;
+							
+							case KeyCode.ArrowRight:
+								this.Window.WindowLocation += new Drawing.Point (1, 0);
+								this.Invalidate ();
+								break;
+						}
+						break;
+					
+					default:
+						return;
+				}
+				
+				message.Consumer = this;
+			}
+
+			
+			public override bool AboutToLoseFocus(Widget.TabNavigationDir dir, Widget.TabNavigationMode mode)
+			{
+				return false;
+			}
+
+			
+			protected override void Dispose(bool disposing)
+			{
+				if (disposing)
+				{
+					if (this.timer != null)
+					{
+						this.timer.Stop ();
+						this.timer.Dispose ();
+						this.timer = null;
+					}
+				}
+				
+				base.Dispose (disposing);
+			}
+
+			
+			protected override void PaintBackgroundImplementation(Graphics graphics, Rectangle clip_rect)
+			{
+				if (this.timer == null)
+				{
+					this.timer = new Timer ();
+					this.timer.AutoRepeat = 0.1;
+					this.timer.Delay = 0.1;
+					this.timer.TimeElapsed += new Epsitec.Common.Support.EventHandler(HandleTimerTimeElapsed);
+					this.timer.Start ();
+				}
+				
+				double dx = this.Client.Width;
+				double dy = this.Client.Height;
+				
+				Drawing.Path path;
+				
+				if ((this.mask == null) ||
+					(this.mask_dx != dx) ||
+					(this.mask_dy != dy))
+				{
+					if (this.mask != null)
+					{
+						this.mask.Dispose ();
+						this.mask = null;
+					}
+					
+					this.mask = graphics.CreateAlphaMask ();
+					this.mask_dx = dx;
+					this.mask_dy = dy;
+					
+					path = Drawing.Path.FromCircle (dx/2, dy/2, dx/2, dy/2);
+					
+					this.mask.Color = Drawing.Color.FromRGB (1, 0, 0);
+					this.mask.PaintSurface (path);
+					
+					path.Dispose ();
+				}
+				
+				Drawing.Point pos = this.MapClientToScreen (new Drawing.Point (dx/2, dy/2));
+				
+				Win32Api.GrabScreen (bitmap, (int)(pos.X), (int)(ScreenInfo.GlobalArea.Height - pos.Y));
+				
+				double sx = dx / this.bitmap.Width;
+				double sy = dy / this.bitmap.Height;
+				
+				graphics.SolidRenderer.SetAlphaMask (this.mask.Pixmap, MaskComponent.R);
+				
+				using (Drawing.Pixmap.RawData raw = new Drawing.Pixmap.RawData (this.bitmap))
+				{
+					int nx = (int) this.bitmap.Width;
+					int ny = (int) this.bitmap.Height;
+					
+					double x = 0;
+					double y = 0;
+					
+					for (int iy = 0; iy < ny; iy++)
+					{
+						x = 0;
+						
+						for (int ix = 0; ix < nx; ix++)
+						{
+							path = Drawing.Path.FromRectangle (x, dy-y-sy, sx+1, sy+1);
+							graphics.Color = raw[ix, iy];
+							graphics.PaintSurface (path);
+							path.Dispose ();
+							x += sx;
+						}
+						y += sy;
+					}
+					
+					this.HotColor = raw[nx/2, ny/2];
+				}
+				
+				double ox = System.Math.Floor (this.bitmap.Width / 2)  * sx - 0.5;
+				double oy = System.Math.Floor (this.bitmap.Height / 2) * sy + 0.5;
+				
+				path = new Drawing.Path ();
+				
+				path.MoveTo (ox+2, oy+0);
+				path.LineTo (ox+0, oy+0);
+				path.LineTo (ox+0, oy+2);
+				path.MoveTo (ox+0, oy+sy+1-2);
+				path.LineTo (ox+0, oy+sy+1-0);
+				path.LineTo (ox+2, oy+sy+1-0);
+				path.MoveTo (ox+sx+1-2, oy+sy+1-0);
+				path.LineTo (ox+sx+1-0, oy+sy+1-0);
+				path.LineTo (ox+sx+1-0, oy+sy+1-2);
+				path.MoveTo (ox+sx+1-0, oy+2);
+				path.LineTo (ox+sx+1-0, oy+0);
+				path.LineTo (ox+sx+1-2, oy+0);
+				path.AppendCircle (dx/2, dy/2, dx/2-0.5, dy/2-0.5);
+				path.AppendCircle (dx/2, dy/2, dx/2, dy/2);
+				
+				graphics.Color = Drawing.Color.FromARGB (0.5, 0, 0, 0.8);
+				graphics.LineWidth = 1.0;
+				graphics.PaintOutline (path);
+				
+				path.Dispose ();
+				
+				graphics.SolidRenderer.SetAlphaMask (null, MaskComponent.None);
+				
+				System.Text.StringBuilder buffer = new System.Text.StringBuilder();
+				
+				buffer.AppendFormat ("{0:X2}", (int)(this.HotColor.R * 255.5));
+				buffer.Append (":");
+				buffer.AppendFormat ("{0:X2}", (int)(this.HotColor.G * 255.5));
+				buffer.Append (":");
+				buffer.AppendFormat ("{0:X2}", (int)(this.HotColor.B * 255.5));
+				
+				this.PaintText (graphics, buffer.ToString ());
+			}
+			
+			protected virtual void PaintText(Drawing.Graphics graphics, string text)
+			{
+				Drawing.Path path = new Drawing.Path ();
+				Drawing.Font font = Drawing.Font.GetFont ("Tahoma", "Regular");
+				double       size = 10;
+				
+				double dx = font.GetTextAdvance (text) * size;
+				
+				double ox = (this.Client.Width - dx) / 2;
+				double oy = 2;
+				
+				graphics.AddFilledRectangle (ox - 1, oy - 2, dx + 2, font.LineHeight * size - 0);
+				graphics.AddFilledRectangle (ox - 2, oy - 1, dx + 4, font.LineHeight * size - 2);
+				graphics.RenderSolid (Drawing.Color.FromARGB (0.8, 1, 1, 1));
+				
+				graphics.AddText (ox, oy, text, font, size);
+				graphics.RenderSolid (Drawing.Color.FromBrightness (0));
+			}
+			
+			
+			private void HandleTimerTimeElapsed(object sender)
+			{
+				if (this.IsVisible)
+				{
+					this.Invalidate ();
+				}
+				else if (this.timer != null)
+				{
+					this.timer.Stop ();
+					this.timer.Dispose ();
+					this.timer = null;
+				}
+			}
+			
+			protected virtual void OnHotColorChanged()
+			{
+				if (this.HotColorChanged != null)
+				{
+					this.HotColorChanged (this);
+				}
+			}
+			
+			
+			public event Support.EventHandler	HotColorChanged;
+			
+			
+			private Drawing.Color				hot_color;
+			private int							scale;
+			
+			private bool						is_dragging;
+			
+			private double						mask_dx, mask_dy;
+			private Drawing.Point				origin;
+			private Drawing.Image				bitmap;
+			private Drawing.Graphics			mask;
+			
+			private Timer						timer;
+		}
+
+		private void HandleGlassHotColorChanged(object sender)
+		{
+			Glass glass = sender as Glass;
+			
+			System.Diagnostics.Debug.WriteLine ("Hot color: " + glass.HotColor.ToString ());
+		}
 	}
 }
