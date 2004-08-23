ffbdae357b43fc3180734e5e793b84853e3d25e3
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:12:57 +0000

    ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.
    **********************
    Label: "Stable-24-03-2004"
    User: Arnaud       Date: 24.03.04   Time: 22:47
    Labeled
    Label comment: With DR contributions. First draft of Common.UI.
    
    **********************
    Label: "Stable-20-03-2004"
    User: Arnaud       Date: 20.03.04   Time: 12:18
    Labeled
    Label comment: 
    
    **********************
    Label: "Stable-15-03-2004"
    User: Arnaud       Date: 15.03.04   Time: 11:30
    Labeled
    Label comment: EditArray et App.Designer bien avancés.
    
    git-svn-id: https://svn.opac.ch/svn/cr@1831 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Pictogram/Data/PropertyLine.cs
+++ Epsitec.Cresus/Common.Pictogram/Data/PropertyLine.cs
@@ -11,10 +11,14 @@ namespace Epsitec.Common.Pictogram.Data
 	{
 		public PropertyLine()
 		{
-			this.width = 1.0;
-			this.cap   = Drawing.CapStyle.Round;
-			this.join  = Drawing.JoinStyle.Round;
-			this.limit = 5.0;
+			this.width        = 1.0;
+			this.cap          = Drawing.CapStyle.Round;
+			this.join         = Drawing.JoinStyle.Round;
+			this.limit        = 5.0;
+			this.patternId    = 0;
+			this.patternSize  = 1.0;
+			this.patternShift = 0.0;
+			this.patternAngle = 0.0;
 		}
 
 		[XmlAttribute]
@@ -45,6 +49,34 @@ namespace Epsitec.Common.Pictogram.Data
 			set { this.limit = value; }
 		}
 
+		[XmlAttribute]
+		public int PatternId
+		{
+			get { return this.patternId; }
+			set { this.patternId = value; }
+		}
+
+		[XmlAttribute]
+		public double PatternSize
+		{
+			get { return this.patternSize; }
+			set { this.patternSize = value; }
+		}
+
+		[XmlAttribute]
+		public double PatternShift
+		{
+			get { return this.patternShift; }
+			set { this.patternShift = value; }
+		}
+
+		[XmlAttribute]
+		public double PatternAngle
+		{
+			get { return this.patternAngle; }
+			set { this.patternAngle = value; }
+		}
+
 		// Indique si un changement de cette propriété modifie la bbox de l'objet.
 		[XmlIgnore]
 		public override bool AlterBoundingBox
@@ -57,10 +89,14 @@ namespace Epsitec.Common.Pictogram.Data
 		{
 			base.CopyTo(property);
 			PropertyLine p = property as PropertyLine;
-			p.Width = this.width;
-			p.Cap   = this.cap;
-			p.Join  = this.join;
-			p.Limit = this.limit;
+			p.Width        = this.width;
+			p.Cap          = this.cap;
+			p.Join         = this.join;
+			p.Limit        = this.limit;
+			p.PatternId    = this.patternId;
+			p.PatternSize  = this.patternSize;
+			p.PatternShift = this.patternShift;
+			p.PatternAngle = this.patternAngle;
 		}
 
 		// Compare deux propriétés.
@@ -69,10 +105,14 @@ namespace Epsitec.Common.Pictogram.Data
 			if ( !base.Compare(property) )  return false;
 
 			PropertyLine p = property as PropertyLine;
-			if ( p.Width != this.width )  return false;
-			if ( p.Cap   != this.cap   )  return false;
-			if ( p.Join  != this.join  )  return false;
-			if ( p.Limit != this.limit )  return false;
+			if ( p.Width        != this.width        )  return false;
+			if ( p.Cap          != this.cap          )  return false;
+			if ( p.Join         != this.join         )  return false;
+			if ( p.Limit        != this.limit        )  return false;
+			if ( p.PatternId    != this.patternId    )  return false;
+			if ( p.PatternSize  != this.patternSize  )  return false;
+			if ( p.PatternShift != this.patternShift )  return false;
+			if ( p.PatternAngle != this.patternAngle )  return false;
 
 			return true;
 		}
@@ -102,9 +142,157 @@ namespace Epsitec.Common.Pictogram.Data
 		}
 
 
-		protected double				width;
-		protected Drawing.CapStyle		cap;
-		protected Drawing.JoinStyle		join;
-		protected double				limit;  // longueur (et non angle) !
+		// Cherche le pattern à utiliser d'après son identificateur.
+		static protected ObjectPattern SearchPattern(IconObjects iconObjects, int id)
+		{
+			int total = iconObjects.TotalPatterns();
+			for ( int i=1 ; i<total ; i++ )
+			{
+				ObjectPattern pattern = iconObjects.Objects[i] as ObjectPattern;
+				if ( pattern.Id == id )  return pattern;
+			}
+			return null;
+		}
+
+		// Dessine le pattern le long d'une droite.
+		protected void DrawPatternLine(Drawing.Graphics graphics, IconContext iconContext, IconObjects iconObjects,
+									   ObjectPattern pattern, ref double advance,
+									   Drawing.Point p1, Drawing.Point p2)
+		{
+			ObjectPage page = pattern.Objects[0] as ObjectPage;
+			IAdorner adorner = Epsitec.Common.Widgets.Adorner.Factory.Active;
+
+			double len = Drawing.Point.Distance(p1,p2);
+			while ( advance <= len )
+			{
+				Drawing.Point pos  = Drawing.Point.Move(p1,p2, advance);
+				Drawing.Point next = Drawing.Point.Move(p1,p2, advance+0.001);
+				advance += this.width;
+
+				double angle = Drawing.Point.ComputeAngle(pos, next);
+
+				if ( this.patternShift != 0 )
+				{
+					pos = Drawing.Transform.RotatePoint(pos, angle+System.Math.PI/2, new Drawing.Point(pos.X+this.width*this.patternShift, pos.Y));
+				}
+
+				Drawing.Transform ot = graphics.SaveTransform();
+				graphics.TranslateTransform(pos.X, pos.Y);
+				graphics.ScaleTransform(this.width/10*this.patternSize, this.width/10*this.patternSize, 0, 0);
+				graphics.RotateTransform((angle+this.patternAngle)*180/System.Math.PI, 0, 0);
+				graphics.TranslateTransform(-5, -5);
+				iconObjects.DrawGeometry(page.Objects, graphics, iconContext, iconObjects, adorner, Drawing.Rectangle.Infinite, true, false);
+				graphics.Transform = ot;
+			}
+			advance -= len;
+		}
+
+		// Dessine le pattern le long d'une courbe.
+		protected void DrawPatternCurve(Drawing.Graphics graphics, IconContext iconContext, IconObjects iconObjects,
+										ObjectPattern pattern, ref double advance,
+										Drawing.Point p1, Drawing.Point s1, Drawing.Point s2, Drawing.Point p2)
+		{
+			Drawing.Point pos = p1;
+			int total = (int)(1.0/PropertyLine.step);
+			for ( int rank=1 ; rank<=100 ; rank ++ )
+			{
+				double t = PropertyLine.step*rank;
+				Drawing.Point next = Drawing.Point.Bezier(p1,s1,s2,p2, t);
+				this.DrawPatternLine(graphics, iconContext, iconObjects, pattern, ref advance, pos, next);
+				pos = next;
+			}
+		}
+
+		// Dessine le pattern le long d'un chemin.
+		protected void DrawPattern(Drawing.Graphics graphics, IconContext iconContext, IconObjects iconObjects,
+								   ObjectPattern pattern, Drawing.Path path)
+		{
+			Drawing.PathElement[] elements;
+			Drawing.Point[] points;
+			path.GetElements(out elements, out points);
+
+			Drawing.Point start = new Drawing.Point(0, 0);
+			Drawing.Point current = new Drawing.Point(0, 0);
+			Drawing.Point p1 = new Drawing.Point(0, 0);
+			Drawing.Point p2 = new Drawing.Point(0, 0);
+			Drawing.Point p3 = new Drawing.Point(0, 0);
+			double advance = 0;
+			int i = 0;
+			while ( i < elements.Length )
+			{
+				switch ( elements[i] & Drawing.PathElement.MaskCommand )
+				{
+					case Drawing.PathElement.MoveTo:
+						current = points[i++];
+						start = current;
+						break;
+
+					case Drawing.PathElement.LineTo:
+						p1 = points[i++];
+						this.DrawPatternLine(graphics, iconContext, iconObjects, pattern, ref advance, current,p1);
+						current = p1;
+						break;
+
+					case Drawing.PathElement.Curve3:
+						p1 = points[i++];
+						p2 = points[i++];
+						this.DrawPatternCurve(graphics, iconContext, iconObjects, pattern, ref advance, current,p1,p1,p2);
+						current = p2;
+						break;
+
+					case Drawing.PathElement.Curve4:
+						p1 = points[i++];
+						p2 = points[i++];
+						p3 = points[i++];
+						this.DrawPatternCurve(graphics, iconContext, iconObjects, pattern, ref advance, current,p1,p2,p3);
+						current = p3;
+						break;
+
+					default:
+						if ( (elements[i] & Drawing.PathElement.FlagClose) != 0 )
+						{
+							this.DrawPatternLine(graphics, iconContext, iconObjects, pattern, ref advance, current,start);
+						}
+						i ++;
+						break;
+				}
+			}
+		}
+
+		// Dessine le trait le long d'un chemin.
+		public void DrawPath(Drawing.Graphics graphics, IconContext iconContext, IconObjects iconObjects, Drawing.Path path, Drawing.Color color)
+		{
+			if ( this.width == 0.0 )  return;
+
+			if ( this.patternId == 0 )
+			{
+				graphics.Rasterizer.AddOutline(path, this.width, this.cap, this.join, this.limit);
+				graphics.RenderSolid(iconContext.AdaptColor(color));
+			}
+			else
+			{
+				ObjectPattern pattern = PropertyLine.SearchPattern(iconObjects, this.patternId);
+				if ( pattern == null )
+				{
+					graphics.Rasterizer.AddOutline(path, this.width, this.cap, this.join, this.limit);
+					graphics.RenderSolid(iconContext.AdaptColor(color));
+				}
+				else
+				{
+					this.DrawPattern(graphics, iconContext, iconObjects, pattern, path);
+				}
+			}
+		}
+
+
+		protected double					width;
+		protected Drawing.CapStyle			cap;
+		protected Drawing.JoinStyle			join;
+		protected double					limit;  // longueur (et non angle) !
+		protected int						patternId;
+		protected double					patternSize;
+		protected double					patternShift;
+		protected double					patternAngle;
+		protected static readonly double	step = 0.01;
 	}
 }
