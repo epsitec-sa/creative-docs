359a60ad12d85d87c4d43c3968733739496e398c
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:31:52 +0000

    ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@921 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Support/CommandAttribute.cs
@@ -0,0 +1,36 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : en chantier/PA
+
+using System;
+
+namespace Epsitec.Common.Support
+{
+	/// <summary>
+	/// La classe CommandAttribute définit un attribut [Command] qui est
+	/// utilisé par le CommandDispatcher pour identifier l'implémentation
+	/// de commandes (l'attribut s'utilise comme préfixe de méthode).
+	/// </summary>
+	
+	[AttributeUsage (AttributeTargets.Method, AllowMultiple = true)]
+	
+	public class CommandAttribute : System.Attribute
+	{
+		public CommandAttribute()
+		{
+		}
+		
+		public CommandAttribute(string command_name)
+		{
+		}
+		
+		
+		public string					CommandName
+		{
+			get { return this.command_name; }
+			set { this.command_name = value; }
+		}
+		
+		
+		protected string				command_name;
+	}
+}
