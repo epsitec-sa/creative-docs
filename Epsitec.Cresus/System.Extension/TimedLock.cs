568bf39e31afb492266545721ed70d5f4c0c745a
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:46:56 +0000

    ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1572 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/System.Extension/TimedLock.cs
@@ -0,0 +1,83 @@
+namespace System
+{
+	//	Thanks to Eric Gunnerson for recommending this be a struct rather
+	//	than a class - avoids a heap allocation.
+	//	(In Debug mode, we make it a class so that we can add a finalizer
+	//	in order to detect when the object is not freed.)
+	//	http://www.interact-sw.co.uk/iangblog/2004/03/23/locking
+
+#if DEBUG
+	public class TimedLock : System.IDisposable
+#else
+	public struct TimedLock : System.IDisposable
+#endif
+	{
+		public static System.IDisposable Lock(object o)
+		{
+			return TimedLock.Lock (o, System.TimeSpan.FromSeconds (10));
+		}
+		
+		public static System.IDisposable Lock (object o, System.TimeSpan timeout)
+		{
+			TimedLock tl = new TimedLock (o);
+			
+			if (!System.Threading.Monitor.TryEnter (o, timeout))
+			{
+				throw new LockTimeoutException ();
+			}
+			
+			return tl;
+		}
+		
+		private TimedLock(object o)
+		{
+			this.target = o;
+		}
+		
+		public void Dispose ()
+		{
+			System.Threading.Monitor.Exit (this.target);
+			
+			//	It's a bad error if someone forgets to call Dispose,
+			//	so in Debug builds, we put a finalizer in to detect
+			//	the error. If Dispose is called, we suppress the
+			//	finalizer.
+#if DEBUG
+			System.GC.SuppressFinalize (this);
+#endif
+		}
+
+#if DEBUG
+		~TimedLock()
+		{
+			//	If this finalizer runs, someone somewhere failed to
+			//	call Dispose, which means we've failed to leave
+			//	a monitor!
+			
+			System.Diagnostics.Debug.Fail ("Undisposed lock.");
+		}
+#endif
+		
+		private object target;
+	}
+	
+	[System.Serializable]
+	public class LockTimeoutException : System.ApplicationException, System.Runtime.Serialization.ISerializable
+	{
+		public LockTimeoutException () : base ("Timed out waiting for lock.")
+		{
+		}
+		
+		
+		#region ISerializable Members
+		protected LockTimeoutException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base (info, context)
+		{
+		}
+		
+		void System.Runtime.Serialization.ISerializable.GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+		{
+			base.GetObjectData (info, context);
+		}
+		#endregion
+	}
+}
