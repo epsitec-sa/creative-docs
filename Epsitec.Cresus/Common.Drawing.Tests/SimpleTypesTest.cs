ae50f05c67dee8094db7f9f1d21bf2298866f065
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:05:43 +0000

    ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.
    **********************
    Label: "11-02-2004"
    User: Arnaud       Date: 12.02.04   Time:  7:47
    Labeled
    Label comment: Stable version.
    
    
    **********************
    Label: "22-01-2004"
    User: Arnaud       Date: 22.01.04   Time: 15:38
    Labeled
    Label comment: Object re-bundling implemented.
    
    **********************
    Label: "21-01-2004"
    User: Arnaud       Date: 21.01.04   Time: 18:14
    Labeled
    Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.
    
    
    **********************
    Label: "19-01-2004"
    User: Arnaud       Date: 19.01.04   Time: 16:24
    Labeled
    Label comment: Before in-depth modification of XML handling in ResourceBundle.
    
    **********************
    Label: "13-01-2004"
    User: Arnaud       Date: 13.01.04   Time: 20:50
    Labeled
    Label comment: Stable, before rework of Layout management.
    
    git-svn-id: https://svn.opac.ch/svn/cr@1213 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Drawing.Tests/SimpleTypesTest.cs
+++ Epsitec.Cresus/Common.Drawing.Tests/SimpleTypesTest.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
 
 namespace Epsitec.Common.Drawing
 {
@@ -23,6 +24,32 @@ namespace Epsitec.Common.Drawing
 			}
 			
 			System.IO.File.Delete ("test.bin");
+			
+			Assertion.AssertNotNull (point_2);
+			Assertion.AssertEquals (typeof (Point), point_2.GetType ());
+			Assertion.AssertEquals (point_1, point_2);
+			
+			using (System.IO.Stream stream = System.IO.File.Open ("test.soap", System.IO.FileMode.Create))
+			{
+				SoapFormatter formatter = new SoapFormatter ();
+				formatter.Serialize (stream, point_1);
+			}
+			
+			using (System.IO.Stream stream = System.IO.File.Open ("test.soap", System.IO.FileMode.Open))
+			{
+				SoapFormatter formatter = new SoapFormatter ();
+				point_2 = formatter.Deserialize (stream);
+			}
+			
+			using (System.IO.Stream stream = System.IO.File.Open ("test.soap", System.IO.FileMode.Open))
+			{
+				System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding ();
+				byte[] buffer = new byte[stream.Length];
+				stream.Read (buffer, 0, (int) stream.Length);
+				System.Console.Out.WriteLine ("{0}", encoding.GetString (buffer));
+			}
+			
+			System.IO.File.Delete ("test.soap");
 			
 			Assertion.AssertNotNull (point_2);
 			Assertion.AssertEquals (typeof (Point), point_2.GetType ());
