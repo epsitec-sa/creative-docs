711c452e8eb927d2568dfd60a9cd2c983923e232
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:51:13 +0000

    ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1092 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/Design/Tools/TextBundleArrayStore.cs
@@ -0,0 +1,90 @@
+namespace Epsitec.Common.Widgets.Design.Tools
+{
+	/// <summary>
+	/// Summary description for TextBundleArrayStore.
+	/// </summary>
+	public class TextBundleArrayStore : Support.Data.ITextArrayStore
+	{
+		public TextBundleArrayStore()
+		{
+		}
+		
+		
+		public Support.ResourceBundle			Bundle
+		{
+			get
+			{
+				return this.bundle;
+			}
+			set
+			{
+				this.bundle = value;
+			}
+		}
+		
+		
+		#region ITextArrayStore Members
+		public void InsertRows(int row, int num)
+		{
+			// TODO:  Add TextBundleArrayStore.InsertRows implementation
+		}
+		
+		public void RemoveRows(int row, int num)
+		{
+			// TODO:  Add TextBundleArrayStore.RemoveRows implementation
+		}
+		
+		public string GetCellText(int row, int column)
+		{
+			Support.ResourceBundle.Field field = this.bundle[row];
+			
+			switch (column)
+			{
+				case 0:
+					return TextLayout.ConvertToTaggedText (field.Name);
+				case 1:
+					return field.AsString;
+			}
+			
+			return null;
+		}
+		
+		public void SetCellText(int row, int column, string value)
+		{
+			Support.ResourceBundle.Field field = this.bundle[row];
+			
+			switch (column)
+			{
+				case 0:
+					field.SetName (TextLayout.ConvertToSimpleText (value));
+					break;
+				case 1:
+					field.SetStringValue (value);
+					break;
+			}
+		}
+		
+		public int GetColumnCount()
+		{
+			return 2;
+		}
+		
+		public int GetRowCount()
+		{
+			if (this.bundle != null)
+			{
+				return this.bundle.CountFields;
+			}
+			
+			return 0;
+		}
+		#endregion
+		
+		protected void DumpXml()
+		{
+			System.Diagnostics.Debug.WriteLine (this.bundle.CreateXmlDocument (false).OuterXml);
+		}
+		
+		protected Support.ResourceBundle		bundle;
+	}
+}
