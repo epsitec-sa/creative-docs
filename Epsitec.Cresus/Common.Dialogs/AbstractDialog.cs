505976072299b35486dc2d310ab471030b74eb60
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:00:50 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:
    **********************
    Label: "Stable-01-04-2004"
    User: Arnaud       Date:  1.04.04   Time: 12:42
    Labeled
    Label comment: TabIndex edition working in GUI.
    
    **********************
    Label: "Stable-24-03-2004"
    User: Arnaud       Date: 24.03.04   Time: 22:47
    Labeled
    Label comment: With DR contributions. First draft of Common.UI.
    
    git-svn-id: https://svn.opac.ch/svn/cr@2277 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Dialogs/AbstractDialog.cs
+++ Epsitec.Cresus/Common.Dialogs/AbstractDialog.cs
@@ -1,3 +1,6 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
 using Epsitec.Common.Widgets;
 using Epsitec.Common.Support;
 
@@ -64,7 +67,37 @@ namespace Epsitec.Common.Dialogs
 		}
 		#endregion
 		
+		public static void LayoutButtons(double width, params Widgets.Button[] buttons)
+		{
+			if (buttons.Length > 0)
+			{
+				double total_width = 0;
+				
+				for (int i = 0; i < buttons.Length; i++)
+				{
+					total_width += buttons[i].Width;
+				}
+				
+				total_width += (buttons.Length-1) * 8;
+				
+				if (total_width < width)
+				{
+					double x = System.Math.Floor ((width - total_width) / 2);
+					
+					for (int i = 0; i < buttons.Length; i++)
+					{
+						buttons[i].Location = new Drawing.Point (x, buttons[i].Bottom);
+						
+						x += buttons[i].Width;
+						x += 8;
+					}
+				}
+			}
+		}
+		
+		
 		protected abstract void CreateWindow();
 		
-		protected Window						window;}
+		protected Window						window;
+	}
 }
