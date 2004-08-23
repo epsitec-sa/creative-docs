f214f7325228c5e3b8d81ba771182a6d8e8bbb6d
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:25:54 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2482 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Designer/UI/CommandController.cs
@@ -0,0 +1,118 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+
+namespace Epsitec.Common.Designer.UI
+{
+	/// <summary>
+	/// Summary description for CommandController.
+	/// </summary>
+	public class CommandController : Epsitec.Common.UI.Controllers.AbstractController
+	{
+		public CommandController()
+		{
+		}
+		
+		
+		public new CommandAdapter				Adapter
+		{
+			get
+			{
+				return base.Adapter as CommandAdapter;
+			}
+		}
+		
+		
+		public override void CreateUI(Widget panel)
+		{
+			this.caption_label = new StaticText (panel);
+			this.combo_cmd    = new TextFieldCombo (panel);
+			
+			double h_line = this.combo_cmd.Height;
+			double h_pane = System.Math.Max (panel.Height, h_line + 6);
+			
+			panel.Height = h_pane;
+			
+			double y = System.Math.Floor ((h_pane - h_line) / 2);
+			
+			this.caption_label.Size          = new Drawing.Size (80, h_line);
+			this.caption_label.Anchor        = AnchorStyles.TopLeft;
+			this.caption_label.AnchorMargins = new Drawing.Margins (0, 0, h_pane - y - h_line, 0);
+			
+			this.combo_cmd.Height         = h_line;
+			this.combo_cmd.Anchor         = AnchorStyles.LeftAndRight | AnchorStyles.Top;
+			this.combo_cmd.AnchorMargins  = new Drawing.Margins (this.caption_label.Right, 0, h_pane - y - h_line, 0);
+			this.combo_cmd.TextChanged   += new Support.EventHandler (this.HandleComboCommandTextChanged);
+			this.combo_cmd.TabIndex       = 10;
+			this.combo_cmd.Name           = "Value";
+			
+			this.OnCaptionChanged ();
+			
+			this.SyncFromCommands ();
+			this.SyncFromAdapter (Common.UI.SyncReason.Initialisation);
+		}
+		
+		public override void SyncFromAdapter(Common.UI.SyncReason reason)
+		{
+			CommandAdapter adapter = this.Adapter;
+			
+			if ((adapter != null) &&
+				(this.combo_cmd != null))
+			{
+				this.combo_cmd.Text = adapter.Value;
+				
+				if ((reason == Common.UI.SyncReason.SourceChanged) ||
+					(reason == Common.UI.SyncReason.Initialisation))
+				{
+					this.combo_cmd.SelectAll ();
+				}
+			}
+		}
+		
+		public override void SyncFromUI()
+		{
+			CommandAdapter adapter = this.Adapter;
+			
+			if ((adapter != null) &&
+				(this.combo_cmd != null))
+			{
+				adapter.Value = this.combo_cmd.Text;
+			}
+		}
+		
+		
+		private void SyncFromCommands()
+		{
+			//	Refait la liste des commandes.
+			
+			this.combo_cmd.Items.Clear ();
+			
+			DialogDesigner designer = this.Adapter.Application.InterfaceEditController.ActiveDialogDesigner;
+			
+			if (designer == null)
+			{
+				return;
+			}
+			
+			Support.CommandDispatcher commands = designer.DialogCommands;
+			
+			if (commands == null)
+			{
+				return;
+			}
+			
+			this.combo_cmd.Items.AddRange (commands.FindCommandNames ());
+		}
+		
+		
+		private void HandleComboCommandTextChanged(object sender)
+		{
+			this.SyncFromUI ();
+		}
+		
+		
+		private TextFieldCombo					combo_cmd;
+	}
+}
