822ffcdbf76406a286f441e37fcb64bd7f9c96e6
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 12:57:42 +0000

    ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:
    **********************
    Label: "08-12-2003"
    User: Arnaud       Date:  8.12.03   Time: 15:25
    Labeled
    Label comment: Stable version.
    
    **********************
    Label: "26-11-2003"
    User: Arnaud       Date: 26.11.03   Time: 16:28
    Labeled
    Label comment: DbTable + DbType meta info now stored in database.
    
    
    **********************
    Label: "Stamp-12-11-2003"
    User: Arnaud       Date: 12.11.03   Time: 20:40
    Labeled
    Label comment:
    
    git-svn-id: https://svn.opac.ch/svn/cr@565 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Widgets/Slider.cs
+++ Epsitec.Cresus/Common.Widgets/Slider.cs
@@ -7,11 +7,197 @@ namespace Epsitec.Common.Widgets
 	{
 		public Slider()
 		{
+			IAdorner adorner = Widgets.Adorner.Factory.Active;
+			this.color = adorner.GetColorCaption();
+			this.colorBack = Drawing.Color.FromBrightness(1);
 		}
 		
 		public Slider(Widget embedder) : this()
 		{
 			this.SetEmbedder(embedder);
 		}
+
+		// Valeur numérique représentée.
+		public double Value
+		{
+			get
+			{
+				return this.sliderValue;
+			}
+
+			set
+			{
+				if ( this.sliderValue != value )
+				{
+					this.sliderValue = value;
+					this.Invalidate();
+				}
+			}
+		}
+
+		// Valeur numérique minimale possible.
+		public double MinRange
+		{
+			get
+			{
+				return this.minRange;
+			}
+
+			set
+			{
+				this.minRange = value;
+			}
+		}
+		
+		// Valeur numérique maximale possible.
+		public double MaxRange
+		{
+			get
+			{
+				return this.maxRange;
+			}
+
+			set
+			{
+				this.maxRange = value;
+			}
+		}
+		
+		// Couleur du slider.
+		public Drawing.Color Color
+		{
+			get
+			{
+				return this.color;
+			}
+
+			set
+			{
+				if ( this.color != value )
+				{
+					this.color = value;
+					this.Invalidate();
+				}
+			}
+		}
+		
+		// Couleur de fond du slider.
+		public Drawing.Color ColorBack
+		{
+			get
+			{
+				return this.colorBack;
+			}
+
+			set
+			{
+				if ( this.colorBack != value )
+				{
+					this.colorBack = value;
+					this.Invalidate();
+				}
+			}
+		}
+		
+
+		// Gestion d'un événement.
+		protected override void ProcessMessage(Message message, Drawing.Point pos)
+		{
+			switch ( message.Type )
+			{
+				case MessageType.MouseDown:
+					this.mouseDown = true;
+					this.MouseDown(pos);
+					break;
+				
+				case MessageType.MouseMove:
+					if ( this.mouseDown )
+					{
+						this.MouseMove(pos);
+					}
+					break;
+
+				case MessageType.MouseUp:
+					if ( this.mouseDown )
+					{
+						this.MouseUp(pos);
+						this.mouseDown = false;
+					}
+					break;
+			}
+			
+			message.Consumer = this;
+		}
+
+		protected void MouseDown(Drawing.Point pos)
+		{
+			this.Value = this.Detect(pos);
+			this.OnValueChanged();
+		}
+
+		protected void MouseMove(Drawing.Point pos)
+		{
+			this.Value = this.Detect(pos);
+			this.OnValueChanged();
+		}
+
+		protected void MouseUp(Drawing.Point pos)
+		{
+			this.Value = this.Detect(pos);
+			this.OnValueChanged();
+		}
+
+		protected double Detect(Drawing.Point pos)
+		{
+			Drawing.Rectangle rect = new Drawing.Rectangle(0, 0, this.Client.Width, this.Client.Height);
+			rect.Inflate(-this.margin, -this.margin);
+			double val = this.minRange+(pos.X-rect.Left)*(this.maxRange-this.minRange)/rect.Width;
+			val = System.Math.Max(val, this.minRange);
+			val = System.Math.Min(val, this.maxRange);
+			val = System.Math.Floor(val+0.5);
+			return val;
+		}
+
+		// Génère un événement pour dire que le slider a changé.
+		protected virtual void OnValueChanged()
+		{
+			if ( this.ValueChanged != null )  // qq'un écoute ?
+			{
+				this.ValueChanged(this);
+			}
+		}
+
+		public event EventHandler ValueChanged;
+
+
+		protected override void PaintBackgroundImplementation(Drawing.Graphics graphics, Drawing.Rectangle clipRect)
+		{
+			IAdorner adorner = Widgets.Adorner.Factory.Active;
+
+			Drawing.Rectangle rect   = new Drawing.Rectangle(0, 0, this.Client.Width, this.Client.Height);
+			WidgetState       state  = this.PaintState;
+			Direction         dir    = this.RootDirection;
+			
+			adorner.PaintTextFieldBackground(graphics, rect, state, dir, TextFieldStyle.Normal, false);
+
+			if ( this.IsEnabled )
+			{
+				rect.Inflate(-this.margin, -this.margin);
+				graphics.AddFilledRectangle(rect);
+				graphics.RenderSolid(this.colorBack);
+				rect.Width *= (this.sliderValue-this.minRange)/(this.maxRange-this.minRange);
+				graphics.AddFilledRectangle(rect);
+				graphics.RenderSolid(this.color);
+			}
+		}
+
+		
+		protected double					sliderValue = 0;
+		protected double					minRange = 0;
+		protected double					maxRange = 100;
+		protected Drawing.Color				color;
+		protected Drawing.Color				colorBack;
+		protected bool						mouseDown = false;
+		protected double					margin = 1;
 	}
 }
