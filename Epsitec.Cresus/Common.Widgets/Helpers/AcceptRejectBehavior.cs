a9f4db6680a39c1db3c96e48414b7d31eef1a6be
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:50:05 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2179 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/Helpers/AcceptRejectBehavior.cs
@@ -0,0 +1,143 @@
+namespace Epsitec.Common.Widgets.Helpers
+{
+	/// <summary>
+	/// La classe AcceptRejectBehavior gère les boutons pour accepter/rejeter une
+	/// édition.
+	/// </summary>
+	public class AcceptRejectBehavior
+	{
+		public AcceptRejectBehavior(Widget host)
+		{
+			this.host = host;
+			this.CreateButtons ();
+		}
+		
+		
+		public Widget							Host
+		{
+			get
+			{
+				return this.host;
+			}
+		}
+		
+		public double							DefaultWidth
+		{
+			get
+			{
+				if (this.IsVisible)
+				{
+					double height = this.host.Height;
+					double width  = System.Math.Floor ((height - 4) * 15.0 / 17.0);
+					
+					return width + width - 1;
+				}
+				
+				return 0;
+			}
+		}
+		
+		public bool								IsVisible
+		{
+			get
+			{
+				return this.is_visible;
+			}
+		}
+		
+		public void SetVisible(bool visible)
+		{
+			this.is_visible = visible;
+			
+			this.button_accept.SetVisible (visible);
+			this.button_reject.SetVisible (visible);
+		}
+		
+		public void SetEnabledOk(bool enable_accept)
+		{
+			this.button_accept.SetEnabled (enable_accept);
+		}
+		
+		
+		public void UpdateButtonGeometry()
+		{
+			AbstractTextField text = this.host as AbstractTextField;
+			
+			if (text != null)
+			{
+				Drawing.Rectangle bounds = text.GetButtonBounds ();
+				Drawing.Rectangle rect_1 = bounds;
+				Drawing.Rectangle rect_2 = bounds;
+				
+				rect_1.Right = rect_1.Left + rect_1.Width / 2 + 0.5;
+				rect_2.Left  = rect_1.Right - 1;
+				
+				this.button_accept.Bounds = rect_1;
+				this.button_reject.Bounds = rect_2;
+			}
+		}
+		
+		
+		protected void CreateButtons()
+		{
+			this.button_accept = new GlyphButton(this.host);
+			this.button_reject = new GlyphButton(this.host);
+			
+			IFeel feel = Feel.Factory.Active;
+			
+			this.button_accept.Name        = "Accept";
+			this.button_accept.GlyphShape  = GlyphShape.Validate;
+			this.button_accept.ButtonStyle = ButtonStyle.ExListMiddle;
+			this.button_accept.Clicked    += new MessageEventHandler(this.HandleButtonOkClicked);
+			this.button_accept.Shortcut    = feel.AcceptShortcut;
+			
+			this.button_reject.Name        = "Reject";
+			this.button_reject.GlyphShape  = GlyphShape.Cancel;
+			this.button_reject.ButtonStyle = ButtonStyle.ExListRight;
+			this.button_reject.Clicked    += new MessageEventHandler(this.HandleButtonRejectClicked);
+			this.button_reject.Shortcut    = feel.CancelShortcut;
+		}
+		
+		
+		private void HandleButtonOkClicked(object sender, MessageEventArgs e)
+		{
+			System.Diagnostics.Debug.Assert (sender == this.button_accept);
+			this.OnAcceptClicked ();
+		}		
+		
+		private void HandleButtonRejectClicked(object sender, MessageEventArgs e)
+		{
+			System.Diagnostics.Debug.Assert (sender == this.button_reject);
+			this.OnRejectClicked ();
+		}		
+		
+		
+		protected virtual void OnAcceptClicked()
+		{
+			if (this.AcceptClicked != null)
+			{
+				this.AcceptClicked (this);
+			}
+		}
+		
+		protected virtual void OnRejectClicked()
+		{
+			if (this.RejectClicked != null)
+			{
+				this.RejectClicked (this);
+			}
+		}
+		
+		
+		public event Support.EventHandler		AcceptClicked;
+		public event Support.EventHandler		RejectClicked;
+		
+		
+		protected Widget						host;
+		
+		protected GlyphButton					button_accept;
+		protected GlyphButton					button_reject;
+		
+		protected bool							is_visible;
+	}
+}
