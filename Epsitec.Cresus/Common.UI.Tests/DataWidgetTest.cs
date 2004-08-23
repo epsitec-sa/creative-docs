7edb9c3ee5b2f0c7264fc56ba88d2f0c4f4ed7a8
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:09:14 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2352 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.UI.Tests/DataWidgetTest.cs
@@ -0,0 +1,60 @@
+using NUnit.Framework;
+using Epsitec.Common.Widgets;
+
+namespace Epsitec.Common.UI
+{
+	[TestFixture] public class DataWidgetTest
+	{
+		[SetUp] public void SetUp()
+		{
+			Common.Widgets.Widget.Initialise ();
+			Common.Widgets.Adorner.Factory.SetActive ("LookMetal");
+			
+			Support.Resources.SetupProviders ("Test");
+		}
+		
+		[Test] public void CheckUnboundDataWidget()
+		{
+			Window window = new Window ();
+			window.Text = "CheckUnboundDataWidget";
+			window.Root.DockPadding = new Drawing.Margins (8, 8, 16, 16);
+			
+			Widgets.DataWidget data_widget = new Widgets.DataWidget ();
+			
+			data_widget.Dock   = DockStyle.Fill;
+			data_widget.Parent = window.Root;
+			
+			window.Show ();
+		}
+		
+		[Test] public void CheckSimpleDataWidgets()
+		{
+			Data.Field field_1 = new Data.Field ("Field1", "Hello, world...", new Types.StringType ());
+			Data.Field field_2 = new Data.Field ("Field2", 10.0M, new Types.DecimalType (-100, 100, 0.05M));
+			
+			Window window = new Window ();
+			window.Text = "CheckSimpleDataWidgets";
+			window.Root.DockPadding = new Drawing.Margins (8, 8, 16, 16);
+			
+			Widgets.DataWidget data_widget_1 = new Widgets.DataWidget ();
+			Widgets.DataWidget data_widget_2 = new Widgets.DataWidget ();
+			
+			field_1.DefineCaption ("Text");
+			field_2.DefineCaption ("Numeric value");
+			
+			data_widget_1.Dock           = DockStyle.Top;
+			data_widget_1.Parent         = window.Root;
+			data_widget_1.DataSource     = field_1;
+			data_widget_1.Representation = Data.Representation.Automatic;
+			data_widget_1.DockMargins    = new Drawing.Margins (0, 0, 0, 2);
+			
+			data_widget_2.Dock           = DockStyle.Top;
+			data_widget_2.Parent         = window.Root;
+			data_widget_2.DataSource     = field_2;
+			data_widget_2.Representation = Data.Representation.Automatic;
+			data_widget_2.DockMargins    = new Drawing.Margins (0, 0, 0, 2);
+			
+			window.Show ();
+		}
+	}
+}
