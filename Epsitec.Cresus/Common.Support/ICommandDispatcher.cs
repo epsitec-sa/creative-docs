3ef8b2b9ec0af93c4945b216492a4bb55daa9b36
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:40:41 +0000

    ********************** Label: "Stable-25-06-2004" User: Arnaud       Date: 25.06.04   Time: 16:09 Labeled Label comment: Version with Drawing.TextStyle implemented, Support.EventHandler moved to its own assembly and script almost done.  ********************** Label: "Stable-22-06-2004" User: Arnaud       Date: 22.06.04   Time: 18:13 Labeled Label comment: First version with dynamic script compilation.  ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2611 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Support/ICommandDispatcher.cs
@@ -0,0 +1,12 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+namespace Epsitec.Common.Support
+{
+	public interface ICommandDispatcher
+	{
+		bool DispatchCommand(CommandDispatcher sender, CommandEventArgs e);
+		
+		string		Name	{ get; }
+	}
+}
---
+++ Epsitec.Cresus/Common.Support/RegexConstraint.cs
@@ -0,0 +1,192 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+using System.Text.RegularExpressions;
+
+namespace Epsitec.Common.Support
+{
+	/// <summary>
+	/// La classe RegexConstraint implémente une contrainte sur des chaînes, au
+	/// moyen d'une expression régulière (qui peut être définie de diverses
+	/// manières).
+	/// </summary>
+	
+	[System.ComponentModel.TypeConverter (typeof (RegexConstraint.Converter))]
+	
+	public class RegexConstraint : Types.IDataConstraint
+	{
+		public RegexConstraint(Regex regex)
+		{
+			this.regex   = regex;
+			this.pattern = null;
+			this.options = RegexFactory.Options.None;
+			this.pre_reg = PredefinedRegex.None;
+		}
+		
+		public RegexConstraint(string pattern, RegexFactory.Options options)
+		{
+			this.regex   = RegexFactory.FromSimpleJoker (pattern, options);
+			this.pattern = pattern;
+			this.options = options;
+			this.pre_reg = PredefinedRegex.None;
+		}
+		
+		public RegexConstraint(PredefinedRegex regex)
+		{
+			this.regex   = RegexFactory.FromPredefinedRegex (regex);
+			this.pattern = null;
+			this.options = RegexFactory.Options.None;
+			this.pre_reg = regex;
+		}
+		
+		
+		public Regex							Regex
+		{
+			get
+			{
+				return this.regex;
+			}
+		}
+		
+		public string							Pattern
+		{
+			get
+			{
+				return this.pattern;
+			}
+		}
+		
+		public RegexFactory.Options				PatternOptions
+		{
+			get
+			{
+				return this.options;
+			}
+		}
+		
+		public PredefinedRegex					PredefinedRegex
+		{
+			get
+			{
+				return this.pre_reg;
+			}
+		}
+		
+		
+		#region IDataConstraint Members
+		public bool CheckConstraint(object value)
+		{
+			string text;
+			
+			if ((this.regex != null) &&
+				(Types.Converter.Convert (value, out text)))
+			{
+				return this.regex.IsMatch (text);
+			}
+			
+			return false;
+		}
+		#endregion
+		
+		public override string ToString()
+		{
+			System.Text.StringBuilder buffer = new System.Text.StringBuilder ();
+			
+			if (this.pattern != null)
+			{
+				buffer.Append ("pat:");
+				buffer.Append (this.options.ToString ());
+				buffer.Append (":");
+				buffer.Append (this.pattern);
+			}
+			else if (this.pre_reg != PredefinedRegex.None)
+			{
+				buffer.Append ("def:");
+				buffer.Append (this.pre_reg.ToString ());
+			}
+			else if (this.regex != null)
+			{
+				buffer.Append ("exp:");
+				buffer.Append (this.regex.ToString ());
+			}
+			else
+			{
+				throw new System.InvalidOperationException ("Cannot convert empty RegexConstraint to string.");
+			}
+			
+			return buffer.ToString ();
+		}
+		
+		public static RegexConstraint FromString(string value)
+		{
+			if ((value.Length < 4) ||
+				(value[3] != ':'))
+			{
+				return null;
+			}
+			
+			switch (value.Substring (0, 3))
+			{
+				case "pat": return RegexConstraint.FromStringPattern (value.Substring (4));
+				case "def": return RegexConstraint.FromStringPredefined (value.Substring (4));
+				case "exp": return RegexConstraint.FromStringRegex (value.Substring (4));
+			}
+			
+			return null;
+		}
+		
+		
+		protected static RegexConstraint FromStringPattern(string value)
+		{
+			int pos = value.IndexOf (":");
+			
+			string arg_options = value.Substring (0, pos);
+			string arg_pattern = value.Substring (pos+1);
+			
+			System.Enum options;
+			
+			Types.Converter.Convert (arg_options, typeof (RegexFactory.Options), out options);
+			
+			return new RegexConstraint (arg_pattern, (RegexFactory.Options) options);
+		}
+		
+		protected static RegexConstraint FromStringPredefined(string value)
+		{
+			System.Enum predefined;
+			
+			Types.Converter.Convert (value, typeof (PredefinedRegex), out predefined);
+			
+			return new RegexConstraint ((PredefinedRegex) predefined);
+		}
+		
+		protected static RegexConstraint FromStringRegex(string value)
+		{
+			RegexOptions options = RegexOptions.Compiled | RegexOptions.ExplicitCapture;
+			
+			return new RegexConstraint (new Regex (value, options));
+		}
+		
+		
+		#region Converter Class
+		public class Converter : Epsitec.Common.Types.AbstractStringConverter
+		{
+			public override object ParseString(string value, System.Globalization.CultureInfo culture)
+			{
+				return RegexConstraint.FromString (value);
+			}
+			
+			public override string ToString(object value, System.Globalization.CultureInfo culture)
+			{
+				RegexConstraint regex = (RegexConstraint) value;
+				
+				return regex.ToString ();
+			}
+		}
+		#endregion
+		
+		private Regex							regex;
+		private string							pattern;
+		private RegexFactory.Options			options;
+		private PredefinedRegex					pre_reg;
+	}
+}
