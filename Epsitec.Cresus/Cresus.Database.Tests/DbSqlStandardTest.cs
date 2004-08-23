922ef416dfaffaf3990abd1b66f0f59507883698
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:25:21 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:
    **********************
    Label: "Stable-01-04-2004"
    User: Arnaud       Date:  1.04.04   Time: 12:42
    Labeled
    Label comment: TabIndex edition working in GUI.
    
    **********************
    Label: "Stable-24-03-2004"
    User: Arnaud       Date: 24.03.04   Time: 22:47
    Labeled
    Label comment: With DR contributions. First draft of Common.UI.
    
    **********************
    Label: "Stable-20-03-2004"
    User: Arnaud       Date: 20.03.04   Time: 12:18
    Labeled
    Label comment: 
    
    **********************
    Label: "Stable-15-03-2004"
    User: Arnaud       Date: 15.03.04   Time: 11:30
    Labeled
    Label comment: EditArray et App.Designer bien avancés.
    
    **********************
    Label: "12-02-2004"
    User: Arnaud       Date: 13.02.04   Time:  9:42
    Labeled
    Label comment: ScrollArray reworked.
    
    **********************
    Label: "11-02-2004"
    User: Arnaud       Date: 12.02.04   Time:  7:47
    Labeled
    Label comment: Stable version.
    
    
    **********************
    Label: "22-01-2004"
    User: Arnaud       Date: 22.01.04   Time: 15:38
    Labeled
    Label comment: Object re-bundling implemented.
    
    **********************
    Label: "21-01-2004"
    User: Arnaud       Date: 21.01.04   Time: 18:14
    Labeled
    Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.
    
    
    **********************
    Label: "19-01-2004"
    User: Arnaud       Date: 19.01.04   Time: 16:24
    Labeled
    Label comment: Before in-depth modification of XML handling in ResourceBundle.
    
    **********************
    Label: "13-01-2004"
    User: Arnaud       Date: 13.01.04   Time: 20:50
    Labeled
    Label comment: Stable, before rework of Layout management.
    
    **********************
    Label: "12.12.2003"
    User: Arnaud       Date: 13.12.03   Time: 18:39
    Labeled
    Label comment: 
    
    **********************
    Label: "08-12-2003"
    User: Arnaud       Date:  8.12.03   Time: 15:25
    Labeled
    Label comment: Stable version.
    
    git-svn-id: https://svn.opac.ch/svn/cr@1968 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Database.Tests/DbSqlStandardTest.cs
+++ Epsitec.Cresus/Cresus.Database.Tests/DbSqlStandardTest.cs
@@ -7,81 +7,81 @@ namespace Epsitec.Cresus.Database
 	{
 		[Test] public void CheckValidateName()
 		{
-			Assertion.AssertEquals (true,  DbSqlStandard.ValidateName (@"ABC_123"));
-			Assertion.AssertEquals (false, DbSqlStandard.ValidateName (@"ABC-123"));
-			Assertion.AssertEquals (false, DbSqlStandard.ValidateName (@"123_ABC"));
-			Assertion.AssertEquals (false, DbSqlStandard.ValidateName (@"ABC:ABC"));
-			Assertion.AssertEquals (true,  DbSqlStandard.ValidateName (@"""ABC-123"""));
-			Assertion.AssertEquals (true,  DbSqlStandard.ValidateName (@"""123_ABC"""));
-			Assertion.AssertEquals (true,  DbSqlStandard.ValidateName (@"""ABC:ABC"""));
-			Assertion.AssertEquals (false, DbSqlStandard.ValidateName (@"""ABC""ABC"""));
+			Assert.AreEqual (true,  DbSqlStandard.ValidateName (@"ABC_123"));
+			Assert.AreEqual (false, DbSqlStandard.ValidateName (@"ABC-123"));
+			Assert.AreEqual (false, DbSqlStandard.ValidateName (@"123_ABC"));
+			Assert.AreEqual (false, DbSqlStandard.ValidateName (@"ABC:ABC"));
+			Assert.AreEqual (true,  DbSqlStandard.ValidateName (@"""ABC-123"""));
+			Assert.AreEqual (true,  DbSqlStandard.ValidateName (@"""123_ABC"""));
+			Assert.AreEqual (true,  DbSqlStandard.ValidateName (@"""ABC:ABC"""));
+			Assert.AreEqual (false, DbSqlStandard.ValidateName (@"""ABC""ABC"""));
 		}
 		
 		[Test] public void CheckValidateString()
 		{
-			Assertion.AssertEquals (true,  DbSqlStandard.ValidateString (@"'ABC_123'"));
-			Assertion.AssertEquals (false, DbSqlStandard.ValidateString (@"ABC-123"));
-			Assertion.AssertEquals (false, DbSqlStandard.ValidateString (@"'123'ABC'"));
-			Assertion.AssertEquals (true,  DbSqlStandard.ValidateString (@"'123''ABC'"));
+			Assert.AreEqual (true,  DbSqlStandard.ValidateString (@"'ABC_123'"));
+			Assert.AreEqual (false, DbSqlStandard.ValidateString (@"ABC-123"));
+			Assert.AreEqual (false, DbSqlStandard.ValidateString (@"'123'ABC'"));
+			Assert.AreEqual (true,  DbSqlStandard.ValidateString (@"'123''ABC'"));
 		}
 		
 		[Test] public void CheckValidateNumber()
 		{
-			Assertion.AssertEquals (true,  DbSqlStandard.ValidateNumber (@"123"));
-			Assertion.AssertEquals (true,  DbSqlStandard.ValidateNumber (@"-123"));
-			Assertion.AssertEquals (false, DbSqlStandard.ValidateNumber (@"123_123"));
-			Assertion.AssertEquals (false, DbSqlStandard.ValidateNumber (@"A123"));
-			Assertion.AssertEquals (true,  DbSqlStandard.ValidateNumber (@"123.456"));
-			Assertion.AssertEquals (true,  DbSqlStandard.ValidateNumber (@"-123.4"));
-			Assertion.AssertEquals (false, DbSqlStandard.ValidateNumber (@"12.34.56"));
-			Assertion.AssertEquals (false, DbSqlStandard.ValidateNumber (@"12E3"));
+			Assert.AreEqual (true,  DbSqlStandard.ValidateNumber (@"123"));
+			Assert.AreEqual (true,  DbSqlStandard.ValidateNumber (@"-123"));
+			Assert.AreEqual (false, DbSqlStandard.ValidateNumber (@"123_123"));
+			Assert.AreEqual (false, DbSqlStandard.ValidateNumber (@"A123"));
+			Assert.AreEqual (true,  DbSqlStandard.ValidateNumber (@"123.456"));
+			Assert.AreEqual (true,  DbSqlStandard.ValidateNumber (@"-123.4"));
+			Assert.AreEqual (false, DbSqlStandard.ValidateNumber (@"12.34.56"));
+			Assert.AreEqual (false, DbSqlStandard.ValidateNumber (@"12E3"));
 		}
 		
 		[Test] public void CheckValidateQualifiedName()
 		{
-			Assertion.AssertEquals (false, DbSqlStandard.ValidateQualifiedName (@"ABC_123"));
-			Assertion.AssertEquals (true,  DbSqlStandard.ValidateQualifiedName (@"AB.C123"));
-			Assertion.AssertEquals (false, DbSqlStandard.ValidateQualifiedName (@"123.ABC"));
-			Assertion.AssertEquals (false, DbSqlStandard.ValidateQualifiedName (@"AB.AB.AB"));
-			Assertion.AssertEquals (true,  DbSqlStandard.ValidateQualifiedName (@"""ABC.123"".XYZ"));
-			Assertion.AssertEquals (true,  DbSqlStandard.ValidateQualifiedName (@"ABC.""123.ABC"""));
-			Assertion.AssertEquals (true,  DbSqlStandard.ValidateQualifiedName (@"""AB.AB.AB"".AB"));
-			Assertion.AssertEquals (false, DbSqlStandard.ValidateQualifiedName (@"""ABC""ABC"".XY"));
+			Assert.AreEqual (false, DbSqlStandard.ValidateQualifiedName (@"ABC_123"));
+			Assert.AreEqual (true,  DbSqlStandard.ValidateQualifiedName (@"AB.C123"));
+			Assert.AreEqual (false, DbSqlStandard.ValidateQualifiedName (@"123.ABC"));
+			Assert.AreEqual (false, DbSqlStandard.ValidateQualifiedName (@"AB.AB.AB"));
+			Assert.AreEqual (true,  DbSqlStandard.ValidateQualifiedName (@"""ABC.123"".XYZ"));
+			Assert.AreEqual (true,  DbSqlStandard.ValidateQualifiedName (@"ABC.""123.ABC"""));
+			Assert.AreEqual (true,  DbSqlStandard.ValidateQualifiedName (@"""AB.AB.AB"".AB"));
+			Assert.AreEqual (false, DbSqlStandard.ValidateQualifiedName (@"""ABC""ABC"".XY"));
 		}
 		
 		[Test] public void CheckConcatNames()
 		{
 			string	name;
 			name = DbSqlStandard.ConcatNames("ABC", "XYZ");
-			Assertion.AssertEquals ("ABCXYZ", name);
+			Assert.AreEqual ("ABCXYZ", name);
 			name = DbSqlStandard.ConcatNames("ABC", @"""XYZ""");
-			Assertion.AssertEquals (@"""ABCXYZ""", name);
+			Assert.AreEqual (@"""ABCXYZ""", name);
 			name = DbSqlStandard.ConcatNames(@"""ABC""", "XYZ");
-			Assertion.AssertEquals (@"""ABCXYZ""", name);
+			Assert.AreEqual (@"""ABCXYZ""", name);
 		}
 		
 		[Test] public void CheckConcatStrings()
 		{
 			string name = DbSqlStandard.ConcatStrings("'ABC'", "'123'");
-			Assertion.AssertEquals ("'ABC123'", name);
+			Assert.AreEqual ("'ABC123'", name);
 		}
 		
 		[Test] public void CheckQualifyName()
 		{
 			string name = DbSqlStandard.QualifyName ("ABC" , "XYZ");
-			Assertion.AssertEquals ("ABC.XYZ", name);
+			Assert.AreEqual ("ABC.XYZ", name);
 		}
 		
 		[Test] public void CheckSplitQualifiedName()
 		{
 			string	qualifier, name;
 			DbSqlStandard.SplitQualifiedName ("ABC.X12", out qualifier, out name);
-			Assertion.AssertEquals ("ABC", qualifier);
-			Assertion.AssertEquals ("X12", name);
+			Assert.AreEqual ("ABC", qualifier);
+			Assert.AreEqual ("X12", name);
 
 			DbSqlStandard.SplitQualifiedName ("\"ABC.XY\".Y12", out qualifier, out name);
-			Assertion.AssertEquals ("\"ABC.XY\"", qualifier);
-			Assertion.AssertEquals ("Y12", name);
+			Assert.AreEqual ("\"ABC.XY\"", qualifier);
+			Assert.AreEqual ("Y12", name);
 		}
 		
 		[Test] public void CheckCreateSqlTableName()
@@ -91,8 +91,8 @@ namespace Epsitec.Cresus.Database
 			string std1 = DbSqlStandard.CreateSqlTableName (nam1, DbElementCat.UserDataManaged, new DbKey (123));
 			string std2 = DbSqlStandard.CreateSqlTableName (nam2, DbElementCat.Internal, null);
 			
-			Assertion.AssertEquals ("U_TEST_X_123", std1);
-			Assertion.AssertEquals ("CR_TEST",      std2);
+			Assert.AreEqual ("U_TEST_X_123", std1);
+			Assert.AreEqual ("CR_TEST",      std2);
 		}
 		
 		[Test] [ExpectedException (typeof (DbException))] public void CheckCreateSqlTableNameEx1()
@@ -100,7 +100,7 @@ namespace Epsitec.Cresus.Database
 			string name = "test-x";
 			string std  = DbSqlStandard.CreateSqlTableName (name, DbElementCat.Internal, new DbKey (123));
 			
-			Assertion.AssertEquals ("U_TEST_X_123", std);
+			Assert.AreEqual ("U_TEST_X_123", std);
 		}
 	}
 }
