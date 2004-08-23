cb4e947095e0717bf9f4d2185f27bfa46c3d71c4
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:25:44 +0000

    ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1364 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/Helpers/CopyPasteBehavior.cs
@@ -0,0 +1,164 @@
+namespace Epsitec.Common.Widgets.Helpers
+{
+	/// <summary>
+	/// La classe CopyPasteBehavior gère le copier & coller.
+	/// </summary>
+	public class CopyPasteBehavior
+	{
+		public CopyPasteBehavior(Widget host)
+		{
+			this.host = host;
+		}
+		
+		
+		public Widget							Host
+		{
+			get
+			{
+				return this.host;
+			}
+		}
+		
+		
+		public bool ProcessMessage(Message message, Drawing.Point pos)
+		{
+			if (this.host.IsEnabled)
+			{
+				if (message.Type == MessageType.KeyPress)
+				{
+					if (message.IsCtrlPressed)
+					{
+						switch (message.KeyCode)
+						{
+							case KeyCode.AlphaC:
+								if (this.ProcessCopy ())
+								{
+									message.Consumer = this.host;
+								}
+								break;
+                            
+							case KeyCode.AlphaV:
+								if (this.ProcessPaste ())
+								{
+									message.Consumer = this.host;
+								}
+								break;
+							
+							case KeyCode.AlphaX:
+								if (this.ProcessCopy () && this.ProcessErase ())
+								{
+									message.Consumer = this.host;
+								}
+								break;
+							
+							case KeyCode.AlphaA:
+								if (this.ProcessSelectAll ())
+								{
+									message.Consumer = this.host;
+								}
+								break;
+						}
+					}
+				}
+			}
+			
+			return message.Handled;
+		}
+		
+		
+		public bool ProcessCopy()
+		{
+			AbstractTextField text = this.host as AbstractTextField;
+			
+			if (text != null)
+			{
+				string value = text.Selection;
+				
+				if (value == "")
+				{
+					value = text.Text;
+				}
+				
+				Support.Clipboard.WriteData data = new Support.Clipboard.WriteData ();
+				
+				data.WriteHtmlFragment (Support.Clipboard.ConvertSimpleXmlToHtml (value));
+				Support.Clipboard.SetData (data);
+				
+				return true;
+			}
+			
+			return false;
+		}
+		
+		public bool ProcessPaste()
+		{
+			AbstractTextField text = this.host as AbstractTextField;
+			
+			if (text != null)
+			{
+				Support.Clipboard.ReadData data = Support.Clipboard.GetData ();
+				
+				string html = data.ReadHtmlFragment ();
+				
+				if (html != null)
+				{
+					html = Support.Clipboard.ConvertHtmlToSimpleXml (html);
+				}
+				else
+				{
+					html = TextLayout.ConvertToTaggedText (data.ReadText ());
+				}
+				
+				if (html != null)
+				{
+					if (text.TextFieldStyle != TextFieldStyle.Multi)
+					{
+						html = html.Replace ("<br/>", " ");
+					}
+					
+					text.Selection = html;
+					return true;
+				}
+			}
+			
+			return false;
+		}
+		
+		public bool ProcessErase()
+		{
+			AbstractTextField text = this.host as AbstractTextField;
+			
+			if (text != null)
+			{
+				string value = text.Selection;
+				
+				if (value == "")
+				{
+					text.SelectAll ();
+				}
+				
+				text.DeleteSelection ();
+				
+				return true;
+			}
+			
+			return false;
+		}
+		
+		public bool ProcessSelectAll()
+		{
+			AbstractTextField text = this.host as AbstractTextField;
+			
+			if (text != null)
+			{
+				text.SelectAll ();
+				return true;
+			}
+			
+			return false;
+		}
+		
+		
+		protected Widget						host;
+	}
+}
