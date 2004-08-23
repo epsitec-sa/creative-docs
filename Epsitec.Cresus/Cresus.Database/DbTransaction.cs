b388beb5185b24941ac2052509a1b6101c415698
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 12:43:23 +0000

    ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@415 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Database/DbTransaction.cs
@@ -0,0 +1,81 @@
+//	Copyright © 2003, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : en chantier/PA
+
+namespace Epsitec.Cresus.Database
+{
+	/// <summary>
+	/// Summary description for DbTransaction.
+	/// </summary>
+	public class DbTransaction : System.IDisposable, System.Data.IDbTransaction
+	{
+		internal DbTransaction(System.Data.IDbTransaction transaction, DbInfrastructure infrastructure)
+		{
+			this.transaction    = transaction;
+			this.infrastructure = infrastructure;
+			this.infrastructure.NotifyBeginTransaction (this);
+		}
+		
+		
+		public System.Data.IDbTransaction		Transaction
+		{
+			get { return this.transaction; }
+		}
+		
+		public DbInfrastructure					Infrastructure
+		{
+			get { return this.infrastructure; }
+		}
+		
+		
+		#region IDbTransaction Members
+		public void Rollback()
+		{
+			this.transaction.Rollback ();
+			this.infrastructure.NotifyEndTransaction (this);
+			this.infrastructure = null;
+		}
+
+		public void Commit()
+		{
+			this.transaction.Commit ();
+			this.infrastructure.NotifyEndTransaction (this);
+			this.infrastructure = null;
+		}
+
+		public System.Data.IDbConnection Connection
+		{
+			get
+			{
+				return this.transaction.Connection;
+			}
+		}
+
+		public System.Data.IsolationLevel IsolationLevel
+		{
+			get
+			{
+				return this.transaction.IsolationLevel;
+			}
+		}
+		#endregion
+		
+		#region IDisposable Members
+		public void Dispose()
+		{
+			if (this.infrastructure != null)
+			{
+				this.infrastructure.NotifyEndTransaction (this);
+				this.infrastructure = null;
+			}
+			if (this.transaction != null)
+			{
+				this.transaction.Dispose ();
+				this.transaction = null;
+			}
+		}
+		#endregion
+		
+		protected System.Data.IDbTransaction	transaction;
+		protected DbInfrastructure				infrastructure;
+	}
+}
