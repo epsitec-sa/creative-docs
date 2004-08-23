a3ddb5190aa2049304498acff4f77efdbce786fc
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:44:24 +0000

    git-svn-id: https://svn.opac.ch/svn/cr@2648 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Script.Tests/DeveloperTest.cs
+++ Epsitec.Cresus/Common.Script.Tests/DeveloperTest.cs
@@ -11,19 +11,32 @@ namespace Epsitec.Common.Script
 			Common.Widgets.Adorner.Factory.SetActive ("LookMetal");
 		}
 		
+		[Test] public void CheckEditorEngine()
+		{
+			Assert.IsNotNull (Editor.Engine);
+			
+			Common.UI.Data.Record record = new Epsitec.Common.UI.Data.Record ();
+			Types.IDataValue[]    values = SourceTest.CreateValues (out record);
+			Source                source = SourceTest.CreateSource (values);
+			
+			object document = Editor.Engine.CreateDocument (source);
+			
+			Editor.Engine.ShowMethod (document, "Mysterious");
+		}
+		
 		[Test] public void CheckDeveloperUI()
 		{
 			Widgets.Window window = new Widgets.Window ();
 			
-			Common.UI.Data.Record record  = new Epsitec.Common.UI.Data.Record ();
-			Types.IDataValue[]    values  = SourceTest.CreateValues (out record);
+			Common.UI.Data.Record record = new Epsitec.Common.UI.Data.Record ();
+			Types.IDataValue[]    values = SourceTest.CreateValues (out record);
+			Source                source = SourceTest.CreateSource (values);
 			
 			Developer.EditionController controller = new Developer.EditionController ();
 			
 			window.Text       = "CheckDeveloperUI";
 			window.ClientSize = new Drawing.Size (600, 400);
 			
-			Source source = SourceTest.CreateSource (values);
 			
 			controller.Source = source;
 			controller.CreateWidgets (window.Root);
