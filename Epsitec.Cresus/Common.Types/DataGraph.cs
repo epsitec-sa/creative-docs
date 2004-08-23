d9ec1476bb18e56d32d22f969a1a1168212cc2cb
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:17:45 +0000

    ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1878 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/DataGraph.cs
@@ -0,0 +1,245 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : OK/PA, 26/04/2004
+
+namespace Epsitec.Common.Types
+{
+	/// <summary>
+	/// La classe DataGraph fournit une implémentation de base de
+	/// l'interface IDataGraph
+	/// </summary>
+	public class DataGraph : IDataGraph
+	{
+		public DataGraph()
+		{
+		}
+		
+		
+		public void DefineRoot(IDataFolder folder)
+		{
+			this.root = folder;
+		}
+		
+		
+		#region IDataGraph Members
+		public IDataFolder						Root
+		{
+			get
+			{
+				return this.root;
+			}
+		}
+
+		public IDataItem Navigate(string path)
+		{
+			return DataGraph.Navigate (path, this.Root);
+		}
+		
+		public IDataCollection Select(string query)
+		{
+			return DataGraph.Select (query, this.Root);
+		}
+		#endregion
+		
+		public static IDataItem Navigate(string path, IDataFolder start)
+		{
+			string[] names = System.Utilities.Split (path, '.');
+			
+			IDataFolder root;
+			IDataItem   item = start;
+			
+			for (int i = 0; i < names.Length; i++)
+			{
+				string name = names[i];
+				
+				if ((item.Classes & DataItemClasses.Folder) == 0)
+				{
+					throw new System.ArgumentOutOfRangeException ("path", path, string.Format ("Path element {0} does not refer to a folder.", i-1));
+				}
+				
+				root = item as IDataFolder;
+				
+				System.Diagnostics.Debug.Assert (root != null);
+				
+				if ((name.Length > 2) &&
+					(name[0] == '[') &&
+					(name[name.Length-1] == ']'))
+				{
+					int index = System.Int32.Parse (name.Substring (1, name.Length-2), System.Globalization.CultureInfo.InvariantCulture);
+					int count = root.Count;
+					
+					if ((index > 0) &&
+						(index < count))
+					{
+						item = root[index];
+					}
+					else
+					{
+						throw new System.ArgumentOutOfRangeException ("path", path, string.Format ("Path element {0} (index {1}) is not valid.", i, index));
+					}
+				}
+				else
+				{
+					item = root[name];
+					
+					if (item == null)
+					{
+						throw new System.ArgumentOutOfRangeException ("path", path, string.Format ("Path element {0} ({1}) is not valid.", i, name));
+					}
+				}
+				
+				System.Diagnostics.Debug.Assert (item != null);
+			}
+			
+			return item;
+		}
+		
+		public static IDataCollection Select(string query, IDataFolder start)
+		{
+			QueryResult result = new QueryResult ();
+			string[]    names  = System.Utilities.Split (query, '.');
+			
+			result.Select (names, start);
+			
+			return result;
+		}
+		
+		
+		#region QueryResult Class
+		protected class QueryResult : AbstractDataCollection
+		{
+			public QueryResult()
+			{
+			}
+			
+			
+			public void Select(string[] query, IDataFolder root)
+			{
+				this.Select (query, 0, root);
+				this.ClearCachedItemArray ();
+			}
+			
+			
+			protected void Select(string[] query, int offset, IDataFolder root)
+			{
+				if ((offset >= query.Length) ||
+					(root == null))
+				{
+					return;
+				}
+				
+				string  name = query[offset];
+				bool is_leaf = (offset == query.Length-1);
+				
+				if (name == "*")
+				{
+					//	Considérons tout d'abord le cas où "*" ne correspond à aucun élément :
+					
+					this.Select (query, offset+1, root);
+					
+					//	Ensuite, considérons le cas où "*" correspond à l'élément en cours, sans
+					//	puis avec descente récursive.
+					
+					foreach (IDataItem item in root)
+					{
+						if (is_leaf)
+						{
+							this.list.Add (item);
+							continue;
+						}
+						
+						if ((item.Classes & DataItemClasses.Folder) != 0)
+						{
+							this.Select (query, offset+1, item as IDataFolder);		//	sans descente récursive
+							this.Select (query, offset, item as IDataFolder);		//	avec descente récursive
+						}
+					}
+					
+					return;
+				}
+				
+				if (name == "?")
+				{
+					foreach (IDataItem item in root)
+					{
+						if (is_leaf)
+						{
+							this.list.Add (item);
+							continue;
+						}
+						
+						if ((item.Classes & DataItemClasses.Folder) != 0)
+						{
+							this.Select (query, offset+1, item as IDataFolder);
+						}
+					}
+					
+					return;
+				}
+				
+				if ((name.Length > 2) &&
+					(name[0] == '[') &&
+					(name[name.Length-1] == ']'))
+				{
+					int index = System.Int32.Parse (name.Substring (1, name.Length-2), System.Globalization.CultureInfo.InvariantCulture);
+					int count = root.Count;
+						
+					if ((index > 0) &&
+						(index < count))
+					{
+						IDataItem item = root[index];
+						
+						if (is_leaf)
+						{
+							this.list.Add (item);
+						}
+						else if ((item.Classes & DataItemClasses.Folder) != 0)
+						{
+							this.Select (query, offset+1, item as IDataFolder);
+						}
+					}
+				}
+				else
+				{
+					IDataItem item = root[name];
+					
+					if (item != null)
+					{
+						if (is_leaf)
+						{
+							this.list.Add (item);
+						}
+						else if ((item.Classes & DataItemClasses.Folder) != 0)
+						{
+							this.Select (query, offset+1, item as IDataFolder);
+						}
+					}
+				}
+			}
+			
+			
+			protected override void ClearCachedItemArray()
+			{
+				base.ClearCachedItemArray ();
+				this.items = null;
+			}
+			
+			protected override void UpdateCachedItemArray()
+			{
+				base.UpdateCachedItemArray ();
+				this.items = new IDataItem[this.list.Count];
+				this.list.CopyTo (this.items);
+			}
+			
+			protected override IDataItem[] GetCachedItemArray()
+			{
+				return this.items;
+			}
+			
+			
+			private IDataItem[]					items;
+		}
+		#endregion
+		
+		private IDataFolder						root;
+	}
+}
