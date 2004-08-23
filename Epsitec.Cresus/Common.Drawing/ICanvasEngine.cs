df499ae79b574ee4218e4595c7556db880c30598
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 12:55:58 +0000

    ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@545 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Drawing/ICanvasEngine.cs
@@ -0,0 +1,12 @@
+namespace Epsitec.Common.Drawing
+{
+	/// <summary>
+	/// L'interface ICanvasEngine définit un moteur capable de peindre dans
+	/// une surface de type Bitmap pour remplir un Canvas à une taille donnée.
+	/// </summary>
+	public interface ICanvasEngine
+	{
+		void GetSizeAndOrigin(byte[] data, out Drawing.Size size, out Drawing.Point origin);
+		void Paint(Drawing.Graphics graphics, Drawing.Size size, byte[] data);
+	}
+}
