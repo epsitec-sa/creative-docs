b9c9c562a481c2ca1fff31ce9dceeac3588159f3
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:58:40 +0000

    ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1673 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Support/IO/Decompression.cs
@@ -0,0 +1,102 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : OK/PA, 15/04/2004
+
+namespace Epsitec.Common.Support.IO
+{
+	/// <summary>
+	/// La classe Decompression supporte la décompression d'un stream
+	/// avec détection automatique du format utilisé à la compression.
+	/// </summary>
+	public class Decompression : AbstractStream
+	{
+		protected Decompression(System.IO.Stream stream)
+		{
+			this.Stream = stream;
+		}
+		
+		
+		public static System.IO.Stream CreateStream(System.IO.Stream stream)
+		{
+			string name;
+			return Decompression.CreateStream (stream, out name);
+		}
+		
+		public static System.IO.Stream CreateStream(System.IO.Stream stream, out string name)
+		{
+			byte[] header = new byte[8];
+			name = null;
+			
+			try
+			{
+				stream.Read (header, 0, 8);
+				
+				return Decompression.CreateStreamFromHeader (stream, header, out name);
+			}
+			catch (System.IO.IOException)
+			{
+			}
+			
+			return null;
+		}
+		
+		public static System.IO.Stream CreateStreamFromHeader(System.IO.Stream stream, byte[] header, out string name)
+		{
+			name = null;
+			
+			if ((header.Length == 8) &&
+				(header[0] == '$') &&
+				(header[6] == 13) &&
+				(header[7] == 10))
+			{
+				if ((header[1] == 'B') &&
+					(header[2] == 'Z') &&
+					(header[3] == 'I') &&
+					(header[4] == 'P') &&
+					(header[5] == '2'))
+				{
+					ICSharpCode.SharpZipLib.BZip2.BZip2InputStream decompressor = new ICSharpCode.SharpZipLib.BZip2.BZip2InputStream (stream);
+					
+					return new Decompression (decompressor);
+				}
+				
+				if ((header[1] == 'Z') &&
+					(header[2] == 'I') &&
+					(header[3] == 'P') &&
+					(header[4] == 32) &&
+					(header[5] == 32))
+				{
+					ICSharpCode.SharpZipLib.Zip.ZipInputStream decompressor = new ICSharpCode.SharpZipLib.Zip.ZipInputStream (stream);
+					ICSharpCode.SharpZipLib.Zip.ZipEntry       entry        = decompressor.GetNextEntry ();
+					
+					name = entry.Name;
+					
+					return new Decompression (decompressor);
+				}
+				
+				if ((header[1] == 'Z') &&
+					(header[2] == '1') &&
+					(header[3] == '9') &&
+					(header[4] == '5') &&
+					(header[5] == '1'))
+				{
+					ICSharpCode.SharpZipLib.Zip.Compression.Streams.InflaterInputStream decompressor = new ICSharpCode.SharpZipLib.Zip.Compression.Streams.InflaterInputStream (stream);
+					
+					return new Decompression (decompressor);
+				}
+				
+				if ((header[1] == 'G') &&
+					(header[2] == 'Z') &&
+					(header[3] == 'I') &&
+					(header[4] == 'P') &&
+					(header[5] == 32))
+				{
+					ICSharpCode.SharpZipLib.GZip.GZipInputStream decompressor = new ICSharpCode.SharpZipLib.GZip.GZipInputStream (stream);
+					
+					return new Decompression (decompressor);
+				}
+			}
+			
+			return null;
+		}
+	}
+}
