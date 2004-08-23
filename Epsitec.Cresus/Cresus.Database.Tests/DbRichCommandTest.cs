97f37002901a2714f6ed9eae8f9d6c1b84035455
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 12:43:04 +0000

    ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@411 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Database.Tests/DbRichCommandTest.cs
@@ -0,0 +1,88 @@
+using NUnit.Framework;
+
+namespace Epsitec.Cresus.Database
+{
+	[TestFixture] public class DbRichCommandTest
+	{
+		[Test] public void CheckSelect()
+		{
+			DbInfrastructure infrastructure = DbInfrastructureTest.GetInfrastructureFromBase ("fiche", false);
+			ISqlBuilder sql_builder = infrastructure.SqlBuilder;
+			ISqlEngine  sql_engine  = infrastructure.SqlEngine;
+			
+			DbTable db_table_a = infrastructure.CreateDbTable ("Personnes", DbElementCat.UserDataManaged);
+			DbTable db_table_b = infrastructure.CreateDbTable ("Domiciles", DbElementCat.UserDataManaged);
+			
+			DbType db_type_name = infrastructure.ResolveDbType (null, "CR.Name");
+			DbType db_type_id   = infrastructure.ResolveDbType (null, "CR.KeyId");
+			DbType db_type_rev  = infrastructure.ResolveDbType (null, "CR.KeyRevision");
+			
+			db_table_a.Columns.Add (new DbColumn ("Nom", db_type_name));
+			db_table_a.Columns.Add (new DbColumn ("Prenom", db_type_name));
+			
+			db_table_b.Columns.Add (new DbColumn ("RefId",  db_type_id));
+			db_table_b.Columns.Add (new DbColumn ("RefRev", db_type_rev));
+			db_table_b.Columns.Add (new DbColumn ("Ville", db_type_name));
+			
+			infrastructure.RegisterNewDbTable (null, db_table_a);
+			infrastructure.RegisterNewDbTable (null, db_table_b);
+			
+			SqlSelect select_a = new SqlSelect ();
+			SqlSelect select_b = new SqlSelect ();
+			
+			select_a.Fields.Add (SqlField.CreateAll ());
+			select_b.Fields.Add (SqlField.CreateAll ());
+			
+			select_a.Tables.Add ("A", SqlField.CreateName (db_table_a.CreateSqlName ()));
+			select_b.Tables.Add ("B", SqlField.CreateName (db_table_b.CreateSqlName ()));
+			
+			System.Data.IDbTransaction transaction = infrastructure.BeginTransaction ();
+			
+			DbRichCommand command = new DbRichCommand ();
+			
+			sql_builder.SelectData (select_a);
+			command.Commands.Add (sql_builder.Command);
+			
+			sql_builder.SelectData (select_b);
+			command.Commands.Add (sql_builder.Command);
+			
+			command.Transaction = transaction;
+			command.Tables.Add (db_table_a);
+			command.Tables.Add (db_table_b);
+			
+			sql_engine.Execute (command);
+			
+			System.Console.Out.WriteLine ("Tables : {0}", command.DataSet.Tables.Count);
+			foreach (System.Data.DataTable table in command.DataSet.Tables)
+			{
+				System.Console.Out.WriteLine ("Table {0} has {1} columns:", table.TableName, table.Columns.Count);
+				foreach (System.Data.DataColumn column in table.Columns)
+				{
+					System.Console.Out.WriteLine ("  {0} {1} {2}", column.ColumnName, column.Unique, column.DataType);
+				}
+			}
+			
+			System.Data.DataTable ado_table_a = command.DataSet.Tables["Personnes"];
+			System.Data.DataTable ado_table_b = command.DataSet.Tables["Domiciles"];
+			
+			ado_table_a.Rows.Add (new object[] { 1, 0, 0, "Arnaud",   "Pierre" });
+			ado_table_a.Rows.Add (new object[] { 2, 0, 0, "Dumoulin", "Denis" });
+			ado_table_a.Rows.Add (new object[] { 3, 0, 0, "Roux",     "Daniel" });
+			
+			ado_table_b.Rows.Add (new object[] { 1, 0, 0, 1, 0, "Yverdon-les-Bains" });
+			ado_table_b.Rows.Add (new object[] { 2, 0, 0, 2, 0, "Morges" });
+			ado_table_b.Rows.Add (new object[] { 3, 0, 0, 2, 0, "Saverne" });
+			ado_table_b.Rows.Add (new object[] { 4, 0, 0, 3, 0, "Crissier" });
+			
+			DbInfrastructureTest.DisplayDataSet (infrastructure, ado_table_a.TableName, ado_table_a);
+			DbInfrastructureTest.DisplayDataSet (infrastructure, ado_table_b.TableName, ado_table_b);
+			
+			command.UpdateTables ();
+			
+			transaction.Commit ();
+			
+			infrastructure.UnregisterDbTable (null, db_table_a);
+			infrastructure.UnregisterDbTable (null, db_table_b);
+		}
+	}
+}
