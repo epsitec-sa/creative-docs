3969908e043363e1bbc0a1fba10b041b90b153ab
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:25:48 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:
    **********************
    Label: "Stable-01-04-2004"
    User: Arnaud       Date:  1.04.04   Time: 12:42
    Labeled
    Label comment: TabIndex edition working in GUI.
    
    **********************
    Label: "Stable-24-03-2004"
    User: Arnaud       Date: 24.03.04   Time: 22:47
    Labeled
    Label comment: With DR contributions. First draft of Common.UI.
    
    **********************
    Label: "Stable-20-03-2004"
    User: Arnaud       Date: 20.03.04   Time: 12:18
    Labeled
    Label comment: 
    
    **********************
    Label: "Stable-15-03-2004"
    User: Arnaud       Date: 15.03.04   Time: 11:30
    Labeled
    Label comment: EditArray et App.Designer bien avancés.
    
    **********************
    Label: "12-02-2004"
    User: Arnaud       Date: 13.02.04   Time:  9:42
    Labeled
    Label comment: ScrollArray reworked.
    
    **********************
    Label: "11-02-2004"
    User: Arnaud       Date: 12.02.04   Time:  7:47
    Labeled
    Label comment: Stable version.
    
    
    **********************
    Label: "22-01-2004"
    User: Arnaud       Date: 22.01.04   Time: 15:38
    Labeled
    Label comment: Object re-bundling implemented.
    
    **********************
    Label: "21-01-2004"
    User: Arnaud       Date: 21.01.04   Time: 18:14
    Labeled
    Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.
    
    
    **********************
    Label: "19-01-2004"
    User: Arnaud       Date: 19.01.04   Time: 16:24
    Labeled
    Label comment: Before in-depth modification of XML handling in ResourceBundle.
    
    **********************
    Label: "13-01-2004"
    User: Arnaud       Date: 13.01.04   Time: 20:50
    Labeled
    Label comment: Stable, before rework of Layout management.
    
    **********************
    Label: "12.12.2003"
    User: Arnaud       Date: 13.12.03   Time: 18:39
    Labeled
    Label comment: 
    
    **********************
    Label: "08-12-2003"
    User: Arnaud       Date:  8.12.03   Time: 15:25
    Labeled
    Label comment: Stable version.
    
    git-svn-id: https://svn.opac.ch/svn/cr@1976 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Database.Tests/SqlFieldTest.cs
+++ Epsitec.Cresus/Cresus.Database.Tests/SqlFieldTest.cs
@@ -12,48 +12,48 @@ namespace Epsitec.Cresus.Database
 			//	constructeurs, avec nom qualifié ou non
 			
 			sql_field = SqlField.CreateName ("TestNotQualified");
-			Assertion.AssertEquals (SqlFieldType.Name, sql_field.Type);
-			Assertion.AssertEquals ("TestNotQualified", sql_field.AsName);
-			Assertion.AssertEquals (null, sql_field.AsQualifiedName);
-			Assertion.AssertEquals (null, sql_field.Alias);
-			Assertion.AssertEquals (SqlFieldOrder.None, sql_field.Order);
-			Assertion.AssertEquals (DbRawType.Unknown, sql_field.RawType);
+			Assert.AreEqual (SqlFieldType.Name, sql_field.Type);
+			Assert.AreEqual ("TestNotQualified", sql_field.AsName);
+			Assert.AreEqual (null, sql_field.AsQualifiedName);
+			Assert.AreEqual (null, sql_field.Alias);
+			Assert.AreEqual (SqlFieldOrder.None, sql_field.Order);
+			Assert.AreEqual (DbRawType.Unknown, sql_field.RawType);
 			
 			sql_field = SqlField.CreateName ("Test", "Qualified");
-			Assertion.AssertEquals (SqlFieldType.QualifiedName, sql_field.Type);
-			Assertion.AssertEquals ("Test.Qualified", sql_field.AsQualifiedName);
-			Assertion.AssertEquals ("Qualified", sql_field.AsName);
-			Assertion.AssertEquals (null, sql_field.Alias);
-			Assertion.AssertEquals (SqlFieldOrder.None, sql_field.Order);
-			Assertion.AssertEquals (DbRawType.Unknown, sql_field.RawType);
+			Assert.AreEqual (SqlFieldType.QualifiedName, sql_field.Type);
+			Assert.AreEqual ("Test.Qualified", sql_field.AsQualifiedName);
+			Assert.AreEqual ("Qualified", sql_field.AsName);
+			Assert.AreEqual (null, sql_field.Alias);
+			Assert.AreEqual (SqlFieldOrder.None, sql_field.Order);
+			Assert.AreEqual (DbRawType.Unknown, sql_field.RawType);
 			
 			sql_field.Alias = "SetAlias";
-			Assertion.AssertEquals ("SetAlias", sql_field.Alias);
+			Assert.AreEqual ("SetAlias", sql_field.Alias);
 			sql_field.Order = SqlFieldOrder.Normal;
-			Assertion.AssertEquals (SqlFieldOrder.Normal, sql_field.Order);
+			Assert.AreEqual (SqlFieldOrder.Normal, sql_field.Order);
 			
 			//	constructeurs, avec un alias
 			
 			sql_field = SqlField.CreateName ("TestNotQualified");
 			sql_field.Alias = "NotQ";
-			Assertion.AssertEquals (SqlFieldType.Name, sql_field.Type);
-			Assertion.AssertEquals ("TestNotQualified", sql_field.AsName);
-			Assertion.AssertEquals (null, sql_field.AsQualifiedName);
-			Assertion.AssertEquals ("NotQ", sql_field.Alias);
-			Assertion.AssertEquals (SqlFieldOrder.None, sql_field.Order);
-			Assertion.AssertEquals (DbRawType.Unknown, sql_field.RawType);
+			Assert.AreEqual (SqlFieldType.Name, sql_field.Type);
+			Assert.AreEqual ("TestNotQualified", sql_field.AsName);
+			Assert.AreEqual (null, sql_field.AsQualifiedName);
+			Assert.AreEqual ("NotQ", sql_field.Alias);
+			Assert.AreEqual (SqlFieldOrder.None, sql_field.Order);
+			Assert.AreEqual (DbRawType.Unknown, sql_field.RawType);
 			
 			//	constructeurs, avec un alias et un ordre de tri
 			
 			sql_field = SqlField.CreateName ("Test", "Qualified");
 			sql_field.Alias = "TQ";
 			sql_field.Order = SqlFieldOrder.Inverse;
-			Assertion.AssertEquals (SqlFieldType.QualifiedName, sql_field.Type);
-			Assertion.AssertEquals ("Test.Qualified", sql_field.AsQualifiedName);
-			Assertion.AssertEquals ("Qualified", sql_field.AsName);
-			Assertion.AssertEquals ("TQ", sql_field.Alias);
-			Assertion.AssertEquals (SqlFieldOrder.Inverse, sql_field.Order);
-			Assertion.AssertEquals (DbRawType.Unknown, sql_field.RawType);
+			Assert.AreEqual (SqlFieldType.QualifiedName, sql_field.Type);
+			Assert.AreEqual ("Test.Qualified", sql_field.AsQualifiedName);
+			Assert.AreEqual ("Qualified", sql_field.AsName);
+			Assert.AreEqual ("TQ", sql_field.Alias);
+			Assert.AreEqual (SqlFieldOrder.Inverse, sql_field.Order);
+			Assert.AreEqual (DbRawType.Unknown, sql_field.RawType);
 		}
 	
 		[Test] public void CheckValidate()
@@ -67,16 +67,16 @@ namespace Epsitec.Cresus.Database
 			SqlField sql_field;
 
 			sql_field = SqlField.CreateName ("TestNotQualifiedButValid");
-			Assertion.AssertEquals (SqlFieldType.Name, sql_field.Type);
-			Assertion.AssertEquals ("TestNotQualifiedButValid", sql_field.AsName);
-			Assertion.AssertEquals (true, sql_field.Validate(sql_builder));
+			Assert.AreEqual (SqlFieldType.Name, sql_field.Type);
+			Assert.AreEqual ("TestNotQualifiedButValid", sql_field.AsName);
+			Assert.AreEqual (true, sql_field.Validate(sql_builder));
 
 			sql_field = SqlField.CreateName ("Test", "QualifiedValid");
 			sql_field.Alias = "TQ";
 			sql_field.Order = SqlFieldOrder.Inverse;
-			Assertion.AssertEquals (SqlFieldType.QualifiedName, sql_field.Type);
-			Assertion.AssertEquals ("Test.QualifiedValid", sql_field.AsQualifiedName);
-			Assertion.AssertEquals (true, sql_field.Validate(sql_builder));
+			Assert.AreEqual (SqlFieldType.QualifiedName, sql_field.Type);
+			Assert.AreEqual ("Test.QualifiedValid", sql_field.AsQualifiedName);
+			Assert.AreEqual (true, sql_field.Validate(sql_builder));
 		}
 		
 		[Test] [ExpectedException (typeof (DbFormatException))] public void CheckValidateEx1()
@@ -102,112 +102,112 @@ namespace Epsitec.Cresus.Database
 
 			//	crée un champ nul
 			sql_field = SqlField.CreateNull ();
-			Assertion.AssertEquals (SqlFieldType.Null, sql_field.Type);
+			Assert.AreEqual (SqlFieldType.Null, sql_field.Type);
 			
 			//	crée un champ défaut
 			sql_field = SqlField.CreateDefault ();
-			Assertion.AssertEquals (SqlFieldType.Default, sql_field.Type);
+			Assert.AreEqual (SqlFieldType.Default, sql_field.Type);
 
 			//	crée un champ pour une constante
 			sql_field = SqlField.CreateConstant (123.45M, DbRawType.LargeDecimal);
-			Assertion.AssertEquals (SqlFieldType.Constant, sql_field.Type);
-			Assertion.AssertEquals (DbRawType.LargeDecimal, sql_field.RawType);
-			Assertion.AssertEquals (123.45M, sql_field.AsConstant);
+			Assert.AreEqual (SqlFieldType.Constant, sql_field.Type);
+			Assert.AreEqual (DbRawType.LargeDecimal, sql_field.RawType);
+			Assert.AreEqual (123.45M, sql_field.AsConstant);
 
 			//	crée un champ pour un paramètre d'entrée (== constante)
 			object val = System.Guid.NewGuid ();
 			sql_field = SqlField.CreateParameterIn (val, DbRawType.Guid);
-			Assertion.AssertEquals (SqlFieldType.ParameterIn, sql_field.Type);
-			Assertion.AssertEquals (DbRawType.Guid, sql_field.RawType);
-			Assertion.AssertEquals (val, sql_field.AsParameter);
+			Assert.AreEqual (SqlFieldType.ParameterIn, sql_field.Type);
+			Assert.AreEqual (DbRawType.Guid, sql_field.RawType);
+			Assert.AreEqual (val, sql_field.AsParameter);
 
 			//	crée un champ pour un paramètre de sortie
 			sql_field = SqlField.CreateParameterOut (DbRawType.String);
-			Assertion.AssertEquals (SqlFieldType.ParameterOut, sql_field.Type);
-			Assertion.AssertEquals (DbRawType.String, sql_field.RawType);
-			Assertion.AssertEquals (null, sql_field.AsParameter);
+			Assert.AreEqual (SqlFieldType.ParameterOut, sql_field.Type);
+			Assert.AreEqual (DbRawType.String, sql_field.RawType);
+			Assert.AreEqual (null, sql_field.AsParameter);
 
 			//	crée un champ pour un paramètre en entrée/sortie
 			sql_field = SqlField.CreateParameterInOut ("abc", DbRawType.String);
-			Assertion.AssertEquals (SqlFieldType.ParameterInOut, sql_field.Type);
-			Assertion.AssertEquals (DbRawType.String, sql_field.RawType);
-			Assertion.AssertEquals ("abc", sql_field.AsParameter);
+			Assert.AreEqual (SqlFieldType.ParameterInOut, sql_field.Type);
+			Assert.AreEqual (DbRawType.String, sql_field.RawType);
+			Assert.AreEqual ("abc", sql_field.AsParameter);
 
 			//	crée un champ pour un résultat de fonction
 			sql_field = SqlField.CreateParameterResult (DbRawType.Time);
-			Assertion.AssertEquals (SqlFieldType.ParameterResult, sql_field.Type);
-			Assertion.AssertEquals (DbRawType.Time, sql_field.RawType);
-			Assertion.AssertEquals (null, sql_field.AsParameter);
+			Assert.AreEqual (SqlFieldType.ParameterResult, sql_field.Type);
+			Assert.AreEqual (DbRawType.Time, sql_field.RawType);
+			Assert.AreEqual (null, sql_field.AsParameter);
 
 			// place la valeur du résultat (il n'y a pas de contrôle de type ici
 			sql_field.SetParameterOutResult("abc");
-			Assertion.AssertEquals ("abc", sql_field.AsParameter);
+			Assert.AreEqual ("abc", sql_field.AsParameter);
 
 			//	crée un champ pour une extraction sur tout ( * ) 
 			sql_field = SqlField.CreateAll();
-			Assertion.AssertEquals (SqlFieldType.All, sql_field.Type);
-			Assertion.AssertEquals (null, sql_field.AsName);
-			Assertion.AssertEquals (null, sql_field.AsQualifiedName);
-			Assertion.AssertEquals (null, sql_field.Alias);
-			Assertion.AssertEquals (SqlFieldOrder.None, sql_field.Order);
-			Assertion.AssertEquals (DbRawType.Unknown, sql_field.RawType);
+			Assert.AreEqual (SqlFieldType.All, sql_field.Type);
+			Assert.AreEqual (null, sql_field.AsName);
+			Assert.AreEqual (null, sql_field.AsQualifiedName);
+			Assert.AreEqual (null, sql_field.Alias);
+			Assert.AreEqual (SqlFieldOrder.None, sql_field.Order);
+			Assert.AreEqual (DbRawType.Unknown, sql_field.RawType);
 
 			//	crée un champ pour un nom (qualifié ou non)
 			sql_field = SqlField.CreateName ("Test", "Qualified");
-			Assertion.AssertEquals (SqlFieldType.QualifiedName, sql_field.Type);
-			Assertion.AssertEquals ("Test.Qualified", sql_field.AsQualifiedName);
-			Assertion.AssertEquals ("Qualified", sql_field.AsName);
-			Assertion.AssertEquals (null, sql_field.Alias);
-			Assertion.AssertEquals (SqlFieldOrder.None, sql_field.Order);
-			Assertion.AssertEquals (DbRawType.Unknown, sql_field.RawType);
+			Assert.AreEqual (SqlFieldType.QualifiedName, sql_field.Type);
+			Assert.AreEqual ("Test.Qualified", sql_field.AsQualifiedName);
+			Assert.AreEqual ("Qualified", sql_field.AsName);
+			Assert.AreEqual (null, sql_field.Alias);
+			Assert.AreEqual (SqlFieldOrder.None, sql_field.Order);
+			Assert.AreEqual (DbRawType.Unknown, sql_field.RawType);
 
 			//	crée un champ pour un nom de table / colonne
 			sql_field1 = SqlField.CreateName ("Table", "Colonne");
-			Assertion.AssertEquals (SqlFieldType.QualifiedName, sql_field1.Type);
-			Assertion.AssertEquals ("Table.Colonne", sql_field1.AsQualifiedName);
-			Assertion.AssertEquals ("Colonne", sql_field1.AsName);
-			Assertion.AssertEquals (null, sql_field1.Alias);
-			Assertion.AssertEquals (SqlFieldOrder.None, sql_field1.Order);
-			Assertion.AssertEquals (DbRawType.Unknown, sql_field1.RawType);
+			Assert.AreEqual (SqlFieldType.QualifiedName, sql_field1.Type);
+			Assert.AreEqual ("Table.Colonne", sql_field1.AsQualifiedName);
+			Assert.AreEqual ("Colonne", sql_field1.AsName);
+			Assert.AreEqual (null, sql_field1.Alias);
+			Assert.AreEqual (SqlFieldOrder.None, sql_field1.Order);
+			Assert.AreEqual (DbRawType.Unknown, sql_field1.RawType);
 
 			//	crée un champ pour un aggrégat concernant la colonne ci-dessus
 			sql_field2 = SqlField.CreateAggregate (new SqlAggregate (SqlAggregateType.Sum, sql_field1));
-			Assertion.AssertEquals (SqlFieldType.Aggregate, sql_field2.Type);
-			Assertion.AssertEquals (SqlAggregateType.Sum, sql_field2.AsAggregate.Type);
-			Assertion.AssertEquals (null, sql_field2.AsQualifiedName);
-			Assertion.AssertEquals (null, sql_field2.AsName);
-			Assertion.AssertEquals (null, sql_field2.Alias);
-			Assertion.AssertEquals (SqlFieldOrder.None, sql_field2.Order);
-			Assertion.AssertEquals (DbRawType.Unknown, sql_field2.RawType);
+			Assert.AreEqual (SqlFieldType.Aggregate, sql_field2.Type);
+			Assert.AreEqual (SqlAggregateType.Sum, sql_field2.AsAggregate.Type);
+			Assert.AreEqual (null, sql_field2.AsQualifiedName);
+			Assert.AreEqual (null, sql_field2.AsName);
+			Assert.AreEqual (null, sql_field2.Alias);
+			Assert.AreEqual (SqlFieldOrder.None, sql_field2.Order);
+			Assert.AreEqual (DbRawType.Unknown, sql_field2.RawType);
 
 			//	idem en donnant juste le type d'aggrégat souhaité
 			sql_field2 = SqlField.CreateAggregate (SqlAggregateType.Count, sql_field1);
-			Assertion.AssertEquals (SqlFieldType.Aggregate, sql_field2.Type);
-			Assertion.AssertEquals (SqlAggregateType.Count, sql_field2.AsAggregate.Type);
-			Assertion.AssertEquals (null, sql_field2.AsQualifiedName);
-			Assertion.AssertEquals (null, sql_field2.AsName);
-			Assertion.AssertEquals (null, sql_field2.Alias);
-			Assertion.AssertEquals (SqlFieldOrder.None, sql_field2.Order);
-			Assertion.AssertEquals (DbRawType.Unknown, sql_field2.RawType);
+			Assert.AreEqual (SqlFieldType.Aggregate, sql_field2.Type);
+			Assert.AreEqual (SqlAggregateType.Count, sql_field2.AsAggregate.Type);
+			Assert.AreEqual (null, sql_field2.AsQualifiedName);
+			Assert.AreEqual (null, sql_field2.AsName);
+			Assert.AreEqual (null, sql_field2.Alias);
+			Assert.AreEqual (SqlFieldOrder.None, sql_field2.Order);
+			Assert.AreEqual (DbRawType.Unknown, sql_field2.RawType);
 
 			//	crée un champ pour une varialble
 			sql_field = SqlField.CreateVariable ();
-			Assertion.AssertEquals (SqlFieldType.Variable, sql_field.Type);
+			Assert.AreEqual (SqlFieldType.Variable, sql_field.Type);
 
 			//	crée un champ pour une fonction
 			sql_field = SqlField.CreateFunction (null);
-			Assertion.AssertEquals (SqlFieldType.Function, sql_field.Type);
+			Assert.AreEqual (SqlFieldType.Function, sql_field.Type);
 
 			//	crée un champ pour une procédure
 			sql_field = SqlField.CreateProcedure ("ProcedureName");
-			Assertion.AssertEquals (SqlFieldType.Procedure, sql_field.Type);
-			Assertion.AssertEquals ("ProcedureName", sql_field.AsProcedure);
+			Assert.AreEqual (SqlFieldType.Procedure, sql_field.Type);
+			Assert.AreEqual ("ProcedureName", sql_field.AsProcedure);
 
 			//	crée un champ pour une sous-requête
 			SqlSelect select = new SqlSelect ();
 			sql_field = SqlField.CreateSubQuery (select);
-			Assertion.AssertEquals (SqlFieldType.SubQuery, sql_field.Type);
-			Assertion.AssertEquals (select, sql_field.AsSubQuery);
+			Assert.AreEqual (SqlFieldType.SubQuery, sql_field.Type);
+			Assert.AreEqual (select, sql_field.AsSubQuery);
 		}
 	}
 }
