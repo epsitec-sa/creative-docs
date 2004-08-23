5cf817713175e1ddef52ce4962b977c05e25d139
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:00:47 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2276 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Dialogs.Tests/MessageTest.cs
@@ -0,0 +1,50 @@
+using NUnit.Framework;
+
+namespace Epsitec.Common.Dialogs
+{
+	using CommandAttribute = Support.CommandAttribute;
+	
+	[TestFixture] public class MessageTest
+	{
+		[SetUp] public void Initialise()
+		{
+			Common.Widgets.Widget.Initialise ();
+			Common.Pictogram.Engine.Initialise ();
+			Common.Widgets.Adorner.Factory.SetActive ("LookMetal");
+		}
+		
+		public MessageTest()
+		{
+			this.dispatcher = new Support.CommandDispatcher ("MessageTestDispatcher", true);
+			this.dispatcher.RegisterController (this);
+		}
+		
+		[Test] public void CheckYesNoCancel()
+		{
+			IDialog dialog = Message.CreateYesNoCancel ("Notepad", "manifest:Epsitec.Common.Dialogs.Images.Warning.icon", "The text in the <i>c:\\Documents and Settings\\Tester\\Desktop\\dummy file.txt</i> file has changed.<br/><br/>Do you want to save the changes ?", "ExecYes", "ExecNo", this.dispatcher);
+			
+			dialog.Show ();
+		}
+
+		[Test] public void CheckOkCancel()
+		{
+			IDialog dialog = Message.CreateOkCancel ("Notepad", "manifest:Epsitec.Common.Dialogs.Images.Warning.icon", "About to mogrify this document...", "ExecYes", this.dispatcher);
+			
+			dialog.Show ();
+		}
+
+		
+		[Command ("ExecYes")] private void CommandExecYes()
+		{
+			System.Diagnostics.Debug.WriteLine ("Yes...");
+		}
+		
+		[Command ("ExecNo")]  private void CommandExecNo()
+		{
+			System.Diagnostics.Debug.WriteLine ("No...");
+		}
+		
+		
+		private Support.CommandDispatcher		dispatcher;
+	}
+}
