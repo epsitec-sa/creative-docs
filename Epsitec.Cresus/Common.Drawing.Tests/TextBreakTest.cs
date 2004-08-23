1947d6b7dd3a5e625e0a43d54b87be4e760645ab
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:43:49 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2120 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Drawing.Tests/TextBreakTest.cs
@@ -0,0 +1,69 @@
+using NUnit.Framework;
+
+namespace Epsitec.Common.Drawing
+{
+	[TestFixture] public class TextBreakTest
+	{
+		[Test] public void CheckBreak()
+		{
+			TextBreakNew tb = new TextBreakNew ();
+			
+			string[] text = new string[3];
+			
+			text[0] = "Bonjour, ";
+			text[1] = "comment";
+			text[2] = " va-t-il ? C'est   juste une question.";
+			
+			tb.SetText (string.Concat (text), TextBreakMode.Hyphenate | TextBreakMode.Ellipsis);
+			
+			Font[] fonts = new Font[2];
+			
+			fonts[0] = Font.GetFont ("Tahoma", "Regular");
+			fonts[1] = Font.GetFont ("Tahoma", "Bold");
+			
+			int[]    run_lengths = new int[3];
+			int[]    run_fonts   = new int[3];
+			double[] run_sizes   = new double[3];
+			
+			run_lengths[0] = text[0].Length;	run_fonts[0] = 0;	run_sizes[0] = 12.0;
+			run_lengths[1] = text[1].Length;	run_fonts[1] = 1;	run_sizes[1] = 12.0;
+			run_lengths[2] = text[2].Length;	run_fonts[2] = 0;	run_sizes[2] = 12.0;
+			
+			tb.SetFonts (fonts);
+			tb.SetRuns (run_lengths, run_fonts, run_sizes);
+			
+			double space = 300;
+			double width;
+			int    n_char;
+			string chunk;
+			
+			double last_width  = -1;
+			int    last_n_char = -1;
+			
+			while (space > 1)
+			{
+				space--;
+				
+				tb.Rewind ();
+				tb.GetNextBreak (space, out chunk, out width, out n_char);
+				
+				if ((n_char == last_n_char) &&
+					(width  == last_width))
+				{
+					continue;
+				}
+				
+				double width_2;
+				int    n_char_2;
+				string chunk_2;
+				
+				tb.GetNextBreak (1000, out chunk_2, out width_2, out n_char_2);
+				
+				System.Console.Out.WriteLine ("{0}, used {1} => {2} chars,\t'{3}'//'{4}'", space, (int) width, n_char, chunk, chunk_2);
+				
+				last_width  = width;
+				last_n_char = n_char;
+			}
+		}
+	}
+}
