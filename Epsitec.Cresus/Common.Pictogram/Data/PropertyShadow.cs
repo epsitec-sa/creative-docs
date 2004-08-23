1f347fbdb260f2bf0a5a1153ae6d38c7601de468
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:04:19 +0000

    ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@628 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Pictogram/Data/PropertyShadow.cs
@@ -0,0 +1,141 @@
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Pictogram.Widgets;
+using System.Xml.Serialization;
+
+namespace Epsitec.Common.Pictogram.Data
+{
+	/// <summary>
+	/// La classe PropertyShadow représente une propriété d'un objet graphique.
+	/// </summary>
+	public class PropertyShadow : AbstractProperty
+	{
+		public PropertyShadow()
+		{
+			this.color  = Drawing.Color.FromARGB(0.0, 0.5, 0.5, 0.5);
+			this.radius =  2.0;
+			this.ox     =  1.0;
+			this.oy     = -1.0;
+		}
+
+		// Couleur de l'ombre.
+		public Drawing.Color Color
+		{
+			get
+			{
+				return this.color;
+			}
+
+			set
+			{
+				this.color = value;
+			}
+		}
+
+		// Rayon de l'ombre.
+		[XmlAttribute]
+		public double Radius
+		{
+			get
+			{
+				return this.radius;
+			}
+
+			set
+			{
+				this.radius = value;
+			}
+		}
+
+		// Offset x de l'ombre.
+		[XmlAttribute]
+		public double Ox
+		{
+			get
+			{
+				return this.ox;
+			}
+
+			set
+			{
+				this.ox = value;
+			}
+		}
+
+		// Offset y de l'ombre.
+		[XmlAttribute]
+		public double Oy
+		{
+			get
+			{
+				return this.oy;
+			}
+
+			set
+			{
+				this.oy = value;
+			}
+		}
+
+		// Effectue une copie de la propriété.
+		public override void CopyTo(AbstractProperty property)
+		{
+			base.CopyTo(property);
+			PropertyShadow p = property as PropertyShadow;
+			p.Color  = this.color;
+			p.Radius = this.radius;
+			p.Ox     = this.ox;
+			p.Oy     = this.oy;
+		}
+
+		// Compare deux propriétés.
+		public override bool Compare(AbstractProperty property)
+		{
+			if ( !base.Compare(property) )  return false;
+
+			PropertyShadow p = property as PropertyShadow;
+			if ( p.Color  != this.color  )  return false;
+			if ( p.Radius != this.radius )  return false;
+			if ( p.Ox     != this.ox     )  return false;
+			if ( p.Oy     != this.oy     )  return false;
+
+			return true;
+		}
+
+		// Crée le panneau permettant d'éditer la propriété.
+		public override AbstractPanel CreatePanel()
+		{
+			return new PanelShadow();
+		}
+
+
+		// Effectue le rendu d'un chemin flou.
+		public void Render(Drawing.Graphics graphics, IconContext iconContext, Drawing.Path path)
+		{
+			if ( this.color.A == 0 )  return;
+
+			Drawing.Transform save = graphics.SaveTransform();
+			graphics.TranslateTransform(this.ox, this.oy);
+
+			if ( this.radius == 0 )
+			{
+				graphics.Rasterizer.AddSurface(path);
+				graphics.RenderSolid(this.color);
+			}
+			else
+			{
+				graphics.SmoothRenderer.Color = this.color;
+				graphics.SmoothRenderer.SetParameters(this.radius*iconContext.ScaleX, this.radius*iconContext.ScaleY);
+				graphics.SmoothRenderer.AddPath(path);
+			}
+
+			graphics.Transform = save;
+		}
+
+
+		[XmlAttribute]
+		protected Drawing.Color			color;
+		protected double				radius;
+		protected double				ox;
+		protected double				oy;
+	}
+}
