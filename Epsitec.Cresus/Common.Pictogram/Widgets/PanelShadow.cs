4ce0194e154bab1fa02e4a92ed0133bf60c61c65
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:04:31 +0000

    ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@630 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Pictogram/Widgets/PanelShadow.cs
@@ -0,0 +1,197 @@
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Pictogram.Data;
+
+namespace Epsitec.Common.Pictogram.Widgets
+{
+	/// <summary>
+	/// La classe PanelShadow permet de choisir une ombre.
+	/// </summary>
+	public class PanelShadow : AbstractPanel
+	{
+		public PanelShadow()
+		{
+			this.label = new StaticText(this);
+			this.label.Alignment = Drawing.ContentAlignment.MiddleLeft;
+
+			this.fieldColor = new ColorSample(this);
+			this.fieldColor.PossibleOrigin = true;
+			this.fieldColor.Clicked += new MessageEventHandler(this.FieldColorClicked);
+
+			this.fieldRadius = new TextFieldSlider(this);
+			this.fieldRadius.MinRange =  0;
+			this.fieldRadius.MaxRange = 10;
+			this.fieldRadius.Step = 1;
+			this.fieldRadius.TextChanged += new EventHandler(this.TextChanged);
+
+			this.fieldOx = new TextFieldSlider(this);
+			this.fieldOx.MinRange = -10;
+			this.fieldOx.MaxRange =  10;
+			this.fieldOx.Step = 1;
+			this.fieldOx.TextChanged += new EventHandler(this.TextChanged);
+
+			this.fieldOy = new TextFieldSlider(this);
+			this.fieldOy.MinRange = -10;
+			this.fieldOy.MaxRange =  10;
+			this.fieldOy.Step = 1;
+			this.fieldOy.TextChanged += new EventHandler(this.TextChanged);
+
+			this.labelRadius = new StaticText(this);
+			this.labelRadius.Text = "R";
+			this.labelRadius.Alignment = Drawing.ContentAlignment.MiddleCenter;
+
+			this.labelOx = new StaticText(this);
+			this.labelOx.Text = "X";
+			this.labelOx.Alignment = Drawing.ContentAlignment.MiddleCenter;
+
+			this.labelOy = new StaticText(this);
+			this.labelOy.Text = "Y";
+			this.labelOy.Alignment = Drawing.ContentAlignment.MiddleCenter;
+
+			this.isNormalAndExtended = true;
+		}
+		
+		public PanelShadow(Widget embedder) : this()
+		{
+			this.SetEmbedder(embedder);
+		}
+		
+		protected override void Dispose(bool disposing)
+		{
+			this.fieldColor.Clicked -= new MessageEventHandler(this.FieldColorClicked);
+			this.fieldRadius.TextChanged -= new EventHandler(this.TextChanged);
+			this.fieldOx.TextChanged -= new EventHandler(this.TextChanged);
+			this.fieldOy.TextChanged -= new EventHandler(this.TextChanged);
+
+			base.Dispose(disposing);
+		}
+
+		
+		// Retourne la hauteur standard.
+		public override double DefaultHeight
+		{
+			get
+			{
+				return ( this.extendedSize ? 55 : 30 );
+			}
+		}
+
+
+		// Propriété -> widget.
+		public override void SetProperty(AbstractProperty property)
+		{
+			base.SetProperty(property);
+			this.label.Text = this.text;
+
+			PropertyShadow p = property as PropertyShadow;
+			if ( p == null )  return;
+
+			this.fieldColor.Color  = p.Color;
+			this.fieldRadius.Value = p.Radius;
+			this.fieldOx.Value     = p.Ox;
+			this.fieldOy.Value     = p.Oy;
+		}
+
+		// Widget -> propriété.
+		public override AbstractProperty GetProperty()
+		{
+			PropertyShadow p = new PropertyShadow();
+			base.GetProperty(p);
+
+			p.Color  = this.fieldColor.Color;
+			p.Radius = this.fieldRadius.Value;
+			p.Ox     = this.fieldOx.Value;
+			p.Oy     = this.fieldOy.Value;
+
+			return p;
+		}
+
+
+		// Désélectionne toutes les origines de couleurs possibles.
+		public override void OriginColorDeselect()
+		{
+			this.fieldColor.ActiveState = WidgetState.ActiveNo;
+		}
+
+		// Sélectionne l'origine de couleur.
+		public override void OriginColorSelect(int rank)
+		{
+			this.fieldColor.ActiveState = WidgetState.ActiveYes;
+		}
+
+		// Modifie la couleur d'origine.
+		public override void OriginColorChange(Drawing.Color color)
+		{
+			this.fieldColor.Color = color;
+		}
+
+		// Donne la couleur d'origine.
+		public override Drawing.Color OriginColorGet()
+		{
+			return this.fieldColor.Color;
+		}
+
+		
+		// Met à jour la géométrie.
+		protected override void UpdateClientGeometry()
+		{
+			base.UpdateClientGeometry();
+
+			if ( this.fieldColor == null )  return;
+
+			Drawing.Rectangle rect = this.Client.Bounds;
+			rect.Left += this.extendedZoneWidth;
+			rect.Inflate(-5, -5);
+
+			Drawing.Rectangle r = rect;
+			r.Bottom = r.Top-20;
+			r.Right = rect.Right-50;
+			this.label.Bounds = r;
+
+			r.Left = rect.Right-50;
+			r.Right = rect.Right;
+			this.fieldColor.Bounds = r;
+
+			r.Top = r.Bottom-5;
+			r.Bottom = r.Top-20;
+			r.Left = rect.Left;
+			r.Width = 14;
+			this.labelRadius.Bounds = r;
+			r.Left = r.Right;
+			r.Width = 45;
+			this.fieldRadius.Bounds = r;
+			r.Left = r.Right;
+			r.Width = 13;
+			this.labelOx.Bounds = r;
+			r.Left = r.Right;
+			r.Width = 45;
+			this.fieldOx.Bounds = r;
+			r.Left = r.Right;
+			r.Width = 13;
+			this.labelOy.Bounds = r;
+			r.Left = r.Right;
+			r.Width = 45;
+			this.fieldOy.Bounds = r;
+		}
+		
+
+		private void FieldColorClicked(object sender, MessageEventArgs e)
+		{
+			this.OnOriginColorChanged();
+		}
+
+		private void TextChanged(object sender)
+		{
+			this.OnChanged();
+		}
+
+
+		protected StaticText				label;
+		protected ColorSample				fieldColor;
+		protected TextFieldSlider			fieldRadius;
+		protected TextFieldSlider			fieldOx;
+		protected TextFieldSlider			fieldOy;
+		protected StaticText				labelRadius;
+		protected StaticText				labelOx;
+		protected StaticText				labelOy;
+	}
+}
