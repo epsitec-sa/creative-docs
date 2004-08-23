52e0c69dba2649d1a312b72aff9bd08e8e5f5009
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:21:49 +0000

    ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1334 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/SmartTag.cs
@@ -0,0 +1,84 @@
+namespace Epsitec.Common.Widgets
+{
+	/// <summary>
+	/// La classe SmartTag représente un Tag avec un menu associé.
+	/// </summary>
+	public class SmartTag : Tag
+	{
+		public SmartTag() : this (null, null)
+		{
+		}
+		
+		public SmartTag(Widget embedder) : this ()
+		{
+			this.SetEmbedder (embedder);
+		}
+		
+		public SmartTag(string command) : this (command, null)
+		{
+		}
+		
+		public SmartTag(string command, string name) : base (command, name)
+		{
+		}
+		
+		
+		public VMenu							Menu
+		{
+			get
+			{
+				return this.menu;
+			}
+			set
+			{
+				this.menu = value;
+			}
+		}
+		
+		public object						Context
+		{
+			get
+			{
+				return this.context;
+			}
+			set
+			{
+				this.context = value;
+			}
+		}
+		
+		
+		protected virtual VMenu GetMenu()
+		{
+			this.OnPrepareMenu ();
+			return this.menu;
+		}
+		
+		protected override void OnClicked(MessageEventArgs e)
+		{
+			base.OnClicked (e);
+			
+			Drawing.Point pos  = this.MapClientToScreen (new Drawing.Point (0, this.Height));
+			VMenu         menu = this.GetMenu ();
+			
+			menu.Host = this;
+			pos.X    -= menu.Width;
+			
+			menu.ShowContextMenu (this.Window, pos);
+		}
+		
+		protected virtual  void OnPrepareMenu()
+		{
+			if (this.PrepareMenu != null)
+			{
+				this.PrepareMenu (this);
+			}
+		}
+		
+		
+		public event Support.EventHandler		PrepareMenu;
+		
+		protected VMenu							menu;
+		protected object						context;
+	}
+}
