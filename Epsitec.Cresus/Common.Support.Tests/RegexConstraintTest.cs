831fd6bab6d4e2f3177ea0dc671278a064452375
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:40:56 +0000

    ********************** Label: "Stable-25-06-2004" User: Arnaud       Date: 25.06.04   Time: 16:09 Labeled Label comment: Version with Drawing.TextStyle implemented, Support.EventHandler moved to its own assembly and script almost done.  ********************** Label: "Stable-22-06-2004" User: Arnaud       Date: 22.06.04   Time: 18:13 Labeled Label comment: First version with dynamic script compilation.  ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2613 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Support.Tests/RegexConstraintTest.cs
@@ -0,0 +1,51 @@
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace Epsitec.Common.Support
+{
+	[TestFixture] public class RegexConstraintTest
+	{
+		[Test] public void CheckConstraint()
+		{
+			RegexConstraint c1 = new RegexConstraint ("a*", RegexFactory.Options.IgnoreCase);
+			RegexConstraint c2 = new RegexConstraint (RegexFactory.DecimalNum);
+			RegexConstraint c3 = new RegexConstraint (PredefinedRegex.AlphaNum);
+			
+			Assert.IsTrue (c1.CheckConstraint ("abc"));
+			Assert.IsTrue (c2.CheckConstraint ("10.5"));
+			Assert.IsTrue (c3.CheckConstraint ("abc"));
+			
+			Assert.IsFalse (c1.CheckConstraint ("xyz"));
+			Assert.IsFalse (c2.CheckConstraint ("abc"));
+			Assert.IsFalse (c3.CheckConstraint ("10"));
+		}
+		
+		[Test] public void CheckToStringFromString()
+		{
+			RegexConstraint c1 = new RegexConstraint ("a*", RegexFactory.Options.IgnoreCase);
+			RegexConstraint c2 = new RegexConstraint (RegexFactory.DecimalNum);
+			RegexConstraint c3 = new RegexConstraint (PredefinedRegex.AlphaNum);
+			
+			string s1 = c1.ToString ();
+			string s2 = c2.ToString ();
+			string s3 = c3.ToString ();
+			
+			c1 = RegexConstraint.FromString (s1);
+			c2 = RegexConstraint.FromString (s2);
+			c3 = RegexConstraint.FromString (s3);
+			
+			Assert.IsTrue (c1.CheckConstraint ("abc"));
+			Assert.IsTrue (c2.CheckConstraint ("10.5"));
+			Assert.IsTrue (c3.CheckConstraint ("abc"));
+			
+			Assert.IsFalse (c1.CheckConstraint ("xyz"));
+			Assert.IsFalse (c2.CheckConstraint ("abc"));
+			Assert.IsFalse (c3.CheckConstraint ("10"));
+			
+			Assert.AreEqual ("a*", c1.Pattern);
+			Assert.AreEqual (RegexFactory.Options.IgnoreCase, c1.PatternOptions);
+			Assert.AreEqual (RegexFactory.DecimalNum.ToString (), c2.Regex.ToString ());
+			Assert.AreEqual (PredefinedRegex.AlphaNum, c3.PredefinedRegex);
+		}
+	}
+}
