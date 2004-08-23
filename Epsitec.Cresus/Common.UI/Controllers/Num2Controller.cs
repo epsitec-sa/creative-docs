832aa13ae141e0d46f7bb95a04f9fcb21443934f
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:52:59 +0000

    ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1611 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.UI/Controllers/Num2Controller.cs
@@ -0,0 +1,132 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : en chantier/PA
+
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+
+namespace Epsitec.Common.UI.Controllers
+{
+	/// <summary>
+	/// Summary description for Num2Controller.
+	/// </summary>
+	public class Num2Controller : AbstractController
+	{
+		public Num2Controller()
+		{
+		}
+		
+		
+		public override void CreateUI(Widget panel)
+		{
+			this.caption_label = new StaticText (panel);
+			this.container     = new Widget (panel);
+			this.text_field_1  = new TextFieldUpDown (this.container);
+			this.text_field_2  = new TextFieldUpDown (this.container);
+			
+			double h_line = this.text_field_1.Height;
+			double h_pane = System.Math.Max (panel.Height, h_line + 6);
+			
+			panel.Height = h_pane;
+			
+			double y = System.Math.Floor ((h_pane - h_line) / 2);
+			
+			this.caption_label.Bounds = new Drawing.Rectangle (0, y, 80, h_line);
+			this.caption_label.Anchor = AnchorStyles.TopLeft;
+			
+			this.container.Bounds = new Drawing.Rectangle (this.caption_label.Right, y, panel.Width - this.caption_label.Width, h_line);
+			this.container.Anchor = AnchorStyles.LeftAndRight | AnchorStyles.Top;
+			this.container.PreferHorizontalDockLayout = true;
+			
+			this.text_field_1.Dock       = DockStyle.Fill;
+			this.text_field_1.DockSpacer = new Drawing.Margins (0, 2, 0, 0);
+			this.text_field_1.MinValue   = 0;
+			this.text_field_1.MaxValue   = 9999;
+			this.text_field_1.Resolution = 1;
+			this.text_field_2.Dock       = DockStyle.Fill;
+			this.text_field_2.DockSpacer = new Drawing.Margins (2, 0, 0, 0);
+			this.text_field_2.MinValue   = 0;
+			this.text_field_2.MaxValue   = 9999;
+			this.text_field_2.Resolution = 1;
+			
+			this.text_field_1.TextChanged += new EventHandler (this.HandleTextFieldTextChanged);
+			this.text_field_2.TextChanged += new EventHandler (this.HandleTextFieldTextChanged);
+			
+			this.OnCaptionChanged ();
+			
+			this.SyncFromAdapter ();
+		}
+		
+		public override void SyncFromAdapter()
+		{
+			Adapters.Num2Adapter adapter = this.Adapter as Adapters.Num2Adapter;
+			
+			if (this.inhibit_events > 0)
+			{
+				return;
+			}
+			
+			this.inhibit_events++;
+			
+			if ((adapter != null) &&
+				(this.text_field_1 != null) &&
+				(this.text_field_2 != null))
+			{
+				string   value = adapter.Value;
+				string[] args  = value == null ? null : value.Split (';');
+				
+				if ((args != null) &&
+					(args.Length == 2))
+				{
+					this.text_field_1.Text = args[0];
+					this.text_field_2.Text = args[1];
+				}
+				else
+				{
+					this.text_field_1.Text = "";
+					this.text_field_2.Text = "";
+				}
+			}
+			
+			this.inhibit_events--;
+		}
+		
+		public override void SyncFromUI()
+		{
+			Adapters.Num2Adapter adapter = this.Adapter as Adapters.Num2Adapter;
+			
+			if (this.inhibit_events > 0)
+			{
+				return;
+			}
+			
+			this.inhibit_events++;
+			
+			if ((adapter != null) &&
+				(this.text_field_1 != null) &&
+				(this.text_field_2 != null))
+			{
+				try
+				{
+					adapter.Value = this.text_field_1.Text + ";" + this.text_field_2.Text;
+				}
+				catch
+				{
+				}
+			}
+			
+			this.inhibit_events--;
+		}
+		
+		
+		private void HandleTextFieldTextChanged(object sender)
+		{
+			this.SyncFromUI ();
+		}
+		
+		
+		private int								inhibit_events;
+		private Widget							container;
+		private TextFieldUpDown					text_field_1;
+		private TextFieldUpDown					text_field_2;
+	}
+}
