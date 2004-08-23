28f601dfc43d770fc71b347155bce01a31bd6d2c
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:41:27 +0000

    ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1535 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Pictogram/Widgets/PanelFont.cs
@@ -0,0 +1,256 @@
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+using Epsitec.Common.Pictogram.Data;
+
+namespace Epsitec.Common.Pictogram.Widgets
+{
+	/// <summary>
+	/// La classe PanelFont permet de choisir une police de caractères.
+	/// </summary>
+	public class PanelFont : AbstractPanel
+	{
+		public PanelFont()
+		{
+			this.label = new StaticText(this);
+			this.label.Alignment = Drawing.ContentAlignment.MiddleLeft;
+
+			this.fontName = new TextFieldCombo(this);
+			this.fontName.IsReadOnly = true;
+			this.fontName.Items.Add("Tahoma");
+			this.fontName.Items.Add("Arial");
+			this.fontName.Items.Add("Courier New");
+			this.fontName.Items.Add("Times New Roman");
+			this.fontName.TextChanged += new EventHandler(this.HandleFieldChanged);
+			this.fontName.TabIndex = 1;
+			this.fontName.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+
+			this.fontOptical = new TextFieldCombo(this);
+			this.fontOptical.IsReadOnly = true;
+			this.fontOptical.Items.Add("Regular");
+			this.fontOptical.Items.Add("Bold");
+			this.fontOptical.Items.Add("Italic");
+			this.fontOptical.Items.Add("Bold Italic");
+			this.fontOptical.TextChanged += new EventHandler(this.HandleFieldChanged);
+			this.fontOptical.TabIndex = 2;
+			this.fontOptical.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+
+			this.fontSize = new TextFieldSlider(this);
+			this.fontSize.MinValue =  0.1M;
+			this.fontSize.MaxValue = 20.0M;
+			this.fontSize.Step = 0.1M;
+			this.fontSize.Resolution = 0.01M;
+			this.fontSize.TextChanged += new EventHandler(this.HandleFieldChanged);
+			this.fontSize.TabIndex = 3;
+			this.fontSize.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+
+			this.fontColor = new ColorSample(this);
+			this.fontColor.PossibleOrigin = true;
+			this.fontColor.Clicked += new MessageEventHandler(this.HandleColorClicked);
+			this.fontColor.TabIndex = 4;
+			this.fontColor.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+
+			this.labelOptical = new StaticText(this);
+			this.labelOptical.Text = "Variation";
+			this.labelOptical.Alignment = Drawing.ContentAlignment.MiddleLeft;
+
+			this.labelSize = new StaticText(this);
+			this.labelSize.Text = "Taille";
+			this.labelSize.Alignment = Drawing.ContentAlignment.MiddleLeft;
+
+			this.labelColor = new StaticText(this);
+			this.labelColor.Text = "Couleur";
+			this.labelColor.Alignment = Drawing.ContentAlignment.MiddleLeft;
+
+			this.isNormalAndExtended = true;
+		}
+		
+		public PanelFont(Widget embedder) : this()
+		{
+			this.SetEmbedder(embedder);
+		}
+		
+		protected override void Dispose(bool disposing)
+		{
+			if ( disposing )
+			{
+				this.fontName.TextChanged -= new EventHandler(this.HandleFieldChanged);
+				this.fontOptical.TextChanged -= new EventHandler(this.HandleFieldChanged);
+				this.fontSize.TextChanged -= new EventHandler(this.HandleFieldChanged);
+				this.fontColor.Clicked -= new MessageEventHandler(this.HandleColorClicked);
+			}
+			
+			base.Dispose(disposing);
+		}
+
+		
+		// Retourne la hauteur standard.
+		public override double DefaultHeight
+		{
+			get
+			{
+				return ( this.extendedSize ? 105 : 30 );
+			}
+		}
+
+		// Propriété -> widget.
+		public override void SetProperty(AbstractProperty property)
+		{
+			base.SetProperty(property);
+			this.label.Text = this.textStyle;
+
+			PropertyFont p = property as PropertyFont;
+			if ( p == null )  return;
+
+			//?this.fontName.SelectedName = p.FontName;
+			//?this.fontOptical.SelectedName = p.FontOptical;
+			this.ComboSelectedName(this.fontName, p.FontName);
+			this.ComboSelectedName(this.fontOptical, p.FontOptical);
+			this.fontSize.Value = (decimal) p.FontSize;
+			this.fontColor.Color = p.FontColor;
+		}
+
+		// Widget -> propriété.
+		public override AbstractProperty GetProperty()
+		{
+			PropertyFont p = new PropertyFont();
+			base.GetProperty(p);
+
+			//?string name = this.fontName.SelectedName;
+			string name = this.ComboSelectedName(this.fontName);
+			if ( name == null )  p.FontName = "";
+			else                 p.FontName = name;
+
+			//?string optical = this.fontOptical.SelectedName;
+			string optical = this.ComboSelectedName(this.fontOptical);
+			if ( optical == null )  p.FontOptical = "";
+			else                    p.FontOptical = optical;
+
+			p.FontSize = (double) this.fontSize.Value;
+			p.FontColor = this.fontColor.Color;
+
+			return p;
+		}
+
+
+		// TODO: TextFieldCombo.SelectedName ne marche pas !!!
+		protected void ComboSelectedName(TextFieldCombo combo, string name)
+		{
+			int total = combo.Items.Count;
+			for ( int i=0 ; i<total ; i++ )
+			{
+				string s = combo.Items[i];
+				if ( s == name )
+				{
+					combo.SelectedIndex = i;
+					return;
+				}
+			}
+		}
+
+		// TODO: TextFieldCombo.SelectedName ne marche pas !!!
+		protected string ComboSelectedName(TextFieldCombo combo)
+		{
+			if ( combo.SelectedIndex == -1 )  return "";
+			return combo.Items[combo.SelectedIndex] as string;
+		}
+
+
+		// Désélectionne toutes les origines de couleurs possibles.
+		public override void OriginColorDeselect()
+		{
+			this.fontColor.ActiveState = WidgetState.ActiveNo;
+		}
+
+		// Sélectionne l'origine de couleur.
+		public override void OriginColorSelect(int rank)
+		{
+			this.fontColor.ActiveState = WidgetState.ActiveYes;
+		}
+
+		// Modifie la couleur d'origine.
+		public override void OriginColorChange(Drawing.Color color)
+		{
+			this.fontColor.Color = color;
+		}
+
+		// Donne la couleur d'origine.
+		public override Drawing.Color OriginColorGet()
+		{
+			return this.fontColor.Color;
+		}
+
+		
+		// Met à jour la géométrie.
+		protected override void UpdateClientGeometry()
+		{
+			base.UpdateClientGeometry();
+
+			if ( this.fontName == null )  return;
+
+			Drawing.Rectangle rect = this.Client.Bounds;
+			rect.Deflate(this.extendedZoneWidth, 0);
+			rect.Deflate(5);
+
+			Drawing.Rectangle r = rect;
+			r.Bottom = r.Top-20;
+			r.Left = rect.Left;
+			r.Right = rect.Right-110;
+			this.label.Bounds = r;
+			r.Left = rect.Right-110;
+			r.Right = rect.Right;
+			this.fontName.Bounds = r;
+
+			r.Offset(0, -25);
+			r.Left = rect.Left;
+			r.Right = rect.Right-110;
+			this.labelOptical.Bounds = r;
+			this.labelOptical.SetVisible(this.extendedSize);
+			r.Left = rect.Right-110;
+			r.Right = rect.Right;
+			this.fontOptical.Bounds = r;
+			this.fontOptical.SetVisible(this.extendedSize);
+
+			r.Offset(0, -25);
+			r.Left = rect.Left;
+			r.Right = rect.Right-50;
+			this.labelSize.Bounds = r;
+			this.labelSize.SetVisible(this.extendedSize);
+			r.Left = rect.Right-50;
+			r.Right = rect.Right;
+			this.fontSize.Bounds = r;
+			this.fontSize.SetVisible(this.extendedSize);
+
+			r.Offset(0, -25);
+			r.Left = rect.Left;
+			r.Right = rect.Right-50;
+			this.labelColor.Bounds = r;
+			this.labelColor.SetVisible(this.extendedSize);
+			r.Left = rect.Right-50;
+			r.Right = rect.Right;
+			this.fontColor.Bounds = r;
+			this.fontColor.SetVisible(this.extendedSize);
+		}
+
+
+		// Un champ a été changé.
+		private void HandleFieldChanged(object sender)
+		{
+			this.OnChanged();
+		}
+
+		private void HandleColorClicked(object sender, MessageEventArgs e)
+		{
+			this.OnOriginColorChanged();
+		}
+
+
+		protected StaticText				label;
+		protected StaticText				labelOptical;
+		protected StaticText				labelSize;
+		protected StaticText				labelColor;
+		protected TextFieldCombo			fontName;
+		protected TextFieldCombo			fontOptical;
+		protected TextFieldSlider			fontSize;
+		protected ColorSample				fontColor;
+	}
+}
