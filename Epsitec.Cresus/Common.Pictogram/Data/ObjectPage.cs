4d2249b1158c1ed2e8c9fa3dc54e2e27ceb9ca29
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:14:49 +0000

    Pictogram avec styles, calques, multi-pages, mais sans undo/redo.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1280 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Pictogram/Data/ObjectPage.cs
@@ -0,0 +1,39 @@
+using System.Xml.Serialization;
+
+namespace Epsitec.Common.Pictogram.Data
+{
+	/// <summary>
+	/// La classe ObjectPage est la classe de l'objet graphique "page".
+	/// </summary>
+	public class ObjectPage : AbstractObject
+	{
+		public ObjectPage()
+		{
+			this.objects = new System.Collections.ArrayList();
+		}
+
+		protected override AbstractObject CreateNewObject()
+		{
+			return new ObjectPage();
+		}
+
+		[XmlAttribute]
+		public string Name
+		{
+			get { return this.name; }
+			set { this.name = value; }
+		}
+
+
+		// Reprend toutes les caractéristiques d'un objet.
+		public override void CloneObject(AbstractObject src)
+		{
+			base.CloneObject(src);
+			ObjectPage page = src as ObjectPage;
+			this.name = page.name;
+		}
+
+
+		protected string		name = "";
+	}
+}
