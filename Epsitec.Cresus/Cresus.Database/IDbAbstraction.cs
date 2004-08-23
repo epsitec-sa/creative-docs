33ba45dd156b7135ab3027c25d8d51e332907916
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 12:34:54 +0000

    ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@332 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Database/IDbAbstraction.cs
+++ Epsitec.Cresus/Cresus.Database/IDbAbstraction.cs
@@ -7,7 +7,7 @@ namespace Epsitec.Cresus.Database
 	/// L'interface IDbAbstraction est utilisée pour accéder aux mécanismes ADO.NET
 	/// dépendants d'un provider spécifique.
 	/// </summary>
-	public interface IDbAbstraction
+	public interface IDbAbstraction : System.IDisposable
 	{
 		IDbAbstractionFactory		Factory				{ get; }
 		System.Data.IDbConnection	Connection			{ get; }
