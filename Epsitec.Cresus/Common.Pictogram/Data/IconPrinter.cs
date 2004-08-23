290f2e393c64d2c77c01e9dbe288ab8cb7440164
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:37:49 +0000

    ********************** Label: "Stable-25-06-2004" User: Arnaud       Date: 25.06.04   Time: 16:09 Labeled Label comment: Version with Drawing.TextStyle implemented, Support.EventHandler moved to its own assembly and script almost done.  ********************** Label: "Stable-22-06-2004" User: Arnaud       Date: 22.06.04   Time: 18:13 Labeled Label comment: First version with dynamic script compilation.  ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2583 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Pictogram/Data/IconPrinter.cs
@@ -0,0 +1,141 @@
+using Epsitec.Common.Pictogram.Widgets;
+using Epsitec.Common.Widgets;
+
+namespace Epsitec.Common.Pictogram.Data
+{
+	/// <summary>
+	/// La classe IconPrinter implémente l'impression d'un document.
+	/// </summary>
+	public class IconPrinter
+	{
+		public IconPrinter()
+		{
+		}
+
+		public IconObjects IconObjects
+		{
+			set { this.iconObjects = value; }
+		}
+
+		public void Print(Dialogs.Print dp, IconContext iconContext)
+		{
+			PrintEngine printEngine = new PrintEngine();
+			printEngine.IconPrinter = this;
+			printEngine.IconContext = iconContext;
+			dp.Document.Print(printEngine);
+		}
+
+
+		protected class PrintEngine : Printing.IPrintEngine
+		{
+			public IconPrinter IconPrinter
+			{
+				set { this.iconPrinter = value; }
+			}
+
+			public IconContext IconContext
+			{
+				set { this.iconContext = value; }
+			}
+
+			#region IPrintEngine Members
+			public void PrepareNewPage(Epsitec.Common.Printing.PageSettings settings)
+			{
+				settings.Margins = new Drawing.Margins(0, 0, 0, 0);
+				this.paperSize = settings.PaperSize;
+			}
+			
+			public void StartingPrintJob()
+			{
+			}
+			
+			public void FinishingPrintJob()
+			{
+			}
+			
+			public Printing.PrintEngineStatus PrintPage(Printing.PrintPort port)
+			{
+				double zoom = this.paperSize.Width / this.iconPrinter.iconObjects.Size.Width;
+				port.ScaleTransform(zoom, zoom, 0, 0);
+				this.iconPrinter.PrintGeometry(port, this.iconContext, 0, true);
+				return Printing.PrintEngineStatus.FinishJob;
+			}
+			#endregion
+
+			protected IconPrinter			iconPrinter;
+			protected IconContext			iconContext;
+			protected Printing.PaperSize	paperSize;
+		}
+
+		
+		// Imprime la géométrie de tous les objets.
+		public void PrintGeometry(Printing.PrintPort port,
+								  IconContext iconContext,
+								  int pageNumber,
+								  bool showAllLayers)
+		{
+			if ( this.iconObjects.Objects.Count == 0 )  return;
+			ObjectPattern pattern = this.iconObjects.Objects[0] as ObjectPattern;
+			if ( pattern.Objects.Count == 0 )  return;
+			ObjectPage page = pattern.Objects[pageNumber] as ObjectPage;
+			this.PrintGeometry(page.Objects, port, iconContext, showAllLayers, !showAllLayers);
+		}
+
+		public void PrintGeometry(System.Collections.ArrayList objects,
+								 Printing.PrintPort port,
+								 IconContext iconContext,
+								 bool showAllLayers,
+								 bool dimmed)
+		{
+			System.Collections.ArrayList root = this.iconObjects.CurrentGroup;
+			if ( objects == root )  dimmed = false;
+			iconContext.IsDimmed = dimmed;
+
+			int total = objects.Count;
+			for ( int index=0 ; index<total ; index++ )
+			{
+				AbstractObject obj = objects[index] as AbstractObject;
+				ObjectLayer layer = obj as ObjectLayer;
+
+				if ( obj.Objects != null && obj.Objects.Count > 0 )
+				{
+					if ( layer != null )
+					{
+
+						PropertyModColor modColor = layer.PropertyModColor(0);
+						iconContext.modifyColor = new IconContext.ModifyColor(modColor.ModifyColor);
+
+						if ( layer.Actif || showAllLayers )
+						{
+							this.PrintGeometry(obj.Objects, port, iconContext, showAllLayers, dimmed);
+						}
+						else
+						{
+							if ( layer.Type != LayerType.Hide )
+							{
+								bool newDimmed = dimmed;
+								if ( layer.Type == LayerType.Show )  newDimmed = false;
+								this.PrintGeometry(obj.Objects, port, iconContext, showAllLayers, newDimmed);
+							}
+						}
+					}
+					else
+					{
+						this.PrintGeometry(obj.Objects, port, iconContext, showAllLayers, dimmed);
+					}
+				}
+
+				if ( obj is ObjectGroup )
+				{
+					if ( objects != root )  continue;
+				}
+
+				iconContext.IsDimmed = dimmed;
+				obj.PrintGeometry(port, iconContext, this.iconObjects);
+			}
+		}
+
+
+		protected IconObjects				iconObjects;
+	}
+}
