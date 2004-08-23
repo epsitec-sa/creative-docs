d0778b6d74d75a8331985fdc93c2e9222f31a622
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:09:51 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:
    **********************
    Label: "Stable-01-04-2004"
    User: Arnaud       Date:  1.04.04   Time: 12:42
    Labeled
    Label comment: TabIndex edition working in GUI.
    
    **********************
    Label: "Stable-24-03-2004"
    User: Arnaud       Date: 24.03.04   Time: 22:47
    Labeled
    Label comment: With DR contributions. First draft of Common.UI.
    
    **********************
    Label: "Stable-20-03-2004"
    User: Arnaud       Date: 20.03.04   Time: 12:18
    Labeled
    Label comment:
    
    git-svn-id: https://svn.opac.ch/svn/cr@2358 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Widgets/AbstractGroup.cs
+++ Epsitec.Cresus/Common.Widgets/AbstractGroup.cs
@@ -1,10 +1,15 @@
+//	Copyright © 2003-2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
 namespace Epsitec.Common.Widgets
 {
 	/// <summary>
 	/// La classe AbstractGroup sert de base aux autres classes qui
 	/// implémentent des groupes de widgets.
 	/// </summary>
+	
 	[Support.SuppressBundleSupport]
+	
 	public abstract class AbstractGroup : Widget
 	{
 		public AbstractGroup()
@@ -13,7 +18,7 @@ namespace Epsitec.Common.Widgets
 			this.TabNavigation  = Widget.TabNavigationMode.ForwardTabPassive;
 		}
 		
-		public AbstractGroup(Widget embedder) : this()
+		public AbstractGroup(Widget embedder) : this ()
 		{
 			this.SetEmbedder(embedder);
 		}
