fd0bece8dcd4993268037b6b078e31000511a64b
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:55:36 +0000

    ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1641 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Support.Tests/OpletTest.cs
@@ -0,0 +1,287 @@
+using NUnit.Framework;
+
+namespace Epsitec.Common.Support
+{
+	[TestFixture] public class OpletTest
+	{
+		[Test] public void CheckInsertUndoRedo()
+		{
+			OpletQueue queue = new OpletQueue ();
+			
+			
+			//	Insère une action composée de deux oplets :
+			
+			using (queue.BeginAction ())
+			{
+				queue.Insert (this.CreateOplet ("A1"));
+				queue.Insert (this.CreateOplet ("A2"));
+				queue.ValidateAction ();
+			}
+			
+			System.Diagnostics.Debug.WriteLine ("Action A created");
+			
+			Assertion.Assert (queue.CanUndo == true);
+			Assertion.Assert (queue.CanRedo == false);
+			Assertion.AssertEquals (1, queue.UndoActionCount);
+			Assertion.AssertEquals (0, queue.RedoActionCount);
+			
+			//	Défait l'action.
+			
+			queue.UndoAction ();
+			
+			System.Diagnostics.Debug.WriteLine ("Action A undone");
+			
+			Assertion.Assert (queue.CanUndo == false);
+			Assertion.Assert (queue.CanRedo == true);
+			Assertion.AssertEquals (0, queue.UndoActionCount);
+			Assertion.AssertEquals (1, queue.RedoActionCount);
+			
+			//	Refait l'action.
+			
+			queue.RedoAction ();
+			
+			System.Diagnostics.Debug.WriteLine ("Action A redone");
+			
+			Assertion.Assert (queue.CanUndo == true);
+			Assertion.Assert (queue.CanRedo == false);
+			Assertion.AssertEquals (1, queue.UndoActionCount);
+			Assertion.AssertEquals (0, queue.RedoActionCount);
+			
+			//	Commence la création d'une action avec abandon en cours de
+			//	route :
+			
+			try
+			{
+				using (queue.BeginAction ())
+				{
+					queue.Insert (this.CreateOplet ("B1"));
+					throw new System.Exception ();
+				}
+			}
+			catch
+			{
+			}
+			
+			System.Diagnostics.Debug.WriteLine ("Action B cancelled automatically");
+			
+			Assertion.AssertEquals (1, queue.UndoActionCount);
+			Assertion.AssertEquals (0, queue.RedoActionCount);
+			
+			//	Commence la création d'une action avec abandon explicite :
+			
+			using (queue.BeginAction ())
+			{
+				queue.Insert (this.CreateOplet ("B'1"));
+				queue.CancelAction ();
+			}
+			
+			System.Diagnostics.Debug.WriteLine ("Action B' cancelled manually");
+			
+			Assertion.AssertEquals (1, queue.UndoActionCount);
+			Assertion.AssertEquals (0, queue.RedoActionCount);
+			
+			//	Crée une action avec une sous-action qui est abandonnée :
+			
+			using (queue.BeginAction ())
+			{
+				queue.Insert (this.CreateOplet ("C1"));
+				using (queue.BeginAction ())
+				{
+					queue.Insert (this.CreateOplet ("C1-1"));
+					queue.Insert (this.CreateOplet ("C1-2"));
+				}
+				queue.Insert (this.CreateOplet ("C2"));
+				queue.ValidateAction ();
+			}
+			
+			System.Diagnostics.Debug.WriteLine ("Action C inserted / C1 cancelled");
+			
+			Assertion.AssertEquals (2, queue.UndoActionCount);
+			Assertion.AssertEquals (0, queue.RedoActionCount);
+			
+			//	Crée une action avec une sous-action qui est validée :
+			
+			using (queue.BeginAction ())
+			{
+				queue.Insert (this.CreateOplet ("D1"));
+				using (queue.BeginAction ())
+				{
+					queue.Insert (this.CreateOplet ("D1-1"));
+					queue.Insert (this.CreateOplet ("D1-2"));
+					queue.ValidateAction ();
+				}
+				queue.Insert (this.CreateOplet ("D2"));
+				queue.ValidateAction ();
+			}
+			
+			System.Diagnostics.Debug.WriteLine ("Action D inserted / D1 validated");
+			
+			Assertion.AssertEquals (3, queue.UndoActionCount);
+			Assertion.AssertEquals (0, queue.RedoActionCount);
+			
+			queue.UndoAction ();
+			queue.UndoAction ();
+			
+			System.Diagnostics.Debug.WriteLine ("Undone twice");
+			
+			Assertion.AssertEquals (1, queue.UndoActionCount);
+			Assertion.AssertEquals (2, queue.RedoActionCount);
+			
+			queue.PurgeRedo ();
+			
+			System.Diagnostics.Debug.WriteLine ("Purged redo");
+			
+			Assertion.AssertEquals (1, queue.UndoActionCount);
+			Assertion.AssertEquals (0, queue.RedoActionCount);
+			
+			queue.PurgeUndo ();
+			
+			System.Diagnostics.Debug.WriteLine ("Purged undo");
+			
+			Assertion.AssertEquals (0, queue.UndoActionCount);
+			Assertion.AssertEquals (0, queue.RedoActionCount);
+		}
+		
+		
+		[Test] public void CheckFailUndo()
+		{
+			OpletQueue queue = new OpletQueue ();
+			Assertion.Assert (! queue.UndoAction ());
+		}
+		
+		[Test] public void CheckFailRedo()
+		{
+			OpletQueue queue = new OpletQueue ();
+			Assertion.Assert (! queue.RedoAction ());
+		}
+		
+		[Test] [ExpectedException (typeof (System.InvalidOperationException))] public void CheckInsertUndoRedoEx1()
+		{
+			OpletQueue queue = new OpletQueue ();
+			queue.Insert (this.CreateOplet ("test"));
+		}
+		
+		[Test] [ExpectedException (typeof (System.InvalidOperationException))] public void CheckInsertUndoRedoEx2()
+		{
+			OpletQueue queue = new OpletQueue ();
+			
+			using (queue.BeginAction ())
+			{
+				queue.Insert (new BadOplet (queue));
+				queue.ValidateAction ();
+			}
+			
+			queue.UndoAction ();
+		}
+		
+		[Test] [ExpectedException (typeof (System.InvalidOperationException))] public void CheckInsertUndoRedoEx3()
+		{
+			OpletQueue queue = new OpletQueue ();
+			
+			using (queue.BeginAction ())
+			{
+				queue.PurgeUndo ();
+			}
+		}
+		
+		[Test] [ExpectedException (typeof (System.InvalidOperationException))] public void CheckInsertUndoRedoEx4()
+		{
+			OpletQueue queue = new OpletQueue ();
+			
+			using (queue.BeginAction ())
+			{
+				queue.PurgeRedo ();
+			}
+		}
+		
+		[Test] [ExpectedException (typeof (System.InvalidOperationException))] public void CheckInsertUndoRedoEx5()
+		{
+			OpletQueue queue = new OpletQueue ();
+			
+			using (queue.BeginAction ())
+			{
+				queue.UndoAction ();
+			}
+		}
+		
+		[Test] [ExpectedException (typeof (System.InvalidOperationException))] public void CheckInsertUndoRedoEx6()
+		{
+			OpletQueue queue = new OpletQueue ();
+			
+			using (queue.BeginAction ())
+			{
+				queue.RedoAction ();
+			}
+		}
+		
+		
+		private IOplet CreateOplet(string name)
+		{
+			return new NamedOplet (name);
+		}
+		
+		
+		private class NamedOplet : AbstractOplet
+		{
+			public NamedOplet(string name)
+			{
+				this.name = name;
+			}
+			
+			
+			public override IOplet Undo()
+			{
+				System.Diagnostics.Debug.WriteLine ("Undo " + this.name);
+				return this;
+			}
+			
+			public override IOplet Redo()
+			{
+				System.Diagnostics.Debug.WriteLine ("Redo " + this.name);
+				return this;
+			}
+			
+			public override void Dispose()
+			{
+				System.Diagnostics.Debug.WriteLine ("Dispose " + this.name);
+			}
+			
+			
+			private string						name;
+		}
+		
+		private class BadOplet : AbstractOplet
+		{
+			public BadOplet(OpletQueue queue)
+			{
+				this.queue = queue;
+			}
+			
+			
+			public override IOplet Undo()
+			{
+				//	On n'a pas le droit de faire cela dans un UNDO/REDO :
+				
+				using (queue.BeginAction ())
+				{
+					queue.Insert (new NamedOplet ("t"));
+					queue.ValidateAction ();
+				}
+				 
+				return this;
+			}
+			
+			public override IOplet Redo()
+			{
+				return this;
+			}
+			
+			public override void Dispose()
+			{
+			}
+			
+			
+			private OpletQueue					queue;
+		}
+	}
+}
