e38105c07debfe74b9e34bc67326a47c783f6925
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:26:02 +0000

    ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.
    **********************
    Label: "21-01-2004"
    User: Arnaud       Date: 21.01.04   Time: 18:14
    Labeled
    Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.
    
    
    **********************
    Label: "19-01-2004"
    User: Arnaud       Date: 19.01.04   Time: 16:24
    Labeled
    Label comment: Before in-depth modification of XML handling in ResourceBundle.
    
    git-svn-id: https://svn.opac.ch/svn/cr@871 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Widgets/Design/HiliteAdorner.cs
+++ Epsitec.Cresus/Common.Widgets/Design/HiliteAdorner.cs
@@ -12,7 +12,14 @@ namespace Epsitec.Common.Widgets.Design
 	{
 		public HiliteAdorner()
 		{
+			this.path = new Drawing.Path ();
 		}
+		
+		public Drawing.Path					Path
+		{
+			get { return this.path; }
+		}
+		
 		
 		protected override void PaintDecoration(Drawing.Graphics graphics, Drawing.Rectangle repaint)
 		{
@@ -23,7 +30,16 @@ namespace Epsitec.Common.Widgets.Design
 			graphics.AddFilledRectangle (Drawing.Rectangle.Inflate (this.widget.Client.Bounds, -2, -2));
 			graphics.RenderSolid (Drawing.Color.FromARGB (0.3, 1.0, 0.0, 0.0));
 			
+			if (this.path.IsValid)
+			{
+				graphics.Rasterizer.FillMode = Drawing.FillMode.NonZero;
+				graphics.Rasterizer.AddOutline (this.path, 1.0, Drawing.CapStyle.Square, Drawing.JoinStyle.Miter);
+				graphics.RenderSolid (Drawing.Color.FromARGB (0.8, 0.0, 0.0, 1.0));
+			}
+			
 			graphics.Rasterizer.FillMode = old_fill_mode;
 		}
+		
+		protected Drawing.Path				path;
 	}
 }
