c329e187987142d702ba9a655cdbe1ec18fe2af1
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:01:06 +0000

    ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1183 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Support/Data/StringArrayStore.cs
@@ -0,0 +1,98 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : OK/PA, 20/02/2004
+
+namespace Epsitec.Common.Support.Data
+{
+	/// <summary>
+	/// Summary description for StringArrayStore.
+	/// </summary>
+	public class StringArrayStore : ITextArrayStore
+	{ 
+		public StringArrayStore(string[] array)
+		{
+			this.array = array;
+		}
+		
+		
+		public string						this[int index]
+		{
+			get
+			{
+				return this.array[index];
+			}
+			set
+			{
+				if (this.array[index] != value)
+				{
+					this.array[index] = value;
+					this.OnStoreChanged ();
+				}
+			}
+		}
+		
+		public int							Count
+		{
+			get
+			{
+				return this.array.Length;
+			}
+		}
+		
+		#region ITextArrayStore Members
+		public int GetRowCount()
+		{
+			return this.array.Length;
+		}
+
+		public int GetColumnCount()
+		{
+			return 1;
+		}
+
+		public string GetCellText(int row, int column)
+		{
+			if (column == 0)
+			{
+				return this.array[row];
+			}
+			
+			return null;
+		}
+
+		public void SetCellText(int row, int column, string value)
+		{
+			if (column == 0)
+			{
+				this.array[row] = value;
+			}
+			else
+			{
+				throw new System.NotSupportedException (string.Format ("Cannot set cell in column {0}.", column));
+			}
+		}
+
+		public void InsertRows(int row, int num)
+		{
+			throw new System.NotSupportedException (string.Format ("Cannot insert rows."));
+		}
+
+		public void RemoveRows(int row, int num)
+		{
+			throw new System.NotSupportedException (string.Format ("Cannot remove rows."));
+		}
+
+		public event Epsitec.Common.Support.EventHandler StoreChanged;
+		#endregion
+		
+		protected virtual void OnStoreChanged()
+		{
+			if (this.StoreChanged != null)
+			{
+				this.StoreChanged (this);
+			}
+		}
+		
+		
+		protected string[]					array;
+	}
+}
