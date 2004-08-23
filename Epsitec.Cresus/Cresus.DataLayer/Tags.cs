bf0fef9aba17f4debe9fb24e422d7c2fd5f3e914
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 12:29:48 +0000

    ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@268 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.DataLayer/Tags.cs
@@ -0,0 +1,12 @@
+namespace Epsitec.Cresus.DataLayer
+{
+	public class Tags
+	{
+		public const string	Id			= "id";
+		public const string	Name		= "name";
+		public const string	Binding		= "bind";
+		public const string	Data		= "data";
+		public const string	Label		= "label";
+		public const string	Description	= "descr";
+	}
+}
