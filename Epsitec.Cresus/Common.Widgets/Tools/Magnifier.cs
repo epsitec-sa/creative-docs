130b24a96bd0e9f5ccf98a97bfce7e7deb46a407
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:57:39 +0000

    ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1663 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/Tools/Magnifier.cs
@@ -0,0 +1,465 @@
+namespace Epsitec.Common.Widgets.Tools
+{
+	/// <summary>
+	/// La classe Magnifier permet de gérer une loupe.
+	/// </summary>
+	public class Magnifier
+	{
+		public Magnifier()
+		{
+			this.zoom_window = new Window ();
+			this.zoom_view   = new ZoomView (this);
+			
+			this.zoom_window.MakeTopLevelWindow ();
+			this.zoom_window.MakeFramelessWindow ();
+			this.zoom_window.MakeLayeredWindow ();
+			
+			this.zoom_window.ClientSize = new Drawing.Size (111, 111);
+			this.zoom_window.Root.BackColor = Drawing.Color.Transparent;
+			
+			this.zoom_view.Dock   = DockStyle.Fill;
+			this.zoom_view.Parent = this.zoom_window.Root;
+			this.zoom_view.SetFocused (true);
+			
+			this.DisplayRadius = 55;
+			this.SampleRadius  = 3;
+		}
+		
+		
+		public void Show()
+		{
+			this.zoom_window.Show ();
+		}
+		
+		public void Hide()
+		{
+			this.zoom_window.Hide ();
+		}
+		
+		
+		public double							DisplayRadius
+		{
+			get
+			{
+				return this.display_radius;
+			}
+			set
+			{
+				if (value < 40)  value = 40;
+				if (value > 200) value = 200;
+				
+				if (this.display_radius != value)
+				{
+					this.display_radius = value;
+					this.OnDisplayRadiusChanged ();
+				}
+			}
+		}
+		
+		public int								SampleRadius
+		{
+			get
+			{
+				return this.sample_radius;
+			}
+			set
+			{
+				if (value < 2)  value = 2;
+				if (value > 10) value = 10;
+				
+				if (this.sample_radius != value)
+				{
+					this.sample_radius = value;
+					this.OnSampleRadiusChanged ();
+				}
+			}
+		}
+		
+		public Drawing.Color					HotColor
+		{
+			get
+			{
+				return this.hot_color;
+			}
+				
+			set
+			{
+				if (this.hot_color != value)
+				{
+					this.hot_color = value;
+					this.OnHotColorChanged ();
+				}
+			}
+		}
+		
+		public bool								IsColorPicker
+		{
+			get
+			{
+				return this.color_picker;
+			}
+			set
+			{
+				if (this.color_picker != value)
+				{
+					this.color_picker = value;
+					this.zoom_view.Invalidate ();
+				}
+			}
+		}
+		
+		
+		protected virtual void OnHotColorChanged()
+		{
+			if (this.HotColorChanged != null)
+			{
+				this.HotColorChanged (this);
+			}
+		}
+		
+		protected virtual void OnSampleRadiusChanged()
+		{
+			this.UpdateBitmapSize ();
+			
+			if (this.SampleRadiusChanged != null)
+			{
+				this.SampleRadiusChanged (this);
+			}
+		}
+		
+		protected virtual void OnDisplayRadiusChanged()
+		{
+			this.UpdateWindowSize ();
+			
+			if (this.DisplayRadiusChanged != null)
+			{
+				this.DisplayRadiusChanged (this);
+			}
+		}
+		
+		
+		protected virtual void UpdateBitmapSize()
+		{
+			int dx = this.SampleRadius * 2 + 1;
+			int dy = dx;
+			
+			if ((this.bitmap == null) ||
+				(this.bitmap.BitmapImage.PixelWidth != dx) ||
+				(this.bitmap.BitmapImage.PixelHeight != dy))
+			{
+				if (this.bitmap != null)
+				{
+					this.bitmap.Dispose ();
+					this.bitmap = null;
+				}
+				
+				this.bitmap = Drawing.Bitmap.FromNativeBitmap (dx, dy);
+				
+				this.zoom_view.Invalidate ();
+			}
+		}
+		
+		protected virtual void UpdateWindowSize()
+		{
+			double dx = this.DisplayRadius * 2 + 1;
+			double dy = dx;
+			
+			this.zoom_window.ClientSize = new Drawing.Size (dx, dy);
+		}
+		
+		
+		public event Support.EventHandler		HotColorChanged;
+		public event Support.EventHandler		SampleRadiusChanged;
+		public event Support.EventHandler		DisplayRadiusChanged;
+		
+		
+		private Drawing.Color					hot_color;
+		private double							display_radius = 0;
+		private int								sample_radius  = 0;
+		private Drawing.Image					bitmap;
+		private ZoomView						zoom_view;
+		private Window							zoom_window;
+		private bool							color_picker;
+		
+		public class ZoomView : Widget
+		{
+			public ZoomView(Magnifier magnifier)
+			{
+				this.magnifier = magnifier;
+			}
+			
+			
+			protected override void ProcessMessage(Message message, Drawing.Point pos)
+			{
+				switch (message.Type)
+				{
+					case MessageType.MouseDown:
+						if (message.IsLeftButton)
+						{
+							this.is_dragging = true;
+							this.origin      = this.MapClientToScreen (pos);
+							
+							MouseCursor.Hide ();
+							message.Captured = true;
+						}
+						break;
+					
+					case MessageType.MouseMove:
+						if (this.is_dragging)
+						{
+							pos = this.MapClientToScreen (pos);
+							
+							this.Window.WindowLocation += pos - this.origin;
+							this.Invalidate ();
+							
+							this.origin = pos;
+						}
+						break;
+				
+					case MessageType.MouseUp:
+						MouseCursor.Show ();
+						this.is_dragging = false;
+						break;
+					
+					case MessageType.MouseWheel:
+						this.magnifier.SampleRadius += message.Wheel < 0 ? -1 : 1;
+						break;
+					
+					case MessageType.KeyDown:
+						switch (message.KeyCode)
+						{
+							case KeyCode.ArrowUp:
+								this.Window.WindowLocation += new Drawing.Point (0, 1);
+								this.Invalidate ();
+								break;
+								
+							case KeyCode.ArrowDown:
+								this.Window.WindowLocation += new Drawing.Point (0, -1);
+								this.Invalidate ();
+								break;
+								
+							case KeyCode.ArrowLeft:
+								this.Window.WindowLocation += new Drawing.Point (-1, 0);
+								this.Invalidate ();
+								break;
+								
+							case KeyCode.ArrowRight:
+								this.Window.WindowLocation += new Drawing.Point (1, 0);
+								this.Invalidate ();
+								break;
+						}
+						break;
+					
+					default:
+						return;
+				}
+				
+				message.Consumer = this;
+			}
+
+			protected override bool AboutToLoseFocus(Widget.TabNavigationDir dir, Widget.TabNavigationMode mode)
+			{
+				return false;
+			}
+
+			protected override void Dispose(bool disposing)
+			{
+				if (disposing)
+				{
+					if (this.timer != null)
+					{
+						this.timer.Stop ();
+						this.timer.Dispose ();
+						this.timer = null;
+					}
+					if (this.mask != null)
+					{
+						this.mask.Dispose ();
+						this.mask = null;
+					}
+				}
+				
+				base.Dispose (disposing);
+			}
+
+			protected override void PaintBackgroundImplementation(Drawing.Graphics graphics, Drawing.Rectangle clip_rect)
+			{
+				if (this.magnifier.SampleRadius < 1)
+				{
+					return;
+				}
+				
+				this.ConfigureTimer ();
+				
+				double dx = this.Client.Width;
+				double dy = this.Client.Height;
+				
+				double cx = dx / 2;
+				double cy = dy / 2;
+				
+				Drawing.Path path;
+				
+				if ((this.mask == null) ||
+					(this.mask_dx != dx) ||
+					(this.mask_dy != dy))
+				{
+					if (this.mask != null)
+					{
+						this.mask.Dispose ();
+						this.mask = null;
+					}
+					
+					this.mask = graphics.CreateAlphaMask ();
+					this.mask_dx = dx;
+					this.mask_dy = dy;
+					
+					path = Drawing.Path.FromCircle (cx, cy, cx, cy);
+					
+					this.mask.Color = Drawing.Color.FromRGB (1, 0, 0);
+					this.mask.PaintSurface (path);
+					
+					path.Dispose ();
+				}
+				
+				Drawing.Point  pos    = this.MapClientToScreen (new Drawing.Point (cx, cy));
+				Drawing.Bitmap bitmap = this.magnifier.bitmap.BitmapImage;
+				
+				int nx = bitmap.PixelWidth;
+				int ny = bitmap.PixelHeight;
+				
+				int px = (int) (pos.X) - nx / 2;
+				int py = (int) (pos.Y) - ny / 2;
+				
+				Win32Api.GrabScreen (bitmap, px, py);
+				
+				double sx = dx / nx;
+				double sy = dy / ny;
+				
+				graphics.SolidRenderer.SetAlphaMask (this.mask.Pixmap, Drawing.MaskComponent.R);
+				
+				using (Drawing.Pixmap.RawData raw = new Drawing.Pixmap.RawData (bitmap))
+				{
+					double x = 0;
+					double y = 0;
+					
+					for (int iy = 0; iy < ny; iy++)
+					{
+						x = 0;
+						
+						for (int ix = 0; ix < nx; ix++)
+						{
+							path = Drawing.Path.FromRectangle (x, dy-y-sy, sx+1, sy+1);
+							graphics.Color = raw[ix, iy];
+							graphics.PaintSurface (path);
+							path.Dispose ();
+							x += sx;
+						}
+						y += sy;
+					}
+					
+					this.magnifier.HotColor = raw[nx/2, ny/2];
+				}
+				
+				double ox = (nx / 2) * sx - 0.5;
+				double oy = (ny / 2) * sy + 0.5;
+				
+				path = new Drawing.Path ();
+				
+				if (this.magnifier.IsColorPicker)
+				{
+					path.MoveTo (ox+2, oy+0);
+					path.LineTo (ox+0, oy+0);
+					path.LineTo (ox+0, oy+2);
+					path.MoveTo (ox+0, oy+sy+1-2);
+					path.LineTo (ox+0, oy+sy+1-0);
+					path.LineTo (ox+2, oy+sy+1-0);
+					path.MoveTo (ox+sx+1-2, oy+sy+1-0);
+					path.LineTo (ox+sx+1-0, oy+sy+1-0);
+					path.LineTo (ox+sx+1-0, oy+sy+1-2);
+					path.MoveTo (ox+sx+1-0, oy+2);
+					path.LineTo (ox+sx+1-0, oy+0);
+					path.LineTo (ox+sx+1-2, oy+0);
+				}
+				
+				path.AppendCircle (dx/2, dy/2, dx/2-0.5, dy/2-0.5);
+				path.AppendCircle (dx/2, dy/2, dx/2, dy/2);
+				
+				graphics.Color = Drawing.Color.FromARGB (0.5, 0, 0, 0.8);
+				graphics.LineWidth = 1.0;
+				graphics.PaintOutline (path);
+				
+				path.Dispose ();
+				
+				graphics.SolidRenderer.SetAlphaMask (null, Drawing.MaskComponent.None);
+				
+				if (this.magnifier.IsColorPicker)
+				{
+					Drawing.Color color = this.magnifier.HotColor;
+					
+					int r = (int)(color.R * 255.5);
+					int g = (int)(color.G * 255.5);
+					int b = (int)(color.B * 255.5);
+					
+					this.PaintText (graphics, string.Format ("{0:X2}:{1:X2}:{2:X2}", r, g, b));
+				}
+			}
+			
+			
+			protected void PaintText(Drawing.Graphics graphics, string text)
+			{
+				Drawing.Path path = new Drawing.Path ();
+				Drawing.Font font = Drawing.Font.GetFont ("Tahoma", "Regular");
+				double       size = 10;
+				
+				double dx = font.GetTextAdvance (text) * size;
+				
+				double ox = (this.Client.Width - dx) / 2;
+				double oy = 2;
+				
+				graphics.AddFilledRectangle (ox - 1, oy - 2, dx + 2, font.LineHeight * size - 0);
+				graphics.AddFilledRectangle (ox - 2, oy - 1, dx + 4, font.LineHeight * size - 2);
+				graphics.RenderSolid (Drawing.Color.FromARGB (0.8, 1, 1, 1));
+				
+				graphics.AddText (ox, oy, text, font, size);
+				graphics.RenderSolid (Drawing.Color.FromBrightness (0));
+			}
+			
+			
+			private void ConfigureTimer()
+			{
+				if (this.timer == null)
+				{
+					this.timer = new Timer ();
+					this.timer.AutoRepeat = 0.1;
+					this.timer.Delay = 0.1;
+					this.timer.TimeElapsed += new Support.EventHandler(HandleTimerTimeElapsed);
+					this.timer.Start ();
+				}
+			}
+			
+			private void HandleTimerTimeElapsed(object sender)
+			{
+				if (this.IsVisible)
+				{
+					this.Invalidate ();
+				}
+				else if (this.timer != null)
+				{
+					this.timer.Stop ();
+					this.timer.Dispose ();
+					this.timer = null;
+				}
+			}
+			
+			
+			private Magnifier					magnifier;
+			
+			private bool						is_dragging;
+			private Drawing.Point				origin;
+			private double						mask_dx, mask_dy;
+			private Drawing.Graphics			mask;
+			
+			private Timer						timer;
+		}
+	}
+}
