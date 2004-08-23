2af9b512af38f9d15ba5f3ded7bc55016d481393
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:57:17 +0000

    ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1151 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Pictogram/Widgets/PanelRegular.cs
@@ -0,0 +1,157 @@
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+using Epsitec.Common.Pictogram.Data;
+
+namespace Epsitec.Common.Pictogram.Widgets
+{
+	/// <summary>
+	/// La classe PanelRegular permet de choisir un type de polygone régulier.
+	/// </summary>
+	public class PanelRegular : AbstractPanel
+	{
+		public PanelRegular()
+		{
+			this.label = new StaticText(this);
+			this.label.Alignment = Drawing.ContentAlignment.MiddleLeft;
+
+			this.fieldNbFaces = new TextFieldSlider(this);
+			this.fieldNbFaces.MinRange = 3;
+			this.fieldNbFaces.MaxRange = 24;
+			this.fieldNbFaces.Step = 1;
+			this.fieldNbFaces.TextChanged += new EventHandler(this.HandleFieldChanged);
+
+			this.checkStar = new CheckButton(this);
+			this.checkStar.Text = "Etoile";
+			this.checkStar.ActiveStateChanged += new EventHandler(this.HandleCheckChanged);
+
+			this.fieldDeep = new TextFieldSlider(this);
+			this.fieldDeep.MinRange = 0;
+			this.fieldDeep.MaxRange = 100;
+			this.fieldDeep.Step = 5;
+			this.fieldDeep.TextChanged += new EventHandler(this.HandleFieldChanged);
+
+			this.labelDeep = new StaticText(this);
+			this.labelDeep.Text = "Renfoncement";
+			this.labelDeep.Alignment = Drawing.ContentAlignment.MiddleCenter;
+
+			this.isNormalAndExtended = true;
+		}
+		
+		public PanelRegular(Widget embedder) : this()
+		{
+			this.SetEmbedder(embedder);
+		}
+		
+		protected override void Dispose(bool disposing)
+		{
+			if ( disposing )
+			{
+				this.fieldNbFaces.TextChanged -= new EventHandler(this.HandleFieldChanged);
+				this.checkStar.ActiveStateChanged -= new EventHandler(this.HandleCheckChanged);
+				this.fieldDeep.TextChanged -= new EventHandler(this.HandleFieldChanged);
+			}
+			
+			base.Dispose(disposing);
+		}
+
+		
+		// Retourne la hauteur standard.
+		public override double DefaultHeight
+		{
+			get
+			{
+				return ( this.extendedSize ? 55 : 30 );
+			}
+		}
+
+		// Propriété -> widget.
+		public override void SetProperty(AbstractProperty property)
+		{
+			base.SetProperty(property);
+			this.label.Text = this.text;
+
+			PropertyRegular p = property as PropertyRegular;
+			if ( p == null )  return;
+
+			this.fieldNbFaces.Value = p.NbFaces;
+			this.checkStar.ActiveState = p.Star ? WidgetState.ActiveYes : WidgetState.ActiveNo;
+			this.fieldDeep.Value = p.Deep*100;
+
+			this.EnableWidgets();
+		}
+
+		// Widget -> propriété.
+		public override AbstractProperty GetProperty()
+		{
+			PropertyRegular p = new PropertyRegular();
+			base.GetProperty(p);
+
+			p.NbFaces = (int)this.fieldNbFaces.Value;
+			p.Star = ( this.checkStar.ActiveState == WidgetState.ActiveYes );
+			p.Deep = this.fieldDeep.Value/100;
+
+			return p;
+		}
+
+		// Grise les widgets nécessaires.
+		protected void EnableWidgets()
+		{
+			bool star = ( this.checkStar.ActiveState == WidgetState.ActiveYes );
+			this.fieldDeep.SetEnabled(star);
+		}
+
+		// Met à jour la géométrie.
+		protected override void UpdateClientGeometry()
+		{
+			base.UpdateClientGeometry();
+
+			if ( this.fieldNbFaces == null )  return;
+
+			Drawing.Rectangle rect = this.Client.Bounds;
+			rect.Left += this.extendedZoneWidth;
+			rect.Inflate(-5, -5);
+
+			Drawing.Rectangle r = rect;
+			r.Bottom = r.Top-20;
+			r.Right = rect.Right-50;
+			this.label.Bounds = r;
+
+			r.Left = rect.Right-50;
+			r.Right = rect.Right;
+			this.fieldNbFaces.Bounds = r;
+
+			rect.Top = r.Bottom-5;
+			rect.Bottom = rect.Top-20;
+			r = rect;
+			r.Height = 20;
+			r.Width = 50;
+			this.checkStar.Bounds = r;
+			r.Left = r.Right;
+			r.Width = 75;
+			this.labelDeep.Bounds = r;
+			r.Left = r.Right;
+			r.Width = 50;
+			this.fieldDeep.Bounds = r;
+		}
+		
+		// Un champ a été changé.
+		private void HandleFieldChanged(object sender)
+		{
+			this.OnChanged();
+		}
+
+		// Un champ a été changé.
+		private void HandleCheckChanged(object sender)
+		{
+			this.EnableWidgets();
+			this.OnChanged();
+		}
+
+
+		protected StaticText				label;
+		protected TextFieldSlider			fieldNbFaces;
+		protected CheckButton				checkStar;
+		protected TextFieldSlider			fieldDeep;
+		protected StaticText				labelDeep;
+	}
+}
