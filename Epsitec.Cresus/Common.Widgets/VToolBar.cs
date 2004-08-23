249d4f2fb98945dba15c508db8c2b6b03e44cbe2
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:11:33 +0000

    ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@715 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/VToolBar.cs
@@ -0,0 +1,112 @@
+namespace Epsitec.Common.Widgets
+{
+	/// <summary>
+	/// La classe VToolBar permet de réaliser des tool bars verticales.
+	/// </summary>
+	public class VToolBar : Widget, Helpers.IWidgetCollectionHost
+	{
+		public VToolBar()
+		{
+			this.items = new Helpers.WidgetCollection(this);
+			
+			IconButton button = new IconButton();
+			this.defaultButtonWidth = button.DefaultWidth;
+			this.defaultButtonHeight = button.DefaultHeight;
+			button.Dispose();
+
+			double m = (this.DefaultWidth-this.defaultButtonWidth)/2;
+			this.DockMargins = new Drawing.Margins(m, m, m, m);
+		}
+		
+		public VToolBar(Widget embedder) : this()
+		{
+			this.SetEmbedder(embedder);
+		}
+
+
+		public override double				DefaultWidth
+		{
+			// Retourne la largeur standard d'une barre.
+			get
+			{
+				return 28;
+			}
+		}
+
+		public Helpers.WidgetCollection		Items
+		{
+			get { return this.items; }
+		}
+
+		
+		protected override void Dispose(bool disposing)
+		{
+			if ( disposing )
+			{
+				Widget[] items = new Widget[this.items.Count];
+				this.items.CopyTo(items, 0);
+				this.items.Clear();
+				
+				foreach ( Widget item in items )
+				{
+					item.Dispose();
+				}
+				
+				this.items.Dispose();
+				this.items = null;
+			}
+			
+			base.Dispose(disposing);
+		}
+
+		protected override void PaintBackgroundImplementation(Drawing.Graphics graphics, Drawing.Rectangle clipRect)
+		{
+			// Dessine la barre.
+			IAdorner adorner = Widgets.Adorner.Factory.Active;
+
+			Drawing.Rectangle rect  = new Drawing.Rectangle(0, 0, this.Client.Width, this.Client.Height);
+			adorner.PaintToolBackground(graphics, rect, WidgetState.None, Direction.None, Direction.Down);
+		}
+
+
+		#region Interface IBundleSupport
+		public override void RestoreFromBundle(Epsitec.Common.Support.ObjectBundler bundler, Epsitec.Common.Support.ResourceBundle bundle)
+		{
+			base.RestoreFromBundle (bundler, bundle);
+			
+			System.Collections.IList item_list = bundle.GetFieldBundleList ("items");
+			
+			if (item_list != null)
+			{
+				//	Notre bundle contient une liste de sous-bundles contenant les descriptions des
+				//	items composant le menu.
+				
+				foreach (Support.ResourceBundle item_bundle in item_list)
+				{
+					Widget item = bundler.CreateFromBundle (item_bundle) as Widget;
+					
+					this.Items.Add (item);
+				}
+			}
+		}
+		#endregion
+		
+		#region IWidgetCollectionHost Members
+		public void NotifyInsertion(Widget widget)
+		{
+			widget.Dock = DockStyle.Top;
+			widget.SetEmbedder (this);
+		}
+
+		public void NotifyRemoval(Widget widget)
+		{
+			this.Children.Remove (widget);
+		}
+		#endregion
+		
+		protected Helpers.WidgetCollection	items;
+		
+		protected double					defaultButtonWidth;
+		protected double					defaultButtonHeight;
+	}
+}
