fbcb345a87529e1d06387d37177f83ace9848347
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:10:25 +0000

    ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.
    **********************
    Label: "13-01-2004"
    User: Arnaud       Date: 13.01.04   Time: 20:50
    Labeled
    Label comment: Stable, before rework of Layout management.
    
    **********************
    Label: "12.12.2003"
    User: Arnaud       Date: 13.12.03   Time: 18:39
    Labeled
    Label comment: 
    
    **********************
    Label: "08-12-2003"
    User: Arnaud       Date:  8.12.03   Time: 15:25
    Labeled
    Label comment: Stable version.
    
    git-svn-id: https://svn.opac.ch/svn/cr@698 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Widgets/Adorners/LookDany.cs
+++ Epsitec.Cresus/Common.Widgets/Adorners/LookDany.cs
@@ -24,6 +24,7 @@ namespace Epsitec.Common.Widgets.Adorner
 			this.colorControlDarkDark   = Drawing.Color.FromName("ControlDarkDark");
 			this.colorCaption           = Drawing.Color.FromName("ActiveCaption");
 			this.colorCaptionText       = Drawing.Color.FromName("ActiveCaptionText");
+			this.colorInfo              = Drawing.Color.FromName("Info");
 
 			r = 1-(1-this.colorControlLight.R)*0.5;
 			g = 1-(1-this.colorControlLight.G)*0.5;
@@ -392,7 +393,7 @@ namespace Epsitec.Common.Widgets.Adorner
 											 Widgets.TextFieldStyle style,
 											 bool readOnly)
 		{
-			if ( style == TextFieldStyle.Normal )
+			if ( style == TextFieldStyle.Normal || style == TextFieldStyle.UpDown )
 			{
 				graphics.AddFilledRectangle(rect);
 				if ( (state&WidgetState.Enabled) != 0 )  // bouton enable ?
@@ -461,7 +462,7 @@ namespace Epsitec.Common.Widgets.Adorner
 			graphics.AddRectangle(rInside);
 			graphics.RenderSolid(this.colorControlDark);
 
-			if ( !tabRect.IsEmpty )
+			if ( !tabRect.IsSurfaceZero )
 			{
 				graphics.AddFilledRectangle(tabRect);
 				graphics.RenderSolid(this.colorControlDark);
@@ -701,6 +702,29 @@ namespace Epsitec.Common.Widgets.Adorner
 		{
 		}
 
+		// Dessine le fond d'un tableau.
+		public void PaintArrayBackground(Drawing.Graphics graphics, Drawing.Rectangle rect, WidgetState state, Direction shadow)
+		{
+			graphics.AddFilledRectangle(rect);
+			if ( (state&WidgetState.Enabled) != 0 )  // bouton enable ?
+			{
+				graphics.RenderSolid(this.colorControlLightLight);
+			}
+			else
+			{
+				graphics.RenderSolid(this.colorControl);
+			}
+
+			graphics.LineWidth = 1;
+			graphics.LineCap = Drawing.CapStyle.Butt;
+
+			Drawing.Rectangle rInside = rect;
+			rInside.Inflate(-0.5, -0.5);
+
+			graphics.AddRectangle(rInside);
+			graphics.RenderSolid(this.colorBlack);
+		}
+
 		// Dessine le fond d'une cellule.
 		public void PaintCellBackground(Drawing.Graphics graphics, Drawing.Rectangle rect, WidgetState state, Direction shadow)
 		{
@@ -806,7 +830,7 @@ namespace Epsitec.Common.Widgets.Adorner
 			}
 
 			rect.Inflate(-0.5, -0.5);
-			if ( parentRect.IsEmpty )
+			if ( parentRect.IsSurfaceZero )
 			{
 				graphics.AddRectangle(rect);
 				graphics.RenderSolid(this.colorControlDark);
@@ -907,6 +931,84 @@ namespace Epsitec.Common.Widgets.Adorner
 		{
 		}
 
+		// Dessine un séparateur horizontal ou vertical.
+		public void PaintSeparatorBackground(Drawing.Graphics graphics,
+											 Drawing.Rectangle rect,
+											 WidgetState state,
+											 Direction shadow,
+											 Direction type,
+											 bool optional)
+		{
+			if ( type == Direction.Right )
+			{
+				Drawing.Point p1 = new Drawing.Point(rect.Left+rect.Width/2, rect.Bottom);
+				Drawing.Point p2 = new Drawing.Point(rect.Left+rect.Width/2, rect.Top);
+				graphics.Align(ref p1);
+				graphics.Align(ref p2);
+				p1.X += 0.5;
+				p2.X += 0.5;
+				graphics.AddLine(p1, p2);
+			}
+			else
+			{
+				Drawing.Point p1 = new Drawing.Point(rect.Left, rect.Bottom+rect.Height/2);
+				Drawing.Point p2 = new Drawing.Point(rect.Right, rect.Bottom+rect.Height/2);
+				graphics.Align(ref p1);
+				graphics.Align(ref p2);
+				p1.Y += 0.5;
+				p2.Y += 0.5;
+				graphics.AddLine(p1, p2);
+			}
+
+			graphics.RenderSolid(Drawing.Color.FromBrightness(0.75));
+		}
+
+		public void PaintSeparatorForeground(Drawing.Graphics graphics,
+											 Drawing.Rectangle rect,
+											 WidgetState state,
+											 Direction shadow,
+											 Direction type,
+											 bool optional)
+		{
+		}
+
+		// Dessine une case de statuts.
+		public void PaintStatusBackground(Drawing.Graphics graphics,
+										  Drawing.Rectangle rect,
+										  WidgetState state,
+										  Direction shadow)
+		{
+			rect.Width -= 1;
+			rect.Inflate(-0.5, -0.5);
+			graphics.AddRectangle(rect);
+			graphics.RenderSolid(Drawing.Color.FromBrightness(0.5));
+		}
+
+		public void PaintStatusForeground(Drawing.Graphics graphics,
+										  Drawing.Rectangle rect,
+										  WidgetState state,
+										  Direction shadow)
+		{
+		}
+
+		// Dessine le fond d'une bulle d'aide.
+		public void PaintTooltipBackground(Drawing.Graphics graphics, Drawing.Rectangle rect, Direction shadow)
+		{
+			graphics.AddFilledRectangle(rect);
+			graphics.RenderSolid(this.colorInfo);  // fond jaune pale
+			
+			rect.Inflate(-0.5, -0.5);
+			graphics.AddRectangle(rect);
+			graphics.RenderSolid(this.colorBlack);  // cadre noir
+		}
+
+		// Dessine le texte d'une bulle d'aide.
+		public void PaintTooltipTextLayout(Drawing.Graphics graphics, Drawing.Point pos, TextLayout text, Direction shadow)
+		{
+			text.Paint(pos, graphics, Drawing.Rectangle.Infinite, this.colorBlack);
+		}
+
+
 		// Dessine le rectangle pour indiquer le focus.
 		public void PaintFocusBox(Drawing.Graphics graphics,
 								  Drawing.Rectangle rect)
@@ -972,7 +1074,7 @@ namespace Epsitec.Common.Widgets.Adorner
 			{
 				Drawing.Rectangle rFocus = text.StandardRectangle;
 				rFocus.Offset(pos);
-				graphics.Align (ref rFocus);
+				graphics.Align(ref rFocus);
 				rFocus.Inflate(2.5, -0.5);
 				PaintFocusBox(graphics, rFocus);
 			}
@@ -1051,6 +1153,20 @@ namespace Epsitec.Common.Widgets.Adorner
 		}
 
 
+		public void AdaptEnabledTextColor(ref Drawing.Color color)
+		{
+		}
+
+		public void AdaptDisabledTextColor(ref Drawing.Color color, Drawing.Color uniqueColor)
+		{
+			double alpha = color.A;
+			double intensity = color.GetBrightness ();
+			intensity = 0.5+(intensity-0.5)*0.25;  // diminue le contraste
+			intensity = System.Math.Min(intensity+0.3, 1.0);  // augmente l'intensité
+			color = Drawing.Color.FromBrightness(intensity);
+			color.A = alpha;
+		}
+
 		public Drawing.Color GetColorCaption()
 		{
 			return this.colorCaption;
@@ -1066,8 +1182,12 @@ namespace Epsitec.Common.Widgets.Adorner
 			return this.colorWindow;
 		}
 
+		public Drawing.Color GetColorBorder()
+		{
+			return Drawing.Color.FromBrightness(0.5);
+		}
 
-		// Variables membres de TextLayout.
+
 		protected Drawing.Color		colorBlack;
 		protected Drawing.Color		colorControl;
 		protected Drawing.Color		colorControlLight;
@@ -1079,6 +1199,7 @@ namespace Epsitec.Common.Widgets.Adorner
 		protected Drawing.Color		colorCaption;
 		protected Drawing.Color		colorCaptionText;
 		protected Drawing.Color		colorCaptionLight;
+		protected Drawing.Color		colorInfo;
 		protected Drawing.Color		colorButton;
 		protected Drawing.Color		colorHilite;
 		protected Drawing.Color		colorWindow;
