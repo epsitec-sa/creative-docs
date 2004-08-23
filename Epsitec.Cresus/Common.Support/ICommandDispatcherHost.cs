11e1bda19a3bdd113751be4000abab64a00dfa0d
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:13:48 +0000

    ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.
    **********************
    Label: "11-02-2004"
    User: Arnaud       Date: 12.02.04   Time:  7:47
    Labeled
    Label comment: Stable version.
    
    git-svn-id: https://svn.opac.ch/svn/cr@1273 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Support/ICommandDispatcherHost.cs
+++ Epsitec.Cresus/Common.Support/ICommandDispatcherHost.cs
@@ -1,7 +1,10 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : OK/PA, 06/02/2004
+
 namespace Epsitec.Common.Support
 {
 	public interface ICommandDispatcherHost
 	{
-		Support.CommandDispatcher CommandDispatcher { get; }
+		Support.CommandDispatcher	CommandDispatcher	{ get; set; }
 	}
 }
