5b8b5822361252dd55967d2cd18983cfccfeddbc
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:17:15 +0000

    ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1874 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/AbstractDataCollection.cs
@@ -0,0 +1,135 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : OK/PA, 26/04/2004
+
+namespace Epsitec.Common.Types
+{
+	/// <summary>
+	/// La classe Record décrit un ensemble de champs utilisés pour échanger des
+	/// données entre une application et son interface via mapper/binder/...
+	/// </summary>
+	public abstract class AbstractDataCollection : IDataCollection
+	{
+		public AbstractDataCollection()
+		{
+		}
+		
+		
+		#region IDataCollection Members
+		public IDataItem						this[string name]
+		{
+			get
+			{
+				//	TODO: on pourrait utiliser une table de hachage pour accélérer la recherche
+				//	par les noms; à partir de combien d'éléments ceci deviendrait-il utile ?
+				
+				IDataItem[] items = this.GetCachedItemArray ();
+				
+				for (int i = 0; i < items.Length; i++)
+				{
+					if (items[i].Name == name)
+					{
+						return items[i];
+					}
+				}
+				
+				return null;
+			}
+		}
+
+		public IDataItem						this[int index]
+		{
+			get
+			{
+				IDataItem[] items = this.GetCachedItemArray ();
+				return items[index];
+			}
+		}
+
+		public int IndexOf(IDataItem item)
+		{
+			IDataItem[] items = this.GetCachedItemArray ();
+			
+			for (int i = 0; i < items.Length; i++)
+			{
+				if (items[i] == item)
+				{
+					return i;
+				}
+			}
+			
+			return -1;
+		}
+		#endregion
+		
+		#region ICollection Members
+		public bool								IsSynchronized
+		{
+			get
+			{
+				return false;
+			}
+		}
+		
+		public int								Count
+		{
+			get
+			{
+				return this.list.Count;
+			}
+		}
+		
+		public object							SyncRoot
+		{
+			get
+			{
+				return this;
+			}
+		}
+		
+		public void CopyTo(System.Array array, int index)
+		{
+			this.list.CopyTo (array, index);
+		}
+		#endregion
+		
+		#region IEnumerable Members
+		public System.Collections.IEnumerator GetEnumerator()
+		{
+			return this.list.GetEnumerator ();
+		}
+		#endregion
+		
+		protected IDataItem[]					CachedItemArray
+		{
+			get
+			{
+				if (this.is_dirty)
+				{
+					this.UpdateCachedItemArray ();
+				}
+				
+				return this.GetCachedItemArray ();
+			}
+		}
+		
+		
+		protected abstract IDataItem[] GetCachedItemArray();
+		
+		protected virtual void UpdateCachedItemArray()
+		{
+			this.is_dirty = false;
+		}
+		
+		protected virtual void ClearCachedItemArray()
+		{
+			//	TODO: le jour où un accès aux noms est implémenté via une table de hachage, il
+			//	faudra purger la table ici...
+			
+			this.is_dirty = true;
+		}
+		
+		
+		protected System.Collections.ArrayList	list = new System.Collections.ArrayList ();
+		private bool							is_dirty;
+	}
+}
