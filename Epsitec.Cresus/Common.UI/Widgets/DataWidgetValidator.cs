f44a84b865dae581e4be2db571e53e9714178ca9
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:42:04 +0000

    ********************** Label: "Stable-25-06-2004" User: Arnaud       Date: 25.06.04   Time: 16:09 Labeled Label comment: Version with Drawing.TextStyle implemented, Support.EventHandler moved to its own assembly and script almost done.  ********************** Label: "Stable-22-06-2004" User: Arnaud       Date: 22.06.04   Time: 18:13 Labeled Label comment: First version with dynamic script compilation.  ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2625 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.UI/Widgets/DataWidgetValidator.cs
@@ -0,0 +1,67 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+namespace Epsitec.Common.UI.Widgets
+{
+	/// <summary>
+	/// La classe DataWidgetValidator implémente IValidator pour un DataWidget.
+	/// </summary>
+	public class DataWidgetValidator : Common.Widgets.Validators.AbstractValidator
+	{
+		public DataWidgetValidator(Common.Widgets.Widget widget) : base (widget)
+		{
+		}
+		
+		
+		public override void Validate()
+		{
+			DataWidget  data_widget = this.widget as DataWidget;
+			
+			if ((data_widget == null) ||
+				(data_widget.DataSource == null))
+			{
+				this.state = Support.ValidationState.Error;
+				return;
+			}
+			
+			Types.IDataValue      data_source = data_widget.DataSource;
+			Types.IDataConstraint constraint  = data_source.DataConstraint;
+			
+			bool ok = data_source.IsValueValid;
+			
+			if ((ok) &&
+				(constraint != null))
+			{
+				ok = constraint.CheckConstraint (data_source.ReadValue ());
+			}
+			
+			this.state = ok ? Support.ValidationState.Ok : Support.ValidationState.Error;
+		}
+		
+		
+		protected override void AttachWidget(Common.Widgets.Widget widget)
+		{
+			base.AttachWidget (widget);
+			
+			DataWidget data_widget = widget as DataWidget;
+			
+			data_widget.DataChanged += new Support.EventHandler (this.HandleWidgetDataChanged);
+		}
+		
+		protected override void DetachWidget(Common.Widgets.Widget widget)
+		{
+			base.DetachWidget (widget);
+			
+			DataWidget data_widget = widget as DataWidget;
+			
+			data_widget.TextChanged -= new Support.EventHandler (this.HandleWidgetDataChanged);
+		}
+		
+		
+		private void HandleWidgetDataChanged(object sender)
+		{
+			this.MakeDirty (false);
+		}
+	}
+}
+
