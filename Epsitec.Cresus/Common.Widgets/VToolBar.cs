49f112b7eeaeaaa274375fd8238efb575f0a3678
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:17:08 +0000

    ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.
    git-svn-id: https://svn.opac.ch/svn/cr@1303 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Widgets/VToolBar.cs
+++ Epsitec.Cresus/Common.Widgets/VToolBar.cs
@@ -3,17 +3,13 @@ namespace Epsitec.Common.Widgets
 	/// <summary>
 	/// La classe VToolBar permet de réaliser des tool bars verticales.
 	/// </summary>
-	public class VToolBar : Widget, Helpers.IWidgetCollectionHost
+	public class VToolBar : AbstractToolBar
 	{
 		public VToolBar()
 		{
-			this.items = new Helpers.WidgetCollection(this);
+			this.direction     = Direction.Left;
+			this.iconDockStyle = DockStyle.Top;
 			
-			IconButton button = new IconButton();
-			this.defaultButtonWidth = button.DefaultWidth;
-			this.defaultButtonHeight = button.DefaultHeight;
-			button.Dispose();
-
 			double m = (this.DefaultWidth-this.defaultButtonWidth)/2;
 			this.DockMargins = new Drawing.Margins(m, m, m, m);
 		}
@@ -32,91 +28,5 @@ namespace Epsitec.Common.Widgets
 				return 28;
 			}
 		}
-
-		public Helpers.WidgetCollection		Items
-		{
-			get { return this.items; }
-		}
-
-		
-		protected override void Dispose(bool disposing)
-		{
-			if ( disposing )
-			{
-				Widget[] items = new Widget[this.items.Count];
-				this.items.CopyTo(items, 0);
-				this.items.Clear();
-				
-				foreach ( Widget item in items )
-				{
-					item.Dispose();
-				}
-				
-				this.items.Dispose();
-				this.items = null;
-			}
-			
-			base.Dispose(disposing);
-		}
-
-		protected override void PaintBackgroundImplementation(Drawing.Graphics graphics, Drawing.Rectangle clipRect)
-		{
-			// Dessine la barre.
-			IAdorner adorner = Widgets.Adorner.Factory.Active;
-
-			Drawing.Rectangle rect  = this.Client.Bounds;
-			adorner.PaintToolBackground(graphics, rect, WidgetState.None, Direction.Left);
-		}
-
-
-		#region Interface IBundleSupport
-		public override void RestoreFromBundle(Epsitec.Common.Support.ObjectBundler bundler, Epsitec.Common.Support.ResourceBundle bundle)
-		{
-			base.RestoreFromBundle (bundler, bundle);
-			
-			Support.ResourceBundle.FieldList item_list = bundle["items"].AsList;
-			
-			if (item_list != null)
-			{
-				//	Notre bundle contient une liste de sous-bundles contenant les descriptions des
-				//	items composant le menu.
-				
-				foreach (Support.ResourceBundle.Field field in item_list)
-				{
-					Support.ResourceBundle item_bundle = field.AsBundle;
-					Widget                 item_widget = bundler.CreateFromBundle (item_bundle) as Widget;
-					
-					this.Items.Add (item_widget);
-				}
-			}
-		}
-		#endregion
-		
-		#region IWidgetCollectionHost Members
-		Helpers.WidgetCollection Helpers.IWidgetCollectionHost.GetWidgetCollection()
-		{
-			return this.Items;
-		}
-		
-		public void NotifyInsertion(Widget widget)
-		{
-			widget.Dock = DockStyle.Top;
-			widget.SetEmbedder (this);
-		}
-
-		public void NotifyRemoval(Widget widget)
-		{
-			this.Children.Remove (widget);
-		}
-		
-		public void NotifyPostRemoval(Widget widget)
-		{
-		}
-		#endregion
-		
-		protected Helpers.WidgetCollection	items;
-		
-		protected double					defaultButtonWidth;
-		protected double					defaultButtonHeight;
 	}
 }
