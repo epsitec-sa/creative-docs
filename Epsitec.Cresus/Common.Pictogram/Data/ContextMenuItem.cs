63f8a9fb963a6b0f3f5b6caabddc6e521b29bf66
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:24:38 +0000

    ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.
    **********************
    Label: "21-01-2004"
    User: Arnaud       Date: 21.01.04   Time: 18:14
    Labeled
    Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.
    
    
    **********************
    Label: "19-01-2004"
    User: Arnaud       Date: 19.01.04   Time: 16:24
    Labeled
    Label comment: Before in-depth modification of XML handling in ResourceBundle.
    
    **********************
    Label: "13-01-2004"
    User: Arnaud       Date: 13.01.04   Time: 20:50
    Labeled
    Label comment: Stable, before rework of Layout management.
    
    git-svn-id: https://svn.opac.ch/svn/cr@857 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Pictogram/Data/ContextMenuItem.cs
+++ Epsitec.Cresus/Common.Pictogram/Data/ContextMenuItem.cs
@@ -9,7 +9,10 @@ namespace Epsitec.Common.Pictogram.Data
 		{
 			this.name = "";
 			this.icon = "";
+			this.iconActiveNo = "";
+			this.iconActiveYes = "";
 			this.text = "";
+			this.active = false;
 		}
 
 		public string Name
@@ -24,14 +27,35 @@ namespace Epsitec.Common.Pictogram.Data
 			set { this.icon = value; }
 		}
 
+		public string IconActiveNo
+		{
+			get { return this.iconActiveNo; }
+			set { this.iconActiveNo = value; }
+		}
+
+		public string IconActiveYes
+		{
+			get { return this.iconActiveYes; }
+			set { this.iconActiveYes = value; }
+		}
+
 		public string Text
 		{
 			get { return this.text; }
 			set { this.text = value; }
 		}
 
+		public bool Active
+		{
+			get { return this.active; }
+			set { this.active = value; }
+		}
+
 		protected string			name;
 		protected string			icon;
+		protected string			iconActiveNo;
+		protected string			iconActiveYes;
 		protected string			text;
+		protected bool				active;
 	}
 }
