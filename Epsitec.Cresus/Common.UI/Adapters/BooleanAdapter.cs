bb059191afebc83402817d4fdd01656f416ea786
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:23:43 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2466 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.UI/Adapters/BooleanAdapter.cs
@@ -0,0 +1,62 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+
+namespace Epsitec.Common.UI.Adapters
+{
+	/// <summary>
+	/// Summary description for BooleanAdapter.
+	/// </summary>
+	
+	[Controller (1, typeof (Controllers.BooleanController))]
+	
+	public class BooleanAdapter : AbstractAdapter
+	{
+		public BooleanAdapter()
+		{
+		}
+		
+		public BooleanAdapter(Binders.IBinder binder) : this ()
+		{
+			this.Binder = binder;
+			this.Binder.Adapter = this;
+		}
+		
+		
+		public bool								Value
+		{
+			get
+			{
+				return this.value;
+			}
+			set
+			{
+				if (this.value != value)
+				{
+					this.value = value;
+					this.OnValueChanged ();
+				}
+			}
+		}
+		
+		
+		protected override object ConvertToObject()
+		{
+			System.ComponentModel.TypeConverter converter = System.ComponentModel.TypeDescriptor.GetConverter (this.binder.GetDataType ());
+			return converter.ConvertFromString (this.Value.ToString ());
+		}
+		
+		protected override bool ConvertFromObject(object data)
+		{
+			System.ComponentModel.TypeConverter converter = System.ComponentModel.TypeDescriptor.GetConverter (this.binder.GetDataType ());
+			this.Value = System.Boolean.Parse (converter.ConvertToString (data));
+			return true;
+		}
+		
+		
+		
+		private bool							value;
+	}
+}
---
+++ Epsitec.Cresus/Common.UI/Adapters/FlagsAdapter.cs
@@ -0,0 +1,72 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+
+namespace Epsitec.Common.UI.Adapters
+{
+	/// <summary>
+	/// Summary description for FlagsAdapter.
+	/// </summary>
+	
+	[Controller (1, typeof (Controllers.FlagsController))]
+	
+	public class FlagsAdapter : AbstractAdapter
+	{
+		public FlagsAdapter(Types.IEnum enum_type)
+		{
+			this.enum_type = enum_type;
+		}
+		
+		public FlagsAdapter(Types.IEnum enum_type, Binders.IBinder binder) : this (enum_type)
+		{
+			this.Binder = binder;
+			this.Binder.Adapter = this;
+		}
+		
+		
+		public string							Value
+		{
+			get
+			{
+				return this.value;
+			}
+			set
+			{
+				if (this.value != value)
+				{
+					this.value = value;
+					this.OnValueChanged ();
+				}
+			}
+		}
+		
+		public Types.IEnum						EnumType
+		{
+			get
+			{
+				return this.enum_type;
+			}
+		}
+		
+		
+		protected override object ConvertToObject()
+		{
+			System.ComponentModel.TypeConverter converter = System.ComponentModel.TypeDescriptor.GetConverter (this.binder.GetDataType ());
+			return converter.ConvertFromString (this.Value);
+		}
+		
+		protected override bool ConvertFromObject(object data)
+		{
+			System.ComponentModel.TypeConverter converter = System.ComponentModel.TypeDescriptor.GetConverter (this.binder.GetDataType ());
+			this.Value = converter.ConvertToString (data);
+			return true;
+		}
+		
+		
+		
+		private string							value;
+		private Types.IEnum						enum_type;
+	}
+}
