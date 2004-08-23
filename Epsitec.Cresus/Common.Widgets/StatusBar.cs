4c5cab74e44e673f350915fbe87721d4d73517a1
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:11:21 +0000

    ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@711 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/StatusBar.cs
@@ -0,0 +1,104 @@
+namespace Epsitec.Common.Widgets
+{
+	/// <summary>
+	/// La classe StatusBar représente une barre de statuts en bas d'une fenêtre.
+	/// </summary>
+	public class StatusBar : Widget, Helpers.IWidgetCollectionHost
+	{
+		public StatusBar()
+		{
+			this.items = new Helpers.WidgetCollection(this);
+
+			this.DockMargins = new Drawing.Margins(1, 1, 2, 1);
+		}
+		
+		public StatusBar(Widget embedder) : this()
+		{
+			this.SetEmbedder(embedder);
+		}
+		
+		public override double DefaultHeight
+		{
+			// Retourne la hauteur standard.
+			get
+			{
+				return this.DefaultFontHeight+6;
+			}
+		}
+
+		public Helpers.WidgetCollection		Items
+		{
+			get { return this.items; }
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if ( disposing )
+			{
+				Widget[] items = new Widget[this.items.Count];
+				this.items.CopyTo(items, 0);
+				this.items.Clear();
+				
+				foreach ( Widget item in items )
+				{
+					item.Dispose();
+				}
+				
+				this.items.Dispose();
+				this.items = null;
+			}
+			
+			base.Dispose(disposing);
+		}
+
+		protected override void PaintBackgroundImplementation(Drawing.Graphics graphics, Drawing.Rectangle clipRect)
+		{
+			IAdorner adorner = Widgets.Adorner.Factory.Active;
+
+			Drawing.Rectangle rect  = new Drawing.Rectangle(0, 0, this.Client.Width, this.Client.Height);
+			WidgetState       state = this.PaintState;
+			Direction         dir   = this.RootDirection;
+			Drawing.Point     pos   = new Drawing.Point(0, 0);
+			
+			graphics.AddLine(rect.Left, rect.Top-0.5, rect.Right, rect.Top-0.5);
+			graphics.RenderSolid(adorner.GetColorBorder());
+		}
+
+		#region Interface IBundleSupport
+		public override void RestoreFromBundle(Epsitec.Common.Support.ObjectBundler bundler, Epsitec.Common.Support.ResourceBundle bundle)
+		{
+			base.RestoreFromBundle (bundler, bundle);
+			
+			System.Collections.IList item_list = bundle.GetFieldBundleList ("items");
+			
+			if (item_list != null)
+			{
+				//	Notre bundle contient une liste de sous-bundles contenant les descriptions des
+				//	items composant le menu.
+				
+				foreach (Support.ResourceBundle item_bundle in item_list)
+				{
+					Widget item = bundler.CreateFromBundle (item_bundle) as Widget;
+					
+					this.Items.Add (item);
+				}
+			}
+		}
+		#endregion
+		
+		#region IWidgetCollectionHost Members
+		public void NotifyInsertion(Widget widget)
+		{
+			widget.Dock = DockStyle.Left;
+			widget.SetEmbedder (this);
+		}
+
+		public void NotifyRemoval(Widget widget)
+		{
+			this.Children.Remove (widget);
+		}
+		#endregion
+		
+		protected Helpers.WidgetCollection	items;
+	}
+}
---
+++ Epsitec.Cresus/Common.Widgets/StatusField.cs
@@ -0,0 +1,95 @@
+namespace Epsitec.Common.Widgets
+{
+	/// <summary>
+	/// La classe StatusField représente une case d'une ligne de statuts.
+	/// </summary>
+	public class StatusField : Widget
+	{
+		public StatusField()
+		{
+			this.BackColor = Drawing.Color.Transparent;
+		}
+		
+		public StatusField(Widget embedder) : this()
+		{
+			this.SetEmbedder(embedder);
+		}
+		public StatusField(string text) : this()
+		{
+			this.Text = text;
+		}
+		
+		public override double DefaultWidth
+		{
+			// Retourne la largeur standard.
+			get
+			{
+				return 120;
+			}
+		}
+
+		public override double DefaultHeight
+		{
+			// Retourne la hauteur standard.
+			get
+			{
+				return this.DefaultFontHeight;
+			}
+		}
+
+		protected override void UpdateLayoutSize()
+		{
+			if ( this.textLayout != null )
+			{
+				double dx = this.Client.Width - this.margin*2;
+				double dy = this.Client.Height;
+				this.textLayout.Alignment = this.Alignment;
+				this.textLayout.LayoutSize = new Drawing.Size(dx, dy);
+			}
+		}
+
+		public override Drawing.ContentAlignment DefaultAlignment
+		{
+			// Retourne l'alignement par défaut d'un bouton.
+			get
+			{
+				return Drawing.ContentAlignment.MiddleLeft;
+			}
+		}
+
+		
+		public Drawing.Size RetRequiredSize()
+		{
+			// Retourne les dimensions minimales pour représenter le texte.
+			return this.textLayout.SingleLineSize();
+		}
+
+		protected override void PaintBackgroundImplementation(Drawing.Graphics graphics, Drawing.Rectangle clipRect)
+		{
+			// Dessine le texte.
+			IAdorner adorner = Widgets.Adorner.Factory.Active;
+
+			if ( this.textLayout == null )  return;
+
+			Drawing.Rectangle rect  = new Drawing.Rectangle(0, 0, this.Client.Width, this.Client.Height);
+			WidgetState       state = this.PaintState;
+			Direction         dir   = this.RootDirection;
+			Drawing.Point     pos   = new Drawing.Point(0, 0);
+			
+			if ( !this.BackColor.IsTransparent )
+			{
+				graphics.AddFilledRectangle(rect);
+				graphics.RenderSolid(this.BackColor);
+			}
+
+			adorner.PaintStatusBackground(graphics, rect, state, dir);
+			pos.X += this.margin;
+			pos.Y += 0.5;
+			this.textLayout.BreakMode = Drawing.TextBreakMode.Ellipsis | Drawing.TextBreakMode.SingleLine;
+			adorner.PaintGeneralTextLayout(graphics, pos, this.textLayout, state, dir);
+		}
+
+
+		protected double			margin = 8.0;
+	}
+}
