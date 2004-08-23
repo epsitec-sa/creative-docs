c75d66f34164d0af53852156f8e36c58aa9076e8
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:14:34 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2404 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Designer/Widgets/Switcher.cs
@@ -0,0 +1,290 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+
+namespace Epsitec.Common.Designer.Widgets
+{
+	using IStringCollectionHost = Epsitec.Common.Widgets.Helpers.IStringCollectionHost;
+	using StringCollection      = Epsitec.Common.Widgets.Helpers.StringCollection;
+	
+	public enum SwitcherMode
+	{
+		None,
+		Select,
+		AcceptReject,
+	}
+	
+	/// <summary>
+	/// Summary description for Switcher.
+	/// </summary>
+	public class Switcher : Widget, IStringCollectionHost, Support.Data.INamedStringSelection
+	{
+		public Switcher()
+		{
+			this.items = new StringCollection (this);
+			
+			this.CreateCaption ();
+			this.CreateButtons ();
+			this.UpdateButtons ();
+		}
+		
+		public Switcher(Widget embedder) : this ()
+		{
+			this.SetEmbedder (embedder);
+		}
+		
+		
+		public override double					DefaultHeight
+		{
+			get
+			{
+				return 32;
+			}
+		}
+		
+		public override Drawing.Rectangle		InnerBounds
+		{
+			get
+			{
+				Drawing.Rectangle bounds = base.InnerBounds;
+				bounds.Deflate (2);
+				return bounds;
+			}
+		}
+		
+		public SwitcherMode						Mode
+		{
+			get
+			{
+				return this.mode;
+			}
+			set
+			{
+				if (this.mode != value)
+				{
+					this.mode = value;
+					this.UpdateButtons ();
+				}
+			}
+		}
+		
+		
+		#region IStringCollectionHost Members
+		public void StringCollectionChanged()
+		{
+		}
+		
+		
+		public StringCollection					Items
+		{
+			get
+			{
+				return this.items;
+			}
+		}
+		#endregion
+		
+		#region INamedStringSelection Members
+		public int									SelectedIndex
+		{
+			get
+			{
+				return this.items.FindExactMatch (this.Text);
+			}
+			set
+			{
+				if ((value >= 0) &&
+					(value < this.items.Count))
+				{
+					if (this.Text != this.items[value])
+					{
+						this.Text = this.items[value];
+						this.OnSelectedIndexChanged();
+					}
+				}
+				else
+				{
+					throw new System.ArgumentOutOfRangeException ("value", value, "Specified index is out of range.");
+				}
+			}
+		}
+		
+		public string								SelectedItem
+		{
+			get
+			{
+				int index = this.SelectedIndex;
+				return (index < 0) ? "" : this.items[index];
+			}
+			set
+			{
+				this.SelectedIndex = this.items.IndexOf (value);
+			}
+		}
+		
+		public string								SelectedName
+		{
+			get
+			{
+				int index = this.SelectedIndex;
+				return (index < 0) ? "" : this.items.GetName (index);
+			}
+			set
+			{
+				this.SelectedIndex = this.items.FindNameIndex(value);
+			}
+		}
+		
+		
+		public event Support.EventHandler			SelectedIndexChanged;
+		#endregion
+		
+		protected void CreateCaption()
+		{
+			this.caption = new StaticText (this);
+			this.caption.Dock = DockStyle.Fill;
+			this.caption.SetClientZoom (1.2);
+		}
+		
+		protected void CreateButtons()
+		{
+			this.button_accept = this.CreateButton ("Accept", GlyphShape.Accept);
+			this.button_reject = this.CreateButton ("Reject", GlyphShape.Reject);
+			this.button_select = this.CreateButton ("Select", GlyphShape.ArrowDown);
+			
+			this.button_accept.Clicked += new MessageEventHandler (this.HandleAcceptClicked);
+			this.button_reject.Clicked += new MessageEventHandler (this.HandleRejectClicked);
+			this.button_select.Pressed += new MessageEventHandler (this.HandleSelectPressed);
+		}
+		
+		protected void UpdateButtons()
+		{
+			switch (this.Mode)
+			{
+				case SwitcherMode.None:
+					this.button_accept.SetVisible (false);
+					this.button_reject.SetVisible (false);
+					this.button_select.SetVisible (false);
+					break;
+				
+				case SwitcherMode.AcceptReject:
+					this.button_accept.SetVisible (true);
+					this.button_reject.SetVisible (true);
+					this.button_select.SetVisible (false);
+					break;
+				
+				case SwitcherMode.Select:
+					this.button_accept.SetVisible (false);
+					this.button_reject.SetVisible (false);
+					this.button_select.SetVisible (true);
+					break;
+					
+				default:
+					throw new System.NotImplementedException (string.Format ("Mode {0} not implemented.", this.Mode));
+			}
+		}
+		
+		
+		protected Button CreateButton(string name, GlyphShape shape)
+		{
+			GlyphButton button = new GlyphButton (this);
+			
+			double dim = this.DefaultHeight - 8;
+			
+			button.Dock        = DockStyle.Right;
+			button.DockMargins = new Drawing.Margins (2, 2, 2, 2);
+			button.Name        = name;
+			button.GlyphShape  = shape;
+			button.Size        = new Drawing.Size (dim, dim);
+			button.ButtonStyle = ButtonStyle.ToolItem;
+			
+			return button;
+		}
+		
+		
+		protected override void OnTextChanged()
+		{
+			base.OnTextChanged ();
+			
+			if (this.caption != null)
+			{
+				this.caption.Text = this.Text;
+			}
+		}
+
+		protected override void PaintBackgroundImplementation(Drawing.Graphics graphics, Drawing.Rectangle clip_rect)
+		{
+			Drawing.Rectangle rect = this.Client.Bounds;
+			
+			rect.Deflate (1);
+			
+			graphics.AddFilledRectangle (rect);
+			graphics.RenderSolid (Drawing.Color.FromRGB (0.60, 0.80, 1.00));
+			
+			rect.Inflate (0.5);
+			
+			graphics.LineWidth = 1;
+			graphics.RenderSolid (Drawing.Color.FromRGB (0.40, 0.53, 0.65));
+		}
+		
+		
+		private void HandleAcceptClicked(object sender, MessageEventArgs e)
+		{
+			System.Diagnostics.Debug.Assert (this.button_accept == sender);
+		}
+		
+		private void HandleRejectClicked(object sender, MessageEventArgs e)
+		{
+			System.Diagnostics.Debug.Assert (this.button_reject == sender);
+		}
+		
+		private void HandleSelectPressed(object sender, MessageEventArgs e)
+		{
+			System.Diagnostics.Debug.Assert (this.button_select == sender);
+			
+			VMenu menu = new VMenu ();
+			
+			for (int i = 0; i < this.items.Count; i++)
+			{
+				menu.Items.Add (new MenuItem ("", "", this.items[i], "", this.items.GetName (i)));
+			}
+			
+			menu.Host = this;
+			menu.AdjustSize ();
+			
+			Drawing.Point pos = this.button_select.MapClientToScreen (new Drawing.Point (this.button_select.Client.Width, 0));
+			
+			pos.X -= menu.Width;
+			
+			menu.Accepted += new MenuEventHandler(this.HandleMenuAccepted);
+			menu.ShowAsContextMenu (this.Window, pos);
+		}
+		
+		private void HandleMenuAccepted(object sender, MenuEventArgs e)
+		{
+			VMenu menu = sender as VMenu;
+			this.SelectedName = e.MenuItem.Name;
+		}
+		
+		
+		
+		protected virtual void OnSelectedIndexChanged()
+		{
+			if (this.SelectedIndexChanged != null)
+			{
+				this.SelectedIndexChanged (this);
+			}
+		}
+		
+		protected StaticText					caption;
+		
+		protected Button						button_accept;
+		protected Button						button_reject;
+		protected Button						button_select;
+		
+		private SwitcherMode					mode;
+		private StringCollection				items;
+	}
+}
