6397f7a717e213d0e2e12efea5ec8a6e51831fa9
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 12:29:28 +0000

    git-svn-id: https://svn.opac.ch/svn/cr@265 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/System.Extension/Utilities.cs
+++ Epsitec.Cresus/System.Extension/Utilities.cs
@@ -8,26 +8,31 @@ namespace System
 	{
 		public static bool StringSimplify(ref string value)
 		{
-			//	Convertie une chaîne entre guillemets en une chaîne sans guillemets
+			return Utilities.StringSimplify(ref value, '"');
+		}
+
+		public static bool StringSimplify(ref string value, char quote)
+		{
+			//	Converti une chaîne entre guillemets en une chaîne sans guillemets
 			//	retourne "true" si la chaîne avait des guillemets "false" sinon
 			//	une exception est levée si des guillemets non apparentés sont trouvés
 			//	enlève les doubles guillemets en milieu de chaîne pour n'en mettre qu'un.
 			//	Faudrait-il traiter aussi les séquences BOA genre \t ?
 
-			if (value.IndexOf ('"') == 0)
+			if (value.IndexOf (quote) == 0)
 			{
-				if ((value.Length > 1) && (value.LastIndexOf ('"') == value.Length-1))
+				if ((value.Length > 1) && (value.LastIndexOf (quote) == value.Length-1))
 				{
 					System.Text.StringBuilder buffer = new System.Text.StringBuilder ();
 
 					for (int i = 1 ; i < value.Length-1 ; i++)
 					{
 						char	c = value[i];
-						if (c == '"')
+						if (c == quote)
 						{
 							i++;
 							c = value[i];
-							if ((i == value.Length-1) || (c != '"'))
+							if ((i == value.Length-1) || (c != quote))
 							{
 								throw new System.Exception (string.Format ("Quotes mismatch in {0}", value));
 							}
@@ -42,48 +47,45 @@ namespace System
 			return false;
 		}
 
-		//	?DD	est-ce que cela vaudrait la peine de définir une classe StringArray 
-		//		dérivée de System.Collections.ArrayList ?
-
-		public static int StringToTokens(string value, char sep, out System.Collections.ArrayList list)
+		public static int StringToTokens(string value, char sep, out string[] tokens)
 		{
-			return StringToTokens (value, sep, null, out list);
+			return StringToTokens (value, sep, null, out tokens);
 		}
 	
-		public static int StringToTokens(string value, char sep, string trimchars, out System.Collections.ArrayList list)
+		public static int StringToTokens(string value, char sep, string trimchars, out string [] tokens)
 		{
-			//	DD: Cette routine peut servir dans tous contextes
-			//		elle devrait être dans une librairie à part
-			//		par exemple Epsitec.Cresus.Utilities
-			//		ou même Epsitec.Utilities ?
-
 			//	transforme une chaîne en une série de Tokens strings
-			//	en fonction du séparateur donné
-			//	rend le nombre de tokens trouvé en sortie
-			//	les chaînes entre guillemets sont conservées
-			//	une exception est levée s'il y a un nombre impair de guillemets
+			//	en fonction du séparateur donné.
+			//	Rend le nombre de tokens trouvé en sortie,
+			//	les chaînes entre guillemets ou apostrophes sont conservées.
+			//	Une exception est levée s'il y a un nombre impair de guillemets.
 
 			//	trimchars permet de supprimer les espaces (et autres) excédentaires 
 			//	devant et derrière les strings
 			//			par exemple pour accepter
 			//			un, deux,    trois , quatre
 
-			list = new System.Collections.ArrayList ();
+			System.Collections.ArrayList list = new System.Collections.ArrayList ();
 			System.Text.StringBuilder buffer = new System.Text.StringBuilder ();
 
 			string	s;
 			int		nb = 0;
-			int		lv = 0;
+			int		n1 = 0;
+			int		n2 = 0;
 
 			for (int i = 0 ; i < value.Length ; i++)
 			{
 				char	c = value[i];
-				if (c == '"')
+				if ((c == '"') && (n2 == 0))
 				{
-					lv = 1-lv;
+					n1 = 1-n1;
+				}
+				if ((c == '\'') && (n1 == 0))
+				{
+					n2 = 1-n2;
 				}
 
-				if ( (lv == 0) && (c == sep) )
+				if ((n1 == 0) && (n2 == 0) && (c == sep))
 				{
 					s = buffer.ToString ();
 					if ( trimchars != null ) s.Trim (trimchars.ToCharArray ());
@@ -98,40 +100,26 @@ namespace System
 			if ( trimchars != null ) s.Trim (trimchars.ToCharArray ());
 			list.Add (s);
 			nb++;
-			if ( lv > 0 )
+			if ( (n1 > 0) || (n2 > 0))
 			{
 				throw new System.Exception (string.Format ("Quotes mismatch in {0}", value));
 			}
+			tokens = new string [list.Count];
+			list.CopyTo (tokens, 0);
 			return nb;
 		}
 		
 		public static bool CheckForDuplicates(System.Collections.ICollection data)
 		{
-			object[] temp = new object[data.Count];
-			data.CopyTo (temp, 0);
-			
-			System.Array.Sort (temp);
-			
-			for (int i = 1; i < temp.Length; i++)
-			{
-				if (temp[i-1] == temp[i])
-				{
-					return true;
-				}
-			}
-			
-			return false;
+			return Utilities.CheckForDuplicates (data, true);
 		}
 		
 		public static bool CheckForDuplicates(System.Collections.ICollection data, bool sort)
 		{
-			if (sort)
-			{
-				return Utilities.CheckForDuplicates (data);
-			}
-			
 			object[] temp = new object[data.Count];
 			data.CopyTo (temp, 0);
+
+			if (sort) System.Array.Sort (temp);
 			
 			for (int i = 1; i < temp.Length; i++)
 			{
@@ -146,31 +134,15 @@ namespace System
 		
 		public static bool CheckForDuplicates(System.Collections.ICollection data, System.Collections.IComparer comparer)
 		{
-			object[] temp = new object[data.Count];
-			data.CopyTo (temp, 0);
-			
-			System.Array.Sort (temp, comparer);
-			
-			for (int i = 1; i < temp.Length; i++)
-			{
-				if (comparer.Compare (temp[i-1], temp[i]) == 0)
-				{
-					return true;
-				}
-			}
-			
-			return false;
+			return Utilities.CheckForDuplicates (data, comparer, true);
 		}
 		
 		public static bool CheckForDuplicates(System.Collections.ICollection data, System.Collections.IComparer comparer, bool sort)
 		{
-			if (sort)
-			{
-				return Utilities.CheckForDuplicates (data, comparer);
-			}
-			
 			object[] temp = new object[data.Count];
 			data.CopyTo (temp, 0);
+			
+			if (sort) System.Array.Sort (temp, comparer);
 			
 			for (int i = 1; i < temp.Length; i++)
 			{
