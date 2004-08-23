aafad32b28f5ff29cffa474f1a0bb8bd462d59b7
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:37:20 +0000

    ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:
    **********************
    Label: "Stable-15-03-2004"
    User: Arnaud       Date: 15.03.04   Time: 11:30
    Labeled
    Label comment: EditArray et App.Designer bien avancés.
    
    git-svn-id: https://svn.opac.ch/svn/cr@1501 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/Dialogs/SubBundleSpec.cs
+++ Epsitec.Cresus/Common.Designer/Dialogs/SubBundleSpec.cs
@@ -1,3 +1,6 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : en chantier/PA
+
 using Epsitec.Common.Widgets;
 using Epsitec.Common.Support;
 using System.Globalization;
