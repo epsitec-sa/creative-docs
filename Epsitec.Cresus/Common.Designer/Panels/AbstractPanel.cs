af1619a9d7e132becd9f24e69cfe9da3f6ddd901
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:30:15 +0000

    ********************** Label: "Stable-22-06-2004" User: Arnaud       Date: 22.06.04   Time: 18:13 Labeled Label comment: First version with dynamic script compilation.
    git-svn-id: https://svn.opac.ch/svn/cr@2513 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/Panels/AbstractPanel.cs
+++ Epsitec.Cresus/Common.Designer/Panels/AbstractPanel.cs
@@ -1,112 +1,15 @@
 //	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
-//	Statut : en chantier/PA
-
-using Epsitec.Common.Support;
-using Epsitec.Common.Widgets;
+//	Responsable: Pierre ARNAUD
 
 namespace Epsitec.Common.Designer.Panels
 {
 	/// <summary>
 	/// La classe AbstractPanel est la base de toutes les classes XyzPanel.
 	/// </summary>
-	public abstract class AbstractPanel : Support.ICommandDispatcherHost, System.IDisposable
+	public abstract class AbstractPanel : Common.UI.AbstractPanel
 	{
 		public AbstractPanel()
 		{
 		}
-		
-		
-		public void SetCommandDispatcher(Support.CommandDispatcher dispatcher)
-		{
-			if (this.dispatcher != dispatcher)
-			{
-				this.dispatcher = dispatcher;
-				this.OnCommandDispatcherChanged ();
-			}
-		}
-		
-		
-		public Drawing.Size						Size
-		{
-			get
-			{
-				return this.size;
-			}
-		}
-		
-		public Widget							Widget
-		{
-			get
-			{
-				if (this.widget == null)
-				{
-					this.CreateWidget ();
-					this.widget.SetCommandDispatcher (this.CommandDispatcher);
-				}
-				
-				return this.widget;
-			}
-		}
-		
-		public Support.CommandDispatcher		CommandDispatcher
-		{
-			get
-			{
-				return this.dispatcher;
-			}
-			set
-			{
-				if (this.dispatcher != value)
-				{
-					this.SetCommandDispatcher (value);
-				}
-			}
-		}
-		
-		
-		#region IDisposable Members
-		public void Dispose()
-		{
-			this.Dispose (true);
-			System.GC.SuppressFinalize (this);
-		}
-		#endregion
-		
-		protected virtual void Dispose(bool disposing)
-		{
-			if (disposing)
-			{
-				this.widget.Dispose ();
-				
-				this.widget     = null;
-				this.dispatcher = null;
-			}
-		}
-		
-		protected virtual void CreateWidget()
-		{
-			this.widget = new Widget ();
-			
-			this.widget.Size    = this.Size;
-			this.widget.MinSize = this.Size;
-			this.widget.TabNavigation = Widget.TabNavigationMode.ForwardTabPassive;
-			
-			this.CreateWidgets (this.widget);
-		}
-
-		protected abstract void CreateWidgets(Widget parent);
-		
-		protected virtual void OnCommandDispatcherChanged()
-		{
-			if (this.widget != null)
-			{
-				this.widget.SetCommandDispatcher (this.dispatcher);
-			}
-		}
-		
-		
-		protected Drawing.Size					size;
-		protected Widget						widget;
-		protected Support.CommandDispatcher		dispatcher;
 	}
 }
