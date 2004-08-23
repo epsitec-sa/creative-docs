6ca749c7da876183193ee46742345a7d738117b1
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:57:08 +0000

    ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1149 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Pictogram/Widgets/PanelCorner.cs
@@ -0,0 +1,214 @@
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+using Epsitec.Common.Pictogram.Data;
+
+namespace Epsitec.Common.Pictogram.Widgets
+{
+	/// <summary>
+	/// La classe PanelCorner permet de choisir un type de coin.
+	/// </summary>
+	public class PanelCorner : AbstractPanel
+	{
+		public PanelCorner()
+		{
+			this.label = new StaticText(this);
+			this.label.Alignment = Drawing.ContentAlignment.MiddleLeft;
+
+			this.fieldType = new TextFieldCombo(this);
+			this.fieldType.IsReadOnly = true;
+			for ( int i=0 ; i<100 ; i++ )
+			{
+				CornerType type = PropertyCorner.ConvType(i);
+				if ( type == CornerType.None )  break;
+				this.fieldType.Items.Add(PropertyCorner.GetName(type));
+			}
+			this.fieldType.TextChanged += new EventHandler(this.HandleTypeChanged);
+
+			this.fieldRadius = new TextFieldSlider(this);
+			this.fieldRadius.MinRange = 0;
+			this.fieldRadius.MaxRange = 10;
+			this.fieldRadius.Step = 1;
+			this.fieldRadius.TextChanged += new EventHandler(this.HandleFieldChanged);
+
+			this.fieldEffect1 = new TextFieldSlider(this);
+			this.fieldEffect1.MinRange = -100;
+			this.fieldEffect1.MaxRange = 200;
+			this.fieldEffect1.Step = 5;
+			this.fieldEffect1.TextChanged += new EventHandler(this.HandleFieldChanged);
+
+			this.fieldEffect2 = new TextFieldSlider(this);
+			this.fieldEffect2.MinRange = -100;
+			this.fieldEffect2.MaxRange = 200;
+			this.fieldEffect2.Step = 5;
+			this.fieldEffect2.TextChanged += new EventHandler(this.HandleFieldChanged);
+
+			this.labelRadius = new StaticText(this);
+			this.labelRadius.Text = "R";
+			this.labelRadius.Alignment = Drawing.ContentAlignment.MiddleCenter;
+
+			this.labelEffect1 = new StaticText(this);
+			this.labelEffect1.Text = "A";
+			this.labelEffect1.Alignment = Drawing.ContentAlignment.MiddleRight;
+
+			this.labelEffect2 = new StaticText(this);
+			this.labelEffect2.Text = "B";
+			this.labelEffect2.Alignment = Drawing.ContentAlignment.MiddleRight;
+
+			this.isNormalAndExtended = true;
+		}
+		
+		public PanelCorner(Widget embedder) : this()
+		{
+			this.SetEmbedder(embedder);
+		}
+		
+		protected override void Dispose(bool disposing)
+		{
+			if ( disposing )
+			{
+				this.fieldType.TextChanged -= new EventHandler(this.HandleTypeChanged);
+				this.fieldRadius.TextChanged -= new EventHandler(this.HandleFieldChanged);
+				this.fieldEffect1.TextChanged -= new EventHandler(this.HandleFieldChanged);
+				this.fieldEffect2.TextChanged -= new EventHandler(this.HandleFieldChanged);
+			}
+			
+			base.Dispose(disposing);
+		}
+
+		
+		// Retourne la hauteur standard.
+		public override double DefaultHeight
+		{
+			get
+			{
+				return ( this.extendedSize ? 55 : 30 );
+			}
+		}
+
+		// Propriété -> widget.
+		public override void SetProperty(AbstractProperty property)
+		{
+			base.SetProperty(property);
+			this.label.Text = this.text;
+
+			PropertyCorner p = property as PropertyCorner;
+			if ( p == null )  return;
+
+			this.fieldType.SelectedIndex = PropertyCorner.ConvType(p.CornerType);
+			this.fieldRadius.Value  = p.Radius;
+			this.fieldEffect1.Value = p.Effect1*100;
+			this.fieldEffect2.Value = p.Effect2*100;
+
+			this.EnableWidgets();
+		}
+
+		// Widget -> propriété.
+		public override AbstractProperty GetProperty()
+		{
+			PropertyCorner p = new PropertyCorner();
+			base.GetProperty(p);
+
+			p.CornerType = PropertyCorner.ConvType(this.fieldType.SelectedIndex);
+			p.Radius  = this.fieldRadius.Value;
+			p.Effect1 = this.fieldEffect1.Value/100;
+			p.Effect2 = this.fieldEffect2.Value/100;
+
+			return p;
+		}
+
+		// Grise les widgets nécessaires.
+		protected void EnableWidgets()
+		{
+			// Initialise les min/max en fonction du type choisi.
+			CornerType type = PropertyCorner.ConvType(this.fieldType.SelectedIndex);
+			bool enableRadius, enable1, enable2;
+			double effect1, min1, max1;
+			double effect2, min2, max2;
+			PropertyCorner.GetFieldsParam(type, out enableRadius, out enable1, out effect1, out min1, out max1, out enable2, out effect2, out min2, out max2);
+
+			this.fieldEffect1.MinRange = min1*100;
+			this.fieldEffect1.MaxRange = max1*100;
+			this.fieldEffect2.MinRange = min2*100;
+			this.fieldEffect2.MaxRange = max2*100;
+
+			this.fieldRadius.SetEnabled(enableRadius);
+			this.fieldEffect1.SetEnabled(enable1);
+			this.fieldEffect2.SetEnabled(enable2);
+		}
+
+		// Met à jour la géométrie.
+		protected override void UpdateClientGeometry()
+		{
+			base.UpdateClientGeometry();
+
+			if ( this.fieldType == null )  return;
+
+			Drawing.Rectangle rect = this.Client.Bounds;
+			rect.Left += this.extendedZoneWidth;
+			rect.Inflate(-5, -5);
+
+			Drawing.Rectangle r = rect;
+			r.Bottom = r.Top-20;
+			r.Right = rect.Right-100;
+			this.label.Bounds = r;
+
+			r.Left = rect.Right-100;
+			r.Right = rect.Right;
+			this.fieldType.Bounds = r;
+
+			rect.Top = r.Bottom-5;
+			rect.Bottom = rect.Top-20;
+			r = rect;
+			r.Height = 20;
+			r.Width = 14;
+			this.labelRadius.Bounds = r;
+			r.Left = r.Right;
+			r.Width = 44;
+			this.fieldRadius.Bounds = r;
+			r.Left = r.Right;
+			r.Width = 15;
+			this.labelEffect1.Bounds = r;
+			r.Left = r.Right+2;
+			r.Width = 44;
+			this.fieldEffect1.Bounds = r;
+			r.Left = r.Right;
+			r.Width = 10;
+			this.labelEffect2.Bounds = r;
+			r.Left = r.Right+2;
+			r.Width = 44;
+			this.fieldEffect2.Bounds = r;
+		}
+		
+		// Le type a été changé.
+		private void HandleTypeChanged(object sender)
+		{
+			// Met les valeurs par défaut correspondant au type choisi.
+			CornerType type = PropertyCorner.ConvType(this.fieldType.SelectedIndex);
+			bool enableRadius, enable1, enable2;
+			double effect1, min1, max1;
+			double effect2, min2, max2;
+			PropertyCorner.GetFieldsParam(type, out enableRadius, out enable1, out effect1, out min1, out max1, out enable2, out effect2, out min2, out max2);
+			this.fieldEffect1.Value = effect1*100;
+			this.fieldEffect2.Value = effect2*100;
+
+			this.EnableWidgets();
+			this.OnChanged();
+		}
+
+		// Un champ a été changé.
+		private void HandleFieldChanged(object sender)
+		{
+			this.OnChanged();
+		}
+
+
+		protected StaticText				label;
+		protected TextFieldCombo			fieldType;
+		protected TextFieldSlider			fieldRadius;
+		protected TextFieldSlider			fieldEffect1;
+		protected TextFieldSlider			fieldEffect2;
+		protected StaticText				labelRadius;
+		protected StaticText				labelEffect1;
+		protected StaticText				labelEffect2;
+	}
+}
