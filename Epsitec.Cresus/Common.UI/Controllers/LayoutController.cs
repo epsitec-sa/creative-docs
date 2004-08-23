266d47f406316ad783187850ec10d34adab46403
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:06:49 +0000

    ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1768 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.UI/Controllers/LayoutController.cs
@@ -0,0 +1,443 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : en chantier/PA
+
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+
+namespace Epsitec.Common.UI.Controllers
+{
+	/// <summary>
+	/// Summary description for LayoutController.
+	/// </summary>
+	public class LayoutController : AbstractController
+	{
+		public LayoutController()
+		{
+		}
+		
+		
+		public override void CreateUI(Widget panel)
+		{
+			this.caption_label = new StaticText (panel);
+			this.radio_manual  = new RadioButton (panel);
+			this.radio_anchor  = new RadioButton (panel);
+			this.radio_dock    = new RadioButton (panel);
+			this.edge_select   = new EdgeSelectWidget (panel, this);
+			
+			double h_pane = System.Math.Max (panel.Height, 80 + 6);
+			
+			panel.Height = h_pane;
+			
+			this.caption_label.Width         = 80;
+			this.caption_label.Anchor        = AnchorStyles.TopLeft;
+			this.caption_label.AnchorMargins = new Drawing.Margins (0, 0, 8, 0);
+			
+			this.radio_manual.Text           = "manual";
+			this.radio_manual.Width          = this.caption_label.Width;
+			this.radio_manual.Anchor         = this.caption_label.Anchor;
+			this.radio_manual.AnchorMargins  = new Drawing.Margins (0, 0, 8+20, 0);
+			this.radio_manual.Group          = "mode";
+			this.radio_manual.Index          = (int) ShowMode.Manual;
+			this.radio_manual.TabIndex       = 10;
+			this.radio_manual.ActiveStateChanged += new EventHandler (this.HandleRadioActiveStateChanged);
+			
+			this.radio_anchor.Text           = "anchor";
+			this.radio_anchor.Width          = this.caption_label.Width;
+			this.radio_anchor.Anchor         = this.caption_label.Anchor;
+			this.radio_anchor.AnchorMargins  = new Drawing.Margins (0, 0, 8+20+16, 0);
+			this.radio_anchor.Group          = "mode";
+			this.radio_anchor.Index          = (int) ShowMode.Anchor;
+			this.radio_anchor.TabIndex       = 10;
+			this.radio_anchor.ActiveStateChanged += new EventHandler (this.HandleRadioActiveStateChanged);
+			
+			this.radio_dock.Text             = "dock";
+			this.radio_dock.Width            = this.caption_label.Width;
+			this.radio_dock.Anchor           = this.caption_label.Anchor;
+			this.radio_dock.AnchorMargins    = new Drawing.Margins (0, 0, 8+20+16*2, 0);
+			this.radio_dock.Group            = "mode";
+			this.radio_dock.Index            = (int) ShowMode.Dock;
+			this.radio_dock.TabIndex         = 10;
+			this.radio_dock.ActiveStateChanged += new EventHandler (this.HandleRadioActiveStateChanged);
+			
+			this.edge_select.Anchor          = AnchorStyles.All;
+			this.edge_select.AnchorMargins   = new Drawing.Margins (80, 0, 4, 4);
+			
+			this.OnCaptionChanged ();
+			
+			this.SyncFromAdapter ();
+		}
+		
+		public override void SyncFromAdapter()
+		{
+			Adapters.LayoutAdapter adapter = this.Adapter as Adapters.LayoutAdapter;
+			
+			if (adapter != null)
+			{
+				if (this.layout != adapter.Value)
+				{
+					this.layout = adapter.Value;
+					
+					if (this.edge_select != null)
+					{
+						this.edge_select.Invalidate ();
+					}
+					
+					switch (this.layout)
+					{
+						case LayoutStyles.Manual:
+							this.SetShowMode (ShowMode.Manual);
+							break;
+						
+						case LayoutStyles.DockLeft:
+						case LayoutStyles.DockRight:
+						case LayoutStyles.DockBottom:
+						case LayoutStyles.DockTop:
+							this.SetShowMode (ShowMode.Dock);
+							break;
+						
+						default:
+							this.SetShowMode (ShowMode.Anchor);
+							break;
+					}
+				}
+			}
+		}
+		
+		public override void SyncFromUI()
+		{
+			Adapters.LayoutAdapter adapter = this.Adapter as Adapters.LayoutAdapter;
+			
+			if (adapter != null)
+			{
+				adapter.Value = this.layout;
+			}
+		}
+		
+		protected void SetShowMode(ShowMode mode)
+		{
+			if (this.show_mode != mode)
+			{
+				this.show_mode = mode;
+				
+				if ((this.edge_select != null) &&
+					(this.radio_manual != null))
+				{
+					RadioButton.Activate (this.radio_manual.Parent, this.radio_manual.Group, (int) this.show_mode);
+					this.edge_select.Invalidate ();
+				}
+			}
+		}
+		
+		
+		private void HandleRadioActiveStateChanged(object sender)
+		{
+			RadioButton radio = sender as RadioButton;
+			
+			if ((radio.ActiveState == WidgetState.ActiveYes) &&
+				(radio.Group == "mode"))
+			{
+				this.SetShowMode ((ShowMode) radio.Index);
+			}
+		}
+		
+		protected enum ShowMode
+		{
+			Manual, Anchor, Dock
+		}
+		
+		[SuppressBundleSupport]
+		protected class EdgeSelectWidget : Widget
+		{
+			public EdgeSelectWidget(Widget embedder, LayoutController host)
+			{
+				this.SetEmbedder (embedder);
+				this.host = host;
+			}
+			
+			
+			public override Drawing.Rectangle GetShapeBounds()
+			{
+				Drawing.Rectangle bounds = base.GetShapeBounds ();
+				bounds.Inflate (2, 2, 2, 2);
+				return bounds;
+			}
+
+			protected override void ProcessMessage(Message message, Epsitec.Common.Drawing.Point pos)
+			{
+				if (message.IsMouseType)
+				{
+					Drawing.Rectangle hilite = this.hilite;
+					
+					if (this.host.show_mode == ShowMode.Dock)
+					{
+						double dx = this.Client.Width;
+						double dy = this.Client.Height;
+						double dd = 25;
+						
+						switch (message.Type)
+						{
+							case MessageType.MouseLeave:
+								hilite = Drawing.Rectangle.Empty;
+								break;
+							
+							case MessageType.MouseMove:
+								if (! this.Client.Bounds.Contains (pos))
+								{
+									hilite = Drawing.Rectangle.Empty;
+								}
+								else if (pos.X < dd)
+								{
+									if (pos.Y < pos.X)
+									{
+										hilite = new Drawing.Rectangle (0, 0, dx, dd);
+										this.candidate = LayoutStyles.DockBottom;
+									}
+									else if ((dy - pos.Y) < pos.X)
+									{
+										hilite = new Drawing.Rectangle (0, dy-dd, dx, dd);
+										this.candidate = LayoutStyles.DockTop;
+									}
+									else
+									{
+										hilite = new Drawing.Rectangle (0, 0, dd, dy);
+										this.candidate = LayoutStyles.DockLeft;
+									}
+								}
+								else if ((dx - pos.X) < dd)
+								{
+									if ((dy - pos.Y) < (dx - pos.X))
+									{
+										hilite = new Drawing.Rectangle (0, dy-dd, dx, dd);
+										this.candidate = LayoutStyles.DockTop;
+									}
+									else if (pos.Y < (dx - pos.X))
+									{
+										hilite = new Drawing.Rectangle (0, 0, dx, dd);
+										this.candidate = LayoutStyles.DockBottom;
+									}
+									else
+									{
+										hilite = new Drawing.Rectangle (dx-dd, 0, dd, dy);
+										this.candidate = LayoutStyles.DockRight;
+									}
+								}
+								else if (pos.Y < dd)
+								{
+									hilite = new Drawing.Rectangle (0, 0, dx, dd);
+									this.candidate = LayoutStyles.DockBottom;
+								}
+								else if ((dy - pos.Y) < dd)
+								{
+									hilite = new Drawing.Rectangle (0, dy-dd, dx, dd);
+									this.candidate = LayoutStyles.DockTop;
+								}
+								else
+								{
+									hilite = new Drawing.Rectangle (0, 0, dx, dy);
+									this.candidate = LayoutStyles.DockFill;
+								}
+								break;
+						}
+					}
+					else
+					{
+						this.hilite = Drawing.Rectangle.Empty;
+					}
+					
+					if ((message.Type == MessageType.MouseDown) &&
+						(message.IsLeftButton) &&
+						(this.hilite.IsValid))
+					{
+						if (this.host.show_mode == ShowMode.Dock)
+						{
+							if (this.host.layout == this.candidate)
+							{
+								this.host.layout = LayoutStyles.Manual;
+								this.host.SyncFromUI ();
+								this.Invalidate ();
+							}
+							else
+							{
+								this.host.layout = this.candidate;
+								this.host.SyncFromUI ();
+								this.Invalidate ();
+							}
+						}
+						else
+						{
+						}
+					}
+					
+					if (this.hilite != hilite)
+					{
+						this.hilite = hilite;
+						this.Invalidate ();
+					}
+					
+					message.Consumer = this;
+					return;
+				}
+				
+				base.ProcessMessage (message, pos);
+			}
+
+			
+			protected override void PaintBackgroundImplementation(Drawing.Graphics graphics, Drawing.Rectangle clip_rect)
+			{
+				IAdorner adorner = Epsitec.Common.Widgets.Adorner.Factory.Active;
+				
+				Drawing.Rectangle rect_1 = this.Client.Bounds;
+				Drawing.Rectangle rect_2 = this.Client.Bounds;
+				
+				rect_1.Deflate ( 0.5,  0.5,  0.5,  0.5);
+				rect_2.Deflate (25.5, 25.5, 20.5, 20.5);
+				
+				double cx = System.Math.Floor (this.Client.Width / 2) + 0.5;
+				double cy = System.Math.Floor (this.Client.Height / 2) + 0.5;
+				
+				Drawing.Path surface = new Drawing.Path ();
+				Drawing.Path outline = new Drawing.Path ();
+				Drawing.Path picture = new Drawing.Path ();
+				
+				surface.AppendRectangle (rect_1);
+				
+				graphics.Rasterizer.AddSurface (surface);
+				graphics.RenderSolid (Drawing.Color.FromARGB (0.8, 1, 1, 1));
+				
+				surface.Clear ();
+				
+				outline.AppendRectangle (rect_1);
+				
+				switch (this.host.show_mode)
+				{
+					case ShowMode.Manual:
+						break;
+					
+					case ShowMode.Anchor:
+						outline.AppendRectangle (rect_2);
+						break;
+					
+					case ShowMode.Dock:
+						this.AppendArrow (rect_1.Left  + 1, cy, 16, picture, Direction.Left);
+						this.AppendArrow (rect_1.Right - 1, cy, 16, picture, Direction.Right);
+						this.AppendArrow (cx, rect_1.Top    - 1, 16, picture, Direction.Up);
+						this.AppendArrow (cx, rect_1.Bottom + 1, 16, picture, Direction.Down);
+						
+						switch (this.host.layout)
+						{
+							case LayoutStyles.DockLeft:		this.AppendArrow (rect_1.Left  + 1, cy, 16, surface, Direction.Left);	break;
+							case LayoutStyles.DockRight:	this.AppendArrow (rect_1.Right - 1, cy, 16, surface, Direction.Right);	break;
+							case LayoutStyles.DockTop:		this.AppendArrow (cx, rect_1.Top    - 1, 16, surface, Direction.Up);	break;
+							case LayoutStyles.DockBottom:	this.AppendArrow (cx, rect_1.Bottom + 1, 16, surface, Direction.Down);	break;
+							
+							case LayoutStyles.DockFill:
+								this.AppendArrow (rect_1.Left  + 1, cy, 16, surface, Direction.Left);
+								this.AppendArrow (rect_1.Right - 1, cy, 16, surface, Direction.Right);
+								this.AppendArrow (cx, rect_1.Top    - 1, 16, surface, Direction.Up);
+								this.AppendArrow (cx, rect_1.Bottom + 1, 16, surface, Direction.Down);
+								break;
+						}
+						
+						break;
+				}
+				
+				if (surface.IsValid)
+				{
+					graphics.Rasterizer.AddSurface (surface);
+					graphics.RenderSolid (Drawing.Color.FromRGB (0, 0, 0.8));
+				}
+				
+				if ((this.hilite.IsValid) &&
+					(picture.IsValid))
+				{
+					Drawing.Rectangle clip = graphics.SaveClippingRectangle ();
+					graphics.SetClippingRectangle (this.MapClientToRoot (this.hilite));
+					graphics.Rasterizer.AddSurface (picture);
+					graphics.RenderSolid (Drawing.Color.FromARGB (0.5, 0, 0, 1));
+					graphics.RestoreClippingRectangle (clip);
+				}
+				
+				outline.Append (picture);
+				
+				if (outline.IsValid)
+				{
+					graphics.Rasterizer.AddOutline (outline, 1.0, Drawing.CapStyle.Square, Drawing.JoinStyle.Miter);
+					graphics.RenderSolid (adorner.ColorBorder);
+				}
+				
+				surface.Dispose ();
+				outline.Dispose ();
+				picture.Dispose ();
+			}
+			
+			protected void AppendArrow(double x, double y, double s, Drawing.Path path, Direction direction)
+			{
+				switch (direction)
+				{
+					case Direction.Up:
+						path.MoveTo (x, y);
+						path.LineTo (x-0.50*s, y-0.50*s);
+						path.LineTo (x-0.25*s, y-0.50*s);
+						path.LineTo (x-0.25*s, y-1.00*s);
+						path.LineTo (x+0.25*s, y-1.00*s);
+						path.LineTo (x+0.25*s, y-0.50*s);
+						path.LineTo (x+0.50*s, y-0.50*s);
+						path.LineTo (x, y);
+						path.Close ();
+						break;
+					
+					case Direction.Down:
+						path.MoveTo (x, y);
+						path.LineTo (x-0.50*s, y+0.50*s);
+						path.LineTo (x-0.25*s, y+0.50*s);
+						path.LineTo (x-0.25*s, y+1.00*s);
+						path.LineTo (x+0.25*s, y+1.00*s);
+						path.LineTo (x+0.25*s, y+0.50*s);
+						path.LineTo (x+0.50*s, y+0.50*s);
+						path.LineTo (x, y);
+						path.Close ();
+						break;
+					
+					case Direction.Left:
+						path.MoveTo (x, y);
+						path.LineTo (x+0.50*s, y-0.50*s);
+						path.LineTo (x+0.50*s, y-0.25*s);
+						path.LineTo (x+1.00*s, y-0.25*s);
+						path.LineTo (x+1.00*s, y+0.25*s);
+						path.LineTo (x+0.50*s, y+0.25*s);
+						path.LineTo (x+0.50*s, y+0.50*s);
+						path.LineTo (x, y);
+						path.Close ();
+						break;
+					
+					case Direction.Right:
+						path.MoveTo (x, y);
+						path.LineTo (x-0.50*s, y-0.50*s);
+						path.LineTo (x-0.50*s, y-0.25*s);
+						path.LineTo (x-1.00*s, y-0.25*s);
+						path.LineTo (x-1.00*s, y+0.25*s);
+						path.LineTo (x-0.50*s, y+0.25*s);
+						path.LineTo (x-0.50*s, y+0.50*s);
+						path.LineTo (x, y);
+						path.Close ();
+						break;
+				}
+			}
+			
+			
+			private Drawing.Rectangle			hilite;
+			private LayoutController			host;
+			private LayoutStyles				candidate;
+		}
+		
+		
+		private RadioButton						radio_manual;
+		private RadioButton						radio_anchor;
+		private RadioButton						radio_dock;
+		
+		private EdgeSelectWidget				edge_select;
+		
+		private LayoutStyles					layout;
+		private ShowMode						show_mode;
+	}
+}
