86baef9217835c76ae54a6ea9ac8e52e033d3a69
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:52:53 +0000

    ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1609 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.UI/Adapters/Num2Adapter.cs
@@ -0,0 +1,56 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : en chantier/PA
+
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+
+namespace Epsitec.Common.UI.Adapters
+{
+	/// <summary>
+	/// Summary description for Num2Adapter.
+	/// </summary>
+	
+	[Controller (1, typeof (Controllers.Num2Controller))]
+	
+	public class Num2Adapter : AbstractAdapter
+	{
+		public Num2Adapter()
+		{
+		}
+		
+		
+		public string							Value
+		{
+			get
+			{
+				return this.value;
+			}
+			set
+			{
+				if (this.value != value)
+				{
+					this.value = value;
+					this.OnValueChanged ();
+				}
+			}
+		}
+		
+		
+		protected override object ConvertToObject()
+		{
+			System.ComponentModel.TypeConverter converter = System.ComponentModel.TypeDescriptor.GetConverter (this.binder.GetDataType ());
+			return converter.ConvertFromString (this.Value);
+		}
+		
+		protected override bool ConvertFromObject(object data)
+		{
+			System.ComponentModel.TypeConverter converter = System.ComponentModel.TypeDescriptor.GetConverter (this.binder.GetDataType ());
+			this.Value = converter.ConvertToString (data);
+			return true;
+		}
+		
+		
+		
+		private string							value;
+	}
+}
