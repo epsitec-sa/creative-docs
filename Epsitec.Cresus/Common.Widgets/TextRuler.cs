fb744611ec84f77d8f4d2587a9fb831056e82ddf
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:26:16 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2485 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/TextRuler.cs
@@ -0,0 +1,405 @@
+namespace Epsitec.Common.Widgets
+{
+	/// <summary>
+	/// La class TextRuler représente une règle pour un pavé de texte.
+	/// </summary>
+	public class TextRuler : Widget
+	{
+		public TextRuler()
+		{
+			this.AutoFocus = false;
+
+			this.fontName = "Toto";
+			this.fontSize = 10;
+
+			this.fieldFontName = new TextFieldCombo(this);
+			this.fieldFontName.AutoFocus = false;
+			this.fieldFontName.IsReadOnly = true;
+			this.fieldFontName.Items.Add("Tahoma");
+			this.fieldFontName.Items.Add("Arial");
+			this.fieldFontName.Items.Add("Courier New");
+			this.fieldFontName.Items.Add("Times New Roman");
+			this.fieldFontName.SelectedIndexChanged += new Support.EventHandler(this.HandleFieldFontNameChanged);
+
+			this.fieldFontSize = new TextFieldUpDown(this);
+			this.fieldFontSize.AutoFocus = false;
+			this.fieldFontSize.MinValue =  0.1M;
+			this.fieldFontSize.MaxValue = 20.0M;
+			this.fieldFontSize.Step = 0.1M;
+			this.fieldFontSize.Resolution = 0.01M;
+			this.fieldFontSize.TextChanged += new Support.EventHandler(this.HandleFieldFontSizeChanged);
+
+			this.buttonBold = new IconButton(this);
+			this.buttonBold.AutoFocus = false;
+			this.buttonBold.Text = "<b>G</b>";
+			this.buttonBold.Clicked += new MessageEventHandler(this.HandleButtonBoldClicked);
+
+			this.buttonItalic = new IconButton(this);
+			this.buttonItalic.AutoFocus = false;
+			this.buttonItalic.Text = "<i>I</i>";
+			this.buttonItalic.Clicked += new MessageEventHandler(this.HandleButtonItalicClicked);
+
+			this.buttonUnderlined = new IconButton(this);
+			this.buttonUnderlined.AutoFocus = false;
+			this.buttonUnderlined.Text = "<u>S</u>";
+			this.buttonUnderlined.Clicked += new MessageEventHandler(this.HandleButtonUnderlinedClicked);
+		}
+		
+		public TextRuler(Widget embedder) : this()
+		{
+			this.SetEmbedder(embedder);
+		}
+		
+		
+		public double							MinimalWidth
+		{
+			// Retourne la largeur minimale d'une règle.
+			get
+			{
+				return TextRuler.buttonMargin*2 +
+					   TextRuler.buttonFontNameMinWidth +
+					   TextRuler.buttonFontSizeWidth +
+					   TextRuler.buttonWidth*3 + 5;
+			}
+		}
+
+		public override double					DefaultWidth
+		{
+			// Retourne la largeur standard d'une règle.
+			get
+			{
+				return 200;
+			}
+		}
+
+		public override double					DefaultHeight
+		{
+			// Retourne la hauteur standard d'une règle.
+			get
+			{
+				return TextRuler.zoneSupHeight + TextRuler.zoneInfHeight;
+			}
+		}
+
+		public string							FontName
+		{
+			get
+			{
+				return this.fontName;
+			}
+
+			set
+			{
+				if ( this.fontName != value )
+				{
+					this.fontName = value;
+					this.UpdateButtons();
+
+					if ( this.textLayout != null && this.textNavigator != null )
+					{
+						this.textLayout.SetSelectionFontName(this.textNavigator.Context, this.fontName);
+					}
+				}
+			}
+		}
+
+		public double							FontSize
+		{
+			get
+			{
+				return this.fontSize;
+			}
+
+			set
+			{
+				if ( this.fontSize != value )
+				{
+					this.fontSize = value;
+					this.UpdateButtons();
+
+					if ( this.textLayout != null && this.textNavigator != null )
+					{
+						this.textLayout.SetSelectionFontSize(this.textNavigator.Context, this.fontSize);
+					}
+				}
+			}
+		}
+
+		public bool								Bold
+		{
+			// Attribut typographique "gras".
+			get
+			{
+				return this.bold;
+			}
+
+			set
+			{
+				if ( this.bold != value )
+				{
+					this.bold = value;
+					this.UpdateButtons();
+
+					if ( this.textLayout != null && this.textNavigator != null )
+					{
+						this.textLayout.SetSelectionBold(this.textNavigator.Context, this.bold);
+					}
+				}
+			}
+		}
+
+		public bool								Italic
+		{
+			// Attribut typographique "italique".
+			get
+			{
+				return this.italic;
+			}
+
+			set
+			{
+				if ( this.italic != value )
+				{
+					this.italic = value;
+					this.UpdateButtons();
+
+					if ( this.textLayout != null && this.textNavigator != null )
+					{
+						this.textLayout.SetSelectionItalic(this.textNavigator.Context, this.italic);
+					}
+				}
+			}
+		}
+
+		public bool								Underlined
+		{
+			// Attribut typographique "souligné".
+			get
+			{
+				return this.underlined;
+			}
+
+			set
+			{
+				if ( this.underlined != value )
+				{
+					this.underlined = value;
+					this.UpdateButtons();
+
+					if ( this.textLayout != null && this.textNavigator != null )
+					{
+						this.textLayout.SetSelectionUnderlined(this.textNavigator.Context, this.underlined);
+					}
+				}
+			}
+		}
+
+
+		public void AttachToText(TextLayout textLayout, TextNavigator textNavigator)
+		{
+			// Lie la règle à un texte éditable.
+			if ( this.textLayout    == textLayout    &&
+				this.textNavigator == textNavigator )  return;
+
+			if ( this.textNavigator != null )
+			{
+				this.textNavigator.CursorChanged -= new Epsitec.Common.Support.EventHandler(this.HandleCursorChanged);
+			}
+
+			this.textLayout    = textLayout;
+			this.textNavigator = textNavigator;
+
+			if ( this.textNavigator != null )
+			{
+				this.textNavigator.CursorChanged += new Epsitec.Common.Support.EventHandler(this.HandleCursorChanged);
+			}
+		}
+		
+		public void DetachFromText()
+		{
+			this.AttachToText(null, null);
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if ( disposing )
+			{
+				this.DetachFromText();
+				this.fieldFontName.SelectedIndexChanged -= new Support.EventHandler(this.HandleFieldFontNameChanged);
+				this.fieldFontSize.TextChanged -= new Support.EventHandler(this.HandleFieldFontSizeChanged);
+				this.buttonBold.Clicked -= new MessageEventHandler(this.HandleButtonBoldClicked);
+				this.buttonItalic.Clicked -= new MessageEventHandler(this.HandleButtonItalicClicked);
+				this.buttonUnderlined.Clicked -= new MessageEventHandler(this.HandleButtonUnderlinedClicked);
+			}
+			
+			base.Dispose(disposing);
+		}
+		
+		
+		private void HandleFieldFontNameChanged(object sender)
+		{
+			this.FontName = this.ComboSelectedName(this.fieldFontName);
+			this.OnChanged();
+		}
+
+		private void HandleFieldFontSizeChanged(object sender)
+		{
+			this.FontSize = (double) this.fieldFontSize.Value;
+			this.OnChanged();
+		}
+
+		private void HandleButtonBoldClicked(object sender, MessageEventArgs e)
+		{
+			this.Bold = !this.Bold;
+			this.OnChanged();
+		}
+
+		private void HandleButtonItalicClicked(object sender, MessageEventArgs e)
+		{
+			this.Italic = !this.Italic;
+			this.OnChanged();
+		}
+
+		private void HandleButtonUnderlinedClicked(object sender, MessageEventArgs e)
+		{
+			this.Underlined = !this.Underlined;
+			this.OnChanged();
+		}
+
+		private void HandleCursorChanged(object sender)
+		{
+			this.fontName   = this.textLayout.GetSelectionFontName(this.textNavigator.Context);
+			this.fontSize   = this.textLayout.GetSelectionFontSize(this.textNavigator.Context);
+			this.bold       = this.textLayout.IsSelectionBold(this.textNavigator.Context);
+			this.italic     = this.textLayout.IsSelectionItalic(this.textNavigator.Context);
+			this.underlined = this.textLayout.IsSelectionUnderlined(this.textNavigator.Context);
+			this.UpdateButtons();
+		}
+
+
+		protected override void UpdateClientGeometry()
+		{
+			base.UpdateClientGeometry();
+			this.UpdateGeometry();
+		}
+
+		protected void UpdateGeometry()
+		{
+			if ( this.buttonBold == null )  return;
+
+			Drawing.Rectangle rect = this.Client.Bounds;
+			rect.Bottom += TextRuler.zoneInfHeight;
+			rect.Deflate(TextRuler.buttonMargin);
+
+			double widthName = rect.Width-(TextRuler.buttonFontSizeWidth+TextRuler.buttonWidth*3+5);
+			widthName = System.Math.Max(widthName, TextRuler.buttonFontNameMinWidth);
+			widthName = System.Math.Min(widthName, TextRuler.buttonFontNameMaxWidth);
+			rect.Width = widthName;
+			this.fieldFontName.Bounds = rect;
+
+			rect.Offset(rect.Width+3, 0);
+			rect.Width = TextRuler.buttonFontSizeWidth;
+			this.fieldFontSize.Bounds = rect;
+
+			rect.Offset(rect.Width+3, 0);
+			rect.Width = TextRuler.buttonWidth;
+			this.buttonBold.Bounds = rect;
+
+			rect.Offset(rect.Width, 0);
+			this.buttonItalic.Bounds = rect;
+
+			rect.Offset(rect.Width, 0);
+			this.buttonUnderlined.Bounds = rect;
+		}
+
+		protected void UpdateButtons()
+		{
+			this.ComboSelectedName(this.fieldFontName, this.fontName);
+			this.fieldFontSize.Value = (decimal) this.fontSize;
+			this.ButtonActive(this.buttonBold,       this.bold      );
+			this.ButtonActive(this.buttonItalic,     this.italic    );
+			this.ButtonActive(this.buttonUnderlined, this.underlined);
+		}
+
+		protected void ButtonActive(Button button, bool active)
+		{
+			button.ActiveState = active ? WidgetState.ActiveYes : WidgetState.ActiveNo;
+		}
+
+		
+		protected override void PaintBackgroundImplementation(Drawing.Graphics graphics, Drawing.Rectangle clipRect)
+		{
+			// Dessine la règle.
+			IAdorner adorner = Widgets.Adorner.Factory.Active;
+
+			Drawing.Rectangle rect = this.Client.Bounds;
+			graphics.AddFilledRectangle(rect);
+			graphics.RenderSolid(adorner.ColorWindow);
+
+			rect.Deflate(0.5);
+			graphics.AddRectangle(rect);
+			graphics.AddLine(rect.Left, rect.Bottom+TextRuler.zoneInfHeight, rect.Right, rect.Bottom+TextRuler.zoneInfHeight);
+			graphics.RenderSolid(adorner.ColorTextFieldBorder(this.IsEnabled));
+		}
+
+
+		// TODO: TextFieldCombo.SelectedName ne marche pas !!!
+		protected void ComboSelectedName(TextFieldCombo combo, string name)
+		{
+			int total = combo.Items.Count;
+			for ( int i=0 ; i<total ; i++ )
+			{
+				string s = combo.Items[i];
+				if ( s == name )
+				{
+					combo.SelectedIndex = i;
+					return;
+				}
+			}
+		}
+
+		// TODO: TextFieldCombo.SelectedName ne marche pas !!!
+		protected string ComboSelectedName(TextFieldCombo combo)
+		{
+			if ( combo.SelectedIndex == -1 )  return "";
+			return combo.Items[combo.SelectedIndex] as string;
+		}
+
+
+		// Génère un événement pour dire que la règle a changé.
+		protected void OnChanged()
+		{
+			if ( this.Changed != null )  // qq'un écoute ?
+			{
+				this.Changed(this);
+			}
+		}
+
+		
+		
+		public event Support.EventHandler	Changed;
+
+		
+		protected TextLayout				textLayout;
+		protected TextNavigator				textNavigator;
+		protected string					fontName = "";
+		protected double					fontSize = 10;
+		protected bool						bold = false;
+		protected bool						italic = false;
+		protected bool						underlined = false;
+
+		protected TextFieldCombo			fieldFontName;
+		protected TextFieldUpDown			fieldFontSize;
+		protected IconButton				buttonBold;
+		protected IconButton				buttonItalic;
+		protected IconButton				buttonUnderlined;
+
+		protected static readonly double	buttonMargin = 3;
+		protected static readonly double	buttonWidth = 20;
+		protected static readonly double	buttonFontNameMinWidth = 60;
+		protected static readonly double	buttonFontNameMaxWidth = 150;
+		protected static readonly double	buttonFontSizeWidth = 45;
+		protected static readonly double	zoneSupHeight = TextRuler.buttonMargin*2+TextRuler.buttonWidth;
+		protected static readonly double	zoneInfHeight = 15;
+	}
+}
