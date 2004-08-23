2f8aa1cdc4219a129827ae7be3310c2fda5d4101
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:40:20 +0000

    ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1525 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Pictogram/Data/ObjectImage.cs
@@ -0,0 +1,383 @@
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+using System.Xml.Serialization;
+
+namespace Epsitec.Common.Pictogram.Data
+{
+	/// <summary>
+	/// La classe ObjectImage est la classe de l'objet graphique "image bitmap".
+	/// </summary>
+	public class ObjectImage : AbstractObject
+	{
+		public ObjectImage()
+		{
+			PropertyImage image = new PropertyImage();
+			image.Type = PropertyType.Image;
+			this.AddProperty(image);
+		}
+
+		protected override AbstractObject CreateNewObject()
+		{
+			return new ObjectImage();
+		}
+
+		public override void Dispose()
+		{
+			base.Dispose();
+		}
+
+
+		// Nom de l'icône.
+		public override string IconName
+		{
+			get { return @"file:images/image.icon"; }
+		}
+
+
+		// Détecte si la souris est sur l'objet.
+		public override bool Detect(Drawing.Point pos)
+		{
+			if ( this.isHide )  return false;
+
+			Drawing.Rectangle bbox = this.BoundingBox;
+			if ( !bbox.Contains(pos) )  return false;
+
+			return AbstractObject.DetectSurface(this.PathBuildSurface(), pos);
+		}
+
+
+		// Déplace une poignée.
+		public override void MoveHandleProcess(int rank, Drawing.Point pos, IconContext iconContext)
+		{
+			if ( rank >= this.handles.Count )  // poignée d'une propriété ?
+			{
+				base.MoveHandleProcess(rank, pos, iconContext);
+				return;
+			}
+
+			iconContext.ConstrainSnapPos(ref pos);
+			iconContext.SnapGrid(ref pos);
+
+			     if ( rank == 0 )  this.MoveCorner(pos, 0, 2,3, 1);
+			else if ( rank == 1 )  this.MoveCorner(pos, 1, 3,2, 0);
+			else if ( rank == 2 )  this.MoveCorner(pos, 2, 0,1, 3);
+			else if ( rank == 3 )  this.MoveCorner(pos, 3, 1,0, 2);
+			else                   this.Handle(rank).Position = pos;
+
+			this.dirtyBbox = true;
+		}
+
+
+		// Début de la création d'un objet.
+		public override void CreateMouseDown(Drawing.Point pos, IconContext iconContext)
+		{
+			iconContext.ConstrainFixStarting(pos, ConstrainType.Square);
+			this.HandleAdd(pos, HandleType.Primary);  // rang = 0
+			this.HandleAdd(pos, HandleType.Primary);  // rang = 1
+		}
+
+		// Déplacement pendant la création d'un objet.
+		public override void CreateMouseMove(Drawing.Point pos, IconContext iconContext)
+		{
+			iconContext.ConstrainSnapPos(ref pos);
+			iconContext.SnapGrid(ref pos);
+			this.Handle(1).Position = pos;
+			this.dirtyBbox = true;
+		}
+
+		// Fin de la création d'un objet.
+		public override void CreateMouseUp(Drawing.Point pos, IconContext iconContext)
+		{
+			iconContext.ConstrainSnapPos(ref pos);
+			iconContext.SnapGrid(ref pos);
+			this.Handle(1).Position = pos;
+			iconContext.ConstrainDelStarting();
+
+			// Crée les 2 autres poignées dans les coins opposés.
+			Drawing.Rectangle rect = Drawing.Rectangle.FromCorners(this.Handle(0).Position, this.Handle(1).Position);
+			Drawing.Point p1 = rect.BottomLeft;
+			Drawing.Point p2 = rect.TopRight;
+			this.Handle(0).Position = p1;
+			this.Handle(1).Position = p2;
+			this.HandleAdd(new Drawing.Point(p1.X, p2.Y), HandleType.Primary);  // rang = 2
+			this.HandleAdd(new Drawing.Point(p2.X, p1.Y), HandleType.Primary);  // rang = 3
+		}
+
+		// Indique si l'objet doit exister. Retourne false si l'objet ne peut
+		// pas exister et doit être détruit.
+		public override bool CreateIsExist(IconContext iconContext)
+		{
+			double len = Drawing.Point.Distance(this.Handle(0).Position, this.Handle(1).Position);
+			return ( len > this.minimalSize );
+		}
+
+		// Indique s'il faut sélectionner l'objet après sa création.
+		public override bool SelectAfterCreation()
+		{
+			return true;
+		}
+
+
+		// Met à jour le rectangle englobant l'objet.
+		protected override void UpdateBoundingBox()
+		{
+			Drawing.Path path = this.PathBuildSurface();
+			this.bboxThin = path.ComputeBounds();
+
+			path = this.PathBuildImage();
+			this.bboxThin.MergeWith(path.ComputeBounds());
+
+			this.bboxGeom = this.bboxThin;
+			this.bboxFull = this.bboxThin;
+		}
+
+		// Crée le chemin de l'objet pour dessiner la surface exacte de l'image.
+		protected Drawing.Path PathBuildImage()
+		{
+			Drawing.Point center;
+			double width, height, angle;
+			this.ImageGeometry(out center, out width, out height, out angle);
+
+			Drawing.Point p = new Drawing.Point();
+
+			p.X = center.X-width/2;
+			p.Y = center.Y-height/2;
+			Drawing.Point p1 = Drawing.Transform.RotatePoint(center, angle, p);
+
+			p.X = center.X+width/2;
+			p.Y = center.Y-height/2;
+			Drawing.Point p2 = Drawing.Transform.RotatePoint(center, angle, p);
+
+			p.X = center.X+width/2;
+			p.Y = center.Y+height/2;
+			Drawing.Point p3 = Drawing.Transform.RotatePoint(center, angle, p);
+
+			p.X = center.X-width/2;
+			p.Y = center.Y+height/2;
+			Drawing.Point p4 = Drawing.Transform.RotatePoint(center, angle, p);
+
+			Drawing.Path path = new Drawing.Path();
+			path.MoveTo(p1);
+			path.LineTo(p2);
+			path.LineTo(p3);
+			path.LineTo(p4);
+			path.Close();
+			return path;
+		}
+
+		// Crée le chemin de l'objet pour dessiner sa surface.
+		protected Drawing.Path PathBuildSurface()
+		{
+			Drawing.Point pbl, pbr, ptl, ptr;
+			this.Corners(out pbl, out pbr, out ptl, out ptr);
+
+			Drawing.Path path = new Drawing.Path();
+			path.MoveTo(pbl);
+			path.LineTo(pbr);
+			path.LineTo(ptr);
+			path.LineTo(ptl);
+			path.Close();
+			return path;
+		}
+
+		// Crée le chemin de l'objet pour dessiner son pourtour.
+		protected Drawing.Path PathBuildOutline()
+		{
+			Drawing.Point pbl, pbr, ptl, ptr;
+			this.Corners(out pbl, out pbr, out ptl, out ptr);
+
+			Drawing.Path path = new Drawing.Path();
+			path.MoveTo(pbl);
+			path.LineTo(pbr);
+			path.LineTo(ptr);
+			path.LineTo(ptl);  // rectangle
+			path.Close();
+
+			path.MoveTo(pbl);
+			path.LineTo(ptr);  // / 
+
+			path.MoveTo(pbr);
+			path.LineTo(ptl);  // \ 
+			return path;
+		}
+
+		// Donne les 4 coins du rectangle.
+		protected void Corners(out Drawing.Point pbl, out Drawing.Point pbr, out Drawing.Point ptl, out Drawing.Point ptr)
+		{
+			pbl = this.Handle(0).Position;
+			ptr = this.Handle(1).Position;
+
+			if ( this.handles.Count < 4 )
+			{
+				ptl = new Drawing.Point(pbl.X, ptr.Y);
+				pbr = new Drawing.Point(ptr.X, pbl.Y);
+			}
+			else
+			{
+				ptl = this.Handle(2).Position;
+				pbr = this.Handle(3).Position;
+			}
+		}
+
+		// Calcule le centre, les dimensions et l'angle de l'image en fonction
+		// du quadrilatère de l'objet, qui n'est pas forcément rectangulaire.
+		protected void ImageGeometry(out Drawing.Point center, out double width, out double height, out double angle)
+		{
+			Drawing.Point pbl, pbr, ptl, ptr;
+			this.Corners(out pbl, out pbr, out ptl, out ptr);
+
+			center = (pbl+pbr+ptl+ptr)/4;
+			width  = System.Math.Min(Drawing.Point.Distance(pbl,pbr), Drawing.Point.Distance(ptl,ptr));
+			height = System.Math.Min(Drawing.Point.Distance(pbl,ptl), Drawing.Point.Distance(pbr,ptr));
+			angle  = Drawing.Point.ComputeAngle(pbl, pbr);
+		}
+
+		// Ouvre le bitmap de l'image si nécessaire.
+		protected void OpenBitmapOriginal()
+		{
+			PropertyImage image = this.PropertyImage(0);
+			if ( image.Filename == "" )
+			{
+				this.imageOriginal = null;
+				this.imageDimmed = null;
+				this.filename = "";
+			}
+			else
+			{
+				if ( image.Filename != this.filename )
+				{
+					this.filename = image.Filename;
+					try
+					{
+						this.imageOriginal = Drawing.Bitmap.FromFile(this.filename);
+					}
+					catch
+					{
+						this.imageOriginal = null;
+					}
+					this.imageDimmed = null;
+				}
+			}
+		}
+
+		// Ouvre le bitmap de la variante estompée de l'image si nécessaire.
+		protected void OpenBitmapDimmed(IconContext iconContext)
+		{
+			if ( this.imageOriginal == null )  return;
+			if ( this.imageDimmed != null )  return;
+			if ( !iconContext.IsDimmed )  return;
+
+			this.imageDimmed = Drawing.Bitmap.CopyImage(this.imageOriginal);
+			Drawing.Pixmap.RawData data = new Drawing.Pixmap.RawData(this.imageDimmed);
+			using ( data )
+			{
+				for ( int y=0 ; y<data.Height ; y++ )
+				{
+					for ( int x=0 ; x<data.Width ; x++ )
+					{
+						Drawing.Color pixel = data[x,y];
+						data[x,y] = iconContext.AdaptColor(pixel);
+					}
+				}
+			}
+		}
+
+		// Dessine l'objet.
+		public override void DrawGeometry(Drawing.Graphics graphics, IconContext iconContext)
+		{
+			if ( base.IsFullHide(iconContext) )  return;
+			base.DrawGeometry(graphics, iconContext);
+
+			if ( this.TotalHandle < 2 )  return;
+
+			Drawing.Path path;
+
+			if ( this.TotalHandle == 2 )  // construction ?
+			{
+				path = this.PathBuildSurface();
+				graphics.Rasterizer.AddSurface(path);
+				graphics.RenderSolid(iconContext.HiliteSurfaceColor);
+
+				path = this.PathBuildOutline();
+				graphics.Rasterizer.AddOutline(path, 1.0/iconContext.ScaleX);
+				graphics.RenderSolid(iconContext.HiliteOutlineColor);
+
+				return;
+			}
+
+			this.OpenBitmapOriginal();
+			this.OpenBitmapDimmed(iconContext);
+
+			Drawing.Image image = iconContext.IsDimmed ? this.imageDimmed : this.imageOriginal;
+			if ( image == null )
+			{
+				path = this.PathBuildOutline();
+				graphics.Rasterizer.AddOutline(path, 1.0/iconContext.ScaleX);
+				graphics.RenderSolid(Drawing.Color.FromBrightness(0.5));
+			}
+			else
+			{
+				Drawing.Transform ot = graphics.SaveTransform();
+
+				Drawing.Point center;
+				double width, height, angle;
+				this.ImageGeometry(out center, out width, out height, out angle);
+
+				if ( width > 0 && height > 0 )
+				{
+					PropertyImage property = this.PropertyImage(0);
+
+					if ( property.Homo )  // conserve les proportions ?
+					{
+						double rapport = image.Height/image.Width;
+						if ( rapport < height/width )  height = width*rapport;
+						else                           width  = height/rapport;
+					}
+
+					graphics.TranslateTransform(center.X, center.Y);
+
+					angle *= 180.0/System.Math.PI;  // radians -> degrés
+					graphics.RotateTransform(angle, 0, 0);
+
+					double mirrorx = property.MirrorH ? -1 : 1;
+					double mirrory = property.MirrorV ? -1 : 1;
+					graphics.ScaleTransform(mirrorx, mirrory, 0, 0);
+
+					Drawing.Rectangle rect = new Drawing.Rectangle(-width/2, -height/2, width, height);
+					graphics.PaintImage(image, rect);
+				}
+
+				graphics.Transform = ot;
+			}
+
+			if ( this.selected )
+			{
+				path = this.PathBuildOutline();
+				graphics.Rasterizer.AddOutline(path, 1.0/iconContext.ScaleX);
+				graphics.RenderSolid(Drawing.Color.FromBrightness(0.5));
+
+				path = new Drawing.Path();
+				path.MoveTo(this.Handle(0).Position);
+				path.LineTo(this.Handle(3).Position);
+				graphics.Rasterizer.AddOutline(path, 3.0/iconContext.ScaleX);
+				graphics.RenderSolid(Drawing.Color.FromBrightness(0.5));
+			}
+
+			if ( this.IsHilite && iconContext.IsEditable )
+			{
+				path = this.PathBuildSurface();
+				graphics.Rasterizer.AddSurface(path);
+				graphics.RenderSolid(iconContext.HiliteSurfaceColor);
+
+				path = this.PathBuildOutline();
+				graphics.Rasterizer.AddOutline(path, 1.0/iconContext.ScaleX);
+				graphics.RenderSolid(iconContext.HiliteOutlineColor);
+			}
+		}
+
+
+		protected string					filename;
+		protected Drawing.Image				imageOriginal;
+		protected Drawing.Image				imageDimmed;
+	}
+}
