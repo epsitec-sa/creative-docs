7ab1d851611f97a8cbdf47b80595d356b58b7d41
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:40:47 +0000

    ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1529 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Pictogram/Data/PropertyFont.cs
@@ -0,0 +1,150 @@
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Pictogram.Widgets;
+using System.Xml.Serialization;
+
+namespace Epsitec.Common.Pictogram.Data
+{
+	/// <summary>
+	/// La classe PropertyFont représente une propriété d'un objet graphique.
+	/// </summary>
+	public class PropertyFont : AbstractProperty
+	{
+		public PropertyFont()
+		{
+			this.fontName    = "Arial";
+			this.fontOptical = "Regular";
+			this.fontSize    = 1.0;
+			this.fontColor   = Drawing.Color.FromBrightness(0);
+		}
+
+		[XmlAttribute]
+		public string FontName
+		{
+			get
+			{
+				return this.fontName;
+			}
+
+			set
+			{
+				if ( this.fontName != value )
+				{
+					this.fontName = value;
+					this.OnChanged();
+				}
+			}
+		}
+
+		[XmlAttribute]
+		public string FontOptical
+		{
+			get
+			{
+				return this.fontOptical;
+			}
+
+			set
+			{
+				if ( this.fontOptical != value )
+				{
+					this.fontOptical = value;
+					this.OnChanged();
+				}
+			}
+		}
+
+		[XmlAttribute]
+		public double FontSize
+		{
+			get
+			{
+				return this.fontSize;
+			}
+
+			set
+			{
+				if ( this.fontSize != value )
+				{
+					this.fontSize = value;
+					this.OnChanged();
+				}
+			}
+		}
+
+		public Drawing.Color FontColor
+		{
+			get
+			{
+				return this.fontColor;
+			}
+
+			set
+			{
+				if ( this.fontColor != value )
+				{
+					this.fontColor = value;
+					this.OnChanged();
+				}
+			}
+		}
+
+		// Détermine le nom de la propriété dans la liste (Lister).
+		public string GetListName()
+		{
+			return this.fontName;
+		}
+
+		// Indique si un changement de cette propriété modifie la bbox de l'objet.
+		[XmlIgnore]
+		public override bool AlterBoundingBox
+		{
+			get { return true; }
+		}
+
+		// Effectue une copie de la propriété.
+		public override void CopyTo(AbstractProperty property)
+		{
+			base.CopyTo(property);
+			PropertyFont p = property as PropertyFont;
+			p.FontName    = this.fontName;
+			p.FontOptical = this.fontOptical;
+			p.FontSize    = this.fontSize;
+			p.FontColor   = this.fontColor;
+		}
+
+		// Compare deux propriétés.
+		public override bool Compare(AbstractProperty property)
+		{
+			if ( !base.Compare(property) )  return false;
+
+			PropertyFont p = property as PropertyFont;
+			if ( p.FontName    != this.fontName    )  return false;
+			if ( p.FontOptical != this.fontOptical )  return false;
+			if ( p.FontSize    != this.fontSize    )  return false;
+			if ( p.FontColor   != this.fontColor   )  return false;
+
+			return true;
+		}
+
+		// Crée le panneau permettant d'éditer la propriété.
+		public override AbstractPanel CreatePanel()
+		{
+			return new PanelFont();
+		}
+
+
+		// Retourne la fonte à utiliser.
+		public Drawing.Font GetFont()
+		{
+			return Drawing.Font.GetFont(this.fontName, this.fontOptical);
+		}
+
+
+		protected string				fontName;
+		protected string				fontOptical;
+		protected double				fontSize;
+
+		[XmlAttribute]
+		protected Drawing.Color			fontColor;
+	}
+}
