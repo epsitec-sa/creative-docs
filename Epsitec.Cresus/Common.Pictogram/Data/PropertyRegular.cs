ecadb9faf83ef28d90fe72fa772a513c35af4e39
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:55:02 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:
    **********************
    Label: "Stable-01-04-2004"
    User: Arnaud       Date:  1.04.04   Time: 12:42
    Labeled
    Label comment: TabIndex edition working in GUI.
    
    **********************
    Label: "Stable-24-03-2004"
    User: Arnaud       Date: 24.03.04   Time: 22:47
    Labeled
    Label comment: With DR contributions. First draft of Common.UI.
    
    **********************
    Label: "Stable-20-03-2004"
    User: Arnaud       Date: 20.03.04   Time: 12:18
    Labeled
    Label comment: 
    
    **********************
    Label: "Stable-15-03-2004"
    User: Arnaud       Date: 15.03.04   Time: 11:30
    Labeled
    Label comment: EditArray et App.Designer bien avancés.
    
    git-svn-id: https://svn.opac.ch/svn/cr@2224 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Pictogram/Data/PropertyRegular.cs
+++ Epsitec.Cresus/Common.Pictogram/Data/PropertyRegular.cs
@@ -96,9 +96,9 @@ namespace Epsitec.Common.Pictogram.Data
 		}
 
 		// Crée le panneau permettant d'éditer la propriété.
-		public override AbstractPanel CreatePanel()
+		public override AbstractPanel CreatePanel(Drawer drawer)
 		{
-			return new PanelRegular();
+			return new PanelRegular(drawer);
 		}
 
 
--- Epsitec.Cresus/Common.Pictogram/Data/PropertyShadow.cs
+++ Epsitec.Cresus/Common.Pictogram/Data/PropertyShadow.cs
@@ -74,9 +74,9 @@ namespace Epsitec.Common.Pictogram.Data
 		}
 
 		// Crée le panneau permettant d'éditer la propriété.
-		public override AbstractPanel CreatePanel()
+		public override AbstractPanel CreatePanel(Drawer drawer)
 		{
-			return new PanelShadow();
+			return new PanelShadow(drawer);
 		}
 
 
