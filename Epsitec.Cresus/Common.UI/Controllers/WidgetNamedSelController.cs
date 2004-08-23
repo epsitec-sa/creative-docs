53d53e157f28918d6eaea6f2f1855b3b57b9ea1b
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:27:21 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1991 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.UI/Controllers/WidgetNamedSelController.cs
@@ -0,0 +1,221 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : en chantier/PA, 29/04/2004
+
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+
+namespace Epsitec.Common.UI.Controllers
+{
+	/// <summary>
+	/// La classe WidgetNamedSelController réalise un contrôleur très simple qui
+	/// s'appuie sur un widget existant implémentant INamedStringSelection; elle
+	/// interagit avec les propriétés SelectedIndex et SelectedName.
+	/// </summary>
+	public class WidgetNamedSelController : AbstractConstrainedController
+	{
+		public WidgetNamedSelController()
+		{
+		}
+		
+		public WidgetNamedSelController(Adapters.IAdapter adapter) : this ()
+		{
+			this.Adapter = adapter;
+		}
+		
+		public WidgetNamedSelController(Adapters.IAdapter adapter, Widget widget) : this ()
+		{
+			this.Adapter = adapter;
+			this.CreateUI (widget);
+		}
+		
+		public WidgetNamedSelController(Adapters.IAdapter adapter, Widget widget, Types.IDataConstraint constraint, Types.INamedType type) : this ()
+		{
+			this.Adapter    = adapter;
+			this.Constraint = constraint;
+			this.DataType   = type;
+			this.CreateUI (widget);
+		}
+		
+		
+		public Types.INamedType					DataType
+		{
+			get
+			{
+				return this.data_type;
+			}
+			set
+			{
+				this.data_type = value;
+			}
+		}
+		
+		
+		public override void CreateUI(Widget widget)
+		{
+			Support.Data.INamedStringSelection           named_sel   = widget as Support.Data.INamedStringSelection;
+			Common.Widgets.Helpers.IStringCollectionHost string_host = widget as Common.Widgets.Helpers.IStringCollectionHost;
+			
+			if (named_sel == null)
+			{
+				throw new System.ArgumentException ("The specified widget does not conform to INamedStringSelection interface.", "widget");
+			}
+			
+			this.widget    = widget;
+			this.named_sel = named_sel;
+			
+			if ((this.data_type is Types.IEnum) &&
+				(string_host != null))
+			{
+				Types.IEnum        enum_type   = this.data_type as Types.IEnum;
+				Types.IEnumValue[] enum_values = enum_type.Values;
+				
+				this.enum_values = enum_values;
+				
+				if (string_host.Items.Count == 0)
+				{
+					//	Le widget est vide; on va donc le remplir. On va utiliser les 'captions'
+					//	si elles existent, sinon on utilisera le nom brut :
+					
+					for (int i = 0; i < enum_values.Length; i++)
+					{
+						string name    = enum_values[i].Name;
+						string caption = enum_values[i].Caption;
+						
+						if (name != null)
+						{
+							if (caption == null)
+							{
+								caption = name;
+							}
+							
+							string_host.Items.Add (name, caption);
+						}
+					}
+				}
+				else
+				{
+					bool clear = true;
+					
+					//	Le widget a déjà des valeurs définies. Si l'énumération fournie possède
+					//	des 'captions', alors on redéfinit le contenu du widget, sinon on le
+					//	laisse dans son état initial :
+					
+					for (int i = 0; i < enum_values.Length; i++)
+					{
+						string name    = enum_values[i].Name;
+						string caption = enum_values[i].Caption;
+						
+						if ((name != null) &&
+							(caption != null))
+						{
+							if (clear)
+							{
+								string_host.Items.Clear ();
+								clear = false;
+							}
+							
+							string_host.Items.Add (name, caption);
+						}
+					}
+				}
+			}
+			
+			this.named_sel.SelectedIndexChanged += new EventHandler(HandleSelectedIndexChanged);
+			
+			this.SyncFromAdapter ();
+		}
+		
+		public override void SyncFromAdapter()
+		{
+			Adapters.DecimalAdapter num_adapter  = this.Adapter as Adapters.DecimalAdapter;
+			Adapters.StringAdapter  text_adapter = this.Adapter as Adapters.StringAdapter;
+			
+			if (this.named_sel == null)
+			{
+				return;
+			}
+			
+			if ((num_adapter != null) &&
+				(this.enum_values != null))
+			{
+				int rank  = (int) num_adapter.Value;
+				int index = -1;
+				
+				for (int i = 0; i < this.enum_values.Length; i++)
+				{
+					if (this.enum_values[i].Rank == rank)
+					{
+						index = i;
+						break;
+					}
+				}
+				
+				this.named_sel.SelectedIndex = index;
+				
+				return;
+			}
+			
+			if (text_adapter != null)
+			{
+				this.named_sel.SelectedName = text_adapter.Value;
+			}
+		}
+		
+		public override void SyncFromUI()
+		{
+			Adapters.DecimalAdapter num_adapter  = this.Adapter as Adapters.DecimalAdapter;
+			Adapters.StringAdapter  text_adapter = this.Adapter as Adapters.StringAdapter;
+			
+			if (this.named_sel == null)
+			{
+				return;
+			}
+			
+			if ((num_adapter != null) &&
+				(this.enum_values != null))
+			{
+				int index = this.named_sel.SelectedIndex;
+				
+				if ((index < 0) ||
+					(index >= this.enum_values.Length))
+				{
+					//	On ne peut pas affecter de valeur à l'adaptateur, car elle ne
+					//	fait pas partie des choix possibles.
+				}
+				else
+				{
+					int rank = this.enum_values[index].Rank;
+					
+					if (this.CheckConstraint (rank))
+					{
+						num_adapter.Value = rank;
+					}
+				}
+				
+				return;
+			}
+			
+			if (text_adapter != null)
+			{
+				string value = this.named_sel.SelectedName;
+				
+				if (this.CheckConstraint (value))
+				{
+					text_adapter.Value = value;
+				}
+			}
+		}
+		
+		
+		private void HandleSelectedIndexChanged(object sender)
+		{
+			this.SyncFromUI ();
+		}
+		
+		
+		private Widget							widget;
+		Support.Data.INamedStringSelection		named_sel;
+		private Types.INamedType				data_type;
+		private Types.IEnumValue[]				enum_values;
+	}
+}
