100f4808f8e23854d70ad5c5fb0e7092f07ed9b5
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 12:53:38 +0000

    ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@533 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/Scrollable.cs
@@ -0,0 +1,155 @@
+//	Copyright © 2003, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : OK/PA, 12/12/2003
+
+namespace Epsitec.Common.Widgets
+{
+	/// <summary>
+	/// La classe Scrollable permet de représenter un widget de taille
+	/// quelconque dans une surface de taille déterminée, en ajoutant
+	/// si besoin des ascenceurs.
+	/// </summary>
+	public class Scrollable : Widget
+	{
+		public Scrollable()
+		{
+			this.h_scroller = new HScroller (this);
+			this.v_scroller = new VScroller (this);
+			
+			this.h_scroller.Parent = this;
+			this.v_scroller.Parent = this;
+			
+			this.UpdateGeometry ();
+		}
+		
+		public Panel					Panel
+		{
+			get { return this.panel; }
+			set
+			{
+				if (this.panel != value)
+				{
+					if (this.panel != null)
+					{
+						this.DetachPanel (this.panel);
+					}
+					
+					this.panel = value;
+					
+					if (this.panel != null)
+					{
+						this.AttachPanel (this.panel);
+						this.UpdateGeometry ();
+					}
+				}
+			}
+		}
+		
+		
+		protected void AttachPanel(Panel panel)
+		{
+			panel.LayoutChanged += new EventHandler (HandlePanelLayoutChanged);
+			panel.Parent = this;
+		}
+		
+		protected void DetachPanel(Panel panel)
+		{
+			panel.LayoutChanged -= new EventHandler (HandlePanelLayoutChanged);
+			panel.Parent = null;
+		}
+		
+		protected override void UpdateClientGeometry()
+		{
+			base.UpdateClientGeometry();
+			this.UpdateGeometry ();
+		}
+		
+		protected void UpdateGeometry()
+		{
+			if ((this.h_scroller == null) ||
+				(this.v_scroller == null))
+			{
+				return;
+			}
+			
+			this.UpdatePanelLocation ();
+			
+			double margin_x = (this.v_scroller.IsVisible) ? this.v_scroller.Width : 0;
+			double margin_y = (this.h_scroller.IsVisible) ? this.h_scroller.Height : 0;
+			
+			if (this.v_scroller.IsVisible)
+			{
+				this.v_scroller.Bounds = new Drawing.Rectangle (this.Client.Width - margin_x, margin_y, margin_x, this.Client.Height - margin_y);
+			}
+			
+			if (this.h_scroller.IsVisible)
+			{
+				this.h_scroller.Bounds = new Drawing.Rectangle (0, 0, this.Client.Width - margin_x, margin_y);
+			}
+		}
+		
+		
+		protected void UpdatePanelLocation()
+		{
+			if (this.panel == null)
+			{
+				this.h_scroller.SetVisible (false);
+				this.v_scroller.SetVisible (false);
+				return;
+			}
+			
+			double margin_x = 0;
+			double margin_y = 0;
+			
+			double delta_dx;
+			double delta_dy;
+			
+			for (;;)
+			{
+				delta_dx = this.panel.Width  - this.Client.Width  + margin_x;
+				delta_dy = this.panel.Height - this.Client.Height + margin_y;
+				
+				if (delta_dx > 0)
+				{
+					if (margin_y == 0)
+					{
+						margin_y = this.h_scroller.Height;
+						continue;
+					}
+				}
+				
+				if (delta_dy > 0)
+				{
+					if (margin_x == 0)
+					{
+						margin_x = this.v_scroller.Width;
+						continue;
+					}
+				}
+				
+				break;
+			}
+			
+			this.panel.Location = new Drawing.Point (0, margin_y - delta_dy);
+			
+			this.h_scroller.SetVisible (margin_y > 0);
+			this.v_scroller.SetVisible (margin_x > 0);
+		}
+		
+		
+		protected Panel					panel;
+		protected Drawing.Size			panel_size;
+		protected VScroller				v_scroller;
+		protected HScroller				h_scroller;
+		
+		private void HandlePanelLayoutChanged(object sender)
+		{
+			System.Diagnostics.Debug.Assert (this.panel == sender);
+			
+			if (this.panel_size != panel.Size)
+			{
+				this.panel_size = panel.Size;
+				this.UpdatePanelLocation ();
+			}
+		}
+	}
+}
