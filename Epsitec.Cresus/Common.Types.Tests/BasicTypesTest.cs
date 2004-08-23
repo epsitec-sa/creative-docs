3cf8106f4dea10b9444480a9100a1c658356e8d7
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:29:47 +0000

    git-svn-id: https://svn.opac.ch/svn/cr@2012 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Types.Tests/BasicTypesTest.cs
+++ Epsitec.Cresus/Common.Types.Tests/BasicTypesTest.cs
@@ -9,6 +9,7 @@ namespace Epsitec.Common.Types
 			EnumType et = new EnumType (typeof (MyEnum));
 			
 			Assert.AreEqual (5, et.Values.Length);
+			Assert.IsFalse (et.IsCustomizable);
 			
 			Assert.AreEqual ("None",   et.Values[0].Name);
 			Assert.AreEqual ("First",  et.Values[1].Name);
@@ -27,6 +28,44 @@ namespace Epsitec.Common.Types
 			Assert.AreEqual ("Second", et[ 2].Name);
 			Assert.AreEqual ("Third" , et[ 3].Name);
 			Assert.AreEqual ("Extra" , et[99].Name);
+			
+			Assert.IsTrue (et.CheckConstraint ("3"));
+			Assert.IsTrue (et.CheckConstraint (-1));
+			Assert.IsTrue (et.CheckConstraint ("Extra"));
+			Assert.IsFalse (et.CheckConstraint (18));
+			Assert.IsFalse (et.CheckConstraint ("{Other}"));
+		}
+		
+		[Test] public void CheckCustomEnumType()
+		{
+			EnumType et = new CustomEnumType (typeof (MyEnum));
+			
+			Assert.AreEqual (5, et.Values.Length);
+			Assert.IsTrue (et.IsCustomizable);
+			
+			Assert.AreEqual ("None",   et.Values[0].Name);
+			Assert.AreEqual ("First",  et.Values[1].Name);
+			Assert.AreEqual ("Second", et.Values[2].Name);
+			Assert.AreEqual ("Third",  et.Values[3].Name);
+			Assert.AreEqual ("Extra",  et.Values[4].Name);
+			
+			Assert.AreEqual (-1, et["None"]  .Rank);
+			Assert.AreEqual ( 1, et["First"] .Rank);
+			Assert.AreEqual ( 2, et["Second"].Rank);
+			Assert.AreEqual ( 3, et["Third"] .Rank);
+			Assert.AreEqual (99, et["Extra"] .Rank);
+			
+			Assert.AreEqual ("None"  , et[-1].Name);
+			Assert.AreEqual ("First" , et[ 1].Name);
+			Assert.AreEqual ("Second", et[ 2].Name);
+			Assert.AreEqual ("Third" , et[ 3].Name);
+			Assert.AreEqual ("Extra" , et[99].Name);
+			
+			Assert.IsTrue (et.CheckConstraint ("3"));
+			Assert.IsTrue (et.CheckConstraint (-1));
+			Assert.IsTrue (et.CheckConstraint ("Extra"));
+			Assert.IsFalse (et.CheckConstraint (18));
+			Assert.IsTrue (et.CheckConstraint ("{Other}"));
 		}
 		
 		private enum MyEnum
