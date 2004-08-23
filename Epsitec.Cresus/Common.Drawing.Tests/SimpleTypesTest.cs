4e3bd029be1e14d651409443f9c9f3d3ab1773d2
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:00:18 +0000

    ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@595 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Drawing.Tests/SimpleTypesTest.cs
@@ -0,0 +1,104 @@
+using NUnit.Framework;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Epsitec.Common.Drawing
+{
+	[TestFixture] public class SimpleTypesTest
+	{
+		[Test] public void CheckPointSerialisation()
+		{
+			Point  point_1 = new Point (10, 20.5);
+			object point_2;
+			
+			using (System.IO.Stream stream = System.IO.File.Open ("test.bin", System.IO.FileMode.Create))
+			{
+				BinaryFormatter formatter = new BinaryFormatter ();
+				formatter.Serialize (stream, point_1);
+			}
+			
+			using (System.IO.Stream stream = System.IO.File.Open ("test.bin", System.IO.FileMode.Open))
+			{
+				BinaryFormatter formatter = new BinaryFormatter ();
+				point_2 = formatter.Deserialize (stream);
+			}
+			
+			System.IO.File.Delete ("test.bin");
+			
+			Assertion.AssertNotNull (point_2);
+			Assertion.AssertEquals (typeof (Point), point_2.GetType ());
+			Assertion.AssertEquals (point_1, point_2);
+		}
+		
+		[Test] public void CheckSizeSerialisation()
+		{
+			Size   size_1 = new Size (10, 20.5);
+			object size_2;
+			
+			using (System.IO.Stream stream = System.IO.File.Open ("test.bin", System.IO.FileMode.Create))
+			{
+				BinaryFormatter formatter = new BinaryFormatter ();
+				formatter.Serialize (stream, size_1);
+			}
+			
+			using (System.IO.Stream stream = System.IO.File.Open ("test.bin", System.IO.FileMode.Open))
+			{
+				BinaryFormatter formatter = new BinaryFormatter ();
+				size_2 = formatter.Deserialize (stream);
+			}
+			
+			System.IO.File.Delete ("test.bin");
+			
+			Assertion.AssertNotNull (size_2);
+			Assertion.AssertEquals (typeof (Size), size_2.GetType ());
+			Assertion.AssertEquals (size_1, size_2);
+		}
+		
+		[Test] public void CheckColorSerialisation()
+		{
+			Color  color_1 = new Color (0.1, 0.2, 0.3, 0.4);
+			object color_2;
+			
+			using (System.IO.Stream stream = System.IO.File.Open ("test.bin", System.IO.FileMode.Create))
+			{
+				BinaryFormatter formatter = new BinaryFormatter ();
+				formatter.Serialize (stream, color_1);
+			}
+			
+			using (System.IO.Stream stream = System.IO.File.Open ("test.bin", System.IO.FileMode.Open))
+			{
+				BinaryFormatter formatter = new BinaryFormatter ();
+				color_2 = formatter.Deserialize (stream);
+			}
+			
+			System.IO.File.Delete ("test.bin");
+			
+			Assertion.AssertNotNull (color_2);
+			Assertion.AssertEquals (typeof (Color), color_2.GetType ());
+			Assertion.AssertEquals (color_1, color_2);
+		}
+		
+		[Test] public void CheckRectangleSerialisation()
+		{
+			Rectangle rect_1 = new Rectangle (5, 6, 10, 20.5);
+			object    rect_2;
+			
+			using (System.IO.Stream stream = System.IO.File.Open ("test.bin", System.IO.FileMode.Create))
+			{
+				BinaryFormatter formatter = new BinaryFormatter ();
+				formatter.Serialize (stream, rect_1);
+			}
+			
+			using (System.IO.Stream stream = System.IO.File.Open ("test.bin", System.IO.FileMode.Open))
+			{
+				BinaryFormatter formatter = new BinaryFormatter ();
+				rect_2 = formatter.Deserialize (stream);
+			}
+			
+			System.IO.File.Delete ("test.bin");
+			
+			Assertion.AssertNotNull (rect_2);
+			Assertion.AssertEquals (typeof (Rectangle), rect_2.GetType ());
+			Assertion.AssertEquals (rect_1, rect_2);
+		}
+	}
+}
