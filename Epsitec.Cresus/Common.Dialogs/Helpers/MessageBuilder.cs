d820e87296cfef5360e223b5e2a150da95d18271
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:01:25 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2283 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Dialogs/Helpers/MessageBuilder.cs
@@ -0,0 +1,42 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+namespace Epsitec.Common.Dialogs.Helpers
+{
+	/// <summary>
+	/// Summary description for MessageBuilder.
+	/// </summary>
+	public class MessageBuilder
+	{
+		public static Widgets.Widget CreateAlert(string message)
+		{
+			return null;
+		}
+		
+		public static Widgets.Widget CreateIconAndText(string icon_name, string message)
+		{
+			Widgets.Widget container = new Widgets.Widget ();
+			Widgets.StaticImage widget_icon = new Widgets.StaticImage (container, icon_name);
+			Widgets.StaticText  widget_text = new Widgets.StaticText (container, message);
+			
+			widget_icon.Size          = new Drawing.Size (48, 48);
+			widget_icon.Anchor        = Widgets.AnchorStyles.TopLeft;
+			widget_icon.AnchorMargins = new Drawing.Margins (0, 0, 0, 0);
+			
+			double min_width = System.Math.Min (400, widget_text.TextLayout.SingleLineSize.Width+4);
+			
+			widget_text.TextLayout.LayoutSize = new Drawing.Size (min_width, Widgets.TextLayout.Infinite);
+			
+			double width  = widget_text.TextLayout.LayoutSize.Width;
+			double height = System.Math.Ceiling (widget_text.TextLayout.TotalRectangle.Height + 4);
+			
+			widget_text.TextBreakMode = Drawing.TextBreakMode.Hyphenate;
+			widget_text.Anchor        = Widgets.AnchorStyles.All;
+			widget_text.AnchorMargins = new Drawing.Margins (widget_icon.Width + 8, 0, 0, 0);
+			
+			container.Size = new Drawing.Size (widget_icon.Width + 8 + width, System.Math.Max (widget_icon.Height, height));
+			
+			return container;
+		}
+	}
+}
