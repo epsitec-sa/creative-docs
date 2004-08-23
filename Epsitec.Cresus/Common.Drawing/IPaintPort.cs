db96fe1cb704cfea7a0776083368cbbd6cebf9a0
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:47:59 +0000

    ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1580 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Drawing/IPaintPort.cs
@@ -0,0 +1,41 @@
+namespace Epsitec.Common.Drawing
+{
+	/// <summary>
+	/// Summary description for IPaintPort.
+	/// </summary>
+	public interface IPaintPort
+	{
+		double		LineWidth		{ get; set; }
+		JoinStyle	LineJoin		{ get; set; }
+		CapStyle	LineCap			{ get; set; }
+		double		LineMiterLimit	{ get; set; }
+		Color		Color			{ get; set; }
+		
+		void SetClippingRectangle(Rectangle rect);
+		void SetClippingRectangle(Point p, Size s);
+		void SetClippingRectangle(double x, double y, double width, double height);
+		Rectangle SaveClippingRectangle();
+		void RestoreClippingRectangle(Rectangle rect);
+		void ResetClippingRectangle();
+		bool TestForEmptyClippingRectangle();
+		
+		Transform SaveTransform();
+		void RestoreTransform(Transform transform);
+		void ScaleTransform(double sx, double sy, double cx, double cy);
+		void RotateTransform(double angle, double cx, double cy);
+		void TranslateTransform(double ox, double oy);
+		
+		void PaintOutline(Path path);
+		void PaintSurface(Path path);
+		
+		double PaintText(double x, double y, string text, Font font, double size);
+		double PaintText(double x, double y, string text, Font font, double size, Font.ClassInfo[] infos);
+		
+		void PaintImage(Image bitmap, Rectangle fill);
+		void PaintImage(Image bitmap, double fill_x, double fill_y, double fill_width, double fill_height);
+		void PaintImage(Image bitmap, Rectangle fill, Point image_origin);
+		void PaintImage(Image bitmap, Rectangle fill, Rectangle image_rect);
+		void PaintImage(Image bitmap, double fill_x, double fill_y, double fill_width, double fill_height, double image_origin_x, double image_origin_y);
+		void PaintImage(Image bitmap, double fill_x, double fill_y, double fill_width, double fill_height, double image_origin_x, double image_origin_y, double image_width, double image_height);
+	}
+}
