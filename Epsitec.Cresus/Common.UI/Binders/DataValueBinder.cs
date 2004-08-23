7e57db79d16444e223c8ac3caeda0437cf04a08a
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:19:29 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1896 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.UI/Binders/DataValueBinder.cs
@@ -0,0 +1,119 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : en chantier/PA
+
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+
+namespace Epsitec.Common.UI.Binders
+{
+	/// <summary>
+	/// La classe DataValueBinder permet d'accéder à la propriété d'un objet
+	/// comme source de données.
+	/// </summary>
+	public class DataValueBinder : AbstractBinder, System.IDisposable
+	{
+		public DataValueBinder()
+		{
+		}
+		
+		public DataValueBinder(Types.IDataValue source) : this ()
+		{
+			this.Source = source;
+		}
+		
+		
+		public Types.IDataValue					Source
+		{
+			get
+			{
+				return this.source;
+			}
+			set
+			{
+				if (this.source != value)
+				{
+					this.source = value;
+					this.OnSourceChanged ();
+				}
+			}
+		}
+		
+		public override string					Caption
+		{
+			get
+			{
+				return this.source.Caption;
+			}
+		}
+		
+		public override bool					IsValid
+		{
+			get
+			{
+				return this.source != null;
+			}
+		}
+		
+		
+		public override System.Type GetDataType()
+		{
+			return this.source == null ? null : this.source.DataType.SystemType;
+		}
+		
+		public override bool ReadData(out object data)
+		{
+			if (this.IsValid)
+			{
+				data = this.source.ReadValue ();
+				return true;
+			}
+			
+			data = null;
+			
+			return false;
+		}
+		
+		public override bool WriteData(object data)
+		{
+			if (this.IsValid)
+			{
+				this.source.WriteValue (data);
+				return true;
+			}
+			
+			return false;
+		}
+		
+		
+		protected virtual void OnSourceChanged()
+		{
+			this.SyncToAdapter ();
+		}
+		
+		protected virtual void OnPropertyChanged()
+		{
+			this.SyncToAdapter ();
+		}
+		
+		
+		#region IDisposable Members
+		public void Dispose()
+		{
+			this.Dispose (true);
+			System.GC.SuppressFinalize (this);
+		}
+		#endregion
+		
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				this.source = null;
+			}
+		}
+		
+		
+		
+		private Types.IDataValue				source;
+	}
+}
