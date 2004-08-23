be147a62dc10561f899d072f1f98d657e2e035c1
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:19:43 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1901 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.UI/Data/Record.cs
+++ Epsitec.Cresus/Common.UI/Data/Record.cs
@@ -7,10 +7,49 @@ namespace Epsitec.Common.UI.Data
 	/// La classe Record décrit un ensemble de champs utilisés pour échanger des
 	/// données entre une application et son interface via mapper/binder/...
 	/// </summary>
-	public class Record : Types.AbstractDataCollection, Types.IDataFolder
+	public class Record : Types.AbstractDataCollection, Types.IDataFolder, Types.IDataGraph
 	{
 		public Record()
 		{
+			this.graph = new Types.DataGraph (this);
+		}
+		
+		
+		public void Add(Field field)
+		{
+			this.list.Add (field);
+			this.ClearCachedItemArray ();
+		}
+		
+		public void AddRange(System.Collections.ICollection fields)
+		{
+			foreach (object field in fields)
+			{
+				if (!(field is Field))
+				{
+					throw new System.ArgumentException ("Collection contains invalid field.", "fields");
+				}
+			}
+			
+			this.list.AddRange (fields);
+			this.ClearCachedItemArray ();
+		}
+		
+		
+		public new Field						this[string name]
+		{
+			get
+			{
+				return base[name] as Field;
+			}
+		}
+
+		public new Field						this[int index]
+		{
+			get
+			{
+				return base[index] as Field;
+			}
 		}
 		
 		
@@ -50,6 +89,26 @@ namespace Epsitec.Common.UI.Data
 		}
 		#endregion
 		
+		#region IDataGraph Members
+		Types.IDataCollection Types.IDataGraph.Select(string query)
+		{
+			return this.graph.Select (query);
+		}
+		
+		Types.IDataFolder						Types.IDataGraph.Root
+		{
+			get
+			{
+				return this;
+			}
+		}
+		
+		Types.IDataItem Types.IDataGraph.Navigate(string path)
+		{
+			return this.graph.Navigate (path);
+		}
+		#endregion
+		
 		protected override Types.IDataItem[] GetCachedItemArray()
 		{
 			return this.fields;
@@ -72,5 +131,6 @@ namespace Epsitec.Common.UI.Data
 
 		
 		private Field[]							fields;
+		private Types.DataGraph					graph;
 	}
 }
