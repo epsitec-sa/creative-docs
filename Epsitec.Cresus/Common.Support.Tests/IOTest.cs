9198aa39d64e8bdfcc1d844ef59d19af6e7b65a6
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:58:20 +0000

    ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1670 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Support.Tests/IOTest.cs
@@ -0,0 +1,241 @@
+using NUnit.Framework;
+
+namespace Epsitec.Common.Support
+{
+	[TestFixture] public class IOTest
+	{
+		[Test] public void CheckBZip2()
+		{
+			byte[] buffer = System.Text.Encoding.UTF8.GetBytes (IOTest.SampleText);
+			
+			System.IO.MemoryStream target = new System.IO.MemoryStream ();
+			System.IO.Stream   compressor = IO.CompressionStream.CreateBZip2 (target);
+			
+			compressor.Write (buffer, 0, buffer.Length);
+			compressor.Close ();
+			
+			byte[] data = target.ToArray ();
+			long length = data.Length;
+			
+			System.Console.Out.WriteLine ("Source size : {0} bytes", buffer.Length);
+			System.Console.Out.WriteLine ("Compressed size : {0} bytes, using BZIP2", length);
+			
+			System.IO.MemoryStream source = new System.IO.MemoryStream (data);
+			System.IO.Stream decompressor = IO.DecompressionStream.CreateAuto (source);
+			
+			byte[] read = new byte[buffer.Length];
+			
+			decompressor.Read (read, 0, buffer.Length);
+			decompressor.Close ();
+			
+			for (int i = 0; i < buffer.Length; i++)
+			{
+				Assertion.AssertEquals (string.Format ("offset {0}: {1} != {2}", i, (char) buffer[i], (char) read[i]), buffer[i], read[i]);
+			}
+		}
+		
+		[Test] public void CheckZip()
+		{
+			byte[] buffer = System.Text.Encoding.UTF8.GetBytes (IOTest.SampleText);
+			
+			System.IO.MemoryStream target = new System.IO.MemoryStream ();
+			System.IO.Stream   compressor = IO.CompressionStream.CreateZip (target, "test");
+			
+			compressor.Write (buffer, 0, buffer.Length);
+			compressor.Close ();
+			
+			byte[] data = target.ToArray ();
+			long length = data.Length;
+			
+			System.Console.Out.WriteLine ("Compressed size : {0} bytes, using ZIP", length);
+			
+			System.IO.MemoryStream source = new System.IO.MemoryStream (data);
+			System.IO.Stream decompressor = IO.DecompressionStream.CreateAuto (source);
+			
+			byte[] read = new byte[buffer.Length];
+			
+			decompressor.Read (read, 0, buffer.Length);
+			decompressor.Close ();
+			
+			for (int i = 0; i < buffer.Length; i++)
+			{
+				Assertion.AssertEquals (string.Format ("offset {0}: {1} != {2}", i, (char) buffer[i], (char) read[i]), buffer[i], read[i]);
+			}
+		}
+		
+		[Test] public void CheckDeflateLevel1()
+		{
+			byte[] buffer = System.Text.Encoding.UTF8.GetBytes (IOTest.SampleText);
+			
+			System.IO.MemoryStream target = new System.IO.MemoryStream ();
+			System.IO.Stream   compressor = IO.CompressionStream.CreateDeflate (target, 1);
+			
+			compressor.Write (buffer, 0, buffer.Length);
+			compressor.Close ();
+			
+			byte[] data = target.ToArray ();
+			long length = data.Length;
+			
+			System.Console.Out.WriteLine ("Compressed size : {0} bytes, using DEFLATE-1", length);
+			
+			System.IO.MemoryStream source = new System.IO.MemoryStream (data);
+			System.IO.Stream decompressor = IO.DecompressionStream.CreateAuto (source);
+			
+			byte[] read = new byte[buffer.Length];
+			
+			decompressor.Read (read, 0, buffer.Length);
+			decompressor.Close ();
+			
+			for (int i = 0; i < buffer.Length; i++)
+			{
+				Assertion.AssertEquals (string.Format ("offset {0}: {1} != {2}", i, (char) buffer[i], (char) read[i]), buffer[i], read[i]);
+			}
+		}
+		
+		[Test] public void CheckDeflateLevel9()
+		{
+			byte[] buffer = System.Text.Encoding.UTF8.GetBytes (IOTest.SampleText);
+			
+			System.IO.MemoryStream target = new System.IO.MemoryStream ();
+			System.IO.Stream   compressor = IO.CompressionStream.CreateDeflate (target, 9);
+			
+			compressor.Write (buffer, 0, buffer.Length);
+			compressor.Close ();
+			
+			byte[] data = target.ToArray ();
+			long length = data.Length;
+			
+			System.Console.Out.WriteLine ("Compressed size : {0} bytes, using DEFLATE-9", length);
+			
+			System.IO.MemoryStream source = new System.IO.MemoryStream (data);
+			System.IO.Stream decompressor = IO.DecompressionStream.CreateAuto (source);
+			
+			byte[] read = new byte[buffer.Length];
+			
+			decompressor.Read (read, 0, buffer.Length);
+			decompressor.Close ();
+			
+			for (int i = 0; i < buffer.Length; i++)
+			{
+				Assertion.AssertEquals (string.Format ("offset {0}: {1} != {2}", i, (char) buffer[i], (char) read[i]), buffer[i], read[i]);
+			}
+		}
+		
+		[Test] public void CheckGZip()
+		{
+			byte[] buffer = System.Text.Encoding.UTF8.GetBytes (IOTest.SampleText);
+			
+			System.IO.MemoryStream target = new System.IO.MemoryStream ();
+			System.IO.Stream   compressor = IO.CompressionStream.CreateGZip (target);
+			
+			compressor.Write (buffer, 0, buffer.Length);
+			compressor.Close ();
+			
+			byte[] data = target.ToArray ();
+			long length = data.Length;
+			
+			System.Console.Out.WriteLine ("Compressed size : {0} bytes, using GZIP", length);
+			
+			System.IO.MemoryStream source = new System.IO.MemoryStream (data);
+			System.IO.Stream decompressor = IO.DecompressionStream.CreateAuto (source);
+			
+			byte[] read = new byte[buffer.Length];
+			
+			decompressor.Read (read, 0, buffer.Length);
+			decompressor.Close ();
+			
+			for (int i = 0; i < buffer.Length; i++)
+			{
+				Assertion.AssertEquals (string.Format ("offset {0}: {1} != {2}", i, (char) buffer[i], (char) read[i]), buffer[i], read[i]);
+			}
+		}
+		
+		[Test] public void CheckChecksumCRC()
+		{
+			IO.IChecksum checksum = IO.Checksum.CreateCrc32 ();
+			byte[] buffer = System.Text.Encoding.UTF8.GetBytes (IOTest.SampleText);
+			
+			Assertion.AssertEquals (0, checksum.Value);
+			
+			checksum.Reset ();
+			Assertion.AssertEquals (0, checksum.Value);
+			checksum.Update (buffer);
+			
+			long value_0 = checksum.Value;
+			
+			byte b10 = buffer[10];
+			
+			buffer[10] = buffer[20];
+			buffer[20] = b10;
+			
+			checksum.Reset ();
+			Assertion.AssertEquals (0, checksum.Value);
+			checksum.Update (buffer);
+			
+			long value_1 = checksum.Value;
+			
+			System.Console.Out.WriteLine ();
+			System.Console.Out.WriteLine ("CRC32 : {0:X}, after byte swap {1:X}", value_0, value_1);
+			
+			Assertion.Assert (value_0 != value_1);
+		}
+		
+		[Test] public void CheckChecksumAdler()
+		{
+			IO.IChecksum checksum = IO.Checksum.CreateAdler32 ();
+			byte[] buffer = System.Text.Encoding.UTF8.GetBytes (IOTest.SampleText);
+			
+			Assertion.AssertEquals (1, checksum.Value);
+			
+			checksum.Reset ();
+			Assertion.AssertEquals (1, checksum.Value);
+			checksum.Update (buffer);
+			
+			long value_0 = checksum.Value;
+			
+			byte b10 = buffer[10];
+			
+			buffer[10] = buffer[20];
+			buffer[20] = b10;
+			
+			checksum.Reset ();
+			Assertion.AssertEquals (1, checksum.Value);
+			checksum.Update (buffer);
+			
+			long value_1 = checksum.Value;
+			
+			System.Console.Out.WriteLine ("Adler : {0:X}, after byte swap {1:X}", value_0, value_1);
+			
+			Assertion.Assert (value_0 != value_1);
+		}
+		
+		
+		private static string					SampleText
+		{
+			get
+			{
+				return "Ceci est un petit essai qui devrait permettre de comparer la qualité de la "
+					 + "compression de divers algorithmes, appliqués à un morceau de texte relativement "
+					 + "simple. L'utilisation de tags <italique>XML</italique> dans un tel <gras>texte</gras> "
+					 + "est quelque peu <italique>artificielle</italique>, mais c'est un <gras>test</gras>, "
+					 + "alors que veut-on de plus&#160;? Voici donc le XML alibi&#160;:\n"
+					 + "<root>\n"
+					+ "  <element name='abc'>\n"
+					+ "    <node arg='1'/>\n"
+					+ "    <node arg='2'/>\n"
+					+ "    <node arg='3'/>\n"
+					+ "    <node arg='4'/>\n"
+					+ "    <node arg='5'/>\n"
+					+ "    <node arg='6'/>\n"
+					+ "  </element>\n"
+					+ "  <element name='xyz'>\n"
+					+ "    <node arg='3'/>\n"
+					+ "    <node arg='6'/>\n"
+					+ "    <node arg='8'/>\n"
+					+ "    <node arg='10'/>\n"
+					+ "  </element>\n"
+					+ "</root>\n";
+			}
+		}
+	}
+}
