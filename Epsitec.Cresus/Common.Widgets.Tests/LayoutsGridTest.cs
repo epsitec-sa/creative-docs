7778301b43e804c6d5a79d7630ac8ae5f2fdac53
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 12:49:23 +0000

    git-svn-id: https://svn.opac.ch/svn/cr@484 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Widgets.Tests/LayoutsGridTest.cs
+++ Epsitec.Cresus/Common.Widgets.Tests/LayoutsGridTest.cs
@@ -4,7 +4,7 @@ namespace Epsitec.Common.Widgets
 {
 	[TestFixture] public class LayoutsGridTest
 	{
-		[Test] public void CheckSetup()
+		[Test] public void CheckAlgorithm()
 		{
 			Layouts.Grid layout = new Layouts.Grid ();
 			
@@ -54,7 +54,8 @@ namespace Epsitec.Common.Widgets
 			wa3.SetLayoutArgs (0, 3);
 			
 			layout.Root = wa;
-			layout.DesiredWidth = 100;
+			layout.DesiredWidth  = 100;
+			layout.DesiredHeight = 100;
 			
 			min_dx  = layout.MinColumnWidths;
 			live_dx = layout.CurrentColumnWidths;
@@ -69,6 +70,11 @@ namespace Epsitec.Common.Widgets
 			Assertion.AssertEquals (50, live_dx[2]);
 			Assertion.AssertEquals (100, layout.DesiredWidth);
 			Assertion.AssertEquals (150, layout.CurrentWidth);
+			
+			System.Console.Out.WriteLine ("{0} : {1}", wa1.Name,  wa1.Bounds);
+			System.Console.Out.WriteLine ("{0} : {1}", wa11.Name, wa11.Bounds);
+			System.Console.Out.WriteLine ("{0} : {1}", wa2.Name,  wa2.Bounds);
+			System.Console.Out.WriteLine ("{0} : {1}", wa3.Name,  wa3.Bounds);
 			
 			layout.DesiredWidth = 200;
 			live_dx = layout.CurrentColumnWidths;
@@ -100,6 +106,68 @@ namespace Epsitec.Common.Widgets
 			Assertion.AssertEquals (40, min_dx[0]);
 			Assertion.AssertEquals (80, min_dx[1]);
 			Assertion.AssertEquals (50, min_dx[2]);
+		}
+		
+		[Test] public void CheckDesigner()
+		{
+			Layouts.Grid layout = new Layouts.Grid ();
+			Window       window = new Window ();
+			
+			Widget wa   = new StaticText ("");		wa.BackColor   = Drawing.Color.FromBrightness (1.0);
+			Widget wa1  = new StaticText ("A1");	wa1.BackColor  = Drawing.Color.FromARGB (0.33, 1.0, 1.0, 0.0);	wa1.Height = 40;
+			Widget wa2  = new StaticText ("A2");	wa2.BackColor  = Drawing.Color.FromARGB (0.33, 0.0, 1.0, 1.0);
+			Widget wa3  = new StaticText ("A3");	wa3.BackColor  = Drawing.Color.FromARGB (0.33, 0.0, 1.0, 0.0);
+			Widget wa4  = new StaticText ("A4");	wa4.BackColor  = Drawing.Color.FromARGB (0.33, 1.0, 0.5, 0.0);
+			Widget wa5  = new StaticText ("A5");	wa5.BackColor  = Drawing.Color.FromARGB (0.33, 1.0, 0.0, 0.5);	wa5.Height = 60;
+			
+			layout.Columns.Add (new Layouts.Grid.Column (50, 1));
+			layout.Columns.Add (new Layouts.Grid.Column (50, 0));
+			layout.Columns.Add (new Layouts.Grid.Column (50, 1));
+			
+			wa1.Parent      = wa;
+			wa1.Name        = "A1";
+			wa1.MinSize     = new Drawing.Size (100, 30);
+			wa1.Dock        = DockStyle.Layout;
+			wa1.LayoutFlags = LayoutFlags.IncludeChildren;
+			wa1.SetLayoutArgs (0, 2);
+			
+			wa2.Parent      = wa;
+			wa2.Name        = "A2";
+			wa2.MinSize     = new Drawing.Size (30, 30);
+			wa2.Dock        = DockStyle.Layout;
+			wa2.SetLayoutArgs (2, 3);
+			
+			wa3.Parent      = wa;
+			wa3.Name        = "A3";
+			wa3.MinSize     = new Drawing.Size (140, 30);
+			wa3.LayoutFlags = LayoutFlags.StartNewLine;
+			wa3.Dock        = DockStyle.Layout;
+			wa3.SetLayoutArgs (0, 3);
+			
+			wa4.Parent      = wa;
+			wa4.Name        = "A4";
+			wa4.MinSize     = new Drawing.Size (30, 30);
+			wa4.LayoutFlags = LayoutFlags.StartNewLine;
+			wa4.Dock        = DockStyle.Layout;
+			wa4.SetLayoutArgs (0, 1);
+			
+			wa5.Parent      = wa;
+			wa5.Name        = "A5";
+			wa5.MinSize     = new Drawing.Size (30, 30);
+			wa5.Dock        = DockStyle.Layout;
+			wa5.SetLayoutArgs (1, 3);
+			
+			layout.Root = wa;
+			layout.DesiredWidth  = 200;
+			layout.DesiredHeight = 120;
+			
+			window.ClientSize = new Drawing.Size (layout.CurrentWidth + 20, layout.CurrentHeight + 20);
+			
+			wa.Parent = window.Root;
+			wa.Left   = 10;
+			wa.Bottom = 10;
+			
+			window.Show ();
 		}
 	}
 }
