7a2adc3eb5a68f0b00dd7d413d1080b62afb2b5e
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:30:13 +0000

    ********************** Label: "Stable-22-06-2004" User: Arnaud       Date: 22.06.04   Time: 18:13 Labeled Label comment: First version with dynamic script compilation.
    **********************
    Label: "Stable-26-04-2004"
    User: Arnaud       Date: 26.04.04   Time: 21:22
    Labeled
    Label comment: 
    
    **********************
    Label: "Stable-01-04-2004"
    User: Arnaud       Date:  1.04.04   Time: 12:42
    Labeled
    Label comment: TabIndex edition working in GUI.
    
    **********************
    Label: "Stable-24-03-2004"
    User: Arnaud       Date: 24.03.04   Time: 22:47
    Labeled
    Label comment: With DR contributions. First draft of Common.UI.
    
    git-svn-id: https://svn.opac.ch/svn/cr@2512 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/Panels/AbstractPalette.cs
+++ Epsitec.Cresus/Common.Designer/Panels/AbstractPalette.cs
@@ -1,15 +1,12 @@
 //	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
-//	Statut : en chantier/PA
-
-using Epsitec.Common.Support;
-using Epsitec.Common.Widgets;
+//	Responsable: Pierre ARNAUD
 
 namespace Epsitec.Common.Designer.Panels
 {
 	/// <summary>
 	/// La classe AbstractPalette est la base de toutes les palettes.
 	/// </summary>
-	public abstract class AbstractPalette : AbstractPanel
+	public abstract class AbstractPalette : Common.UI.AbstractPanel
 	{
 		public AbstractPalette()
 		{
