bc92e912b3514ef1272d6fc7bbe8c7e3e40d723b
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:00:56 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2279 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Dialogs/AbstractYesNoCancel.cs
@@ -0,0 +1,120 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+
+namespace Epsitec.Common.Dialogs
+{
+	/// <summary>
+	/// Summary description for AbstractYesNoCancel.
+	/// </summary>
+	public abstract class AbstractYesNoCancel : AbstractDialog
+	{
+		public AbstractYesNoCancel(string dialog_title, string command_yes_template, string command_no_template, CommandDispatcher command_dispatcher)
+		{
+			this.dialog_title         = dialog_title;
+			this.command_yes_template = command_yes_template;
+			this.command_no_template  = command_no_template;
+			this.command_dispatcher   = command_dispatcher;
+			this.private_dispatcher   = new CommandDispatcher ("Dialog", true);
+		}
+		
+		
+		public abstract string[]				CommandArgs
+		{
+			get;
+		}
+		
+		public CommandDispatcher				CommandDispatcher
+		{
+			get
+			{
+				return this.private_dispatcher;
+			}
+		}
+		
+		
+		protected abstract Widget CreateBodyWidget();
+		protected override void   CreateWindow()
+		{
+			Widget body = this.CreateBodyWidget ();
+			Button button1;
+			Button button2;
+			Button button3;
+			
+			double dx = body.Width;
+			double dy = body.Height;
+			
+			dx = System.Math.Max (dx, 3*75+4*8);
+			
+			this.window = new Window ();
+			
+			this.window.Text              = this.dialog_title;
+			this.window.Name              = "Dialog";
+			this.window.ClientSize        = new Drawing.Size (dx+2*8, dy+2*16+24+16);
+			this.window.PreventAutoClose  = true;
+			this.window.CommandDispatcher = this.private_dispatcher;
+			this.window.CommandDispatcher.RegisterController (this);
+			this.window.MakeFixedSizeWindow ();
+			this.window.MakeSecondaryWindow ();
+			
+			body.Parent          = this.window.Root;
+			body.Bounds          = new Drawing.Rectangle (8, 16+24+16, dx, dy);
+			body.TabIndex        = 1;
+			body.TabNavigation   = Widget.TabNavigationMode.ForwardTabPassive;
+			
+			button1               = new Button (this.window.Root);
+			button1.Bounds        = new Drawing.Rectangle (this.window.Root.Width - 3*75 - 3*8, 16, 75, button1.Height);
+			button1.Text          = "<m>Y</m>es";
+			button1.Command       = "ValidateDialogYes";
+			button1.TabIndex      = 2;
+			button1.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+			button1.ButtonStyle   = ButtonStyle.DefaultAccept;
+			
+			button2               = new Button (this.window.Root);
+			button2.Bounds        = new Drawing.Rectangle (this.window.Root.Width - 2*75 - 2*8, 16, 75, button2.Height);
+			button2.Text          = "<m>N</m>o";
+			button2.Command       = "ValidateDialogNo";
+			button2.TabIndex      = 3;
+			button2.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+			
+			button3               = new Button (this.window.Root);
+			button3.Bounds        = new Drawing.Rectangle (this.window.Root.Width - 75 - 8, 16, 75, button3.Height);
+			button3.Text          = "Annuler";
+			button3.Command       = "QuitDialog";
+			button3.TabIndex      = 4;
+			button3.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+			button3.ButtonStyle   = ButtonStyle.DefaultCancel;
+			
+			AbstractDialog.LayoutButtons (this.window.Root.Width, button1, button2, button3);
+			
+			this.window.FocusedWidget = body.FindTabWidget (Widget.TabNavigationDir.Forwards, Widget.TabNavigationMode.ActivateOnTab);
+		}
+		
+		
+		[Command ("ValidateDialogYes")] protected void CommandValidateDialogYes()
+		{
+			this.window.QueueCommand (this, string.Format (this.command_yes_template, this.CommandArgs), this.command_dispatcher);
+			this.Close ();
+		}
+		
+		[Command ("ValidateDialogNo")]  protected void CommandValidateDialogNo()
+		{
+			this.window.QueueCommand (this, string.Format (this.command_no_template, this.CommandArgs), this.command_dispatcher);
+			this.Close ();
+		}
+		
+		[Command ("QuitDialog")]        protected void CommandQuitDialog()
+		{
+			this.Close ();
+		}
+		
+		
+		protected string						dialog_title;
+		protected string						command_yes_template;
+		protected string						command_no_template;
+		protected Support.CommandDispatcher		command_dispatcher;
+		protected Support.CommandDispatcher		private_dispatcher;
+	}
+}
