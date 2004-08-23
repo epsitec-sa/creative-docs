9e3acd0aea6d00a6b1faea971f8ace3514f1166a
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:11:21 +0000

    ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@1815 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Database/Tags.cs
@@ -0,0 +1,43 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : OK/PA, 23/04/2004
+
+namespace Epsitec.Cresus.Database
+{
+	public class Tags : Epsitec.Common.Support.Tags
+	{
+		//	Noms des types "Crésus" fondamentaux :
+		
+		public const string	TypeKeyId			= "CR_KeyIdType";
+		public const string	TypeKeyRevision		= "CR_KeyRevisionType";
+		public const string	TypeKeyStatus		= "CR_KeyStatusType";
+		public const string	TypeName			= "CR_NameType";
+		public const string	TypeCaption			= "CR_CaptionType";
+		public const string	TypeDescription		= "CR_DescriptionType";
+		public const string	TypeInfoXml			= "CR_InfoXmlType";
+		
+		//	Noms des colonnes "Crésus" fondamentales :
+		
+		public const string	ColumnId			= "CR_ID";
+		public const string	ColumnRevision		= "CR_REV";
+		public const string	ColumnStatus		= "CR_STAT";
+		public const string	ColumnName			= "CR_NAME";
+		public const string	ColumnCaption		= "CR_CAPTION";
+		public const string	ColumnDescription	= "CR_DESCRIPTION";
+		public const string	ColumnInfoXml		= "CR_INFO";
+		public const string	ColumnNextId		= "CR_NEXT_ID";
+		
+		public const string	ColumnRefTable		= "CREF_TABLE";
+		public const string	ColumnRefType		= "CREF_TYPE";
+		public const string	ColumnRefColumn		= "CREF_COLUMN";
+		public const string	ColumnRefSource		= "CREF_SOURCE";
+		public const string	ColumnRefTarget		= "CREF_TARGET";
+		
+		//	Noms des tables "Crésus" fondamentales :
+		
+		public const string	TableTableDef		= "CR_TABLE_DEF";
+		public const string	TableColumnDef		= "CR_COLUMN_DEF";
+		public const string	TableTypeDef		= "CR_TYPE_DEF";
+		public const string	TableEnumValDef		= "CR_ENUMVAL_DEF";
+		public const string	TableRelationDef	= "CR_RELATION_DEF";
+	}
+}
