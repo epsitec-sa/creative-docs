0355161471cee1250b0d536c07841e2e74eb6eed
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 15:48:50 +0000

    ********************** Label: "Stable-26-04-2004" User: Arnaud       Date: 26.04.04   Time: 21:22 Labeled Label comment:   ********************** Label: "Stable-01-04-2004" User: Arnaud       Date:  1.04.04   Time: 12:42 Labeled Label comment: TabIndex edition working in GUI.  ********************** Label: "Stable-24-03-2004" User: Arnaud       Date: 24.03.04   Time: 22:47 Labeled Label comment: With DR contributions. First draft of Common.UI.  ********************** Label: "Stable-20-03-2004" User: Arnaud       Date: 20.03.04   Time: 12:18 Labeled Label comment:   ********************** Label: "Stable-15-03-2004" User: Arnaud       Date: 15.03.04   Time: 11:30 Labeled Label comment: EditArray et App.Designer bien avancés.  ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.  ********************** Label: "11-02-2004" User: Arnaud       Date: 12.02.04   Time:  7:47 Labeled Label comment: Stable version.   ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@2168 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Support/MulticastValidator.cs
@@ -0,0 +1,270 @@
+//	Copyright © 2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Statut : OK/PA, 26/05/2004
+
+namespace Epsitec.Common.Support
+{
+	/// <summary>
+	/// La classe MulticastValidator représente le "et" logique d'un ensemble de
+	/// règles règles de validation.
+	/// </summary>
+	public class MulticastValidator : IValidator
+	{
+		public MulticastValidator()
+		{
+			this.validators = new IValidator[0];
+		}
+		
+		
+		public IValidator[]						Validators
+		{
+			get
+			{
+				return this.validators.Clone () as IValidator[];
+			}
+		}
+		
+		
+		#region IValidator Members
+		public ValidationState						State
+		{
+			get
+			{
+				return this.state;
+			}
+		}
+		
+		public bool									IsValid
+		{
+			get
+			{
+				if (this.state == ValidationState.Dirty)
+				{
+					this.Validate ();
+				}
+				
+				return (this.state == ValidationState.Ok);
+			}
+		}
+		
+		public string								ErrorMessage
+		{
+			get
+			{
+				//	TODO: détermine le message en fonction des dépendances...
+				
+				return null;
+			}
+		}
+		
+		
+		public event EventHandler					BecameDirty;
+		
+		public void Validate()
+		{
+			this.state = ValidationState.Unknown;
+			
+			bool valid   = true;
+			bool unknown = false;
+			
+			//	Recalcule l'état... Pendant toute la phase de recalcul, on passe dans un
+			//	état spécial (Unknown) pour éviter que des boucles dans les dépendances ne
+			//	puissent causer des récursions.
+			
+			foreach (IValidator validator in this.validators)
+			{
+				if (! validator.IsValid)
+				{
+					//	Oups... L'un des éléments dont nous dépendons n'est pas valide; c'est
+					//	soit qu'il est dans un état inconnu, soit qu'il n'est réellement pas
+					//	valide.
+					
+					if (validator.State == ValidationState.Unknown)
+					{
+						unknown = true;
+					}
+					
+					valid = false;
+				}
+			}
+			
+			if (unknown)
+			{
+				this.OnUpdateState (ValidationState.Unknown);
+			}
+			else
+			{
+				this.OnUpdateState (valid ? ValidationState.Ok : ValidationState.Error);
+			}
+		}
+		
+		public void MakeDirty(bool deep)
+		{
+			this.state = ValidationState.Dirty;
+			this.OnBecameDirty ();
+			
+			if (deep)
+			{
+				foreach (IValidator validator in this.validators)
+				{
+					validator.MakeDirty (deep);
+				}
+			}
+		}
+		#endregion
+		
+		public virtual void Add(IValidator validator)
+		{
+			MulticastValidator mv = validator as MulticastValidator;
+			
+			if (mv == null)
+			{
+				this.AddValidator (validator);
+			}
+			else
+			{
+				foreach (IValidator item in mv.Validators)
+				{
+					this.AddValidator (item);
+				}
+			}
+		}
+		
+		public virtual void Remove(IValidator validator)
+		{
+			MulticastValidator mv = validator as MulticastValidator;
+			
+			if (mv == null)
+			{
+				this.RemoveValidator (validator);
+			}
+			else
+			{
+				foreach (IValidator item in mv.Validators)
+				{
+					this.RemoveValidator (item);
+				}
+			}
+		}
+		
+		
+		protected void AddValidator(IValidator validator)
+		{
+			System.Collections.ArrayList list = new System.Collections.ArrayList ();
+			list.AddRange (this.validators);
+			
+			if (list.Contains (validator))
+			{
+				throw new System.InvalidOperationException ("Cannot insert twice same IValidator.");
+			}
+			if (validator == null)
+			{
+				throw new System.ArgumentNullException ("validator", "Expected an IValidator.");
+			}
+			
+			list.Add (validator);
+			
+			IValidator[] validators = new IValidator[list.Count];
+			list.CopyTo (validators);
+			
+			validator.BecameDirty += new EventHandler (this.HandleBecameDirty);
+			this.validators = validators;
+			
+			this.OnValidatorsChanged ();
+		}
+		
+		protected void RemoveValidator(IValidator validator)
+		{
+			System.Collections.ArrayList list = new System.Collections.ArrayList ();
+			list.AddRange (this.validators);
+			
+			if (! list.Contains (validator))
+			{
+				throw new System.InvalidOperationException ("Cannot remove IValidator, not found.");
+			}
+			if (validator == null)
+			{
+				throw new System.ArgumentNullException ("validator", "Expected an IValidator.");
+			}
+			
+			list.Remove (validator);
+			IValidator[] validators = new IValidator[list.Count];
+			list.CopyTo (validators);
+			
+			validator.BecameDirty -= new EventHandler (this.HandleBecameDirty);
+			this.validators = validators;
+			
+			this.OnValidatorsChanged ();
+		}
+		
+		
+		private void HandleBecameDirty(object sender)
+		{
+			this.MakeDirty (false);
+		}
+		
+		
+		protected virtual void OnUpdateState(ValidationState new_state)
+		{
+			System.Diagnostics.Debug.Assert (new_state != ValidationState.Dirty);
+			
+			this.state = new_state;
+		}
+		
+		protected virtual void OnBecameDirty()
+		{
+			if (this.BecameDirty != null)
+			{
+				this.BecameDirty (this);
+			}
+		}
+		
+		protected virtual void OnValidatorsChanged()
+		{
+			this.MakeDirty (false);
+		}
+		
+		
+		public static IValidator Combine(IValidator validator_a, IValidator validator_b)
+		{
+			if (validator_a == null)
+			{
+				return validator_b;
+			}
+			
+			if (validator_b == null)
+			{
+				return validator_a;
+			}
+			
+			MulticastValidator mv = new MulticastValidator ();
+			
+			mv.Add (validator_a);
+			mv.Add (validator_b);
+			
+			return mv;
+		}
+		
+		public static IValidator Simplify(IValidator validator)
+		{
+			MulticastValidator mv = validator as MulticastValidator;
+			
+			if (mv != null)
+			{
+				if (mv.validators.Length == 1)
+				{
+					return mv.validators[0];
+				}
+				if (mv.validators.Length == 0)
+				{
+					return null;
+				}
+			}
+			
+			return validator;
+		}
+		
+		
+		protected ValidationState					state = ValidationState.Dirty;
+		protected IValidator[]						validators;
+	}
+}
