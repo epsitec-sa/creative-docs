7fc5d4df855c2d1517360e7471384ef0d5d90d6e
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:28:25 +0000

    ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@887 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Pictogram/Data/PropertyCombo.cs
@@ -0,0 +1,90 @@
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Pictogram.Widgets;
+using System.Xml.Serialization;
+
+namespace Epsitec.Common.Pictogram.Data
+{
+	/// <summary>
+	/// La classe PropertyCombo représente une propriété d'un objet graphique.
+	/// </summary>
+	public class PropertyCombo : AbstractProperty
+	{
+		public PropertyCombo()
+		{
+		}
+
+		[XmlAttribute]
+		public int Choice
+		{
+			get
+			{
+				return this.choice;
+			}
+
+			set
+			{
+				this.choice = value;
+			}
+		}
+
+		public void Clear()
+		{
+			this.list.Clear();
+		}
+
+		[XmlIgnore]
+		public int Count
+		{
+			get
+			{
+				return this.list.Count;
+			}
+		}
+
+		public void Add(string text)
+		{
+			this.list.Add(text);
+		}
+
+		public string Get(int index)
+		{
+			return this.list[index] as string;
+		}
+
+
+		// Effectue une copie de la propriété.
+		public override void CopyTo(AbstractProperty property)
+		{
+			base.CopyTo(property);
+			PropertyCombo p = property as PropertyCombo;
+
+			p.Choice = this.choice;
+
+			p.Clear();
+			for ( int i=0 ; i<this.Count ; i++ )
+			{
+				p.Add(this.Get(i));
+			}
+		}
+
+		// Compare deux propriétés.
+		public override bool Compare(AbstractProperty property)
+		{
+			if ( !base.Compare(property) )  return false;
+
+			PropertyCombo p = property as PropertyCombo;
+			if ( p.Choice != this.choice )  return false;
+
+			return true;
+		}
+
+		// Crée le panneau permettant d'éditer la propriété.
+		public override AbstractPanel CreatePanel()
+		{
+			return new PanelCombo();
+		}
+
+		protected int							choice = 0;
+		protected System.Collections.ArrayList	list = new System.Collections.ArrayList();
+	}
+}
---
+++ Epsitec.Cresus/Common.Pictogram/Widgets/PanelCombo.cs
@@ -0,0 +1,101 @@
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Pictogram.Data;
+
+namespace Epsitec.Common.Pictogram.Widgets
+{
+	/// <summary>
+	/// La classe PanelCombo permet de choisir un rang dans un TextFieldCombo.
+	/// </summary>
+	public class PanelCombo : AbstractPanel
+	{
+		public PanelCombo()
+		{
+			this.label = new StaticText(this);
+			this.label.Alignment = Drawing.ContentAlignment.MiddleLeft;
+
+			this.list = new TextFieldCombo(this);
+			this.list.IsReadOnly = true;
+			this.list.TextChanged += new EventHandler(this.ListChanged);
+		}
+		
+		public PanelCombo(Widget embedder) : this()
+		{
+			this.SetEmbedder(embedder);
+		}
+		
+		protected override void Dispose(bool disposing)
+		{
+			if ( disposing )
+			{
+				this.list.TextChanged -= new EventHandler(this.ListChanged);
+			}
+			
+			base.Dispose(disposing);
+		}
+
+
+		// Propriété -> widget.
+		public override void SetProperty(AbstractProperty property)
+		{
+			base.SetProperty(property);
+			this.label.Text = this.text;
+
+			PropertyCombo p = property as PropertyCombo;
+			if ( p == null )  return;
+
+			for ( int i=0 ; i<p.Count ; i++ )
+			{
+				this.list.Items.Add(p.Get(i));
+			}
+			this.list.SelectedIndex = p.Choice;
+		}
+
+		// Widget -> propriété.
+		public override AbstractProperty GetProperty()
+		{
+			PropertyCombo p = new PropertyCombo();
+			base.GetProperty(p);
+
+			p.Choice = this.list.SelectedIndex;
+			p.Clear();
+			for ( int i=0 ; i<this.list.Items.Count ; i++ )
+			{
+				p.Add(this.list.Items[i]);
+			}
+
+			return p;
+		}
+
+
+		// Met à jour la géométrie.
+		protected override void UpdateClientGeometry()
+		{
+			base.UpdateClientGeometry();
+
+			if ( this.list == null )  return;
+
+			Drawing.Rectangle rect = this.Client.Bounds;
+			rect.Left += this.extendedZoneWidth;
+			rect.Inflate(-5, -5);
+
+			Drawing.Rectangle r = rect;
+			r.Bottom = r.Top-20;
+			r.Right = rect.Right-100;
+			this.label.Bounds = r;
+
+			r = rect;
+			r.Left = r.Right-100;
+			this.list.Bounds = r;
+		}
+		
+		// Une valeur a été changée.
+		private void ListChanged(object sender)
+		{
+			this.OnChanged();
+		}
+
+
+		protected StaticText				label;
+		protected TextFieldCombo			list;
+	}
+}
