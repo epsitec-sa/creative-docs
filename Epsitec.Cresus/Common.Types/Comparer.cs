917812c8844a713f00eb164af5a1d4dd8484fc5e
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 16:06:48 +0000

    git-svn-id: https://svn.opac.ch/svn/cr@2329 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Types/Comparer.cs
+++ Epsitec.Cresus/Common.Types/Comparer.cs
@@ -1,5 +1,5 @@
 //	Copyright © 2003-2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
-//	Statut : en chantier/PA, 27/04/2004
+//	Responsable: Pierre ARNAUD
 
 namespace Epsitec.Common.Types
 {
--- Epsitec.Cresus/Common.Types/Converter.cs
+++ Epsitec.Cresus/Common.Types/Converter.cs
@@ -1,5 +1,5 @@
 //	Copyright © 2003-2004, EPSITEC SA, CH-1092 BELMONT, Switzerland
-//	Statut : en chantier/PA
+//	Responsable: Pierre ARNAUD
 
 namespace Epsitec.Common.Types
 {
