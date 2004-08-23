22c39d8a233f7162577899c2f605f13510d1cf3b
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:38:25 +0000

    ********************** Label: "22-01-2004" User: Arnaud       Date: 22.01.04   Time: 15:38 Labeled Label comment: Object re-bundling implemented.  ********************** Label: "21-01-2004" User: Arnaud       Date: 21.01.04   Time: 18:14 Labeled Label comment: XML DOM used by ResourceBundle. This allows serialisation of a bundle, not only deserialisation.   ********************** Label: "19-01-2004" User: Arnaud       Date: 19.01.04   Time: 16:24 Labeled Label comment: Before in-depth modification of XML handling in ResourceBundle.  ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.  ********************** Label: "12.12.2003" User: Arnaud       Date: 13.12.03   Time: 18:39 Labeled Label comment:   ********************** Label: "08-12-2003" User: Arnaud       Date:  8.12.03   Time: 15:25 Labeled Label comment: Stable version.  ********************** Label: "26-11-2003" User: Arnaud       Date: 26.11.03   Time: 16:28 Labeled Label comment: DbTable + DbType meta info now stored in database.   ********************** Label: "Stamp-12-11-2003" User: Arnaud       Date: 12.11.03   Time: 20:40 Labeled Label comment:
    git-svn-id: https://svn.opac.ch/svn/cr@974 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/Platform/Win32Const.cs
@@ -0,0 +1,78 @@
+namespace Epsitec.Common.Widgets
+{
+	/// <summary>
+	/// La classe Win32Const exporte des constantes liées à Windows.
+	/// </summary>
+	public class Win32Const
+	{
+		public const int WM_ACTIVATE		= 0x0006;
+		public const int WM_ACTIVATEAPP		= 0x001C;
+		public const int WM_MOUSEACTIVATE	= 0x0021;
+		public const int WM_NCACTIVATE		= 0x0086;
+		public const int WM_NCLBUTTONDOWN	= 0x00A1;
+		public const int WM_NCRBUTTONDOWN	= 0x00A4;
+		public const int WM_NCMBUTTONDOWN	= 0x00A7;
+		public const int WM_NCXBUTTONDOWN	= 0x00AB;
+		public const int WM_KEYDOWN			= 0x0100;
+		public const int WM_SYSKEYDOWN		= 0x0104;
+		public const int WM_KEYUP			= 0x0101;
+		public const int WM_SYSKEYUP		= 0x0105;
+		public const int WM_CHAR			= 0x0102;
+		public const int WM_SYSCHAR			= 0x0106;
+		public const int WM_DEADCHAR		= 0x0103;
+		public const int WM_SYSDEADCHAR		= 0x0107;
+		public const int WM_MOUSEMOVE		= 0x0200;
+		public const int WM_LBUTTONDOWN		= 0x0201;
+		public const int WM_LBUTTONUP		= 0x0202;
+		public const int WM_LBUTTONDBLCLK	= 0x0203;
+		public const int WM_RBUTTONDOWN		= 0x0204;
+		public const int WM_RBUTTONUP		= 0x0205;
+		public const int WM_RBUTTONDBLCLK	= 0x0206;
+		public const int WM_MBUTTONDOWN		= 0x0207;
+		public const int WM_MBUTTONUP		= 0x0208;
+		public const int WM_MBUTTONDBLCLK	= 0x0209;
+		public const int WM_MOUSEWHEEL		= 0x020A;
+		public const int WM_XBUTTONDOWN		= 0x020B;
+		public const int WM_XBUTTONUP		= 0x020C;
+		public const int WM_XBUTTONDBLCLK	= 0x020D;
+		public const int WM_MOUSELEAVE		= 0x02A3;
+		public const int WM_NCCALCSIZE		= 0x0083;
+		public const int WM_CHANGEUISTATE	= 0x0127;
+		
+		public const int WM_APP				= 0x8000;
+		public const int WM_APP_DISPOSE		= WM_APP + 1;
+		public const int WM_APP_EXEC_CMD	= WM_APP + 2;
+		
+		public const int VK_SHIFT			= 0x0010;
+		public const int VK_CONTROL			= 0x0011;
+		public const int VK_MENU			= 0x0012;
+		public const int VK_SPACE			= 0x0020;
+		
+		public const int MK_LBUTTON			= 0x01;
+		public const int MK_RBUTTON			= 0x02;
+		public const int MK_MBUTTON			= 0x10;
+		public const int MK_XBUTTON1		= 0x20;
+		public const int MK_XBUTTON2		= 0x40;
+
+		public const int GWL_EXSTYLE		= -20;
+		
+		public const int WS_EX_LAYERED		= 0x080000;
+		
+		public const int ULW_COLORKEY		= 0x00000001;
+		public const int ULW_ALPHA			= 0x00000002;
+		public const int ULW_OPAQUE			= 0x00000004;
+		
+		public const byte AC_SRC_OVER		= 0x00;
+		public const byte AC_SRC_ALPHA		= 0x01;
+		
+		public const int MA_ACTIVATE		= 1;
+		public const int MA_ACTIVATEANDEAT	= 2;
+		public const int MA_NOACTIVATE		= 3;
+		public const int MA_NOACTIVATEANDEAT= 4;
+		
+		public const int GW_HWNDFIRST		= 0;
+		public const int GW_HWNDLAST		= 1;
+		public const int GW_HWNDNEXT		= 2;
+		public const int GW_HWNDPREV		= 3;
+	}
+}
