8dd83f6d7fe98119b095b2ea3aba39d846510d65
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 13:06:57 +0000

    ********************** Label: "13-01-2004" User: Arnaud       Date: 13.01.04   Time: 20:50 Labeled Label comment: Stable, before rework of Layout management.
    git-svn-id: https://svn.opac.ch/svn/cr@659 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Widgets/Layouts/Abstract.cs
+++ Epsitec.Cresus/Common.Widgets/Layouts/Abstract.cs
@@ -148,7 +148,7 @@ namespace Epsitec.Common.Widgets.Layouts
 			this.panel.ChildrenChanged += new EventHandler (this.HandlePanelChildrenChanged);
 			this.panel.PreparePaint    += new EventHandler (this.HandlePanelPreparePaint);
 			
-			this.panel.SetPropagationModes (Widget.PropagationModes.UpChildrenChanged, true, true);
+			this.panel.SetPropagationModes (Widget.PropagationModes.UpChildrenChanged, true, Widget.PropagationSetting.IncludeChildren);
 		}
 		
 		protected void DetachPanel()
