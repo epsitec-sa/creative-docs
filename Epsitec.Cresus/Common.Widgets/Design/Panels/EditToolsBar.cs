067b3f99fe6f8e44f39d0b0a2a486e03e2826d6e
Pierre Arnaud <arnaud@epsitec.ch>
2004-08-23 14:18:21 +0000

    ********************** Label: "12-02-2004" User: Arnaud       Date: 13.02.04   Time:  9:42 Labeled Label comment: ScrollArray reworked.
    **********************
    Label: "11-02-2004"
    User: Arnaud       Date: 12.02.04   Time:  7:47
    Labeled
    Label comment: Stable version.
    
    git-svn-id: https://svn.opac.ch/svn/cr@1315 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Widgets/Design/Panels/EditToolsBar.cs
+++ Epsitec.Cresus/Common.Widgets/Design/Panels/EditToolsBar.cs
@@ -18,9 +18,9 @@ namespace Epsitec.Common.Widgets.Design.Panels
 			
 			tools.Size = this.Size;
 			
-			tools.Items.Add (new IconButton ("CreateNewWindow", "file:images/new1.icon"));
+			tools.Items.Add (new IconButton ("CreateNewWindow", "file:images/new.icon"));
 			tools.Items.Add (new IconSeparator ());
-			tools.Items.Add (new IconButton ("DeleteActiveSelection", "file:images/delete1.icon"));
+			tools.Items.Add (new IconButton ("DeleteActiveSelection", "file:images/delete.icon"));
 			
 			return tools;
 		}
