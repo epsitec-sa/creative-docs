a868019dc124f23e6a6a04f7d952e2d68e239a11
Pierre Arnaud <arnaud@opac.ch>
2004-10-07 08:54:34 +0000

    - Implémenté AssemblyLoader.Load de manière fonctionnelle.
    git-svn-id: https://svn.opac.ch/svn/cr@2776 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Support/AssemblyLoader.cs
+++ Epsitec.Cresus/Common.Support/AssemblyLoader.cs
@@ -10,10 +10,46 @@ namespace Epsitec.Common.Support
 	{
 		public static System.Reflection.Assembly Load(string name)
 		{
-			string file_name = string.Concat (name, ".dll");
-			string full_name = System.IO.Path.Combine (Globals.Directories.Executable, file_name);
+			//	Charge l'assembly spécifiée par son nom court, sans extension. Le fichier DLL va être
+			//	recherché dans le même dossier que "Common.Support" qui n'est pas nécessairement celui
+			//	de l'exécutable (en tout cas quand NUnit est utilisé pour les tests).
 			
-			return System.Reflection.Assembly.Load (full_name);
+			System.Reflection.Assembly current = System.Reflection.Assembly.GetAssembly (typeof (AssemblyLoader));
+			
+			string file_name = string.Concat (name, ".dll");
+			string path_name = Globals.Directories.Executable;
+			
+			string load_name = current.CodeBase;
+			
+			if (load_name.StartsWith ("file:///"))
+			{
+				load_name = load_name.Substring (8);
+				load_name = load_name.Replace ('/', System.IO.Path.DirectorySeparatorChar);
+				load_name = System.IO.Path.GetDirectoryName (load_name);
+				
+				if (path_name != load_name)
+				{
+					if (AssemblyLoader.load_path_differences++ == 0)
+					{
+						System.Diagnostics.Debug.WriteLine (string.Format ("Assembly load path different from executable path\n" +
+							/**/										   "  DLL load path: '{0}'\n" +
+							/**/										   "  EXE load path: '{1}'", load_name, path_name));
+					}
+					
+					path_name = load_name;
+				}
+			}
+			else
+			{
+				System.Diagnostics.Debug.WriteLine (string.Format ("Support assembly not loaded from a file ? CodeBase = '{0}'", load_name));
+			}
+			
+			string full_name = System.IO.Path.Combine (path_name, file_name);
+			
+			return System.Reflection.Assembly.LoadFrom (full_name);
 		}
+		
+		
+		private static int						load_path_differences = 0;
 	}
 }
