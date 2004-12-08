4eb7e421bd88ca33dfd298c59521a29635d2521f
Pierre Arnaud <arnaud@opac.ch>
2004-12-08 11:30:23 +0000

    - Ajouté méthodes PackColumnToArray et UnpackColumnFromArray dans Replication.DataCruncher.
    git-svn-id: https://svn.opac.ch/svn/cr@2892 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Database.Tests/ReplicationTest.cs
+++ Epsitec.Cresus/Cresus.Database.Tests/ReplicationTest.cs
@@ -54,10 +54,74 @@ namespace Epsitec.Cresus.Database
 						{
 							buffer.Append (", ");
 						}
-						buffer.Append (x.ToString ());
+						
+						if (x == System.DBNull.Value)
+						{
+							buffer.Append ("<DBNull>");
+						}
+						else
+						{
+							buffer.Append (x.ToString ());
+						}
 					}
 					
 					System.Console.WriteLine (buffer.ToString ());
+				}
+			}
+		}
+		
+		[Test] public void Check03DataCruncherPackColumnToArray()
+		{
+			using (DbTransaction transaction = this.infrastructure.BeginTransaction (DbTransactionMode.ReadOnly))
+			{
+				Replication.DataCruncher cruncher = new Replication.DataCruncher (this.infrastructure, transaction);
+				DbTable table = this.infrastructure.ResolveDbTable (transaction, Tags.TableTypeDef);
+				
+				System.Data.DataTable data = cruncher.ExtractData (table, DbId.CreateId (1, 1));
+				
+				for (int i = 0; i < data.Columns.Count; i++)
+				{
+					System.Array array;
+					bool[] null_array;
+					int    null_count;
+					
+					cruncher.PackColumnToArray (data, i, out array, out null_array, out null_count);
+					
+					System.Console.WriteLine ("Column {0}: type {1}, {2} rows, {3} are null.", i, array.GetType (), array.Length, null_count);
+				}			
+			}
+		}
+		
+		[Test] public void Check02DataCruncherUnpackColumnFromArray()
+		{
+			using (DbTransaction transaction = this.infrastructure.BeginTransaction (DbTransactionMode.ReadOnly))
+			{
+				Replication.DataCruncher cruncher = new Replication.DataCruncher (this.infrastructure, transaction);
+				DbTable table = this.infrastructure.ResolveDbTable (transaction, Tags.TableTypeDef);
+				
+				System.Data.DataTable data = cruncher.ExtractData (table, DbId.CreateId (1, 1));
+				
+				object[][] store = new object[data.Rows.Count][];
+				
+				for (int i = 0; i < data.Columns.Count; i++)
+				{
+					System.Array array;
+					bool[] null_array;
+					int    null_count;
+					
+					cruncher.PackColumnToArray (data, i, out array, out null_array, out null_count);
+					cruncher.UnpackColumnFromArray (store, i, data.Columns.Count, array, (null_count == 0) ? null : null_array);
+				}
+				
+				for (int r = 0; r < data.Rows.Count; r++)
+				{
+					for (int i = 0; i < data.Columns.Count; i++)
+					{
+						object a = data.Rows[r][i];
+						object b = store[r][i];
+						
+						Assert.AreEqual (a, b, string.Format ("Row {0}, Column {1} mismatch: {2}, {3}", r, i, a, b));
+					}
 				}
 			}
 		}
--- Epsitec.Cresus/Cresus.Replication/DataCruncher.cs
+++ Epsitec.Cresus/Cresus.Replication/DataCruncher.cs
@@ -16,6 +16,7 @@ namespace Epsitec.Cresus.Replication
 			this.transaction    = transaction;
 		}
 		
+		
 		public System.Data.DataTable ExtractData(DbTable table, DbId sync_id)
 		{
 			//	Extrait les données de la table spécifiée en ne considérant que ce qui a changé
@@ -35,6 +36,258 @@ namespace Epsitec.Cresus.Replication
 			using (DbRichCommand command = DbRichCommand.CreateFromTable (this.infrastructure, this.transaction, table, condition))
 			{
 				return command.DataSet.Tables[0];
+			}
+		}
+		
+		
+		public void PackColumnToArray(System.Data.DataTable table, int column, out System.Array array, out bool[] null_values, out int null_count)
+		{
+			//	Passe en revue une colonne de la table et remplit un tableau avec les
+			//	valeurs trouvées; le tableau utilise le type natif sous-jacent s'il fait
+			//	partie de la liste des types reconnus :
+			//	
+			//	- bool, short, int, long, decimal
+			//	- string, DateTime, byte[]
+			//
+			//	En plus, les lignes contenant DBNull sont marquées comme telles dans le
+			//	tableau null_values et comptées avec null_count.
+			
+			System.Type type = table.Columns[column].DataType;
+			
+			int n = table.Rows.Count;
+			
+			null_values = new bool[n];
+			null_count  = 0;
+			
+			if (type == typeof (bool))
+			{
+				bool[] data = new bool[n];
+				
+				for (int i = 0; i < n; i++)
+				{
+					if (table.Rows[i][column] == System.DBNull.Value)
+					{
+						null_values[i] = true;
+						data[i]        = false;
+						null_count++;
+					}
+					else
+					{
+						null_values[i] = false;
+						data[i]        = (bool) table.Rows[i][column];
+					}
+				}
+				
+				array = data;
+			}
+			else if (type == typeof (short))
+			{
+				short[] data = new short[n];
+				
+				for (int i = 0; i < n; i++)
+				{
+					if (table.Rows[i][column] == System.DBNull.Value)
+					{
+						null_values[i] = true;
+						data[i]        = 0;
+						null_count++;
+					}
+					else
+					{
+						null_values[i] = false;
+						data[i]        = (short) table.Rows[i][column];
+					}
+				}
+				
+				array = data;
+			}
+			else if (type == typeof (int))
+			{
+				int[] data = new int[n];
+				
+				for (int i = 0; i < n; i++)
+				{
+					if (table.Rows[i][column] == System.DBNull.Value)
+					{
+						null_values[i] = true;
+						data[i]        = 0;
+						null_count++;
+					}
+					else
+					{
+						null_values[i] = false;
+						data[i]        = (int) table.Rows[i][column];
+					}
+				}
+				
+				array = data;
+			}
+			else if (type == typeof (long))
+			{
+				long[] data = new long[n];
+				
+				for (int i = 0; i < n; i++)
+				{
+					if (table.Rows[i][column] == System.DBNull.Value)
+					{
+						null_values[i] = true;
+						data[i]        = 0;
+						null_count++;
+					}
+					else
+					{
+						null_values[i] = false;
+						data[i]        = (long) table.Rows[i][column];
+					}
+				}
+				
+				array = data;
+			}
+			else if (type == typeof (decimal))
+			{
+				decimal[] data = new decimal[n];
+				
+				for (int i = 0; i < n; i++)
+				{
+					if (table.Rows[i][column] == System.DBNull.Value)
+					{
+						null_values[i] = true;
+						data[i]        = 0;
+						null_count++;
+					}
+					else
+					{
+						null_values[i] = false;
+						data[i]        = (decimal) table.Rows[i][column];
+					}
+				}
+				
+				array = data;
+			}
+			else if (type == typeof (string))
+			{
+				string[] data = new string[n];
+				
+				for (int i = 0; i < n; i++)
+				{
+					if (table.Rows[i][column] == System.DBNull.Value)
+					{
+						null_values[i] = true;
+						data[i]        = null;
+						null_count++;
+					}
+					else
+					{
+						null_values[i] = false;
+						data[i]        = (string) table.Rows[i][column];
+					}
+				}
+				
+				array = data;
+			}
+			else if (type == typeof (System.DateTime))
+			{
+				System.DateTime[] data = new System.DateTime[n];
+				System.DateTime   zero = new System.DateTime (0);
+				
+				for (int i = 0; i < n; i++)
+				{
+					if (table.Rows[i][column] == System.DBNull.Value)
+					{
+						null_values[i] = true;
+						data[i]        = zero;
+						null_count++;
+					}
+					else
+					{
+						null_values[i] = false;
+						data[i]        = (System.DateTime) table.Rows[i][column];
+					}
+				}
+				
+				array = data;
+			}
+			else if (type == typeof (byte[]))
+			{
+				byte[][] data = new byte[n][];
+				
+				for (int i = 0; i < n; i++)
+				{
+					if (table.Rows[i][column] == System.DBNull.Value)
+					{
+						null_values[i] = true;
+						data[i]        = null;
+						null_count++;
+					}
+					else
+					{
+						null_values[i] = false;
+						data[i]        = (byte[]) table.Rows[i][column];
+					}
+				}
+				
+				array = data;
+			}
+			else
+			{
+				object[] data = new object[n];
+				
+				for (int i = 0; i < n; i++)
+				{
+					if (table.Rows[i][column] == System.DBNull.Value)
+					{
+						null_values[i] = true;
+						data[i]        = null;
+						null_count++;
+					}
+					else
+					{
+						null_values[i] = false;
+						data[i]        = table.Rows[i][column];
+					}
+				}
+				
+				array = data;
+			}
+		}
+		
+		public void UnpackColumnFromArray(object[][] table, int column, int column_count, System.Array array, bool[] null_values)
+		{
+			System.Diagnostics.Debug.Assert (table.Length == array.Length);
+			
+			int n = table.Length;
+			
+			if ((null_values != null) &&
+				(null_values.Length > 0))
+			{
+				for (int i = 0; i < n; i++)
+				{
+					if (table[i] == null)
+					{
+						table[i] = new object[column_count];
+					}
+					
+					if (null_values[i])
+					{
+						table[i][column] = System.DBNull.Value;
+					}
+					else
+					{
+						table[i][column] = array.GetValue (i);
+					}
+				}
+			}
+			else
+			{
+				for (int i = 0; i < n; i++)
+				{
+					if (table[i] == null)
+					{
+						table[i] = new object[column_count];
+					}
+					
+					table[i][column] = array.GetValue (i);
+				}
 			}
 		}
 		
