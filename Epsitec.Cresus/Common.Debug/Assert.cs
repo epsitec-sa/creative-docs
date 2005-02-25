fa33b84d33133e65d9f5061ec5c47222305c55a3
Pierre Arnaud <arnaud@opac.ch>
2005-02-25 15:55:49 +0000

    - Ajouté supporté pour Assert avec ulong.
    git-svn-id: https://svn.opac.ch/svn/cr@3052 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Debug/Assert.cs
+++ Epsitec.Cresus/Common.Debug/Assert.cs
@@ -35,6 +35,14 @@ namespace Epsitec.Common.Debug
 			}
 		}
 		
+		public static void IsInBounds(ulong value, ulong min, ulong max)
+		{
+			if ((value < min) || (value > max))
+			{
+				throw new AssertFailedException (string.Format ("Value {0} out of bounds [{1}..{2}].", value, min, max));
+			}
+		}
+		
 		
 		public static void IsTrue(bool condition)
 		{
