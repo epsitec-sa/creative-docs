03bb1fbf9558752adb8bbafcc728f84575eb5d31
Pierre Arnaud <arnaud@opac.ch>
2005-11-29 06:27:20 +0000

    - Ajouté ApplicationCommands (préliminaire).
    git-svn-id: https://svn.opac.ch/svn/cr@3747 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/ApplicationCommands.cs
@@ -0,0 +1,41 @@
+//	Copyright © 2005, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+namespace Epsitec.Common.Widgets
+{
+	/// <summary>
+	/// La classe statique ApplicationCommands exporte les noms de commandes les
+	/// plus utilisées.
+	/// </summary>
+	public class ApplicationCommands
+	{
+		private ApplicationCommands()
+		{
+		}
+		
+		
+		//	Référence: http://winfx.msdn.microsoft.com/library/default.asp?url=/library/en-us/cpref/html/T_System_Windows_Input_ApplicationCommands_Members.asp
+		
+		public static readonly string	Close			= "Close";
+		public static readonly string	ContextMenu		= "ContextMenu";
+		public static readonly string	Copy			= "Copy";
+		public static readonly string	CorrectionList	= "CorrectionList";
+		public static readonly string	Cut				= "Cut";
+		public static readonly string	Delete			= "Delete";
+		public static readonly string	Find			= "Find";
+		public static readonly string	Help			= "Help";
+		public static readonly string	New				= "New";
+		public static readonly string	Open			= "Open";
+		public static readonly string	Paste			= "Paste";
+		public static readonly string	Print			= "Print";
+		public static readonly string	PrintPreview	= "PrintPreview";
+		public static readonly string	Properties		= "Properties";
+		public static readonly string	Redo			= "Redo";
+		public static readonly string	Replace			= "Replace";
+		public static readonly string	Save			= "Save";
+		public static readonly string	SaveAs			= "SaveAs";
+		public static readonly string	SelectAll		= "SelectAll";
+		public static readonly string	Stop			= "Stop";
+		public static readonly string	Undo			= "Undo";
+	}
+}
