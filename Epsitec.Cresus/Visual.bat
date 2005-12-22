b6e072feb8d0c6614e7b8ed346dcb58aa26f0ef6
Pierre Arnaud <arnaud@opac.ch>
2005-12-22 14:57:25 +0000

    - Ajouté des fichiers manquants dans SVN.
    git-svn-id: https://svn.opac.ch/svn/cr@4068 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Visual.bat
@@ -0,0 +1,5 @@
+
+subst S: D:\Cresus > NUL:
+
+cd "C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\"
+start "DevStudio" /B "C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.exe" s:\Epsitec.Cresus\Epsitec.Cresus.sln
