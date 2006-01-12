a433cad0b077b33a81a75dcc0e45617bca50bc38
Pierre Arnaud <arnaud@opac.ch>
2006-01-12 17:06:12 +0000

    - Passé en version 1.6.0 les AssemblyInfo.
    git-svn-id: https://svn.opac.ch/svn/cr@4241 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/App.CreativeDocs/AssemblyInfo.cs
+++ Epsitec.Cresus/App.CreativeDocs/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
-[assembly: AssemblyVersion("1.5.3.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
 
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile(@"..\..\..\Epsitec.Cresus.snk")]
