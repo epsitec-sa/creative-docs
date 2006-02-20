e03104e13db0365ace7c63f7fbad2b55ac687f38
Pierre Arnaud <arnaud@opac.ch>
2006-02-20 17:25:20 +0000

    - Ajouté Common.Support.Implementation comme référence à App.CreativeDocs et App.CresusDocuments, puisqu'il semble qu'il y a des problèmes avec VS.NET 2005 et son calcul des dépendances. - Ajouté ObjectTreeSnapshot. - Ajouté support préliminaire pour des "attached properties" gérées autrement.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@4567 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/ObjectTreeSnapshot.cs
@@ -0,0 +1,103 @@
+//	Copyright © 2005-2006, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+using System.Collections.Generic;
+
+namespace Epsitec.Common.Types
+{
+	/// <summary>
+	/// The ObjectTreeSnapshot is used to compute differences between sets of
+	/// object properties.
+	/// </summary>
+	public sealed class ObjectTreeSnapshot
+	{
+		public ObjectTreeSnapshot()
+		{
+		}
+
+		public void Record(Object obj)
+		{
+			//	Record the state of all inherited properties.
+
+			System.Type type = obj.GetType ();
+
+			foreach (Types.Property property in obj.ObjectType.GetProperties ())
+			{
+				PropertyMetadata metadata = property.GetMetadata (type);
+
+				if ((metadata != null) &&
+					(metadata.InheritsValue))
+				{
+					this.list.Add (new SnapshotValue (obj, property));
+				}
+			}
+		}
+
+		public void Record(Object obj, Types.Property property)
+		{
+			System.Diagnostics.Debug.Assert (property == null);
+			
+			this.list.Add (new SnapshotValue (obj, property));
+		}
+
+		public void InvalidateDifferent()
+		{
+			foreach (SnapshotValue snapshot in this.list)
+			{
+				object oldValue = snapshot.Value;
+				object newValue = snapshot.Object.GetValue (snapshot.Property);
+				
+				if (oldValue == newValue)
+				{
+					//	Nothing changed, skip.
+				}
+				else if ((oldValue == null) ||
+					/**/ (! oldValue.Equals (newValue)))
+				{
+					snapshot.Object.InvalidateProperty (snapshot.Property, oldValue, newValue);
+				}
+			}
+		}
+		
+		
+		#region Private SnapshotValue Structure
+		private struct SnapshotValue
+		{
+			public SnapshotValue(Object obj, Types.Property property)
+			{
+				this.obj      = obj;
+				this.property = property;
+				this.value    = obj.GetValue (property);
+			}
+			
+			public Object						Object
+			{
+				get
+				{
+					return this.obj;
+				}
+			}
+			public Types.Property				Property
+			{
+				get
+				{
+					return this.property;
+				}
+			}
+			public object						Value
+			{
+				get
+				{
+					return this.value;
+				}
+			}
+
+			private Object						obj;
+			private Types.Property				property;
+			private object						value;
+		}
+		#endregion
+
+		private List<SnapshotValue>				list = new List<SnapshotValue> ();
+	}
+}
