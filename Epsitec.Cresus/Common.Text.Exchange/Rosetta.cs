1efd107cd01d5a221086143256b45d1d29dc597e
Michael Walz <walz@epsitec.ch>
2006-03-02 17:19:36 +0000

    ça commence à rentrer. Et en plus maintenant la compilation pendant l'édition fonctionne !! Donc je peux faire des corrections qui sont compilées et appliquées "on the fly" *pendant* le debug !! C'est la première fois que ça fonctionne, je ne sais pas trop ce que j'ai fait pour que ça marche !!
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@4624 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Text.Exchange/Rosetta.cs
+++ Epsitec.Cresus/Common.Text.Exchange/Rosetta.cs
@@ -20,11 +20,162 @@ namespace Epsitec.Common.Text.Exchange
 			return "TODO";
 		}
 
+		public static void TestCode1(TextStory story, TextNavigator navigator)
+		{
+			Wrappers.TextWrapper textWrapper = new Wrappers.TextWrapper ();
+			Wrappers.ParagraphWrapper paraWrapper = new Wrappers.ParagraphWrapper ();
+
+			System.Windows.Forms.Clipboard clipboard;
+			System.Text.StringBuilder output = new System.Text.StringBuilder ();
+
+			while (true)
+			{
+				int runLength = navigator.GetRunLength (1000000);
+
+				if (runLength == 0)
+				{
+					break;
+				}
+
+				string runText       = navigator.ReadText (runLength);
+
+				navigator.MoveTo (TextNavigator.Target.CharacterNext, runLength);
+
+				Property[] runProperties = navigator.AccumulatedTextProperties;
+				TextStyle[] runStyles     = navigator.TextStyles;
+
+				System.Console.Out.WriteLine ("Run: >>{0}<< with {1} properties", runText, runProperties.Length);
+
+				output.AppendFormat ("Run: \"{0}\"\r\n", runText);
+
+				string text;
+
+				foreach (Property p in runProperties)
+				{
+
+					text = p.ToString ();
+
+					output.AppendFormat ("  {0}  {1}\r\n", p.WellKnownType, text);
+
+					if (p.WellKnownType == Properties.WellKnownType.Font)
+					{
+						Properties.FontProperty fontProperty = p as Properties.FontProperty;
+
+						string fontFace = fontProperty.FaceName;
+						string fontStyle = fontProperty.StyleName;
+
+						fontStyle = OpenType.FontCollection.GetStyleHash (fontStyle);
+
+						System.Console.Out.WriteLine ("- Font: {0} {1}", fontFace, fontStyle);
+					}
+					else
+					{
+						//-						System.Console.Out.WriteLine ("- {0}", p.WellKnownType);
+					}
+				}
+
+				//	La façon "haut niveau" de faire :
+
+				if (textWrapper.Defined.IsFontFaceDefined)
+				{
+					System.Console.Out.WriteLine ("- Font Face: {0}", textWrapper.Defined.FontFace, textWrapper.Defined.FontStyle, textWrapper.Defined.InvertItalic ? "(italic)" : "");
+				}
+				if (textWrapper.Defined.IsFontStyleDefined)
+				{
+					System.Console.Out.WriteLine ("- Font Style: {0}", textWrapper.Defined.FontStyle);
+				}
+				if (textWrapper.Defined.IsInvertItalicDefined)
+				{
+					System.Console.Out.WriteLine ("- Invert Italic: {0}", textWrapper.Defined.InvertItalic);
+				}
+				if (textWrapper.Defined.IsInvertBoldDefined)
+				{
+					System.Console.Out.WriteLine ("- Invert Bold: {0}", textWrapper.Defined.InvertBold);
+				}
+			}
+
+			System.Windows.Forms.Clipboard.SetData (System.Windows.Forms.DataFormats.Text, output);
+		
+			System.Diagnostics.Debug.WriteLine ("Code de test 1 appelé.");
+		}
+
+
 		public static void TestCode(TextStory story, TextNavigator navigator)
 		{
-			//	TODO: ajouter le code de test; appelé par Crésus Documents
-			
-			System.Diagnostics.Debug.WriteLine ("Code de test appelé.");
+			Wrappers.TextWrapper textWrapper = new Wrappers.TextWrapper ();
+			Wrappers.ParagraphWrapper paraWrapper = new Wrappers.ParagraphWrapper ();
+
+			System.Text.StringBuilder output = new System.Text.StringBuilder ();
+			string[] opentag = new string[10];
+			string[] closetag = new string[10];
+			int tagindex = 0;
+
+			textWrapper.Attach (navigator);
+			paraWrapper.Attach (navigator);
+
+			System.Windows.Forms.Clipboard clipboard;
+
+			navigator.MoveTo (0,0);
+
+			while (true)
+			{
+				int runLength = navigator.GetRunLength (1000000);
+
+				if (runLength == 0)
+				{
+					break;
+				}
+				
+				if (textWrapper.Defined.IsFontFaceDefined)
+				{
+					System.Console.Out.WriteLine ("- Font Face: {0}", textWrapper.Defined.FontFace, textWrapper.Defined.FontStyle, textWrapper.Defined.InvertItalic ? "(italic)" : "");
+				}
+				if (textWrapper.Defined.IsFontStyleDefined)
+				{
+					System.Console.Out.WriteLine ("- Font Style: {0}", textWrapper.Defined.FontStyle);
+				}
+				if (textWrapper.Defined.IsInvertItalicDefined)
+				{
+					if (textWrapper.Defined.InvertItalic)
+					{
+						opentag[tagindex] = "<i>";
+						closetag[tagindex] += "</i>";
+						tagindex++;
+					}
+				}
+				if (textWrapper.Defined.IsInvertBoldDefined)
+				{
+					if (textWrapper.Defined.InvertBold)
+					{
+						opentag[tagindex] = "<b>";
+						closetag[tagindex] += "</b>";
+						tagindex++;
+					}
+				}
+
+				int i;
+
+				for (i = 0; i < tagindex; i++)
+				{
+					output.Append(opentag[i]) ;
+				}
+
+				output.Append(navigator.ReadText (runLength) );
+				
+				for (i = tagindex-1; i >= tagindex; i--)
+				{
+					output.Append(opentag[i]) ;
+				}
+
+
+				navigator.MoveTo (Epsitec.Common.Text.TextNavigator.Target.CharacterNext , runLength);
+				navigator.MoveTo (Epsitec.Common.Text.TextNavigator.Target.CharacterNext, 1);
+				navigator.MoveTo (Epsitec.Common.Text.TextNavigator.Target.CharacterPrevious, 1);
+
+			}
+
+			System.Windows.Forms.Clipboard.SetData (System.Windows.Forms.DataFormats.Text, output);
+			System.Diagnostics.Debug.WriteLine ("Code de test 1 appelé.");
 		}
 	}
 }
