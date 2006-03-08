3f259f6559e728f71ecdd454b5da84e16b83171c
Pierre Arnaud <arnaud@opac.ch>
2006-03-08 08:56:58 +0000

    - Corrigé deux problèmes dans Document: HandleStyleListStyleRedefined était attaché sur le document temporaire de désérialisation et NotifyStyleChanged n'était pas appelé en cas d'édition de style de texte.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@4656 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Document/Document.cs
+++ Epsitec.Cresus/Common.Document/Document.cs
@@ -713,6 +713,12 @@ namespace Epsitec.Common.Document
 			this.uniqueParagraphStyleId = doc.uniqueParagraphStyleId;
 			this.uniqueCharacterStyleId = doc.uniqueCharacterStyleId;
 			
+			if ( this.textContext != null )
+			{
+				this.textContext.StyleList.StyleRedefined += new Support.EventHandler(this.HandleStyleListStyleRedefined);
+				this.textContext.StyleList.StyleRedefined -= new Support.EventHandler(doc.HandleStyleListStyleRedefined);
+			}
+			
 			if ( this.type == DocumentType.Pictogram )
 			{
 				this.size = doc.size;
