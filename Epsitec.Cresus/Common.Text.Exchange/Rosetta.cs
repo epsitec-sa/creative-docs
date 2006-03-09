d7c43f7a61a41867ad4d2b26e69e63aba3fa1347
Michael Walz <walz@epsitec.ch>
2006-03-09 13:31:32 +0000

    - code de test nettoyé, mais ne fonctionne toujours pas
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@4682 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Text.Exchange/Rosetta.cs
+++ Epsitec.Cresus/Common.Text.Exchange/Rosetta.cs
@@ -1,6 +1,8 @@
 //	Copyright © 2006, EPSITEC SA, CH-1092 BELMONT, Switzerland
 //	Responsable: Michael WALZ
 
+using NUnit.Framework;
+
 namespace Epsitec.Common.Text.Exchange
 {
 
@@ -24,7 +26,123 @@ namespace Epsitec.Common.Text.Exchange
 
     public class HtmlText
     {
-        enum htmlattribute { bold, italic };
+		public HtmlText()
+		{
+		}
+
+		public override string ToString()
+		{
+			return output.ToString ();
+		}
+
+		// ferme les tags ouverts avant de fermer
+		// un tag
+		private void CloseTag(HtmlAttribute closeattribute)
+		{
+			while (this.openTags.Count > 0)
+			{
+				HtmlAttribute attribute = (HtmlAttribute)this.openTags.Pop ();
+
+				if (attribute != closeattribute)
+				{
+					this.output.Append (this.AttributeToString(attribute, HtmlTagMode.Close , null));
+				}
+			}
+		}
+
+		public void AppendText(string thestring)
+		{
+			if (precedIsItalic && !isItalic)
+			{
+				this.CloseTag (HtmlAttribute.Italic);
+				output.Append ("</i>");
+			}
+
+			if (!precedIsItalic && isItalic)
+			{
+				this.output.Append ("<i>");
+				this.openTags.Push (HtmlAttribute.Italic);
+				precedIsItalic = true;
+			}
+
+			if (precedIsBold && !isBold)
+			{
+				this.CloseTag (HtmlAttribute.Bold);
+				this.output.Append ("</b>");
+			}
+
+			if (!precedIsBold && isBold)
+			{
+				this.openTags.Push (HtmlAttribute.Bold);
+				output.Append ("<b>");
+				precedIsBold = true;
+			}
+
+			precedIsItalic = isItalic;
+			precedIsBold = isBold;
+
+			this.output.Append (thestring);
+		}
+
+		public void SetItalic(bool italic)
+		{
+			this.isItalic = italic;
+		}
+
+		public void SetBold(bool bold)
+		{
+			this.isBold = bold;
+		}
+
+		public void SetFont(string fontName, int fontSize)
+		{
+		}
+
+		private string GetHtmlTag(string attribute, HtmlTagMode tagMode)
+		{
+			string retval = "" ;
+			switch (tagMode)
+			{
+				case HtmlTagMode.Open:
+					retval = "<" + attribute + ">";
+					break;
+				case HtmlTagMode.Close:
+					retval = "</" + attribute + ">";
+					break;
+				case HtmlTagMode.StartOnly:
+					break;
+			}
+			return retval ;
+		}
+
+		private string AttributeToString(HtmlAttribute attribute, HtmlTagMode tagmode, string parameter)
+		{
+			string retval = "" ;
+
+			switch (attribute)
+			{
+				case HtmlAttribute.Italic:
+					retval = this.GetHtmlTag ("i", tagmode);
+					break;
+				case HtmlAttribute.Bold:
+					retval = this.GetHtmlTag ("b", tagmode);
+					break;
+			}
+
+			return retval;
+		}
+
+		enum HtmlAttribute
+		{
+			Bold,
+			Italic,
+			Font
+		};
+
+		enum HtmlTagMode
+		{
+			Open, Close, StartOnly
+		} ;
 
         private bool isItalic = false ;
         private bool isBold = false;
@@ -32,62 +150,13 @@ namespace Epsitec.Common.Text.Exchange
         private bool precedIsItalic = false;
         private bool precedIsBold = false;
         
-        enum Days {Sat, Sun, Mon, Tue, Wed, Thu, Fri};
-
-        htmlattribute lastAttributeSet;
+        private HtmlAttribute lastAttributeSet;
 
         private System.Collections.ArrayList runList;
 
-        System.Text.StringBuilder output = new System.Text.StringBuilder ();
+        private System.Text.StringBuilder output = new System.Text.StringBuilder ();
 
-        public HtmlText()
-        {
-        }
-
-        public override string ToString()
-        {
-            return output.ToString();
-        }
-
-        public void AppendText(string thestring)
-        {
-            if (precedIsItalic && !isItalic)
-                output.Append("</i>");
-
-            if (!precedIsItalic && isItalic)
-            {
-                output.Append("<i>");
-                precedIsItalic = true;
-            }
-
-            if (precedIsBold && !isBold)
-                output.Append("</b>");
-
-            if (!precedIsBold && isBold)
-            {
-                output.Append("<b>");
-                precedIsBold = true;
-            }
-
-            precedIsItalic = isItalic;
-            precedIsBold = isBold;
-
-            output.Append(thestring);
-        }
-
-        public void SetItalic(bool italic)
-        {
-            this.isItalic = italic;
-        }
-
-        public void SetBold(bool bold)
-        {
-            this.isBold = bold;
-        }
-
-        public void SetFont(string fontName, int fontSize)
-        {
-        }
+		System.Collections.Stack openTags = new System.Collections.Stack();
 
     }
 
@@ -108,6 +177,73 @@ namespace Epsitec.Common.Text.Exchange
 			return "TODO";
 		}
 
+		public static void TestCode(TextStory story, TextNavigator navigator)
+		{
+			Wrappers.TextWrapper textWrapper = new Wrappers.TextWrapper ();
+			Wrappers.ParagraphWrapper paraWrapper = new Wrappers.ParagraphWrapper ();
+
+			System.Text.StringBuilder output = new System.Text.StringBuilder ();
+			string[] opentag = new string[10];
+			string[] closetag = new string[10];
+			int tagindex ;
+
+			textWrapper.Attach (navigator);
+			paraWrapper.Attach (navigator);
+
+			System.Windows.Forms.Clipboard clipboard;
+
+            HtmlText htmlText = new HtmlText() ;
+
+			navigator.MoveTo (0,0);
+
+			while (true)
+			{
+				int runLength = navigator.GetRunLength (1000000);
+				tagindex = 0;
+
+				if (runLength == 0)
+				{
+					break;
+				}
+				
+				if (textWrapper.Defined.IsFontFaceDefined)
+				{
+					System.Console.Out.WriteLine ("- Font Face: {0}", textWrapper.Defined.FontFace, textWrapper.Defined.FontStyle, textWrapper.Defined.InvertItalic ? "(italic)" : "");
+				}
+				if (textWrapper.Defined.IsFontStyleDefined)
+				{
+					System.Console.Out.WriteLine ("- Font Style: {0}", textWrapper.Defined.FontStyle);
+				}
+
+                htmlText.SetItalic(textWrapper.Defined.IsInvertItalicDefined && textWrapper.Defined.InvertItalic) ;
+                htmlText.SetBold(textWrapper.Defined.IsInvertBoldDefined && textWrapper.Defined.InvertBold) ;
+
+				int i;
+
+				//output.Append(navigator.ReadText (runLength) );
+                htmlText.AppendText(navigator.ReadText(runLength));
+
+				// avance au run suivant
+				navigator.MoveTo (Epsitec.Common.Text.TextNavigator.Target.CharacterNext , runLength);
+
+                // avance encore d'un seul caractère afin de se trouver véritablement dans
+                // le contexte su run
+				navigator.MoveTo (Epsitec.Common.Text.TextNavigator.Target.CharacterNext, 1);
+
+				if (navigator.GetRunLength (1000000) == 0)
+					break; // arrête si on est à la fin
+
+                // recule au début du run
+				navigator.MoveTo (Epsitec.Common.Text.TextNavigator.Target.CharacterPrevious, 1);
+
+			}
+
+			System.Windows.Forms.Clipboard.SetData (System.Windows.Forms.DataFormats.Text, htmlText);
+			System.Diagnostics.Debug.WriteLine ("Code de test 1 appelé.");
+		}
+
+
+		#region TestCode1
 		public static void TestCode1(TextStory story, TextNavigator navigator)
 		{
 			Wrappers.TextWrapper textWrapper = new Wrappers.TextWrapper ();
@@ -183,85 +319,10 @@ namespace Epsitec.Common.Text.Exchange
 			}
 
 			System.Windows.Forms.Clipboard.SetData (System.Windows.Forms.DataFormats.Text, output);
-		
+
 			System.Diagnostics.Debug.WriteLine ("Code de test 1 appelé.");
 		}
+		#endregion
 
-
-		public static void TestCode(TextStory story, TextNavigator navigator)
-		{
-			Wrappers.TextWrapper textWrapper = new Wrappers.TextWrapper ();
-			Wrappers.ParagraphWrapper paraWrapper = new Wrappers.ParagraphWrapper ();
-
-			System.Text.StringBuilder output = new System.Text.StringBuilder ();
-			string[] opentag = new string[10];
-			string[] closetag = new string[10];
-			int tagindex ;
-
-			textWrapper.Attach (navigator);
-			paraWrapper.Attach (navigator);
-
-			System.Windows.Forms.Clipboard clipboard;
-
-            HtmlText htmlText = new HtmlText() ;
-
-			navigator.MoveTo (0,0);
-
-			while (true)
-			{
-				int runLength = navigator.GetRunLength (1000000);
-				tagindex = 0;
-
-				if (runLength == 0)
-				{
-					break;
-				}
-				
-				if (textWrapper.Defined.IsFontFaceDefined)
-				{
-					System.Console.Out.WriteLine ("- Font Face: {0}", textWrapper.Defined.FontFace, textWrapper.Defined.FontStyle, textWrapper.Defined.InvertItalic ? "(italic)" : "");
-				}
-				if (textWrapper.Defined.IsFontStyleDefined)
-				{
-					System.Console.Out.WriteLine ("- Font Style: {0}", textWrapper.Defined.FontStyle);
-				}
-
-                htmlText.SetItalic(textWrapper.Defined.IsInvertItalicDefined && textWrapper.Defined.InvertItalic) ;
-                htmlText.SetBold(textWrapper.Defined.IsInvertBoldDefined && textWrapper.Defined.InvertBold) ;
-
-				int i;
-
-				for (i = 0; i < tagindex; i++)
-				{
-// 					output.Append(opentag[i]) ;
-				}
-
-				//output.Append(navigator.ReadText (runLength) );
-                htmlText.AppendText(navigator.ReadText(runLength));
-				
-				for (i = tagindex-1; i >= 0; i--)
-				{
-					// output.Append(closetag[i]) ;
-				}
-
-
-                // avance au run suivant
-				navigator.MoveTo (Epsitec.Common.Text.TextNavigator.Target.CharacterNext , runLength);
-
-                // avance encore d'un seul caractère afin de se trouver véritablement dans
-                // le contexte su run
-				navigator.MoveTo (Epsitec.Common.Text.TextNavigator.Target.CharacterNext, 1);
-
-				if (navigator.GetRunLength (1000000) == 0)
-					break; // arrête si on est à la fin
-
-                // recule au début du run
-				navigator.MoveTo (Epsitec.Common.Text.TextNavigator.Target.CharacterPrevious, 1);
-
-			}
-
-			System.Windows.Forms.Clipboard.SetData (System.Windows.Forms.DataFormats.Text, htmlText);
-			System.Diagnostics.Debug.WriteLine ("Code de test 1 appelé.");
-		}
 	}
 }
