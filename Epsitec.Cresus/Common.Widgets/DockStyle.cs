3a893177e77f3eebe563f6748ec99500196ae013
Pierre Arnaud <arnaud@opac.ch>
2006-04-02 17:34:35 +0000

    - Ajouté des fichiers sources manquants.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005-visual-march-2006@4893 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/DockStyle.cs
@@ -0,0 +1,16 @@
+//	Copyright © 2006, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+namespace Epsitec.Common.Widgets
+{
+	public enum DockStyle
+	{
+		None	= 0,
+
+		Top		= 1,				//	colle en haut
+		Bottom	= 2,				//	colle en bas
+		Left	= 3,				//	colle à gauche
+		Right	= 4,				//	colle à droite
+		Fill	= 5,				//	remplit tout
+	}
+}
