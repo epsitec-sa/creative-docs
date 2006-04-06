d0b8458e2bb272d551e0a420ed44c828fe866a78
Pierre Arnaud <arnaud@opac.ch>
2006-04-06 20:32:30 +0000

    - Ajouté ressources pour le designer.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@4941 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/MainWindow.cs
+++ Epsitec.Cresus/Common.Designer/MainWindow.cs
@@ -9,46 +9,51 @@ namespace Epsitec.Common.Designer
 	/// </summary>
 	public class MainWindow
 	{
+		static MainWindow()
+		{
+			Res.Initialise (typeof (MainWindow), "Designer");
+		}
+		
 		public void Show()
 		{
 			//	Crée et montre la fenêtre de l'éditeur.
-			if ( this.window == null )
+			if (this.window == null)
 			{
-				this.window = new Window();
-				this.window.MakeFixedSizeWindow();
-				this.window.MakeSecondaryWindow();
-				this.window.WindowSize = new Size(400, 400);
+				this.window = new Window ();
+				this.window.MakeFixedSizeWindow ();
+				this.window.MakeSecondaryWindow ();
+				this.window.WindowSize = new Size (400, 400);
 				this.window.Text = "Ressources Editor";
 				this.window.PreventAutoClose = true;
-				this.window.WindowCloseClicked += new EventHandler(this.HandleWindowAboutCloseClicked);
+				this.window.WindowCloseClicked += new EventHandler (this.HandleWindowAboutCloseClicked);
 
 				//	Bouton de fermeture.
-				Button buttonClose = new Button(this.window.Root);
+				Button buttonClose = new Button (this.window.Root);
 				buttonClose.Width = 75;
 				buttonClose.Text = "Close";
 				buttonClose.ButtonStyle = ButtonStyle.DefaultAccept;
 				buttonClose.Anchor = AnchorStyles.BottomLeft;
-				buttonClose.AnchorMargins = new Margins(10, 0, 0, 10);
-				buttonClose.Clicked += new MessageEventHandler(this.HandleAboutButtonCloseClicked);
+				buttonClose.AnchorMargins = new Margins (10, 0, 0, 10);
+				buttonClose.Clicked += new MessageEventHandler (this.HandleAboutButtonCloseClicked);
 				buttonClose.TabIndex = 1000;
 				buttonClose.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
 			}
 
-			this.window.ShowDialog();
+			this.window.ShowDialog ();
 		}
 
 
 		private void HandleWindowAboutCloseClicked(object sender)
 		{
-			this.window.Hide();
+			this.window.Hide ();
 		}
 
 		private void HandleAboutButtonCloseClicked(object sender, MessageEventArgs e)
 		{
-			this.window.Hide();
+			this.window.Hide ();
 		}
 
 
-		protected Window						window;
+		protected Window window;
 	}
 }
