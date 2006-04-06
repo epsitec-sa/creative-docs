ac83f3671ae93d23a9d8a27a6172d186145ecaa6
Pierre Arnaud <arnaud@opac.ch>
2006-04-06 07:36:59 +0000

    - Ajouté le canevas de base pour Common.Designer.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@4931 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Designer/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿//	Copyright © 2006, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("Epsitec Common Designer Engine")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Epsitec")]
+[assembly: AssemblyProduct ("Designer")]
+[assembly: AssemblyCopyright ("Copyright © 2006, Pierre Arnaud, EPSITEC SA, CH-1092 BELMONT, Switzerland")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+[assembly: ComVisible (false)]
+[assembly: Guid ("e5e11a4d-5400-4e61-a450-af45d1684332")]
+
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
