05c479acb983e68737ec801fd7a45507f83e2009
Daniel Roux <roux@epsitec.ch>
2006-04-10 07:57:53 +0000

    - RibbonButton et RibbonContainer dupliqués dans le projet Common.Widgets.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@4958 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/RibbonContainer.cs
@@ -0,0 +1,57 @@
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+using Epsitec.Common.Drawing;
+
+namespace Epsitec.Common.Widgets
+{
+	/// <summary>
+	/// La classe RibbonContainer permet de réaliser des rubans horizontaux.
+	/// </summary>
+	public class RibbonContainer : AbstractToolBar
+	{
+		public RibbonContainer()
+		{
+		}
+		
+		public RibbonContainer(Widget embedder) : this()
+		{
+			this.SetEmbedder(embedder);
+		}
+
+
+		public override double DefaultHeight
+		{
+			//	Retourne la hauteur standard d'une barre.
+			get
+			{
+				return 66;
+			}
+		}
+		
+		protected double LabelHeight
+		{
+			//	Retourne la hauteur pour le label supérieur.
+			get
+			{
+				return 14;
+			}
+		}
+
+		public override DockStyle DefaultIconDockStyle
+		{
+			get
+			{
+				return DockStyle.Left;
+			}
+		}
+
+		protected override void PaintBackgroundImplementation(Drawing.Graphics graphics, Drawing.Rectangle clipRect)
+		{
+			IAdorner adorner = Epsitec.Common.Widgets.Adorners.Factory.Active;
+
+			Rectangle rect = this.Client.Bounds;
+			WidgetState state = this.PaintState;
+			adorner.PaintRibbonTabBackground(graphics, rect, this.LabelHeight, state);
+		}
+	}
+}
