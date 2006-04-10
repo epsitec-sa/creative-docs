68564ce61429aef74d2eceb5d0582896a67550c9
Daniel Roux <roux@epsitec.ch>
2006-04-10 14:50:32 +0000

    - Le viewer contient un premier widget pour afficher le nom de la culture par défaut, et un second (TextFieldCombo) pour choisir la culture secondaire.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@4971 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/Viewer.cs
+++ Epsitec.Cresus/Common.Designer/Viewer.cs
@@ -8,11 +8,24 @@ namespace Epsitec.Common.Designer
 	/// <summary>
 	/// Permet de représenter les ressources d'un module.
 	/// </summary>
-	public class Viewer
+	public class Viewer : Widget
 	{
 		public Viewer(Module module)
 		{
 			this.module = module;
+
+			this.primaryCulture = new TextField(this);
+			this.primaryCulture.IsReadOnly = true;
+			this.primaryCulture.Width = 200;
+			this.primaryCulture.Anchor = AnchorStyles.Left | AnchorStyles.Top;
+			this.primaryCulture.Margins = new Margins(10, 0, 10, 0);
+
+			this.secondaryCulture = new TextFieldCombo(this);
+			this.secondaryCulture.Width = 200;
+			this.secondaryCulture.Anchor = AnchorStyles.Left | AnchorStyles.Top;
+			this.secondaryCulture.Margins = new Margins(10+200+10, 0, 10, 0);
+
+			this.UpdateCultures();
 		}
 
 		public void Dispose()
@@ -20,6 +33,33 @@ namespace Epsitec.Common.Designer
 		}
 
 
+		protected void UpdateCultures()
+		{
+			ResourceBundleCollection bundles = this.module.Bundles;
+
+			ResourceBundle def = bundles[ResourceLevel.Default];
+			this.primaryCulture.Text = def.Culture.Name;
+
+			string first = "";
+			this.secondaryCulture.Items.Clear();
+			for ( int b=0 ; b<bundles.Count ; b++ )
+			{
+				ResourceBundle bundle = bundles[b];
+				if ( bundle != def )
+				{
+					this.secondaryCulture.Items.Add(bundle.Culture.Name);
+					if ( first == "" )
+					{
+						first = bundle.Culture.Name;
+					}
+				}
+			}
+			this.secondaryCulture.Text = first;
+		}
+
+
 		protected Module					module;
+		protected TextField					primaryCulture;
+		protected TextFieldCombo			secondaryCulture;
 	}
 }
