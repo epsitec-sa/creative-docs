e986ad783999f019795418fedc999705b6d6287e
Daniel Roux <roux@epsitec.ch>
2006-04-12 13:16:00 +0000

    - Les lignes éditables primaire/secondaire pour les textes et les commentaires des ressources sont grisées lorsqu'il n'y a aucune ressource sélectionnée dans le tableau.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@5005 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/MainWindow.cs
+++ Epsitec.Cresus/Common.Designer/MainWindow.cs
@@ -37,12 +37,10 @@ namespace Epsitec.Common.Designer
 												WindowStyles.HasCloseButton;
 
 				Point parentCenter = parent.WindowBounds.Center;
-
 				this.window.WindowBounds = new Rectangle(parentCenter.X-600/2, parentCenter.Y-500/2, 600, 500);
 				this.window.Root.MinSize = new Size(500, 400);
 				this.window.Text = Res.Strings.Application.Title;
 				this.window.PreventAutoClose = true;
-				//?this.window.Owner = parent;
 				this.window.AsyncNotification += new EventHandler(this.HandleWindowAsyncNotification);
 				this.window.WindowCloseClicked += new EventHandler(this.HandleWindowCloseClicked);
 
