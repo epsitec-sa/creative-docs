f0e492eb4420487c8a565cd482ab5bbbd195ca0d
Daniel Roux <roux@epsitec.ch>
2006-04-12 13:11:24 +0000

    - La fenêtre de Designer est créée au centre de celle de CrDoc.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@5004 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/MainWindow.cs
+++ Epsitec.Cresus/Common.Designer/MainWindow.cs
@@ -35,8 +35,10 @@ namespace Epsitec.Common.Designer
 												WindowStyles.CanMinimize |
 												WindowStyles.CanMaximize |
 												WindowStyles.HasCloseButton;
-				
-				this.window.WindowSize = new Size(600, 500);
+
+				Point parentCenter = parent.WindowBounds.Center;
+
+				this.window.WindowBounds = new Rectangle(parentCenter.X-600/2, parentCenter.Y-500/2, 600, 500);
 				this.window.Root.MinSize = new Size(500, 400);
 				this.window.Text = Res.Strings.Application.Title;
 				this.window.PreventAutoClose = true;
