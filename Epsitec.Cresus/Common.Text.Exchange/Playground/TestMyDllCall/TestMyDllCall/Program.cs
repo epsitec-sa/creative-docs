e753e33473baf9033a043a761c220f50a2185979
Michael Walz <walz@epsitec.ch>
2006-04-13 09:54:22 +0000

    - Playground: la lecture du presse papiers avec une dll auxiliaire "unmanaged" fonctionne
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@5020 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Text.Exchange/Playground/TestMyDllCall/TestMyDllCall/Program.cs
+++ Epsitec.Cresus/Common.Text.Exchange/Playground/TestMyDllCall/TestMyDllCall/Program.cs
@@ -11,9 +11,49 @@ namespace TestMyDllCall
 		[DllImport ("ClipboardDll.dll")]
 		static extern int TestEntry(int value);
 
+		[DllImport ("ClipboardDll.dll")]
+		static unsafe extern byte* ReadHtmlFromClipboard();
+
+		[DllImport ("ClipboardDll.dll")]
+		static extern int GetClipboardSize();
+
+		[DllImport ("ClipboardDll.dll")]
+		static extern void FreeClipboard();
+
+
+		static unsafe byte[] ReadClipBoard()
+		{
+			int size;
+			byte* pBuffer;
+
+			pBuffer = ReadHtmlFromClipboard ();
+
+			if (pBuffer != null)
+			{
+
+				size = GetClipboardSize ();
+
+				byte[] managedBuffer = new byte[size];
+
+				for (int i = 0; i < size; i++)
+				{
+					managedBuffer[i] = pBuffer[i];
+				}
+
+				return managedBuffer;
+			}
+
+			return null;
+
+		}
+
 		static void Main(string[] args)
 		{
 			int b = TestEntry(12);
+
+			byte[] p = ReadClipBoard ();
+
+
 		}
 	}
 }
