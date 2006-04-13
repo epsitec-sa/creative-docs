716d889aec83a361047d39f5c7d505f2b3324f3e
Michael Walz <walz@epsitec.ch>
2006-04-13 09:54:22 +0000

    - Playground: la lecture du presse papiers avec une dll auxiliaire "unmanaged" fonctionne
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@5020 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Text.Exchange/Playground/ClipboardDll/ClipboardDll.cpp
+++ Epsitec.Cresus/Common.Text.Exchange/Playground/ClipboardDll/ClipboardDll.cpp
@@ -13,10 +13,11 @@ BOOL APIENTRY DllMain( HMODULE hModule,
                        LPVOID lpReserved
 					 )
 {
+#if 0
 	TCHAR buffer[100] ;
 	wsprintf (buffer, L"reason = %d, reserved = %d", ul_reason_for_call, lpReserved) ;
 	MessageBox(0, buffer, L"2", 0);
-
+#endif
     return TRUE;
 }
 
@@ -27,6 +28,43 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 extern "C" __declspec( dllexport ) int TestEntry(int value)
 {
 	return 2*value ;
+}
+
+
+static HANDLE globalLockHandle ;
+static int clipboardSize ;
+
+extern "C" __declspec( dllexport ) BYTE *ReadHtmlFromClipboard()
+{
+  if (OpenClipboard(NULL))
+  {
+	  globalLockHandle = GetClipboardData(CF_TEXT) ;
+
+	  if (globalLockHandle != NULL)
+	  {
+		BYTE* pData = (BYTE*)GlobalLock(globalLockHandle);
+		clipboardSize = GlobalSize(globalLockHandle);
+		CloseClipboard() ;
+		return pData ;
+	  }
+	  
+	  return (BYTE*)1 ;
+  }
+
+  return NULL ;
+}
+
+
+extern "C" __declspec( dllexport ) int GetClipboardSize()
+{
+	return clipboardSize ;
+}
+
+
+extern "C" __declspec( dllexport ) void FreeClipboard()
+{
+	GlobalUnlock(globalLockHandle) ;
+	GlobalFree(globalLockHandle) ;
 }
 
 
