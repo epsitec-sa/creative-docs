96e2b4f89df2e03550df3286d8dd098eff508bad
Michael Walz <walz@epsitec.ch>
2006-04-28 19:41:37 +0000

    - n'insère plus le contenu du tag <style> en tant que texte (ce qui foire d'ailleurs à cause des \t qu'ils essaie d'insérer à ce moment - gère correctement l'en-tête MSHTML de Word
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@5175 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Text.Exchange/Clipboard.Win32/ClipboardDll.cpp
+++ Epsitec.Cresus/Common.Text.Exchange/Clipboard.Win32/ClipboardDll.cpp
@@ -14,7 +14,7 @@
 
 #define EXPORT extern "C" __declspec( dllexport )
 
-#
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
