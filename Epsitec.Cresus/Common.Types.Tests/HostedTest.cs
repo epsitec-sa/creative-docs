c449a1795ae69a575e94106bbbd29fb5d343117e
Pierre Arnaud <arnaud@opac.ch>
2006-05-02 18:01:19 +0000

    - Test passé avec succès de la classe Types.HostedList<T>.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@5197 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Types.Tests/HostedTest.cs
+++ Epsitec.Cresus/Common.Types.Tests/HostedTest.cs
@@ -6,37 +6,126 @@ namespace Epsitec.Common.Types
 {
 	[TestFixture] public class HostedTest
 	{
-		[Test] public void CheckHostedList()
+		[Test]
+		public void CheckHostedIntList()
 		{
-			
+			Host<int> hostInt = new Host<int> ();
+
+			hostInt.SetExpectedInsertions (1, 2, 3, 4, 5, 6);
+			hostInt.SetExpectedRemovals (2, 1, 5, 6);
+
+			hostInt.Items.Add (1);
+			hostInt.Items.Insert (0, 2);
+			hostInt.Items.Add (3);
+			hostInt.Items.RemoveAt (0);
+			hostInt.Items.RemoveAt (0);
+			hostInt.Items.AddRange (new int[] { 4, 5 });
+
+			Assert.AreEqual (3, hostInt.Items[0]);
+			Assert.AreEqual (4, hostInt.Items[1]);
+			Assert.AreEqual (5, hostInt.Items[2]);
+
+			hostInt.Items[2] = 6;
+
+			Assert.AreEqual (2, hostInt.Items.IndexOf (6));
+
+			hostInt.Items.Remove (6);
+
+			Assert.AreEqual (2, hostInt.Items.Count);
+		}
+
+		[Test]
+		public void CheckHostedStringList()
+		{
+			Host<string> hostString = new Host<string> ();
+
+			hostString.SetExpectedInsertions ("1", "2", "3", "4", "5", "6");
+			hostString.SetExpectedRemovals ("2", "1", "5", "6");
+
+			hostString.Items.Add ("1");
+			hostString.Items.Insert (0, "2");
+			hostString.Items.Add ("3");
+			hostString.Items.RemoveAt (0);
+			hostString.Items.RemoveAt (0);
+			hostString.Items.AddRange (new string[] { "4", "5" });
+
+			Assert.AreEqual ("3", hostString.Items[0]);
+			Assert.AreEqual ("4", hostString.Items[1]);
+			Assert.AreEqual ("5", hostString.Items[2]);
+
+			hostString.Items[2] = "6";
+
+			Assert.AreEqual (2, hostString.Items.IndexOf ("6"));
+
+			hostString.Items.Remove ("6");
+
+			Assert.AreEqual (2, hostString.Items.Count);
 		}
 
 		private class Host<T> : IListHost<T>
 		{
 			public Host()
 			{
+				this.list = new HostedList<T> (this);
+			}
+
+			public HostedList<T> Items
+			{
+				get
+				{
+					return this.list;
+				}
+			}
+
+			public void SetExpectedInsertions(params T[] items)
+			{
+				this.SetExpectedInsertions ((IEnumerable<T>) (items));
+			}
+
+			public void SetExpectedRemovals(params T[] items)
+			{
+				this.SetExpectedRemovals ((IEnumerable<T>) (items));
 			}
 
 			public void SetExpectedInsertions(IEnumerable<T> collection)
 			{
+				this.expectedInsertions.Clear ();
+
+				foreach (T item in collection)
+				{
+					this.expectedInsertions.Enqueue (item);
+				}
+			}
+			
+			public void SetExpectedRemovals(IEnumerable<T> collection)
+			{
+				this.expectedRemovals.Clear ();
+
+				foreach (T item in collection)
+				{
+					this.expectedRemovals.Enqueue (item);
+				}
 			}
 			
 			#region IListHost<T> Members
 
 			public void NotifyListInsertion(T item)
 			{
-				
+				T expect = this.expectedInsertions.Dequeue ();
+				Assert.AreEqual (expect, item);
 			}
 
 			public void NotifyListRemoval(T item)
 			{
-				
+				T expect = this.expectedRemovals.Dequeue ();
+				Assert.AreEqual (expect, item);
 			}
 
 			#endregion
 
-			private List<T> expectedInsertions;
-			private List<T> expectedRemovals;
+			private HostedList<T> list;
+			private Queue<T> expectedInsertions = new Queue<T> ();
+			private Queue<T> expectedRemovals = new Queue<T> ();
 		}
 	}
 }
