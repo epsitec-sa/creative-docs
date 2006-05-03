34b02ec00be60e2832a51449291017658262d222
Pierre Arnaud <arnaud@opac.ch>
2006-05-03 13:28:04 +0000

    - Premier essai avec StructuredRecordType.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@5214 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Types/StructuredRecordType.cs
+++ Epsitec.Cresus/Common.Types/StructuredRecordType.cs
@@ -9,10 +9,126 @@ namespace Epsitec.Common.Types
 	/// The <c>StructuredRecordType</c> class describes the type of the data stored in
 	/// a <see cref="T:StructuredRecord"/> class.
 	/// </summary>
-	public class StructuredRecordType
+	public class StructuredRecordType : INamedType
 	{
 		public StructuredRecordType()
 		{
+			this.fields = new HostedDictionary<string, INamedType> (this.NotifyFieldInserted, this.NotifyFieldRemoved);
+			this.name = null;
+			this.caption = null;
+			this.description = null;
 		}
+
+		public HostedDictionary<string, INamedType> Fields
+		{
+			get
+			{
+				return this.fields;
+			}
+		}
+
+		public bool IsPathValid(string path)
+		{
+			string[] names = StructuredRecordType.SplitPath (path);
+
+			if (names.Length == 0)
+			{
+				return false;
+			}
+
+			StructuredRecordType record = this;
+
+			for (int i = 0; i < names.Length; i++)
+			{
+				if (record == null)
+				{
+					return false;
+				}
+				
+				INamedType value;
+
+				if (record.Fields.TryGetValue (names[i], out value))
+				{
+					record = value as StructuredRecordType;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			
+			return true;
+		}
+
+		public static string[] SplitPath(string path)
+		{
+			if ((path == null) ||
+				(path.Length == 0))
+			{
+				return StructuredRecordType.emptyPath;
+			}
+			else
+			{
+				return path.Split ('.');
+			}
+		}
+
+		#region INamedType Members
+
+		public System.Type SystemType
+		{
+			get
+			{
+				return null;
+			}
+		}
+
+		#endregion
+
+		#region INameCaption Members
+
+		public string Caption
+		{
+			get
+			{
+				return this.caption;
+			}
+		}
+
+		public string Description
+		{
+			get
+			{
+				return this.description;
+			}
+		}
+
+		#endregion
+
+		#region IName Members
+
+		public string Name
+		{
+			get
+			{
+				return this.name;
+			}
+		}
+
+		#endregion
+
+		private void NotifyFieldInserted(string name, INamedType type)
+		{
+		}
+
+		private void NotifyFieldRemoved(string name, INamedType type)
+		{
+		}
+
+		private static string[] emptyPath = new string[0];
+		private string name;
+		private string caption;
+		private string description;
+		private HostedDictionary<string, INamedType> fields;
 	}
 }
