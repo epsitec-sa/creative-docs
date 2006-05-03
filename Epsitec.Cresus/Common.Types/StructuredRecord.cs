05c25409a67b255d8c367c21b588ff1c1099d45b
Pierre Arnaud <arnaud@opac.ch>
2006-05-03 12:57:43 +0000

    - Fait marche arrière dans la définition des données structurées: StructuredRecord décrit les données au moyen d'un StructuredRecordType.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@5213 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/StructuredRecord.cs
@@ -0,0 +1,14 @@
+//	Copyright © 2006, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+using System.Collections.Generic;
+
+namespace Epsitec.Common.Types
+{
+	public class StructuredRecord
+	{
+		public StructuredRecord()
+		{
+		}
+	}
+}
---
+++ Epsitec.Cresus/Common.Types/StructuredRecordType.cs
@@ -0,0 +1,18 @@
+//	Copyright © 2006, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+using System.Collections.Generic;
+
+namespace Epsitec.Common.Types
+{
+	/// <summary>
+	/// The <c>StructuredRecordType</c> class describes the type of the data stored in
+	/// a <see cref="T:StructuredRecord"/> class.
+	/// </summary>
+	public class StructuredRecordType
+	{
+		public StructuredRecordType()
+		{
+		}
+	}
+}
