4addf1ba657e4f51544ddb829bc2959e59402c03
Pierre Arnaud <arnaud@opac.ch>
2006-05-03 13:35:33 +0000

    - Ajouté StructuredRecordType.GetFieldType et test correspondant.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@5216 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Types.Tests/HostedTest.cs
+++ Epsitec.Cresus/Common.Types.Tests/HostedTest.cs
@@ -1,5 +1,7 @@
-using NUnit.Framework;
+//	Copyright © 2006, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
 
+using NUnit.Framework;
 using System.Collections.Generic;
 
 namespace Epsitec.Common.Types
