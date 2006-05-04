8098450613fca1763a92bf7cc28d0014e239a4a4
Pierre Arnaud <arnaud@opac.ch>
2006-05-04 03:42:23 +0000

    - Fini les modifications commencées par Daniel. Un certain nombre de projets de test ne compilaient toujours pas...
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005-cleanup-may-2006@5225 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Support.Tests/SerializationTest.cs
+++ Epsitec.Cresus/Common.Support.Tests/SerializationTest.cs
@@ -56,59 +56,5 @@ namespace Epsitec.Common.Support
 			Assert.AreEqual (typeof (ResourceException), exc_2.GetType ());
 			Assert.AreEqual (exc_1.Message, exc_2.Message);
 		}
-		
-		[Test] public void CheckBundleAttributeSerialization()
-		{
-			BundleAttribute   bat_1 = new BundleAttribute ("my bundle attribute");
-			BundleAttribute   bat_2;
-			
-			bat_1.DefaultValue = "xyz";
-			
-			using (System.IO.Stream stream = System.IO.File.Open ("test.bin", System.IO.FileMode.Create))
-			{
-				BinaryFormatter formatter = new BinaryFormatter ();
-				formatter.Serialize (stream, bat_1);
-			}
-			
-			using (System.IO.Stream stream = System.IO.File.Open ("test.bin", System.IO.FileMode.Open))
-			{
-				BinaryFormatter formatter = new BinaryFormatter ();
-				bat_2 = (BundleAttribute)   formatter.Deserialize (stream);
-			}
-			
-			System.IO.File.Delete ("test.bin");
-			
-			Assert.IsNotNull (bat_2);
-			Assert.AreEqual (typeof (BundleAttribute), bat_2.GetType ());
-			Assert.AreEqual (bat_1.PropertyName, bat_2.PropertyName);
-			Assert.AreEqual (bat_1.DefaultValue, bat_2.DefaultValue);
-			
-			using (System.IO.Stream stream = System.IO.File.Open ("test.soap", System.IO.FileMode.Create))
-			{
-				SoapFormatter formatter = new SoapFormatter ();
-				formatter.Serialize (stream, bat_1);
-			}
-			
-			using (System.IO.Stream stream = System.IO.File.Open ("test.soap", System.IO.FileMode.Open))
-			{
-				SoapFormatter formatter = new SoapFormatter ();
-				bat_2 = (BundleAttribute)   formatter.Deserialize (stream);
-			}
-			
-			using (System.IO.Stream stream = System.IO.File.Open ("test.soap", System.IO.FileMode.Open))
-			{
-				System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding ();
-				byte[] buffer = new byte[stream.Length];
-				stream.Read (buffer, 0, (int) stream.Length);
-				System.Console.Out.WriteLine ("{0}", encoding.GetString (buffer));
-			}
-			
-			System.IO.File.Delete ("test.soap");
-			
-			Assert.IsNotNull (bat_2);
-			Assert.AreEqual (typeof (BundleAttribute), bat_2.GetType ());
-			Assert.AreEqual (bat_1.PropertyName, bat_2.PropertyName);
-			Assert.AreEqual (bat_1.DefaultValue, bat_2.DefaultValue);
-		}
 	}
 }
