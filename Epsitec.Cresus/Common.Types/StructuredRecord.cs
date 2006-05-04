390784e9b73cda6660ced0f546622cec8476dddf
Pierre Arnaud <arnaud@opac.ch>
2006-05-04 08:16:13 +0000

    - StructuredRecord implémente maintenant IStructuredTree.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@5237 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Types/StructuredRecord.cs
+++ Epsitec.Cresus/Common.Types/StructuredRecord.cs
@@ -5,13 +5,52 @@ using System.Collections.Generic;
 
 namespace Epsitec.Common.Types
 {
-	public class StructuredRecord
+	public class StructuredRecord : IStructuredTree
 	{
 		public StructuredRecord(StructuredRecordType type)
 		{
 			this.type = type;
 		}
 
+		
+		#region IStructuredTree Members
+
+		public string[] GetFieldNames()
+		{
+			if (this.type == null)
+			{
+				return this.GetDynamicFieldNames ();
+			}
+			else
+			{
+				return this.type.GetFieldNames ();
+			}
+		}
+
+		public string[] GetFieldPaths(string path)
+		{
+			if (this.type == null)
+			{
+				return this.GetDynamicFieldPaths (path);
+			}
+			else
+			{
+				return this.type.GetFieldPaths (path);
+			}
+		}
+
+		#endregion
+
+		protected virtual string[] GetDynamicFieldNames()
+		{
+			return new string[0];
+		}
+
+		protected virtual string[] GetDynamicFieldPaths(string path)
+		{
+			return null;
+		}
+		
 		private StructuredRecordType type;
 	}
 }
