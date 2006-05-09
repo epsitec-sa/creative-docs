ad6abd03a7f732a4b1c30a1a6ec59e2ccd38f397
Daniel Roux <roux@epsitec.ch>
2006-05-09 14:20:02 +0000

    - Pour essais...
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@5321 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Widgets.Tests/RibbonTest.cs
+++ Epsitec.Cresus/Common.Widgets.Tests/RibbonTest.cs
@@ -43,9 +43,36 @@ namespace Epsitec.Common.Widgets
 			//	Crée l'onglet 1.
 			RibbonPage page1 = new RibbonPage();
 			page1.RibbonTitle = "Principal";
+			page1.MinHeight = 71;
 			page1.TabIndex = 1;
 			page1.TabNavigation = Widget.TabNavigationMode.ActivateOnTab | Widget.TabNavigationMode.ForwardToChildren | Widget.TabNavigationMode.ForwardOnly;
 			book.Items.Add(page1);
+
+			RibbonSection s1p1 = new RibbonSection();
+			s1p1.MinHeight = 71;
+			s1p1.Title = "Rouge";
+			s1p1.PreferredWidth = 100;
+			page1.Items.Add(s1p1);
+
+			Button b1s1p1 = new Button(s1p1);
+			b1s1p1.Text = "A";
+			b1s1p1.PreferredWidth = 30;
+			b1s1p1.Dock = DockStyle.Left;
+
+			Button b2s1p1 = new Button(s1p1);
+			b2s1p1.Text = "B";
+			b2s1p1.PreferredWidth = 30;
+			b2s1p1.Dock = DockStyle.Left;
+
+			RibbonSection s2p1 = new RibbonSection();
+			s2p1.Title = "Vert";
+			s2p1.PreferredWidth = 100;
+			page1.Items.Add(s2p1);
+
+			RibbonSection s3p1 = new RibbonSection();
+			s3p1.Title = "Bleu";
+			s3p1.PreferredWidth = 100;
+			page1.Items.Add(s3p1);
 
 			//	Crée l'onglet 2.
 			RibbonPage page2 = new RibbonPage();
@@ -53,6 +80,16 @@ namespace Epsitec.Common.Widgets
 			page2.TabIndex = 2;
 			page2.TabNavigation = Widget.TabNavigationMode.ActivateOnTab | Widget.TabNavigationMode.ForwardToChildren | Widget.TabNavigationMode.ForwardOnly;
 			book.Items.Add(page2);
+
+			RibbonSection s1p2 = new RibbonSection();
+			s1p2.Title = "Lausanne";
+			s1p2.PreferredWidth = 150;
+			page2.Items.Add(s1p2);
+
+			RibbonSection s2p2 = new RibbonSection();
+			s2p2.Title = "Genève";
+			s2p2.PreferredWidth = 100;
+			page2.Items.Add(s2p2);
 
 			book.ActivePage = page1;
 
