9bb73b0d784b6c3eff825ca947579aa09825cb0b
Daniel Roux <roux@epsitec.ch>
2006-05-12 14:55:38 +0000

    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@5408 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Designer/MyWidgets/BundleType.cs
@@ -0,0 +1,111 @@
+using System.Collections.Generic;
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+using Epsitec.Common.Drawing;
+
+namespace Epsitec.Common.Designer.MyWidgets
+{
+	/// <summary>
+	/// Widget permettant de choisir une catégorie de bundle.
+	/// </summary>
+	public class BundleType : Widget
+	{
+		public BundleType() : base()
+		{
+			this.PreferredHeight = 50;  // TODO: pourquoi cela ne fonctionne pas sans cette ligne ?
+
+			Separator sep = new Separator(this);
+			sep.PreferredHeight = 1;
+			sep.Margins = new Margins(0, 0, 10, 0);
+			sep.Dock = DockStyle.Bottom;
+
+			this.buttonStrings = new IconButtonMark(this);
+			//?this.buttonStrings.Text = Res.Strings.BundleType.Strings;
+			this.buttonStrings.Text = "Textes";
+			this.buttonStrings.Name = "Texts";
+			this.buttonStrings.ButtonStyle = ButtonStyle.ActivableIcon;
+			this.buttonStrings.SiteMark = SiteMark.OnBottom;
+			this.buttonStrings.MarkDimension = 5;
+			this.buttonStrings.PreferredWidth = 100;
+			this.buttonStrings.PreferredHeight = 20;
+			this.buttonStrings.AutoFocus = false;
+			this.buttonStrings.Margins = new Margins(10, 0, 10, 0);
+			this.buttonStrings.Dock = DockStyle.Left;
+			this.buttonStrings.Clicked += new MessageEventHandler(this.HandleButtonClicked);
+
+			this.buttonPanels = new IconButtonMark(this);
+			//?this.buttonPanels.Text = Res.Strings.BundleType.Panels;
+			this.buttonPanels.Text = "Interfaces";
+			this.buttonPanels.Name = "Panels";
+			this.buttonPanels.ButtonStyle = ButtonStyle.ActivableIcon;
+			this.buttonPanels.SiteMark = SiteMark.OnBottom;
+			this.buttonPanels.MarkDimension = 5;
+			this.buttonPanels.PreferredWidth = 100;
+			this.buttonPanels.PreferredHeight = 20;
+			this.buttonPanels.AutoFocus = false;
+			this.buttonPanels.Margins = new Margins(2, 0, 10, 0);
+			this.buttonPanels.Dock = DockStyle.Left;
+			this.buttonPanels.Clicked += new MessageEventHandler(this.HandleButtonClicked);
+
+			this.buttonScripts = new IconButtonMark(this);
+			//?this.buttonScripts.Text = Res.Strings.BundleType.Scripts;
+			this.buttonScripts.Text = "Scripts";
+			this.buttonScripts.Name = "Scripts";
+			this.buttonScripts.ButtonStyle = ButtonStyle.ActivableIcon;
+			this.buttonScripts.SiteMark = SiteMark.OnBottom;
+			this.buttonScripts.MarkDimension = 5;
+			this.buttonScripts.PreferredWidth = 100;
+			this.buttonScripts.PreferredHeight = 20;
+			this.buttonScripts.AutoFocus = false;
+			this.buttonScripts.Margins = new Margins(2, 0, 10, 0);
+			this.buttonScripts.Dock = DockStyle.Left;
+			this.buttonScripts.Clicked += new MessageEventHandler(this.HandleButtonClicked);
+
+			this.UpdateButtons();
+		}
+
+		public BundleType(Widget embedder) : this()
+		{
+			this.SetEmbedder(embedder);
+		}
+
+
+		public string CurrentType
+		{
+			get
+			{
+				return this.currentType;
+			}
+
+			set
+			{
+				if (this.currentType != value)
+				{
+					this.currentType = value;
+					this.UpdateButtons();
+				}
+			}
+		}
+
+
+		protected void UpdateButtons()
+		{
+			this.buttonStrings.ActiveState = (this.currentType == "Texts"  ) ? ActiveState.Yes : ActiveState.No;
+			this.buttonPanels .ActiveState = (this.currentType == "Panels" ) ? ActiveState.Yes : ActiveState.No;
+			this.buttonScripts.ActiveState = (this.currentType == "Scripts") ? ActiveState.Yes : ActiveState.No;
+		}
+
+
+		void HandleButtonClicked(object sender, MessageEventArgs e)
+		{
+			IconButtonMark button = sender as IconButtonMark;
+			this.CurrentType = button.Name;
+		}
+
+
+		protected string					currentType = "Texts";
+		protected IconButtonMark			buttonStrings;
+		protected IconButtonMark			buttonPanels;
+		protected IconButtonMark			buttonScripts;
+	}
+}
