dd2e104f673504d44f611f97eb4235cb3ad00da3
Pierre Arnaud <arnaud@opac.ch>
2006-05-17 15:26:39 +0000

    - Ajouté code manquant (à la suite du Merge) dans Viewers/Abstract.cs.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@5448 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/Viewers/Abstract.cs
+++ Epsitec.Cresus/Common.Designer/Viewers/Abstract.cs
@@ -202,8 +202,20 @@ namespace Epsitec.Common.Designer.Viewers
 			int count = this.labelsIndex.Count;
 			bool build = (this.module.Mode == DesignerMode.Build);
 
-			this.GetCommandState("Save").Enable = this.module.Modifier.IsDirty;
-			this.GetCommandState("SaveAs").Enable = true;
+			////////////////////////////////////////////////////////////////////
+			//	TODO:
+			//	
+			//	Dans tout le code de "designer", remplacer les :
+			//
+			//			GetCommandState("xxx").Enable = ...
+			//
+			//	par des :
+			//	
+			//			CommandContext.SetLocalEnable("xxx", ...);
+			////////////////////////////////////////////////////////////////////
+
+			this.CommandContext.SetLocalEnable ("Save", this.module.Modifier.IsDirty);
+			this.CommandContext.SetLocalEnable ("SaveAs", true);
 
 			this.GetCommandState("Filter").Enable = true;
 
@@ -218,6 +230,14 @@ namespace Epsitec.Common.Designer.Viewers
 
 			this.GetCommandState("Up").Enable = (sel != -1 && sel > 0 && build);
 			this.GetCommandState("Down").Enable = (sel != -1 && sel < count-1 && build);
+		}
+
+		protected CommandContext CommandContext
+		{
+			get
+			{
+				return this.module.MainWindow.CommandContext;
+			}
 		}
 
 		protected CommandState GetCommandState(string command)
