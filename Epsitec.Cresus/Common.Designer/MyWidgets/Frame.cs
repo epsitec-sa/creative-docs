50349988a9e59b71f9360a7c81328ed77ffc91a0
Daniel Roux <roux@epsitec.ch>
2006-05-17 14:50:33 +0000

    - Viewers.Panels est peuplé par un Frame (nouveau widget) et un TabBook.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@5447 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Designer/MyWidgets/Frame.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+using Epsitec.Common.Drawing;
+
+namespace Epsitec.Common.Designer.MyWidgets
+{
+	/// <summary>
+	/// Widget de type 'groupe' avec un cadre.
+	/// </summary>
+	public class Frame : Widget
+	{
+		public Frame() : base()
+		{
+		}
+
+		public Frame(Widget embedder) : this()
+		{
+			this.SetEmbedder(embedder);
+		}
+
+		static Frame()
+		{
+			Widgets.Helpers.VisualPropertyMetadata metadata = new Widgets.Helpers.VisualPropertyMetadata(ContentAlignment.TopLeft, Widgets.Helpers.VisualPropertyMetadataOptions.AffectsTextLayout);
+			Widgets.Visual.ContentAlignmentProperty.OverrideMetadata(typeof(Frame), metadata);
+		}
+
+
+		protected override void PaintBackgroundImplementation(Graphics graphics, Rectangle clipRect)
+		{
+			//	Dessine le texte.
+			IAdorner adorner = Widgets.Adorners.Factory.Active;
+
+			Rectangle rect = this.Client.Bounds;
+			rect.Deflate(0.5);
+			graphics.AddRectangle(rect);
+			graphics.RenderSolid(adorner.ColorBorder);
+		}
+
+	}
+}
