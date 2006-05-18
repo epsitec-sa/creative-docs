b8769827e26afe1ee358f7d7244ea6fcede30395
Daniel Roux <roux@epsitec.ch>
2006-05-18 12:47:20 +0000

    - Renommé MyWidgets.Frame en MyWidgets.PanelFrame. - Déplacé toute la partie interactive de dessin de Viewer.Panels dans MyWidgets.PanelFrame.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@5480 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/Viewers/Panels.cs
+++ Epsitec.Cresus/Common.Designer/Viewers/Panels.cs
@@ -11,17 +11,6 @@ namespace Epsitec.Common.Designer.Viewers
 	/// </summary>
 	public class Panels : Abstract
 	{
-		protected enum MouseCursorType
-		{
-			Arrow,
-			ArrowPlus,
-			Global,
-			Hand,
-			Edit,
-			Pen,
-			Zoom,
-		}
-
 		public Panels(Module module) : base(module)
 		{
 			int tabIndex = 0;
@@ -71,15 +60,12 @@ namespace Epsitec.Common.Designer.Viewers
 			this.ToolBarAdd(Widgets.Command.Get("ObjectButton"));
 			this.ToolBarAdd(Widgets.Command.Get("ObjectText"));
 
-			this.container = new MyWidgets.Frame(this);
+			this.container = new MyWidgets.PanelFrame(this);
 			this.container.MinWidth = 100;
 			this.container.MinHeight = 100;
 			this.container.ChildrenLayoutMode = Widgets.Layouts.LayoutMode.Anchored;
 			this.container.Margins = new Margins(1, 1, 1, 1);
 			this.container.Dock = DockStyle.Fill;
-			this.container.MouseDown += new EventHandler<Point>(this.HandleContainerMouseDown);
-			this.container.MouseMove += new EventHandler<Point>(this.HandleContainerMouseMove);
-			this.container.MouseUp += new EventHandler<Point>(this.HandleContainerMouseUp);
 
 			this.tabBook = new TabBook(this);
 			this.tabBook.PreferredWidth = 150;
@@ -225,6 +211,13 @@ namespace Epsitec.Common.Designer.Viewers
 		public override void DoFont(string name)
 		{
 			//	Effectue une modification de typographie.
+		}
+
+		public override void DoTool(string name)
+		{
+			//	Choix de l'outil.
+			base.DoTool(name);
+			this.container.Tool = this.tool;
 		}
 
 
@@ -454,160 +447,6 @@ namespace Epsitec.Common.Designer.Viewers
 		}
 
 
-		#region Drawing select
-		protected void SelectDown(Point pos)
-		{
-			//	Sélection ponctuelle, souris pressée.
-		}
-
-		protected void SelectMove(Point pos)
-		{
-			//	Sélection ponctuelle, souris déplacée.
-			this.ChangeMouseCursor(MouseCursorType.Arrow);
-		}
-
-		protected void SelectUp(Point pos)
-		{
-			//	Sélection ponctuelle, souris relâchée.
-		}
-		#endregion
-
-		#region Drawing global
-		protected void GlobalDown(Point pos)
-		{
-			//	Sélection rectangulaire, souris pressée.
-		}
-
-		protected void GlobalMove(Point pos)
-		{
-			//	Sélection rectangulaire, souris déplacée.
-			this.ChangeMouseCursor(MouseCursorType.Global);
-		}
-
-		protected void GlobalUp(Point pos)
-		{
-			//	Sélection rectangulaire, souris relâchée.
-		}
-		#endregion
-
-		#region Drawing edit
-		protected void EditDown(Point pos)
-		{
-			//	Edition, souris pressée.
-		}
-
-		protected void EditMove(Point pos)
-		{
-			//	Edition, souris déplacée.
-			this.ChangeMouseCursor(MouseCursorType.Edit);
-		}
-
-		protected void EditUp(Point pos)
-		{
-			//	Edition, souris relâchée.
-		}
-		#endregion
-
-		#region Drawing zoom
-		protected void ZoomDown(Point pos)
-		{
-			//	Loupe, souris pressée.
-		}
-
-		protected void ZoomMove(Point pos)
-		{
-			//	Loupe, souris déplacée.
-			this.ChangeMouseCursor(MouseCursorType.Zoom);
-		}
-
-		protected void ZoomUp(Point pos)
-		{
-			//	Loupe, souris relâchée.
-		}
-		#endregion
-
-		#region Drawing hand
-		protected void HandDown(Point pos)
-		{
-			//	Main, souris pressée.
-		}
-
-		protected void HandMove(Point pos)
-		{
-			//	Main, souris déplacée.
-			this.ChangeMouseCursor(MouseCursorType.Hand);
-		}
-
-		protected void HandUp(Point pos)
-		{
-			//	Main, souris relâchée.
-		}
-		#endregion
-
-		#region Drawing object
-		protected void ObjectDown(string tool, Point pos)
-		{
-			//	Dessin d'un objet, souris pressée.
-			this.ContainerLock();
-
-			if (tool == "ObjectLine")
-			{
-				this.creatingObject = new Separator(this.container);
-				this.creatingObject.PreferredHeight = 1;
-			}
-
-			if (tool == "ObjectButton")
-			{
-				this.creatingObject = new Button(this.container);
-				this.creatingObject.Text = "Button";
-			}
-			
-			if (tool == "ObjectText")
-			{
-				this.creatingObject = new TextField(this.container);
-				this.creatingObject.Text = "TextField";
-			}
-
-			this.creatingObject.Anchor = AnchorStyles.BottomLeft;
-			this.creatingObject.Margins = new Margins(pos.X, 0, 0, pos.Y);
-		}
-
-		protected void ObjectMove(string tool, Point pos)
-		{
-			//	Dessin d'un objet, souris déplacée.
-			this.ChangeMouseCursor(MouseCursorType.Pen);
-
-			if (this.creatingObject != null)
-			{
-				this.creatingObject.Margins = new Margins(pos.X, 0, 0, pos.Y);
-			}
-		}
-
-		protected void ObjectUp(string tool, Point pos)
-		{
-			//	Dessin d'un objet, souris relâchée.
-			this.creatingObject = null;
-			this.ContainerUnlock();
-		}
-		#endregion
-
-		protected void ContainerLock()
-		{
-#if false  // TODO: génère des asserts 'dirty layout' !
-			this.container.MinSize = this.container.ActualSize;
-			this.container.MaxSize = this.container.ActualSize;
-#endif
-		}
-
-		protected void ContainerUnlock()
-		{
-#if false
-			this.container.MinSize = new Size(100, 100);
-			this.container.MaxSize = new Size(10000, 10000);
-#endif
-		}
-
-
 		void HandleArrayCellsQuantityChanged(object sender)
 		{
 			//	Le nombre de lignes a changé.
@@ -639,178 +478,15 @@ namespace Epsitec.Common.Designer.Viewers
 			this.module.Modifier.IsDirty = true;
 		}
 
-		void HandleContainerMouseDown(object sender, Point pos)
-		{
-			//	La souris a été pressée dans l'objet 'container'.
-			if (this.tool == "ToolSelect")
-			{
-				this.SelectDown(pos);
-			}
-
-			if (this.tool == "ToolGlobal")
-			{
-				this.GlobalDown(pos);
-			}
-
-			if (this.tool == "ToolEdit")
-			{
-				this.EditDown(pos);
-			}
-
-			if (this.tool == "ToolZoom")
-			{
-				this.ZoomDown(pos);
-			}
-
-			if (this.tool == "ToolHand")
-			{
-				this.HandDown(pos);
-			}
-
-			if (this.tool.StartsWith("Object"))
-			{
-				this.ObjectDown(this.tool, pos);
-			}
-		}
-
-		void HandleContainerMouseMove(object sender, Point pos)
-		{
-			//	La souris a été déplacée dans l'objet 'container'.
-			if (this.tool == "ToolSelect")
-			{
-				this.SelectMove(pos);
-			}
-
-			if (this.tool == "ToolGlobal")
-			{
-				this.GlobalMove(pos);
-			}
-
-			if (this.tool == "ToolEdit")
-			{
-				this.EditMove(pos);
-			}
-
-			if (this.tool == "ToolZoom")
-			{
-				this.ZoomMove(pos);
-			}
-
-			if (this.tool == "ToolHand")
-			{
-				this.HandMove(pos);
-			}
-
-			if (this.tool.StartsWith("Object"))
-			{
-				this.ObjectMove(this.tool, pos);
-			}
-		}
-
-		void HandleContainerMouseUp(object sender, Point pos)
-		{
-			//	La souris a été relâchée dans l'objet 'container'.
-			if (this.tool == "ToolSelect")
-			{
-				this.SelectUp(pos);
-			}
-
-			if (this.tool == "ToolGlobal")
-			{
-				this.GlobalUp(pos);
-			}
-
-			if (this.tool == "ToolEdit")
-			{
-				this.EditUp(pos);
-			}
-
-			if (this.tool == "ToolZoom")
-			{
-				this.ZoomUp(pos);
-			}
-
-			if (this.tool == "ToolHand")
-			{
-				this.HandUp(pos);
-			}
-
-			if (this.tool.StartsWith("Object"))
-			{
-				this.ObjectUp(this.tool, pos);
-			}
-		}
 
 
-		#region MouseCursor
-		protected void ChangeMouseCursor(MouseCursorType cursor)
-		{
-			//	Change le sprite de la souris.
-			switch (cursor)
-			{
-				case MouseCursorType.Arrow:
-					this.MouseCursorImage(ref this.mouseCursorArrow, Misc.Icon("CursorArrow"));
-					break;
-
-				case MouseCursorType.ArrowPlus:
-					this.MouseCursorImage(ref this.mouseCursorArrowPlus, Misc.Icon("CursorArrowPlus"));
-					break;
-
-				case MouseCursorType.Global:
-					this.MouseCursorImage(ref this.mouseCursorGlobal, Misc.Icon("CursorGlobal"));
-					break;
-
-				case MouseCursorType.Edit:
-					this.MouseCursorImage(ref this.mouseCursorEdit, Misc.Icon("CursorEdit"));
-					break;
-
-				case MouseCursorType.Hand:
-					this.MouseCursorImage(ref this.mouseCursorHand, Misc.Icon("CursorHand"));
-					break;
-
-				case MouseCursorType.Pen:
-					this.MouseCursorImage(ref this.mouseCursorPen, Misc.Icon("CursorPen"));
-					break;
-
-				case MouseCursorType.Zoom:
-					this.MouseCursorImage(ref this.mouseCursorZoom, Misc.Icon("CursorZoom"));
-					break;
-
-				default:
-					this.MouseCursor = MouseCursor.AsArrow;
-					break;
-			}
-
-			this.Window.MouseCursor = this.MouseCursor;
-		}
-
-		protected void MouseCursorImage(ref Image image, string name)
-		{
-			//	Choix du sprite de la souris.
-			if (image == null)
-			{
-				image = Support.Resources.DefaultManager.GetImage(name);
-			}
-
-			this.MouseCursor = MouseCursor.FromImage(image);
-		}
-		#endregion
 
 
 		protected TextFieldEx				labelEdit;
 		protected VToolBar					toolBar;
-		protected MyWidgets.Frame			container;
+		protected MyWidgets.PanelFrame		container;
 		protected TabBook					tabBook;
 		protected TabPage					tabPageProperties;
 		protected TabPage					tabPageObjects;
-		protected Widget					creatingObject;
-
-		protected Image						mouseCursorArrow = null;
-		protected Image						mouseCursorArrowPlus = null;
-		protected Image						mouseCursorGlobal = null;
-		protected Image						mouseCursorEdit = null;
-		protected Image						mouseCursorPen = null;
-		protected Image						mouseCursorZoom = null;
-		protected Image						mouseCursorHand = null;
 	}
 }
