51f51cc1e21de132956450f1261b13bd774cb2a8
Pierre Arnaud <arnaud@opac.ch>
2006-06-08 13:43:06 +0000

    - CloneView est capable de redimensionner temporairement son modèle quand cela est nécessaire.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@5800 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Widgets/CloneView.cs
+++ Epsitec.Cresus/Common.Widgets/CloneView.cs
@@ -42,25 +42,75 @@ namespace Epsitec.Common.Widgets
 			}
 			else
 			{
-				Drawing.Point offset = this.model.ActualLocation - this.ActualLocation;
-				Drawing.Point pos = this.MapClientToRoot (Drawing.Point.Zero);
-				Drawing.Point originalClipOffset = graphics.ClipOffset;
-				Drawing.Transform originalTransform = graphics.Transform;
-				Drawing.Transform graphicsTransform = Drawing.Transform.FromTranslation (- offset);
-
-				Drawing.Point clipOffset = pos - this.model.MapClientToRoot (this.model.GetClipBounds ()).Location;
-
-				repaint.Offset (offset);
+				Drawing.Size displaySize = this.ActualSize;
+				Drawing.Point modelPos = this.model.ActualLocation;
+				Drawing.Size modelSize = this.model.ActualSize;
 				
-				graphicsTransform.MultiplyBy (originalTransform);
-				graphics.Transform = graphicsTransform;
-				graphics.ClipOffset = clipOffset;
-				
-				this.model.PaintHandler(graphics, repaint, paintFilter);
+				if (modelSize != displaySize)
+				{
+					//	Le modèle n'a pas la bonne taille. Il va falloir changer
+					//	temporairement sa taille.
+					
+					//	Attache le contexte de layout directement au modèle; de
+					//	cette manière le modèle sera considéré comme la base de
+					//	l'arbre des widgets et son conteneur ne sera pas affecté
+					//	par le changement de taille :
+					
+					Layouts.LayoutContext context = Helpers.VisualTree.GetLayoutContext (this.model);
+					Layouts.LayoutContext.SetLayoutContext (this.model, context);
 
-				graphics.Transform = originalTransform;
-				graphics.ClipOffset = originalClipOffset;
+					//	Modifie (brutalement) les dimensions du modèle, sans pour
+					//	autant le déplacer, puis force un "arrange" de son contenu
+					//	sans toucher au reste :
+					
+					this.model.SetBounds (new Drawing.Rectangle (modelPos, displaySize));
+					Layouts.LayoutContext.AddToArrangeQueue (this.model);
+					context.ExecuteArrange ();
+					
+					//	Peint normalement le modèle dans notre propre vue :
+					
+					this.PaintModelView (graphics, repaint, paintFilter);
+
+					//	Remet la taille initiale du modèle et refait ensuite le
+					//	layout de son contenu. Une fois de plus, le conteneur n'y
+					//	aura vu que du feu :
+					
+					this.model.SetBounds (new Drawing.Rectangle (modelPos, modelSize));
+					Layouts.LayoutContext.AddToArrangeQueue (this.model);
+					context.ExecuteArrange ();
+
+					//	Finalement, détache le contexte de layout du modèle pour
+					//	qu'il réintègre sa position normale dans l'arbre de layout :
+
+					Layouts.LayoutContext.ClearLayoutContext (this.model);
+				}
+				else
+				{
+					this.PaintModelView (graphics, repaint, paintFilter);
+				}
 			}
+		}
+
+		private void PaintModelView(Graphics graphics, Rectangle repaint, IPaintFilter paintFilter)
+		{
+			Drawing.Point offset = this.model.ActualLocation - this.ActualLocation;
+			Drawing.Point pos = this.MapClientToRoot (Drawing.Point.Zero);
+			Drawing.Point originalClipOffset = graphics.ClipOffset;
+			Drawing.Transform originalTransform = graphics.Transform;
+			Drawing.Transform graphicsTransform = Drawing.Transform.FromTranslation (-offset);
+
+			Drawing.Point clipOffset = pos - this.model.MapClientToRoot (this.model.GetClipBounds ()).Location;
+
+			repaint.Offset (offset);
+
+			graphicsTransform.MultiplyBy (originalTransform);
+			graphics.Transform = graphicsTransform;
+			graphics.ClipOffset = clipOffset;
+
+			this.model.PaintHandler (graphics, repaint, paintFilter);
+
+			graphics.Transform = originalTransform;
+			graphics.ClipOffset = originalClipOffset;
 		}
 
 
