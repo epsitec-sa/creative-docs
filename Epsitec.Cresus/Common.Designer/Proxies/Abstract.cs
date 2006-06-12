bff31bb7bba80103735b5782132193d39f2a4cf8
Pierre Arnaud <arnaud@opac.ch>
2006-06-12 13:44:28 +0000

    - Commenté et nettoyé Proxies.Abstract et Proxies.Geometry dans le designer.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@5845 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/Proxies/Abstract.cs
+++ Epsitec.Cresus/Common.Designer/Proxies/Abstract.cs
@@ -14,40 +14,90 @@ namespace Epsitec.Common.Designer.Proxies
 
 		public void AddWidget(Widget widget)
 		{
+			//	Ajoute un widget à notre liste de widgets connectés. Tous les
+			//	widgets de cette liste doivent partager exactement les mêmes
+			//	propriétés en ce qui concerne notre proxy.
+			
 			System.Diagnostics.Debug.Assert (this.widgets.Contains (widget) == false);
+			
 			this.widgets.Add (widget);
 
 			if (this.widgets.Count == 1)
 			{
+				//	Quand on ajoute le premier widget, on lit les propriétés
+				//	du widget et on initialise le proxy.
+				
 				this.ReadFromWidget ();
 			}
 		}
 
 		public void ReadFromWidget()
 		{
+			//	Synchronise le proxy avec le widget en lisant les propriétés du
+			//	widget connecté.
+			
 			if (this.widgets.Count > 0)
 			{
-				this.suspendSetWidgetProperty++;
-				this.InitialisePropertyValues ();
-				this.suspendSetWidgetProperty--;
+				//	Evite des appels récursifs de SetWidgetProperty (on ne veut
+				//	pas que l'initialisation des propriétés soit perçue comme
+				//	un changement qui provoque une mise à jour du widget, car
+				//	pendant l'initialisation, certaines valeurs seront forcément
+				//	encore invalides).
+				
+				this.SuspendChanges ();
+				
+				try
+				{
+					this.InitialisePropertyValues ();
+				}
+				finally
+				{
+					this.ResumeChanges ();
+				}
 			}
 		}
 
 		protected abstract void InitialisePropertyValues();
+
+		protected void SuspendChanges()
+		{
+			this.suspendChanges++;
+		}
+
+		protected void ResumeChanges()
+		{
+			this.suspendChanges--;
+		}
 		
 		protected void SetWidgetProperty(DependencyProperty property, object value)
 		{
-			if (this.suspendSetWidgetProperty == 0)
+			//	Met à jour la propriété du (ou des) widget(s) connecté(s), pour
+			//	autant que cela soit permis.
+			
+			if (this.suspendChanges == 0)
 			{
-				foreach (Widget widget in this.widgets)
+				this.SuspendChanges ();
+
+				try
 				{
-					widget.SetValue (property, value);
+					foreach (Widget widget in this.widgets)
+					{
+						widget.SetValue (property, value);
+					}
+				}
+				finally
+				{
+					this.ResumeChanges ();
 				}
 			}
 		}
 
 		protected object GetWidgetProperty(DependencyProperty property)
 		{
+			//	Lit une propriété du widget connecté. S'il y a plusieurs widgets,
+			//	on lit la propriété du premier; mais ça ne doit pas changer grand
+			//	chose, vu que tous les widgets ont des propriétés identiques.
+			
 			if (this.widgets.Count > 0)
 			{
 				return this.widgets[0].GetValue (property);
@@ -59,6 +109,6 @@ namespace Epsitec.Common.Designer.Proxies
 		}
 
 		List<Widget> widgets = new List<Widget> ();
-		private int suspendSetWidgetProperty = 0;
+		private int suspendChanges = 0;
 	}
 }
--- Epsitec.Cresus/Common.Designer/Proxies/Geometry.cs
+++ Epsitec.Cresus/Common.Designer/Proxies/Geometry.cs
@@ -61,6 +61,12 @@ namespace Epsitec.Common.Designer.Proxies
 
 		protected override void InitialisePropertyValues()
 		{
+			//	Cette méthode est appelée par Proxies.Abstract quand on connecte
+			//	le premier widget avec le proxy.
+			
+			//	Recopie localement les diverses propriétés du widget sélectionné
+			//	pour pouvoir ensuite travailler dessus :
+			
 			Drawing.Margins margins = (Drawing.Margins) this.GetWidgetProperty (Visual.MarginsProperty);
 
 			this.LeftMargin   = margins.Left;
@@ -71,15 +77,22 @@ namespace Epsitec.Common.Designer.Proxies
 
 		private static void NotifyMarginChanged(DependencyObject o, object oldValue, object newValue)
 		{
+			//	Cette méthode est appelée à la suite de la modification d'une de
+			//	nos propriétés de définition de la marge (LeftMargin, RightMargin,
+			//	etc.) pour permettre de mettre à jour les widgets connectés :
+			
 			Geometry that = (Geometry) o;
 			Drawing.Margins margins = new Drawing.Margins (that.LeftMargin, that.RightMargin, that.TopMargin, that.BottomMargin);
+			
+			//	Demande à Proxies.Abstract de mettre à jour la propriété qui
+			//	définit les marges du ou des widget(s) sélectionné(s) :
 			
 			that.SetWidgetProperty (Visual.MarginsProperty, margins);
 		}
 
-		public static readonly DependencyProperty LeftMarginProperty = DependencyProperty.Register ("LeftMargin", typeof (double), typeof (Geometry), new DependencyPropertyMetadata (0.0, Geometry.NotifyMarginChanged));
-		public static readonly DependencyProperty RightMarginProperty = DependencyProperty.Register ("RightMargin", typeof (double), typeof (Geometry), new DependencyPropertyMetadata (0.0, Geometry.NotifyMarginChanged));
-		public static readonly DependencyProperty TopMarginProperty = DependencyProperty.Register ("TopMargin", typeof (double), typeof (Geometry), new DependencyPropertyMetadata (0.0, Geometry.NotifyMarginChanged));
-		public static readonly DependencyProperty BottomMarginProperty = DependencyProperty.Register ("BottomMargin", typeof (double), typeof (Geometry), new DependencyPropertyMetadata (0.0, Geometry.NotifyMarginChanged));
+		public static readonly DependencyProperty LeftMarginProperty	= DependencyProperty.Register ("LeftMargin", typeof (double), typeof (Geometry), new DependencyPropertyMetadata (0.0, Geometry.NotifyMarginChanged));
+		public static readonly DependencyProperty RightMarginProperty	= DependencyProperty.Register ("RightMargin", typeof (double), typeof (Geometry), new DependencyPropertyMetadata (0.0, Geometry.NotifyMarginChanged));
+		public static readonly DependencyProperty TopMarginProperty		= DependencyProperty.Register ("TopMargin", typeof (double), typeof (Geometry), new DependencyPropertyMetadata (0.0, Geometry.NotifyMarginChanged));
+		public static readonly DependencyProperty BottomMarginProperty	= DependencyProperty.Register ("BottomMargin", typeof (double), typeof (Geometry), new DependencyPropertyMetadata (0.0, Geometry.NotifyMarginChanged));
 	}
 }
