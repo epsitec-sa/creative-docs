8264584aa86c9be68ee45d33987edc0541b2ab38
Pierre Arnaud <arnaud@opac.ch>
2006-06-20 05:05:14 +0000

    - Etendu DependencyProperty et DependencyPropertyMetadata pour fournir adhérer à IName et ICaption, respectivement. - CaptionCache sait retrouver des Caption à partir des diverses sources de données (DependencyObject/Property/Metadata/etc.).
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@5972 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Support.Tests/CaptionCacheTest.cs
+++ Epsitec.Cresus/Common.Support.Tests/CaptionCacheTest.cs
@@ -14,36 +14,37 @@ namespace Epsitec.Common.Support
 		[SetUp]
 		public void SetUp()
 		{
+			this.manager_en = new ResourceManager (@"S:\Epsitec.Cresus\Common.Support.Tests");
+			this.manager_en.DefineDefaultModuleName ("Test");
+			this.manager_en.ActivePrefix = "file";
+			this.manager_en.ActiveCulture = Resources.FindSpecificCultureInfo ("en");
+
+			this.manager_fr = new ResourceManager (@"S:\Epsitec.Cresus\Common.Support.Tests");
+			this.manager_fr.DefineDefaultModuleName ("Test");
+			this.manager_fr.ActivePrefix = "file";
+			this.manager_fr.ActiveCulture = Resources.FindSpecificCultureInfo ("fr");
+		}
+
+		[Test]
+		public void CheckEmptyCaptions()
+		{
+			Assert.IsNull (CaptionCache.Instance.GetCaption (this.manager_en, -1));
+			Assert.IsNull (CaptionCache.Instance.GetCaption (this.manager_en, Druid.Empty));
 		}
 
 		[Test]
 		public void CheckFillCache()
 		{
-			ResourceManager manager_en;
-			ResourceManager manager_fr;
-
-			manager_en = new ResourceManager (@"S:\Epsitec.Cresus\Common.Support.Tests");
-			manager_en.DefineDefaultModuleName ("Test");
-			manager_en.ActivePrefix = "file";
-			manager_en.ActiveCulture = Resources.FindSpecificCultureInfo ("en");
-
-			manager_fr = new ResourceManager (@"S:\Epsitec.Cresus\Common.Support.Tests");
-			manager_fr.DefineDefaultModuleName ("Test");
-			manager_fr.ActivePrefix = "file";
-			manager_fr.ActiveCulture = Resources.FindSpecificCultureInfo ("fr");
-
 			Druid idA = Druid.Parse ("[4002]");
 			Druid idQ = Druid.Parse ("[4003]");
-
-			List<Caption> scrap = new List<Caption> ();
 
 			Caption captionA_en;
 			Caption captionQ_en;
 			Caption captionA_fr;
 			Caption captionQ_fr;
 
-			captionA_en = CaptionCache.Instance.GetCaption (manager_en, idA);
-			captionQ_en = CaptionCache.Instance.GetCaption (manager_en, idQ);
+			captionA_en = CaptionCache.Instance.GetCaption (this.manager_en, idA);
+			captionQ_en = CaptionCache.Instance.GetCaption (this.manager_en, idQ);
 			
 			Assert.AreEqual ("Pattern angle expressed in degrees.", captionA_en.Description);
 			Assert.AreEqual ("Quality coefficient.", captionQ_en.Description);
@@ -51,8 +52,8 @@ namespace Epsitec.Common.Support
 			Assert.AreEqual ("Pattern angle", Collection.Extract (captionA_en.SortedLabels, 2));
 			Assert.AreEqual ("Q", Collection.Extract (captionQ_en.SortedLabels, 0));
 
-			captionA_fr = CaptionCache.Instance.GetCaption (manager_fr, idA);
-			captionQ_fr = CaptionCache.Instance.GetCaption (manager_fr, idQ);
+			captionA_fr = CaptionCache.Instance.GetCaption (this.manager_fr, idA);
+			captionQ_fr = CaptionCache.Instance.GetCaption (this.manager_fr, idQ);
 
 			Assert.AreEqual ("Angle de rotation de la trame, exprimé en degrés.", captionA_fr.Description);
 			Assert.AreEqual ("Coefficient de Qualité.", captionQ_fr.Description);
@@ -60,15 +61,15 @@ namespace Epsitec.Common.Support
 			Assert.AreEqual ("Angle de la trame", Collection.Extract (captionA_fr.SortedLabels, 2));
 			Assert.AreEqual ("Q", Collection.Extract (captionQ_fr.SortedLabels, 0));
 
-			Assert.AreEqual (captionA_en, CaptionCache.Instance.GetCaption (manager_en, idA));
-			Assert.AreEqual (captionA_en, CaptionCache.Instance.GetCaption (manager_en, idA.ToLong ()));
-			Assert.AreEqual (captionQ_en, CaptionCache.Instance.GetCaption (manager_en, idQ));
-			Assert.AreEqual (captionQ_en, CaptionCache.Instance.GetCaption (manager_en, idQ.ToLong ()));
+			Assert.AreEqual (captionA_en, CaptionCache.Instance.GetCaption (this.manager_en, idA));
+			Assert.AreEqual (captionA_en, CaptionCache.Instance.GetCaption (this.manager_en, idA.ToLong ()));
+			Assert.AreEqual (captionQ_en, CaptionCache.Instance.GetCaption (this.manager_en, idQ));
+			Assert.AreEqual (captionQ_en, CaptionCache.Instance.GetCaption (this.manager_en, idQ.ToLong ()));
 
-			Assert.AreEqual (captionA_fr, CaptionCache.Instance.GetCaption (manager_fr, idA));
-			Assert.AreEqual (captionA_fr, CaptionCache.Instance.GetCaption (manager_fr, idA.ToLong ()));
-			Assert.AreEqual (captionQ_fr, CaptionCache.Instance.GetCaption (manager_fr, idQ));
-			Assert.AreEqual (captionQ_fr, CaptionCache.Instance.GetCaption (manager_fr, idQ.ToLong ()));
+			Assert.AreEqual (captionA_fr, CaptionCache.Instance.GetCaption (this.manager_fr, idA));
+			Assert.AreEqual (captionA_fr, CaptionCache.Instance.GetCaption (this.manager_fr, idA.ToLong ()));
+			Assert.AreEqual (captionQ_fr, CaptionCache.Instance.GetCaption (this.manager_fr, idQ));
+			Assert.AreEqual (captionQ_fr, CaptionCache.Instance.GetCaption (this.manager_fr, idQ.ToLong ()));
 
 			Assert.AreEqual (4, CaptionCache.Instance.DebugCountLiveCaptions ());
 
@@ -88,5 +89,8 @@ namespace Epsitec.Common.Support
 
 			Assert.AreEqual (2, CaptionCache.Instance.DebugCountLiveCaptions ());
 		}
+		
+		ResourceManager manager_en;
+		ResourceManager manager_fr;
 	}
 }
