72d881bfd813d5a61888933dc52717c261583757
Michael Walz <walz@epsitec.ch>
2006-06-21 15:43:21 +0000

    - Copier/coller: insertion avec tabulateur fonctionne - Ctrl-X et Ctrl-C ne plantent plus lorsqu'aucune sélection n'est active.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@6043 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Text.Exchange/EpsitecFormat.cs
+++ Epsitec.Cresus/Common.Text.Exchange/EpsitecFormat.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms;
 
+// La classe EpsitecFormat ne sert qu'à pouvoir stocker le format presse-papier interne (qui existe sous forme de string)
+// dans le presse papier Windows. C'est nécessaire à cause de la gestion particulière du presse-papier avec .Net
+
+// Responsable: Michael Walz
+
+
 namespace Epsitec.Common.Text.Exchange
 {
 	[Serializable ()]
