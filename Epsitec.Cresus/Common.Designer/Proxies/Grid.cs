42bb67ed6f5fa91ee7b47825b2d4abc90e695d96
Daniel Roux <roux@epsitec.ch>
2006-06-27 09:18:19 +0000

    - Un nouveau Proxies.Grid permet de choisir le nombre de lignes/colonnes d'un tableau. - Comme il semble manquer des choses pour gérer des propriétés 'int', j'ai utilisé le type 'double' avec des cast !
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@6103 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Designer/Proxies/Grid.cs
@@ -0,0 +1,148 @@
+using System.Collections.Generic;
+
+using Epsitec.Common.Types;
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Drawing;
+
+namespace Epsitec.Common.Designer.Proxies
+{
+	public class Grid : Abstract
+	{
+		public Grid(ProxyManager manager) : base (manager)
+		{
+		}
+
+		public override int Rank
+		{
+			//	Retourne le rang de ce proxy parmi la liste de tous les proxies.
+			//	Plus le numéro est petit, plus le proxy apparaîtra haut dans la
+			//	liste.
+			get
+			{
+				return 4;
+			}
+		}
+
+		public override string IconName
+		{
+			get
+			{
+				return "PropertyGrid";
+			}
+		}
+
+		public double GridColumnsCount
+		{
+			get
+			{
+				return (double) this.GetValue(Grid.GridColumnsCountProperty);
+			}
+			set
+			{
+				this.SetValue(Grid.GridColumnsCountProperty, value);
+			}
+		}
+
+		public double GridRowsCount
+		{
+			get
+			{
+				return (double) this.GetValue(Grid.GridRowsCountProperty);
+			}
+			set
+			{
+				this.SetValue(Grid.GridRowsCountProperty, value);
+			}
+		}
+
+
+		protected override void InitialisePropertyValues()
+		{
+			//	Cette méthode est appelée par Proxies.Abstract quand on connecte
+			//	le premier widget avec le proxy.
+			
+			//	Recopie localement les diverses propriétés du widget sélectionné
+			//	pour pouvoir ensuite travailler dessus :
+			if (this.ObjectModifier.AreChildrenGrid(this.DefaultWidget))
+			{
+				int columns = this.ObjectModifier.GetGridColumnsCount(this.DefaultWidget);
+				int rows    = this.ObjectModifier.GetGridRowsCount(this.DefaultWidget);
+
+				this.GridColumnsCount = (double) columns;
+				this.GridRowsCount    = (double) rows;
+			}
+		}
+
+		private void NotifyColumnsCountChanged(double columns)
+		{
+			//	Cette méthode est appelée à la suite de la modification d'une de nos propriétés
+			//	de définition pour permettre de mettre à jour les widgets connectés.
+			if (this.IsNotSuspended)
+			{
+				this.SuspendChanges();
+
+				try
+				{
+					foreach (Widget obj in this.Widgets)
+					{
+						this.ObjectModifier.SetGridColumnsCount(obj, (int) columns);
+					}
+				}
+				finally
+				{
+					this.ResumeChanges();
+				}
+			}
+		}
+
+		private void NotifyRowsCountChanged(double rows)
+		{
+			//	Cette méthode est appelée à la suite de la modification d'une de nos propriétés
+			//	de définition pour permettre de mettre à jour les widgets connectés.
+			if (this.IsNotSuspended)
+			{
+				this.SuspendChanges();
+
+				try
+				{
+					foreach (Widget obj in this.Widgets)
+					{
+						this.ObjectModifier.SetGridRowsCount(obj, (int) rows);
+					}
+				}
+				finally
+				{
+					this.ResumeChanges();
+				}
+			}
+		}
+
+		private static void NotifyGridColumnsCountChanged(DependencyObject o, object oldValue, object newValue)
+		{
+			double value = (double) newValue;
+			Grid that = (Grid) o;
+			that.NotifyColumnsCountChanged(value);
+		}
+
+		private static void NotifyGridRowsCountChanged(DependencyObject o, object oldValue, object newValue)
+		{
+			double value = (double) newValue;
+			Grid that = (Grid) o;
+			that.NotifyRowsCountChanged(value);
+		}
+
+
+		static Grid()
+		{
+			Grid.GridColumnsCountProperty.DefaultMetadata.DefineNamedType(ProxyManager.GridNumericType);
+			Grid.GridRowsCountProperty.DefaultMetadata.DefineNamedType(ProxyManager.GridNumericType);
+
+			Grid.GridColumnsCountProperty.DefaultMetadata.DefineCaptionId(new Support.Druid("[100J]").ToLong());
+			Grid.GridRowsCountProperty.DefaultMetadata.DefineCaptionId(new Support.Druid("[100K]").ToLong());
+		}
+
+
+		public static readonly DependencyProperty GridColumnsCountProperty = DependencyProperty.Register("GridColumnsCount", typeof(double), typeof(Grid), new DependencyPropertyMetadata(2.0, Grid.NotifyGridColumnsCountChanged));
+		public static readonly DependencyProperty GridRowsCountProperty	   = DependencyProperty.Register("GridRowsCount",    typeof(double), typeof(Grid), new DependencyPropertyMetadata(2.0, Grid.NotifyGridRowsCountChanged));
+	}
+}
