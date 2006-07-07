981c62c66220fa3e132de47e96f2e4773bd2437a
Daniel Roux <roux@epsitec.ch>
2006-07-07 13:57:45 +0000

    - Préparation de 3 nouveaux Viewers Captions, Commands et Types.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@6258 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Designer/Viewers/Types.cs
@@ -0,0 +1,237 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+using Epsitec.Common.Drawing;
+
+namespace Epsitec.Common.Designer.Viewers
+{
+	/// <summary>
+	/// Permet de représenter les ressources d'un module.
+	/// </summary>
+	public class Types : Abstract
+	{
+		public Types(Module module, PanelsContext context) : base(module, context)
+		{
+			int tabIndex = 0;
+
+			Widget left = new Widget(this);
+			left.MinWidth = 80;
+			left.MaxWidth = 400;
+			left.PreferredWidth = 200;
+			left.Dock = DockStyle.Left;
+			left.Padding = new Margins(10, 10, 10, 10);
+
+			this.labelEdit = new TextFieldEx(left);
+			this.labelEdit.Name = "LabelEdit";
+			this.labelEdit.Margins = new Margins(0, 0, 10, 0);
+			this.labelEdit.Dock = DockStyle.Bottom;
+			this.labelEdit.EditionAccepted += new EventHandler(this.HandleTextChanged);
+			this.labelEdit.KeyboardFocusChanged += new EventHandler<Epsitec.Common.Types.DependencyPropertyChangedEventArgs>(this.HandleEditKeyboardFocusChanged);
+			this.labelEdit.TabIndex = tabIndex++;
+			this.labelEdit.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+			this.labelEdit.Visibility = (this.module.Mode == DesignerMode.Build);
+
+			this.array = new MyWidgets.StringArray(left);
+			this.array.Columns = 1;
+			this.array.SetColumnsRelativeWidth(0, 1.00);
+			this.array.SetDynamicsToolTips(0, true);
+			this.array.Margins = new Margins(0, 0, 0, 0);
+			this.array.Dock = DockStyle.Fill;
+			this.array.CellCountChanged += new EventHandler (this.HandleArrayCellCountChanged);
+			this.array.SelectedRowChanged += new EventHandler(this.HandleArraySelectedRowChanged);
+			this.array.TabIndex = tabIndex++;
+			this.array.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+
+			VSplitter splitter = new VSplitter(this);
+			splitter.Dock = DockStyle.Left;
+			VSplitter.SetAutoCollapseEnable(left, true);
+
+			this.edit = new TextFieldMulti(this);
+			this.edit.TextLayout.DefaultFont = Font.GetFont("Courier New", "Regular");
+			this.edit.Margins = new Margins(10, 10, 10, 10);
+			this.edit.Dock = DockStyle.Fill;
+			this.edit.EditionAccepted += new EventHandler(this.HandleTextChanged);
+			this.edit.KeyboardFocusChanged += new EventHandler<Epsitec.Common.Types.DependencyPropertyChangedEventArgs>(this.HandleEditKeyboardFocusChanged);
+			this.edit.TabIndex = tabIndex++;
+			this.edit.TabNavigation = Widget.TabNavigationMode.ActivateOnTab;
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				this.array.CellCountChanged -= new EventHandler (this.HandleArrayCellCountChanged);
+				this.array.SelectedRowChanged -= new EventHandler(this.HandleArraySelectedRowChanged);
+
+				this.labelEdit.EditionAccepted -= new EventHandler(this.HandleTextChanged);
+				this.labelEdit.KeyboardFocusChanged -= new EventHandler<Epsitec.Common.Types.DependencyPropertyChangedEventArgs>(this.HandleEditKeyboardFocusChanged);
+
+				this.edit.EditionAccepted -= new EventHandler(this.HandleTextChanged);
+				this.edit.KeyboardFocusChanged -= new EventHandler<Epsitec.Common.Types.DependencyPropertyChangedEventArgs>(this.HandleEditKeyboardFocusChanged);
+			}
+
+			base.Dispose(disposing);
+		}
+
+
+		public override void DoSearch(string search, Searcher.SearchingMode mode)
+		{
+			//	Effectue une recherche.
+		}
+
+		public override void DoCount(string search, Searcher.SearchingMode mode)
+		{
+			//	Effectue un comptage.
+		}
+
+		public override void DoReplace(string search, string replace, Searcher.SearchingMode mode)
+		{
+			//	Effectue un remplacement.
+		}
+
+		public override void DoReplaceAll(string search, string replace, Searcher.SearchingMode mode)
+		{
+			//	Effectue un 'remplacer tout'.
+		}
+
+		public override void DoModification(string name)
+		{
+			//	Change la ressource modifiée visible.
+		}
+
+		public override void DoDelete()
+		{
+			//	Supprime la ressource sélectionnée.
+		}
+
+		public override void DoDuplicate(bool duplicate)
+		{
+			//	Duplique la ressource sélectionnée.
+		}
+
+		public override void DoMove(int direction)
+		{
+			//	Déplace la ressource sélectionnée.
+		}
+
+		public override void DoNewCulture()
+		{
+			//	Crée une nouvelle culture.
+		}
+
+		public override void DoDeleteCulture()
+		{
+			//	Supprime la culture courante.
+		}
+
+		public override void DoClipboard(string name)
+		{
+			//	Effectue une action avec le bloc-notes.
+		}
+
+		public override void DoFont(string name)
+		{
+			//	Effectue une modification de typographie.
+		}
+
+
+		public override void UpdateCommands()
+		{
+			//	Met à jour les commandes en fonction de la ressource sélectionnée.
+			base.UpdateCommands();
+
+			int sel = this.array.SelectedRow;
+			int count = this.druidsIndex.Count;
+			bool build = (this.module.Mode == DesignerMode.Build);
+
+			this.GetCommandState("NewCulture").Enable = false;
+			this.GetCommandState("DeleteCulture").Enable = false;
+
+			this.GetCommandState("Search").Enable = false;
+			this.GetCommandState("SearchPrev").Enable = false;
+			this.GetCommandState("SearchNext").Enable = false;
+
+			this.GetCommandState("ModificationPrev").Enable = false;
+			this.GetCommandState("ModificationNext").Enable = false;
+			this.GetCommandState("ModificationAll").Enable = false;
+			this.GetCommandState("ModificationClear").Enable = false;
+
+			this.GetCommandState("FontBold").Enable = false;
+			this.GetCommandState("FontItalic").Enable = false;
+			this.GetCommandState("FontUnderlined").Enable = false;
+			this.GetCommandState("Glyphs").Enable = false;
+
+			this.GetCommandState("PanelDelete").Enable = false;
+			this.GetCommandState("PanelDuplicate").Enable = false;
+			this.GetCommandState("PanelDeselectAll").Enable = false;
+			this.GetCommandState("PanelSelectAll").Enable = false;
+			this.GetCommandState("PanelSelectInvert").Enable = false;
+
+			this.GetCommandState("PanelShowGrid").Enable = false;
+			this.GetCommandState("PanelShowConstrain").Enable = false;
+			this.GetCommandState("PanelShowAttachment").Enable = false;
+			this.GetCommandState("PanelShowExpand").Enable = false;
+			this.GetCommandState("PanelShowZOrder").Enable = false;
+			this.GetCommandState("PanelShowTabIndex").Enable = false;
+			this.GetCommandState("PanelRun").Enable = false;
+
+			this.GetCommandState("MoveLeft").Enable = false;
+			this.GetCommandState("MoveRight").Enable = false;
+			this.GetCommandState("MoveDown").Enable = false;
+			this.GetCommandState("MoveUp").Enable = false;
+
+			this.GetCommandState("AlignLeft").Enable = false;
+			this.GetCommandState("AlignCenterX").Enable = false;
+			this.GetCommandState("AlignRight").Enable = false;
+			this.GetCommandState("AlignTop").Enable = false;
+			this.GetCommandState("AlignCenterY").Enable = false;
+			this.GetCommandState("AlignBottom").Enable = false;
+			this.GetCommandState("AlignBaseLine").Enable = false;
+			this.GetCommandState("AdjustWidth").Enable = false;
+			this.GetCommandState("AdjustHeight").Enable = false;
+			this.GetCommandState("AlignGrid").Enable = false;
+
+			this.GetCommandState("OrderUpAll").Enable = false;
+			this.GetCommandState("OrderDownAll").Enable = false;
+			this.GetCommandState("OrderUpOne").Enable = false;
+			this.GetCommandState("OrderDownOne").Enable = false;
+
+			this.GetCommandState("TabIndexClear").Enable = false;
+			this.GetCommandState("TabIndexRenum").Enable = false;
+			this.GetCommandState("TabIndexLast").Enable = false;
+			this.GetCommandState("TabIndexPrev").Enable = false;
+			this.GetCommandState("TabIndexNext").Enable = false;
+			this.GetCommandState("TabIndexFirst").Enable = false;
+
+			this.module.MainWindow.UpdateInfoCurrentModule();
+			this.module.MainWindow.UpdateInfoAccess();
+			this.module.MainWindow.UpdateInfoViewer();
+		}
+
+
+		void HandleArrayCellCountChanged(object sender)
+		{
+			//	Le nombre de lignes a changé.
+			//?this.UpdateArray();
+		}
+
+		void HandleArraySelectedRowChanged(object sender)
+		{
+			//	La ligne sélectionnée a changé.
+			this.UpdateCommands();
+		}
+
+		void HandleTextChanged(object sender)
+		{
+			//	Un texte éditable a changé.
+			if ( this.ignoreChange )  return;
+
+			AbstractTextField edit = sender as AbstractTextField;
+		}
+
+
+		protected TextFieldEx				labelEdit;
+		protected TextFieldMulti			edit;
+	}
+}
