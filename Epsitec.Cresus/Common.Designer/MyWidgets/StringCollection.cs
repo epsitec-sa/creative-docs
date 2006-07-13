9177e13c9012181aa117e95d682317487c29738c
Daniel Roux <roux@epsitec.ch>
2006-07-13 06:37:24 +0000

    - La deuxième colonne contient un GlyphButton avec une flèche ">" pour marquer la ligne sélectionnée.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@6299 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/MyWidgets/StringCollection.cs
+++ Epsitec.Cresus/Common.Designer/MyWidgets/StringCollection.cs
@@ -20,16 +20,12 @@ namespace Epsitec.Common.Designer.MyWidgets
 			this.InternalState |= InternalState.Focusable;
 			this.InternalState |= InternalState.Engageable;
 
-			this.grid = new GridLayoutEngine();
-			this.grid.ColumnDefinitions.Add(new ColumnDefinition(new GridLength(20, GridUnitType.Absolute)));
-			this.grid.ColumnDefinitions.Add(new ColumnDefinition(new GridLength(1, GridUnitType.Proportional)));
-			this.grid.RowDefinitions.Add(new RowDefinition());
-			this.grid.RowDefinitions[0].BottomBorder = 3;
-			LayoutEngine.SetLayoutEngine(this, this.grid);
-			this.CreateToolbar();
-
 			this.staticTexts = new List<StaticText>();
+			this.glyphButtons = new List<GlyphButton>();
 			this.textFields = new List<TextField>();
+
+			this.CreateGrid();
+			this.CreateToolbar();
 		}
 
 		public StringCollection(Widget embedder) : this()
@@ -54,10 +50,27 @@ namespace Epsitec.Common.Designer.MyWidgets
 		}
 
 
+		protected void CreateGrid()
+		{
+			//	Crée le tableau principal.
+			this.grid = new GridLayoutEngine();
+
+			this.grid.ColumnDefinitions.Add(new ColumnDefinition(new GridLength(20, GridUnitType.Absolute)));
+			this.grid.ColumnDefinitions.Add(new ColumnDefinition(new GridLength(20, GridUnitType.Absolute)));
+			this.grid.ColumnDefinitions.Add(new ColumnDefinition(new GridLength(1, GridUnitType.Proportional)));
+			this.grid.ColumnDefinitions[1].RightBorder = -1;
+			
+			this.grid.RowDefinitions.Add(new RowDefinition());
+			this.grid.RowDefinitions[0].BottomBorder = 3;
+			
+			LayoutEngine.SetLayoutEngine(this, this.grid);
+		}
+
 		protected void CreateToolbar()
 		{
+			//	Crée la barre d'outils dans la première ligne du tableau.
 			Widget toolbar = new Widget();
-			GridLayoutEngine.SetColumn(toolbar, 1);
+			GridLayoutEngine.SetColumn(toolbar, 2);
 			GridLayoutEngine.SetRow(toolbar, 0);
 			this.Children.Add(toolbar);
 
@@ -104,25 +117,41 @@ namespace Epsitec.Common.Designer.MyWidgets
 					this.Children.Add(fix);
 					this.staticTexts.Add(fix);
 
+					GlyphButton button = new GlyphButton();
+					button.GlyphShape = GlyphShape.None;
+					button.Pressed += new MessageEventHandler(this.HandleButtonPressed);
+					GridLayoutEngine.SetColumn(button, 1);
+					GridLayoutEngine.SetRow(button, count);
+					this.Children.Add(button);
+					this.glyphButtons.Add(button);
+
 					TextField field = new TextField();
+					field.KeyboardFocusChanged += new EventHandler<Epsitec.Common.Types.DependencyPropertyChangedEventArgs>(this.HandleTextFocusChanged);
 					field.VerticalAlignment = VerticalAlignment.BaseLine;
-					GridLayoutEngine.SetColumn(field, 1);
+					GridLayoutEngine.SetColumn(field, 2);
 					GridLayoutEngine.SetRow(field, count);
 					this.Children.Add(field);
 					this.textFields.Add(field);
 				}
 				else
 				{
+					this.glyphButtons[count-2].Pressed -= new MessageEventHandler(this.HandleButtonPressed);
+					this.textFields[count-2].KeyboardFocusChanged -= new EventHandler<Epsitec.Common.Types.DependencyPropertyChangedEventArgs>(this.HandleTextFocusChanged);
+
 					this.Children.Remove(this.staticTexts[count-2]);
+					this.Children.Remove(this.glyphButtons[count-2]);
 					this.Children.Remove(this.textFields[count-2]);
 
 					this.grid.RowDefinitions.RemoveAt(count-1);
+
 					this.staticTexts.RemoveAt(count-2);
+					this.glyphButtons.RemoveAt(count-2);
 					this.textFields.RemoveAt(count-2);
 				}
 			}
 
 			System.Diagnostics.Debug.Assert(this.collection.Count == this.staticTexts.Count);
+			System.Diagnostics.Debug.Assert(this.collection.Count == this.glyphButtons.Count);
 			System.Diagnostics.Debug.Assert(this.collection.Count == this.textFields.Count);
 		}
 
@@ -139,6 +168,51 @@ namespace Epsitec.Common.Designer.MyWidgets
 		}
 
 
+		protected int SelectedRow
+		{
+			//	Ligne sélectionnée dans le tableau.
+			get
+			{
+				if (this.selectedRow >= this.textFields.Count)
+				{
+					return -1;
+				}
+
+				return this.selectedRow;
+			}
+			set
+			{
+				if (this.selectedRow != value)
+				{
+					this.selectedRow = value;
+
+					//	Met à jour les flèches ">" dans les boutons de la 2ème colonne.
+					for (int i=0; i<this.textFields.Count; i++)
+					{
+						this.glyphButtons[i].GlyphShape = (this.selectedRow == i) ? GlyphShape.ArrowRight : GlyphShape.None;
+					}
+				}
+			}
+		}
+
+		protected void HandleButtonPressed(object sender, MessageEventArgs e)
+		{
+			GlyphButton button = sender as GlyphButton;
+			this.SelectedRow = glyphButtons.IndexOf(button);
+		}
+
+		protected void HandleTextFocusChanged(object sender, Epsitec.Common.Types.DependencyPropertyChangedEventArgs e)
+		{
+			//	Appelé lorsqu'une ligne éditable voit son focus changer.
+			TextField field = sender as TextField;
+			bool focused = (bool) e.NewValue;
+
+			if (focused)
+			{
+				this.SelectedRow = textFields.IndexOf(field);
+			}
+		}
+
 
 		protected ICollection<string>		collection;
 		protected GridLayoutEngine			grid;
@@ -147,6 +221,8 @@ namespace Epsitec.Common.Designer.MyWidgets
 		protected IconButton				buttonPrev;
 		protected IconButton				buttonNext;
 		protected List<StaticText>			staticTexts;
+		protected List<GlyphButton>			glyphButtons;
 		protected List<TextField>			textFields;
+		protected int						selectedRow = -1;
 	}
 }
