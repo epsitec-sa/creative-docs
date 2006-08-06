33b7ffcd9c9bd3247659e7f688c16dc3c387318a
Pierre Arnaud <arnaud@opac.ch>
2006-08-06 17:05:07 +0000

    - StringType et AbstractNumericType stockent leurs paramètres dans Caption.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@6426 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Support.Tests/ImageProviderTest.cs
+++ Epsitec.Cresus/Common.Support.Tests/ImageProviderTest.cs
@@ -5,18 +5,20 @@ namespace Epsitec.Common.Support
 {
 	[TestFixture] public class ImageProviderTest
 	{
-		[SetUp] public void Initialise()
+		[SetUp]
+		public void Initialise()
 		{
 			Epsitec.Common.Document.Engine.Initialise ();
 			Epsitec.Common.Widgets.Widget.Initialise ();
 		}
-		
-		[Test] public void CheckGetImage()
+
+		[Test]
+		public void CheckGetImage()
 		{
 			Drawing.Image im1 = Support.ImageProvider.Default.GetImage ("file:images/open.png", Support.Resources.DefaultManager);
 			Drawing.Image im2 = Support.ImageProvider.Default.GetImage ("file:images/open.icon", Support.Resources.DefaultManager);
 			Drawing.Image im3 = Support.ImageProvider.Default.GetImage ("file:images/non-existing-image.png", Support.Resources.DefaultManager);
-			
+
 			Assert.IsNotNull (im1);
 			Assert.IsNotNull (im2);
 			Assert.IsNull (im3);
@@ -35,16 +37,17 @@ namespace Epsitec.Common.Support
 			names.Clear ();
 			names.AddRange (Support.ImageProvider.Default.GetImageNames ("manifest", Support.Resources.DefaultManager));
 
-			Assert.AreEqual (@"manifest:Epsitec.Common.Widgets.Images.ActiveNo.icon", names[0]);
+			Assert.AreEqual (@"manifest:Epsitec.Common.Dialogs.Images.Information.icon", names[0]);
 		}
-		
-		[Test] public void CheckGetManifestResourceNames()
+
+		[Test]
+		public void CheckGetManifestResourceNames()
 		{
 			System.Text.RegularExpressions.Regex regex = RegexFactory.FromSimpleJoker ("*.icon", RegexFactory.Options.IgnoreCase);
 			string[] names = ImageProvider.GetManifestResourceNames (regex);
-			
+
 			int i = 0;
-			
+
 			foreach (string name in names)
 			{
 				System.Console.Out.WriteLine ("{0}: {1}", ++i, name);
