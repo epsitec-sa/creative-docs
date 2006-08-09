c541593a3ff04807120667056e562b3efd04d8d1
Pierre Arnaud <arnaud@opac.ch>
2006-08-09 07:37:54 +0000

    - Ajouté fichier oublié (désolé).
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@6448 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/Collections/EnumValueCollection.cs
@@ -0,0 +1,15 @@
+//	Copyright © 2005, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+namespace Epsitec.Common.Types.Collections
+{
+	/// <summary>
+	/// </summary>
+	public class EnumValueCollection : HostedDependencyObjectList<EnumValue>
+	{
+		public EnumValueCollection()
+			: base (null)
+		{
+		}
+	}
+}
