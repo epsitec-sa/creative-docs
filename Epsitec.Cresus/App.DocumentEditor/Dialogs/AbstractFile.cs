a128c06052e91eed1771521e3c537229a362368d
Daniel Roux <roux@epsitec.ch>
2006-09-05 14:44:16 +0000

    - Déplacement de presque tout dans AbstractFile.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@6716 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/App.DocumentEditor/Dialogs/AbstractFile.cs
+++ Epsitec.Cresus/App.DocumentEditor/Dialogs/AbstractFile.cs
@@ -69,6 +69,7 @@ namespace Epsitec.App.DocumentEditor.Dialogs
 
 		protected void CreateResizer()
 		{
+			//	Crée l'icône en bas à droite pour signaler que la fenêtre est redimensionnable.
 			ResizeKnob resize = new ResizeKnob(this.window.Root);
 			resize.Anchor = AnchorStyles.BottomRight;
 			resize.Margins = new Margins(0, -8, 0, -8);
@@ -77,6 +78,7 @@ namespace Epsitec.App.DocumentEditor.Dialogs
 
 		protected void CreateTable(double cellHeight)
 		{
+			//	Crée la table principale contenant la liste des fichiers et dossiers.
 			CellArrayStyles sh = CellArrayStyles.Stretch | CellArrayStyles.Separator | CellArrayStyles.Header | CellArrayStyles.Mobile;
 			CellArrayStyles sv = CellArrayStyles.ScrollNorm | CellArrayStyles.Separator | CellArrayStyles.SelectLine;
 
@@ -102,6 +104,8 @@ namespace Epsitec.App.DocumentEditor.Dialogs
 
 		protected void CreateRename()
 		{
+			//	Crée le widget permettant de renommer un fichier/dossier.
+			//	Normalement, ce widget est caché.
 			this.fieldRename = new TextFieldEx(this.window.Root);
 			this.fieldRename.Visibility = false;
 			this.fieldRename.ButtonShowCondition = ShowCondition.Always;
@@ -110,9 +114,72 @@ namespace Epsitec.App.DocumentEditor.Dialogs
 			this.fieldRename.IsFocusedChanged += new EventHandler<Epsitec.Common.Types.DependencyPropertyChangedEventArgs>(this.HandleRenameFocusChanged);
 		}
 
+		protected void CreateAccess()
+		{
+			//	Crée la partie controlant le chemin d'accès.
+			Widget access = new Widget(this.window.Root);
+			access.PreferredHeight = 20;
+			access.Margins = new Margins(0, 0, 0, 8);
+			access.Dock = DockStyle.Top;
+
+			StaticText label = new StaticText(access);
+			label.Text = Res.Strings.Dialog.Open.LabelPath;
+			label.PreferredWidth = 80;
+			label.Dock = DockStyle.Left;
+
+			this.fieldPath = new TextFieldCombo(access);
+			this.fieldPath.IsReadOnly = true;
+			this.fieldPath.Dock = DockStyle.Fill;
+			this.fieldPath.Margins = new Margins(0, 5, 0, 0);
+			this.fieldPath.ComboOpening += new EventHandler<CancelEventArgs>(this.HandleFieldPathComboOpening);
+			this.fieldPath.ComboClosed += new EventHandler(this.HandleFieldPathComboClosed);
+			this.fieldPath.TextChanged += new EventHandler(this.HandleFieldPathTextChanged);
+
+			this.buttonDelete = new IconButton(access);
+			this.buttonDelete.IconName = Misc.Icon("FileDelete");
+			this.buttonDelete.Dock = DockStyle.Right;
+			this.buttonDelete.Clicked += new MessageEventHandler(this.HandleButtonDeleteClicked);
+			ToolTip.Default.SetToolTip(this.buttonDelete, Res.Strings.Dialog.Open.Delete);
+
+			this.buttonRename = new IconButton(access);
+			this.buttonRename.IconName = Misc.Icon("FileRename");
+			this.buttonRename.Dock = DockStyle.Right;
+			this.buttonRename.Clicked += new MessageEventHandler(this.HandleButtonRenameClicked);
+			ToolTip.Default.SetToolTip(this.buttonRename, Res.Strings.Dialog.Open.Rename);
+
+			this.buttonNew = new IconButton(access);
+			this.buttonNew.IconName = Misc.Icon("NewDirectory");
+			this.buttonNew.Dock = DockStyle.Right;
+			this.buttonNew.Clicked += new MessageEventHandler(this.HandleButtonNewClicked);
+			ToolTip.Default.SetToolTip(this.buttonNew, Res.Strings.Dialog.Open.NewDirectory);
+
+			this.buttonParent = new IconButton(access);
+			this.buttonParent.IconName = Misc.Icon("ParentDirectory");
+			this.buttonParent.Dock = DockStyle.Right;
+			this.buttonParent.Clicked += new MessageEventHandler(this.HandleButtonParentClicked);
+			ToolTip.Default.SetToolTip(this.buttonParent, Res.Strings.Dialog.Open.ParentDirectory);
+		}
+
+		protected void CreateFilename()
+		{
+			Widget file = new Widget(this.window.Root);
+			file.PreferredHeight = 20;
+			file.Margins = new Margins(0, 0, 8, 0);
+			file.Dock = DockStyle.Bottom;
+
+			StaticText label = new StaticText(file);
+			label.Text = Res.Strings.Dialog.Open.LabelDoc;
+			label.PreferredWidth = 80;
+			label.Dock = DockStyle.Left;
+
+			this.fieldFilename = new TextField(file);
+			this.fieldFilename.Dock = DockStyle.Fill;
+			this.fieldFilename.KeyboardFocusChanged += new EventHandler<Epsitec.Common.Types.DependencyPropertyChangedEventArgs>(this.HandleKeyboardFocusChanged);
+		}
+
 		protected void CreateFooter()
 		{
-			//	Crée le pied du dialogue.
+			//	Crée le pied du dialogue, avec les boutons 'ouvrir' et 'annuler'.
 			Widget footer = new Widget(this.window.Root);
 			footer.PreferredHeight = 22;
 			footer.Margins = new Margins(0, 0, 8, 0);
@@ -327,14 +394,26 @@ namespace Epsitec.App.DocumentEditor.Dialogs
 			}
 		}
 
-		protected virtual void UpdateButtons()
+		protected void UpdateButtons()
 		{
 			//	Met à jour les boutons en fonction du fichier sélectionné dans la liste.
+			if (this.buttonRename != null)
+			{
+				int sel = this.table.SelectedRow;
+				this.buttonRename.Enable = (sel != -1);
+				this.buttonDelete.Enable = (sel != -1);
+			}
 		}
 
-		protected virtual void UpdateInitialDirectory()
+		protected void UpdateInitialDirectory()
 		{
 			//	Met à jour le chemin d'accès.
+			if (this.fieldPath != null)
+			{
+				this.ignoreChanged = true;
+				this.fieldPath.Text = AbstractFile.RemoveStartingSpaces(AbstractFile.GetIllustredPath(this.initialDirectory));
+				this.ignoreChanged = false;
+			}
 		}
 
 
@@ -742,6 +821,102 @@ namespace Epsitec.App.DocumentEditor.Dialogs
 			}
 		}
 
+		private void HandleFieldPathComboOpening(object sender, CancelEventArgs e)
+		{
+			//	Le menu pour le chemin d'accès va être ouvert.
+			this.comboTexts = new List<string>();
+			this.comboDirectories = new List<string>();
+
+			System.IO.DriveInfo[] drives = System.IO.DriveInfo.GetDrives();
+
+			this.fieldPath.Items.Clear();
+
+			foreach (System.IO.DriveInfo drive in drives)
+			{
+				string text = AbstractFile.GetIllustredDriveString(drive);
+				this.fieldPath.Items.Add(text);
+				this.comboTexts.Add(text);
+				this.comboDirectories.Add(drive.Name);
+
+				if (this.initialDirectory.Length > 3 && this.initialDirectory.StartsWith(drive.Name))
+				{
+					string[] dirs = this.initialDirectory.Split('\\');
+					for (int i=1; i<dirs.Length; i++)
+					{
+						string dir = "";
+						text = "";
+						for (int j=0; j<=i; j++)
+						{
+							dir += dirs[j]+"\\";
+							text += "   ";
+						}
+						text += Misc.Image("FileTypeDirectory");
+						text += " ";
+						text += dirs[i];
+
+						this.fieldPath.Items.Add(text);
+						this.comboTexts.Add(text);
+
+						if (dir.Length > 3 && dir.EndsWith("\\"))
+						{
+							dir = dir.Substring(0, dir.Length-1);
+						}
+						this.comboDirectories.Add(dir);
+					}
+				}
+			}
+
+			this.comboSelected = -1;
+		}
+
+		private void HandleFieldPathComboClosed(object sender)
+		{
+			//	Le menu pour le chemin d'accès a été fermé.
+			if (this.comboSelected != -1)
+			{
+				this.InitialDirectory = this.comboDirectories[this.comboSelected];
+				this.UpdateTable(-1);
+			}
+		}
+
+		void HandleFieldPathTextChanged(object sender)
+		{
+			//	Le texte pour le chemin d'accès a changé.
+			if (this.ignoreChanged)
+			{
+				return;
+			}
+
+			this.ignoreChanged = true;
+			this.comboSelected = this.comboTexts.IndexOf(this.fieldPath.Text);
+			this.fieldPath.Text = AbstractFile.RemoveStartingSpaces(this.fieldPath.Text);
+			this.ignoreChanged = false;
+		}
+
+		private void HandleButtonParentClicked(object sender, MessageEventArgs e)
+		{
+			//	Le bouton 'dossier parent' a été cliqué.
+			this.ParentDirectory();
+		}
+
+		private void HandleButtonNewClicked(object sender, MessageEventArgs e)
+		{
+			//	Le bouton 'nouveau dossier' a été cliqué.
+			this.NewDirectory();
+		}
+
+		private void HandleButtonRenameClicked(object sender, MessageEventArgs e)
+		{
+			//	Le bouton 'renommer' a été cliqué.
+			this.RenameStarting();
+		}
+
+		private void HandleButtonDeleteClicked(object sender, MessageEventArgs e)
+		{
+			//	Le bouton 'supprimer' a été cliqué.
+			this.FileDelete();
+		}
+
 		private void HandleSliderChanged(object sender)
 		{
 			//	Slider pour la taille des miniatures changé.
@@ -1047,6 +1222,12 @@ namespace Epsitec.App.DocumentEditor.Dialogs
 
 		protected CellTable					table;
 		protected HSlider					slider;
+		protected TextFieldCombo			fieldPath;
+		protected IconButton				buttonParent;
+		protected IconButton				buttonNew;
+		protected IconButton				buttonRename;
+		protected IconButton				buttonDelete;
+		protected TextField					fieldFilename;
 		protected TextFieldEx				fieldRename;
 
 		protected string					fileExtension;
@@ -1060,5 +1241,8 @@ namespace Epsitec.App.DocumentEditor.Dialogs
 		protected int						renameSelected = -1;
 		protected Widget					focusedWidget;
 		protected bool						ignoreChanged = false;
+		protected List<string>				comboDirectories;
+		protected List<string>				comboTexts;
+		protected int						comboSelected;
 	}
 }
