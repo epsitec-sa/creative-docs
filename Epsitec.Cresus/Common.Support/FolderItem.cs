0f93c4b601368b3ee20b64d2361b21c84f30d969
Pierre Arnaud <arnaud@opac.ch>
2006-09-06 14:49:03 +0000

    - Commenté FolderItem.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@6739 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Support/FolderItem.cs
+++ Epsitec.Cresus/Common.Support/FolderItem.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace Epsitec.Common.Support
 {
+	/// <summary>
+	/// The <c>FolderItem</c> structure represents an item in a folder. This
+	/// can be a folder, a file or a virtual folder (provided by the shell).
+	/// </summary>
 	public struct FolderItem
 	{
 		internal FolderItem(Common.Drawing.Image icon, string displayName, string typeName, string fullPath, Platform.FolderItemHandle handle)
@@ -14,7 +18,11 @@ namespace Epsitec.Common.Support
 			this.fullPath = fullPath;
 			this.handle = handle;
 		}
-		
+
+		/// <summary>
+		/// Gets the icon for this item.
+		/// </summary>
+		/// <value>The icon or <c>null</c>.</value>
 		public Drawing.Image Icon
 		{
 			get
@@ -23,6 +31,10 @@ namespace Epsitec.Common.Support
 			}
 		}
 
+		/// <summary>
+		/// Gets the display name for the item.
+		/// </summary>
+		/// <value>The display name.</value>
 		public string DisplayName
 		{
 			get
@@ -31,6 +43,10 @@ namespace Epsitec.Common.Support
 			}
 		}
 
+		/// <summary>
+		/// Gets the type name for the item (e.g. "Folder", "Text File", ...).
+		/// </summary>
+		/// <value>The type name.</value>
 		public string TypeName
 		{
 			get
@@ -39,6 +55,11 @@ namespace Epsitec.Common.Support
 			}
 		}
 
+		/// <summary>
+		/// Gets the full path for the item. Some virtual elements don't have a
+		/// file path.
+		/// </summary>
+		/// <value>The full path.</value>
 		public string FullPath
 		{
 			get
@@ -47,11 +68,30 @@ namespace Epsitec.Common.Support
 			}
 		}
 
+		/// <summary>
+		/// Gets a value indicating whether this instance is purely virtual (i.e. it
+		/// has no full path and only exists as a virtual construct, such as "My Computer").
+		/// </summary>
+		/// <value>
+		/// 	<c>true</c> if this item is purely virtual; otherwise, <c>false</c>.
+		/// </value>
 		public bool IsVirtual
 		{
 			get
 			{
 				return string.IsNullOrEmpty (this.fullPath);
+			}
+		}
+
+		/// <summary>
+		/// Gets a value indicating whether this item is empty.
+		/// </summary>
+		/// <value><c>true</c> if this item is empty; otherwise, <c>false</c>.</value>
+		public bool IsEmpty
+		{
+			get
+			{
+				return this.displayName == null;
 			}
 		}
 
