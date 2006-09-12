8aa2cc99e4b35d712866ac91a02a7b72b11c3845
Pierre Arnaud <arnaud@opac.ch>
2006-09-12 14:00:20 +0000

    - Complété test pour FileManager. - Ajusté les "prerequisites" pour les installateurs MSI.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@6834 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Support.Tests/FileManagerTest.cs
+++ Epsitec.Cresus/Common.Support.Tests/FileManagerTest.cs
@@ -104,8 +104,9 @@ namespace Epsitec.Common.Support
 
 			foreach (FolderItem item in FileManager.GetFolderItems (FileManager.GetFolderItem (FolderId.Recent, FolderQueryMode.NoIcons), FolderQueryMode.NoIcons))
 			{
+				System.Console.Out.WriteLine ("{0}", item);
 				string resolvesTo = FileManager.ResolveShortcut (item, FolderQueryMode.NoIcons).FullPath;
-				System.Console.Out.WriteLine ("{0} --> {1}", item, resolvesTo);
+				System.Console.Out.WriteLine ("  --> {0}", resolvesTo);
 				
 				if (path == resolvesTo)
 				{
