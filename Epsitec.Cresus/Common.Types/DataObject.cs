8d4d1d7ecf3d3b25d7402f84b601631316a69944
Pierre Arnaud <arnaud@opac.ch>
2006-10-03 07:15:36 +0000

    - Commentaires et documentation.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@7105 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Types/DataObject.cs
+++ Epsitec.Cresus/Common.Types/DataObject.cs
@@ -1,22 +1,40 @@
 //	Copyright © 2006, EPSITEC SA, CH-1092 BELMONT, Switzerland
 //	Responsable: Pierre ARNAUD
 
-using System.Collections.Generic;
-
 [assembly: Epsitec.Common.Types.DependencyClass (typeof (Epsitec.Common.Types.DataObject))]
 
 namespace Epsitec.Common.Types
 {
+	/// <summary>
+	/// The <c>DataObject</c> is only used as a support for the <c>DataContext</c>
+	/// property.
+	/// </summary>
 	public abstract class DataObject : DependencyObject
 	{
+		/// <summary>
+		/// Gets the data context (an instance of <see cref="Binding"/>) associated with an object.
+		/// </summary>
+		/// <param name="o">The object to query.</param>
+		/// <returns>The data context.</returns>
 		public static Binding GetDataContext(DependencyObject o)
 		{
 			return o.GetValue (DataObject.DataContextProperty) as Binding;
 		}
+
+		/// <summary>
+		/// Sets the data context (an instance of <see cref="Binding"/>) associated with an object.
+		/// </summary>
+		/// <param name="o">The object.</param>
+		/// <param name="value">The data context.</param>
 		public static void SetDataContext(DependencyObject o, Binding value)
 		{
 			o.SetValue (DataObject.DataContextProperty, value);
 		}
+
+		/// <summary>
+		/// Clears the data context associated with an object.
+		/// </summary>
+		/// <param name="o">The object to clear.</param>
 		public static void ClearDataContext(DependencyObject o)
 		{
 			o.ClearValue (DataObject.DataContextProperty);
