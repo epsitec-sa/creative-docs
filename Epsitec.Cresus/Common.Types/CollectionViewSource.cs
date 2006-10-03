1ebd19a0c7659f5986188559ba982c18546c1011
Pierre Arnaud <arnaud@opac.ch>
2006-10-03 15:56:53 +0000

    - Ajouté début de l'implémentation de CollectionViewSource.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@7117 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/CollectionViewSource.cs
@@ -0,0 +1,88 @@
+//	Copyright © 2006, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+namespace Epsitec.Common.Types
+{
+	/// <summary>
+	/// The <c>CollectionViewSource</c> class represents a view of a data
+	/// collection.
+	/// </summary>
+	public class CollectionViewSource : DependencyObject
+	{
+		public CollectionViewSource()
+		{
+			this.sortDescriptions = new Collections.HostedList<SortDescription> (this.HandleSortInsertion, this.HandleSortRemoval);
+			this.groupDescriptions = new Collections.HostedList<AbstractGroupDescription> (this.HandleGroupInsertion, this.HandleGroupRemoval);
+		}
+
+		public object Source
+		{
+			get
+			{
+				return this.source;
+			}
+			set
+			{
+				if (this.source != value)
+				{
+					object oldValue = this.source;
+					object newValue = value;
+
+					this.source = newValue;
+
+					this.InvalidateProperty (CollectionViewSource.SourceProperty, oldValue, newValue);
+				}
+			}
+		}
+
+		public Collections.ObservableList<SortDescription> SortDescriptions
+		{
+			get
+			{
+				return this.sortDescriptions;
+			}
+		}
+
+		public Collections.ObservableList<AbstractGroupDescription> GroupDescriptions
+		{
+			get
+			{
+				return this.groupDescriptions;
+			}
+		}
+
+		private void HandleSortInsertion(SortDescription item)
+		{
+		}
+
+		private void HandleSortRemoval(SortDescription item)
+		{
+		}
+
+		private void HandleGroupInsertion(AbstractGroupDescription item)
+		{
+		}
+
+		private void HandleGroupRemoval(AbstractGroupDescription item)
+		{
+		}
+
+		private static object GetSourceValue(DependencyObject obj)
+		{
+			CollectionViewSource that = (CollectionViewSource) obj;
+			return that.Source;
+		}
+
+		private static void SetSourceValue(DependencyObject obj, object value)
+		{
+			CollectionViewSource that = (CollectionViewSource) obj;
+			that.Source = value;
+		}
+
+		public static readonly DependencyProperty SourceProperty = DependencyProperty.Register ("Source", typeof (object), typeof (CollectionViewSource), new DependencyPropertyMetadata (CollectionViewSource.GetSourceValue, CollectionViewSource.SetSourceValue));
+
+		private Collections.HostedList<SortDescription> sortDescriptions;
+		private Collections.HostedList<AbstractGroupDescription> groupDescriptions;
+		private object source;
+	}
+}
