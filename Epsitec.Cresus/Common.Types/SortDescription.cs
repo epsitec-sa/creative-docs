6e7c2923e3b930e3aa64c9d28b07e17335e533c5
Pierre Arnaud <arnaud@opac.ch>
2006-10-03 08:48:04 +0000

    - Ajouté ListSortDirection et SortDescription en vue du binding avec views.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@7107 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/SortDescription.cs
@@ -0,0 +1,145 @@
+//	Copyright © 2006, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Responsable: Pierre ARNAUD
+
+namespace Epsitec.Common.Types
+{
+	/// <summary>
+	/// The <c>SortDescription</c> structure defines the direction and the property
+	/// name to be used as the criteria for sorting a collection or view.
+	/// </summary>
+	[SerializationConverter (typeof (SortDescription.SerializationConverter))]
+	public struct SortDescription : System.IEquatable<SortDescription>
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SortDescription"/> class.
+		/// </summary>
+		/// <param name="direction">The sort direction.</param>
+		/// <param name="propertyName">Name of the property used for sorting.</param>
+		public SortDescription(ListSortDirection direction, string propertyName)
+		{
+			if ((direction != ListSortDirection.Ascending) &&
+				(direction != ListSortDirection.Descending))
+			{
+				throw new System.ComponentModel.InvalidEnumArgumentException ("direction", (int) direction, typeof (ListSortDirection));
+			}
+
+			if (propertyName == null)
+			{
+				throw new System.ArgumentNullException ("propertyName");
+			}
+			if (propertyName.Length == 0)
+			{
+				throw new System.ArgumentException ("Empty property name");
+			}
+
+			this.direction = direction;
+			this.propertyName = propertyName;
+		}
+
+		/// <summary>
+		/// Gets the name of the property used for sorting.
+		/// </summary>
+		/// <value>The name of the property.</value>
+		public string PropertyName
+		{
+			get
+			{
+				return this.propertyName;
+			}
+		}
+
+		/// <summary>
+		/// Gets the sort direction.
+		/// </summary>
+		/// <value>The sort direction.</value>
+		public ListSortDirection Direction
+		{
+			get
+			{
+				return this.direction;
+			}
+		}
+
+		#region IEquatable<SortDescription> Members
+
+		public bool Equals(SortDescription other)
+		{
+			return this == other;
+		}
+
+		#endregion
+
+		public override bool Equals(object obj)
+		{
+			if (obj is SortDescription)
+			{
+				return this == (SortDescription) obj;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public override int GetHashCode()
+		{
+			return this.propertyName.GetHashCode () ^ this.direction.GetHashCode ();
+		}
+
+		public static bool operator==(SortDescription a, SortDescription b)
+		{
+			return (a.direction == b.direction)
+				&& (a.propertyName == b.propertyName);
+		}
+
+		public static bool operator!=(SortDescription a, SortDescription b)
+		{
+			return (a.direction != b.direction)
+				|| (a.propertyName != b.propertyName);
+		}
+
+		#region SerializationConverter Class
+
+		public class SerializationConverter : ISerializationConverter
+		{
+			#region ISerializationConverter Members
+
+			public string ConvertToString(object value, IContextResolver context)
+			{
+				SortDescription sort = (SortDescription) value;
+
+				switch (sort.direction)
+				{
+					case ListSortDirection.Ascending:
+						return string.Concat ("A;", sort.propertyName);
+					case ListSortDirection.Descending:
+						return string.Concat ("D;", sort.propertyName);
+				}
+
+				throw new System.InvalidOperationException (string.Format ("ListSortDirection.{0} not supported", sort.direction));
+			}
+
+			public object ConvertFromString(string value, IContextResolver context)
+			{
+				System.Diagnostics.Debug.Assert (value[1] == ';');
+
+				switch (value[0])
+				{
+					case 'A':
+						return new SortDescription (ListSortDirection.Ascending, value.Substring (2));
+					case 'D':
+						return new SortDescription (ListSortDirection.Descending, value.Substring (2));
+				}
+
+				throw new System.FormatException ("Unknown format or ListSortDirection");
+			}
+
+			#endregion
+		}
+
+		#endregion
+
+		private string propertyName;
+		private ListSortDirection direction;
+	}
+}
