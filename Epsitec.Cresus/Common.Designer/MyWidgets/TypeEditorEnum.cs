c6df095de71832c94e04ef54add3283fee6f5348
Daniel Roux <roux@epsitec.ch>
2006-10-05 08:46:22 +0000

    - Les boutons de la toolbar sont définis par des ressources Captions.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@7149 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/MyWidgets/TypeEditorEnum.cs
+++ Epsitec.Cresus/Common.Designer/MyWidgets/TypeEditorEnum.cs
@@ -16,26 +16,26 @@ namespace Epsitec.Common.Designer.MyWidgets
 			this.toolbar.Dock = DockStyle.StackBegin;
 
 			this.buttonAdd = new IconButton();
-			this.buttonAdd.IconName = Misc.Icon("StringAdd");
+			this.buttonAdd.CaptionDruid = Res.Captions.Editor.Type.Add.Druid;
 			this.buttonAdd.Clicked += new MessageEventHandler(this.HandleButtonClicked);
 			this.toolbar.Items.Add(this.buttonAdd);
 
 			this.toolbar.Items.Add(new IconSeparator());
 
 			this.buttonPrev = new IconButton();
-			this.buttonPrev.IconName = Misc.Icon("StringPrev");
+			this.buttonPrev.CaptionDruid = Res.Captions.Editor.Type.Prev.Druid;
 			this.buttonPrev.Clicked += new MessageEventHandler(this.HandleButtonClicked);
 			this.toolbar.Items.Add(this.buttonPrev);
 
 			this.buttonNext = new IconButton();
-			this.buttonNext.IconName = Misc.Icon("StringNext");
+			this.buttonNext.CaptionDruid = Res.Captions.Editor.Type.Next.Druid;
 			this.buttonNext.Clicked += new MessageEventHandler(this.HandleButtonClicked);
 			this.toolbar.Items.Add(this.buttonNext);
 
 			this.toolbar.Items.Add(new IconSeparator());
 
 			this.buttonRemove = new IconButton();
-			this.buttonRemove.IconName = Misc.Icon("StringRemove");
+			this.buttonRemove.CaptionDruid = Res.Captions.Editor.Type.Remove.Druid;
 			this.buttonRemove.Clicked += new MessageEventHandler(this.HandleButtonClicked);
 			this.toolbar.Items.Add(this.buttonRemove);
 
