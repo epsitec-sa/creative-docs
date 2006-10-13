5c2962c5bdb6252d010ff1687cb51c380d885b0b
Pierre Arnaud <arnaud@opac.ch>
2006-10-13 03:52:33 +0000

    - Commenté CommandAttribute.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@7206 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Support/CommandAttribute.cs
+++ Epsitec.Cresus/Common.Support/CommandAttribute.cs
@@ -5,7 +5,7 @@ namespace Epsitec.Common.Support
 {
 	/// <summary>
 	/// The <c>CommandAttribute</c> class defines a <c>[Command]</c> attribute,
-	/// which is used by <see cref="T:CommandDispatcher"/> to locate the methods
+	/// which is used by <see cref="CommandDispatcher"/> to locate the methods
 	/// implementating specific commands.
 	/// </summary>
 	
@@ -14,21 +14,36 @@ namespace Epsitec.Common.Support
 	
 	public class CommandAttribute : System.Attribute
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CommandAttribute"/> class.
+		/// </summary>
 		public CommandAttribute()
 		{
 		}
-		
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CommandAttribute"/> class.
+		/// </summary>
+		/// <param name="commandName">Name of the command.</param>
 		public CommandAttribute(string commandName)
 		{
 			this.commandName = commandName;
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CommandAttribute"/> class.
+		/// </summary>
+		/// <param name="druid">The DRUID (encoded as a raw <c>long</c> value) of the command.</param>
 		public CommandAttribute(long druid)
 		{
 			this.druid = druid;
 		}
 
 
+		/// <summary>
+		/// Gets or sets the name of the command.
+		/// </summary>
+		/// <value>The name of the command.</value>
 		public string							CommandName
 		{
 			get
@@ -41,6 +56,10 @@ namespace Epsitec.Common.Support
 			}
 		}
 
+		/// <summary>
+		/// Gets or sets the DRUID of the command.
+		/// </summary>
+		/// <value>The DRUID of the command.</value>
 		public long								Druid
 		{
 			get
