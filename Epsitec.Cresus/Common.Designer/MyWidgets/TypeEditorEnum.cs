aa7ddaa44dd5f76e0a25ba69abe735c1862807ef
Daniel Roux <roux@epsitec.ch>
2006-10-13 07:53:34 +0000

    - La nouvelle interaction pour choisir les ressources d'une énumération fonctionne. On peut ajouter ou supprimer des valeurs à l'aide des deux boutons [+] et [-] ou en cliquant dans la première colonne. On peut également déplacer des valeurs, pour définir l'ordre.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@7208 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/MyWidgets/TypeEditorEnum.cs
+++ Epsitec.Cresus/Common.Designer/MyWidgets/TypeEditorEnum.cs
@@ -21,6 +21,11 @@ namespace Epsitec.Common.Designer.MyWidgets
 			this.buttonAdd.Clicked += new MessageEventHandler(this.HandleButtonClicked);
 			this.toolbar.Items.Add(this.buttonAdd);
 
+			this.buttonRemove = new IconButton();
+			this.buttonRemove.CaptionDruid = Res.Captions.Editor.Type.Remove.Druid;
+			this.buttonRemove.Clicked += new MessageEventHandler(this.HandleButtonClicked);
+			this.toolbar.Items.Add(this.buttonRemove);
+
 			this.toolbar.Items.Add(new IconSeparator());
 
 			this.buttonPrev = new IconButton();
@@ -33,21 +38,16 @@ namespace Epsitec.Common.Designer.MyWidgets
 			this.buttonNext.Clicked += new MessageEventHandler(this.HandleButtonClicked);
 			this.toolbar.Items.Add(this.buttonNext);
 
-			this.toolbar.Items.Add(new IconSeparator());
-
-			this.buttonRemove = new IconButton();
-			this.buttonRemove.CaptionDruid = Res.Captions.Editor.Type.Remove.Druid;
-			this.buttonRemove.Clicked += new MessageEventHandler(this.HandleButtonClicked);
-			this.toolbar.Items.Add(this.buttonRemove);
-
 			this.array = new StringArray(this);
-			this.array.Columns = 3;
-			this.array.SetColumnsRelativeWidth(0, 0.4);
-			this.array.SetColumnsRelativeWidth(1, 0.5);
-			this.array.SetColumnsRelativeWidth(2, 0.1);
-			this.array.SetColumnAlignment(0, ContentAlignment.MiddleLeft);
+			this.array.Columns = 4;
+			this.array.SetColumnsRelativeWidth(0, 0.05);
+			this.array.SetColumnsRelativeWidth(1, 0.40);
+			this.array.SetColumnsRelativeWidth(2, 0.50);
+			this.array.SetColumnsRelativeWidth(3, 0.05);
+			this.array.SetColumnAlignment(0, ContentAlignment.MiddleCenter);
 			this.array.SetColumnAlignment(1, ContentAlignment.MiddleLeft);
-			this.array.SetColumnAlignment(2, ContentAlignment.MiddleCenter);
+			this.array.SetColumnAlignment(2, ContentAlignment.MiddleLeft);
+			this.array.SetColumnAlignment(3, ContentAlignment.MiddleCenter);
 			this.array.LineHeight = 30;  // plus haut, à cause des descriptions et des icônes
 			this.array.Dock = DockStyle.StackBegin;
 			this.array.PreferredHeight = 200;
@@ -85,10 +85,13 @@ namespace Epsitec.Common.Designer.MyWidgets
 			//	Met à jour le contenu de l'éditeur.
 			Types.Collections.EnumValueCollection collection = this.Collection;
 
+			this.allDruids = new List<Druid>();
+			this.selDruids = new List<Druid>();
+			this.listDruids = new List<Druid>();
+
 			//	Cherche tous les Druids de type Values existants.
 			this.resourceAccess.BypassFilterOpenAccess(ResourceAccess.Type.Values, null);
 			int count = this.resourceAccess.BypassFilterCount;
-			this.allDruids = new List<Druid>(count);
 			for (int i=0; i<count; i++)
 			{
 				this.allDruids.Add(this.resourceAccess.BypassFilterGetDruid(i));
@@ -96,14 +99,10 @@ namespace Epsitec.Common.Designer.MyWidgets
 			this.resourceAccess.BypassFilterCloseAccess();
 
 			//	Construit le contenu de la liste.
-			this.listDruids = new List<Druid>();
-			List<int> sels = new List<int>();
-
-			int sel = 0;
 			foreach (EnumValue value in collection)
 			{
+				this.selDruids.Add(value.Caption.Druid);
 				this.listDruids.Add(value.Caption.Druid);
-				sels.Add(sel++);
 			}
 
 			foreach (Druid druid in this.allDruids)
@@ -115,8 +114,7 @@ namespace Epsitec.Common.Designer.MyWidgets
 			}
 
 			this.array.TotalRows = this.listDruids.Count;
-			this.array.AllowMultipleSelection = true;
-			this.array.SelectedRows = sels;
+			this.array.FirstVisibleRow = 0;
 
 			this.ignoreChange = true;
 			this.UpdateArray();
@@ -126,17 +124,41 @@ namespace Epsitec.Common.Designer.MyWidgets
 
 		protected void UpdateButtons()
 		{
-			Types.Collections.EnumValueCollection collection = this.Collection;
-			int sel = this.array.SelectedRow;
+			bool add    = false;
+			bool remove = false;
+			bool prev   = false;
+			bool next   = false;
 
-			this.buttonPrev.Enable = (sel != -1 && sel > 0);
-			this.buttonNext.Enable = (sel != -1 && sel < collection.Count-1);
-			this.buttonRemove.Enable = (sel != -1);
+			int sel = this.array.SelectedRow;
+			if (sel != -1)
+			{
+				Druid druid = this.listDruids[sel];  // Druid de la ligne sélectionnée
+
+				if (this.selDruids.Contains(druid))  // fait partie de l'énumération ?
+				{
+					remove = true;
+
+					int index = this.selDruids.IndexOf(druid);
+					prev = index > 0;
+					next = index < this.selDruids.Count-1;
+				}
+				else  // hors de l'énumération ?
+				{
+					add = true;
+				}
+			}
+
+			this.buttonAdd.Enable    = add;
+			this.buttonRemove.Enable = remove;
+			this.buttonPrev.Enable   = prev;
+			this.buttonNext.Enable   = next;
 		}
 
 		protected void UpdateArray()
 		{
 			//	Met à jour tout le contenu du tableau.
+			Types.Collections.EnumValueCollection collection = this.Collection;
+
 			int first = this.array.FirstVisibleRow;
 			for (int i=0; i<this.array.LineCount; i++)
 			{
@@ -145,6 +167,10 @@ namespace Epsitec.Common.Designer.MyWidgets
 					Druid druid = this.listDruids[first+i];
 					Caption caption = this.resourceAccess.DirectGetCaption(druid);
 
+					bool active = this.selDruids.Contains(druid);
+					this.array.SetLineString(0, first+i, active ? Misc.Image("Check") : "");
+					this.array.SetLineState(0, first+i, MyWidgets.StringList.CellState.Normal);
+
 					//	Ne surtout pas utiliser caption.Name ou value.Name, car cette
 					//	information n'est pas mise à jour pendant l'utilisation de
 					//	Designer, mais seulement lors de l'enregistrement
@@ -152,22 +178,22 @@ namespace Epsitec.Common.Designer.MyWidgets
 					string name = this.resourceAccess.DirectGetDisplayName(druid);
 					string text = ResourceAccess.GetCaptionNiceDescription(caption);
 
-					this.array.SetLineString(0, first+i, name);
-					this.array.SetLineState(0, first+i, MyWidgets.StringList.CellState.Normal);
-
-					this.array.SetLineString(1, first+i, text);
+					this.array.SetLineString(1, first+i, name);
 					this.array.SetLineState(1, first+i, MyWidgets.StringList.CellState.Normal);
+
+					this.array.SetLineString(2, first+i, text);
+					this.array.SetLineState(2, first+i, MyWidgets.StringList.CellState.Normal);
 
 					string icon = caption.Icon;
 					if (string.IsNullOrEmpty(icon))
 					{
-						this.array.SetLineString(2, first+i, "");
+						this.array.SetLineString(3, first+i, "");
 					}
 					else
 					{
-						this.array.SetLineString(2, first+i, Misc.ImageFull(icon));
+						this.array.SetLineString(3, first+i, Misc.ImageFull(icon));
 					}
-					this.array.SetLineState(2, first+i, MyWidgets.StringList.CellState.Normal);
+					this.array.SetLineState(3, first+i, MyWidgets.StringList.CellState.Normal);
 				}
 				else
 				{
@@ -179,6 +205,9 @@ namespace Epsitec.Common.Designer.MyWidgets
 
 					this.array.SetLineString(2, first+i, "");
 					this.array.SetLineState(2, first+i, MyWidgets.StringList.CellState.Disabled);
+
+					this.array.SetLineString(3, first+i, "");
+					this.array.SetLineState(3, first+i, MyWidgets.StringList.CellState.Disabled);
 				}
 			}
 		}
@@ -188,80 +217,23 @@ namespace Epsitec.Common.Designer.MyWidgets
 			//	Ajoute une nouvelle valeur dans l'énumération.
 			Types.Collections.EnumValueCollection collection = this.Collection;
 
-#if false
 			int sel = this.array.SelectedRow;
-			if (sel > collection.Count-1)
-			{
-				sel = collection.Count-1;
-			}
-
-			List<Druid> exclude = new List<Druid>();
-			foreach (EnumValue value in collection)
-			{
-				exclude.Add(value.Caption.Druid);
-			}
-
-			Druid druid = Druid.Empty;
-			druid = this.mainWindow.DlgResourceSelector(this.module, ResourceAccess.Type.Values, druid, exclude);
-			if (druid.IsEmpty)
+			if (sel == -1)
 			{
 				return;
 			}
 
-			Caption caption = this.module.ResourceManager.GetCaption(druid);
-			System.Diagnostics.Debug.Assert(caption != null);
+			Druid druid = this.listDruids[sel];  // Druid de la ligne sélectionnée
+			System.Diagnostics.Debug.Assert(!this.selDruids.Contains(druid));
+			this.selDruids.Add(druid);
 
-			EnumValue item = new EnumValue(0, caption);
-			collection.Insert(sel+1, item);
+			this.BuildCollection();
 			this.RenumCollection();
 
 			this.UpdateArray();
 			this.UpdateButtons();
 
-			this.array.SelectedRow = sel+1;
-			this.array.ShowSelectedRow();
-
 			this.OnContentChanged();
-#else
-			int sel = this.array.SelectedRow;
-			if (sel > collection.Count-1)
-			{
-				sel = collection.Count-1;
-			}
-
-			List<Druid> druids = new List<Druid>();
-			List<Druid> exclude = new List<Druid>();
-			foreach (EnumValue value in collection)
-			{
-				exclude.Add(value.Caption.Druid);
-			}
-
-			druids = this.mainWindow.DlgResourceSelector(this.module, ResourceAccess.Type.Values, druids, exclude);
-			if (druids == null)
-			{
-				return;
-			}
-
-			foreach (Druid druid in druids)
-			{
-				Caption caption = this.module.ResourceManager.GetCaption(druid);
-				System.Diagnostics.Debug.Assert(caption != null);
-
-				EnumValue item = new EnumValue(0, caption);
-				sel++;
-				collection.Insert(sel, item);
-			}
-
-			this.RenumCollection();
-
-			this.UpdateArray();
-			this.UpdateButtons();
-
-			this.array.SelectedRow = sel;
-			this.array.ShowSelectedRow();
-
-			this.OnContentChanged();
-#endif
 		}
 
 		protected void ArrayRemove()
@@ -275,18 +247,15 @@ namespace Epsitec.Common.Designer.MyWidgets
 				return;
 			}
 
-			collection.RemoveAt(sel);
+			Druid druid = this.listDruids[sel];  // Druid de la ligne sélectionnée
+			System.Diagnostics.Debug.Assert(this.selDruids.Contains(druid));
+			this.selDruids.Remove(druid);
+
+			this.BuildCollection();
 			this.RenumCollection();
 
 			this.UpdateArray();
 			this.UpdateButtons();
-
-			if (sel > collection.Count-1)
-			{
-				sel = collection.Count-1;
-			}
-			this.array.SelectedRow = sel;
-			this.array.ShowSelectedRow();
 
 			this.OnContentChanged();
 		}
@@ -302,18 +271,38 @@ namespace Epsitec.Common.Designer.MyWidgets
 				return;
 			}
 
-			EnumValue value = collection[sel];
-			collection.RemoveAt(sel);
-			collection.Insert(sel+direction, value);
+			Druid druid = this.listDruids[sel];  // Druid de la ligne sélectionnée
+			System.Diagnostics.Debug.Assert(this.selDruids.Contains(druid));
+
+			int index = this.selDruids.IndexOf(druid);
+			this.selDruids.RemoveAt(index);
+			this.selDruids.Insert(index+direction, druid);
+
+			this.BuildCollection();
 			this.RenumCollection();
 
-			this.UpdateArray();
-			this.UpdateButtons();
+			this.UpdateContent();
 
-			this.array.SelectedRow = sel+direction;
+			this.array.SelectedRow = this.listDruids.IndexOf(druid);
 			this.array.ShowSelectedRow();
 
 			this.OnContentChanged();
+		}
+
+		protected void BuildCollection()
+		{
+			//	Renumérote toute la collection.
+			Types.Collections.EnumValueCollection collection = this.Collection;
+			collection.Clear();
+
+			foreach (Druid druid in this.selDruids)
+			{
+				Caption caption = this.module.ResourceManager.GetCaption(druid);
+				System.Diagnostics.Debug.Assert(caption != null);
+
+				EnumValue item = new EnumValue(0, caption);
+				collection.Add(item);
+			}
 		}
 
 		protected void RenumCollection()
@@ -380,6 +369,18 @@ namespace Epsitec.Common.Designer.MyWidgets
 		{
 			//	La ligne sélectionnée a changé.
 			this.UpdateButtons();
+
+			if (this.array.SelectedColumn == 0)
+			{
+				if (this.buttonAdd.Enable)
+				{
+					this.ArrayAdd();
+				}
+				else
+				{
+					this.ArrayRemove();
+				}
+			}
 		}
 
 
@@ -390,6 +391,7 @@ namespace Epsitec.Common.Designer.MyWidgets
 		protected IconButton					buttonRemove;
 		protected MyWidgets.StringArray			array;
 		protected List<Druid>					allDruids;
+		protected List<Druid>					selDruids;
 		protected List<Druid>					listDruids;
 	}
 }
