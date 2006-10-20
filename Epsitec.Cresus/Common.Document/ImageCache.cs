d0cda9c2f42a5ac0384ad52e9a80af701e5006cc
Daniel Roux <roux@epsitec.ch>
2006-10-20 06:37:53 +0000

    - Plus d'assert si le fichier correspondant à une image n'existe pas.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@7296 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Document/ImageCache.cs
+++ Epsitec.Cresus/Common.Document/ImageCache.cs
@@ -107,7 +107,7 @@ namespace Epsitec.Common.Document
 			else
 			{
 				GlobalImageCache.Item gItem = GlobalImageCache.Add(filename, data);
-				if (gItem.Data == null)
+				if (gItem == null || gItem.Data == null)
 				{
 					return null;
 				}
