92eba9155a97ac9d4045b244f4c1ecf566c58553
Pierre Arnaud <arnaud@opac.ch>
2006-10-23 14:07:41 +0000

    - Ajouté le lien entre Panel et DataSourceMetadata. - Simplifié l'utilisation de SimpleSerialization.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@7325 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.UI.Tests/DataSourceMetadataTest.cs
+++ Epsitec.Cresus/Common.UI.Tests/DataSourceMetadataTest.cs
@@ -27,14 +27,40 @@ namespace Epsitec.Common.UI
 			metadata.Fields.Add (new StructuredTypeField ("Price", DecimalType.Default));
 
 			xml = DataSourceMetadataTest.SerializeToString (metadata);
-			
-			DataSourceMetadata copy = DataSourceMetadataTest.DeserializeFromString (xml);
+
+			DataSourceMetadata copy = DataSourceMetadataTest.DeserializeMetadataFromString (xml);
 
 			Assert.AreEqual (metadata.Fields.Count, copy.Fields.Count);
-			
+
 			Assert.AreEqual (metadata.Fields[0].Id, copy.Fields[0].Id);
 			Assert.AreEqual (metadata.Fields[1].Id, copy.Fields[1].Id);
-			
+
+			Assert.AreEqual (metadata.Fields[0].Type, copy.Fields[0].Type);
+			Assert.AreEqual (metadata.Fields[1].Type, copy.Fields[1].Type);
+		}
+		
+		[Test]
+		public void CheckSerializationWithPanel()
+		{
+			string xml;
+			Panel panel = new Panel ();
+			DataSourceMetadata metadata = panel.DataSourceMetadata;
+
+			metadata.Fields.Add (new StructuredTypeField ("Name", StringType.Default));
+			metadata.Fields.Add (new StructuredTypeField ("Price", DecimalType.Default));
+
+			xml = DataSourceMetadataTest.SerializeToString (panel);
+
+			System.Console.Out.WriteLine (xml);
+
+			Panel panelCopy = DataSourceMetadataTest.DeserializePanelFromString (xml);
+			DataSourceMetadata copy = panelCopy.DataSourceMetadata;
+
+			Assert.AreEqual (metadata.Fields.Count, copy.Fields.Count);
+
+			Assert.AreEqual (metadata.Fields[0].Id, copy.Fields[0].Id);
+			Assert.AreEqual (metadata.Fields[1].Id, copy.Fields[1].Id);
+
 			Assert.AreEqual (metadata.Fields[0].Type, copy.Fields[0].Type);
 			Assert.AreEqual (metadata.Fields[1].Type, copy.Fields[1].Type);
 		}
@@ -43,40 +69,22 @@ namespace Epsitec.Common.UI
 
 		private static string SerializeToString(DataSourceMetadata metadata)
 		{
-			System.Text.StringBuilder buffer = new System.Text.StringBuilder ();
-			System.IO.StringWriter stringWriter = new System.IO.StringWriter (buffer);
-			System.Xml.XmlTextWriter xmlWriter = new System.Xml.XmlTextWriter (stringWriter);
-
-			using (Types.Serialization.Context context = new Types.Serialization.SerializerContext (new Types.Serialization.IO.XmlWriter (xmlWriter)))
-			{
-				xmlWriter.Formatting = System.Xml.Formatting.Indented;
-				xmlWriter.WriteStartElement ("metadata");
-
-				context.ActiveWriter.WriteAttributeStrings ();
-
-				Types.Storage.Serialize (metadata, context);
-
-				xmlWriter.WriteEndElement ();
-				xmlWriter.Flush ();
-				xmlWriter.Close ();
-
-				return buffer.ToString ();
-			}
+			return Types.Serialization.SimpleSerialization.SerializeToString (metadata, "metadata", System.Xml.Formatting.Indented);
 		}
-		
-		private static DataSourceMetadata DeserializeFromString(string xml)
+
+		private static DataSourceMetadata DeserializeMetadataFromString(string xml)
 		{
-			System.IO.StringReader stringReader = new System.IO.StringReader (xml);
-			System.Xml.XmlTextReader xmlReader = new System.Xml.XmlTextReader (stringReader);
+			return Types.Serialization.SimpleSerialization.DeserializeFromString (xml, "metadata") as DataSourceMetadata;
+		}
 
-			xmlReader.Read ();
+		private static string SerializeToString(Panel panel)
+		{
+			return Types.Serialization.SimpleSerialization.SerializeToString (panel, "panel", System.Xml.Formatting.Indented);
+		}
 
-			System.Diagnostics.Debug.Assert (xmlReader.NodeType == System.Xml.XmlNodeType.Element);
-			System.Diagnostics.Debug.Assert (xmlReader.LocalName == "metadata");
-
-			Types.Serialization.Context context = new Types.Serialization.DeserializerContext (new Types.Serialization.IO.XmlReader (xmlReader));
-
-			return Types.Storage.Deserialize (context) as DataSourceMetadata;
+		private static Panel DeserializePanelFromString(string xml)
+		{
+			return Types.Serialization.SimpleSerialization.DeserializeFromString (xml, "panel") as Panel;
 		}
 
 		#endregion
