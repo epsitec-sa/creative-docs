b36ea4c811c47c6bfd552b52b487d1aa3d660b50
Daniel Roux <roux@epsitec.ch>
2006-10-26 11:32:31 +0000

    - ObjectModifier permet d'accéder au mode des boutons ButtonDisplayMode. - Le proxy Aspect permet de choisir le mode des boutons ButtonDisplayMode avec 4 icônes explicites.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005@7364 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/Proxies/Aspect.cs
+++ Epsitec.Cresus/Common.Designer/Proxies/Aspect.cs
@@ -31,6 +31,35 @@ namespace Epsitec.Common.Designer.Proxies
 			}
 		}
 
+		public override double DataColumnWidth
+		{
+			get
+			{
+				return 22*4+1;
+			}
+		}
+
+		public override double RowsSpacing
+		{
+			get
+			{
+				return 3;
+			}
+		}
+
+		public ButtonDisplayMode ButtonDisplayMode
+		{
+			get
+			{
+				return (ButtonDisplayMode) this.GetValue(Aspect.ButtonDisplayModeProperty);
+			}
+			set
+			{
+				this.SetValue(Aspect.ButtonDisplayModeProperty, value);
+			}
+		}
+
+
 		protected override void InitializePropertyValues()
 		{
 			//	Cette méthode est appelée par Proxies.Abstract quand on connecte
@@ -38,13 +67,50 @@ namespace Epsitec.Common.Designer.Proxies
 			
 			//	Recopie localement les diverses propriétés du widget sélectionné
 			//	pour pouvoir ensuite travailler dessus :
+			if (this.ObjectModifier.HasButtonDisplayMode(this.DefaultWidget))
+			{
+				ButtonDisplayMode bdm = this.ObjectModifier.GetButtonDisplayMode(this.DefaultWidget);
+
+				this.ButtonDisplayMode = bdm;
+			}
 		}
 
 
+		private static void NotifyButtonDisplayModeChanged(DependencyObject o, object oldValue, object newValue)
+		{
+			//	Cette méthode est appelée à la suite de la modification d'une de
+			//	nos propriétés de définition pour permettre de mettre à jour les
+			//	widgets connectés :
+			Aspect that = (Aspect) o;
+			ButtonDisplayMode bdm = (ButtonDisplayMode) newValue;
+
+			if (that.IsNotSuspended)
+			{
+				that.SuspendChanges();
+
+				try
+				{
+					foreach (Widget obj in that.Widgets)
+					{
+						that.ObjectModifier.SetButtonDisplayMode(obj, bdm);
+					}
+				}
+				finally
+				{
+					that.ResumeChanges();
+				}
+			}
+		}
+
+		
 		static Aspect()
 		{
+			EnumType buttonDisplayModeEnumType = Res.Types.Widgets.ButtonDisplayMode;
+			Aspect.ButtonDisplayModeProperty.DefaultMetadata.DefineNamedType(buttonDisplayModeEnumType);
+			Aspect.ButtonDisplayModeProperty.DefaultMetadata.DefineCaptionId(Res.Captions.Aspect.ButtonDisplayMode.Druid);
 		}
 
 
+		public static readonly DependencyProperty ButtonDisplayModeProperty = DependencyProperty.Register("ButtonDisplayMode", typeof(ButtonDisplayMode), typeof(Aspect), new DependencyPropertyMetadata(ButtonDisplayMode.Automatic, Aspect.NotifyButtonDisplayModeChanged));
 	}
 }
