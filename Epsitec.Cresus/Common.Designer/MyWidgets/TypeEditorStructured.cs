412d4d8f1d34995b73153657e32a919c302bc00f
Daniel Roux <roux@epsitec.ch>
2006-11-01 17:06:39 +0000

    - Petites adaptations géométriques.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@7463 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/MyWidgets/TypeEditorStructured.cs
+++ Epsitec.Cresus/Common.Designer/MyWidgets/TypeEditorStructured.cs
@@ -91,7 +91,7 @@ namespace Epsitec.Common.Designer.MyWidgets
 			this.array.SetColumnBreakMode(3, TextBreakMode.Ellipsis | TextBreakMode.Split);
 			this.array.LineHeight = TypeEditorStructured.arrayLineHeight;
 			this.array.Dock = DockStyle.StackBegin;
-			this.array.PreferredHeight = 200;
+			this.array.PreferredHeight = 300;
 			this.array.ColumnsWidthChanged += new EventHandler(this.HandleArrayColumnsWidthChanged);
 			this.array.CellCountChanged += new EventHandler(this.HandleArrayCellCountChanged);
 			this.array.SelectedRowChanged += new EventHandler(this.HandleArraySelectedRowChanged);
@@ -651,7 +651,7 @@ namespace Epsitec.Common.Designer.MyWidgets
 		}
 
 
-		protected static double					arrayLineHeight = 30;
+		protected static double					arrayLineHeight = 20;
 
 		protected HToolBar						toolbar;
 		protected IconButton					buttonAdd;
