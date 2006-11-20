dfd9d2253106d3a4ead53ad37df7d48cf184722a
Pierre Arnaud <arnaud@opac.ch>
2006-11-20 17:14:28 +0000

    - Préparé le terrain pour l'adaptateur entre Cresus.Database et Common.UI.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@7700 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.DataLayer/Adapter.cs
@@ -0,0 +1,16 @@
+//	Copyright © 2006, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
+using Epsitec.Common.UI;
+
+using Epsitec.Cresus.Database;
+
+using System.Collections.Generic;
+
+namespace Epsitec.Cresus.DataLayer
+{
+	class Adapter
+	{
+	}
+}
