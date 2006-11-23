95c525b5a1705678db2be4dbecfae63356fa2f7e
Pierre Arnaud <arnaud@opac.ch>
2006-11-23 04:59:51 +0000

    - Complété les tests de ItemPanel pour vérifier le bon fonctionnement des mécanismes de sélection.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@7752 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.UI.Tests/ItemPanelTest.cs
+++ Epsitec.Cresus/Common.UI.Tests/ItemPanelTest.cs
@@ -22,6 +22,77 @@ namespace Epsitec.Common.UI
 		}
 
 		[Test]
+		public void CheckSelection()
+		{
+			ItemPanel panel = new ItemPanel ();
+
+			panel.Items = ItemPanelTest.GetStringItems ();
+			panel.Layout = ItemViewLayout.VerticalList;
+
+			panel.SelectionMode = ItemViewSelectionMode.None;
+
+			panel.SelectItemView (panel.GetItemView (0));
+			Assert.AreEqual (0, panel.GetSelectedItemViews ().Count);
+			Assert.IsFalse (panel.GetItemView (0).IsSelected);
+
+			panel.SelectionMode = ItemViewSelectionMode.ZeroOrOne;
+			
+			panel.SelectItemView (panel.GetItemView (0));
+			Assert.AreEqual (1, panel.GetSelectedItemViews ().Count);
+			Assert.IsTrue (panel.GetItemView (0).IsSelected);
+			Assert.IsFalse (panel.GetItemView (1).IsSelected);
+
+			panel.SelectItemView (panel.GetItemView (1));
+			Assert.AreEqual (1, panel.GetSelectedItemViews ().Count);
+			Assert.IsFalse (panel.GetItemView (0).IsSelected);
+			Assert.IsTrue (panel.GetItemView (1).IsSelected);
+
+			panel.DeselectItemView (panel.GetItemView (1));
+			Assert.AreEqual (0, panel.GetSelectedItemViews ().Count);
+			Assert.IsFalse (panel.GetItemView (0).IsSelected);
+			Assert.IsFalse (panel.GetItemView (1).IsSelected);
+
+			panel.SelectionMode = ItemViewSelectionMode.ExactlyOne;
+
+			panel.SelectItemView (panel.GetItemView (0));
+			Assert.AreEqual (1, panel.GetSelectedItemViews ().Count);
+			Assert.IsTrue (panel.GetItemView (0).IsSelected);
+			Assert.IsFalse (panel.GetItemView (1).IsSelected);
+
+			panel.SelectItemView (panel.GetItemView (1));
+			Assert.AreEqual (1, panel.GetSelectedItemViews ().Count);
+			Assert.IsFalse (panel.GetItemView (0).IsSelected);
+			Assert.IsTrue (panel.GetItemView (1).IsSelected);
+
+			panel.DeselectItemView (panel.GetItemView (1));
+			Assert.AreEqual (1, panel.GetSelectedItemViews ().Count);
+			Assert.IsFalse (panel.GetItemView (0).IsSelected);
+			Assert.IsTrue (panel.GetItemView (1).IsSelected);
+
+			panel.SelectionMode = ItemViewSelectionMode.Multiple;
+
+			panel.SelectItemView (panel.GetItemView (0));
+			Assert.AreEqual (2, panel.GetSelectedItemViews ().Count);
+			Assert.IsTrue (panel.GetItemView (0).IsSelected);
+			Assert.IsTrue (panel.GetItemView (1).IsSelected);
+
+			panel.SelectItemView (panel.GetItemView (1));
+			Assert.AreEqual (2, panel.GetSelectedItemViews ().Count);
+			Assert.IsTrue (panel.GetItemView (0).IsSelected);
+			Assert.IsTrue (panel.GetItemView (1).IsSelected);
+
+			panel.DeselectItemView (panel.GetItemView (0));
+			Assert.AreEqual (1, panel.GetSelectedItemViews ().Count);
+			Assert.IsFalse (panel.GetItemView (0).IsSelected);
+			Assert.IsTrue (panel.GetItemView (1).IsSelected);
+
+			panel.DeselectItemView (panel.GetItemView (1));
+			Assert.AreEqual (0, panel.GetSelectedItemViews ().Count);
+			Assert.IsFalse (panel.GetItemView (0).IsSelected);
+			Assert.IsFalse (panel.GetItemView (1).IsSelected);
+		}
+
+		[Test]
 		public void CheckVerticalLayout()
 		{
 			ItemPanel panel = new ItemPanel ();
@@ -67,7 +138,7 @@ namespace Epsitec.Common.UI
 			Assert.AreEqual (panel, panel.GetItemView (5).Widget.Parent);
 			Assert.AreEqual (panel, panel.GetItemView (6).Widget.Parent);
 			Assert.AreEqual (new Drawing.Rectangle (0, 20, 80, 20), panel.GetItemView (5).Widget.ActualBounds);
-			Assert.AreEqual (new Drawing.Rectangle (0,  0, 80, 20), panel.GetItemView (6).Widget.ActualBounds);
+			Assert.AreEqual (new Drawing.Rectangle (0, 0, 80, 20), panel.GetItemView (6).Widget.ActualBounds);
 		}
 
 		private static CollectionView GetStringItems()
