5d6c9b199e9e0462fd455f29dff37269453ad699
Pierre Arnaud <arnaud@opac.ch>
2006-11-29 07:17:30 +0000

    - Ajouté la structure DataBrokerItem qui permettra de faire le lien entre une ligne d'une table et un StructuredData.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@7833 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.DataLayer/DataBrokerItem.cs
@@ -0,0 +1,109 @@
+//	Copyright © 2006, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Database;
+
+using System.Collections.Generic;
+
+namespace Epsitec.Cresus.DataLayer
+{
+	/// <summary>
+	/// The <c>DataBrokerItem</c> structure stores a reference to a piece of
+	/// structured data, which can either be read only (in which case it is
+	/// volatile and subject to garbage collection) or writable (in which case
+	/// it is persistent).
+	/// </summary>
+	public struct DataBrokerItem
+	{
+		public DbId								KeyId
+		{
+			get
+			{
+				return this.keyId;
+			}
+		}
+
+		public StructuredData					Data
+		{
+			get
+			{
+				if (this.writableData != null)
+				{
+					return this.writableData;
+				}
+				else if (this.data != null)
+				{
+					return this.data.Target;
+				}
+				else
+				{
+					return null;
+				}
+			}
+		}
+
+		public bool								IsReadOnly
+		{
+			get
+			{
+				return this.writableData == null;
+			}
+		}
+
+		public bool								IsWritable
+		{
+			get
+			{
+				return this.writableData != null;
+			}
+		}
+
+		public bool								IsAlive
+		{
+			get
+			{
+				if (this.writableData != null)
+				{
+					return true;
+				}
+
+				if ((this.data != null) &&
+					(this.data.IsAlive))
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+		
+		public static DataBrokerItem CreateReadOnlyItem(DbId keyId, StructuredData data)
+		{
+			DataBrokerItem item = new DataBrokerItem ();
+
+			item.keyId = keyId;
+			item.data = new Weak<StructuredData> (data);
+
+			return item;
+		}
+
+		public static DataBrokerItem CreateWritableItem(DbId keyId, StructuredData data)
+		{
+			DataBrokerItem item = new DataBrokerItem ();
+
+			item.keyId = keyId;
+			item.data = null;
+			item.writableData = data;
+
+			return item;
+		}
+
+		private DbId keyId;
+		private Weak<StructuredData> data;
+		private StructuredData writableData;
+	}
+}
