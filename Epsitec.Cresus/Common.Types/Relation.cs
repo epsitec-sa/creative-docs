d6671f55c121639f9d8e7f1367476bfe094f34a8
Pierre Arnaud <arnaud@opac.ch>
2006-11-29 13:57:07 +0000

    - Ajouté des commentaires à Relation.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@7846 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Types/Relation.cs
+++ Epsitec.Cresus/Common.Types/Relation.cs
@@ -3,17 +3,58 @@
 
 namespace Epsitec.Common.Types
 {
+	/// <summary>
+	/// The <c>Relation</c> enumeration defines what relation binds two
+	/// structures (or tables, when speaking the database lingua). See
+	/// <see cref="StructuredTypeField"/>.
+	/// </summary>
 	public enum Relation : byte
 	{
-		None = 0,
+		/// <summary>
+		/// There is no relation defined for this field.
+		/// </summary>
+		None=0,
 
+		/// <summary>
+		/// The field defines a reference (pointer) to another structure.
+		/// </summary>
 		Reference=1,
+		
+		/// <summary>
+		/// The field defines a bijective reference (bidirectional pointer)
+		/// with another structure. This a synonym for <c>OneToOne</c>.
+		/// </summary>
 		Bijective=2,
+		
+		/// <summary>
+		/// The field defines a collection of structures. This is a synonym
+		/// for <c>OneToMany</c>.
+		/// </summary>
 		Collection=3,
+		
+		/// <summary>
+		/// The field defines a bijective cluster reference; this means that
+		/// several source structures reference several target structures,
+		/// and the other way round too. This is a synonym for <c>ManyToMany</c>.
+		/// </summary>
 		Cluster=4,
 
+		/// <summary>
+		/// The field defines a one-to-one relation. This is a synonym for
+		/// <c>Bijective</c>.
+		/// </summary>
 		OneToOne=Bijective,
+
+		/// <summary>
+		/// The field defines a one-to-many relation. This is a synonym for
+		/// <c>Collection</c>.
+		/// </summary>
 		OneToMany=Collection,
+		
+		/// <summary>
+		/// The field defines a many-to-many relation. This is a synonym for
+		/// <c>Cluster</c>.
+		/// </summary>
 		ManyToMany=Cluster,
 	}
 }
