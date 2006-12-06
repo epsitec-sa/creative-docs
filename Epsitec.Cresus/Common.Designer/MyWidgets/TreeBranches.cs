1c50af5bb5c9fbf64e6b9caaf5adf005b17151e3
Daniel Roux <roux@epsitec.ch>
2006-12-06 15:56:44 +0000

    - Plus joli (3 styles à choix).
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@8043 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/MyWidgets/TreeBranches.cs
+++ Epsitec.Cresus/Common.Designer/MyWidgets/TreeBranches.cs
@@ -10,8 +10,17 @@ namespace Epsitec.Common.Designer.MyWidgets
 	/// </summary>
 	public class TreeBranches : Widget
 	{
+		public enum TreeBrancheStyle
+		{
+			Simple,		// droites simples
+			Rich,		// droites avec cercles aux points d'attache
+			Curve,		// courbes simples
+		}
+
+
 		public TreeBranches() : base()
 		{
+			this.style = TreeBrancheStyle.Curve;
 			this.branches = new List<TreeBranche>();
 		}
 
@@ -20,6 +29,22 @@ namespace Epsitec.Common.Designer.MyWidgets
 			this.SetEmbedder(embedder);
 		}
 
+		public TreeBrancheStyle Style
+		{
+			//	Style de représentation pour les liaisons.
+			get
+			{
+				return this.style;
+			}
+			set
+			{
+				if (this.style != value)
+				{
+					this.style = value;
+					this.Invalidate();
+				}
+			}
+		}
 
 		public void AddBranche(double p1, double p2)
 		{
@@ -36,47 +61,108 @@ namespace Epsitec.Common.Designer.MyWidgets
 			Rectangle rect = this.Client.Bounds;
 
 			//	Dessine toutes les liaisons.
-#if false
-			foreach (TreeBranche branche in this.branches)
+			if (this.style == TreeBrancheStyle.Simple)
 			{
-				Point p1 = new Point(branche.P1, rect.Top);
-				Point p2 = new Point(branche.P2, rect.Bottom);
+				this.UpdateShift();
 
-				Misc.AlignForLine(graphics, ref p1);
-				Misc.AlignForLine(graphics, ref p2);
+				foreach (TreeBranche branche in this.branches)
+				{
+					Point p1 = new Point(branche.P1+branche.Shift, rect.Top);
+					Point p2 = new Point(branche.P2, rect.Bottom);
 
-				graphics.AddLine(p1, p2);
-				graphics.RenderSolid(adorner.ColorBorder);
+					Misc.AlignForLine(graphics, ref p1);
+					Misc.AlignForLine(graphics, ref p2);
 
-				p2.Y--;
-				graphics.AddFilledCircle(p1, 3);
-				graphics.AddFilledCircle(p2, 3);
+					graphics.AddLine(p1, p2);
+				}
+
 				graphics.RenderSolid(adorner.ColorBorder);
 			}
-#else
-			double r1 = 3;
-			double r2 = 3;
 
-			foreach (TreeBranche branche in this.branches)
+			if (this.style == TreeBrancheStyle.Curve)
 			{
-				Point p1 = new Point(branche.P1, rect.Top);
-				Point p2 = new Point(branche.P2, rect.Bottom);
-				p2.Y--;
+				this.UpdateShift();
 
-				Misc.AlignForLine(graphics, ref p1);
-				Misc.AlignForLine(graphics, ref p2);
+				Path path = new Path();
+				double h = rect.Height*0.6;
 
-				Point pp1 = Point.Move(p1, p2, r1);
-				Point pp2 = Point.Move(p2, p1, r2);
+				foreach (TreeBranche branche in this.branches)
+				{
+					Point p1 = new Point(branche.P1+branche.Shift, rect.Top);
+					Point p2 = new Point(branche.P2, rect.Bottom);
 
-				graphics.AddLine(pp1, pp2);
-				graphics.RenderSolid(adorner.ColorBorder);
+					Misc.AlignForLine(graphics, ref p1);
+					Misc.AlignForLine(graphics, ref p2);
 
-				graphics.AddCircle(p1, r1);
-				graphics.AddCircle(p2, r2);
+					p1.Y += 0.5;
+					p2.Y -= 0.5;
+
+					path.MoveTo(p1);
+					path.CurveTo(new Point(p1.X, p1.Y-h), new Point(p2.X, p2.Y+h), p2);
+				}
+
+				graphics.Rasterizer.AddOutline(path);
 				graphics.RenderSolid(adorner.ColorBorder);
 			}
-#endif
+
+			if (this.style == TreeBrancheStyle.Rich)
+			{
+				double r1 = 3;
+				double r2 = 3;
+
+				foreach (TreeBranche branche in this.branches)
+				{
+					Point p1 = new Point(branche.P1, rect.Top);
+					Point p2 = new Point(branche.P2, rect.Bottom);
+					p2.Y--;
+
+					Misc.AlignForLine(graphics, ref p1);
+					Misc.AlignForLine(graphics, ref p2);
+
+					Point pp1 = Point.Move(p1, p2, r1);
+					Point pp2 = Point.Move(p2, p1, r2);
+
+					graphics.AddLine(pp1, pp2);
+					graphics.RenderSolid(adorner.ColorBorder);
+
+					graphics.AddCircle(p1, r1);
+					graphics.AddCircle(p2, r2);
+					graphics.RenderSolid(adorner.ColorBorder);
+				}
+			}
+		}
+
+
+		protected void UpdateShift()
+		{
+			//	Met à jour le décalage, pour que les points de départ d'un même
+			//	parent soient répartis.
+			double last = -999;
+			int first = -1;
+
+			for (int i=0; i<=this.branches.Count; i++)
+			{
+				if (i == this.branches.Count || last != this.branches[i].P1)
+				{
+					if (first != -1 && i-first > 1)
+					{
+						double width = 10;
+						double delta = width/(i-first-1);
+						delta = System.Math.Min(3, delta);
+
+						for (int j=first; j<i; j++)
+						{
+							this.branches[j].Shift = delta*((j-first)-(i-first-1)/2.0);
+						}
+					}
+
+					if (i < this.branches.Count)
+					{
+						last = this.branches[i].P1;
+						first = i;
+					}
+				}
+			}
 		}
 
 
@@ -87,6 +173,7 @@ namespace Epsitec.Common.Designer.MyWidgets
 			{
 				this.p1 = p1;
 				this.p2 = p2;
+				this.shift = 0;
 			}
 
 			public double P1
@@ -105,11 +192,25 @@ namespace Epsitec.Common.Designer.MyWidgets
 				}
 			}
 
+			public double Shift
+			{
+				get
+				{
+					return this.shift;
+				}
+				set
+				{
+					this.shift = value;
+				}
+			}
+
 			protected double					p1;
 			protected double					p2;
+			protected double					shift;
 		}
 
 
+		protected TreeBrancheStyle				style;
 		protected List<TreeBranche>				branches;
 	}
 }
