5dfbd0a4b2a588b3ff1588a38bed29bdb004c525
Pierre Arnaud <arnaud@opac.ch>
2007-03-15 09:47:24 +0000

    - Adapté le système de signature numérique du projet Creative Docs .NET pour pouvoir utiliser SignTool plutôt que SignCode.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@8556 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/External/CodeSigning/sign-opac.bat
+++ Epsitec.Cresus/External/CodeSigning/sign-opac.bat
@@ -5,4 +5,4 @@ cd %~dp0
 
 echo Signing %1
 
-for %%f in (%1) do S:\Epsitec.Cresus\External\CodeSigning\signcode -a sha1 -ky signature -spc S:\Epsitec.Cresus\External\CodeSigning\opac.spc -t http://timestamp.verisign.com/scripts/timstamp.dll -v S:\Epsitec.Cresus\External\CodeSigning\opac.pvk "%%f"
+for %%f in (%1) do S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n OPaC /d "OPaC bright ideas" /t http://timestamp.verisign.com/scripts/timstamp.dll "%%f"
