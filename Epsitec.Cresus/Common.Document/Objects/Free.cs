82026fdb48bf5ac925a3d9173951858784c8c75a
Daniel Roux <roux@epsitec.ch>
2007-03-23 11:36:23 +0000

    - Le dessin d'un trait à main levée produit des angles aigus lorsque l'angle avec le trait précédent dépasse 90 degrés.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@8658 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Document/Objects/Free.cs
+++ Epsitec.Cresus/Common.Document/Objects/Free.cs
@@ -439,6 +439,16 @@ namespace Epsitec.Common.Document.Objects
 				}
 				else
 				{
+					int total = this.TotalHandle;
+					if (total >= 3)
+					{
+						double a1 = Point.ComputeAngleDeg(this.Handle(total-1).Position, this.Handle(total-2).Position);
+						double a2 = Point.ComputeAngleDeg(this.Handle(total-2).Position, this.Handle(total-3).Position);
+						double a = Math.ClipAngleDeg(a1-a2);
+						System.Diagnostics.Debug.WriteLine(string.Format("a1={0} a2={1} a={2}", a1, a2, a));
+						this.Handle(total-2).ConstrainType = (a < 90 || a > 270) ? HandleConstrainType.Symmetric : HandleConstrainType.SharpSharp;
+					}
+
 					this.HandleAdd(pos, HandleType.Primary);
 				}
 			}
