faf219c3847c5eef4d56bfbe3c50a59330b2081f
Pierre Arnaud <arnaud@opac.ch>
2007-05-12 08:32:16 +0000

    - Supprimé un message de debug dans ItemView. - Ajouté le code nécessaire à Daniel pour continuer sur l'implémentation du nouveau panneau "Strings2".
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@8945 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/Viewers/Strings2.cs
+++ Epsitec.Cresus/Common.Designer/Viewers/Strings2.cs
@@ -15,13 +15,15 @@ namespace Epsitec.Common.Designer.Viewers
 		public Strings2(Module module, PanelsContext context, ResourceAccess access, MainWindow mainWindow) : base(module, context, access, mainWindow)
 		{
 			this.accessor = new Support.ResourceAccessors.StringResourceAccessor();
-			accessor.Load(access.ResourceManager);
+			this.accessor.Load(access.ResourceManager);
 
 			StructuredType cultureMapType = new StructuredType();
-			cultureMapType.Fields.Add("Name", StringType.Default);
+			cultureMapType.Fields.Add ("Name", StringType.Default);
+			cultureMapType.Fields.Add ("Primary", StringType.Default);
+			cultureMapType.Fields.Add ("Secondary", StringType.Default);
 
-			this.collectionView = new CollectionView(accessor.Collection);
-
+			this.collectionView = new CollectionView(this.accessor.Collection);
+			this.itemViewFactory = new ItemViewFactory(this);
 			
 			this.primaryCulture = new IconButtonMark(this);
 			this.primaryCulture.Text = "?";
@@ -34,11 +36,34 @@ namespace Epsitec.Common.Designer.Viewers
 			this.primaryCulture.Margins = new Drawing.Margins(10, 10, 10, 0);
 
 			this.table = new UI.ItemTable(this);
+			
+			//	Spécifie quel "factory" utiliser pour les éléments représentés dans cet
+			//	ItemTable/ItemPanel :
+			
+			this.table.ItemPanel.CustomItemViewFactoryGetter =
+				delegate (UI.ItemView itemView)
+				{
+					if ((itemView.Item == null) ||
+						(itemView.Item.GetType () != typeof (CultureMap)))
+					{
+						return null;
+					}
+					else
+					{
+						return this.itemViewFactory;
+					}
+				};
+			
 			this.table.SourceType = cultureMapType;
 			this.table.Items = this.collectionView;
-			this.table.Columns.Add(new UI.ItemTableColumn("Name", 200));
-			this.table.Columns.Add(new UI.ItemTableColumn("Primary", 200));
-			this.table.Columns.Add(new UI.ItemTableColumn("Secondary", 200));
+
+			//	PA: On dirait que le mode "largeur proportionnelle" n'est pas implémenté dans l'en-tête
+			//	des tables... tant pis pour le moment ?
+
+			this.table.Columns.Add(new UI.ItemTableColumn("Name", new Epsitec.Common.Widgets.Layouts.GridLength (200, Epsitec.Common.Widgets.Layouts.GridUnitType.Proportional)));
+			this.table.Columns.Add (new UI.ItemTableColumn ("Primary", new Epsitec.Common.Widgets.Layouts.GridLength (200, Epsitec.Common.Widgets.Layouts.GridUnitType.Proportional)));
+			this.table.Columns.Add (new UI.ItemTableColumn ("Secondary", new Epsitec.Common.Widgets.Layouts.GridLength (200, Epsitec.Common.Widgets.Layouts.GridUnitType.Proportional)));
+			
 			this.table.HorizontalScrollMode = UI.ItemTableScrollMode.Linear;
 			//?this.table.HorizontalScrollMode = UI.ItemTableScrollMode.None;
 			this.table.VerticalScrollMode = UI.ItemTableScrollMode.ItemBased;
@@ -50,6 +75,12 @@ namespace Epsitec.Common.Designer.Viewers
 			this.table.Dock = Widgets.DockStyle.Fill;
 			this.table.Margins = new Drawing.Margins(10, 10, 0, 10);
 			this.table.SizeChanged += this.HandleTableSizeChanged;
+			this.table.ColumnHeader.ColumnWidthChanged += this.HandleColumnHeaderColumnWidthChanged;
+		}
+
+		void HandleColumnHeaderColumnWidthChanged(object sender, Epsitec.Common.UI.ColumnWidthChangeEventArgs e)
+		{
+			System.Diagnostics.Debug.WriteLine (string.Format ("{0} : {1} --> {2}", e.Column, e.OldWidth, e.NewWidth));
 		}
 
 		protected override void Dispose(bool disposing)
@@ -114,16 +145,75 @@ namespace Epsitec.Common.Designer.Viewers
 			Drawing.Size size = (Drawing.Size) e.NewValue;
 
 			double width = size.Width - table.GetPanelPadding().Width;
-			table.ColumnHeader.SetColumnWidth(0, width);
+			//?table.ColumnHeader.SetColumnWidth(0, width);
 
 			table.ItemPanel.ItemViewDefaultSize = new Epsitec.Common.Drawing.Size(width, 20);
 		}
 
 
 
+		private class ItemViewFactory : Epsitec.Common.UI.AbstractItemViewFactory
+		{
+			public ItemViewFactory(Strings2 owner)
+			{
+				this.owner = owner;
+			}
+
+			protected override Widget CreateElement(string name, Epsitec.Common.UI.ItemPanel panel, Epsitec.Common.UI.ItemView view, Epsitec.Common.UI.ItemViewShape shape)
+			{
+				CultureMap item = view.Item as CultureMap;
+
+				switch (name)
+				{
+					case "Name":
+						return this.CreateName (item);
+					case "Primary":
+						return this.CreatePrimary (item);
+					case "Secondary":
+						return this.CreateSecondary (item);
+				}
+
+				return null;
+			}
+
+			private Widget CreateName(CultureMap item)
+			{
+				StaticText widget = new StaticText ();
+
+				widget.Text = TextLayout.ConvertToTaggedText (item.Name);
+
+				return widget;
+			}
+
+			private Widget CreatePrimary(CultureMap item)
+			{
+				StaticText widget = new StaticText ();
+				StructuredData data = item.GetCultureData (Resources.DefaultTwoLetterISOLanguageName);
+				string text = data.GetValue (Epsitec.Common.Support.Res.Fields.ResourceString.Text) as string;
+
+				widget.Text = TextLayout.ConvertToTaggedText (text);
+
+				return widget;
+			}
+
+			private Widget CreateSecondary(CultureMap item)
+			{
+				StaticText widget = new StaticText ();
+				StructuredData data = item.GetCultureData ("en"); // TODO: choisir ici la culture secondaire qui a été sélectionnée
+				string text = data.GetValue (Epsitec.Common.Support.Res.Fields.ResourceString.Text) as string;
+
+				widget.Text = TextLayout.ConvertToTaggedText (text);
+
+				return widget;
+			}
+			
+
+			Strings2 owner;
+		}
 
 		protected Support.ResourceAccessors.StringResourceAccessor accessor;
 		protected UI.ItemTable table;
 		protected CollectionView collectionView;
+		private ItemViewFactory itemViewFactory;
 	}
 }
