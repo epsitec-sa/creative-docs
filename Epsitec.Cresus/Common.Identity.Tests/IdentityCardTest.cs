64b5e1040cf2246f472bc823823bd47eb7f3ebcd
Pierre Arnaud <arnaud@opac.ch>
2007-06-27 12:03:45 +0000

    - Ajouté un projet de test pour Common.Identity.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@9553 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Identity.Tests/IdentityCardTest.cs
@@ -0,0 +1,30 @@
+//	Copyright © 2007, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Epsitec.Common.Identity
+{
+	[TestFixture]
+	public class IdentityCardTest
+	{
+		[Test]
+		public void CheckSerialization()
+		{
+			IdentityCard card = new IdentityCard ();
+
+			card.DeveloperId = 123;
+			card.RawImage = new byte[] { 1, 2, 3 };
+
+			string xml = Epsitec.Common.Types.Serialization.SimpleSerialization.SerializeToString (card);
+
+			System.Console.Out.WriteLine (xml);
+
+			IdentityCard restoredCard = Epsitec.Common.Types.Serialization.SimpleSerialization.DeserializeFromString (xml) as IdentityCard;
+
+			Assert.AreEqual (card.DeveloperId, restoredCard.DeveloperId);
+			Assert.AreEqual (card.RawImage, restoredCard.RawImage);
+		}
+	}
+}
--- Epsitec.Cresus/Common.Identity/IdentityCard.cs
+++ Epsitec.Cresus/Common.Identity/IdentityCard.cs
@@ -1,11 +1,56 @@
 //	Copyright © 2007, EPSITEC SA, CH-1092 BELMONT, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
+using Epsitec.Common.Identity;
+using Epsitec.Common.Types;
+
 using System.Collections.Generic;
 
-namespace Common.Identity
+[assembly: DependencyClass (typeof (IdentityCard))]
+
+namespace Epsitec.Common.Identity
 {
-	public class IdentityCard
+	public class IdentityCard : DependencyObject
 	{
+		public IdentityCard()
+		{
+		}
+
+		public int DeveloperId
+		{
+			get
+			{
+				return (int) this.GetValue (IdentityCard.DeveloperIdProperty);
+			}
+			set
+			{
+				this.SetValue (IdentityCard.DeveloperIdProperty, value);
+			}
+		}
+
+		public byte[] RawImage
+		{
+			get
+			{
+				object value = this.GetValue (IdentityCard.RawImageProperty);
+
+				if (UndefinedValue.IsUndefinedValue (value))
+				{
+					return null;
+				}
+				else
+				{
+					return (byte[]) value;
+				}
+			}
+			set
+			{
+				this.SetValue (IdentityCard.RawImageProperty, value);
+			}
+		}
+
+
+		public static readonly DependencyProperty DeveloperIdProperty = DependencyProperty.Register ("DeveloperId", typeof (int), typeof (IdentityCard), new DependencyPropertyMetadata (-1));
+		public static readonly DependencyProperty RawImageProperty = DependencyProperty.Register ("RawImage", typeof (byte[]), typeof (IdentityCard), new DependencyPropertyMetadata ());
 	}
 }
