2fefd628553338283f1a883f02d407b81b43a4b2
Daniel Roux <roux@epsitec.ch>
2007-08-09 08:34:25 +0000

    - Le bouton [VIDEO] réduit fortement la taille des 4 afficheurs à 7 segments. - Noms des bus en français.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@9846 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/App.Dolphin/Geometry.cs
+++ Epsitec.Cresus/App.Dolphin/Geometry.cs
@@ -46,6 +46,68 @@ namespace Epsitec.App.Dolphin
 			graphics.GradientRenderer.Transform = ot;
 		}
 
+		public static void RenderVerticalGradient(Graphics graphics, Rectangle rect, Color bottomColor, Color topColor, int repeat, double middle)
+		{
+			//	Peint la surface avec un dégradé vertical.
+			Color c1 = topColor;
+			Color c2 = bottomColor;
+
+			double[] r = new double[256];
+			double[] g = new double[256];
+			double[] b = new double[256];
+			double[] a = new double[256];
+
+			for ( int i=0 ; i<256 ; i++ )
+			{
+				double factor = Geometry.GradientRepeatFactor(i/255.0, repeat, middle);
+				r[i] = c1.R + (c2.R-c1.R)*factor;
+				g[i] = c1.G + (c2.G-c1.G)*factor;
+				b[i] = c1.B + (c2.B-c1.B)*factor;
+				a[i] = c1.A + (c2.A-c1.A)*factor;
+			}
+
+			graphics.FillMode = FillMode.NonZero;
+			graphics.GradientRenderer.Fill = GradientFill.Y;
+			graphics.GradientRenderer.SetColors(r, g, b, a);
+			graphics.GradientRenderer.SetParameters(-100, 100);
+			
+			Transform ot = graphics.GradientRenderer.Transform;
+			Transform t = new Transform();
+			Point center = rect.Center;
+			t.Scale(rect.Width/100/2, rect.Height/100/2);
+			t.Translate(center);
+			graphics.GradientRenderer.Transform = t;
+			graphics.RenderGradient();
+			graphics.GradientRenderer.Transform = ot;
+		}
+
+		protected static double GradientRepeatFactor(double progress, int repeat, double middle)
+		{
+			//	Calcule le facteur de progression dans la couleur [0..1].
+			//	Si M>0:  P=1-(1-P)^(1+M)
+			//	Si M<0:  P=P^(1-M)
+			if (repeat > 1)
+			{
+				int i = (int) (progress*repeat);
+				progress = (progress*repeat)%1.0;
+				if (i%2 != 0)  progress = 1.0-progress;
+			}
+
+			if (middle != 0)
+			{
+				if (middle > 0.0)
+				{
+					progress = 1.0-System.Math.Pow(1.0-progress, 1.0+middle);
+				}
+				else
+				{
+					progress = System.Math.Pow(progress, 1.0-middle);
+				}
+			}
+
+			return progress;
+		}
+
 		public static void RenderCircularGradient(Graphics graphics, Point center, double radius, Color extColor, Color intColor)
 		{
 			//	Peint la surface avec un dégradé circulaire.
