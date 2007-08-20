7b2f9eee2d2077477cc7f9ab5444191b41d28886
Pierre Arnaud <arnaud@opac.ch>
2007-08-20 09:07:48 +0000

    - Ajouté ResourceModuleVersion qui permettra d'associer des numéros de versions aux modules.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@9975 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/Support/ResourceModuleVersion.cs
@@ -0,0 +1,145 @@
+//	Copyright © 2007, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Collections.Generic;
+
+namespace Epsitec.Common.Support
+{
+	/// <summary>
+	/// The <c>ResourceModuleVersion</c> class stores a version information.
+	/// </summary>
+	public sealed class ResourceModuleVersion : Epsitec.Common.Types.IReadOnly
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ResourceModuleVersion"/> class.
+		/// </summary>
+		public ResourceModuleVersion()
+		{
+		}
+
+
+		/// <summary>
+		/// Gets or sets the developer id.
+		/// </summary>
+		/// <value>The developer id.</value>
+		public int DeveloperId
+		{
+			get
+			{
+				return this.developerId;
+			}
+			set
+			{
+				if (this.developerId != value)
+				{
+					this.VerifyWritable ("DeveloperId");
+					this.developerId = value;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the build number.
+		/// </summary>
+		/// <value>The build number.</value>
+		public int BuildNumber
+		{
+			get
+			{
+				return this.buildNumber;
+			}
+			set
+			{
+				if (this.buildNumber != value)
+				{
+					this.VerifyWritable ("BuildNumber");
+					this.buildNumber = value;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the build date. The build date must always be specified
+		/// using a UTC representation.
+		/// </summary>
+		/// <value>The build date.</value>
+		public System.DateTime BuildDate
+		{
+			get
+			{
+				return this.buildDate;
+			}
+			set
+			{
+				if (this.buildDate != value)
+				{
+					this.VerifyWritable ("BuildDate");
+
+					if (value.Kind != System.DateTimeKind.Utc)
+					{
+						throw new System.ArgumentException (string.Format ("{0} uses DateTimeKind.{1}; only UTC allowed", "BuildDate", value.Kind));
+					}
+
+					this.buildDate = value;
+				}
+			}
+		}
+
+
+		#region Interface IReadOnly
+
+		/// <summary>
+		/// Gets a value indicating whether this instance is read only.
+		/// </summary>
+		/// <value>
+		/// 	<c>true</c> if this instance is read only; otherwise, <c>false</c>.
+		/// </value>
+		public bool IsReadOnly
+		{
+			get
+			{
+				return this.isFrozen;
+			}
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Freezes this instance. This makes the instance read only. No further
+		/// modification will be possible. Any attempt to modify the properties
+		/// will throw a <see cref="System.InvalidOperationException"/>.
+		/// </summary>
+		public void Freeze()
+		{
+			this.isFrozen = true;
+		}
+
+		/// <summary>
+		/// Returns a copy of this instance. The copy is modifiable.
+		/// </summary>
+		/// <returns>The copy of this instance.</returns>
+		public ResourceModuleVersion Clone()
+		{
+			ResourceModuleVersion copy = new ResourceModuleVersion ();
+
+			copy.developerId = this.developerId;
+			copy.buildNumber = this.buildNumber;
+			copy.buildDate   = this.buildDate;
+			
+			return copy;
+		}
+
+		private void VerifyWritable(string property)
+		{
+			if (this.isFrozen)
+			{
+				throw new System.InvalidOperationException (string.Format ("Property {0} is not writable", property));
+			}
+		}
+
+		bool isFrozen;
+		int developerId;
+		int buildNumber;
+		System.DateTime buildDate;
+	}
+}
