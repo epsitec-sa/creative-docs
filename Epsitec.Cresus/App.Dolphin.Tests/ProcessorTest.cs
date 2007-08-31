d94756fe8861c1f610c69574a4aaadb56331271d
Daniel Roux <roux@epsitec.ch>
2007-08-31 08:35:17 +0000

    - Nouveau projet de test pour le Dauphin.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@10075 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/App.Dolphin.Tests/ProcessorTest.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Epsitec.App.Dolphin
+{
+	[TestFixture] public class ProcessorTest
+	{
+		[Test] public void CheckInstructions()
+		{
+		}
+	}
+}
