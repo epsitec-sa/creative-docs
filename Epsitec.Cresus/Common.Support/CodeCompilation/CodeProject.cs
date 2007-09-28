717fcb53dcad26c121fe83d2373cf12453ca8255
Pierre Arnaud <arnaud@opac.ch>
2007-09-28 04:43:52 +0000

    - Ajouté des commentaires.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@10307 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Support/CodeCompilation/CodeProject.cs
+++ Epsitec.Cresus/Common.Support/CodeCompilation/CodeProject.cs
@@ -9,13 +9,27 @@ using System.Collections.Generic;
 
 namespace Epsitec.Common.Support.CodeCompilation
 {
+	/// <summary>
+	/// The <c>CodeProject</c> class defines a compilation project (library file
+	/// name, source files, references, etc.).
+	/// </summary>
 	public class CodeProject
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CodeProject"/> class.
+		/// </summary>
 		public CodeProject()
 		{
 			this.templateItems = new Dictionary<TemplateItem, string> ();
 		}
 
+		/// <summary>
+		/// Adds the specified template item to the project file. Some template
+		/// items may only be added once; trying to add them more than once will
+		/// throw a <see cref="System.NotSupportedException"/> exception.
+		/// </summary>
+		/// <param name="item">The template item.</param>
+		/// <param name="value">The template value.</param>
 		public void Add(TemplateItem item, string value)
 		{
 			if (value != null)
@@ -29,7 +43,7 @@ namespace Epsitec.Common.Support.CodeCompilation
 					{
 						case TemplateItem.CompileInsertionPoint:
 						case TemplateItem.ReferenceInsertionPoint:
-							this.templateItems[item] = string.Concat (oldValue, "\r\n", newValue);
+							this.templateItems[item] = string.Concat (oldValue, CodeProject.LineSeparator, newValue);
 							break;
 
 						default:
@@ -43,6 +57,12 @@ namespace Epsitec.Common.Support.CodeCompilation
 			}
 		}
 
+		/// <summary>
+		/// Creates the project file based on an internal template file. The
+		/// template items inserted with the <see cref="Add"/> method are
+		/// replaced by their values.
+		/// </summary>
+		/// <returns>The source for the project file.</returns>
 		public string CreateProjectFile()
 		{
 			string source = CodeProject.GetProjectFileTemplate ();
@@ -65,6 +85,12 @@ namespace Epsitec.Common.Support.CodeCompilation
 			return source;
 		}
 
+
+		/// <summary>
+		/// Gets the project file template from the resources (this is really
+		/// a .NET manifest resource stream).
+		/// </summary>
+		/// <returns></returns>
 		private static string GetProjectFileTemplate()
 		{
 			System.Type                hostType = typeof (CodeProject);
@@ -79,6 +105,14 @@ namespace Epsitec.Common.Support.CodeCompilation
 			}
 		}
 
+		/// <summary>
+		/// Replaces the specified template item through its (multi-lined) value
+		/// in the template source.
+		/// </summary>
+		/// <param name="source">The template source.</param>
+		/// <param name="item">The template item.</param>
+		/// <param name="value">The value.</param>
+		/// <returns>The updated template source.</returns>
 		private static string Replace(string source, TemplateItem item, string value)
 		{
 			string search = CodeProject.GetTemplateItemString (item);
@@ -94,27 +128,38 @@ namespace Epsitec.Common.Support.CodeCompilation
 
 			if (lineStart.Trim ().Length > 0)
 			{
+				//	The template item is inserted within an existing line; just
+				//	insert the value where the placeholder was.
+
 				return string.Concat (source.Remove (pos), value, source.Substring (pos+len));
 			}
 			else if (value.Length == 0)
 			{
+				//	The template item is empty and is inserted in an empty line.
+				//	The empty line will simply be removed from the source.
+
 				string end = source.Substring (pos+len);
 
-				if (end.StartsWith ("\r\n"))
+				if (end.StartsWith (CodeProject.LineSeparator))
 				{
-					end = end.Substring (2);
+					end = end.Substring (CodeProject.LineSeparator.Length);
 				}
 
 				return string.Concat (source.Remove (lineStartPos), end);
 			}
 			else
 			{
+				//	The template item has to be inserted as a liste of properly
+				//	indented lines. This needs a little bit of reformating.
+
 				System.Text.StringBuilder buffer = new System.Text.StringBuilder ();
 
 				buffer.Append (source.Remove (pos));
-				bool first = true;
+				
+				bool   first  = true;
+				string indent = CodeProject.LineSeparator + lineStart;
 
-				foreach (string line in value.Split (new string[] { "\r\n" }, System.StringSplitOptions.RemoveEmptyEntries))
+				foreach (string line in value.Split (new string[] { CodeProject.LineSeparator }, System.StringSplitOptions.RemoveEmptyEntries))
 				{
 					if (first)
 					{
@@ -122,8 +167,7 @@ namespace Epsitec.Common.Support.CodeCompilation
 					}
 					else
 					{
-						buffer.Append ("\r\n");
-						buffer.Append (lineStart);
+						buffer.Append (indent);
 					}
 
 					buffer.Append (line);
@@ -135,6 +179,12 @@ namespace Epsitec.Common.Support.CodeCompilation
 			}
 		}
 
+		/// <summary>
+		/// Gets the string for a template item. This builds a <c>&lt;--XYZ-ABC--&gt;</c>
+		/// string for an item named <c>XyzAbc</c>.
+		/// </summary>
+		/// <param name="item">The template item.</param>
+		/// <returns>The string representation of the template item.</returns>
 		private static string GetTemplateItemString(TemplateItem item)
 		{
 			System.Text.StringBuilder buffer = new System.Text.StringBuilder();
@@ -159,6 +209,8 @@ namespace Epsitec.Common.Support.CodeCompilation
 
 			return buffer.ToString ();
 		}
+
+		private const string LineSeparator = "\r\n";
 
 		Dictionary<TemplateItem, string> templateItems;
 	}
