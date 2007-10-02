7083616d074fb3a3809fcda87864f7233dba4bb0
Pierre Arnaud <arnaud@opac.ch>
2007-10-02 09:48:58 +0000

    - Déplacé le code de Module.Merge dans la classe Designer.ModuleSupport.ModuleMerger.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@10370 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Designer/Module.cs
+++ Epsitec.Cresus/Common.Designer/Module.cs
@@ -319,78 +319,9 @@ namespace Epsitec.Common.Designer
 		}
 
 
-		public static void Merge(ResourceModuleId moduleId, string outputPath)
-		{
-			//	Fusionne les ressources du module de patch spécifié avec celles
-			//	du module de référence et enregistre le résultat dans le dossier
-			//	de sortie.
-			string prefix = "file";
-			
-			Module             patchModule = new Module (null, DesignerMode.Build, prefix, moduleId);
-			ResourceModuleInfo patchInfo   = patchModule.ModuleInfo;
-			ResourceManager    refManager  = patchModule.ResourceManager.GetManagerForReferenceModule ();
-			ResourceModuleInfo refInfo     = refManager.DefaultModuleInfo;
-
-			System.Diagnostics.Debug.Assert (refInfo.FullId.Id == patchInfo.FullId.Id);
-			
-			ResourceModuleId    mergedModule  = new ResourceModuleId (moduleId.Name, outputPath, moduleId.Id, moduleId.Layer);
-			ResourceManagerPool mergedPool    = new ResourceManagerPool ();
-			ResourceManager     mergedManager = new ResourceManager (mergedPool, mergedModule);
-			ResourceModuleInfo  mergedInfo    = new ResourceModuleInfo ();
-
-			mergedInfo.FullId          = mergedModule;
-			mergedInfo.SourceNamespace = refInfo.SourceNamespace;
-
-			//	Reprend les versions du module de référence et ajoute celles
-			//	du module de patch. Le vecteur des versions résultant inclut
-			//	toutes les versions uniques les plus récentes.
-			foreach (ResourceModuleVersion version in refInfo.Versions)
-			{
-				mergedInfo.UpdateVersion (version);
-			}
-			foreach (ResourceModuleVersion version in patchInfo.Versions)
-			{
-				mergedInfo.UpdateVersion (version);
-			}
-
-			//	Crée le fichier "module.info" pour le module à générer.
-			System.DateTime mergeTime = System.DateTime.Now.ToUniversalTime ();
-			IdentityCard authorIdentityCard = Settings.ActiveDeveloperIdentityCard;
-			string author  = authorIdentityCard == null ? "" : string.Concat (" by ", authorIdentityCard.UserName);
-			string comment = string.Concat ("Merged on ", mergeTime.ToShortDateString (), " ", mergeTime.ToShortTimeString (), " UTC", author);
-			ResourceModule.SaveManifest (mergedInfo, comment);
-
-			//	Comme on va utiliser le mécanisme d'enregistrement standard
-			//	des accesseurs, on doit "surcharger" la méthode SetBundle de
-			//	du gestionnaire de ressources standard pour utiliser le bon
-			//	gestionnaire à la place, avec des bundles adaptés...
-
-			patchModule.ResourceManager.ReplaceSetBundle
-				(
-					delegate (ResourceBundle bundle, ResourceSetMode mode)
-					{
-						if (mode == ResourceSetMode.Write)
-						{
-							//	Clone manuellement le bundle en l'ajustant pour
-							//	qu'il s'intègre dans le nouveau module :
-							ResourceBundle copy = ResourceBundle.Create (mergedManager, prefix, mergedModule, bundle.Name, bundle.ResourceLevel, bundle.Culture, 0);
-							copy.DefineCaption (bundle.Caption);
-							copy.DefineRank (bundle.Rank);
-							copy.Compile (bundle.CreateXmlAsData ());
-
-							mergedManager.SetBundle (copy, ResourceSetMode.Write);
-						}
-
-						return false;
-					}
-				);
-
-			patchModule.Regenerate ();
-			patchModule.SaveResources ();
-		}
 
 
-		private void Regenerate()
+		internal void Regenerate()
 		{
 			foreach (ResourceAccess access in this.Accesses)
 			{
@@ -406,7 +337,7 @@ namespace Epsitec.Common.Designer
 			}
 		}
 
-		private void SaveResources()
+		internal void SaveResources()
 		{
 			//	Enregistre toutes les ressources.
 			foreach (ResourceAccess access in this.Accesses)
--- Epsitec.Cresus/Common.Designer/ModuleSupport/ModuleMerger.cs
+++ Epsitec.Cresus/Common.Designer/ModuleSupport/ModuleMerger.cs
@@ -1,6 +1,7 @@
 //	Copyright © 2007, EPSITEC SA, CH-1092 BELMONT, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
+using Epsitec.Common.Identity;
 using Epsitec.Common.Support;
 using Epsitec.Common.Support.CodeGeneration;
 using Epsitec.Common.Support.EntityEngine;
@@ -31,7 +32,7 @@ namespace Epsitec.Common.Designer.ModuleSupport
 
 			try
 			{
-				Module.Merge (info.FullId, outputPath);
+				ModuleMerger.Merge (info.FullId, outputPath);
 			}
 			catch (System.Exception ex)
 			{
@@ -40,6 +41,76 @@ namespace Epsitec.Common.Designer.ModuleSupport
 			}
 
 			return true;
+		}
+		
+		public static void Merge(ResourceModuleId moduleId, string outputPath)
+		{
+			//	Fusionne les ressources du module de patch spécifié avec celles
+			//	du module de référence et enregistre le résultat dans le dossier
+			//	de sortie.
+			string prefix = "file";
+
+			Module patchModule = new Module (null, DesignerMode.Build, prefix, moduleId);
+			ResourceModuleInfo patchInfo   = patchModule.ModuleInfo;
+			ResourceManager refManager  = patchModule.ResourceManager.GetManagerForReferenceModule ();
+			ResourceModuleInfo refInfo     = refManager.DefaultModuleInfo;
+
+			System.Diagnostics.Debug.Assert (refInfo.FullId.Id == patchInfo.FullId.Id);
+
+			ResourceModuleId mergedModule  = new ResourceModuleId (moduleId.Name, outputPath, moduleId.Id, moduleId.Layer);
+			ResourceManagerPool mergedPool    = new ResourceManagerPool ();
+			ResourceManager mergedManager = new ResourceManager (mergedPool, mergedModule);
+			ResourceModuleInfo mergedInfo    = new ResourceModuleInfo ();
+
+			mergedInfo.FullId          = mergedModule;
+			mergedInfo.SourceNamespace = refInfo.SourceNamespace;
+
+			//	Reprend les versions du module de référence et ajoute celles
+			//	du module de patch. Le vecteur des versions résultant inclut
+			//	toutes les versions uniques les plus récentes.
+			foreach (ResourceModuleVersion version in refInfo.Versions)
+			{
+				mergedInfo.UpdateVersion (version);
+			}
+			foreach (ResourceModuleVersion version in patchInfo.Versions)
+			{
+				mergedInfo.UpdateVersion (version);
+			}
+
+			//	Crée le fichier "module.info" pour le module à générer.
+			System.DateTime mergeTime = System.DateTime.Now.ToUniversalTime ();
+			IdentityCard authorIdentityCard = Settings.ActiveDeveloperIdentityCard;
+			string author  = authorIdentityCard == null ? "" : string.Concat (" by ", authorIdentityCard.UserName);
+			string comment = string.Concat ("Merged on ", mergeTime.ToShortDateString (), " ", mergeTime.ToShortTimeString (), " UTC", author);
+			ResourceModule.SaveManifest (mergedInfo, comment);
+
+			//	Comme on va utiliser le mécanisme d'enregistrement standard
+			//	des accesseurs, on doit "surcharger" la méthode SetBundle de
+			//	du gestionnaire de ressources standard pour utiliser le bon
+			//	gestionnaire à la place, avec des bundles adaptés...
+
+			patchModule.ResourceManager.ReplaceSetBundle
+				(
+					delegate (ResourceBundle bundle, ResourceSetMode mode)
+					{
+						if (mode == ResourceSetMode.Write)
+						{
+							//	Clone manuellement le bundle en l'ajustant pour
+							//	qu'il s'intègre dans le nouveau module :
+							ResourceBundle copy = ResourceBundle.Create (mergedManager, prefix, mergedModule, bundle.Name, bundle.ResourceLevel, bundle.Culture, 0);
+							copy.DefineCaption (bundle.Caption);
+							copy.DefineRank (bundle.Rank);
+							copy.Compile (bundle.CreateXmlAsData ());
+
+							mergedManager.SetBundle (copy, ResourceSetMode.Write);
+						}
+
+						return false;
+					}
+				);
+
+			patchModule.Regenerate ();
+			patchModule.SaveResources ();
 		}
 	}
 }
