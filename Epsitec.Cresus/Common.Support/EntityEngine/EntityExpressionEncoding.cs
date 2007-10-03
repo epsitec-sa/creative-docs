96303d46069c198ec047e86727ca96a97daea66f
Pierre Arnaud <arnaud@opac.ch>
2007-10-03 08:46:23 +0000

    - Ajouté la classe EntityExpression qui décrit une expression pour un champ d'entité. C'est elle qui va permettre de manipuler la description des calculs.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@10378 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Support/EntityEngine/EntityExpressionEncoding.cs
@@ -0,0 +1,15 @@
+//	Copyright © 2007, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Support;
+using Epsitec.Common.Types;
+
+namespace Epsitec.Common.Support.EntityEngine
+{
+	public enum EntityExpressionEncoding
+	{
+		Invalid,
+
+		LambdaCSharpSourceCode,
+	}
+}
