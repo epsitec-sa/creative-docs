c5958476ea7c3f382d59cc9b79f4135dbbea361d
Pierre Arnaud <arnaud@opac.ch>
2007-10-03 09:08:22 +0000

    - Commentaire.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@10379 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Support/EntityEngine/EntityExpressionEncoding.cs
+++ Epsitec.Cresus/Common.Support/EntityEngine/EntityExpressionEncoding.cs
@@ -1,15 +1,22 @@
 //	Copyright © 2007, EPSITEC SA, CH-1092 BELMONT, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
-using Epsitec.Common.Support;
-using Epsitec.Common.Types;
-
 namespace Epsitec.Common.Support.EntityEngine
 {
+	/// <summary>
+	/// The <c>EntityExpressionEncoding</c> enumeration lists the encodings
+	/// which can be used to represent an expression.
+	/// </summary>
 	public enum EntityExpressionEncoding
 	{
+		/// <summary>
+		/// Invalid encoding.
+		/// </summary>
 		Invalid,
 
+		/// <summary>
+		/// C# lambda expression, in source code.
+		/// </summary>
 		LambdaCSharpSourceCode,
 	}
 }
