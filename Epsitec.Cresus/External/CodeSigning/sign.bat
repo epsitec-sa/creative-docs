c19c750fdc14ac2a5a6ad74d492ba036315ec885
Pierre Arnaud <arnaud@opac.ch>
2007-10-23 07:27:34 +0000

    - Mise à jour du système de signature numérique.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@10502 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/External/CodeSigning/sign.bat
+++ Epsitec.Cresus/External/CodeSigning/sign.bat
@@ -1,11 +1,11 @@
 @echo off
 
+%~d0
+cd %~dp0
+
 echo Signing %1
 
-S:
-cd S:\Epsitec.Cresus\External\CodeSigning
-
-signcode -a sha1 -ky signature -spc epsitec.spc -t http://timestamp.verisign.com/scripts/timstamp.dll -v epsitec.pvk %1
+for %%f in (%1) do S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n Epsitec /d "Epsitec SA" /t http://timestamp.verisign.com/scripts/timstamp.dll "%%f"
 
 if not ErrorLevel 1 goto ok
 
@@ -17,3 +17,4 @@ exit
 :ok
 
 echo OK: Authenticode Signature Applied successfully
+
