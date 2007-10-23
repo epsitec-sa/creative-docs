f572ee6008fefea4962b349c9995a2d34134aea0
Pierre Arnaud <arnaud@opac.ch>
2007-10-23 08:03:41 +0000

    - Mise à jour de sign.bat pour accepter un second argument contenant le nom du produit à signer. - Les projets d'installation pour Crésus Designer, Crésus Documents et le Dauphin ont été adaptés pour passer sur la ligne de commande le nom à attribuer au MSI signé.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@10505 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/External/CodeSigning/sign.bat
+++ Epsitec.Cresus/External/CodeSigning/sign.bat
@@ -3,9 +3,11 @@
 %~d0
 cd %~dp0
 
-echo Signing %1
+echo Signing:
+echo File path is %1
+echo File id is %2
 
-for %%f in (%1) do S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n Epsitec /d "Epsitec SA" /t http://timestamp.verisign.com/scripts/timstamp.dll "%%f"
+for %%f in (%1) do S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n Epsitec /d %2 /t http://timestamp.verisign.com/scripts/timstamp.dll "%%f"
 
 if not ErrorLevel 1 goto ok
 
