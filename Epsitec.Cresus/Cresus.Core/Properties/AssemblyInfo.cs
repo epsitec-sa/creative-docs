1fa2f3c8c9e5c35ee82f56bab382efcaf04e6496
Pierre Arnaud <arnaud@opac.ch>
2007-12-03 09:30:46 +0000

    - Ajouté un projet Cresus.Core dans la solution.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@11014 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿//	Copyright © 2007, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("Cresus.Core")]
+[assembly: AssemblyDescription ("Crésus Application Core")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("EPSITEC SA")]
+[assembly: AssemblyProduct ("Crésus")]
+[assembly: AssemblyCopyright ("Copyright © 2007, EPSITEC SA, CH-1092 BELMONT, Switzerland")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+[assembly: ComVisible (false)]
+[assembly: Guid ("c2169b69-8fd6-42f6-bf46-222afae4e21a")]
+
+[assembly: AssemblyVersion ("2.0.0.0")]
+[assembly: AssemblyFileVersion ("2.0.0.0")]
