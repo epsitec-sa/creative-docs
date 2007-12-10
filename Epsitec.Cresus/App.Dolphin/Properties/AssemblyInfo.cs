b7b457e17015edd2eb85cd6519ad27495b4f5107
Pierre Arnaud <arnaud@opac.ch>
2007-12-10 06:51:15 +0000

    - Commit expérimental pour un Dauphin bilingue, aussi au niveau de l'installateur et des exemples.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@11053 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/App.Dolphin/Properties/AssemblyInfo.cs
+++ Epsitec.Cresus/App.Dolphin/Properties/AssemblyInfo.cs
@@ -2,32 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("App.Dolphin")]
 [assembly: AssemblyDescription ("1977 Dolphin Simulator")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EPSITEC SA")]
 [assembly: AssemblyProduct("Dolphin")]
-[assembly: AssemblyCopyright("Copyright © 2007, Daniel Roux, EPSITEC SA")]
+[assembly: AssemblyCopyright("Copyright © 2007, Daniel Roux, EPSITEC SA, www.epsitec.ch")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("53921c26-fe99-4314-ba9a-0a7faeb973d5")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion ("1.0.7.0")]
-[assembly: AssemblyFileVersion ("1.0.7.0")]
+[assembly: AssemblyVersion ("1.1.0.0")]
+[assembly: AssemblyFileVersion ("1.1.0.0")]
