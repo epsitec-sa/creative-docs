608e4c93501882a9341d2edb95dd013ae26450a5
Pierre Arnaud <arnaud@opac.ch>
2008-01-08 19:32:56 +0000

    - Ajouté ExceptionHelper.PreserveStackTrace qui permet de garantir qu'un (re)throw conserve le stack trace complet de l'endroit où l'exception a été générée au départ.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@11154 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/Support/ExceptionHelper.cs
@@ -0,0 +1,29 @@
+﻿//	Copyright © 2008, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+namespace Epsitec.Common.Support
+{
+	/// <summary>
+	/// The <c>ExceptionHelper</c> class provides support for managing
+	/// exceptions.
+	/// </summary>
+	public static class ExceptionHelper
+	{
+		/// <summary>
+		/// Preserves the full stack trace for the given exception, when it is
+		/// either being rethrown using <code>throw;</code> or <code>throw ex;</code>.
+		/// </summary>
+		/// <param name="ex">The ex.</param>
+		public static void PreserveStackTrace(System.Exception ex)
+		{
+			System.Type exceptionType = typeof (System.Exception);
+			System.Reflection.BindingFlags flags = System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic;
+			System.Reflection.MethodInfo preserve = exceptionType.GetMethod ("InternalPreserveStackTrace", flags);
+
+			if (preserve != null)
+			{
+				preserve.Invoke (ex, null);
+			}
+		}
+	}
+}
