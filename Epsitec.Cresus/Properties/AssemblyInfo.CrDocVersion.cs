7c24d3d4b881d14724c2d6b23f9648b6c3791b35
Pierre Arnaud <arnaud@opac.ch>
2008-01-28 14:11:22 +0000

    - Premières modifications en vue d'un système qui produit des MSI avec updates corrects.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@11332 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Properties/AssemblyInfo.CrDocVersion.cs
@@ -0,0 +1,11 @@
+﻿//	Copyright © 2008, EPSITEC SA, CH-1092 BELMONT, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Reflection;
+
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: AssemblyConfiguration ("")]
+
+[assembly: AssemblyVersion ("2.1.8.0")]
+[assembly: AssemblyFileVersion ("2.1.8.0")]
