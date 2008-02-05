901b27e79d87cddcf4f77790ff71e8ed7a64a55c
Pierre Arnaud <arnaud@opac.ch>
2008-02-05 05:35:56 +0000

    - Mis à jour la version des fichiers de CrDoc vers 2.1.8.1 (nécessaire pour màj MSI).
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@11407 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Properties/AssemblyInfo.CrDocVer.cs
+++ Epsitec.Cresus/Properties/AssemblyInfo.CrDocVer.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture ("")]
 [assembly: AssemblyConfiguration ("")]
 
-[assembly: AssemblyVersion ("2.1.8.0")]
-[assembly: AssemblyFileVersion ("2.1.8.0")]
+[assembly: AssemblyVersion ("2.1.8.1")]
+[assembly: AssemblyFileVersion ("2.1.8.1")]
 
 [assembly: ComVisible (false)]
