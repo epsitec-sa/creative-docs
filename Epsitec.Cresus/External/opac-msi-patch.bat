752e3e9d5d091a39cae31930dc7e29b8ddf6e472
Pierre Arnaud <arnaud@opac.ch>
2008-02-20 09:16:10 +0000

    - Ajouté un moyen de signer numériquement Creative Docs .NET avec le bon nom.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@11488 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/External/opac-msi-patch.bat
+++ Epsitec.Cresus/External/opac-msi-patch.bat
@@ -7,5 +7,5 @@ cd %2
 SDK\msitran -a opac-Cleanup.mst %1
 SDK\msitran -a opac-MsiRMFilesInUseDialog.mst %1
 
-call CodeSigning\sign-opac.bat %1
+call CodeSigning\sign-opac-2.bat %1 "Creative Docs .NET Installer"
 
