b6dc429e871592fc4253b97f049fe408c88a0a47
Pierre Arnaud <arnaud@opac.ch>
2008-03-07 12:42:10 +0000

    - Ajouté une gestion des exceptions dans le projet de test de ModuleRepository.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@11563 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/ModuleRepository/ModuleRepository.TestClient/Program.cs
+++ Epsitec.Cresus/ModuleRepository/ModuleRepository.TestClient/Program.cs
@@ -10,33 +10,40 @@ namespace TestClient
 	{
 		static void Main(string[] args)
 		{
-			System.Console.WriteLine ("Press a key to connect");
-			System.Console.ReadKey ();
-
-			IModuleRepositoryService service = ModuleRepositoryClient.GetService ();
-
-			int moduleId = service.GetNewModuleId ("ModuleRepositoryTest", "Pierre Arnaud");
-			ModuleDirectory directory = service.CreateEmptyModule (moduleId, "A", "Epsitec.ModuleRepository");
-
-			System.Console.WriteLine ("ModuleDirectory.Name = {0}", directory.Name);
-			System.Console.WriteLine ("ModuleDirectory.Files.Length = {0}", directory.Files.Length);
-
-			foreach (ModuleFile file in directory.Files)
+			try
 			{
-				System.Console.WriteLine ("  ModuleFile.Path = {0}", file.Path);
+				System.Console.WriteLine ("Press a key to connect");
+				System.Console.ReadKey ();
 
-				string path = System.IO.Path.Combine (@"S:\Epsitec.Cresus\xxx", file.Path);
+				IModuleRepositoryService service = ModuleRepositoryClient.GetService ();
 
-				System.IO.Directory.CreateDirectory (System.IO.Path.GetDirectoryName (path));
-				System.IO.File.WriteAllBytes (path, file.Data);
+				int moduleId = service.GetNewModuleId ("ModuleRepositoryTest", "Pierre Arnaud");
+				ModuleDirectory directory = service.CreateEmptyModule (moduleId, "A", "Epsitec.ModuleRepository");
+
+				System.Console.WriteLine ("ModuleDirectory.Name = {0}", directory.Name);
+				System.Console.WriteLine ("ModuleDirectory.Files.Length = {0}", directory.Files.Length);
+
+				foreach (ModuleFile file in directory.Files)
+				{
+					System.Console.WriteLine ("  ModuleFile.Path = {0}", file.Path);
+
+					string path = System.IO.Path.Combine (@"S:\Epsitec.Cresus\xxx", file.Path);
+
+					System.IO.Directory.CreateDirectory (System.IO.Path.GetDirectoryName (path));
+					System.IO.File.WriteAllBytes (path, file.Data);
+				}
+
+				System.Console.ReadKey ();
+
+				System.Diagnostics.Debug.Assert (service.RecycleModuleId (moduleId, "Daniel Roux") == false);
+				System.Diagnostics.Debug.Assert (service.RecycleModuleId (moduleId, "Pierre Arnaud") == true);
+
+				ModuleRepositoryClient.CloseService (service);
 			}
-
-			System.Console.ReadKey ();
-
-			System.Diagnostics.Debug.Assert (service.RecycleModuleId (moduleId, "Daniel Roux") == false);
-			System.Diagnostics.Debug.Assert (service.RecycleModuleId (moduleId, "Pierre Arnaud") == true);
-
-			ModuleRepositoryClient.CloseService (service);
+			catch (System.Exception ex)
+			{
+				System.Console.WriteLine ("Exception: {0}\n\n{1}", ex.Message, ex.StackTrace);
+			}
 
 			System.Console.ReadKey ();
 		}
