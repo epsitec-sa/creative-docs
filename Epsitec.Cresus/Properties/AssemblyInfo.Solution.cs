71cb5aa403027afcaa7c404124054a8f798a2c2a
Pierre Arnaud <arnaud@opac.ch>
2008-09-19 14:25:15 +0000

    - Préparé version 2.4.1 de Creative Docs .NET.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@12224 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Properties/AssemblyInfo.Solution.cs
+++ Epsitec.Cresus/Properties/AssemblyInfo.Solution.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture ("")]
 [assembly: AssemblyConfiguration ("")]
 
-[assembly: AssemblyVersion ("2.4.0.0834")]
-[assembly: AssemblyFileVersion ("2.4.0.0834")]
+[assembly: AssemblyVersion ("2.4.1.0838")]
+[assembly: AssemblyFileVersion ("2.4.1.0838")]
 
 [assembly: ComVisibleAttribute (false)]
