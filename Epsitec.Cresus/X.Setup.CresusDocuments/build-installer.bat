f0daa6160da9fe39428e3aa11301c6c276349942
Daniel Roux <roux@epsitec.ch>
2009-04-22 10:24:05 +0000

    - Adapté le fichier de build au nouveau certificat QuoVadis.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@12304 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/X.Setup.CresusDocuments/build-installer.bat
+++ Epsitec.Cresus/X.Setup.CresusDocuments/build-installer.bat
@@ -3,13 +3,13 @@ del Sleep.exe 2>NUL
 copy "S:\Epsitec.Cresus\External\Sleep.exe" Sleep.exe
 
 
-S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n Epsitec /d "EPSITEC SA" /t http://timestamp.verisign.com/scripts/timstamp.dll "Sleep.exe"
+S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n Epsitec /i QV /d "Cresus Documents" /t http://timestamp.verisign.com/scripts/timstamp.dll "Sleep.exe"
 
-S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n Epsitec /d "EPSITEC SA" /t http://timestamp.verisign.com/scripts/timstamp.dll "Debug\Setup.exe"
-S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n Epsitec /d "EPSITEC SA" /t http://timestamp.verisign.com/scripts/timstamp.dll "Debug\CresusDocuments.msi"
+S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n Epsitec /i QV /d "Cresus Documents" /t http://timestamp.verisign.com/scripts/timstamp.dll "Debug\Setup.exe"
+S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n Epsitec /i QV /d "Cresus Documents" /t http://timestamp.verisign.com/scripts/timstamp.dll "Debug\CresusDocuments.msi"
 
 S:\Epsitec.Cresus\External\iexpress /N Installer.sed
 
-S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n Epsitec /d "EPSITEC SA" /t http://timestamp.verisign.com/scripts/timstamp.dll "CrDoc-Installer.exe"
+S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n Epsitec /i QV /d "Cresus Documents" /t http://timestamp.verisign.com/scripts/timstamp.dll "CrDoc-Installer.exe"
 
 del Sleep.exe 2>NUL
