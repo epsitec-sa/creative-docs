51c28cceef7252791b189d7c246675a1a6350b5b
Pierre Arnaud <arnaud@opac.ch>
2010-03-29 13:19:41 +0000

    - Travaux dans Graphe.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2005/Epsitec.Cresus@12722 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Graph/Adorners/CoordinateAxisAdorner.cs
+++ Epsitec.Cresus/Common.Graph/Adorners/CoordinateAxisAdorner.cs
@@ -7,65 +7,116 @@ using System.Collections.Generic;
 
 namespace Epsitec.Common.Graph.Adorners
 {
-	public class CoordinateAxisAdorner : AbstractAdorner
+	public class CoordinateAxisAdorner : AbstractCoordinateAxisAdorner
 	{
 		public CoordinateAxisAdorner()
 		{
-			this.GridColor = Color.FromBrightness (0);
-			this.GridLineWidth = 0.5;
-			this.VisibleGrid = true;
-			this.VisibleLabels = true;
-			this.VisibleTicks = true;
 		}
 
-		public Styles.CaptionStyle Style
+
+		public AbstractCoordinateAxisAdorner HorizontalAxisAdorner
 		{
 			get
 			{
-				return this.style ?? CoordinateAxisAdorner.defaultStyle;
+				return this.horizontalAxisAdorner;
+			}
+		}
+
+		public AbstractCoordinateAxisAdorner VerticalAxisAdorner
+		{
+			get
+			{
+				return this.verticalAxisAdorner;
+			}
+		}
+
+
+		public override Styles.CaptionStyle Style
+		{
+			get
+			{
+				return base.Style;
 			}
 			set
 			{
-				this.style = value;
+				base.Style = value;
+				this.horizontalAxisAdorner.Style = value;
+				this.verticalAxisAdorner.Style = value;
 			}
 		}
+
+		public override Color GridColor
+		{
+			get
+			{
+				return base.GridColor;
+			}
+			set
+			{
+				base.GridColor = value;
+				this.horizontalAxisAdorner.GridColor = value;
+				this.verticalAxisAdorner.GridColor = value;
+			}
+		}
+
+
+		public override double GridLineWidth
+		{
+			get
+			{
+				return base.GridLineWidth;
+			}
+			set
+			{
+				base.GridLineWidth = value;
+				this.horizontalAxisAdorner.GridLineWidth = value;
+				this.verticalAxisAdorner.GridLineWidth = value;
+			}
+		}
+
+		public override bool VisibleGrid
+		{
+			get
+			{
+				return base.VisibleGrid;
+			}
+			set
+			{
+				base.VisibleGrid = value;
+				this.horizontalAxisAdorner.VisibleGrid = value;
+				this.verticalAxisAdorner.VisibleGrid = value;
+			}
+		}
+
+		public override bool VisibleLabels
+		{
+			get
+			{
+				return base.VisibleLabels;
+			}
+			set
+			{
+				base.VisibleLabels = value;
+				this.horizontalAxisAdorner.VisibleLabels = value;
+				this.verticalAxisAdorner.VisibleLabels = value;
+			}
+		}
+
+		public override bool VisibleTicks
+		{
+			get
+			{
+				return base.VisibleTicks;
+			}
+			set
+			{
+				base.VisibleTicks = value;
+				this.horizontalAxisAdorner.VisibleTicks = value;
+				this.verticalAxisAdorner.VisibleTicks = value;
+			}
+		}
+
 		
-		public Color GridColor
-		{
-			get;
-			set;
-		}
-		
-		public double GridLineWidth
-		{
-			get;
-			set;
-		}
-
-		public bool VisibleGrid
-		{
-			get;
-			set;
-		}
-
-		public bool VisibleLabels
-		{
-			get;
-			set;
-		}
-
-		public bool VisibleTicks
-		{
-			get;
-			set;
-		}
-
-		private HorizontalAxisMode HorizontalAxisMode
-		{
-			get;
-			set;
-		}
-
 		public override void Paint(IPaintPort port, Renderers.AbstractRenderer renderer, PaintLayer layer)
 		{
 			if (layer != PaintLayer.Intermediate)
@@ -73,68 +124,28 @@ namespace Epsitec.Common.Graph.Adorners
 				return;
 			}
 
-			this.HorizontalAxisMode = renderer.HorizontalAxisMode;
-
-			Rectangle bounds = renderer.Bounds;
-			Point     origin = renderer.GetPoint (0, 0.0);
-
-			double max = renderer.MaxValue;
-			double min = renderer.MinValue;
-
-			if (this.VisibleGrid)
-			{
+			if ((this.horizontalAxisAdorner.VisibleGrid) ||
+				(this.verticalAxisAdorner.VisibleGrid))
+            {
 				this.PaintGrid (port, renderer);
+            }
+
+			if (this.horizontalAxisAdorner.VisibleLabels)
+			{
+				this.horizontalAxisAdorner.PaintLabels (port, renderer);
+			}
+			if (this.verticalAxisAdorner.VisibleLabels)
+			{
+				this.verticalAxisAdorner.PaintLabels (port, renderer);
 			}
 
-			if (this.VisibleLabels)
+			if (this.horizontalAxisAdorner.VisibleTicks)
 			{
-				this.PaintVerticalAxisLabels (port, renderer);
-				this.PaintHorizontalAxisLabels (port, renderer);
+				this.horizontalAxisAdorner.PaintTicks (port, renderer);
 			}
-
-			port.LineWidth = 1;
-			port.Color = Color.FromBrightness (0);
-
-			this.PaintVerticalAxis (port, bounds.Left, bounds.Bottom, bounds.Top + this.extraLength,
-				path =>
-				{
-					bool first = true;
-
-					foreach (Point point in CoordinateAxisAdorner.GetVerticalAxisTicks (renderer, this.GetOptimalVerticalValueCount (bounds.Height)))
-					{
-						if (first)
-						{
-							first = false;
-						}
-						else
-						{
-							path.MoveTo (point.X - this.tickLength/2, point.Y);
-							path.LineTo (point.X + this.tickLength/2, point.Y);
-						}
-					}
-				});
-
-			if ((0.0 <= max) &&
-				(min <= 0.0))
+			if (this.verticalAxisAdorner.VisibleTicks)
 			{
-				this.PaintHorizonalAxis (port, bounds.Left, origin.Y, bounds.Right + this.extraLength,
-					path =>
-					{
-						bool first = true;
-						
-						foreach (Point point in this.GetHorizontalAxisTicks (renderer))
-						{
-							if (first)
-							{
-								first = false;
-							}
-							else
-							{
-								path.MoveTo (point.X, point.Y - this.tickLength/2);
-								path.LineTo (point.X, point.Y + this.tickLength/2);
-							}
-						}
-					});
+				this.verticalAxisAdorner.PaintTicks (port, renderer);
 			}
 		}
 
@@ -142,178 +153,62 @@ namespace Epsitec.Common.Graph.Adorners
 		{
 			Rectangle bounds = renderer.Bounds;
 
-			if (this.GridLineWidth > 0)
+			if ((this.horizontalAxisAdorner.VisibleGrid) &&
+				(this.verticalAxisAdorner.VisibleGrid) &&
+				(this.GridLineWidth == this.horizontalAxisAdorner.GridLineWidth) &&
+				(this.GridLineWidth == this.verticalAxisAdorner.GridLineWidth) &&
+				(this.GridColor == this.horizontalAxisAdorner.GridColor) &&
+				(this.GridColor == this.verticalAxisAdorner.GridColor))
 			{
+				//	Both horizontal and vertical grid lines have the same color and
+				//	width : optimize and paint the outline in one pass; this avoids
+				//	intersection artifacts.
+
 				port.LineWidth = this.GridLineWidth;
 				port.Color     = this.GridColor;
 
 				using (Path path = new Path ())
 				{
-					foreach (Point point in this.GetHorizontalAxisTicks (renderer))
-					{
-						path.MoveTo (point.X, bounds.Bottom);
-						path.LineTo (point.X, bounds.Top + this.extraLength / 2);
-					}
-
-					foreach (Point point in CoordinateAxisAdorner.GetVerticalAxisTicks (renderer, this.GetOptimalVerticalValueCount (bounds.Height)))
-					{
-						path.MoveTo (bounds.Left, point.Y);
-						path.LineTo (bounds.Right + this.extraLength / 2, point.Y);
-					}
+					this.horizontalAxisAdorner.PaintGrid (path, renderer);
+					this.verticalAxisAdorner.PaintGrid (path, renderer);
 
 					port.PaintOutline (path);
 				}
 			}
-		}
-
-		private void PaintHorizontalAxisLabels(IPaintPort port, Renderers.AbstractRenderer renderer)
-		{
-			if (renderer.ValueCount > 0)
-			{
-				Rectangle bounds = renderer.Bounds;
-				
-				var  style = this.Style;
-				port.Color = style.FontColor;
-
-				double max = renderer.MaxValue;
-				double min = renderer.MinValue;
-
-				int count = this.HorizontalAxisMode == HorizontalAxisMode.Ticks ? renderer.ValueCount-1 : renderer.ValueCount;
-
-				double dx = bounds.Width / count;
-				double x  = bounds.Left;
-				double y  = (0.0 <= max) && (0.0 >= min) ? renderer.GetPoint (0, 0.0).Y : bounds.Bottom;
-				double h  = style.Font.LineHeight * style.FontSize;
-
-				if (this.HorizontalAxisMode == HorizontalAxisMode.Ranges)
-				{
-					x += dx/2;
-				}
-				
-				foreach (var text in renderer.ValueLabels)
-				{
-					double len = style.Font.GetTextAdvance (text) * style.FontSize;
-					port.PaintText (x - len/2, y - h, text, style.Font, style.FontSize);
-					x += dx;
-				}
-			}
-		}
-
-		private void PaintVerticalAxisLabels(IPaintPort port, Renderers.AbstractRenderer renderer)
-		{
-			Rectangle bounds = renderer.Bounds;
-
-			var  style = this.Style;
-			port.Color = style.FontColor;
-
-			foreach (var value in CoordinateAxisAdorner.GetLog10Values (renderer.MinValue, renderer.MaxValue, this.GetOptimalVerticalValueCount (bounds.Height)))
-			{
-				Point pos = renderer.GetPoint (0, value);
-				string text = string.Format (System.Globalization.CultureInfo.CurrentCulture, "{0}  ", value);
-				double len  = style.Font.GetTextAdvance (text) * style.FontSize;
-
-				port.PaintText (pos.X - len, pos.Y, text, style.Font, style.FontSize);
-			}
-		}
-
-		private void PaintHorizonalAxis(IPaintPort port, double x1, double y, double x2, System.Action<Path> addTicks)
-		{
-			using (Path path = new Path ())
-			{
-				path.MoveTo (x1, y);
-				path.LineTo (x2, y);
-				path.MoveTo (x2 - this.arrowLength, y - this.arrowBreadth/2);
-				path.LineTo (x2, y);
-				path.LineTo (x2 - this.arrowLength, y + this.arrowBreadth/2);
-
-				if ((this.tickLength > 0) &&
-					(this.VisibleTicks))
-				{
-					if (addTicks != null)
-					{
-						addTicks (path);
-					}
-				}
-
-				port.PaintOutline (path);
-			}
-		}
-
-		private void PaintVerticalAxis(IPaintPort port, double x, double y1, double y2, System.Action<Path> addTicks)
-		{
-			using (Path path = new Path ())
-			{
-				path.MoveTo (x, y1);
-				path.LineTo (x, y2);
-				path.MoveTo (x - this.arrowBreadth/2, y2 - this.arrowLength);
-				path.LineTo (x, y2);
-				path.LineTo (x + this.arrowBreadth/2, y2 - this.arrowLength);
-
-				if ((this.tickLength > 0) &&
-					(this.VisibleTicks))
-				{
-					if (addTicks != null)
-					{
-						addTicks (path);
-					}
-				}
-
-				port.PaintOutline (path);
-			}
-		}
-
-		private int GetOptimalVerticalValueCount(double height)
-		{
-			var style = this.Style;
-			double lineHeight = style.Font.LineHeight * style.FontSize * 1.2;
-			double ratio = height / lineHeight;
-
-			if (ratio > 25)
-			{
-				return 20;
-			}
-			else if (ratio > 10)
-			{
-				return 10;
-			}
-			else if (ratio > 5)
-			{
-				return 5;
-			}
-			else if (ratio > 2)
-			{
-				return 2;
-			}
 			else
 			{
-				return 1;
+				//	Draw horizontal lines, then vertical lines. Each with their
+				//	own setting.
+
+				if (this.horizontalAxisAdorner.VisibleGrid)
+				{
+					port.LineWidth = this.horizontalAxisAdorner.GridLineWidth;
+					port.Color     = this.horizontalAxisAdorner.GridColor;
+
+					using (Path path = new Path ())
+					{
+						this.horizontalAxisAdorner.PaintGrid (path, renderer);
+
+						port.PaintOutline (path);
+					}
+				}
+
+				if (this.verticalAxisAdorner.VisibleGrid)
+				{
+					port.LineWidth = this.verticalAxisAdorner.GridLineWidth;
+					port.Color     = this.verticalAxisAdorner.GridColor;
+
+					using (Path path = new Path ())
+					{
+						this.verticalAxisAdorner.PaintGrid (path, renderer);
+
+						port.PaintOutline (path);
+					}
+				}
 			}
 		}
 
-		private IEnumerable<Point> GetHorizontalAxisTicks(Renderers.AbstractRenderer renderer)
-		{
-			int count = renderer.ValueCount;
-
-			if (this.HorizontalAxisMode == HorizontalAxisMode.Ranges)
-			{
-				count++;
-			}
-
-			for (int i = 0; i < count; i++)
-			{
-				yield return renderer.GetPoint (i, 0.0);
-			}
-		}
-
-		private static IEnumerable<Point> GetVerticalAxisTicks(Renderers.AbstractRenderer renderer, int optimalVerticalValueCount)
-		{
-			foreach (var value in CoordinateAxisAdorner.GetLog10Values (renderer.MinValue, renderer.MaxValue, optimalVerticalValueCount))
-			{
-				yield return renderer.GetPoint (0, value);
-			}
-		}
-
-		private static IEnumerable<double> GetLog10Values(double min, double max, int optimalVerticalValueCount)
+		internal static IEnumerable<double> GetLog10Values(double min, double max, int optimalVerticalValueCount)
 		{
 			double range     = max - min;
 
@@ -356,13 +251,7 @@ namespace Epsitec.Common.Graph.Adorners
 		}
 
 
-		private static readonly Styles.CaptionStyle	defaultStyle = new Styles.CaptionStyle ();
-		
-		private readonly double extraLength = 12;
-		private readonly double arrowLength = 4;
-		private readonly double arrowBreadth = 3;
-		private readonly double tickLength = 4;
-
-		private Styles.CaptionStyle			style;
+		private readonly CoordinateAxisAdornerX		horizontalAxisAdorner = new CoordinateAxisAdornerX ();
+		private readonly CoordinateAxisAdornerY		verticalAxisAdorner   = new CoordinateAxisAdornerY ();
 	}
 }
--- Epsitec.Cresus/Common.Graph/Adorners/HorizontalAxisMode.cs
+++ Epsitec.Cresus/Common.Graph/Adorners/HorizontalAxisMode.cs
@@ -1,15 +1,29 @@
-﻿//	Copyright © 2009, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+﻿//	Copyright © 2009-2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
-
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Epsitec.Common.Graph.Adorners
 {
+	/// <summary>
+	/// The <c>HorizontalAxisMode</c> enumeration defines how the values will
+	/// be positioned on the horizontal axis.
+	/// </summary>
 	public enum HorizontalAxisMode
 	{
+		/// <summary>
+		/// The horizontal axis is meaningless.
+		/// </summary>
+		None,
+
+		/// <summary>
+		/// The values are centered on the mark. This is the default for a
+		/// line chart.
+		/// </summary>
 		Ticks,
+
+		/// <summary>
+		/// The values are represented between two consecutive marks. This
+		/// is the default for a bar chart.
+		/// </summary>
 		Ranges
 	}
 }
