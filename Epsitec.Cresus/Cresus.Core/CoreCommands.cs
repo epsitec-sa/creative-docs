11650610c120b6bde66aa6c950bec4cceb34085d
Pierre Arnaud <arnaud@opac.ch>
2010-04-22 05:30:08 +0000

    - Encore du nettoyage, avant la grande poubellisation.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@12769 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/CoreCommands.cs
+++ Epsitec.Cresus/Cresus.Core/CoreCommands.cs
@@ -1,4 +1,4 @@
-﻿//	Copyright © 2008, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+﻿//	Copyright © 2008-2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
 using Epsitec.Common.Support;
@@ -10,91 +10,19 @@ using System.Collections.Generic;
 
 namespace Epsitec.Cresus.Core
 {
-	public class CoreCommands
+	/// <summary>
+	/// The <c>CoreCommands</c> class implements the application wide commands.
+	/// </summary>
+	public sealed partial class CoreCommands
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CoreCommands"/> class.
+		/// </summary>
+		/// <param name="application">The application.</param>
 		public CoreCommands(CoreApplication application)
 		{
 			this.application = application;
 			this.application.CommandDispatcher.RegisterController (this);
-		}
-
-
-		[Command (Mai2008.Res.CommandIds.Edition.Accept)]
-		public void EditionAccept()
-		{
-			if (this.application.EndEdit (true))
-			{
-				return;
-			}
-		}
-
-		[Command (Mai2008.Res.CommandIds.Edition.Cancel)]
-		public void EditionCancel()
-		{
-			if (this.application.EndEdit (false))
-			{
-				return;
-			}
-		}
-
-		[Command (Mai2008.Res.CommandIds.Edition.New)]
-		public void EditionNew()
-		{
-			if (this.application.CreateRecord ())
-			{
-				return;
-			}
-		}
-
-		[Command (Mai2008.Res.CommandIds.Edition.Delete)]
-		public void EditionDelete()
-		{
-			//	TODO: implement delete
-		}
-
-		[Command (Mai2008.Res.CommandIds.Edition.Edit)]
-		public void EditionEdit()
-		{
-			this.application.StartEdit ();
-		}
-
-		[Command (Mai2008.Res.CommandIds.SwitchToBase.BillOut)]
-		public void SwitchToBaseBillOut()
-		{
-			this.application.StartNewSearch (Mai2008.Entities.FactureEntity.EntityStructuredTypeId, Mai2008.FormIds.Facture);
-		}
-
-		[Command (Mai2008.Res.CommandIds.SwitchToBase.Customers)]
-		public void SwitchToBaseCustomers()
-		{
-			this.application.StartNewSearch (Mai2008.Entities.ClientEntity.EntityStructuredTypeId, Mai2008.FormIds.Client);
-		}
-
-		[Command (Mai2008.Res.CommandIds.SwitchToBase.Items)]
-		public void SwitchToBaseItems()
-		{
-			this.application.StartNewSearch (Mai2008.Entities.ArticleEntity.EntityStructuredTypeId, Mai2008.FormIds.Article);
-		}
-
-
-
-		[Command (Mai2008.Res.CommandIds.Quick.CreateBillForCustomer)]
-		public void QuickCreateBillForCustomer()
-		{
-			States.FormState formState = this.application.GetCurrentFormWorkspaceState ();
-			
-			if ((formState != null) &&
-				(formState.Item != null))
-			{
-				Mai2008.Entities.ClientEntity address = formState.Item as Mai2008.Entities.ClientEntity;
-
-				if (address != null)
-				{
-					this.application.CreateRecord (Mai2008.Entities.FactureEntity.EntityStructuredTypeId,
-						null,
-						(e) => (e as Mai2008.Entities.FactureEntity).AdresseFacturation = address);
-				}
-			}
 		}
 
 
