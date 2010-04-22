6be50ea7eca9f7e2e3559ce865ea05e6bb931d8d
Daniel Roux <roux@epsitec.ch>
2010-04-22 05:56:24 +0000

    - Cresus.Core: Refactoring selon les directives de Pierre. - Cresus.Core: TileContainer: Utilisation de Widgets.Direction.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@12773 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Misc.cs
+++ Epsitec.Cresus/Cresus.Core/Misc.cs
@@ -13,9 +13,34 @@ namespace Epsitec.Cresus.Core
 {
 	public static class Misc
 	{
+		/// <summary>
+		/// Retourne le texte permettant de mettre une icône sous forme d'image dans un texte riche.
+		/// </summary>
+		/// <param name="icon">Nom brut de l'icône, sans extension.</param>
+		/// <returns></returns>
+		public static string GetResourceIconImage(string icon)
+		{
+			return string.Format (@"<img src=""{0}""/>", Misc.GetResourceIconUri (icon));
+		}
+
+		/// <summary>
+		/// Retourne le texte permettant de mettre une icône sous forme d'image dans un texte riche.
+		/// </summary>
+		/// <param name="icon">Nom brut de l'icône, sans extension.</param>
+		/// <param name="verticalOffset">Offset vertical.</param>
+		/// <returns></returns>
+		 public static string Image(string icon, double verticalOffset)
+		{
+			return string.Format (@"<img src=""{0}"" voff=""{1}""/>", Misc.GetResourceIconUri (icon), verticalOffset.ToString (System.Globalization.CultureInfo.InvariantCulture));
+		}
+
+		/// <summary>
+		/// Retourne le nom complet d'une icône, à utiliser pour la propriété IconButton.IconUri.
+		/// </summary>
+		/// <param name="icon">Nom brut de l'icône, sans extension.</param>
+		/// <returns></returns>
 		public static string GetResourceIconUri(string icon)
 		{
-			//	Retourne le nom complet d'une icône.
 			return string.Format ("manifest:Epsitec.Cresus.Core.Images.{0}.icon", icon);
 		}
 
