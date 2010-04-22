f6477c96b6a32c4a20831b27967730370c6d77c5
Marc Bettex <bettex@epsitec.ch>
2010-04-22 15:47:56 +0000

    - Implémentation fonctionnelle (mais vraiment pas optimale et définitive) de AbstractRepository et GenericRepository. - En bonus CountryRepository et PersonRepository, avec le test unitaire pour CountryRepository, même si tout cela n'est pas définitif non plus.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@12780 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Data/AbstractRepository.cs
+++ Epsitec.Cresus/Cresus.Core/Data/AbstractRepository.cs
@@ -1,23 +1,95 @@
 ﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
+using Epsitec.Common.Support;
 using Epsitec.Common.Support.EntityEngine;
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Database;
+using Epsitec.Cresus.DataLayer;
 
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Epsitec.Cresus.Core.Data
 {
+
 	abstract class AbstractRepository
 	{
-		protected AbstractRepository()
+
+		protected AbstractRepository(DbInfrastructure dbInfrastructure, DataContext dataContext)
 		{
+			this.DbInfrastructure = dbInfrastructure;
+			this.DataContext = dataContext;
 		}
 
-		public EntityContext EntityContext
+		public DbInfrastructure DbInfrastructure
 		{
-			get;
-			set;
+			get
+			{
+				return this.dbInfrastructure;
+			}
+			private set
+			{
+				if (value == null)
+				{
+					throw new System.ArgumentNullException ();
+				}
+
+				this.dbInfrastructure = value;
+			}
 		}
+
+		public DataContext DataContext
+		{
+			get
+			{
+				return dataContext;
+			}
+			private set
+			{
+				if (value == null)
+				{
+					throw new System.ArgumentNullException ();
+				}
+
+				this.dataContext = value;
+			}
+		}
+
+		public EntityType GetEntityByExample<EntityType>(EntityType example) where EntityType : AbstractEntity, new ()
+		{
+			return this.GetEntitiesByExample (example).FirstOrDefault ();
+		}
+
+		public IEnumerable<EntityType> GetEntitiesByExample<EntityType>(EntityType example) where EntityType : AbstractEntity, new ()
+		{
+			DataBrowser dataBrowser = new DataBrowser (this.DbInfrastructure);
+			DataQuery dataQuery = new DataQuery ();
+			
+			foreach (var fieldId in example.GetEntityContext ().GetEntityFieldIds (example))
+			{
+				dataQuery.Columns.Add (new DataQueryColumn (EntityFieldPath.Parse (fieldId)));
+			}
+
+			// HACK Probably not the best way to get the CR_TYPE column because of the "[" and "]".
+			dataQuery.Columns.Add (new DataQueryColumn (EntityFieldPath.CreateRelativePath ("[" + Tags.ColumnInstanceType + "]")));
+			
+			using (DbTransaction transaction = dataBrowser.Infrastructure.BeginTransaction (DbTransactionMode.ReadOnly))
+			{
+				foreach (DataBrowserRow row in dataBrowser.QueryByExample (transaction, example, dataQuery))
+				{
+					yield return this.DataContext.ResolveEntity<EntityType> (row);
+				}
+
+				transaction.Commit ();
+			}
+		}
+
+		private DbInfrastructure dbInfrastructure;
+
+		private DataContext dataContext;
+
 	}
+
 }
--- Epsitec.Cresus/Cresus.Core/Data/CountryRepository.cs
+++ Epsitec.Cresus/Cresus.Core/Data/CountryRepository.cs
@@ -1,18 +1,54 @@
 ﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
+using Epsitec.Common.Support.EntityEngine;
+
 using Epsitec.Cresus.Core.Entities;
+using Epsitec.Cresus.Database;
+using Epsitec.Cresus.DataLayer;
 
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Epsitec.Cresus.Core.Data
 {
-	class CountryRepository : AbstractRepository
+
+	class CountryRepository : GenericRepository<CountryEntity>
 	{
+
+		public CountryRepository(DbInfrastructure dbInfrastructure, DataContext dataContext)
+			: base (dbInfrastructure, dataContext)
+		{
+			
+		}
+
+		public CountryEntity GetCountryByCode(string code)
+		{
+			CountryEntity example = new Entities.CountryEntity ()
+			{
+				Code = code,
+			};
+
+			return this.GetEntityByExample (example);
+		}
+
+		public CountryEntity GetCountryByName(string name)
+		{
+			CountryEntity example = new Entities.CountryEntity ()
+			{
+				Name = name,
+			};
+
+			return this.GetEntityByExample (example);
+		}
+
 		public IEnumerable<CountryEntity> GetAllCountries()
 		{
-			throw new System.NotImplementedException ();
+			CountryEntity example = new Entities.CountryEntity ();
+
+			return this.GetEntitiesByExample (example);
 		}
+
 	}
+
 }
