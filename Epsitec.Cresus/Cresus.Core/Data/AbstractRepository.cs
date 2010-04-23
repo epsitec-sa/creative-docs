15e86a3ccdc05980fb0c2b8c2fbf521e8ad630ce
Marc Bettex <bettex@epsitec.ch>
2010-04-23 13:18:11 +0000

    - Continué l'implémentation de AbstractRepository. - Manque encore la génération des proxys pour les sous types des entités.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@12807 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Data/AbstractRepository.cs
+++ Epsitec.Cresus/Cresus.Core/Data/AbstractRepository.cs
@@ -9,6 +9,7 @@ using Epsitec.Cresus.Database;
 using Epsitec.Cresus.DataLayer;
 
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 
 namespace Epsitec.Cresus.Core.Data
@@ -64,31 +65,79 @@ namespace Epsitec.Cresus.Core.Data
 
 		public IEnumerable<EntityType> GetEntitiesByExample<EntityType>(EntityType example) where EntityType : AbstractEntity, new ()
 		{
-			DataBrowser dataBrowser = new DataBrowser (this.DbInfrastructure);
-			DataQuery dataQuery = new DataQuery ();
-			
-			foreach (var fieldId in example.GetEntityContext ().GetEntityFieldIds (example))
-			{
-				dataQuery.Columns.Add (new DataQueryColumn (EntityFieldPath.Parse (fieldId)));
-			}
+			EntityType dummyEntity = this.DataContext.EntityContext.CreateEmptyEntity<EntityType> ();
+			Druid entityId = dummyEntity.GetEntityStructuredTypeId ();
 
-			// HACK Probably not the best way to get the CR_TYPE column because of the "[" and "]".
-			dataQuery.Columns.Add (new DataQueryColumn (EntityFieldPath.CreateRelativePath ("[" + Tags.ColumnInstanceType + "]")));
-			
+			DataBrowser dataBrowser = new DataBrowser (this.DbInfrastructure);
+			DataQuery dataQuery = this.BuildQuery (this.DataContext.EntityContext.GetStructuredType(dummyEntity) as StructuredType);
+
 			using (DbTransaction transaction = dataBrowser.Infrastructure.BeginTransaction (DbTransactionMode.ReadOnly))
 			{
-				foreach (DataBrowserRow row in dataBrowser.QueryByExample (transaction, example, dataQuery))
+				using (DataTable dataTable = this.BuildDataTable (entityId))
 				{
-					yield return this.DataContext.ResolveEntity<EntityType> (row);
+					foreach (DataBrowserRow dataBrowserRow in dataBrowser.QueryByExample (transaction, example, dataQuery))
+					{
+						DbKey rowKey = dataBrowserRow.Keys[0];
+						Druid realEntityTypeId = Druid.FromLong ((long) dataBrowserRow[AbstractRepository.InstanceTypeFieldPath]);
+						DataRow dataRow = this.BuildDataRow (dataTable, dataBrowserRow);
+
+						yield return this.DataContext.ResolveEntity (realEntityTypeId, entityId, rowKey, dataRow) as EntityType;
+					}
 				}
 
 				transaction.Commit ();
 			}
 		}
 
+		private DataQuery BuildQuery(StructuredType entityType)
+		{
+			DataQuery dataQuery = new DataQuery ();
+
+			foreach (string fieldId in entityType.GetFieldIds())
+			{
+				dataQuery.Columns.Add (new DataQueryColumn (EntityFieldPath.Parse (fieldId)));
+			}
+
+			dataQuery.Columns.Add (new DataQueryColumn (AbstractRepository.InstanceTypeFieldPath));
+
+			return dataQuery;
+		}
+
+		private DataTable BuildDataTable(Druid entityId)
+		{
+			DataTable dataTable = new DataTable ();
+
+			foreach (StructuredTypeField field in this.DataContext.EntityContext.GetEntityFieldDefinitions(entityId))
+			{
+				dataTable.Columns.Add (new DataColumn (this.DataContext.SchemaEngine.GetDataColumnName (field.Id)));
+			}
+
+			return dataTable;
+		}
+
+		private DataRow BuildDataRow(DataTable dataTable, DataBrowserRow dataBrowserRow)
+		{
+			DataRow dataRow = dataTable.NewRow ();
+
+			foreach (DataQueryColumn column in dataBrowserRow.Query.Columns)
+			{
+				if (column.FieldPath != AbstractRepository.InstanceTypeFieldPath)
+				{
+					string name = column.FieldPath.Fields.Last ();
+					object value = dataBrowserRow[column];
+
+					dataRow[this.DataContext.SchemaEngine.GetDataColumnName (name)] = value;
+				}
+			}
+
+			return dataRow;
+		}
+
 		private DbInfrastructure dbInfrastructure;
 
 		private DataContext dataContext;
+
+		private static EntityFieldPath InstanceTypeFieldPath = EntityFieldPath.CreateRelativePath ("[" + Tags.ColumnInstanceType + "]");
 
 	}
 
