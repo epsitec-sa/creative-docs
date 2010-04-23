1d049240640b5b4e24d9ff983af95b8e5f209dd4
Pierre Arnaud <arnaud@opac.ch>
2010-04-23 07:59:05 +0000

    - Layout des colonnes fonctionnel.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@12792 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Controllers/DataViewController.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/DataViewController.cs
@@ -17,27 +17,77 @@ namespace Epsitec.Cresus.Core.Controllers
 	/// </summary>
 	public class DataViewController : CoreViewController
 	{
-		public DataViewController(string name, AbstractEntity entity)
+		public DataViewController(string name)
 			: base (name)
 		{
-			this.entity = entity;
-
-			this.viewController = EntityViewController.CreateViewController ("DataViewController", entity, ViewControllerMode.Compact);
-			System.Diagnostics.Debug.Assert (this.viewController != null);
+			this.viewControllers = new Stack<EntityViewController> ();
 		}
 
 		public override IEnumerable<CoreController> GetSubControllers()
 		{
-			yield return this.viewController;
+			return this.viewControllers;
 		}
 
 		public override void CreateUI(Widget container)
 		{
-			this.viewController.CreateUI (container);
+			this.frame = new FrameBox ()
+			{
+				Parent = container,
+				Dock = DockStyle.Fill,
+			};
+
+			this.viewLayoutController = new ViewLayoutController (this.Name + ".ViewLayout", this.frame);
+		}
+
+		public void SelectEntity(AbstractEntity entity)
+		{
+			this.ClearActiveEntity ();
+
+			this.entity = entity;
+
+			this.PushViewController (EntityViewController.CreateViewController ("ViewController", this.entity, ViewControllerMode.Compact));
+		}
+
+		public void PushViewController(EntityViewController controller)
+		{
+			System.Diagnostics.Debug.Assert (controller != null);
+
+			var column = this.viewLayoutController.CreateColumn ();
+
+			this.viewControllers.Push (controller);
+			
+			controller.CreateUI (column);
+		}
+
+		public void PopViewController()
+		{
+			System.Diagnostics.Debug.Assert (this.viewControllers.Count > 0);
+
+			var controller = this.viewControllers.Pop ();
+
+			controller.Dispose ();
+			this.viewLayoutController.DeleteColumn ();
+		}
+
+		public void ClearActiveEntity()
+		{
+			if (this.entity != null)
+            {
+				while (this.viewControllers.Count > 0)
+                {
+					this.PopViewController ();
+                }
+				
+				this.entity = null;
+            }
 		}
 
 
+		private readonly Stack<EntityViewController> viewControllers;
+		
+		private ViewLayoutController viewLayoutController;
+		private FrameBox frame;
+		
 		private AbstractEntity entity;
-		private EntityViewController viewController;
 	}
 }
