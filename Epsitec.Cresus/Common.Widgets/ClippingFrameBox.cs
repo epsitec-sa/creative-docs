4627fda7bc601f6f8b05ffa2ef5ef05a3169242c
Pierre Arnaud <arnaud@opac.ch>
2010-05-04 03:27:06 +0000

    - Ajouté un ClippingMode pour le ClippingFrameBox.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@12863 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Widgets/ClippingFrameBox.cs
+++ Epsitec.Cresus/Common.Widgets/ClippingFrameBox.cs
@@ -4,14 +4,14 @@
 using Epsitec.Common.Drawing;
 using Epsitec.Common.Types;
 using Epsitec.Common.Widgets;
-using Epsitec.Common.Widgets.Helpers;
 
 [assembly: DependencyClass (typeof (ClippingFrameBox))]
 
 namespace Epsitec.Common.Widgets
 {
 	/// <summary>
-	/// The <c>ClippingFrameBox</c> class ...
+	/// The <c>ClippingFrameBox</c> class is a special <see cref="FrameBox"/> which provides
+	/// the ability to clip its children horizontally and/or vertically.
 	/// </summary>
 	public class ClippingFrameBox : FrameBox
 	{
@@ -19,10 +19,25 @@ namespace Epsitec.Common.Widgets
 		{
 		}
 
-		public ClippingFrameBox(Widget embedder)
-			: this ()
+
+		/// <summary>
+		/// Gets or sets the clipping mode.
+		/// </summary>
+		/// <value>The clipping mode.</value>
+		public ClippingMode ClippingMode
 		{
-			this.SetEmbedder (embedder);
+			get
+			{
+				return this.clippingMode;
+			}
+			set
+			{
+				if (this.clippingMode != value)
+				{
+					this.clippingMode = value;
+					Layouts.LayoutContext.AddToMeasureQueue (this);
+				}
+			}
 		}
 
 		protected override void MeasureMinMax(ref Size min, ref Size max)
@@ -32,8 +47,23 @@ namespace Epsitec.Common.Widgets
 			
 			base.MeasureMinMax (ref min, ref max);
 
-			min = new Size (oldMin.Width, min.Height);
-			max = new Size (oldMax.Width, max.Height);
+			if ((this.clippingMode & Widgets.ClippingMode.ClipWidth) != 0)
+			{
+				//	Don't take into account the children width in the min/max measurement :
+
+				min = new Size (oldMin.Width, min.Height);
+				max = new Size (oldMax.Width, max.Height);
+			}
+
+			if ((this.clippingMode & Widgets.ClippingMode.ClipHeight) != 0)
+			{
+				//	Don't take into account the children height in the min/max measurement :
+
+				min = new Size (min.Width, oldMin.Height);
+				max = new Size (max.Width, oldMax.Height);
+			}
 		}
+		
+		private ClippingMode clippingMode;
 	}
 }
