cd4a81f5fadec22b90eaa0db50d18dcb38ac4a54
Marc Bettex <bettex@epsitec.ch>
2010-05-04 09:56:55 +0000

    - Test de performance pour Cresus.Core.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@12867 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/X.Core.PerformanceTests/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+
+using Epsitec.Cresus.Core.Data;
+using Epsitec.Cresus.Core.Entities;
+
+using Epsitec.Cresus.Database;
+using Epsitec.Cresus.DataLayer;
+using Epsitec.Common.Support;
+
+
+
+namespace Cresus.Core
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ResourceManagerPool.Default = new ResourceManagerPool ("default");
+			ResourceManagerPool.Default.AddResourceProbingPath (@"S:\Epsitec.Cresus\Cresus.Core");
+
+			new UnitTestPerformance ().Check03RetreiveData ();
+		}
+	}
+
+
+	public class UnitTestPerformance
+	{
+
+		public void Check03RetreiveData()
+		{
+			UnitTestPerformance.dbInfrastructure = new DbInfrastructure ();
+			UnitTestPerformance.dbInfrastructure.AttachToDatabase (DbInfrastructure.CreateDatabaseAccess ("CORETEST"));
+			using (DataContext dataContext = new DataContext (UnitTestPerformance.dbInfrastructure))
+			{
+				Repository repository = new Repository (UnitTestPerformance.dbInfrastructure, dataContext);
+
+				System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch ();
+				watch.Start ();
+				repository.GetEntitiesByExample<CountryEntity> (new CountryEntity ()).Count ();
+				watch.Stop ();
+				System.Diagnostics.Debug.WriteLine (watch.ElapsedMilliseconds);
+
+				watch.Start ();
+				repository.GetEntitiesByExample<TelecomContactEntity> (new TelecomContactEntity ()).Count ();
+				watch.Stop ();
+				System.Diagnostics.Debug.WriteLine (watch.ElapsedMilliseconds);
+				watch.Start ();
+				repository.GetEntitiesByExample<TelecomContactEntity> (new TelecomContactEntity ()).Count ();
+				watch.Stop ();
+				System.Diagnostics.Debug.WriteLine (watch.ElapsedMilliseconds);
+			}
+		}
+
+		private static DbInfrastructure dbInfrastructure;
+
+
+	}
+
+
+}
