324a8e654b5b085ca36b8260cc5c52a80d51cb0e
Marc Bettex <bettex@epsitec.ch>
2010-05-18 14:14:58 +0000

    - Le DataBrowser n'utilise plus de DataRow pour communiquer les collections au DataContext.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@12960 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.DataLayer/DataContext.cs
+++ Epsitec.Cresus/Cresus.DataLayer/DataContext.cs
@@ -126,7 +126,7 @@ namespace Epsitec.Cresus.DataLayer
 			return this.ResolveEntity (rowKey, entityId) as T;
 		}
 
-		public AbstractEntity ResolveEntity (Druid realEntityId, Druid askedEntityId, DbKey rowKey, System.Data.DataRow valuesRow, System.Data.DataRow referencesRow, Dictionary<StructuredTypeField, List<System.Data.DataRow>> collectionRows)
+		public AbstractEntity ResolveEntity(Druid realEntityId, Druid askedEntityId, DbKey rowKey, System.Data.DataRow valuesRow, System.Data.DataRow referencesRow, Dictionary<StructuredTypeField, DbKey[]> collectionKeys)
 		{
 			Druid baseEntityId =  this.EntityContext.GetBaseEntityId (askedEntityId);
 
@@ -149,7 +149,7 @@ namespace Epsitec.Cresus.DataLayer
 
 					foreach (Druid currentId in entityIds.SkipWhile(id => id != askedEntityId))
 					{
-						this.DeserializeEntityLocalWithReference (entity, valuesRow, referencesRow, collectionRows, currentId);
+						this.DeserializeEntityLocalWithReference (entity, valuesRow, referencesRow, collectionKeys, currentId);
 					}
 				}
 			}
@@ -514,7 +514,7 @@ namespace Epsitec.Cresus.DataLayer
 		}
 
 
-		private void DeserializeEntityLocalWithReference(AbstractEntity entity, System.Data.DataRow valuesRow, System.Data.DataRow referencesRow, Dictionary<StructuredTypeField, List<System.Data.DataRow>> collectionRows, Druid entityId)
+		private void DeserializeEntityLocalWithReference(AbstractEntity entity, System.Data.DataRow valuesRow, System.Data.DataRow referencesRow, Dictionary<StructuredTypeField, DbKey[]> collectionKeys, Druid entityId)
 		{
 			foreach (StructuredTypeField field in this.entityContext.GetEntityLocalFieldDefinitions (entityId))
 			{
@@ -543,11 +543,9 @@ namespace Epsitec.Cresus.DataLayer
 
 						System.Collections.IList collection = entity.InternalGetFieldCollection (field.Id);
 
-						foreach (System.Data.DataRow dataRow in collectionRows[field])
+						foreach (DbKey key in collectionKeys[field])
 						{
-							DbKey idAsKey = new DbKey (new DbId ((long) dataRow[Tags.ColumnId]));
-							object proxy = this.InternalResolveEntity (idAsKey, field.TypeId, EntityResolutionMode.DelayLoad);
-							collection.Add (proxy);
+							collection.Add (this.InternalResolveEntity (key, field.TypeId, EntityResolutionMode.DelayLoad));
 						}
 
 						break;
