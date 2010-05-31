350f4bcd7a04729a7da5d2c12daa71bb1b460107
Marc Bettex <bettex@epsitec.ch>
2010-05-31 08:25:39 +0000

    - Rajouté une classe EntityCollectionFieldProxy qui est un proxy pour les collections d'entités. - Renomé FieldProxy en ValueFieldProxy pour la cohérence du nommage.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@13013 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.DataLayer/Helpers/EntityCollectionFieldProxy.cs
@@ -0,0 +1,83 @@
+﻿using Epsitec.Common.Support;
+using Epsitec.Common.Support.EntityEngine;
+using Epsitec.Common.Types;
+
+using System.Linq;
+using System.Collections;
+
+namespace Epsitec.Cresus.DataLayer.Helpers
+{
+
+
+	public class EntityCollectionFieldProxy: IEntityProxy
+	{
+
+
+		public EntityCollectionFieldProxy(DataContext dataContext, AbstractEntity entity, StructuredTypeField field)
+		{
+			this.dataContext = dataContext;
+			this.entity = entity;
+			this.field = field;
+		}
+
+
+		#region IEntityProxy Members
+
+
+		public object GetReadEntityValue(IValueStore store, string id)
+		{
+			object value = this.PromoteToRealInstance ();
+
+			store.SetValue (id, value, ValueStoreSetMode.Default);
+
+			return value;
+		}
+
+
+		public object GetWriteEntityValue(IValueStore store, string id)
+		{
+			return this;
+		}
+
+		
+		public bool DiscardWriteEntityValue(IValueStore store, string id, ref object value)
+		{
+			return false;
+		}
+
+
+		public object PromoteToRealInstance()
+		{
+			Druid entityId = this.entity.GetEntityStructuredTypeId ();
+			Druid localEntityId = this.dataContext.EntityContext.GetLocalEntityId (entityId, this.field.CaptionId);
+
+			System.Type itemType = this.dataContext.EntityContext.CreateEmptyEntity (this.field.TypeId).GetType ();
+			System.Type collectionType = typeof (EntityCollection<>).MakeGenericType (itemType);
+
+			IList targets = System.Activator.CreateInstance (collectionType, this.field.Id, this.entity, false) as IList;
+
+			foreach (object item in this.dataContext.ReadFieldRelation (this.entity, localEntityId, this.field, EntityResolutionMode.Load))
+			{
+				targets.Add (item);
+			}
+
+			return targets;
+		}
+
+
+		#endregion
+	
+		
+		private DataContext dataContext;
+
+
+		private AbstractEntity entity;
+
+
+		private StructuredTypeField field;
+
+
+	}
+
+
+}
