adaf0b40f4e4d9cff99c853e5883b9f88f425ef3
Pierre Arnaud <arnaud@opac.ch>
2010-06-02 04:49:12 +0000

    - Renommé StaticItems en SimpleItems dans SummaryDataItems. - Simplifications supplémentaires.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@13033 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Controllers/DataAccessors/SummaryDataType.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/DataAccessors/SummaryDataType.cs
@@ -3,12 +3,30 @@
 
 namespace Epsitec.Cresus.Core.Controllers.DataAccessors
 {
+	/// <summary>
+	/// The <c>SummaryDataType</c> defines the type of a <see cref="SummaryData"/> object.
+	/// </summary>
 	public enum SummaryDataType
 	{
+		/// <summary>
+		/// Undefined type.
+		/// </summary>
 		Undefined,
 
+		/// <summary>
+		/// Simple item: represents plain vanilla fields of an entity.
+		/// </summary>
 		SimpleItem,
+
+		/// <summary>
+		/// Empty item: used as a template to create collection items.
+		/// </summary>
 		EmptyItem,
+
+		/// <summary>
+		/// Collection item: automatically created by a collection accessor, based on
+		/// a template (usually an empty item).
+		/// </summary>
 		CollectionItem,
 	}
 }
