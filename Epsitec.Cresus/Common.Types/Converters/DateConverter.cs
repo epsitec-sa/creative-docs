7d75d3f1b51540c863f7421d3483d811ad0672f0
Pierre Arnaud <arnaud@opac.ch>
2010-06-03 03:49:11 +0000

    - Ajouté une classe DateConverter.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13050 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/Converters/DateConverter.cs
@@ -0,0 +1,56 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Common.Types.Converters
+{
+	/// <summary>
+	/// The <c>DateConverter</c> is used to convert <see cref="Date"/> to/from
+	/// <c>string</c>.
+	/// </summary>
+	public static class DateConverter
+	{
+		public static string ConvertToString(Date date)
+		{
+			return date == Date.Null ? null : date.ToDateTime ().ToShortDateString ();
+		}
+
+		public static string ConvertToString(Date? date)
+		{
+			return date.HasValue ? DateConverter.ConvertToString (date.Value) : null;
+		}
+
+		public static Date? ConvertFromString(string text)
+		{
+			if (string.IsNullOrWhiteSpace (text))
+            {
+				return null;
+            }
+			
+			System.DateTime result;
+
+			if (System.DateTime.TryParse (text, System.Globalization.CultureInfo.CurrentCulture, System.Globalization.DateTimeStyles.AssumeLocal | System.Globalization.DateTimeStyles.AllowWhiteSpaces, out result))
+			{
+				return new Date (result);
+			}
+			
+			return null;
+		}
+
+		public static bool CanConvertFromString(string text)
+		{
+			System.DateTime result;
+
+			if (System.DateTime.TryParse (text, System.Globalization.CultureInfo.CurrentCulture, System.Globalization.DateTimeStyles.AssumeLocal | System.Globalization.DateTimeStyles.AllowWhiteSpaces, out result))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
