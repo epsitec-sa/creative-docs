9ba08e19f18ea8217573d0adbedf55367609d578
Pierre Arnaud <arnaud@opac.ch>
2010-06-03 03:49:35 +0000

    - EditionNaturalPersonViewController n'utilise plus son accesseur.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@13051 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionNaturalPersonViewController.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionNaturalPersonViewController.cs
@@ -1,6 +1,8 @@
 ﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
+using Epsitec.Common.Types.Converters;
+
 using Epsitec.Cresus.Core.Widgets;
 using Epsitec.Cresus.Core.Widgets.Tiles;
 
@@ -46,8 +48,6 @@ namespace Epsitec.Cresus.Core.Controllers.EditionControllers
 
 			var person = this.Entity;
 			var group = builder.CreateEditionGroupingTile ("Data.NaturalPerson", "Personne physique");
-
-			var accessor = new Accessors.NaturalPersonAccessor (person);
 			var tile = builder.CreateEditionTile (group, this.Entity);
 
 			builder.CreateFooterEditorTile ();
@@ -64,13 +64,23 @@ namespace Epsitec.Cresus.Core.Controllers.EditionControllers
 
 			titleCtrl.Attach (titleHint);
 			
-
-
-			builder.CreateTextField (tile.Container, 0, "Prénom", accessor.Entity.Firstname, x => accessor.Entity.Firstname = x, Validators.StringValidator.Validate);
-			builder.CreateTextField (tile.Container, 0, "Nom", accessor.Entity.Lastname, x => accessor.Entity.Lastname = x, Validators.StringValidator.Validate);
+			builder.CreateTextField (tile.Container, 0, "Prénom", this.Entity.Firstname, x => this.Entity.Firstname = x, Validators.StringValidator.Validate);
+			builder.CreateTextField (tile.Container, 0, "Nom", this.Entity.Lastname, x => this.Entity.Lastname = x, Validators.StringValidator.Validate);
 			builder.CreateMargin (tile.Container, true);
-			builder.CreateCombo (tile.Container, 0, "Sexe", accessor.GenderInitializer, true, false, true, accessor.Gender, x => accessor.Gender = x, null);
-			builder.CreateTextField (tile.Container, 75, "Date de naissance", accessor.NaturalBirthDate, x => accessor.NaturalBirthDate = x, null);
+
+			var genderHint = builder.CreateHintEditor (tile.Container, "Sexe", this.Entity.Gender, null, x => this.Entity.Gender = x as Entities.PersonGenderEntity);
+			var genderCtrl = new HintEditorController<Entities.PersonGenderEntity>
+			{
+				ValueGetter = () => this.Entity.Gender,
+				ValueSetter = x => this.Entity.Gender = x,
+				Items = CoreProgram.Application.Data.GetGenders (),
+				ToTextArrayConverter = x => new string[] { x.Name },
+				ToFormattedTextConverter = x => UIBuilder.FormatText (x.Name)
+			};
+
+			genderCtrl.Attach (genderHint);
+			
+			builder.CreateTextField (tile.Container, 75, "Date de naissance", DateConverter.ConvertToString (this.Entity.BirthDate), x => this.Entity.BirthDate = DateConverter.ConvertFromString (x), x => DateConverter.CanConvertFromString (x));
 
 			UI.SetInitialFocus (container);
 #endif
