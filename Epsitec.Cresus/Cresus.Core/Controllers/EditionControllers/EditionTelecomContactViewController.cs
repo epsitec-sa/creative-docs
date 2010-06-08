f62b68d02a016b8698ba555bbd0c2293f960ae32
Daniel Roux <roux@epsitec.ch>
2010-06-08 07:09:35 +0000

    - Cresus.Core: EditionMailContactViewController permet d'éditer les rôles.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13089 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionTelecomContactViewController.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionTelecomContactViewController.cs
@@ -21,49 +21,6 @@ namespace Epsitec.Cresus.Core.Controllers.EditionControllers
 
 		protected override void CreateUI(TileContainer container)
 		{
-#if false
-			UIBuilder builder = new UIBuilder (container, this);
-			
-			TitleTile group;
-			EditionTile tile;
-
-			System.Diagnostics.Debug.Assert (this.Entity != null);
-			var accessor = new Accessors.TelecomContactAccessor (null, this.Entity, false);
-
-			//	Crée les tuiles.
-			builder.CreateHeaderEditorTile ();
-
-			//	Crée le contenu de la tuile d'édition.
-			group = builder.CreateSummaryGroupingTile ("Data.Roles", "Rôles");
-
-			var roleAccessor = new Accessors.RolesContactAccessor (null, accessor.TelecomContact, false)
-			{
-				ViewControllerMode = ViewControllerMode.RolesEdition
-			};
-
-			builder.CreateSummaryTile (group, roleAccessor);
-
-			//	Crée le contenu de la tuile d'édition.
-			group = builder.CreateSummaryGroupingTile ("Data.Type", "Type");
-
-			var telecomTypeAccessor = new Accessors.TelecomTypeAccessor (null, accessor.TelecomContact, false)
-			{
-				ViewControllerMode = ViewControllerMode.TelecomTypeEdition
-			};
-
-			builder.CreateSummaryTile (group, telecomTypeAccessor);
-
-			//	Crée le contenu de la tuile d'édition.
-			group = builder.CreateEditionGroupingTile ("Data.Telecom", "Téléphone");
-			tile = builder.CreateEditionTile (group, accessor);
-
-			builder.CreateLinkButtons (tile.Container);
-
-			builder.CreateTextField (tile.Container, 150, "Numéro de téléphone", accessor.TelecomContact.Number, x => accessor.TelecomContact.Number = x, Validators.StringValidator.Validate);
-			builder.CreateTextField (tile.Container, 100, "Numéro interne", accessor.TelecomContact.Extension, x => accessor.TelecomContact.Extension = x, Validators.StringValidator.Validate);
-
-			UI.SetInitialFocus (container);
-#else
 			var builder = new UIBuilder (container, this);
 
 			builder.CreateHeaderEditorTile ();
@@ -76,8 +33,9 @@ namespace Epsitec.Cresus.Core.Controllers.EditionControllers
 			builder.CreateFooterEditorTile ();
 
 			UI.SetInitialFocus (container);
-#endif
 		}
+
+
 		private void CreateUIRoles(Epsitec.Cresus.Core.UIBuilder builder)
 		{
 			var controller = new SelectionController<Entities.ContactRoleEntity>
