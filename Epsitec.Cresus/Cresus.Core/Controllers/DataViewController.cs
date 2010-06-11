e6adfb2cfaed754c535661a9b82397c519648405
Pierre Arnaud <arnaud@opac.ch>
2010-06-11 05:53:16 +0000

    - Tout recompile proprement. J'espère avoir réintégré toutes les modifications.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13137 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Controllers/DataViewController.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/DataViewController.cs
@@ -124,11 +124,11 @@ namespace Epsitec.Cresus.Core.Controllers
 
 				if (leafController == null)
 				{
-					controller.DataContext = this.DataContext;
+					controller.DataContext = this.data.DataContext;
 				}
 				else
 				{
-					controller.DataContext = leafController.DataContext ?? this.DataContext;
+					controller.DataContext = leafController.DataContext ?? this.data.DataContext;
 				}
 			}
 
@@ -154,6 +154,7 @@ namespace Epsitec.Cresus.Core.Controllers
 				(leafController.DataContext != lastController.DataContext))
 			{
 				this.data.SaveDataContext (lastController.DataContext);
+				this.data.DisposeDataContext (lastController.DataContext);
 			}
 
 			lastController.Dispose ();
