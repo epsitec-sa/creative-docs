85cd3535fa9b24da329940357ee25a852597ca7e
Daniel Roux <roux@epsitec.ch>
2010-06-14 07:09:16 +0000

    - Cresus.Core: Mise en commun du code pour les UriContacts et les TelecomContacts. - Cresus.Core: ATTENTION, ne compile plus !
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13159 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Controllers/SummaryControllers/Common.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/SummaryControllers/Common.cs
@@ -27,9 +27,9 @@ namespace Epsitec.Cresus.Core.Controllers.SummaryControllers
 			where T3 : Entities.MailContactEntity, T2, new ()
 		{
 			var template = new CollectionTemplate<T3> ("MailContact")
-				.DefineTitle (x => UIBuilder.FormatText ("Adresse", "(", string.Join (", ", x.Roles.Select (role => role.Name)), ")"))
-				.DefineText (x => UIBuilder.FormatText (x.LegalPerson.Name, "\n", x.LegalPerson.Complement, "\n", x.Complement, "\n", x.Address.Street.StreetName, "\n", x.Address.Street.Complement, "\n", x.Address.PostBox.Number, "\n", x.Address.Location.Country.Code, "~-", x.Address.Location.PostalCode, x.Address.Location.Name))
-				.DefineCompactText (x => UIBuilder.FormatText (x.Address.Street.StreetName, "~,", x.Address.Location.PostalCode, x.Address.Location.Name));
+				.DefineTitle		(x => UIBuilder.FormatText ("Adresse", "(", string.Join (", ", x.Roles.Select (role => role.Name)), ")"))
+				.DefineText			(x => UIBuilder.FormatText (x.LegalPerson.Name, "\n", x.LegalPerson.Complement, "\n", x.Complement, "\n", x.Address.Street.StreetName, "\n", x.Address.Street.Complement, "\n", x.Address.PostBox.Number, "\n", x.Address.Location.Country.Code, "~-", x.Address.Location.PostalCode, x.Address.Location.Name))
+				.DefineCompactText	(x => UIBuilder.FormatText (x.Address.Street.StreetName, "~,", x.Address.Location.PostalCode, x.Address.Location.Name));
 
 
 			data.Add (
@@ -39,6 +39,68 @@ namespace Epsitec.Cresus.Core.Controllers.SummaryControllers
 					IconUri		 = "Data.Mail",
 					Title		 = UIBuilder.FormatText ("Adresse"),
 					CompactTitle = UIBuilder.FormatText ("Adresse"),
+					Text		 = UIBuilder.FormatText ("<i>vide</i>")
+				});
+
+			data.Add (CollectionAccessor.Create (source, collectionResolver, template));
+		}
+
+
+		public static void CreateUITelecomContacts<T1>(SummaryDataItems data, System.Func<T1> source, System.Func<T1, System.Collections.Generic.IList<Entities.AbstractContactEntity>> collectionResolver)
+			where T1 : AbstractEntity, new ()
+		{
+			Common.InternalCreateUITelecomContacts<T1, Entities.AbstractContactEntity, Entities.TelecomContactEntity> (data, source, collectionResolver);
+		}
+
+		private static void InternalCreateUITelecomContacts<T1, T2, T3>(SummaryDataItems data, System.Func<T1> source, System.Func<T1, System.Collections.Generic.IList<T2>> collectionResolver)
+			where T1 : AbstractEntity, new ()
+			where T2 : AbstractEntity, new ()
+			where T3 : Entities.TelecomContactEntity, T2, new ()
+		{
+			var template = new CollectionTemplate<Entities.TelecomContactEntity> ("TelecomContact")
+				.DefineTitle		(x => UIBuilder.FormatText (x.TelecomType.Name))
+				.DefineText			(x => UIBuilder.FormatText (x.Number, "(", string.Join (", ", x.Roles.Select (role => role.Name)), ")"))
+				.DefineCompactText  (x => UIBuilder.FormatText (x.Number, "(", x.TelecomType.Name, ")"));
+
+			data.Add (
+				new SummaryData
+				{
+					AutoGroup    = true,
+					Name		 = "TelecomContact",
+					IconUri		 = "Data.Telecom",
+					Title		 = UIBuilder.FormatText ("Téléphone"),
+					CompactTitle = UIBuilder.FormatText ("Téléphone"),
+					Text		 = UIBuilder.FormatText ("<i>vide</i>")
+				});
+
+			data.Add (CollectionAccessor.Create (source, collectionResolver, template));
+		}
+
+
+		public static void CreateUIUriContacts<T1>(SummaryDataItems data, System.Func<T1> source, System.Func<T1, System.Collections.Generic.IList<Entities.AbstractContactEntity>> collectionResolver)
+			where T1 : AbstractEntity, new ()
+		{
+			Common.InternalCreateUIUriContacts<T1, Entities.AbstractContactEntity, Entities.UriContactEntity> (data, source, collectionResolver);
+		}
+
+		private static void InternalCreateUIUriContacts<T1, T2, T3>(SummaryDataItems data, System.Func<T1> source, System.Func<T1, System.Collections.Generic.IList<T2>> collectionResolver)
+			where T1 : AbstractEntity, new ()
+			where T2 : AbstractEntity, new ()
+			where T3 : Entities.UriContactEntity, T2, new ()
+		{
+			var template = new CollectionTemplate<Entities.UriContactEntity> ("UriContact", filter: x => x.UriScheme.Code == "mailto")
+				.DefineText			(x => UIBuilder.FormatText (x.Uri, "(", string.Join (", ", x.Roles.Select (role => role.Name)), ")"))
+				.DefineCompactText	(x => UIBuilder.FormatText (x.Uri))
+				.DefineSetupItem	(x => x.UriScheme = CoreProgram.Application.Data.GetUriScheme ("mailto"));
+
+			data.Add (
+				new SummaryData
+				{
+					AutoGroup    = true,
+					Name		 = "UriContact",
+					IconUri		 = "Data.Uri",
+					Title		 = UIBuilder.FormatText ("E-Mail"),
+					CompactTitle = UIBuilder.FormatText ("E-Mail"),
 					Text		 = UIBuilder.FormatText ("<i>vide</i>")
 				});
 
