9801f28818ef2e3660f457350a9eb51a5adf95aa
Pierre Arnaud <arnaud@opac.ch>
2010-06-22 12:54:58 +0000

    - Ajouté les coquilles vides qui permettront d'afficher des réglages liés à la liste de gauche.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13232 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Controllers/BrowserSettingsController.cs
@@ -0,0 +1,29 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Controllers
+{
+	class BrowserSettingsController : CoreViewController
+	{
+		public BrowserSettingsController(string name, BrowserViewController browser)
+			: base (name)
+		{
+			this.browser = browser;
+		}
+
+		public override IEnumerable<CoreController> GetSubControllers()
+		{
+			yield break;
+		}
+
+		public override void CreateUI(Common.Widgets.Widget container)
+		{
+			container.BackColor = Common.Drawing.Color.FromRgb (0.9, 0.9, 1.0);
+		}
+
+		private readonly BrowserViewController browser;
+	}
+}
--- Epsitec.Cresus/Cresus.Core/Controllers/MainViewController.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/MainViewController.cs
@@ -21,8 +21,9 @@ namespace Epsitec.Cresus.Core.Controllers
 		{
 			this.data     = data;
 
-			this.browserController = new BrowserViewController ("MainBrowser", data);
-			this.dataViewController = new DataViewController ("MainViewer", data);
+			this.browserController = new BrowserViewController ("Browser", data);
+			this.browserSettingsController = new BrowserSettingsController ("BrowserSettings", this.browserController);
+			this.dataViewController = new DataViewController ("Data", data);
 
 			this.browserController.SetContents (() => this.data.GetCustomers ());
 			
@@ -39,24 +40,71 @@ namespace Epsitec.Cresus.Core.Controllers
 				};
 		}
 
+		public BrowserSettingsMode BrowserSettingsMode
+		{
+			get
+			{
+				return this.browserSettingsMode;
+			}
+			set
+			{
+				if (this.browserSettingsMode != value)
+                {
+					this.browserSettingsMode = value;
+					this.UpdateBrowserSettingsPanel ();
+                }
+			}
+		}
+
 
 		public override IEnumerable<CoreController> GetSubControllers()
 		{
 			yield return this.browserController;
+			yield return this.browserSettingsController;
 			yield return this.dataViewController;
 		}
 
 		public override void CreateUI(Widget container)
 		{
 			this.CreateUIFrame (container);
-			this.CreateUILeftPanel ();
-			this.CreateUIRightPanel ();
-			this.CreateUISplitter ();
 
 			this.browserController.CreateUI (this.leftPanel);
+			this.browserSettingsController.CreateUI (this.browserSettingsPanel);
 			this.dataViewController.CreateUI (this.rightPanel);
+
+			this.BrowserSettingsMode = Controllers.BrowserSettingsMode.Compact;
 		}
 
+
+		private void UpdateBrowserSettingsPanel()
+		{
+			var expandedPanel = this.topPanel;
+			var compactPanel  = this.browserController.SettingsPanel;
+			
+			switch (this.browserSettingsMode)
+			{
+				case Controllers.BrowserSettingsMode.Hidden:
+					expandedPanel.Visibility = false;
+					compactPanel.Visibility  = false;
+					this.browserSettingsPanel.Parent = null;
+					break;
+
+				case Controllers.BrowserSettingsMode.Compact:
+					expandedPanel.Visibility = false;
+					compactPanel.Visibility  = true;
+					this.browserSettingsPanel.Parent = compactPanel;
+					break;
+
+				case Controllers.BrowserSettingsMode.Expanded:
+					expandedPanel.Visibility = true;
+					compactPanel.Visibility  = false;
+					this.browserSettingsPanel.Parent = expandedPanel;
+					break;
+
+				default:
+					throw new System.NotSupportedException (string.Format ("BrowserSettingsMode.{0} not supported", this.browserSettingsMode));
+			}
+		}
 
 		private void CreateUIFrame(Widget container)
 		{
@@ -66,6 +114,33 @@ namespace Epsitec.Cresus.Core.Controllers
 				Dock = DockStyle.Fill,
 				DrawFullFrame = false,
 				DrawFrameState = FrameState.None
+			};
+			
+			this.CreateUITopPanel ();
+			this.CreateUISettingsPanel ();
+			this.CreateUILeftPanel ();
+			this.CreateUIRightPanel ();
+			this.CreateUISplitter ();
+		}
+
+		private void CreateUITopPanel()
+		{
+			this.topPanel = new FrameBox
+			{
+				Parent = this.frame,
+				Name = "TopPanel",
+				Dock = DockStyle.Top,
+				PreferredHeight = 40,
+			};
+		}
+
+		private void CreateUISettingsPanel()
+		{
+			this.browserSettingsPanel = new FrameBox
+			{
+				Parent = this.topPanel,
+				Name = "SettingsPanel",
+				Dock = DockStyle.Fill,
 			};
 		}
 
@@ -104,12 +179,17 @@ namespace Epsitec.Cresus.Core.Controllers
 		
 		private readonly CoreData data;
 		private readonly BrowserViewController browserController;
+		private readonly BrowserSettingsController browserSettingsController;
 		private readonly DataViewController dataViewController;
 
 		private FrameBox frame;
 
+		private FrameBox topPanel;
+		private FrameBox browserSettingsPanel;
 		private FrameBox leftPanel;
 		private VSplitter splitter;
 		private FrameBox rightPanel;
+
+		private BrowserSettingsMode browserSettingsMode;
 	}
 }
