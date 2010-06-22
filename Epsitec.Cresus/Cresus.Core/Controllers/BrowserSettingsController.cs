9e30091ef18649c958b4ee850b4417bdebeb53aa
Pierre Arnaud <arnaud@opac.ch>
2010-06-22 13:46:49 +0000

    - Ajouté un bouton composite pour les réglages de la liste de gauche.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@13233 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Controllers/BrowserSettingsController.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/BrowserSettingsController.cs
@@ -1,17 +1,27 @@
 ﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
+using Epsitec.Common.Drawing;
+using Epsitec.Common.Widgets;
+
+using Epsitec.Cresus.Core.Widgets;
+
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Epsitec.Cresus.Core.Controllers
 {
+	/// <summary>
+	/// The <c>BrowserSettingsController</c> class manages the settings associated
+	/// with a <see cref="BrowserViewController"/>, such as the search criteria.
+	/// </summary>
 	class BrowserSettingsController : CoreViewController
 	{
 		public BrowserSettingsController(string name, BrowserViewController browser)
 			: base (name)
 		{
 			this.browser = browser;
+			this.browser.CurrentChanged += this.HandleBrowserCurrentChanged;
 		}
 
 		public override IEnumerable<CoreController> GetSubControllers()
@@ -19,11 +29,38 @@ namespace Epsitec.Cresus.Core.Controllers
 			yield break;
 		}
 
-		public override void CreateUI(Common.Widgets.Widget container)
+		public override void CreateUI(Widget container)
 		{
-			container.BackColor = Common.Drawing.Color.FromRgb (0.9, 0.9, 1.0);
+			this.CreateUIMainIcon (container);
+		}
+
+		private void CreateUIMainIcon(Widget container)
+		{
+			this.iconBackground = new LayeredIconButton
+			{
+				Parent = container,
+				PreferredSize = new Size (28, 28),
+				PreferredIconSize = new Size (24, 24),
+				Dock = DockStyle.Left,
+				IconUri = Misc.GetResourceIconUri ("Edition.NewRecord"),
+			};
+		}
+
+		
+		private void HandleBrowserCurrentChanged(object sender)
+		{
+			this.UpdateStaticTextIcon ();
+		}
+
+		private void UpdateStaticTextIcon()
+		{
+			this.iconBackground.ClearOverlays ();
+			this.iconBackground.AddOverlay (Misc.GetResourceIconUri ("Base.Customers"), new Size (20, 20));
 		}
 
 		private readonly BrowserViewController browser;
+
+		private StaticImage iconOverlay;
+		private LayeredIconButton iconBackground;
 	}
 }
