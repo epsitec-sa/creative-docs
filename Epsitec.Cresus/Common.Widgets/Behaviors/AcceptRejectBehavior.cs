b5bd67972a5d7bc11301aaec36f2bdd50fb4c232
Daniel Roux <roux@epsitec.ch>
2010-06-25 11:41:01 +0000

    - Cresus.Core: UIBuilder.CreateAutoCompleteTextField: Le bouton '+' n'apparaît jamais si le valueCreator n'est pas défini. - Cresus.Core: Nouveau look 'LookBusiness' basé sur LookRoyale. Les sélections sont oranges. Le projet de test avec NUnit ne fonctionne plus; difficile de faire qq chose de beau et général dans ces condtions... - Cresus.Core: AcceptRejectBehavior: Correction d'une petite confusion avec le ButtonStyle. J'ai tranché en faveur de ExListMiddle/ExListRight, et laissé tomber DefaultAccept/DefaultCancel.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13273 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Widgets/Behaviors/AcceptRejectBehavior.cs
+++ Epsitec.Cresus/Common.Widgets/Behaviors/AcceptRejectBehavior.cs
@@ -130,15 +130,15 @@ namespace Epsitec.Common.Widgets.Behaviors
 			
 			this.buttonAccept.Name        = "Accept";
 			this.buttonAccept.GlyphShape  = GlyphShape.Accept;
-			this.buttonAccept.ButtonStyle = ButtonStyle.ExListMiddle;
 			this.buttonAccept.Clicked    += this.HandleButtonAcceptClicked;
-			this.buttonAccept.ButtonStyle = ButtonStyle.DefaultAccept;
+			this.buttonAccept.ButtonStyle = ButtonStyle.ExListMiddle;
+			//?this.buttonAccept.ButtonStyle = ButtonStyle.DefaultAccept;
 			
 			this.buttonReject.Name        = "Reject";
 			this.buttonReject.GlyphShape  = GlyphShape.Reject;
-			this.buttonReject.ButtonStyle = ButtonStyle.ExListRight;
 			this.buttonReject.Clicked    += this.HandleButtonRejectClicked;
-			this.buttonReject.ButtonStyle = ButtonStyle.DefaultCancel;
+			this.buttonReject.ButtonStyle = ButtonStyle.ExListRight;
+			//?this.buttonReject.ButtonStyle = ButtonStyle.DefaultCancel;
 			
 			this.SetVisible (this.isVisible);
 			this.SetAcceptEnabled (this.isAcceptEnabled);
