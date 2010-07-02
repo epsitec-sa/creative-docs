8e781f7649f96ea772de458ce1f376fc9068a7c4
Pierre Arnaud <arnaud@opac.ch>
2010-07-02 08:58:14 +0000

    - Ajouté des énumérations pour CurrencyCode, VatCode et UnitOfMeasureCategory.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@13352 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/BusinessLogic/Finance/VatCode.cs
@@ -0,0 +1,32 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.BusinessLogic.Finance
+{
+	[DesignerVisible]
+	public enum VatCode
+	{
+		None = 0,
+
+		Excluded = 10,											//	EXCLU, exclu de l'impôt LTVA art. 21 (http://www.admin.ch/ch/f/rs/641_20/index.html) et (http://www.epsitec.ch/support/faq/cc-tva-06)
+		ZeroRated = 11,											//	EXONERE et EXPORT, exonéré LTVA art. 23
+		
+
+		StandardInputTaxOnMaterialOrServiceExpenses = 20,		//	IPM
+		ReducedInputTaxOnMaterialOrServiceExpenses = 21,		//	IPMRED
+		SpecialInputTaxOnMaterialOrServiceExpenses = 22,		//	IPMHEB (n'existe pas)
+
+		StandardInputTaxOnInvestementOrOperatingExpenses = 30,	//	IPI
+		ReducedInputTaxOnInvestementOrOperatingExpenses = 31,	//	IPIRED
+		SpecialInputTaxOnInvestementOrOperatingExpenses = 32,	//	IP(I)HEB
+
+		StandardTaxOnTurnover = 40,								//	TVA
+		ReducedTaxOnTurnover = 41,								//	TVARED
+		SpecialTaxOnTurnover = 42,								//	TVAHEB
+	}
+}
---
+++ Epsitec.Cresus/Cresus.Core/BusinessLogic/UnitOfMeasureCategory.cs
@@ -0,0 +1,29 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.BusinessLogic
+{
+	[DesignerVisible]
+	public enum UnitOfMeasureCategory
+	{
+		None      = 0,
+
+		Unrelated = 10,
+		Unit      = 20,
+		
+		Mass      = 30,
+		
+		Length    = 40,
+		Surface   = 41,
+		Volume    = 42,
+		
+		Time      = 50,
+		
+		Energy    = 60,
+	}
+}
