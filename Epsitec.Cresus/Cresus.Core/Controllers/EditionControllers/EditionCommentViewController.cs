a6e15a7d3d013292043f8b8d4944243e6984fd1c
Daniel Roux <roux@epsitec.ch>
2010-07-02 05:47:41 +0000

    - Cresus.Core: EditionCommentViewController: Nouvelle classe. - Cresus.Core: Un document peut avoir des commentaires.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13344 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionCommentViewController.cs
@@ -0,0 +1,59 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using Epsitec.Common.Types;
+using Epsitec.Common.Types.Converters;
+
+using Epsitec.Cresus.Core.Entities;
+using Epsitec.Cresus.Core.Controllers;
+using Epsitec.Cresus.Core.Controllers.DataAccessors;
+using Epsitec.Cresus.Core.Widgets;
+using Epsitec.Cresus.Core.Widgets.Tiles;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Controllers.EditionControllers
+{
+	public class EditionCommentViewController : EditionViewController<Entities.CommentEntity>
+	{
+		public EditionCommentViewController(string name, Entities.CommentEntity entity)
+			: base (name, entity)
+		{
+		}
+
+		protected override void CreateUI(TileContainer container)
+		{
+			using (var builder = new UIBuilder (container, this))
+			{
+				builder.CreateHeaderEditorTile ();
+				builder.CreateEditionTitleTile ("Data.Comment", "Commentaire");
+
+				this.CreateUIMain (builder);
+
+				builder.CreateFooterEditorTile ();
+			}
+		}
+
+
+		private void CreateUIMain(Epsitec.Cresus.Core.UIBuilder builder)
+		{
+			var tile = builder.CreateEditionTile ();
+
+			builder.CreateTextFieldMulti (tile, 120, "Texte", Marshaler.Create (() => this.Entity.Text, x => this.Entity.Text = x));
+		}
+
+
+		protected override EditionStatus GetEditionStatus()
+		{
+			if (string.IsNullOrEmpty (this.Entity.Text))
+			{
+				return EditionStatus.Empty;
+			}
+
+			// TODO: Comment implémenter un vraie validation ? Est-ce que le Marshaler sait faire cela ?
+
+			return EditionStatus.Valid;
+		}
+	}
+}
