43514d19fa8c6a424d403efd70bb4effca4f7c1d
Daniel Roux <roux@epsitec.ch>
2010-07-05 10:04:31 +0000

    - Cresus.Core: Printer: Nouvelle classe qui permettra d'imprimer une entité. - Cresus.Core: Câblage de la commande 'Edition.Imprimer'.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13367 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Printer.cs
@@ -0,0 +1,34 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Drawing;
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Support;
+using Epsitec.Common.Support.EntityEngine;
+using Epsitec.Common.Support.Extensions;
+using Epsitec.Common.Types;
+using Epsitec.Common.Types.Converters;
+
+using Epsitec.Cresus.Core.Controllers;
+using Epsitec.Cresus.Core.Controllers.SummaryControllers;
+using Epsitec.Cresus.Core.Widgets;
+using Epsitec.Cresus.Core.Widgets.Tiles;
+
+using System.Collections.Generic;
+using System.Linq;
+using Epsitec.Cresus.DataLayer;
+using System.Linq.Expressions;
+
+namespace Epsitec.Cresus.Core
+{
+	public sealed class Printer
+	{
+		public Printer()
+		{
+		}
+
+		public void Print(DataContext context)
+		{
+		}
+	}
+}
