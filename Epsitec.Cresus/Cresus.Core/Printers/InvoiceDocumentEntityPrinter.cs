f27a1559b80eb1d84e139e5e6fdb5af944fed1fc
Daniel Roux <roux@epsitec.ch>
2010-07-15 13:05:02 +0000

    - Cresus.Core: InvoiceDocumentEntityPrinter: Tient mieux compte de l'exemple complexe dans CoreDtat.Hack.cs, mais il reste du chemin...
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@13464 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Printers/InvoiceDocumentEntityPrinter.cs
+++ Epsitec.Cresus/Cresus.Core/Printers/InvoiceDocumentEntityPrinter.cs
@@ -55,7 +55,7 @@ namespace Epsitec.Cresus.Core.Printers
 		{
 			this.BuildHeader ();
 			this.BuildArticles ();
-			this.BuildBv ();
+			this.BuildBvs ();
 		}
 
 		public override void PrintCurrentPage(IPaintPort port, Rectangle bounds)
@@ -123,9 +123,21 @@ namespace Epsitec.Cresus.Core.Printers
 			//	Ajoute les articles dans le document.
 			this.documentContainer.CurrentVerticalPosition = 210;
 
+			//	Première passe pour déterminer le nombre le lignes du tableau de la facture.
+			int rowCount = 1;  // déjà 1 pour l'en-tête
+
+			foreach (var line in this.entity.Lines)
+			{
+				if (line.Visibility)
+				{
+					rowCount++;
+				}
+			}
+
+			//	Deuxième passe pour générer les lignes du tableau.
 			var table = new TableBand ();
 			table.ColumnsCount = 5;
-			table.RowsCount = 1 + this.entity.Lines.Count;
+			table.RowsCount = rowCount;
 
 			table.SetRelativeColumWidth (0, 15.0);
 			table.SetRelativeColumWidth (1, 12.0);
@@ -149,21 +161,22 @@ namespace Epsitec.Cresus.Core.Printers
 
 			foreach (var line in this.entity.Lines)
 			{
-				var x = line as ArticleDocumentItemEntity;
-				if (x != null)
+				if (line.Visibility)
 				{
-					decimal quantity = this.ArticleQuantity (x);
-					decimal price    = this.ArticlePrice (x);
+					if (line is TextDocumentItemEntity)
+					{
+						this.BuildLineText (table, row, line as TextDocumentItemEntity);
+					}
 
-					table.SetText (0, row, quantity.ToString ());
-					table.SetText (1, row, this.ArticleUnit (x));
-					table.SetText (2, row, x.ArticleDefinition.LongDescription);
-					table.SetText (3, row, price.ToString ());
-					table.SetText (4, row, (quantity*price).ToString ());
+					if (line is ArticleDocumentItemEntity)
+					{
+						this.BuildLineArticle (table, row, line as ArticleDocumentItemEntity);
+					}
 
-					table.SetAlignment (0, row, ContentAlignment.MiddleRight);
-					table.SetAlignment (3, row, ContentAlignment.MiddleRight);
-					table.SetAlignment (4, row, ContentAlignment.MiddleRight);
+					if (line is PriceDocumentItemEntity)
+					{
+						this.BuildLinePrice (table, row, line as PriceDocumentItemEntity);
+					}
 
 					row++;
 				}
@@ -172,7 +185,50 @@ namespace Epsitec.Cresus.Core.Printers
 			this.documentContainer.AddFromTop (table, 5.0);
 		}
 
-		private void BuildBv()
+		private void BuildLineText(TableBand table, int row, TextDocumentItemEntity line)
+		{
+			table.SetText (2, row, line.Text);
+		}
+
+		private void BuildLineArticle(TableBand table, int row, ArticleDocumentItemEntity line)
+		{
+			decimal quantity = this.ArticleQuantity (line);
+			decimal price    = this.ArticlePrice (line);
+
+			if (quantity == 0.0M)  // les frais de port ont une quantité nulle !
+			{
+				quantity = 1.0M;
+			}
+
+			string description = line.ArticleDefinition.LongDescription;
+			if (string.IsNullOrEmpty (description))
+			{
+				description = line.ArticleDefinition.ShortDescription;
+			}
+
+			table.SetText (0, row, quantity.ToString ());
+			table.SetText (1, row, this.ArticleUnit (line));
+			table.SetText (2, row, description);
+			table.SetText (3, row, price.ToString ());
+			table.SetText (4, row, (quantity*price).ToString ());
+
+			table.SetAlignment (0, row, ContentAlignment.MiddleRight);
+			table.SetAlignment (3, row, ContentAlignment.MiddleRight);
+			table.SetAlignment (4, row, ContentAlignment.MiddleRight);
+		}
+
+		private void BuildLinePrice(TableBand table, int row, PriceDocumentItemEntity line)
+		{
+			table.SetText (2, row, line.TextForPrimaryPrice);
+			table.SetText (3, row, line.PrimaryPriceBeforeTax.ToString ());
+			table.SetText (4, row, line.ResultingPriceBeforeTax.ToString ());
+
+			table.SetAlignment (3, row, ContentAlignment.MiddleRight);
+			table.SetAlignment (4, row, ContentAlignment.MiddleRight);
+		}
+
+
+		private void BuildBvs()
 		{
 			//	Met un BV en bas de chaque page.
 			var bounds = new Rectangle (Point.Zero, BvBand.DefautlSize);
