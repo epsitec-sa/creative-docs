60d2cf9b307cdd83e0ea6a92d4ebfb2694939394
Daniel Roux <roux@epsitec.ch>
2010-07-17 09:00:29 +0000

    - Cresus.Core: InvoiceDocumentEntityPrinter: Numérote les pages et met un rappel du titre de la facture. - Cresus.Core: Déplacement de plusieurs méthodes communes dans SummaryInvoiceDocumentViewController. - Cresus.Core: SummaryInvoiceDocumentViewController affiche un résumé de tous les articles facturés. - Cresus.Core: EditionInvoiceDocumentViewController: Ebauche d'un éditeur de facture. - Cresus.Core: Renommé l'icône Base.Invoice en Base.InvoiceDocuments, pour résoudre un problème avec le widget LayeredIconButton.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13491 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Controllers/SummaryControllers/SummaryInvoiceDocumentViewController.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/SummaryControllers/SummaryInvoiceDocumentViewController.cs
@@ -54,22 +54,41 @@ namespace Epsitec.Cresus.Core.Controllers.SummaryControllers
 					IconUri				= "Data.Document",
 					Title				= UIBuilder.FormatText ("Facture"),
 					CompactTitle		= UIBuilder.FormatText ("Facture"),
-					TextAccessor		= Accessor.Create (this.EntityGetter, x => this.GetText (x)),
+					TextAccessor		= Accessor.Create (this.EntityGetter, x => SummaryInvoiceDocumentViewController.GetText (x)),
 					CompactTextAccessor = Accessor.Create (this.EntityGetter, x => UIBuilder.FormatText ("N°", x.IdA)),
 					EntityMarshaler		= this.EntityMarshaler,
 				});
 		}
 
 
-		private FormattedText GetText(Entities.InvoiceDocumentEntity x)
+		private static FormattedText GetText(InvoiceDocumentEntity x)
 		{
-			string date = this.GetDate (x);
-			string total = Misc.DecimalToString (this.GetTotal (x));
+			string date = SummaryInvoiceDocumentViewController.GetDate (x);
+			string total = Misc.DecimalToString (SummaryInvoiceDocumentViewController.GetTotal (x));
 
-			return UIBuilder.FormatText ("N°", x.IdA, "/~", x.IdB, "/~", x.IdC, "\n", "Date: ", date, "\n", "Total: ", total);
+			var builder = new System.Text.StringBuilder ();
+
+			foreach (var line in x.Lines)
+			{
+				if (line.Visibility)
+				{
+					if (line is ArticleDocumentItemEntity)
+					{
+						var quantity = SummaryInvoiceDocumentViewController.GetArticleQuantity (line as ArticleDocumentItemEntity);
+						var desc     = SummaryInvoiceDocumentViewController.GetArticleDescription (line as ArticleDocumentItemEntity, shortDescription: true);
+
+						builder.Append ("● ");
+						builder.Append (string.Join (" ", quantity, desc));
+						builder.Append ("<br/>");
+					}
+				}
+			}
+	
+			return UIBuilder.FormatText ("N°", x.IdA, "/~", x.IdB, "/~", x.IdC, ", ", date, "\n", builder.ToString (), "Total: ", total);
 		}
 
-		private string GetDate(Entities.InvoiceDocumentEntity x)
+
+		public static string GetDate(InvoiceDocumentEntity x)
 		{
 			System.DateTime date;
 			if (x.LastModificationDate.HasValue)
@@ -84,7 +103,87 @@ namespace Epsitec.Cresus.Core.Controllers.SummaryControllers
 			return Misc.GetDateTimeDescription (date);
 		}
 
-		private decimal GetTotal(Entities.InvoiceDocumentEntity x)
+		public static string GetMailContact(InvoiceDocumentEntity x)
+		{
+			string legal = "";
+			string natural = "";
+
+			if (x.BillingMailContact != null)
+			{
+				if (x.BillingMailContact.LegalPerson.IsActive ())
+				{
+					var y = x.BillingMailContact.LegalPerson;
+					legal = UIBuilder.FormatText (y.Name).ToString ();
+				}
+
+				if (x.BillingMailContact.NaturalPerson.IsActive ())
+				{
+					var y = x.BillingMailContact.NaturalPerson;
+					natural = UIBuilder.FormatText (y.Title.Name, "~\n", y.Firstname, y.Lastname).ToString ();
+				}
+
+				return UIBuilder.FormatText (legal, "~\n", natural, "~\n", x.BillingMailContact.Address.Street.StreetName, "\n", x.BillingMailContact.Address.Location.PostalCode, x.BillingMailContact.Address.Location.Name).ToString ();
+			}
+
+			return null;
+		}
+
+		public static decimal GetArticlePrice(ArticleDocumentItemEntity article)
+		{
+			if (article.ArticleDefinition.ArticlePrices.Count != 0)
+			{
+				return article.ArticleDefinition.ArticlePrices[0].ValueBeforeTax;
+			}
+
+			return 0;
+		}
+
+		public static string GetArticleQuantity(ArticleDocumentItemEntity article)
+		{
+			foreach (var quantity in article.ArticleQuantities)
+			{
+				if (quantity.Code != "suivra")
+				{
+					return Misc.FormatUnit (quantity.Quantity, quantity.Unit.Code);
+				}
+			}
+
+			return null;
+		}
+
+		public static string GetArticleDescription(ArticleDocumentItemEntity article, bool shortDescription)
+		{
+			string description = shortDescription ? null : article.ArticleDefinition.LongDescription;
+
+			if (string.IsNullOrEmpty (description))
+			{
+				description = article.ArticleDefinition.ShortDescription;  // description courte s'il n'existe pas de longue
+			}
+
+			return description;
+		}
+
+		public static string GetConcerne(InvoiceDocumentEntity x)
+		{
+			if (x.BillingDetails.Count > 0)
+			{
+				return x.BillingDetails[0].Title;
+			}
+
+			return null;
+		}
+
+		public static string GetConditions(InvoiceDocumentEntity x)
+		{
+			if (x.BillingDetails.Count > 0)
+			{
+				return x.BillingDetails[0].AmountDue.PaymentMode.Description;
+			}
+
+			return null;
+		}
+
+		public static decimal GetTotal(InvoiceDocumentEntity x)
 		{
 			if (x.BillingDetails.Count > 0)
 			{
@@ -94,5 +193,11 @@ namespace Epsitec.Cresus.Core.Controllers.SummaryControllers
 			return 0;
 		}
 
+		public static string GetTitle(InvoiceDocumentEntity x)
+		{
+			return UIBuilder.FormatText ("<b>Facture", x.IdA, "/~", x.IdB, "/~", x.IdC, "</b>").ToString ();
+		}
+	
+	
 	}
 }
