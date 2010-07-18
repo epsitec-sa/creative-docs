3ae0841144fbc5719995b0acd416cce304536938
Daniel Roux <roux@epsitec.ch>
2010-07-18 07:22:52 +0000

    - Cresus.Core: EditionInvoiceDocumentViewController: On peut éditer un ou plusieurs commentaires. - Cresus.Core: EditionArticleDocumentItemViewController: Début des travaux pour éditer les quantités, unités et prix. - Cresus.Core: Marshaler: Création de la classe DecimalConverter. Il reste à gérer les exceptions.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13493 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/Converters/DecimalConverter.cs
@@ -0,0 +1,44 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Common.Types.Converters
+{
+	/// <summary>
+	/// The <c>DecimalConverter</c> class does a transparent conversion;
+	/// it is required so that the <see cref="Marshaler"/> can work with
+	/// <c>decimal</c> values.
+	/// </summary>
+	public class DecimalConverter : GenericConverter<decimal>
+	{
+		public override string ConvertToString(decimal value)
+		{
+			return value.ToString ();
+		}
+
+		public override ConversionResult<decimal> ConvertFromString(string text)
+		{
+			if (text == null)
+            {
+				return new ConversionResult<decimal>
+				{
+					IsNull = true,
+				};
+            }
+			else
+			{
+				return new ConversionResult<decimal>
+				{
+					Value = decimal.Parse (text),  // TODO: si exception ?
+				};
+			}
+		}
+
+		public override bool CanConvertFromString(string text)
+		{
+			return true;  // TODO ...
+		}
+	}
+}
