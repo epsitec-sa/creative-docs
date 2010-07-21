43d991973ea321ac8c5c6878f8d5c55ab0e81071
Marc Bettex <bettex@epsitec.ch>
2010-07-21 14:14:41 +0000

    - Ajouté une classe avec des méthodes d'extensions pour le contrôle des arguments. - Ajouté un projet de test pour Epsitec.Common.Support. - Ajouté un test unitairep pour la nouvelle classe.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010-database-work@13543 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Support.UnitTests/Extensions/UnitTestExceptionThrower.cs
@@ -0,0 +1,72 @@
+﻿using Epsitec.Common.Support.Extensions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Common.Support.UnitTests
+{
+
+
+	[TestClass]
+	public class UnitTestExceptionThrower
+	{
+				
+
+		[TestMethod]
+		public void ThrowIfNullTest1()
+		{
+			object element = new object ();
+
+			element.ThrowIfNull ("element");
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentNullException))]
+		public void ThrowIfNullTest2()
+		{
+			object element = null;
+
+			element.ThrowIfNull ("element");
+		}
+		
+
+		[TestMethod]
+		public void ThrowIfNullOrEmptyTest1()
+		{
+			string string1 = "coucou";
+			string string2 = " ";
+			string string3 = "\t";
+			string string4 = "\n";
+			
+			string1.ThrowIfNullOrEmpty ("string1");
+			string2.ThrowIfNullOrEmpty ("string2");
+			string3.ThrowIfNullOrEmpty ("string3");
+			string4.ThrowIfNullOrEmpty ("string4");
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentException))]
+		public void ThrowIfNullOrEmptyTest2()
+		{
+			string element = null;
+
+			element.ThrowIfNullOrEmpty ("element");
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentException))]
+		public void ThrowIfNullOrEmptyTest3()
+		{
+			string element = "";
+
+			element.ThrowIfNullOrEmpty ("element");
+		}
+
+
+	}
+
+
+}
---
+++ Epsitec.Cresus/Common.Support/Extensions/ExceptionThrower.cs
@@ -0,0 +1,31 @@
+﻿namespace Epsitec.Common.Support.Extensions
+{
+
+
+	public static class ExceptionThrower
+	{
+
+
+		public static void ThrowIfNull<T>(this T element, string elementName) where T : class
+		{
+			if (element == null)
+			{
+				throw new System.ArgumentNullException (elementName);
+			}
+		}
+
+
+		public static void ThrowIfNullOrEmpty(this string element, string elementName)
+		{
+			if (string.IsNullOrEmpty (element))
+			{
+				throw new System.ArgumentException (elementName);
+			}
+		}
+
+
+	}
+
+
+
+}
