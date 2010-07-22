3e7b5ea068dbcedb71dde96d3a814e30398985ff
Marc Bettex <bettex@epsitec.ch>
2010-07-22 09:47:05 +0000

    - Commenté BinaryComparisonFieldWithField. - Commenté BinaryComparisonFieldWithValue.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010-database-work@13555 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.DataLayer/Expressions/BinaryComparisonFieldWithField.cs
+++ Epsitec.Cresus/Cresus.DataLayer/Expressions/BinaryComparisonFieldWithField.cs
@@ -1,19 +1,39 @@
-﻿namespace Epsitec.Cresus.DataLayer.Expressions
+﻿using Epsitec.Common.Support.Extensions;
+
+namespace Epsitec.Cresus.DataLayer.Expressions
 {
 
 
+	/// <summary>
+	/// The <c>BinaryComparisonFieldWithField</c> class represents a comparison between two
+	/// <see cref="Field"/>, such as (a = b).
+	/// </summary>
 	public class BinaryComparisonFieldWithField : Comparison
 	{
 
 
+		/// <summary>
+		/// Builds a new <c>BinaryComparisonFieldWithField</c>.
+		/// </summary>
+		/// <param name="left">The <see cref="Field"/> on the left of the <see cref="BinaryComparator"/>.</param>
+		/// <param name="op">The <see cref="BinaryComparator"/> used by the <c>BinaryComparisonFieldWithField</c>.</param>
+		/// <param name="left">The <see cref="Field"/> on the left of the <see cref="BinaryComparator"/>.</param>
+		/// <exception cref="System.ArgumentNullException">If <paramref name="left"/> is null.</exception>
+		/// <exception cref="System.ArgumentNullException">If <paramref name="right"/> is null.</exception>
 		public BinaryComparisonFieldWithField(Field left, BinaryComparator op, Field right) : base ()
 		{
+			left.ThrowIfNull ("left");
+			right.ThrowIfNull ("right");
+			
 			this.Left = left;
 			this.Operator = op;
 			this.Right = right;
 		}
 
 
+		/// <summary>
+		/// The left side of the <c>BinaryComparisonFieldWithField</c>.
+		/// </summary>
 		public Field Left
 		{
 			get;
@@ -21,6 +41,9 @@
 		}
 
 
+		/// <summary>
+		/// The <see cref="BinaryComparator"/> of the <c>BinaryComparisonFieldWithField</c>.
+		/// </summary>
 		public BinaryComparator Operator
 		{
 			get;
@@ -28,6 +51,9 @@
 		}
 
 
+		/// <summary>
+		/// The right side of the <c>BinaryComparisonFieldWithField</c>.
+		/// </summary>
 		public Field Right
 		{
 			get;
--- Epsitec.Cresus/Cresus.DataLayer/Expressions/BinaryComparisonFieldWithValue.cs
+++ Epsitec.Cresus/Cresus.DataLayer/Expressions/BinaryComparisonFieldWithValue.cs
@@ -1,4 +1,6 @@
-﻿using Epsitec.Cresus.Database;
+﻿using Epsitec.Common.Support.Extensions;
+
+using Epsitec.Cresus.Database;
 
 using System.Text.RegularExpressions;
 
@@ -7,18 +9,36 @@ namespace Epsitec.Cresus.DataLayer.Expressions
 {
 
 
+	/// <summary>
+	/// The <c>BinaryComparisonFieldWithValue</c> class represents a comparison between a
+	/// <see cref="Field"/> and a <see cref="Constant"/>, such as (a = 3).
+	/// </summary>
 	public class BinaryComparisonFieldWithValue : Comparison
 	{
 
 
+		/// <summary>
+		/// Builds a new <c>BinaryComparisonFieldWithValue</c>.
+		/// </summary>
+		/// <param name="left">The <see cref="Field"/> on the left of the <see cref="BinaryComparator"/>.</param>
+		/// <param name="op">The <see cref="BinaryComparator"/> used by the <c>BinaryComparisonFieldWithValue</c>.</param>
+		/// <param name="left">The <see cref="Constant"/> on the left of the <see cref="BinaryComparator"/>.</param>
+		/// <exception cref="System.ArgumentNullException">If <paramref name="left"/> is null.</exception>
+		/// <exception cref="System.ArgumentNullException">If <paramref name="right"/> is null.</exception>
 		public BinaryComparisonFieldWithValue(Field left, BinaryComparator op, Constant right) : base ()
 		{
+			left.ThrowIfNull ("left");
+			right.ThrowIfNull ("right");
+
 			this.Left = left;
 			this.Operator = op;
 			this.Right = right;
 		}
 
 
+		/// <summary>
+		/// The left side of the <c>BinaryComparisonFieldWithValue</c>.
+		/// </summary>
 		public Field Left
 		{
 			get;
@@ -26,6 +46,9 @@ namespace Epsitec.Cresus.DataLayer.Expressions
 		}
 
 
+		/// <summary>
+		/// The <see cref="BinaryComparator"/> of the <c>BinaryComparisonFieldWithValue</c>.
+		/// </summary>
 		public BinaryComparator Operator
 		{
 			get;
@@ -33,6 +56,9 @@ namespace Epsitec.Cresus.DataLayer.Expressions
 		}
 
 
+		/// <summary>
+		/// The right side of the <c>BinaryComparisonFieldWithValue</c>.
+		/// </summary>
 		public Constant Right
 		{
 			get;
@@ -40,6 +66,12 @@ namespace Epsitec.Cresus.DataLayer.Expressions
 		}
 
 
+		/// <summary>
+		/// Escapes a string so that it can be used with the <see cref="BinaryComparator"/> dealing
+		/// with escaped strings.
+		/// </summary>
+		/// <param name="value">The string to escape.</param>
+		/// <returns>The escaped string.</returns>
 		public static string Escape(string value)
 		{
 			string escapeChar = DbSqlStandard.CompareLikeEscape;
