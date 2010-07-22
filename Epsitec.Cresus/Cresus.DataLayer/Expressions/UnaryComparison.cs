b6964c8efbe8158e8376c212bcab3ff80a1ce4f4
Marc Bettex <bettex@epsitec.ch>
2010-07-22 09:27:52 +0000

    - Commenté UnaryComparison. - Ajouté un test unitaire pour UnaryComparison.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010-database-work@13553 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.DataLayer/Expressions/UnaryComparison.cs
+++ Epsitec.Cresus/Cresus.DataLayer/Expressions/UnaryComparison.cs
@@ -1,23 +1,35 @@
-﻿namespace Epsitec.Cresus.DataLayer.Expressions
+﻿using Epsitec.Common.Support.Extensions;
+
+namespace Epsitec.Cresus.DataLayer.Expressions
 {
 	
 
+	/// <summary>
+	/// The <c>UnaryComparison</c> class represents a predicate on a single <see cref="Field"/> such
+	/// as (a is null).
+	/// </summary>
 	public class UnaryComparison : Comparison
 	{
 
 
-		public UnaryComparison(Field field, UnaryComparator op) : this (op, field)
+		/// <summary>
+		/// Builds a new <c>UnaryComparison</c>.
+		/// </summary>
+		/// <param name="field">The field on which to apply the <see cref="UnaryComparator"/>.</param>
+		/// <param name="op">The predicate to apply on the <see cref="Field"/>.</param>
+		/// <exception cref="Sytem.ArgumentNullException">If <paramref name="field"/> is null.</exception>
+		public UnaryComparison(Field field, UnaryComparator op) : base()
 		{
-		}
-
-
-		public UnaryComparison(UnaryComparator op, Field field) : base ()
-		{
+			field.ThrowIfNull ("field");
+			
 			this.Operator = op;
 			this.Field = field;
 		}
 
 
+		/// <summary>
+		/// The <see cref="UnaryOperator"/> of the current instance.
+		/// </summary>
 		public UnaryComparator Operator
 		{
 			get;
@@ -25,6 +37,9 @@
 		}
 
 
+		/// <summary>
+		/// The <see cref="Field"/> of the current instance.
+		/// </summary>
 		public Field Field
 		{
 			get;
