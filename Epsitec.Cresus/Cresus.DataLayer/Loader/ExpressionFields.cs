93abdabd2590ccf15786e55399c9397416c0eaaf
Marc Bettex <bettex@epsitec.ch>
2010-07-23 12:59:58 +0000

    - Commenté et modifié un petit peu ExpressionFields. - Ajouté un test unitaire pour ExpressionFields.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010-database-work@13563 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.DataLayer/Loader/ExpressionFields.cs
+++ Epsitec.Cresus/Cresus.DataLayer/Loader/ExpressionFields.cs
@@ -1,4 +1,5 @@
 ﻿using Epsitec.Common.Support;
+using Epsitec.Common.Support.Extensions;
 
 using Epsitec.Cresus.DataLayer.Expressions;
 
@@ -11,12 +12,24 @@ namespace Epsitec.Cresus.DataLayer.Loader
 {
 
 
+	/// <summary>
+	/// The <c>ExpressionFields</c> class provides extension methods that can retrieve the set of
+	/// <see cref="Druids"/> of the <see cref="Fields"/> present in an <see cref="Expression"/>.
+	/// </summary>
 	internal static class ExpressionFields
 	{
 
 
+		/// <summary>
+		/// Gets the set of <see cref="Druid"/> of the <see cref="Field"/> present in an
+		/// <see cref="Expression"/>.
+		/// </summary>
+		/// <param name="expression">The <see cref="Expression"/> whose <see cref="Druid"/> to get.</param>
+		/// <returns>The set of <see cref="Druid"/>.</returns>
+		/// <exception cref="System.ArgumentException">If <paramref name="expression"/> is null.</exception>
 		public static IEnumerable<Druid> GetFields(this Expression expression)
 		{
+			expression.ThrowIfNull ("expression");
 
 			if (expression is Operation)
 			{
@@ -33,8 +46,17 @@ namespace Epsitec.Cresus.DataLayer.Loader
 		}
 
 
-		private static IEnumerable<Druid> GetFields(this Operation operation)
+		/// <summary>
+		/// Gets the set of <see cref="Druid"/> of the <see cref="Field"/> present in an
+		/// <see cref="Operation"/>.
+		/// </summary>
+		/// <param name="operation">The <see cref="Operation"/> whose <see cref="Druid"/> to get.</param>
+		/// <returns>The set of <see cref="Druid"/>.</returns>
+		/// <exception cref="System.ArgumentException">If <paramref name="operation"/> is null.</exception>
+		public static IEnumerable<Druid> GetFields(this Operation operation)
 		{
+			operation.ThrowIfNull ("operation");
+			
 			if (operation is UnaryOperation)
 			{
 				return (operation as UnaryOperation).GetFields ();
@@ -50,8 +72,17 @@ namespace Epsitec.Cresus.DataLayer.Loader
 		}
 
 
-		private static IEnumerable<Druid> GetFields(this Comparison comparison)
+		/// <summary>
+		/// Gets the set of <see cref="Druid"/> of the <see cref="Field"/> present in an
+		/// <see cref="Comparison"/>.
+		/// </summary>
+		/// <param name="comparison">The <see cref="Comparison"/> whose <see cref="Druid"/> to get.</param>
+		/// <returns>The set of <see cref="Druid"/>.</returns>
+		/// <exception cref="System.ArgumentException">If <paramref name="comparison"/> is null.</exception>
+		public static IEnumerable<Druid> GetFields(this Comparison comparison)
 		{
+			comparison.ThrowIfNull ("comparison");
+
 			if (comparison is UnaryComparison)
 			{
 				return (comparison as UnaryComparison).GetFields ();
@@ -71,35 +102,138 @@ namespace Epsitec.Cresus.DataLayer.Loader
 		}
 
 
-		private static IEnumerable<Druid> GetFields(this UnaryOperation operation)
+		/// <summary>
+		/// Gets the set of <see cref="Druid"/> of the <see cref="Field"/> present in an
+		/// <see cref="UnaryOperation"/>.
+		/// </summary>
+		/// <param name="operation">The <see cref="UnaryOperation"/> whose <see cref="Druid"/> to get.</param>
+		/// <returns>The set of <see cref="Druid"/>.</returns>
+		/// <exception cref="System.ArgumentException">If <paramref name="operation"/> is null.</exception>
+		public static IEnumerable<Druid> GetFields(this UnaryOperation operation)
 		{
+			operation.ThrowIfNull ("operation");
+
 			return operation.Expression.GetFields ();
 		}
 
 
-		private static IEnumerable<Druid> GetFields(this BinaryOperation operation)
+		/// <summary>
+		/// Gets the set of <see cref="Druid"/> of the <see cref="Field"/> present in an
+		/// <see cref="BinaryOperation"/>.
+		/// </summary>
+		/// <param name="operation">The <see cref="BinaryOperation"/> whose <see cref="Druid"/> to get.</param>
+		/// <returns>The set of <see cref="Druid"/>.</returns>
+		/// <exception cref="System.ArgumentException">If <paramref name="operation"/> is null.</exception>
+		public static IEnumerable<Druid> GetFields(this BinaryOperation operation)
 		{
+			operation.ThrowIfNull ("operation");
+
 			IEnumerable<Druid> left = operation.Left.GetFields ();
 			IEnumerable<Druid> right = operation.Right.GetFields ();
 
-			return left.Concat (right);
+			return left.Concat (right).Distinct ();
 		}
 
 
-		private static IEnumerable<Druid> GetFields(this UnaryComparison comparison)
+		/// <summary>
+		/// Gets the set of <see cref="Druid"/> of the <see cref="Field"/> present in an
+		/// <see cref="UnaryComparison"/>.
+		/// </summary>
+		/// <param name="comparison">The <see cref="UnaryComparison"/> whose <see cref="Druid"/> to get.</param>
+		/// <returns>The set of <see cref="Druid"/>.</returns>
+		/// <exception cref="System.ArgumentException">If <paramref name="comparison"/> is null.</exception>
+		public static IEnumerable<Druid> GetFields(this UnaryComparison comparison)
+		{
+			// This method does not do the job itself but call an helper method so the arguments are
+			// checked immediately and the execution of the helper is deferred.
+			// Marc.
+
+			comparison.ThrowIfNull ("comparison");
+
+			return ExpressionFields.GetFieldsHelper (comparison);
+		}
+
+
+		/// <summary>
+		/// Gets the set of <see cref="Druid"/> of the <see cref="Field"/> present in an
+		/// <see cref="UnaryComparison"/>.
+		/// </summary>
+		/// <param name="comparison">The <see cref="UnaryComparison"/> whose <see cref="Druid"/> to get.</param>
+		/// <returns>The set of <see cref="Druid"/>.</returns>
+		private static IEnumerable<Druid> GetFieldsHelper(UnaryComparison comparison)
 		{
 			yield return comparison.Field.FieldId;
 		}
 
 
-		private static IEnumerable<Druid> GetFields(this ComparisonFieldField comparison)
+		/// <summary>
+		/// Gets the set of <see cref="Druid"/> of the <see cref="Field"/> present in an
+		/// <see cref="ComparisonFieldField"/>.
+		/// </summary>
+		/// <param name="comparison">The <see cref="ComparisonFieldField"/> whose <see cref="Druid"/> to get.</param>
+		/// <returns>The set of <see cref="Druid"/>.</returns>
+		/// <exception cref="System.ArgumentException">If <paramref name="comparison"/> is null.</exception>
+		public static IEnumerable<Druid> GetFields(this ComparisonFieldField comparison)
 		{
-			yield return comparison.Left.FieldId;
-			yield return comparison.Right.FieldId;
+			// This method does not do the job itself but call an helper method so the arguments are
+			// checked immediately and the execution of the helper is deferred.
+			// Marc.
+
+			comparison.ThrowIfNull ("comparison");
+
+			return ExpressionFields.GetFieldsHelper (comparison);
 		}
 
 
-		private static IEnumerable<Druid> GetFields(this ComparisonFieldValue comparison)
+		/// <summary>
+		/// Gets the set of <see cref="Druid"/> of the <see cref="Field"/> present in an
+		/// <see cref="ComparisonFieldField"/>.
+		/// </summary>
+		/// <param name="comparison">The <see cref="ComparisonFieldField"/> whose <see cref="Druid"/> to get.</param>
+		/// <returns>The set of <see cref="Druid"/>.</returns>
+		private static IEnumerable<Druid> GetFieldsHelper(ComparisonFieldField comparison)
+		{
+			Druid id1 = comparison.Left.FieldId;
+			Druid id2 = comparison.Right.FieldId;
+
+			if (id1 == id2)
+			{
+				yield return id1;
+			}
+			else
+			{
+				yield return id1;
+				yield return id2;
+			}
+		}
+
+
+		/// <summary>
+		/// Gets the set of <see cref="Druid"/> of the <see cref="Field"/> present in an
+		/// <see cref="ComparisonFieldValue"/>.
+		/// </summary>
+		/// <param name="comparison">The <see cref="ComparisonFieldValue"/> whose <see cref="Druid"/> to get.</param>
+		/// <returns>The set of <see cref="Druid"/>.</returns>
+		/// <exception cref="System.ArgumentException">If <paramref name="comparison"/> is null.</exception>
+		public static IEnumerable<Druid> GetFields(this ComparisonFieldValue comparison)
+		{
+			// This method does not do the job itself but call an helper method so the arguments are
+			// checked immediately and the execution of the helper is deferred.
+			// Marc.
+
+			comparison.ThrowIfNull ("comparison");
+
+			return ExpressionFields.GetFieldsHelper (comparison);
+		}
+
+
+		/// <summary>
+		/// Gets the set of <see cref="Druid"/> of the <see cref="Field"/> present in an
+		/// <see cref="ComparisonFieldValue"/>.
+		/// </summary>
+		/// <param name="comparison">The <see cref="ComparisonFieldValue"/> whose <see cref="Druid"/> to get.</param>
+		/// <returns>The set of <see cref="Druid"/>.</returns>
+		private static IEnumerable<Druid> GetFieldsHelper(ComparisonFieldValue comparison)
 		{
 			yield return comparison.Left.FieldId;
 		}
