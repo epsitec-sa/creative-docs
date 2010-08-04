52253174cd7d52a1068a7f3ba3641273b6223153
Marc Bettex <bettex@epsitec.ch>
2010-08-04 08:47:29 +0000

    - Ajouté 3 classes qui vont servir à la description des événements lancés par le DataContext lié aux entités.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13660 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.DataLayer/Context/EntityEventArgs.cs
@@ -0,0 +1,69 @@
+﻿using Epsitec.Common.Support;
+using Epsitec.Common.Support.EntityEngine;
+using Epsitec.Common.Support.Extensions;
+
+
+namespace Epsitec.Cresus.DataLayer.Context
+{
+
+
+	/// <summary>
+	/// The <c>EntityEventArgs</c> is a container for the data describing an event related to an
+	/// <see cref="AbstractEntity"/>.
+	/// </summary>
+	public class EntityEventArgs : EventArgs
+	{
+
+
+		/// <summary>
+		/// Creates a new <c>EntityEventArgs.</c>
+		/// </summary>
+		/// <param name="entity">The <see cref="AbstractEntity"/> concerned by the event.</param>
+		/// <param name="eventType">The type of the event.</param>
+		/// <param name="eventSource">The source of the event.</param>
+		/// <exception cref="System.ArgumentNullException">If <paramref name="entity"/> is <c>null</c>.</exception>
+		public EntityEventArgs(AbstractEntity entity, EntityEventType eventType, EntityEventSource eventSource)
+			: base ()
+		{
+			entity.ThrowIfNull ("entity");
+
+			this.Entity = entity;
+			this.EventType = eventType;
+			this.EventSource = eventSource;
+		}
+
+
+		/// <summary>
+		/// The <see cref="AbstractEntity"/> concerned by the event.
+		/// </summary>
+		public AbstractEntity Entity
+		{
+			get;
+			private set;
+		}
+
+
+		/// <summary>
+		/// The type of the event.
+		/// </summary>
+		public EntityEventType EventType
+		{
+			get;
+			set;
+		}
+
+
+		/// <summary>
+		/// The source of the event.
+		/// </summary>
+		public EntityEventSource EventSource
+		{
+			get;
+			private set;
+		}
+
+
+	}
+
+
+}
