af87fb045b812f1329428907fd6a14e6c6e6708c
Daniel Roux <roux@epsitec.ch>
2010-08-04 07:59:40 +0000

    - Cresus.Core: Si la description BillingDetail.InstalmentName existe, c'est elle qui figure sur la facture. Sinon, c'est un texte standard calculé ("1/3" par exemple).
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13655 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionInvoiceDocumentViewController.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionInvoiceDocumentViewController.cs
@@ -296,7 +296,7 @@ namespace Epsitec.Cresus.Core.Controllers.EditionControllers
 		{
 			string amount = Misc.PriceToString (billingDetails.AmountDue.Amount);
 			string title = Misc.FirstLine (billingDetails.Title);
-			string ratio = InvoiceDocumentHelper.GetInstalmentRatio (invoiceDocument, billingDetails, true, false);
+			string ratio = InvoiceDocumentHelper.GetInstalmentName (invoiceDocument, billingDetails, true, false);
 
 			if (ratio == null)
 			{
