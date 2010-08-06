dad702797eae4e6e4ed857647e70cc7d93c3c1e9
Marc Bettex <bettex@epsitec.ch>
2010-08-06 14:20:44 +0000

    - Commenté CollectionSynchronizationJob. - Ajouté un test unitaire pour CollectionSynchronizationJob.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@13721 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.DataLayer/Saver/SynchronizationJobs/CollectionSynchronizationJob.cs
+++ Epsitec.Cresus/Cresus.DataLayer/Saver/SynchronizationJobs/CollectionSynchronizationJob.cs
@@ -1,4 +1,6 @@
 ﻿using Epsitec.Common.Support;
+using Epsitec.Common.Support.EntityEngine;
+using Epsitec.Common.Support.Extensions;
 
 using Epsitec.Cresus.DataLayer.Context;
 
@@ -11,17 +13,37 @@ namespace Epsitec.Cresus.DataLayer.Saver.SynchronizationJobs
 {
 
 
+	/// <summary>
+	/// The <c>CollectionSynchronizationJob</c> class describes the modifications that have been made
+	/// to a collection field of an <see cref="AbstractEntity"/>.
+	/// </summary>
 	internal class CollectionSynchronizationJob : AbstractFieldSynchronizationJob
 	{
 
 
+		/// <summary>
+		/// Creates a new <c>ReferenceSynchronizationJob</c>.
+		/// </summary>
+		/// <param name="dataContextId">The unique id of the <see cref="DataContext"/> that is creating the <c>ReferenceSynchronizationJob</c>.</param>
+		/// <param name="sourceKey">The <see cref="EntityKey"/> that identifies the <see cref="AbstractEntity"/> targeted by the <c>ReferenceSynchronizationJob</c>.</param>
+		/// <param name="fieldId">The <see cref="Druid"/> of the field targeted by the <c>ReferenceSynchronizationJob</c>.</param>
+		/// <param name="newTargetKeys">The <see cref="EntityKey"/> of the new targets of the field targeted by the <c>ValueSynchronizationJob</c>.</param>
+		/// <exception cref="System.ArgumentException">If <paramref name="EntityKey"/> is empty.</exception>
+		/// <exception cref="System.ArgumentException">If <paramref name="fieldId"/> is empty.</exception>
+		/// <exception cref="System.ArgumentException">If <paramref name="newTargetKeys"/> is <c>null</c>.</exception>
 		public CollectionSynchronizationJob(int dataContextId, EntityKey sourceKey, Druid fieldId, IEnumerable<EntityKey> newTargetKeys)
 			: base (dataContextId, sourceKey, fieldId)
 		{
+			newTargetKeys.ThrowIfNull ("newTargetKeys");
+
 			this.NewTargetKeys = newTargetKeys.ToList ();
 		}
 
 
+		/// <summary>
+		/// The <see cref="EntityKey"/> of the <see cref="AbstractEntity"/> targeted by the field of
+		/// the <see cref="AbstractEntity"/> targeted by this instance.
+		/// </summary>
 		public IEnumerable<EntityKey> NewTargetKeys
 		{
 			get;
