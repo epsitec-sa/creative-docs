cadc2eb80a3d4c6c6086504af93766e8031d9031
Marc Bettex <bettex@epsitec.ch>
2010-08-06 13:57:23 +0000

    - Renomé AbstractUpdateSynchronizationJob en AbstractFieldSynchronizationJob. - Commenté AbstractFieldSynchronizationJob. - Commenté ValueSynchronizationJob. - Ajouté un test unitaire pour ValueSynchronizationJob.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13718 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.DataLayer.UnitTests/Saver/SynchronizationJobs/UnitTestValueSynchronizationJob.cs
@@ -0,0 +1,67 @@
+﻿using Epsitec.Common.Support;
+
+using Epsitec.Cresus.Database;
+
+using Epsitec.Cresus.DataLayer.Saver.SynchronizationJobs;
+
+using Epsitec.Cresus.DataLayer.Context;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Epsitec.Cresus.DataLayer.UnitTests
+{
+
+
+	[TestClass]
+	public class UnitTestValueSynchronizationJob
+	{
+
+		
+		[TestMethod]
+		public void ValueSynchronizationJobConstructorTest1()
+		{
+			int dataContextId = 0;
+			EntityKey entityKey = new EntityKey (Druid.FromLong (1), new DbKey (new DbId (1)));
+			Druid fieldId = Druid.FromLong (1);
+			object value = "value";
+
+			var job = new ValueSynchronizationJob (dataContextId, entityKey, fieldId, value);
+
+			Assert.AreEqual (dataContextId, job.DataContextId);
+			Assert.AreEqual (entityKey, job.EntityKey);
+			Assert.AreEqual (fieldId, job.FieldId);
+			Assert.AreEqual (value, job.NewValue);
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentException))]
+		public void ValueSynchronizationJobConstructorTest2()
+		{
+			int dataContextId = 0;
+			EntityKey entityKey = EntityKey.Empty;
+			Druid fieldId = Druid.FromLong (1);
+			object value = "value";
+
+			var job = new ValueSynchronizationJob (dataContextId, entityKey, fieldId, value);
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentException))]
+		public void ValueSynchronizationJobConstructorTest3()
+		{
+			int dataContextId = 0;
+			EntityKey entityKey = new EntityKey (Druid.FromLong (1), new DbKey (new DbId (1)));
+			Druid fieldId = Druid.Empty;
+			object value = "value";
+
+			var job = new ValueSynchronizationJob (dataContextId, entityKey, fieldId, value);
+		}
+
+
+	}
+
+
+}
