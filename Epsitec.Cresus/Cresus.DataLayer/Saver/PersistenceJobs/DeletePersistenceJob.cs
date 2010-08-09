9fb07d5030b8b69c2014708e49f9bd88a7a5ea50
Marc Bettex <bettex@epsitec.ch>
2010-08-09 08:50:49 +0000

    - Commenté DeletePersistenceJob.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@13743 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.DataLayer/Saver/PersistenceJobs/DeletePersistenceJob.cs
+++ Epsitec.Cresus/Cresus.DataLayer/Saver/PersistenceJobs/DeletePersistenceJob.cs
@@ -5,10 +5,19 @@ namespace Epsitec.Cresus.DataLayer.Saver.PersistenceJobs
 {
 
 
+	/// <summary>
+	/// The <c>DeletePersistenceJob</c> class indicates that an <see cref="AbstractEntity"/> has been
+	/// deleted and that it must be removed with all its references from the database.
+	/// </summary>
 	internal class DeletePersistenceJob : AbstractPersistenceJob
 	{
 
 
+		/// <summary>
+		/// Creates a new <c>DeletePersistenceJob</c>.
+		/// </summary>
+		/// <param name="entity">The <see cref="AbstractEntity"/> concerned by the <c>DeletePersistenceJob</c>.</param>
+		/// <exception cref="System.ArgumentNullException">If <paramref name="entity"/> is <c>null</c>.</exception>
 		public DeletePersistenceJob(AbstractEntity entity) : base (entity)
 		{
 		}
