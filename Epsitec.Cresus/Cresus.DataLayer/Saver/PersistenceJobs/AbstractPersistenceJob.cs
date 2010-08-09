20b45c3241b1d90dd1026850817afad322239ec7
Marc Bettex <bettex@epsitec.ch>
2010-08-09 08:50:42 +0000

    - Commenté AbstractPersistenceJob.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13742 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.DataLayer/Saver/PersistenceJobs/AbstractPersistenceJob.cs
+++ Epsitec.Cresus/Cresus.DataLayer/Saver/PersistenceJobs/AbstractPersistenceJob.cs
@@ -1,20 +1,36 @@
 ﻿using Epsitec.Common.Support.EntityEngine;
+using Epsitec.Common.Support.Extensions;
 
 
 namespace Epsitec.Cresus.DataLayer.Saver.PersistenceJobs
 {
 
 
+	/// <summary>
+	/// The <c>AbstractPersistenceJob</c> class is the base class for all the descriptions of all
+	/// the modifications that have been made to an <see cref="AbstractEntity"/> and that is used to
+	/// persist these modifications to the database.
+	/// </summary>
 	internal abstract class AbstractPersistenceJob
 	{
 
 
+		/// <summary>
+		/// Creates a new <c>AbstractPersistenceJob</c>.
+		/// </summary>
+		/// <param name="entity">The <see cref="AbstractEntity"/> concerned by the <c>AbstractPersistenceJob</c>.</param>
+		/// <exception cref="System.ArgumentNullException">If <paramref name="entity"/> is <c>null</c>.</exception>
 		protected AbstractPersistenceJob(AbstractEntity entity)
 		{
+			entity.ThrowIfNull ("entity");
+
 			this.Entity = entity;
 		}
 		
 		
+		/// <summary>
+		/// The <see cref="AbstractEntity"/> concerned by this instance.
+		/// </summary>
 		public AbstractEntity Entity
 		{
 			get;
