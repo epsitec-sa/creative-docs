aca5803685b19dd4161fa31baa510473eb009702
Marc Bettex <bettex@epsitec.ch>
2010-08-09 08:50:59 +0000

    - Ajouté un test unitaire pour DeletePersistenceJob.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13744 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.DataLayer.UnitTests/UnitTestDeletePersistenceJob.cs
@@ -0,0 +1,40 @@
+﻿using Epsitec.Common.Support.EntityEngine;
+
+using Epsitec.Cresus.DataLayer.Saver.PersistenceJobs;
+using Epsitec.Cresus.DataLayer.UnitTests.Entities;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Epsitec.Cresus.DataLayer.UnitTests
+{
+
+
+	[TestClass]
+	public sealed class UnitTestDeletePersistenceJob
+	{
+
+
+		[TestMethod]
+		public void DeletePersistenceJobConstructorTest1()
+		{
+			AbstractEntity entity = new NaturalPersonEntity ();
+
+			var job = new DeletePersistenceJob (entity);
+
+			Assert.AreSame (entity, job.Entity);
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentNullException))]
+		public void DeletePersistenceJobConstructorTest2()
+		{
+			new DeletePersistenceJob (null);
+		}
+
+
+	}
+
+
+}
