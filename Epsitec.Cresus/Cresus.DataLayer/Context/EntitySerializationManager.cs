8a0b67c84338e08dd59e65274da84321c6db1214
Marc Bettex <bettex@epsitec.ch>
2010-08-11 08:00:12 +0000

    - Créer le squelette de EntitySerializationManager qui va servir à séarialzer et désérializer des entités.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@13823 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.DataLayer/Context/EntitySerializationManager.cs
@@ -0,0 +1,42 @@
+﻿using Epsitec.Common.Support.EntityEngine;
+
+using Epsitec.Cresus.DataLayer.Loader.Data;
+
+
+namespace Epsitec.Cresus.DataLayer.Context
+{
+
+
+	internal sealed class EntitySerializationManager
+	{
+
+
+		public EntitySerializationManager(DataContext dataContext)
+		{
+			this.DataContext = dataContext;
+		}
+
+
+		private DataContext DataContext
+		{
+			get;
+			set;
+		}
+
+
+		public EntityData Serialize(AbstractEntity entity)
+		{
+			throw new System.NotImplementedException ();
+		}
+
+
+		public AbstractEntity Deserialize(EntityData data)
+		{
+			throw new System.NotImplementedException ();
+		}
+
+
+	}
+
+
+}
