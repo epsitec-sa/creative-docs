3a928db2e80224cb0c4a8687e4ac729bc185482d
Marc Bettex <bettex@epsitec.ch>
2010-08-11 10:03:14 +0000

    - Ajouté un test unitaire pour EntitySerializationManager.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13832 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.DataLayer.UnitTests/Serialization/UnitTestEntitySerializationManager.cs
@@ -0,0 +1,219 @@
+﻿using Epsitec.Common.Support;
+using Epsitec.Common.Support.EntityEngine;
+
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Database;
+
+using Epsitec.Cresus.DataLayer.Context;
+using Epsitec.Cresus.DataLayer.Serialization;
+using Epsitec.Cresus.DataLayer.UnitTests.Entities;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+
+
+namespace Epsitec.Cresus.DataLayer.UnitTests
+{
+
+
+	[TestClass]
+	public sealed class UnitTestEntitySerializationManager
+	{
+
+
+		[ClassInitialize]
+		public static void ClassInitialize(TestContext testContext)
+		{
+			TestHelper.Initialize ();
+
+			DatabaseHelper.CreateAndConnectToDatabase ();
+
+			Assert.IsTrue (DatabaseHelper.DbInfrastructure.IsConnectionOpen);
+
+			using (DataContext dataContext = new DataContext (DatabaseHelper.DbInfrastructure))
+			{
+				DatabaseCreator2.PupulateDatabase (dataContext);
+			}
+		}
+
+
+		[ClassCleanup]
+		public static void ClassCleanup()
+		{
+			DatabaseHelper.DisconnectFromDatabase ();
+		}
+
+
+		[TestMethod]
+		public void EntitySerializationManagerConstructorTest1()
+		{
+			using (DataContext dataContext = new DataContext (DatabaseHelper.DbInfrastructure))
+			{
+				new EntitySerializationManager (dataContext);
+			}
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentNullException))]
+		public void EntitySerializationManagerConstructorTest2()
+		{
+			new EntitySerializationManager (null);
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentNullException))]
+		public void SerializeTest()
+		{
+			using (DataContext dataContext = new DataContext (DatabaseHelper.DbInfrastructure))
+			{
+				new EntitySerializationManager (dataContext).Serialize (null);
+			}
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentNullException))]
+		public void DeserializeTest()
+		{
+			using (DataContext dataContext = new DataContext (DatabaseHelper.DbInfrastructure))
+			{
+				new EntitySerializationManager (dataContext).Deserialize (null);
+			}
+		}
+
+
+		[TestMethod]
+		public void SimpleTest()
+		{
+			using (DataContext dataContext = new DataContext (DatabaseHelper.DbInfrastructure))
+			{
+				EntitySerializationManager serializer = new EntitySerializationManager (dataContext);
+
+				foreach (AbstractEntity entity in this.GetSampleEntities (dataContext))
+				{
+					EntityData serializedEntity = serializer.Serialize (entity);
+					AbstractEntity deserializedEntity = serializer.Deserialize (serializedEntity);
+
+					this.CheckEntitiesAreSimilar (dataContext, entity, deserializedEntity);
+				}
+			}
+
+		}
+
+
+		private void CheckEntitiesAreSimilar(DataContext dataContext, AbstractEntity entity1, AbstractEntity entity2)
+		{
+			Assert.AreEqual (entity1.GetEntityStructuredTypeId (), entity2.GetEntityStructuredTypeId ());
+
+			this.CheckValueFields (dataContext, entity1, entity2);
+			this.CheckReferenceFields (dataContext, entity1, entity2);
+			this.CheckCollectionFields (dataContext, entity1, entity2);
+		}
+
+
+		private void CheckValueFields(DataContext dataContext, AbstractEntity entity1, AbstractEntity entity2)
+		{
+			Druid leafEntityId = entity1.GetEntityStructuredTypeId();
+
+			var fieldIds = from field in dataContext.EntityContext.GetEntityFieldDefinitions (leafEntityId)
+						   where field.Relation == FieldRelation.None
+						   where field.Source == FieldSource.Value
+						   select field.CaptionId;
+
+			foreach (Druid fieldId in fieldIds)
+			{
+				object value1 = entity1.GetField<object> (fieldId.ToResourceId ());
+				object value2 = entity2.GetField<object> (fieldId.ToResourceId ());
+
+				Assert.AreEqual (value1, value2);
+			}
+		}
+
+
+		private void CheckReferenceFields(DataContext dataContext,  AbstractEntity entity1, AbstractEntity entity2)
+		{
+			Druid leafEntityId = entity1.GetEntityStructuredTypeId ();
+
+			var fieldIds = from field in dataContext.EntityContext.GetEntityFieldDefinitions (leafEntityId)
+						   where field.Relation == FieldRelation.Reference
+						   where field.Source == FieldSource.Value
+						   select field.CaptionId;
+
+			foreach (Druid fieldId in fieldIds)
+			{
+				AbstractEntity value1 = entity1.GetField<AbstractEntity> (fieldId.ToResourceId ());
+				AbstractEntity value2 = entity2.GetField<AbstractEntity> (fieldId.ToResourceId ());
+
+				if (value1 == null || !dataContext.IsPersistent (value1))
+				{
+					Assert.IsNull (value2);
+				}
+				else
+				{
+					Assert.AreEqual (value1, value2);
+				}
+			}
+		}
+
+
+		private void CheckCollectionFields(DataContext dataContext, AbstractEntity entity1, AbstractEntity entity2)
+		{
+			Druid leafEntityId = entity1.GetEntityStructuredTypeId ();
+
+			var fieldIds = from field in dataContext.EntityContext.GetEntityFieldDefinitions (leafEntityId)
+						   where field.Relation == FieldRelation.Collection
+						   where field.Source == FieldSource.Value
+						   select field.CaptionId;
+
+			foreach (Druid fieldId in fieldIds)
+			{
+				IList<AbstractEntity> values1 = entity1.GetFieldCollection<AbstractEntity> (fieldId.ToResourceId ());
+				IList<AbstractEntity> values2 = entity2.GetFieldCollection<AbstractEntity> (fieldId.ToResourceId ());
+
+				Assert.IsTrue
+				(
+					values1
+					.Where (t => t!= null)
+					.Where (t => dataContext.IsPersistent (t))
+					.SequenceEqual (values2)
+				);
+
+			}
+		}
+
+
+		private IEnumerable<AbstractEntity> GetSampleEntities(DataContext dataContext)
+		{
+			yield return dataContext.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+			yield return dataContext.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (2)));
+			yield return dataContext.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (3)));
+
+			yield return dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1)));
+			yield return dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (2)));
+			yield return dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (3)));
+			yield return dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (4)));
+
+			yield return dataContext.ResolveEntity<LanguageEntity> (new DbKey (new DbId (1)));
+			yield return dataContext.ResolveEntity<LanguageEntity> (new DbKey (new DbId (2)));
+
+			yield return dataContext.ResolveEntity<PersonTitleEntity> (new DbKey (new DbId (1)));
+			yield return dataContext.ResolveEntity<PersonTitleEntity> (new DbKey (new DbId (2)));
+
+			yield return dataContext.ResolveEntity<PersonGenderEntity> (new DbKey (new DbId (1)));
+			yield return dataContext.ResolveEntity<PersonGenderEntity> (new DbKey (new DbId (2)));
+
+			yield return dataContext.ResolveEntity<UriSchemeEntity> (new DbKey (new DbId (1)));
+		}
+
+
+	}
+
+
+}
