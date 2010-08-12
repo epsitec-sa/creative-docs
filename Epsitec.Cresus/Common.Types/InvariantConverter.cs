fb475a42eb0c7aab12a65e5b55f334d2e39ebc1f
Marc Bettex <bettex@epsitec.ch>
2010-08-12 14:33:47 +0000

    - Modifications mineures dans InvariantConverter.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@13885 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Types/InvariantConverter.cs
+++ Epsitec.Cresus/Common.Types/InvariantConverter.cs
@@ -567,14 +567,7 @@ namespace Epsitec.Common.Types
 			throw new System.NotSupportedException (string.Format ("Type {0}: conversion not supported.", obj.GetType ().Name));
 		}
 
-		public static bool Convert(object obj, StructuredTypeField field, out object value)
-		{
-			return InvariantConverter.Convert (obj, field.Type, out value);
-		}
-		public static bool Convert(object obj, ISystemType type, out object value)
-		{
-			return InvariantConverter.Convert (obj, type.SystemType, out value);
-		}
+		
 		public static bool Convert(object obj, System.Type type, out object value)
 		{
 			if (obj == null)
@@ -605,6 +598,16 @@ namespace Epsitec.Common.Types
 				{
 					bool result;
 					
+					if (InvariantConverter.Convert (obj, out result))
+					{
+						value = result;
+						return true;
+					}
+				}
+				else if (type == typeof (short))
+				{
+					short result;
+
 					if (InvariantConverter.Convert (obj, out result))
 					{
 						value = result;
