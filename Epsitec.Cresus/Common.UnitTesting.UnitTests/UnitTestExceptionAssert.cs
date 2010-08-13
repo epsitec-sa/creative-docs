7e15f77d56cce9134baa72fef80bf2a7bf1f381d
Marc Bettex <bettex@epsitec.ch>
2010-08-13 06:57:37 +0000

    - Implémenté et commenté ExceptionAssert. - Implémenté le test unitaire pour ExceptionAssert.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13891 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.UnitTesting.UnitTests/UnitTestExceptionAssert.cs
+++ Epsitec.Cresus/Common.UnitTesting.UnitTests/UnitTestExceptionAssert.cs
@@ -10,31 +10,68 @@ namespace Common.UnitTesting.UnitTests
 	[TestClass]
 	public class UnitTestExceptionAssert
 	{
-		
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentNullException))]
+		public void ArgumentCheck1()
+		{
+			ExceptionAssert.Throw (null);
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentNullException))]
+		public void ArgumentCheck2()
+		{
+			ExceptionAssert.Throw<System.ArgumentNullException> (null);
+		}
+
 
 		[TestMethod]
 		[ExpectedException (typeof (AssertFailedException))]
 		public void ThrowNoException1()
 		{
-			ExceptionAssert.Throw<System.Exception> (() => { });
+			ExceptionAssert.Throw (() => { });
 		}
-
+		
 
 		[TestMethod]
-		public void ThrowExcpectedException()
+		[ExpectedException (typeof (AssertFailedException))]
+		public void ThrowNoException2()
 		{
-
-
-
+			ExceptionAssert.Throw<System.ArgumentNullException> (() => { });
 		}
 
 
 		[TestMethod]
+		public void ThrowExcpectedException1()
+		{
+			ExceptionAssert.Throw<System.ArgumentNullException> (() =>
+			{
+				throw new System.ArgumentNullException ();
+			});
+		}
+
+
+		[TestMethod]
+		public void ThrowExcpectedException2()
+		{
+			ExceptionAssert.Throw (() =>
+			{
+				throw new System.Exception ();
+			});
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (AssertFailedException))]
 		public void ThrowUnexpectedException()
 		{
-
-
-
+			ExceptionAssert.Throw<System.ArgumentNullException> (() =>
+			{
+				throw new System.Exception ();
+			});
 		}
 
 
