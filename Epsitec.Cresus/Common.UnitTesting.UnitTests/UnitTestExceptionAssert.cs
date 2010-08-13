399ccb4038f429932361007ce233f928cb8809bf
Marc Bettex <bettex@epsitec.ch>
2010-08-13 06:39:54 +0000

    - Un peu de mic mac avec ExceptionAssert. Il fallait le mettre dans un projet séparé qui puisse targetter le framework 4 complet, du coup nouveau projet, nouveau projet de test, suppression du projet Common.Debuc.UnitTests et déplacement de tous les fichiers concernés. - Apparement commit de deux fichiers détruits qui étaient pas passés avec la synchronization svn de visual studio.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13890 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.UnitTesting.UnitTests/UnitTestExceptionAssert.cs
@@ -0,0 +1,44 @@
+﻿using Epsitec.Common.UnitTesting;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Common.UnitTesting.UnitTests
+{
+
+
+	[TestClass]
+	public class UnitTestExceptionAssert
+	{
+		
+
+		[TestMethod]
+		[ExpectedException (typeof (AssertFailedException))]
+		public void ThrowNoException1()
+		{
+			ExceptionAssert.Throw<System.Exception> (() => { });
+		}
+
+
+		[TestMethod]
+		public void ThrowExcpectedException()
+		{
+
+
+
+		}
+
+
+		[TestMethod]
+		public void ThrowUnexpectedException()
+		{
+
+
+
+		}
+
+
+	}
+
+
+}
