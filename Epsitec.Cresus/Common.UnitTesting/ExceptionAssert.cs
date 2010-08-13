e758b7165c76ed40b1b65bda2c995691ea0962ce
Marc Bettex <bettex@epsitec.ch>
2010-08-13 06:39:54 +0000

    - Un peu de mic mac avec ExceptionAssert. Il fallait le mettre dans un projet séparé qui puisse targetter le framework 4 complet, du coup nouveau projet, nouveau projet de test, suppression du projet Common.Debuc.UnitTests et déplacement de tous les fichiers concernés. - Apparement commit de deux fichiers détruits qui étaient pas passés avec la synchronization svn de visual studio.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@13890 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.UnitTesting/ExceptionAssert.cs
@@ -0,0 +1,47 @@
+﻿namespace Epsitec.Common.UnitTesting
+{
+	
+	
+	public static class ExceptionAssert
+	{
+
+
+		public static void Throw(System.Action action)
+		{
+			ExceptionAssert.Throw<System.Exception> (action);
+		}
+
+
+		public static void Throw<TException>(System.Action action) where TException : System.Exception
+		{
+			if (action == null)
+			{
+				throw new System.ArgumentNullException ("action");
+			}
+
+			try
+			{
+				action ();
+
+				string text = "Exception of type {0} expected but no exception was thrown";
+				string message = System.String.Format (text, typeof (TException).FullName);
+
+				throw new Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException (message);
+			}
+			catch (TException)
+			{
+			}
+			catch (System.Exception e)
+			{
+				string text = "Exception of type {0} expected but exception of type {1} was thrown";
+				string message = System.String.Format (text, typeof (TException).FullName, e.GetType ().FullName);
+
+				throw new Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException (message);
+			}
+		}
+
+
+	}
+
+
+}
