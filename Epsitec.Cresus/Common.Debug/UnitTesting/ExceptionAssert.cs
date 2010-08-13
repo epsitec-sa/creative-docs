e68a573b41912a4efeea39ee9998aa1757d90ee8
Marc Bettex <bettex@epsitec.ch>
2010-08-13 05:50:46 +0000

    - Ajouté une classe qui va permettre de mieux tester les exceptions dans les tests unitaires.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@13889 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Debug/UnitTesting/ExceptionAssert.cs
@@ -0,0 +1,36 @@
+﻿namespace Epsitec.Common.Debug.UnitTesting
+{
+	
+	
+	public static class ExceptionAssert
+	{
+
+
+		public static void Throw(System.Action action)
+		{
+			ExceptionAssert.Throw<System.Exception> (action);
+		}
+
+
+		public static void Throw<TException>(System.Action action) where TException : System.Exception
+		{
+			try
+			{
+				action ();
+
+				Assert.Fail ("Exception of type " + typeof (TException).FullName + " expected but no exception was thrown");
+			}
+			catch (TException e)
+			{
+			}
+			catch (System.Exception e)
+			{
+				Assert.Fail ("Exception of type " + typeof (TException).FullName + " expected but exception of type " + e.GetType().FullName + " was thrown");
+			}
+		}
+		
+
+	}
+
+
+}
