f9f67530195855e1d1a4d9afdfccc4a012fe6ad5
Daniel Roux <roux@epsitec.ch>
2010-08-18 11:13:46 +0000

    - Cresus.Core: Tuiles article: Déplacement des groupes pour les goupes et les paramètres de la tuile d'édition vers la tuile résumée.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@13999 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionArticleDefinitionItemViewController.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionArticleDefinitionItemViewController.cs
@@ -42,13 +42,6 @@ namespace Epsitec.Cresus.Core.Controllers.EditionControllers
 
 				builder.CreateFooterEditorTile ();
 			}
-
-			//	Summary:
-			using (var data = TileContainerController.Setup (this))
-			{
-				this.CreateUIGroup (data);
-				this.CreateUIParameters (data);
-			}
 		}
 
 
@@ -118,107 +111,5 @@ namespace Epsitec.Cresus.Core.Controllers.EditionControllers
 		}
 
 
-		private void CreateUIGroup(SummaryDataItems data)
-		{
-			data.Add (
-				new SummaryData
-				{
-					AutoGroup    = true,
-					Name		 = "ArticleGroup",
-					IconUri		 = "Data.ArticleGroup",
-					Title		 = TextFormatter.FormatText ("Groupes d'articles"),
-					CompactTitle = TextFormatter.FormatText ("Groupes"),
-					Text		 = CollectionTemplate.DefaultEmptyText
-				});
-
-			var template = new CollectionTemplate<ArticleGroupEntity> ("ArticleGroup", data.Controller, this.DataContext);
-
-			template.DefineText (x => TextFormatter.FormatText (x.Name));
-			template.DefineCompactText (x => TextFormatter.FormatText (x.Name));
-
-			data.Add (CollectionAccessor.Create (this.EntityGetter, x => x.ArticleGroups, template));
-		}
-
-		private void CreateUIParameters(SummaryDataItems data)
-		{
-			data.Add (
-				new SummaryData
-				{
-					AutoGroup    = true,
-					Name		 = "ArticleParameterDefinition",
-					IconUri		 = "Data.ArticleParameter",
-					Title		 = TextFormatter.FormatText ("Paramètres"),
-					CompactTitle = TextFormatter.FormatText ("Paramètres"),
-					Text		 = CollectionTemplate.DefaultEmptyText,
-				});
-
-			var template = new CollectionTemplate<AbstractArticleParameterDefinitionEntity> ("ArticleParameterDefinition", data.Controller, this.DataContext);
-
-			template.DefineText (x => TextFormatter.FormatText (GetParameterSummary (x)));
-			template.DefineCompactText (x => TextFormatter.FormatText (GetParameterSummary (x)));
-			template.DefineCreateItem (this.CreateParameter);  // le bouton [+] crée une ligne d'article
-
-			data.Add (CollectionAccessor.Create (this.EntityGetter, x => x.ArticleParameterDefinitions, template));
-		}
-
-
-		private static string GetParameterSummary(AbstractArticleParameterDefinitionEntity parameter)
-		{
-			var builder = new System.Text.StringBuilder ();
-
-			if (!string.IsNullOrEmpty (parameter.Name))
-			{
-				builder.Append (parameter.Name);
-				builder.Append (": ");
-			}
-
-			if (parameter is NumericValueArticleParameterDefinitionEntity)
-			{
-				var value = parameter as NumericValueArticleParameterDefinitionEntity;
-
-				if (value.DefaultValue.HasValue ||
-					value.MinValue.HasValue     ||
-					value.MaxValue.HasValue     )
-				{
-					builder.Append (value.DefaultValue.ToString ());
-					builder.Append (" (");
-					builder.Append (value.MinValue.ToString ());
-					builder.Append ("..");
-					builder.Append (value.MaxValue.ToString ());
-					builder.Append (")");
-				}
-				else
-				{
-					builder.Append ("<i>Vide</i>");
-				}
-			}
-
-			if (parameter is EnumValueArticleParameterDefinitionEntity)
-			{
-				var value = parameter as EnumValueArticleParameterDefinitionEntity;
-
-				if (!string.IsNullOrWhiteSpace (value.DefaultValue) ||
-					!string.IsNullOrWhiteSpace (value.Values)       )
-				{
-					builder.Append (value.DefaultValue);
-					builder.Append (" (");
-					builder.Append (Common.EnumInternalToSingleLine (value.Values));
-					builder.Append (")");
-				}
-				else
-				{
-					builder.Append ("<i>Vide</i>");
-				}
-			}
-
-			return builder.ToString ();
-			;
-		}
-
-		private NumericValueArticleParameterDefinitionEntity CreateParameter()
-		{
-			//	Crée un nouvelle ligne dans la facture du type le plus courant, c'est-à-dire ArticleDocumentItemEntity.
-			return this.DataContext.CreateEmptyEntity<NumericValueArticleParameterDefinitionEntity> ();
-		}
 	}
 }
