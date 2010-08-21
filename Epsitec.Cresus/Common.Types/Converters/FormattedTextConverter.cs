5544287492eccdadffb6f76b9341331c9eafe0fa
Pierre Arnaud <arnaud@opac.ch>
2010-08-21 11:34:18 +0000

    - Ajouté un convertisseur pour FormattedText (que GenericConverter<T> peut utiliser). - Refactoring pour extraire les classes liées aux convertisseurs dans des fichiers séparés.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@14047 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/Converters/FormattedTextConverter.cs
@@ -0,0 +1,44 @@
+//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Common.Types.Converters
+{
+	/// <summary>
+	/// The <c>FormattedTextConverter</c> class does a transparent conversion;
+	/// it is required so that the <see cref="Marshaler"/> can work with
+	/// <c>FormattedText</c> values.
+	/// </summary>
+	public class FormattedTextConverter : GenericConverter<FormattedText>
+	{
+		public override string ConvertToString(FormattedText text)
+		{
+			return text.IsNull ? null : text.ToString ();
+		}
+
+		public override ConversionResult<FormattedText> ConvertFromString(string text)
+		{
+			if (text == null)
+			{
+				return new ConversionResult<FormattedText>
+				{
+					IsNull = true,
+				};
+			}
+			else
+			{
+				return new ConversionResult<FormattedText>
+				{
+					Value = new FormattedText (text),
+				};
+			}
+		}
+
+		public override bool CanConvertFromString(string text)
+		{
+			return true;
+		}
+	}
+}
---
+++ Epsitec.Cresus/Common.Types/Converters/GenericConverter`1.cs
@@ -0,0 +1,37 @@
+//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Common.Types.Converters
+{
+	/// <summary>
+	/// The <c>GenericConverter</c> class is the base class for specific type converters.
+	/// </summary>
+	/// <typeparam name="T">The type on which the converter operates.</typeparam>
+	public abstract class GenericConverter<T> : GenericConverter
+	{
+		public abstract string ConvertToString(T value);
+
+		public abstract ConversionResult<T> ConvertFromString(string text);
+
+		public abstract bool CanConvertFromString(string text);
+
+		public static readonly GenericConverter<T> Instance;
+
+		static GenericConverter()
+		{
+			var converterType = GenericConverter.FindConverterType<T> ();
+
+			if (converterType != null)
+			{
+				GenericConverter<T>.Instance = System.Activator.CreateInstance (converterType) as GenericConverter<T>;
+			}
+			else if (typeof (T).IsEnum)
+			{
+				GenericConverter<T>.Instance = new EnumConverter<T> (typeof (T));
+			}
+		}
+	}
+}
