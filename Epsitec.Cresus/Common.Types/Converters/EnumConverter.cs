322a5a1bbbca654748bd838674b0fd13fb028abe
Pierre Arnaud <arnaud@opac.ch>
2010-08-21 11:34:18 +0000

    - Ajouté un convertisseur pour FormattedText (que GenericConverter<T> peut utiliser). - Refactoring pour extraire les classes liées aux convertisseurs dans des fichiers séparés.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@14047 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/Converters/EnumConverter.cs
@@ -0,0 +1,50 @@
+//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Common.Types.Converters
+{
+	public class EnumConverter<T> : GenericConverter<T>
+	{
+		public EnumConverter(System.Type systemType)
+		{
+			this.enumType = EnumType.GetDefault (systemType);
+		}
+
+		public override string ConvertToString(T value)
+		{
+			return EnumConverter<T>.ConvertToNumericString (value);
+		}
+
+		public override ConversionResult<T> ConvertFromString(string text)
+		{
+			if (string.IsNullOrWhiteSpace (text))
+			{
+				return new ConversionResult<T> ()
+				{
+					IsNull = true,
+				};
+			}
+			return new ConversionResult<T> ()
+			{
+				Value = InvariantConverter.ToEnum<T> (text)
+			};
+		}
+
+		public override bool CanConvertFromString(string text)
+		{
+			return this.enumType.IsValidValue (text);
+		}
+
+
+		public static string ConvertToNumericString(T value)
+		{
+			object boxedValue = value;
+			return InvariantConverter.ToString (EnumType.ConvertToInt ((System.Enum) boxedValue));
+		}
+
+		private readonly EnumType enumType;
+	}
+}
