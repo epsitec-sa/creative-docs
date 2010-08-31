9612ce67938233ad3d524db4d4fda8f000511445
Daniel Roux <roux@epsitec.ch>
2010-08-31 18:02:47 +0000

    - Cresus.Core: JobPrintEngine: Nouvelle classe qui imprime un job (plusieurs pages sur différents bacs, sur la même imprimante physique). - Cresus.Core: SectionToPrint: Extraction d'une classe existante dans un fichier séparé. - Cresus.Core: JobToPrint: Nouvelle classe. - Cresus.Core: MultiPagePrintEngine: Suppression de cette classe devenue inutile. Elle est remplacée par JobPrintEngine. - Cresus.Core: L'impression sur plusieurs bacs d'une même imprimante en un seul job fonctionne, ouf !
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@14122 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Printers/JobToPrint.cs
@@ -0,0 +1,50 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Epsitec.Cresus.Core.Printers
+{
+	/// <summary>
+	/// Une instance de JobToPrint représente plusieurs sections (SectionToPrint) à imprimer sur une
+	/// même imprimante physique, mais éventuellement sur plusieurs bacs.
+	/// </summary>
+	public class JobToPrint
+	{
+		public JobToPrint()
+		{
+			this.sections = new List<SectionToPrint> ();
+		}
+
+		public List<SectionToPrint> Sections
+		{
+			get
+			{
+				return this.sections;
+			}
+		}
+
+		public string PrinterPhysicalName
+		{
+			//	Retourne le nom de l'imprimante physique de la première section.
+			//	Toutes les sections doivent utiliser la même imprimante physique.
+			//	Printer peut changer, s'il s'agit d'un autre bac, mais Printer.PhysicalName
+			//	est identique dans toutes les sections.
+			get
+			{
+				if (this.sections.Count == 0)
+				{
+					return null;
+				}
+				else
+				{
+					return this.sections[0].Printer.PhysicalName;
+				}
+			}
+		}
+
+		private List<SectionToPrint> sections;
+	}
+}
---
+++ Epsitec.Cresus/Cresus.Core/Printers/SectionToPrint.cs
@@ -0,0 +1,103 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Epsitec.Cresus.Core.Printers
+{
+	/// <summary>
+	/// Une instance de SectionToPrint représente une section d'une ou plusieurs pages contigües à imprimer
+	/// sur une imprimante physique et un seul bac (Printer).
+	/// Initialement, on crée des sections d'une seule page. Elles sont regroupées par la suite.
+	/// </summary>
+	public class SectionToPrint
+	{
+		public SectionToPrint(Printer printer, int firstPage, int entityRank, AbstractEntityPrinter entityPrinter)
+		{
+			//	Crée une section d'une page.
+			this.printer       = printer;
+			this.firstPage     = firstPage;
+			this.PageCount     = 1;
+			this.entityRank    = entityRank;
+			this.entityPrinter = entityPrinter;
+		}
+
+		public Printer Printer
+		{
+			get
+			{
+				return this.printer;
+			}
+		}
+
+		public int FirstPage
+		{
+			get
+			{
+				return this.firstPage;
+			}
+		}
+
+		public int PageCount
+		{
+			get;
+			set;
+		}
+
+		public int EntityRank
+		{
+			get
+			{
+				return this.entityRank;
+			}
+		}
+
+		public AbstractEntityPrinter EntityPrinter
+		{
+			get
+			{
+				return this.entityPrinter;
+			}
+		}
+
+		public override string ToString()
+		{
+			// Pratique pour le debug.
+			return string.Format ("PrinterLogicalName={0}, PrinterPhysicalName={1}, FirstPage={2}, PageCount={3}, EntityRank={4}", this.printer.LogicalName, this.printer.PhysicalName, this.firstPage, this.PageCount, this.entityRank);
+		}
+
+
+		public static int CompareSectionToPrint(SectionToPrint x, SectionToPrint y)
+		{
+			//	Détermine comment regrouper les pages. On cherche à grouper les pages
+			//	qui utilisent une même imprimante physique.
+			int result;
+
+			result = string.Compare (x.Printer.PhysicalName, y.Printer.PhysicalName);
+			if (result != 0)
+			{
+				return result;
+			}
+
+			if (x.EntityRank != y.EntityRank)
+			{
+				return (x.EntityRank < y.EntityRank) ? -1 : 1;
+			}
+
+			if (x.FirstPage != y.FirstPage)
+			{
+				return (x.FirstPage < y.FirstPage) ? -1 : 1;
+			}
+
+			return 0;
+		}
+	
+		
+		private readonly Printer				printer;
+		private readonly int					firstPage;
+		private readonly int					entityRank;
+		private readonly AbstractEntityPrinter	entityPrinter;
+	}
+}
