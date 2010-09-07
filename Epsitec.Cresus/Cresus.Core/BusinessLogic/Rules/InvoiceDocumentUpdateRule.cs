35912a32bbdbf740d49f8f3823f44f4e95017f39
Pierre Arnaud <arnaud@opac.ch>
2010-09-07 05:12:52 +0000

    - Ajouté l'implémentation d'une première règle de logique business, pour recalculer les totaux d'une facture (ce n'est pas encore câblé).
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@14170 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/BusinessLogic/Rules/InvoiceDocumentUpdateRule.cs
@@ -0,0 +1,25 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Cresus.Core.Entities;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.BusinessLogic.Rules
+{
+	[BusinessRule (RuleType=RuleType.Update)]
+	internal class InvoiceDocumentUpdateRule : GenericBusinessRule<InvoiceDocumentEntity>
+	{
+		public InvoiceDocumentUpdateRule()
+		{
+		}
+
+		protected override void Apply(InvoiceDocumentEntity entity)
+		{
+			var dataContext = Logic.Current.DataContext;
+
+			Epsitec.Cresus.Core.Helpers.InvoiceDocumentHelper.UpdatePrices (entity, dataContext);
+		}
+	}
+}
