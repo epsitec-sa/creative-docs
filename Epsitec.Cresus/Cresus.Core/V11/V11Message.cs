bd3d1b5581f5f769968aafeb5ae89c275d6ebfd8
Daniel Roux <roux@epsitec.ch>
2010-09-17 10:07:40 +0000

    - Cresus.Core: Nouveau dossier V11. - Cresus.Core: V11Error et V11Message pour une gestion propre et complète des erreurs. - Cresus.Core: Début des travaux pour une importation définitive. Pas terminé.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@14326 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/V11/V11Message.cs
@@ -0,0 +1,71 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using Epsitec.Common.Types;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.V11
+{
+	public struct V11Message
+	{
+		public V11Message(V11Error error, int? lineRank, FormattedText description)
+		{
+			this.error       = error;
+			this.lineRank    = lineRank;
+			this.description = description;
+		}
+
+
+		public V11Error Error
+		{
+			get
+			{
+				return this.error;
+			}
+		}
+
+		public int? LineRank
+		{
+			get
+			{
+				return this.lineRank;
+			}
+		}
+
+		public FormattedText Description
+		{
+			get
+			{
+				return this.description;
+			}
+		}
+
+
+		public bool IsOK
+		{
+			get
+			{
+				return this.error == V11Error.OK;
+			}
+		}
+
+		public bool IsError
+		{
+			get
+			{
+				return this.error != V11Error.OK;
+			}
+		}
+
+
+		public static V11Message OK      = new V11Message (V11Error.OK,      null, null);
+		public static V11Message Aborted = new V11Message (V11Error.Aborted, null, null);
+
+
+		private readonly V11Error			error;
+		private readonly int?				lineRank;
+		private readonly FormattedText		description;
+	}
+}
---
+++ Epsitec.Cresus/Cresus.Core/V11/V11Record.cs
@@ -0,0 +1,199 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using Epsitec.Common.Types;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.V11
+{
+	public class V11Record
+	{
+#region Enumerations
+		public enum TypeEnum
+		{
+			Unknown,
+			Type3,
+			Type4,
+		}
+
+		public enum CodeTransactionEnum
+		{
+			Unknown,
+			Normal,
+			Remboursement,
+			PropreCompte,
+		}
+
+		public enum BVRTransactionEnum
+		{
+			Unknown,
+			BVR,
+			BVRPlus,
+		}
+
+		public enum GenreTransactionEnum
+		{
+			Unknown,
+			Credit,
+			ContrePrestation,
+			Correction,
+		}
+
+		public enum OrigineEnum
+		{
+			Unknown,
+			OfficePoste,
+			OPA,
+			yellownet,
+			EuroSIC,
+		}
+
+		public enum GenreRemiseEnum
+		{
+			Unknown,
+			Original,
+			Reconstruction,
+			Test,
+		}
+
+		public enum CodeRejetEnum
+		{
+			Unknown,
+			Aucun,
+			Rejet,
+			RejetMasse,
+		}
+#endregion
+
+
+		public TypeEnum Type
+		{
+			get;
+			set;
+		}
+
+		public CodeTransactionEnum CodeTransaction
+		{
+			get;
+			set;
+		}
+
+		public BVRTransactionEnum BVRTransaction
+		{
+			get;
+			set;
+		}
+
+		public string MonnaieTransaction
+		{
+			get;
+			set;
+		}
+
+		public GenreTransactionEnum GenreTransaction
+		{
+			get;
+			set;
+		}
+
+		public OrigineEnum Origine
+		{
+			get;
+			set;
+		}
+
+		public GenreRemiseEnum GenreRemise
+		{
+			get;
+			set;
+		}
+
+		public string NoReference
+		{
+			get;
+			set;
+		}
+
+		public decimal? Montant
+		{
+			get;
+			set;
+		}
+
+		public string MonnaieMontant
+		{
+			get;
+			set;
+		}
+
+		public string RefDepot
+		{
+			get;
+			set;
+		}
+
+		public Date? DateDepot
+		{
+			get;
+			set;
+		}
+
+		public Date? DateTraitement
+		{
+			get;
+			set;
+		}
+
+		public Date? DateCredit
+		{
+			get;
+			set;
+		}
+
+		public string NoMicrofilm
+		{
+			get;
+			set;
+		}
+
+		public CodeRejetEnum CodeRejet
+		{
+			get;
+			set;
+		}
+
+		public decimal? Taxes
+		{
+			get;
+			set;
+		}
+
+		public string MonnaieTaxes
+		{
+			get;
+			set;
+		}
+
+		public bool IsValid
+		{
+			get
+			{
+				return
+					this.Type             != TypeEnum.Unknown &&
+					this.CodeTransaction  != CodeTransactionEnum.Unknown &&
+					this.BVRTransaction   != BVRTransactionEnum.Unknown &&
+					this.GenreTransaction != GenreTransactionEnum.Unknown &&
+					this.Origine          != OrigineEnum.Unknown &&
+					this.GenreRemise      != GenreRemiseEnum.Unknown &&
+					this.Montant          != null &&
+					this.DateDepot        != null &&
+					this.DateTraitement   != null &&
+					this.DateCredit       != null &&
+					this.CodeRejet        != CodeRejetEnum.Unknown &&
+					this.Taxes            != null;
+			}
+		}
+	}
+}
