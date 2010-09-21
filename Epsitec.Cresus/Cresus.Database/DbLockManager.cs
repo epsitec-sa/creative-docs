5c2f7ffe9e3115ef77e5a56ba6e16c23aae8ee61
Marc Bettex <bettex@epsitec.ch>
2010-09-21 14:25:12 +0000

    - Commenté DbLockManager.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@14364 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Database/DbLockManager.cs
+++ Epsitec.Cresus/Cresus.Database/DbLockManager.cs
@@ -2,28 +2,40 @@
 
 using Epsitec.Cresus.Database.Collections;
 
-using System.Collections.Generic;
-
-using System.Linq;
-
 
 namespace Epsitec.Cresus.Database
 {
 
 
-	// TODO Comment this class.
-	// Marc
-
-
+	/// <summary>
+	/// The <c>DbLockManager</c> class provides the low level tools required to interact with the
+	/// high level locks.
+	/// An high level lock is associated to a connection as defined by <see cref="DbConnectionManager"/>
+	/// and is defined by a name. In addition, a lock contains a value which tells how many time it
+	/// has been acquired by the user, which makes it reentrant.
+	/// </summary>
 	public sealed class DbLockManager : DbAbstractAttachable
 	{
 
 
+		/// <summary>
+		/// Creates a new <c>DbLockManager</c>.
+		/// </summary>
 		public DbLockManager() : base ()
 		{
 		}
 
 
+		/// <summary>
+		/// Request a given lock for a given connectionId. If the lock cannot be acquired, an
+		/// <see cref="System.InvalidOperationException"/> is thrown.
+		/// </summary>
+		/// <param name="lockName">The name of the lock to acquire.</param>
+		/// <param name="connectionId">The id of the connection.</param>
+		/// <exception cref="System.ArgumentException">If <paramref name="lockName"/> is <c>null</c> or empty.</exception>
+		/// <exception cref="System.ArgumentException">If <paramref name="connectionId"/> is lower than zero.</exception>
+		/// <exception cref="System.InvalidOperationException">If the lock cannot be acquired.</exception>
+		/// <exception cref="System.InvalidOperationException">If this instance is not attached.</exception>
 		public void RequestLock(string lockName, long connectionId)
 		{
 			this.CheckIsAttached ();
@@ -52,6 +64,17 @@ namespace Epsitec.Cresus.Database
 		}
 
 
+		/// <summary>
+		/// Releases a given locl for a given connectionId. If the lock cannot be release because it
+		/// does not exists or because it is owned by another connection, an
+		/// <see cref="System.InvalidOperationException"/> is thrown.
+		/// </summary>
+		/// <param name="lockName">The name of the lock to acquire.</param>
+		/// <param name="connectionId">The id of the connection.</param>
+		/// <exception cref="System.ArgumentException">If <paramref name="lockName"/> is <c>null</c> or empty.</exception>
+		/// <exception cref="System.ArgumentException">If <paramref name="connectionId"/> is lower than zero.</exception>
+		/// <exception cref="System.InvalidOperationException">If the lock cannot be released.</exception>
+		/// <exception cref="System.InvalidOperationException">If this instance is not attached.</exception>
 		public void ReleaseLock(string lockName, long connectionId)
 		{
 			this.CheckIsAttached ();
@@ -85,6 +108,11 @@ namespace Epsitec.Cresus.Database
 		}
 
 
+		/// <summary>
+		/// Inserts a brand new lock in the database.
+		/// </summary>
+		/// <param name="lockName">The name of the lock.</param>
+		/// <param name="connectionId">The connection id of the lock.</param>
 		private void InsertLock(string lockName, long connectionId)
 		{
 			SqlFieldList fields = new SqlFieldList ();
@@ -101,6 +129,10 @@ namespace Epsitec.Cresus.Database
 		}
 
 
+		/// <summary>
+		/// Removes a lock in the database.
+		/// </summary>
+		/// <param name="lockName">The name of the lock.</param>
 		private void RemoveLock(string lockName)
 		{
 			SqlFieldList conditions = new SqlFieldList ()
@@ -112,6 +144,10 @@ namespace Epsitec.Cresus.Database
 		}
 
 
+		/// <summary>
+		/// Increments the counter of a given lock by 1.
+		/// </summary>
+		/// <param name="lockName">The name of the lock whose counter to increment.</param>
 		private void IncrementLockCounter(string lockName)
 		{
 			using (DbTransaction transaction = this.DbInfrastructure.InheritOrBeginTransaction (DbTransactionMode.ReadWrite))
@@ -125,6 +161,10 @@ namespace Epsitec.Cresus.Database
 		}
 
 
+		/// <summary>
+		/// Decrements the counter of a given lock by 1.
+		/// </summary>
+		/// <param name="lockName">The name of the lock whose counter to decrement.</param>
 		private void DecrementLockCounter(string lockName)
 		{
 			using (DbTransaction transaction = this.DbInfrastructure.InheritOrBeginTransaction (DbTransactionMode.ReadWrite))
@@ -137,18 +177,36 @@ namespace Epsitec.Cresus.Database
 			}
 		}
 
+
+		/// <summary>
+		/// Gets the current counter value of a given lock.
+		/// </summary>
+		/// <param name="lockName">The name of the lock whose counter value to get.</param>
+		/// <returns>The current counter value of the lock.</returns>
 		private int GetLockCounterValue(string lockName)
 		{
 			return (int) this.GetValue (lockName, Tags.ColumnCounter);
 		}
 
 
+		/// <summary>
+		/// Sets the current counter value of a given lock.
+		/// </summary>
+		/// <param name="lockName">The name of the lock whose counter value to set.</param>
+		/// <param name="counterValue">The current counter value of the lock.</param>
 		private void SetLockCounterValue(string lockName, int counterValue)
 		{
 			this.SetValue (lockName, Tags.ColumnCounter, counterValue);
 		}
 
 		
+		/// <summary>
+		/// Tells whether a given lock is owned.
+		/// </summary>
+		/// <param name="lockName">The name of the lock.</param>
+		/// <returns><c>true</c> if the lock is owned, <c>false</c> if it is not.</returns>
+		/// <exception cref="System.ArgumentException">If <paramref name="lockName"/> is <c>null</c> or empty.</exception>
+		/// <exception cref="System.InvalidOperationException">If this instance is not attached.</exception>
 		public bool IsLockOwned(string lockName)
 		{
 			this.CheckIsAttached ();
@@ -164,6 +222,15 @@ namespace Epsitec.Cresus.Database
 		}
 
 
+
+		/// <summary>
+		/// Gets the connection id of the owner of the given lock.
+		/// </summary>
+		/// <param name="lockName">The name of the lock.</param>
+		/// <returns>The connection id of the lock.</returns>
+		/// <exception cref="System.ArgumentException">If <paramref name="lockName"/> is <c>null</c> or empty.</exception>
+		/// <exception cref="System.InvalidOperationException">If the lock does not exist.</exception>
+		/// <exception cref="System.InvalidOperationException">If this instance is not attached.</exception>
 		public long GetLockConnectionId(string lockName)
 		{
 			this.CheckIsAttached ();
@@ -186,6 +253,11 @@ namespace Epsitec.Cresus.Database
 		}
 
 
+		/// <summary>
+		/// Deletes all locks that are associated with an inactive connection (i.e. a connection that
+		/// is not open).
+		/// <exception cref="System.InvalidOperationException">If this instance is not attached.</exception>
+		/// </summary>
 		public void RemoveInactiveLocks()
 		{
 			this.CheckIsAttached ();
@@ -199,6 +271,12 @@ namespace Epsitec.Cresus.Database
 		}
 
 
+		/// <summary>
+		/// Gets a single value of a single lock.
+		/// </summary>
+		/// <param name="lockName">The name of the lock whose value to get.</param>
+		/// <param name="valueName">The name of the value to get.</param>
+		/// <returns>The value.</returns>
 		private object GetValue(string lockName, string valueName)
 		{
 			DbColumn column = this.DbTable.Columns[valueName];
@@ -212,6 +290,12 @@ namespace Epsitec.Cresus.Database
 		}
 
 
+		/// <summary>
+		/// Sets a single value of a single lock.
+		/// </summary>
+		/// <param name="lockName">The name of the lock whose value to set.</param>
+		/// <param name="valueName">The name of the value to set.</param>
+		/// <param name="value">The new value for the value.</param>
 		private void SetValue(string lockName, string valueName, object value)
 		{
 			DbColumn column = this.DbTable.Columns[valueName];
@@ -230,6 +314,12 @@ namespace Epsitec.Cresus.Database
 		}
 
 
+		/// <summary>
+		/// Creates the <see cref="SqlFunction"/> object that describes the condition that returns
+		/// true only for a given lock name.
+		/// </summary>
+		/// <param name="lockName">The name of the lock.</param>
+		/// <returns>The <see cref="SqlFunction"/> object that defines the condition.</returns>
 		private SqlFunction CreateConditionForLockName(string lockName)
 		{
 			return new SqlFunction
@@ -241,6 +331,11 @@ namespace Epsitec.Cresus.Database
 		}
 
 
+		/// <summary>
+		/// Creates the <see cref="SqlFunction"/> object that describes the condition that returns
+		/// true only for the locks whose connection is active.
+		/// </summary>
+		/// <returns>The <see cref="SqlFunction"/> object that defines the condition.</returns>
 		private SqlFunction CreateConditionForInactiveLocks()
 		{
 			SqlSelect queryForOpenConnectionIds = new SqlSelect ();
