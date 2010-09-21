e70c943f063039489dfa10a256cc725214b60b96
Marc Bettex <bettex@epsitec.ch>
2010-09-21 14:32:30 +0000

    - Commenté ConnectionStatus.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@14366 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.DataLayer/Infrastructure/ConnectionStatus.cs
+++ Epsitec.Cresus/Cresus.DataLayer/Infrastructure/ConnectionStatus.cs
@@ -1,18 +1,44 @@
 //	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Epsitec.Cresus.DataLayer.Infrastructure
 {
-	// TODO Comment this enum.
-	// Marc
+	
+	
+	/// <summary>
+	/// The <c>ConnectionStatus</c> enum defines the different states that a connection as defined
+	/// by <see cref="ConnectionInformation"/> can have.
+	/// </summary>
 	public enum ConnectionStatus
 	{
+
+
+		/// <summary>
+		/// The connection has not yet been open and is thus inactive.
+		/// </summary>
 		NotYetOpen = 0,
+
+
+		/// <summary>
+		/// The connection is open and thus active.
+		/// </summary>
 		Open = 1,
+		
+		
+		/// <summary>
+		/// The connection has been properly closed and is thus inactive.
+		/// </summary>
 		Closed = 2,
+		
+		
+		/// <summary>
+		/// The connection has been interrupted after a time out and is thus inactive.
+		/// </summary>
 		Interrupted = 3,
+
+	
 	}
+
+
 }
