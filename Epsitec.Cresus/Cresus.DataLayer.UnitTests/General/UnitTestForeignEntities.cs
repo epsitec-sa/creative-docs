86ff9492af38751ac4ae3ac1b472849c65286924
Marc Bettex <bettex@epsitec.ch>
2010-09-22 10:26:27 +0000

    - Commencé l'implémentation de la vérification des entités étrangéres dans le DataContext. - Ajouté des tests pour tester ces cas.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@14383 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.DataLayer.UnitTests/General/UnitTestForeignEntities.cs
@@ -0,0 +1,499 @@
+﻿using Epsitec.Common.UnitTesting;
+
+using Epsitec.Cresus.Database;
+
+using Epsitec.Cresus.DataLayer.Context;
+using Epsitec.Cresus.DataLayer.Loader;
+using Epsitec.Cresus.DataLayer.UnitTests.Entities;
+using Epsitec.Cresus.DataLayer.UnitTests.Helpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Epsitec.Cresus.DataLayer.UnitTests.General
+{
+
+
+	[TestClass]
+	public sealed class UnitTestForeignEntities
+	{
+
+
+		[ClassInitialize]
+		public static void ClassInitialize(TestContext testContext)
+		{
+			TestHelper.Initialize ();
+
+			DatabaseHelper.CreateAndConnectToDatabase ();
+
+			using (DataContext dataContext = new DataContext (DatabaseHelper.DbInfrastructure))
+			{
+				DatabaseCreator2.PupulateDatabase (dataContext);
+			}
+
+			DatabaseHelper.DisconnectFromDatabase ();
+		}
+
+
+		[TestMethod]
+		public void EntityInReference()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					NaturalPersonEntity person1 = dataContext1.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+					PersonGenderEntity gender2 = dataContext2.ResolveEntity<PersonGenderEntity> (new DbKey (new DbId (2)));
+
+					// TODO One of those calls should throw an exception.
+					// Marc
+
+					person1.Gender = gender2;
+
+					dataContext1.SaveChanges ();
+				}
+			}
+
+			Assert.Inconclusive ();
+		}
+
+
+		[TestMethod]
+		public void EntityCollection()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					NaturalPersonEntity person1 = dataContext1.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+					UriContactEntity contact2 = dataContext2.ResolveEntity<UriContactEntity> (new DbKey (new DbId (4)));
+
+					// TODO One of those calls should throw an exception.
+					// Marc
+
+					person1.Contacts.Add (contact2);
+					
+					dataContext1.SaveChanges ();
+				}
+			}
+
+			Assert.Inconclusive ();
+		}
+
+		
+		[TestMethod]
+		public void GetByRequest1()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					// TODO One of those calls should throw an exception.
+					// Marc
+
+					NaturalPersonEntity person = new NaturalPersonEntity ();
+
+					person.Contacts.Add (dataContext1.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1))));
+
+					Request request = new Request ()
+					{
+						RootEntity = person,
+					};
+
+					dataContext2.GetByRequest<NaturalPersonEntity> (request);
+				}
+			}
+
+			Assert.Inconclusive ();
+		}
+
+
+		[TestMethod]
+		public void GetByRequest2()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					AbstractContactEntity contact1 = dataContext1.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1)));
+
+					Request request = new Request ()
+					{
+						RequestedEntity = contact1,
+					};
+
+					ExceptionAssert.Throw<System.ArgumentException>
+					(
+						() => dataContext2.GetByRequest<NaturalPersonEntity> (request)
+					);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void GetByRequest3()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					AbstractContactEntity contact1 = dataContext1.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1)));
+					Request request = new Request ()
+					{
+						RootEntity = contact1,
+					};
+
+					ExceptionAssert.Throw<System.ArgumentException>
+					(
+						() => dataContext2.GetByRequest<NaturalPersonEntity> (request)
+					);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void GetByExample1()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					// TODO One of those calls should throw an exception.
+					// Marc
+
+					AbstractContactEntity contact = new AbstractContactEntity ()
+					{
+						NaturalPerson = dataContext1.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1))),
+					};
+
+					dataContext2.GetByExample (contact);
+				}
+			}
+
+			Assert.Inconclusive ();
+		}
+
+
+		[TestMethod]
+		public void GetByExample2()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					NaturalPersonEntity	person1 = dataContext1.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+
+					ExceptionAssert.Throw<System.ArgumentException>
+					(
+						() => dataContext2.GetByExample (person1)
+					);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void DeleteEntity()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					NaturalPersonEntity person1 = dataContext1.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+
+					ExceptionAssert.Throw<System.ArgumentException>
+					(
+						() => dataContext2.DeleteEntity (person1)
+					);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void IsDeleted()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					NaturalPersonEntity person1 = dataContext1.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+
+					ExceptionAssert.Throw<System.ArgumentException>
+					(
+						() => dataContext2.IsDeleted (person1)
+					);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void GetLeafEntityKey()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					NaturalPersonEntity person1 = dataContext1.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+
+					ExceptionAssert.Throw<System.ArgumentException>
+					(
+						() => dataContext2.GetLeafEntityKey (person1)
+					);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void GetNormalizedEntityKey()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					NaturalPersonEntity person1 = dataContext1.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+
+					ExceptionAssert.Throw<System.ArgumentException>
+					(
+						() => dataContext2.GetNormalizedEntityKey (person1)
+					);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void GetPersistedId()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					NaturalPersonEntity person1 = dataContext1.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+
+					ExceptionAssert.Throw<System.ArgumentException>
+					(
+						() => dataContext2.GetPersistedId (person1)
+					);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void IsPersistent()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					NaturalPersonEntity person1 = dataContext1.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+
+					ExceptionAssert.Throw<System.ArgumentException>
+					(
+						() => dataContext2.IsPersistent (person1)
+					);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void IsRegisteredAsEmptyEntity()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					NaturalPersonEntity person1 = dataContext1.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+
+					ExceptionAssert.Throw<System.ArgumentException>
+					(
+						() => dataContext2.IsRegisteredAsEmptyEntity (person1)
+					);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void RegisterEmptyEntity()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					NaturalPersonEntity person1 = dataContext1.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+
+					ExceptionAssert.Throw<System.ArgumentException>
+					(
+						() => dataContext2.RegisterEmptyEntity (person1)
+					);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void UnregisterEmptyEntity()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					NaturalPersonEntity person1 = dataContext1.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+
+					ExceptionAssert.Throw<System.ArgumentException>
+					(
+						() => dataContext2.UnregisterEmptyEntity (person1)
+					);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void UpdateEmptyEntityStatus()
+		{
+			using (DbInfrastructure dbInfrastructure1 = new DbInfrastructure ())
+			using (DbInfrastructure dbInfrastructure2 = new DbInfrastructure ())
+			{
+				DbAccess access = TestHelper.CreateDbAccess ();
+
+				dbInfrastructure1.AttachToDatabase (access);
+				dbInfrastructure2.AttachToDatabase (access);
+
+				using (DataContext dataContext1 = new DataContext (dbInfrastructure1))
+				using (DataContext dataContext2 = new DataContext (dbInfrastructure2))
+				{
+					NaturalPersonEntity person1 = dataContext1.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+
+					ExceptionAssert.Throw<System.ArgumentException>
+					(
+						() => dataContext2.UpdateEmptyEntityStatus (person1, false)
+					);
+				}
+			}
+		}
+
+
+	}
+
+
+}
