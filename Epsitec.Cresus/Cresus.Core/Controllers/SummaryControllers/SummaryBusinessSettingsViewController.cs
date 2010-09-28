37e11fc5db0f5d4eeb31b920007f51bd0b90657a
Daniel Roux <roux@epsitec.ch>
2010-09-28 12:59:57 +0000

    - Cresus.Core: SummaryBusinessSettingsViewController: Embryon de tuile résumée pour ne plus planter lorsqu'on sélectionne la base 'BusinessSettings'.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@14435 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Controllers/SummaryControllers/SummaryBusinessSettingsViewController.cs
@@ -0,0 +1,41 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Core.Widgets;
+using Epsitec.Cresus.Core.Controllers;
+using Epsitec.Cresus.Core.Controllers.DataAccessors;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Controllers.SummaryControllers
+{
+	public class SummaryBusinessSettingsViewController : SummaryViewController<Entities.BusinessSettingsEntity>
+	{
+		public SummaryBusinessSettingsViewController(string name, Entities.BusinessSettingsEntity entity)
+			: base (name, entity)
+		{
+		}
+
+
+		protected override void CreateUI()
+		{
+			using (var data = TileContainerController.Setup (this))
+			{
+				data.Add (
+					new SummaryData
+					{
+						Name				= "BusinessSettings",
+						IconUri				= "Data.BusinessSettings",
+						Title				= TextFormatter.FormatText ("Réglages de l'entreprise"),
+						CompactTitle		= TextFormatter.FormatText ("Réglages de l'entreprise"),
+						TextAccessor		= this.CreateAccessor (x => TextFormatter.FormatText ("Réglages")),
+						CompactTextAccessor = this.CreateAccessor (x => TextFormatter.FormatText ("Réglages")),
+						EntityMarshaler		= this.CreateEntityMarshaler (),
+					});
+			}
+		}
+	}
+}
