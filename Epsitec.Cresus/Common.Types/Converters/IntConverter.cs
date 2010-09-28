78915f0d4f5abec3b868794888a063cf4c470ee6
Daniel Roux <roux@epsitec.ch>
2010-09-28 17:23:11 +0000

    - Cresus.Core: IntConverter: Nouveau convertisseur. - Cresus.Core: EditionPaymentReminderDefinitionViewController: Nouvelle tuile.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@14438 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/Converters/IntConverter.cs
@@ -0,0 +1,64 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Common.Types.Converters
+{
+	/// <summary>
+	/// The <c>IntConverter</c> class does a transparent conversion;
+	/// it is required so that the <see cref="Marshaler"/> can work with
+	/// <c>int</c> values.
+	/// </summary>
+	public class IntConverter : GenericConverter<int>
+	{
+		public override string ConvertToString(int value)
+		{
+			return value.ToString ();
+		}
+
+		public override ConversionResult<int> ConvertFromString(string text)
+		{
+			if (string.IsNullOrWhiteSpace (text))
+			{
+				return new ConversionResult<int>
+				{
+					IsNull = true
+				};
+			}
+
+			int result;
+
+			if (int.TryParse (text, out result))
+			{
+				return new ConversionResult<int>
+				{
+					IsNull = false,
+					Value = result,
+				};
+			}
+			else
+			{
+				return new ConversionResult<int>
+				{
+					IsInvalid = true,
+				};
+			}
+		}
+
+		public override bool CanConvertFromString(string text)
+		{
+			int result;
+
+			if (int.TryParse (text, out result))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
