4bcd86775002850ecbc4293416546cbbc7f6d031
Pierre Arnaud <arnaud@opac.ch>
2010-10-06 12:33:30 +0000

    - Ajouté le projet Cresus.WorkflowDesigner.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@14534 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.WorkflowDesigner/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿//	Copyright © 2010, EPSITEC SA, 1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle ("Epsitec.Cresus.WorkflowDesigner")]
+[assembly: AssemblyDescription ("Crésus Workflow Designer")]
+[assembly: AssemblyProduct ("Crésus")]
+[assembly: AssemblyCopyright ("Copyright © 2010, EPSITEC SA, 1400 Yverdon-les-Bains, Switzerland")]
