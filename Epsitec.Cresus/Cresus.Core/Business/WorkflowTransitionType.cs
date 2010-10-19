98cbae7b153d3c34bfc7780968eafac33dbe1d22
Marc Bettex <bettex@epsitec.ch>
2010-10-19 11:38:58 +0000

    - Complété le schéma des entités liées aux Workflows.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@14604 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Business/WorkflowTransitionType.cs
@@ -0,0 +1,16 @@
+//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
+
+namespace Epsitec.Cresus.Core.Business
+{
+    [DesignerVisible]
+    public enum WorkflowTransitionType
+    {
+        Default,
+
+        Fork,
+        Call,
+    }
+}
