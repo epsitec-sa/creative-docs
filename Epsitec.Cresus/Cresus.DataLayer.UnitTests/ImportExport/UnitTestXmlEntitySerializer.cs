c141ff7513f6fd623bcd3da684167d17d7e8dca4
Marc Bettex <bettex@epsitec.ch>
2010-11-02 15:04:22 +0000

    - Ajouté le namespace DataLayer.ImportExport qui va contenir tout ce qui a trait à l'import et à l'export de données. - Implémenté XmlEntitySerializer qui permet de sérializer et de déserializer des entités au format xml. - Ajouté UnitTestXmlEntitySerializer pour tester cette nouvelle classe.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@14697 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.DataLayer.UnitTests/ImportExport/UnitTestXmlEntitySerializer.cs
@@ -0,0 +1,265 @@
+﻿using Epsitec.Common.Support.EntityEngine;
+
+using Epsitec.Cresus.Database;
+
+using Epsitec.Cresus.DataLayer.Context;
+using Epsitec.Cresus.DataLayer.ImportExport;
+using Epsitec.Cresus.DataLayer.Infrastructure;
+using Epsitec.Cresus.DataLayer.UnitTests.Entities;
+using Epsitec.Cresus.DataLayer.UnitTests.Helpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Xml.Linq;
+
+
+namespace Epsitec.Cresus.DataLayer.UnitTests.ImportExport
+{
+
+
+	[TestClass]
+	public class UnitTestXmlEntitySerializer
+	{
+
+
+		[ClassInitialize]
+		public static void ClassInitialize(TestContext testContext)
+		{
+			TestHelper.Initialize ();
+		}
+
+
+		[ClassCleanup]
+		public static void ClassCleanup()
+		{
+			DatabaseHelper.DisconnectFromDatabase ();
+		}
+
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			DatabaseHelper.CreateAndConnectToDatabase ();
+
+			Assert.IsTrue (DatabaseHelper.DbInfrastructure.IsConnectionOpen);
+
+			using (DataInfrastructure dataInfrastructure = new DataInfrastructure (DatabaseHelper.DbInfrastructure))
+			{
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					DatabaseCreator2.PupulateDatabase (dataContext);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void TestAllExported()
+		{
+			XElement xEntities;
+
+			using (DataInfrastructure dataInfrastructure = new DataInfrastructure (DatabaseHelper.DbInfrastructure))
+			{
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					HashSet<AbstractEntity> entities = new HashSet<AbstractEntity> ();
+
+					entities.Add (dataContext.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1000000001))));
+					entities.Add (dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1000000001))));
+					entities.Add (dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1000000002))));
+					entities.Add (dataContext.ResolveEntity<UriSchemeEntity> (new DbKey (new DbId (1000000001))));
+					entities.Add (dataContext.ResolveEntity<PersonGenderEntity> (new DbKey (new DbId (1000000001))));
+					entities.Add (dataContext.ResolveEntity<LanguageEntity> (new DbKey (new DbId (1000000001))));
+
+					xEntities = XmlEntitySerializer.Serialize (dataContext, entities);
+				}
+			}
+
+			DatabaseHelper.CreateAndConnectToDatabase ();
+			
+			using (DataInfrastructure dataInfrastructure = new DataInfrastructure (DatabaseHelper.DbInfrastructure))
+			{
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					dataContext.CreateSchema<NaturalPersonEntity> ();
+					dataContext.CreateSchema<MailContactEntity> ();
+					dataContext.CreateSchema<TelecomContactEntity> ();
+					dataContext.CreateSchema<UriContactEntity> ();
+				}
+
+				XmlEntitySerializer.Deserialize (dataInfrastructure, xEntities);
+
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					NaturalPersonEntity alfred = dataContext.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1000000001)));
+
+					Assert.IsTrue (DatabaseCreator2.CheckAlfred (alfred));			
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void TestNotAllExported()
+		{
+			XElement xEntities;
+
+			using (DataInfrastructure dataInfrastructure = new DataInfrastructure (DatabaseHelper.DbInfrastructure))
+			{
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					HashSet<AbstractEntity> entities = new HashSet<AbstractEntity> ();
+
+					UriContactEntity uriContact = dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1000000004)));
+					
+					entities.Add (uriContact);
+
+					xEntities = XmlEntitySerializer.Serialize (dataContext, entities);
+
+					dataContext.DeleteEntity (uriContact);
+
+					dataContext.SaveChanges ();
+				}
+
+				XmlEntitySerializer.Deserialize (dataInfrastructure, xEntities);
+
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					UriContactEntity uriContact = dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1000000005)));
+					UriSchemeEntity uriScheme = dataContext.ResolveEntity<UriSchemeEntity> (new DbKey (new DbId (1000000001)));
+
+					Assert.AreEqual (uriScheme, uriContact.UriScheme);
+					Assert.AreEqual ("nobody@nowhere.com", uriContact.Uri);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void TestInwardBrokenRelations()
+		{
+			XElement xEntities;
+
+			using (DataInfrastructure dataInfrastructure = new DataInfrastructure (DatabaseHelper.DbInfrastructure))
+			{
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					HashSet<AbstractEntity> entities = new HashSet<AbstractEntity> ();
+
+					UriSchemeEntity uriScheme = dataContext.ResolveEntity<UriSchemeEntity> (new DbKey (new DbId (1000000001)));
+
+					entities.Add (uriScheme);
+
+					xEntities = XmlEntitySerializer.Serialize (dataContext, entities);
+
+					dataContext.DeleteEntity (uriScheme);
+
+					dataContext.SaveChanges ();
+				}
+
+				XmlEntitySerializer.Deserialize (dataInfrastructure, xEntities);
+
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					UriSchemeEntity uriScheme = dataContext.ResolveEntity<UriSchemeEntity> (new DbKey (new DbId (1000000002)));
+
+					Assert.AreEqual ("mailto:", uriScheme.Code);
+					Assert.AreEqual ("email", uriScheme.Name);
+
+					List<UriContactEntity> uriContacts = new List<UriContactEntity> ()
+					{
+						dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1000000001))),
+						dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1000000002))),
+						dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1000000003))),
+						dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1000000004))),
+					};
+
+					foreach (UriContactEntity uriContact in uriContacts)
+					{
+						Assert.IsNull (uriContact.UriScheme);
+					}
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void TestOutwardBrokenReference()
+		{
+			XElement xEntities;
+
+			using (DataInfrastructure dataInfrastructure = new DataInfrastructure (DatabaseHelper.DbInfrastructure))
+			{
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					HashSet<AbstractEntity> entities = new HashSet<AbstractEntity> ();
+
+					UriContactEntity uriContact = dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1000000004)));
+					UriSchemeEntity uriScheme = dataContext.ResolveEntity<UriSchemeEntity> (new DbKey (new DbId (1000000001)));
+
+					entities.Add (uriContact);
+
+					xEntities = XmlEntitySerializer.Serialize (dataContext, entities);
+
+					dataContext.DeleteEntity (uriScheme);
+
+					dataContext.SaveChanges ();
+				}
+
+				XmlEntitySerializer.Deserialize (dataInfrastructure, xEntities);
+
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					UriContactEntity uriContact = dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1000000005)));
+
+					Assert.IsNull (uriContact.UriScheme);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void TestOutwardBrokenCollection()
+		{
+			XElement xEntities;
+
+			using (DataInfrastructure dataInfrastructure = new DataInfrastructure (DatabaseHelper.DbInfrastructure))
+			{
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					HashSet<AbstractEntity> entities = new HashSet<AbstractEntity> ();
+
+					NaturalPersonEntity person = dataContext.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1000000001)));
+					UriContactEntity uriContact = dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1000000001)));
+
+					entities.Add (person);
+
+					xEntities = XmlEntitySerializer.Serialize (dataContext, entities);
+
+					dataContext.DeleteEntity (uriContact);
+
+					dataContext.SaveChanges ();
+				}
+
+				XmlEntitySerializer.Deserialize (dataInfrastructure, xEntities);
+
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					NaturalPersonEntity person = dataContext.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1000000004)));
+					UriContactEntity uriContact = dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1000000002)));
+
+					Assert.AreEqual (1, person.Contacts.Count);
+					Assert.AreEqual (uriContact, person.Contacts[0]);
+				}
+			}
+		}
+
+
+	}
+
+
+}
