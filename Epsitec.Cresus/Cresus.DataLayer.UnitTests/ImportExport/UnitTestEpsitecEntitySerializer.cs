b5208f69453421038548038d63f842fba0f3a0f6
Marc Bettex <bettex@epsitec.ch>
2010-11-08 15:11:33 +0000

    - Implémenté les fonctionalités liées à l'import/export des entités epsitec.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@14731 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.DataLayer.UnitTests/ImportExport/UnitTestEpsitecEntitySerializer.cs
@@ -0,0 +1,109 @@
+﻿using Epsitec.Cresus.Database;
+
+using Epsitec.Cresus.DataLayer.Context;
+using Epsitec.Cresus.DataLayer.ImportExport;
+using Epsitec.Cresus.DataLayer.Infrastructure;
+using Epsitec.Cresus.DataLayer.UnitTests.Entities;
+using Epsitec.Cresus.DataLayer.UnitTests.Helpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.IO;
+
+
+namespace Epsitec.Cresus.DataLayer.UnitTests.ImportExport
+{
+
+
+	[TestClass]
+	public sealed class UnitTestEpsitecEntitySerializer
+	{
+
+
+		[ClassInitialize]
+		public static void ClassInitialize(TestContext testContext)
+		{
+			TestHelper.Initialize ();
+		}
+
+
+		[ClassCleanup]
+		public static void ClassCleanup()
+		{
+			DatabaseHelper.DisconnectFromDatabase ();
+		}
+
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			DatabaseHelper.CreateAndConnectToDatabase ();
+
+			Assert.IsTrue (DatabaseHelper.DbInfrastructure.IsConnectionOpen);
+
+			using (DataInfrastructure dataInfrastructure = new DataInfrastructure (DatabaseHelper.DbInfrastructure))
+			{
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					DatabaseCreator2.PupulateDatabase (dataContext);
+				}
+			}
+		}
+
+
+		[TestMethod]
+		public void SimpleExportImport()
+		{
+			FileInfo file = new FileInfo ("test.xml");
+
+			EpsitecEntitySerializer.Export (file, DatabaseHelper.DbInfrastructure);
+			EpsitecEntitySerializer.Import (file, DatabaseHelper.DbInfrastructure);
+
+			using (DataInfrastructure dataInfrastructure = new DataInfrastructure(DatabaseHelper.DbInfrastructure))
+			{
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					NaturalPersonEntity alfred = dataContext.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1)));
+					NaturalPersonEntity gertrude = dataContext.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (2)));
+					NaturalPersonEntity hans = dataContext.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (3)));
+
+					Assert.IsTrue (DatabaseCreator2.CheckAlfred (alfred));
+					Assert.IsTrue (DatabaseCreator2.CheckGertrude (gertrude));
+					Assert.IsTrue (DatabaseCreator2.CheckHans (hans));
+				}
+			}
+
+			EpsitecEntitySerializer.CleanDatabase (file, DatabaseHelper.DbInfrastructure);
+
+			using (DataInfrastructure dataInfrastructure = new DataInfrastructure (DatabaseHelper.DbInfrastructure))
+			{
+				using (DataContext dataContext = dataInfrastructure.CreateDataContext ())
+				{
+					Assert.IsNull (dataContext.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (1))));
+					Assert.IsNull (dataContext.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (2))));
+					Assert.IsNull (dataContext.ResolveEntity<NaturalPersonEntity> (new DbKey (new DbId (3))));
+
+					Assert.IsNull (dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (1))));
+					Assert.IsNull (dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (2))));
+					Assert.IsNull (dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (3))));
+					Assert.IsNull (dataContext.ResolveEntity<UriContactEntity> (new DbKey (new DbId (4))));
+
+					Assert.IsNull (dataContext.ResolveEntity<UriSchemeEntity> (new DbKey (new DbId (1))));
+
+					Assert.IsNull (dataContext.ResolveEntity<PersonGenderEntity> (new DbKey (new DbId (1))));
+					Assert.IsNull (dataContext.ResolveEntity<PersonGenderEntity> (new DbKey (new DbId (2))));
+
+					Assert.IsNull (dataContext.ResolveEntity<PersonTitleEntity> (new DbKey (new DbId (1))));
+					Assert.IsNull (dataContext.ResolveEntity<PersonTitleEntity> (new DbKey (new DbId (2))));
+
+					Assert.IsNull (dataContext.ResolveEntity<LanguageEntity> (new DbKey (new DbId (1))));
+					Assert.IsNull (dataContext.ResolveEntity<LanguageEntity> (new DbKey (new DbId (2))));
+				}
+			}
+		}
+
+
+	}
+
+
+}
---
+++ Epsitec.Cresus/Cresus.DataLayer/ImportExport/TableCategory.cs
@@ -0,0 +1,22 @@
+namespace Epsitec.Cresus.DataLayer.ImportExport
+{
+
+
+	// TODO Comment this enum.
+	// Marc
+
+
+	internal enum TableCategory
+	{
+
+
+		Data,
+
+
+		Relation,
+
+
+	}
+
+
+}
