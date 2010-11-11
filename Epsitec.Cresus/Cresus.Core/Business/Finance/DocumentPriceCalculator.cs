2c4aeb104c26dd727975d499fd044355b7cd6db1
Pierre Arnaud <arnaud@opac.ch>
2010-11-11 07:20:04 +0000

    - Calculateur de prix: premiers pas en direction d'une gestion des totaux.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@14773 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Business/Finance/DocumentPriceCalculator.cs
@@ -0,0 +1,50 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Support.Extensions;
+using Epsitec.Common.Types;
+using Epsitec.Common.Types.Collections;
+
+using Epsitec.Cresus.Core.Entities;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Business.Finance
+{
+	public class DocumentPriceCalculator
+	{
+		public DocumentPriceCalculator(BusinessDocumentEntity document)
+		{
+			this.document = document;
+		}
+
+
+		public void Update()
+		{
+			var articles = this.GetArticles ().ToList ();
+			var totals   = this.GetTotals ().ToList ();
+			
+			
+		}
+
+		private IEnumerable<ArticlePriceCalculator> GetArticles()
+		{
+			return from line in this.document.Lines.OfType<ArticleDocumentItemEntity> ()
+				   orderby line.GroupIndex
+				   select new ArticlePriceCalculator (this.document, line);
+		}
+
+		private IEnumerable<TotalPriceCalculator> GetTotals()
+		{
+			return this.document.Lines
+				.OfType<TotalDocumentItemEntity> ()
+				.Select ((line, index) => new { Line = line, Index = index })
+				.OrderBy (x => x.Line.GroupIndex)
+				.ThenBy (x => x.Index)
+				.Select (x => new TotalPriceCalculator (this.document, x.Line));
+		}
+		
+		private readonly BusinessDocumentEntity		document;
+	}
+}
---
+++ Epsitec.Cresus/Cresus.Core/Business/Finance/PriceCalculator.cs
@@ -0,0 +1,52 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Business.Finance
+{
+	public class PriceCalculator
+	{
+		public static decimal ClipPriceValue(decimal value, CurrencyCode currency)
+		{
+			//	Currently, keep only cents for any price value. Maybe we should consider
+			//	using 3 decimal places after the EUR...
+
+			return 0.01M * System.Math.Round (value * 100);
+		}
+		
+		public static decimal? ClipPriceValue(decimal? value, CurrencyCode currency)
+		{
+			if (value.HasValue)
+			{
+				return PriceCalculator.ClipPriceValue (value.Value, currency);
+			}
+			else
+			{
+				return null;
+			}
+		}
+		
+		public static decimal ClipTaxRateValue(decimal value)
+		{
+			//	The tax rate will be expressed as 8.000000 % (six digits after the decimal point)
+			//	in order to limit the rounding errors when the VAT rate is the result of some
+			//	composition of two or more other rates.
+
+			return 0.01M * 0.000001M * System.Math.Round (value * 100 * 1000000);
+		}
+
+		public static decimal? ClipTaxRateValue(decimal? value)
+		{
+			if (value.HasValue)
+			{
+				return PriceCalculator.ClipTaxRateValue (value.Value);
+			}
+			else
+			{
+				return null;
+			}
+		}
+	}
+}
