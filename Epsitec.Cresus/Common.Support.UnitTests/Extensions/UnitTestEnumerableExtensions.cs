3fd1f128615d222f10f399666e1afc541e514067
Marc Bettex <bettex@epsitec.ch>
2010-11-11 11:12:36 +0000

    - Ajout d'une méthode dans EnumerableExtentions. - Ajout d'un test pour cette méthode.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@14781 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Support.UnitTests/Extensions/UnitTestEnumerableExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Epsitec.Common.Support.Extensions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+
+namespace Epsitec.Common.Support.UnitTests.Extensions
+{
+
+
+	[TestClass]
+	public sealed  class UnitTestEnumerableExtensions
+	{
+
+
+		[TestMethod]
+		public void AppendTest()
+		{
+			Assert.IsTrue (this.GetSequence (11).SequenceEqual (this.GetSequence (10).Append (10)));
+			Assert.IsTrue (this.GetSequence (10).SequenceEqual (this.GetSequence (5).Append(5).Concat (this.GetSequence (10).Skip (6))));
+
+			Assert.IsTrue (this.GetSequence (10).SequenceEqual (this.GetSequence (5).Append (5, 6, 7, 8, 9)));
+			Assert.IsTrue (this.GetSequence (10).SequenceEqual (this.GetSequence (5).Append (5, 6).Concat (this.GetSequence (10).Skip (7))));
+		}
+
+
+		private IEnumerable<int> GetSequence(int length)
+		{
+			List<int> list = new List<int> ();
+
+			for (int i = 0; i < length; i++)
+			{
+				list.Add (i);
+			}
+
+			return list;
+		}
+            
+
+	}
+
+
+}
