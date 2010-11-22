b1bedb8464f871a588e81edffb8ceac7e6c1f256
Marc Bettex <bettex@epsitec.ch>
2010-11-22 09:41:03 +0000

    - Déplacé deux méthodes de DbLogger dans DbAbstractAttachable. - Commenté ces deux méthodes, DbLogger et DbLogEntry.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@14928 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Database/Services/DbLogEntry.cs
+++ Epsitec.Cresus/Cresus.Database/Services/DbLogEntry.cs
@@ -2,11 +2,22 @@ namespace Epsitec.Cresus.Database.Services
 {
 	
 	
+	/// <summary>
+	/// The <c>DbLogEntry</c> class is an immutable object that represents an entry in the log
+	/// table in the database.
+	/// </summary>
 	public sealed class DbLogEntry
 	{
 		
 		
-		public DbLogEntry(DbId entryId, DbId connectionId, System.DateTime dateTime, long sequenceNumber)
+		/// <summary>
+		/// Builds a new <c>DbLogEntry</c>.
+		/// </summary>
+		/// <param name="entryId">The <see cref="DbId"/> of the entry.</param>
+		/// <param name="connectionId">The connection <see cref="DbId"/> of the entry.</param>
+		/// <param name="dateTime">The time at which the entry was created.</param>
+		/// <param name="sequenceNumber">The ordered sequence number of the entry.</param>
+		internal DbLogEntry(DbId entryId, DbId connectionId, System.DateTime dateTime, long sequenceNumber)
 		{
 			this.EntryId = entryId;
 			this.ConnectionId = connectionId;
@@ -15,6 +26,9 @@ namespace Epsitec.Cresus.Database.Services
 		}
 
 
+		/// <summary>
+		/// Gets the <see cref="DbId"/> of the entry represented by this instance.
+		/// </summary>
 		public DbId EntryId
 		{
 			get;
@@ -22,6 +36,10 @@ namespace Epsitec.Cresus.Database.Services
 		}
 
 
+		/// <summary>
+		/// Gets the <see cref="DbId"/> of the connection referenced by the entry represented by this
+		/// instance.
+		/// </summary>
 		public DbId ConnectionId
 		{
 			get;
@@ -29,6 +47,9 @@ namespace Epsitec.Cresus.Database.Services
 		}
 
 
+		/// <summary>
+		/// Gets the time at which the entry represented by this instance has been created.
+		/// </summary>
 		public System.DateTime DateTime
 		{
 			get;
@@ -36,6 +57,9 @@ namespace Epsitec.Cresus.Database.Services
 		}
 
 
+		/// <summary>
+		/// Gets the unique ordered sequence number of the entry represented by this instance.
+		/// </summary>
 		public long SequenceNumber
 		{
 			get;
--- Epsitec.Cresus/Cresus.Database/Services/DbLogger.cs
+++ Epsitec.Cresus/Cresus.Database/Services/DbLogger.cs
@@ -2,8 +2,6 @@ using Epsitec.Cresus.Database.Collections;
 
 using System.Collections.Generic;
 
-using System.Data;
-
 using System.Linq;
 
 
@@ -11,15 +9,27 @@ namespace Epsitec.Cresus.Database.Services
 {
 	
 	
+	/// <summary>
+	/// The <c>DbLogger</c> class is used to manage the <c>CR_LOG</c> table in the database, that
+	/// contains the log entries used to archive who has done what and when in the database.
+	/// </summary>
 	public sealed class DbLogger : DbAbstractAttachable
 	{
 		
 		
+		/// <summary>
+		/// Creates a new <c>DbLogger</c>.
+		/// </summary>
 		internal DbLogger() : base()
 		{
 		}
 
 
+		/// <summary>
+		/// Inserts a fresh new log entry in the database.
+		/// </summary>
+		/// <param name="connectionId">The <see cref="DbId"/> of the connection referenced by the new entry.</param>
+		/// <returns>The data of the new entry.</returns>
 		public DbLogEntry CreateLogEntry(DbId connectionId)
 		{
 			this.CheckIsAttached ();
@@ -31,10 +41,15 @@ namespace Epsitec.Cresus.Database.Services
 
 			IList<object> data = this.AddRow (columnNamesToValues);
 
-			return this.CreateLogEntry (data);
+			return this.CreateDbLogEntry (data);
 		}
 
 
+		/// <summary>
+		/// Gets a log entry in the database.
+		/// </summary>
+		/// <param name="entryId">The <see cref="DbId"/> of the entry to get.</param>
+		/// <returns>The data of the entry.</returns>
 		public DbLogEntry GetLogEntry(DbId entryId)
 		{
 			this.CheckIsAttached ();
@@ -46,15 +61,19 @@ namespace Epsitec.Cresus.Database.Services
 					this.CreateConditionForEntryId (entryId),
 				};
 
-				var data = this.GetRowValues (conditions);
+				var data = this.GetRows (conditions);
 
 				transaction.Commit ();
 
-				return data.Any () ? this.CreateLogEntry (data.First ()) : null;
+				return data.Any () ? this.CreateDbLogEntry (data.First ()) : null;
 			}
 		}
 
 
+		/// <summary>
+		/// Removes a log entry from the database.
+		/// </summary>
+		/// <param name="entryId">The <see cref="DbId"/> of the entry to remove.</param>
 		public void RemoveLogEntry(DbId entryId)
 		{
 			this.CheckIsAttached ();
@@ -68,6 +87,11 @@ namespace Epsitec.Cresus.Database.Services
 		}
 
 
+		/// <summary>
+		/// Tells whether a log entry exists in the database.
+		/// </summary>
+		/// <param name="entryId">The <see cref="DbId"/> of the entry whose existence to check.</param>
+		/// <returns><c>true</c> if a log entry with the given <see cref="DbId"/> exists, <c>false</c> if there is n't.</returns>
 		public bool LogEntryExists(DbId entryId)
 		{
 			this.CheckIsAttached ();
@@ -81,6 +105,12 @@ namespace Epsitec.Cresus.Database.Services
 		}
 
 
+		/// <summary>
+		/// Creates the <see cref="SqlFunction"/> that represents the condition which holds for a log
+		/// entry when its <see cref="DbId"/> is equal to the given <see cref="DbId"/>.
+		/// </summary>
+		/// <param name="entryId">The <see cref="DbId"/> that the entry must have in order to satisfy the condition.</param>
+		/// <returns>The <see cref="SqlFunction"/> that represents the condition.</returns>
 		private SqlFunction CreateConditionForEntryId(DbId entryId)
 		{
 			return new SqlFunction
@@ -92,7 +122,12 @@ namespace Epsitec.Cresus.Database.Services
 		}
 
 
-		private DbLogEntry CreateLogEntry(IList<object> data)
+		/// <summary>
+		/// Creates a new instance of <see cref="DbLogEntry"/> based on the given data.
+		/// </summary>
+		/// <param name="data">The data of the log entry.</param>
+		/// <returns>The <see cref="DbLogEntry"/>.</returns>
+		private DbLogEntry CreateDbLogEntry(IList<object> data)
 		{
 			DbId entryId = new DbId ((long) data[0]);
 			DbId connectionId = new DbId ((long) data[1]);
@@ -100,77 +135,6 @@ namespace Epsitec.Cresus.Database.Services
 			long sequenceNumber = (long) data[3];
 
 			return new DbLogEntry (entryId, connectionId, dateTime, sequenceNumber);
-		}
-
-
-		// TODO Move these two functions in DbAbstractAttachable (and merge them with other)?
-		// Marc
-		
-		private IList<object> AddRow(IDictionary<string, object> columnNamesToValues)
-		{
-			using (DbTransaction transaction = this.DbInfrastructure.InheritOrBeginTransaction (DbTransactionMode.ReadWrite))
-			{
-				SqlFieldList fieldsToInsert = new SqlFieldList ();
-				SqlFieldList fieldsToReturn = new SqlFieldList ();
-
-				foreach (var item in columnNamesToValues)
-				{
-					DbColumn dbColumn = this.DbTable.Columns[item.Key];
-					SqlField sqlField = this.DbInfrastructure.CreateSqlFieldFromAdoValue (dbColumn, item.Value);
-
-					fieldsToInsert.Add (sqlField);
-				}
-
-				foreach (DbColumn dbColumn in this.DbTable.Columns)
-				{
-					SqlField sqlField = new SqlField ()
-					{
-						Alias = dbColumn.GetSqlName ()
-					};
-
-					fieldsToReturn.Add (sqlField);
-				}
-
-				transaction.SqlBuilder.InsertData (this.DbTable.GetSqlName (), fieldsToInsert, fieldsToReturn);
-
-				IList<object> outputParameters = this.DbInfrastructure.ExecuteOutputParameters (transaction);
-
-				transaction.Commit ();
-
-				return outputParameters;
-			}
-		}
-
-
-		private IList<IList<object>> GetRowValues(SqlFieldList conditions)
-		{
-			using (DbTransaction transaction = this.DbInfrastructure.InheritOrBeginTransaction (DbTransactionMode.ReadOnly))
-			{
-				SqlSelect query = new SqlSelect ();
-
-				query.Tables.Add (SqlField.CreateName (this.DbTable.GetSqlName ()));
-
-				foreach (DbColumn dbColumn in this.DbTable.Columns)
-				{
-					query.Fields.Add (SqlField.CreateName (dbColumn.GetSqlName ()));
-				}
-				query.Conditions.AddRange (conditions);
-
-				transaction.SqlBuilder.SelectData (query);
-
-				DataTable table = this.DbInfrastructure.ExecuteSqlSelect (transaction, query, 0);
-
-				List<IList<object>> data = new List<IList<object>> ();
-
-				transaction.Commit ();
-
-				foreach (DataRow row in table.Rows)
-				{
-					data.Add (row.ItemArray.ToList ());
-				}
-
-				return data;
-			}
 		}
                 
 		
