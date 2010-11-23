358b190ed1fd8a14594d319f11ece630eb346278
Daniel Roux <roux@epsitec.ch>
2010-11-23 11:25:03 +0000

    - Cresus.Core: Toutes les nouvelles tuiles pour les groupes et les catégories d'images.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@14940 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Controllers/SummaryControllers/SummaryImageGroupListViewController.cs
@@ -0,0 +1,57 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using Epsitec.Common.Support.EntityEngine;
+using Epsitec.Common.Types;
+using Epsitec.Common.Types.Converters;
+
+using Epsitec.Cresus.Core.Widgets;
+using Epsitec.Cresus.Core.Entities;
+using Epsitec.Cresus.Core.Controllers;
+using Epsitec.Cresus.Core.Controllers.DataAccessors;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Controllers.SummaryControllers
+{
+	[ControllerSubType (2)]
+	public class SummaryImageGroupListViewController : SummaryViewController<ImageEntity>
+	{
+		public SummaryImageGroupListViewController(string name, ImageEntity entity)
+			: base (name, entity)
+		{
+		}
+
+
+		protected override void CreateUI()
+		{
+			using (var data = TileContainerController.Setup (this))
+			{
+				this.CreateUIImageGroups (data);
+			}
+		}
+
+
+		private void CreateUIImageGroups(SummaryDataItems data)
+		{
+			data.Add (
+				new SummaryData
+				{
+					AutoGroup    = true,
+					Name		 = "ImageGroups",
+					IconUri		 = "Data.ArticleGroup",
+					Title		 = TextFormatter.FormatText ("Tous les groupes d'image connus"),
+					CompactTitle = TextFormatter.FormatText ("Tous les groupes d'image connus"),
+					Text		 = CollectionTemplate.DefaultEmptyText,
+				});
+
+			var template = new CollectionTemplate<ImageGroupEntity> ("ImageGroups", this.BusinessContext);
+
+			template.DefineText        (x => x.GetSummary ());
+			template.DefineCompactText (x => x.GetCompactSummary ());
+
+			data.Add (this.CreateCollectionAccessor (template));
+		}
+	}
+}
