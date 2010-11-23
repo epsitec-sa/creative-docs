65b114b79ae8ede33b712d39bd6a4587c025ee22
Daniel Roux <roux@epsitec.ch>
2010-11-23 11:25:03 +0000

    - Cresus.Core: Toutes les nouvelles tuiles pour les groupes et les catégories d'images.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@14940 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionImageCategoryViewController.cs
@@ -0,0 +1,46 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using Epsitec.Common.Types;
+using Epsitec.Common.Types.Converters;
+
+using Epsitec.Cresus.Core;
+using Epsitec.Cresus.Core.Entities;
+using Epsitec.Cresus.Core.Widgets;
+using Epsitec.Cresus.Core.Widgets.Tiles;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Controllers.EditionControllers
+{
+	public class EditionImageCategoryViewController : EditionViewController<Entities.ImageCategoryEntity>
+	{
+		public EditionImageCategoryViewController(string name, Entities.ImageCategoryEntity entity)
+			: base (name, entity)
+		{
+		}
+
+		protected override void CreateUI()
+		{
+			using (var builder = new UIBuilder (this))
+			{
+				builder.CreateHeaderEditorTile ();
+				builder.CreateEditionTitleTile ("Data.ArticleCategory", "Catégorie");
+
+				this.CreateUIMain (builder);
+
+				builder.CreateFooterEditorTile ();
+			}
+		}
+
+		private void CreateUIMain(UIBuilder builder)
+		{
+			var tile = builder.CreateEditionTile ();
+
+			builder.CreateWarning        (tile);
+			builder.CreateTextField      (tile, 0, "Nom",         Marshaler.Create (() => this.Entity.Name,        x => this.Entity.Name = x));
+			builder.CreateTextFieldMulti (tile, 0, "Description", Marshaler.Create (() => this.Entity.Description, x => this.Entity.Description = x));
+		}
+	}
+}
---
+++ Epsitec.Cresus/Cresus.Core/Controllers/SummaryControllers/SummaryImageCategoryViewController.cs
@@ -0,0 +1,38 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using Epsitec.Cresus.Core.Controllers;
+using Epsitec.Cresus.Core.Controllers.DataAccessors;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Controllers.SummaryControllers
+{
+	public class SummaryImageCategoryViewController : SummaryViewController<Entities.ImageCategoryEntity>
+	{
+		public SummaryImageCategoryViewController(string name, Entities.ImageCategoryEntity entity)
+			: base (name, entity)
+		{
+		}
+
+
+		protected override void CreateUI()
+		{
+			using (var data = TileContainerController.Setup (this))
+			{
+				data.Add (
+					new SummaryData
+					{
+						Name				= "ImageCategory",
+						IconUri				= "Data.ArticleCategory",
+						Title				= TextFormatter.FormatText ("Catégorie"),
+						CompactTitle		= TextFormatter.FormatText ("Catégorie"),
+						TextAccessor		= this.CreateAccessor (x => x.GetSummary ()),
+						CompactTextAccessor = this.CreateAccessor (x => x.GetCompactSummary ()),
+						EntityMarshaler		= this.CreateEntityMarshaler (),
+					});
+			}
+		}
+	}
+}
