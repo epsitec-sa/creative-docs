3bb9569d3300b57b5acb0787f93b24455d57957b
Daniel Roux <roux@epsitec.ch>
2010-11-23 11:15:53 +0000

    - Cresus.Core: SummaryImageViewController: Nouvelle tuile. - Cresus.Core: EditionImageViewController: Nouvelle tuile.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@14939 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Controllers/SummaryControllers/SummaryImageViewController.cs
@@ -0,0 +1,50 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Drawing;
+using Epsitec.Common.Support.EntityEngine;
+using Epsitec.Common.Types;
+using Epsitec.Common.Widgets;
+
+using Epsitec.Cresus.Core.Controllers;
+using Epsitec.Cresus.Core.Controllers.DataAccessors;
+using Epsitec.Cresus.Core.Entities;
+using Epsitec.Cresus.Core.Factories;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Controllers.SummaryControllers
+{
+	public class SummaryImageViewController : SummaryViewController<ImageEntity>
+	{
+		public SummaryImageViewController(string name, ImageEntity entity)
+			: base (name, entity)
+		{
+		}
+
+
+		protected override void CreateUI()
+		{
+			using (var data = TileContainerController.Setup (this))
+			{
+				this.CreateUIImage (data);
+			}
+		}
+
+		private void CreateUIImage(SummaryDataItems data)
+		{
+			data.Add (
+				new SummaryData
+				{
+					Name				= "Image",
+					IconUri				= "Data.Image",
+					Title				= TextFormatter.FormatText ("Image", "(", TextFormatter.Join (", ", this.Entity.ImageGroups.Select (group => group.Name)), ")"),
+					CompactTitle		= TextFormatter.FormatText ("Image"),
+					TextAccessor		= this.CreateAccessor (x => x.GetSummary ()),
+					CompactTextAccessor = this.CreateAccessor (x => x.GetCompactSummary ()),
+					EntityMarshaler		= this.CreateEntityMarshaler (),
+				});
+		}
+	}
+}
---
+++ Epsitec.Cresus/Cresus.Core/Entities/ImageEntity.cs
@@ -0,0 +1,43 @@
+//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
+using Epsitec.Common.Support.EntityEngine;
+using Epsitec.Common.Support.Extensions;
+
+using Epsitec.Cresus.Core.Business.Finance;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Entities
+{
+	public partial class ImageEntity
+	{
+		public override FormattedText GetSummary()
+		{
+			return TextFormatter.FormatText (this.Name);
+		}
+
+		public override FormattedText GetCompactSummary()
+		{
+			return TextFormatter.FormatText (this.Name);
+		}
+
+		public override string[] GetEntityKeywords()
+		{
+			return new string[] { this.Name.ToSimpleText () };
+		}
+
+		public override EntityStatus GetEntityStatus()
+		{
+			using (var a = new EntityStatusAccumulator ())
+			{
+				a.Accumulate (this.Name.GetEntityStatus ());
+				a.Accumulate (this.Description.GetEntityStatus ().TreatAsOptional ());
+
+				return a.EntityStatus;
+			}
+		}
+	}
+}
