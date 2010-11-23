97a8c8cc28b38e430ba748d38ed1f1eae39869f0
Marc Bettex <bettex@epsitec.ch>
2010-11-23 07:38:01 +0000

    - Ajouté la classe DbLock pour contenir les données d'une ligne dans la table CR_LOCK - Remis DbLockManager au gout du jour. - Adapté son test unitaire et LockTransaction à ces modifications.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@14934 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Database/Services/DbLock.cs
@@ -0,0 +1,73 @@
+﻿namespace Epsitec.Cresus.Database.Services
+{
+
+
+	/// <summary>
+	/// The <c>DbLock</c> class is an immutable object that represents a lock in the CR_LOCK
+	/// table in the database.
+	/// </summary>
+	public sealed class DbLock
+	{
+
+
+		/// <summary>
+		/// Builds a new instance of <c>DbLock</c>.
+		/// </summary>
+		/// <param name="id">The <see cref="DbId"/> of the lock.</param>
+		/// <param name="connectionId">The <see cref="DbId"/> of the connection of the lock.</param>
+		/// <param name="name">The name of the lock.</param>
+		/// <param name="counter">The counter that counts the number of times the lock has been acquired.</param>
+		internal DbLock(DbId id, DbId connectionId, string name, int counter)
+		{
+			this.Id = id;
+			this.ConnectionId = connectionId;
+			this.Name = name;
+			this.Counter = counter;
+		}
+
+
+		/// <summary>
+		/// Gets the <see cref="DbId"/> of the lock represented by this instance.
+		/// </summary>
+		public DbId Id
+		{
+			get;
+			private set;
+		}
+
+
+		/// <summary>
+		/// Gets the <see cref="DbId"/> of the connection of the lock represented by this instance.
+		/// </summary>
+		public DbId ConnectionId
+		{
+			get;
+			private set;
+		}
+
+
+		/// <summary>
+		/// Gets the name of the lock represented by this instance.
+		/// </summary>
+		public string Name
+		{
+			get;
+			private set;
+		}
+
+
+		/// <summary>
+		/// Gets the counter that gives the number of times that the lock represented by this instance
+		/// has been acquired.
+		/// </summary>
+		internal int Counter
+		{
+			get;
+			private set;
+		}
+
+
+	}
+
+
+}
