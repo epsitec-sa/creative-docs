c0d6d362ca0c889a3c5ab1e547f745179ffa2602
Marc Bettex <bettex@epsitec.ch>
2010-11-24 09:37:49 +0000

    - Commencé l'implémentation de PriceCalculatorTable et de classes annexes pour avoir des tabelles de prix à dimensions variables. - Ajouté des tests unitaires pour toutes ces classes.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@14957 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Business/Finance/PriceCalculators/PriceCalculatorEqualityComparer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+
+namespace Epsitec.Cresus.Core.Business.Finance.PriceCalculators
+{
+
+
+	internal sealed class PriceCalculatorEqualityComparer : EqualityComparer<object[]>
+	{
+
+		public override int GetHashCode(object[] obj)
+		{
+			int result = 0;
+
+			if (obj != null)
+			{
+				result = 1000000007;
+
+				result = 37 * result + obj.Length.GetHashCode ();
+
+				for (int i = 0; i < obj.Length; i++)
+				{
+					int c = (obj[i] != null) ? obj[i].GetHashCode () : 0;
+
+					result = 37 * result + c;
+				}
+			}
+
+			return result;
+		}
+
+
+		public override bool Equals(object[] x, object[] y)
+		{
+			bool different = x == null || y == null || x.Length != y.Length;
+
+			for (int i = 0; !different && i < x.Length; i++)
+			{
+				different = !object.Equals (x[i], y[i]);
+			}
+
+			return !different;
+		}
+
+
+	}
+
+
+}
