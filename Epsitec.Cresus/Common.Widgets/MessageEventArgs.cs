00b011ab9eb490c5a7c459d9164511fb575f5b7e
Pierre Arnaud <arnaud@opac.ch>
2010-11-30 05:30:11 +0000

    - Corrigé problème de création d'images: quand on cliquait sur un bouton tel que [Importer une image], on n'avait pas encore pris le verrou lié au BusinessContext. C'est maintenant fait automatiquement par le UIBuilder. - Refactoring: MessageEventArgs.Suppress est devenu MessageEventArgs.Cancel pour être plus cohérent. - Refactoring: PaintEventArgs.Suppress est devenu PaintEventArgs.Cancel pour être plus cohérent. - Refactoring: ménage dans les EventArgs du projet Common.Widgets.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@15014 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Widgets/MessageEventArgs.cs
@@ -0,0 +1,41 @@
+//	Copyright © 2003-2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Drawing;
+
+namespace Epsitec.Common.Widgets
+{
+	/// <summary>
+	/// The <c>MessageEventArgs</c> describes a user generated UI event
+	/// (for instance mouse moved, key pressed, etc.)
+	/// </summary>
+	public class MessageEventArgs : Support.CancelEventArgs
+	{
+		public MessageEventArgs(Message message, Point point)
+		{
+			this.message = message;
+			this.point   = point;
+		}
+
+
+		public Message							Message
+		{
+			get
+			{
+				return this.message;
+			}
+		}
+
+		public Point							Point
+		{
+			get
+			{
+				return this.point;
+			}
+		}
+
+
+		private readonly Message				message;
+		private readonly Point					point;
+	}
+}
