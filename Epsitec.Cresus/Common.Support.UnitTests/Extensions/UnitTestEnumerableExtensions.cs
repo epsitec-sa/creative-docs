7b68e979a6bc2d47f8a4b1205cacb6ab3c20dfa0
Marc Bettex <bettex@epsitec.ch>
2010-11-30 13:07:29 +0000

    - Ajouté la méthode SetEquals(...) dans EnumerableExtensions. - Adapté le test unitaire pour EnumerableExtensions en conséquence.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@15018 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Support.UnitTests/Extensions/UnitTestEnumerableExtensions.cs
+++ Epsitec.Cresus/Common.Support.UnitTests/Extensions/UnitTestEnumerableExtensions.cs
@@ -67,6 +67,44 @@ namespace Epsitec.Common.Support.UnitTests.Extensions
 			}
 		}
 
+
+		[TestMethod]
+		public void SetEqualsArgumentCheck()
+		{
+			ExceptionAssert.Throw<System.ArgumentNullException>
+			(
+				() => ((IEnumerable<int>) null).SetEquals (new List<int> ())
+			);
+
+			ExceptionAssert.Throw<System.ArgumentNullException>
+			(
+				() => new List<int> ().SetEquals (null)
+			);
+		}
+
+
+		[TestMethod]
+		public void SetEqualsTest()
+		{
+			Assert.IsTrue (new List<int> ().SetEquals (new List<int> ()));
+
+			for (int i = 0; i < 100; i++)
+			{
+				List<int> set1 = Enumerable.Range (0, 100).Shuffle ().ToList ();
+				List<int> set2 = Enumerable.Range (0, 100).Shuffle ().ToList ();
+
+				Assert.IsTrue (set1.SetEquals (set2));
+
+				if (set1[0] != set2[0])
+				{
+					Assert.IsFalse (set1.Skip (1).SetEquals (set2.Skip (1)));
+				}
+
+				Assert.IsTrue (set1.Append (set1[0]).SetEquals (set2.Append (set2[0])));
+			}
+		}
+
+
 	}
 
 
