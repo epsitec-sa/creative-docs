fcab1dc2890a5ac5d402e91d754ee7b9aee31441
Marc Bettex <bettex@epsitec.ch>
2010-12-06 08:59:29 +0000

    - Commenté les classes liées à DimensionTable.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@15038 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Business/Finance/PriceCalculators/RoundingMode.cs
+++ Epsitec.Cresus/Cresus.Core/Business/Finance/PriceCalculators/RoundingMode.cs
@@ -1,19 +1,39 @@
 ﻿namespace Epsitec.Cresus.Core.Business.Finance.PriceCalculators
 {
 
+
+	/// <summary>
+	/// The <c>RoundingMode</c> enum defines the different rounding strategies that a
+	/// <see cref="NumericDimension"/> can have to round values.
+	/// </summary>
 	public enum RoundingMode
 	{
 
 
+		/// <summary>
+		/// The values are not rounded.
+		/// </summary>
 		None,
 
 
+		/// <summary>
+		/// The values are rounded down to the nearest value defined which is lower than the given
+		/// one.
+		/// </summary>
 		Down,
 
 
+		/// <summary>
+		/// The values are rounded up or down to the nearest value defined. If there is a tie, the
+		/// value is rounded up.
+		/// </summary>
 		Nearest,
 	
 		
+		/// <summary>
+		/// The values are rounded up to the nearest value defined which is greater than the given
+		/// one.
+		/// </summary>
 		Up,
 
 
