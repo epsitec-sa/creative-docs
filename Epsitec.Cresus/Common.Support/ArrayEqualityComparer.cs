6aafb588c7d9a1a38da13805f99b5eb63080c9cd
Marc Bettex <bettex@epsitec.ch>
2010-12-09 14:23:00 +0000

    - Corrigé un bug dans ArrayEqualityComparer. - Déplacé ArrayEqualityComparer dans Epsitec.Common.Support. - Pareil pour son test unitaire qui est passé dans Epsitec.Common.Support.UnitTests.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@15080 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Support/ArrayEqualityComparer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+
+namespace Epsitec.Common.Support
+{
+
+
+	public sealed class ArrayEqualityComparer<T> : EqualityComparer<T[]>
+	{
+
+
+		public override int GetHashCode(T[] obj)
+		{
+			int result = 0;
+
+			if (obj != null)
+			{
+				result = 1000000007;
+
+				result = 37 * result + obj.Length.GetHashCode ();
+
+				for (int i = 0; i < obj.Length; i++)
+				{
+					int c = (obj[i] != null) ? obj[i].GetHashCode () : 0;
+
+					result = 37 * result + c;
+				}
+			}
+
+			return result;
+		}
+
+
+		public override bool Equals(T[] x, T[] y)
+		{
+			bool different = x == null || y == null || x.Length != y.Length;
+
+			for (int i = 0; !different && i < x.Length; i++)
+			{
+				different = x[i].Equals (y[i]);
+			}
+
+			return !different;
+		}
+
+
+	}
+
+
+}
