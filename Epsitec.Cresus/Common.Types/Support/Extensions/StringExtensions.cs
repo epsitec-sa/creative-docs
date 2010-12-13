121ae65e49f4992f918565af174ec3c00b49545c
Pierre Arnaud <arnaud@opac.ch>
2010-12-13 08:38:21 +0000

    - Fusionné les deux classes StringExtensions. - Modifications en vue d'une compilation simplifiée des versions .NET 3.5 et .NET 4.0.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@15105 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Types/Support/Extensions/StringExtensions.cs
+++ Epsitec.Cresus/Common.Types/Support/Extensions/StringExtensions.cs
@@ -1,13 +1,18 @@
 //	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
-//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+//	Author: Pierre ARNAUD & Marc BETTEX, Maintainer: Pierre ARNAUD
 
 using Epsitec.Common.Support.Extensions;
 
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Epsitec.Common.Support.Extensions
 {
+	/// <summary>
+	/// The <c>StringExtensions</c> class contains some useful extension method for instances of
+	/// <see cref="System.String"/>.
+	/// </summary>
 	public static class StringExtensions
 	{
 		public static bool IsNullOrWhiteSpace(this string text)
@@ -16,8 +21,50 @@ namespace Epsitec.Common.Support.Extensions
 			return string.IsNullOrEmpty (text)
 						|| text.Trim ().Length == 0;
 #else
-					return string.IsNullOrWhiteSpace (text);
+			return string.IsNullOrWhiteSpace (text);
 #endif
+		}
+		
+		/// <summary>
+		/// Checks that <paramref name="value"/> is an alpha numeric <see cref="System.String"/>,
+		/// i.e. that it is empty or that it contains only lower case letters, upper case letters
+		/// or numbers.
+		/// </summary>
+		/// <param name="value">The <see cref="System.String"/> to check.</param>
+		/// <returns><c>true</c> if <paramref name="value"/> is alpha numeric, <c>false</c> if it isn't.</returns>
+		/// <exception cref="System.ArgumentNullException">If <paramref name="value"/> is <c>null</c>.</exception>
+		public static bool IsAlphaNumeric(this string value)
+		{
+			if (value == null)
+			{
+				throw new System.ArgumentNullException ("value");
+			}
+
+			return new Regex ("^[a-zA-Z0-9]*$").IsMatch (value);
+		}
+
+
+		/// <summary>
+		/// Splits <paramref name="value"/> into an array of <see cref="string"/> that contains all
+		/// its substring that are separated by <paramref name="separator"/>.
+		/// </summary>
+		/// <param name="value">The <see cref="System.String"/> to split.</param>
+		/// <param name="separator">The <see cref="System.String"/> used to separate the substrings.</param>
+		/// <returns>The separated substrings.</returns>
+		/// <exception cref="System.ArgumentNullException">If <paramref name="value"/> is <c>null</c>.</exception>
+		/// <exception cref="System.ArgumentNullException">If <paramref name="separator"/> is <c>null</c> or empty.</exception>
+		public static string[] Split(this string value, string separator)
+		{
+			if (value == null)
+			{
+				throw new System.ArgumentNullException ("value");
+			}
+			if (separator == null)
+			{
+				throw new System.ArgumentNullException ("separator");
+			}
+
+			return value.Split (new string[] { separator }, System.StringSplitOptions.None);
 		}
 	}
 }
