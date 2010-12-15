0c17cfb16c80206e56fdd2ef56bbf288d640eb99
Daniel Roux <roux@epsitec.ch>
2010-12-15 10:56:17 +0000

    - Cresus.Core: Nouvelle classe Business.Rules.OptionValueBusinessRules qui initialise la propriété Code.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@15136 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Business/Rules/OptionValueBusinessRules.cs
@@ -0,0 +1,20 @@
+//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Cresus.Core.Data;
+using Epsitec.Cresus.Core.Entities;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Business.Rules
+{
+	[BusinessRule]
+	internal class OptionValueBusinessRules : GenericBusinessRule<OptionValueEntity>
+	{
+		public override void ApplySetupRule(OptionValueEntity entity)
+		{
+			entity.Code = (string) ItemCodeGenerator.NewCode ();
+		}
+	}
+}
