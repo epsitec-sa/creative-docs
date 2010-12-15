0ac5578cce4921a1142c81cc63ef484243329e76
Pierre Arnaud <arnaud@opac.ch>
2010-12-15 08:18:04 +0000

    - Corrections par rapport à la multiplication des bindings. - Nettoyage.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@15131 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Controllers/DataAccessors/AccessorBinding.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/DataAccessors/AccessorBinding.cs
@@ -1,15 +1,19 @@
 //	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
+using Epsitec.Common.Types;
+
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace Epsitec.Cresus.Core.Controllers.DataAccessors
 {
-	public class AccessorBinding
+	public sealed class AccessorBinding : System.IEquatable<AccessorBinding>
 	{
-		public AccessorBinding(System.Action action)
+		private AccessorBinding(string name, System.Action action)
 		{
+			this.name = name;
 			this.action = action;
 		}
 
@@ -18,11 +22,40 @@ namespace Epsitec.Cresus.Core.Controllers.DataAccessors
 			this.action ();
 		}
 
-		public static AccessorBinding Create<T>(Accessor<T> accessor, System.Action<T> setter)
+		public static AccessorBinding Create<T>(Accessor<T> accessor, Expression<System.Func<FormattedText>> getterExpression, System.Action<T> setter)
 		{
-			return new AccessorBinding (() => setter (accessor.ExecuteGetter ()));
+			string name = ExpressionAnalyzer.GetLambdaPropertyInfo (getterExpression).Name;
+
+			return new AccessorBinding (name, () => setter (accessor.ExecuteGetter ()));
 		}
 
+		#region IEquatable<AccessorBinding> Members
+
+		public bool Equals(AccessorBinding other)
+		{
+			if (other == null)
+			{
+				return false;
+			}
+			else
+			{
+				return this.name == other.name;
+			}
+		}
+
+		#endregion
+
+		public override bool Equals(object obj)
+		{
+			return base.Equals (obj as AccessorBinding);
+		}
+
+		public override int GetHashCode()
+		{
+			return this.name.GetHashCode ();
+		}
+
+		private readonly string name;
 		private readonly System.Action action;
 	}
 }
