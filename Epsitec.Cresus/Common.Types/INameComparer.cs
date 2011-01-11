e86f98e8dd9a25c4886c0ed1b87a6ddec0a71782
Marc Bettex <bettex@epsitec.ch>
2011-01-11 13:53:45 +0000

    - Ajouté INameComparer qui permet de comparer des objets implémentant IName. - Ajouté les tests relatifs à cette classe.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@15187 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Types/INameComparer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+
+namespace Epsitec.Common.Types
+{
+
+
+	public sealed class INameComparer : EqualityComparer<IName>
+	{
+
+
+		public INameComparer() : base ()
+		{
+		}
+
+
+		public override bool Equals(IName x, IName y)
+		{
+			return x != null
+				&& y != null
+				&& x.Name != null
+				&& y.Name != null
+				&& string.Equals (x.Name, y.Name);
+		}
+
+
+		public override int GetHashCode(IName obj)
+		{
+			int hash = 0;
+
+			if (obj != null && obj.Name != null)
+			{
+				hash = obj.Name.GetHashCode ();
+			}
+
+			return hash;
+		}
+
+
+	}
+
+
+}
