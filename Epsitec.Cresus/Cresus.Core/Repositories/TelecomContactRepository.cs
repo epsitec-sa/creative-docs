6f745f7055b6f2f1b3ba8ba80aac59cb4abeec67
Pierre Arnaud <arnaud@opac.ch>
2011-01-19 13:41:29 +0000

    - Cresus.Core: corrigé la hiérarchie des repositories (AbstractContactRepository n'a pas le droit d'exister).
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@15274 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Repositories/TelecomContactRepository.cs
+++ Epsitec.Cresus/Cresus.Core/Repositories/TelecomContactRepository.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 
 namespace Epsitec.Cresus.Core.Repositories
 {
-	public class TelecomContactRepository : AbstractContactRepository<TelecomContactEntity>
+	public class TelecomContactRepository : Repository<TelecomContactEntity>
 	{
 		public TelecomContactRepository(CoreData data, DataContext context = null)
 			: base (data, context)
