c0a392c29b92a12787b8cf57241930d599b7d601
Daniel Roux <roux@epsitec.ch>
2011-01-20 10:00:06 +0000

    - Cresus.Core: WorkflowDesigner: Le dialogue pour exporter un workflow contient une option permettant d'exporter tous les workflows.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@15292 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.CorePlugIn.WorkflowDesigner/Dialogs/FileExportDialog.cs
+++ Epsitec.Cresus/Cresus.CorePlugIn.WorkflowDesigner/Dialogs/FileExportDialog.cs
@@ -20,11 +20,28 @@ namespace Epsitec.Cresus.CorePlugIn.WorkflowDesigner.Dialogs
 		{
 			this.parent = parent;
 
-			this.InitialDirectory = FileExportDialog.initialDirectory;
-			this.InitialFileName  = FileExportDialog.initialFilename;
-			this.FileExtension    = ".xml";
-			this.enableNavigation = true;
+			this.InitialDirectory        = FileExportDialog.initialDirectory;
+			this.InitialFileName         = FileExportDialog.initialFilename;
+			this.FileExtension           = ".xml";
+			this.enableNavigation        = true;
 			this.enableMultipleSelection = false;
+		}
+
+
+		public bool ExportAll
+		{
+			get
+			{
+				return FileExportDialog.exportAll;
+			}
+			set
+			{
+				if (FileExportDialog.exportAll != value)
+				{
+					FileExportDialog.exportAll = value;
+					this.UpdateOptionsButtons ();
+				}
+			}
 		}
 
 
@@ -78,6 +95,80 @@ namespace Epsitec.Cresus.CorePlugIn.WorkflowDesigner.Dialogs
 			}
 		}
 
+		protected override void CreateOptionsUserInterface()
+		{
+			//	Crée le panneau facultatif pour les options d'enregistrement.
+			this.optionsContainer = new Widget (this.window.Root);
+			this.optionsContainer.Margins = new Margins (0, 0, 8, 0);
+			this.optionsContainer.Dock = DockStyle.Bottom;
+			this.optionsContainer.TabNavigationMode = TabNavigationMode.None;
+			this.optionsContainer.Visibility = FileExportDialog.showOptions;
+			this.optionsContainer.ContainerLayoutMode = ContainerLayoutMode.HorizontalFlow;
+			this.optionsContainer.Name = "OptionsContainer";
+
+			//	Options pour le zoom.
+			GroupBox group = new GroupBox (this.optionsContainer);
+			group.Text = "Options";
+			group.PreferredWidth = 200;
+			group.Padding = new Margins (10, 0, 0, 3);
+			group.Dock = DockStyle.StackEnd;
+			group.Margins = new Margins (0, 182, 0, 0);
+			group.Name = "Options";
+
+			this.optionsExportAllButton = new CheckButton (group);
+			this.optionsExportAllButton.Text = "Exporte tous les workflows";
+			this.optionsExportAllButton.AutoToggle = false;
+			this.optionsExportAllButton.Dock = DockStyle.Top;
+			this.optionsExportAllButton.Clicked += this.HandleOptionsClicked;
+		}
+
+		protected override void CreateFooterOptions(Widget footer)
+		{
+			this.optionsExtend = new GlyphButton (footer);
+			this.optionsExtend.PreferredWidth = 16;
+			this.optionsExtend.ButtonStyle = ButtonStyle.Slider;
+			this.optionsExtend.AutoFocus = false;
+			this.optionsExtend.TabNavigationMode = TabNavigationMode.None;
+			this.optionsExtend.Dock = DockStyle.Left;
+			this.optionsExtend.Margins = new Margins (0, 8, 3, 3);
+			this.optionsExtend.Clicked += this.HandleOptionsExtendClicked;
+			ToolTip.Default.SetToolTip (this.optionsExtend, "Montre ou cache les options");
+		}
+
+		protected override void UpdateOptions()
+		{
+			base.UpdateOptions ();
+
+			if (this.optionsExtend != null)
+			{
+				this.optionsExtend.GlyphShape = this.optionsContainer.Visibility ? GlyphShape.ArrowDown : GlyphShape.ArrowUp;
+			}
+
+			this.UpdateOptionsButtons ();
+		}
+
+		protected void UpdateOptionsButtons()
+		{
+			if (this.optionsExportAllButton != null)
+			{
+				this.optionsExportAllButton.ActiveState = this.ExportAll ? ActiveState.Yes : ActiveState.No;
+			}
+		}
+
+		private void HandleOptionsExtendClicked(object sender, MessageEventArgs e)
+		{
+			this.optionsContainer.Visibility = !this.optionsContainer.Visibility;
+			FileExportDialog.showOptions = this.optionsContainer.Visibility;
+
+			this.UpdateOptions ();
+		}
+
+		private void HandleOptionsClicked(object sender, MessageEventArgs e)
+		{
+			this.ExportAll = !this.ExportAll;
+			this.UpdateOptionsButtons ();
+		}
+
 
 		public class Settings : IFavoritesSettings
 		{
@@ -123,7 +214,13 @@ namespace Epsitec.Cresus.CorePlugIn.WorkflowDesigner.Dialogs
 		private static Settings			settings = new Settings ();
 		private static string			initialDirectory = null;
 		private static string			initialFilename = null;
+		private static bool				exportAll = false;
+		private static bool				showOptions = true;
 
 		private readonly Widget			parent;
+
+		private GlyphButton				optionsExtend;
+		private Widget					optionsContainer;
+		private CheckButton				optionsExportAllButton;
 	}
 }
