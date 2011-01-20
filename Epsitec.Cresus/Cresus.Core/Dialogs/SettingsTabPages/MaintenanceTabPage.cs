364c32db54db5435d4b05179a65b72f860513a7b
Daniel Roux <roux@epsitec.ch>
2011-01-20 07:12:17 +0000

    - Cresus.Core: Squelette d'une page 'Maintenance' dans le dialogue des réglages globaux.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@15285 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Dialogs/SettingsTabPages/MaintenanceTabPage.cs
@@ -0,0 +1,58 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using Epsitec.Common.Debug;
+using Epsitec.Common.Dialogs;
+using Epsitec.Common.Drawing;
+using Epsitec.Common.IO;
+using Epsitec.Common.Printing;
+using Epsitec.Common.Support;
+using Epsitec.Common.Types;
+using Epsitec.Common.Widgets;
+
+using Epsitec.Common.Support.EntityEngine;
+using Epsitec.Cresus.Core.Entities;
+using Epsitec.Cresus.Core.Printers;
+
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Dialogs.SettingsTabPages
+{
+	/// <summary>
+	/// Onglet de SettingsDialog pour effecture la maintenance de la base de données.
+	/// </summary>
+	public class MaintenanceTabPage : AbstractSettingsTabPage
+	{
+		public MaintenanceTabPage(CoreApplication application)
+			: base (application)
+		{
+		}
+
+
+		public override void AcceptChangings()
+		{
+		}
+
+		public override void CreateUI(Widget parent)
+		{
+			int tabIndex = 0;
+
+			var frame = new FrameBox
+			{
+				Parent = parent,
+				Dock = DockStyle.Fill,
+				Margins = new Margins (10),
+			};
+
+			this.UpdateWidgets ();
+		}
+
+		private void UpdateWidgets()
+		{
+		}
+
+
+	}
+}
