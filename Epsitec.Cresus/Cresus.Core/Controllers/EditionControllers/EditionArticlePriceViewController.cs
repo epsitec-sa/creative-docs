86769e40c05d68089cf64b6002069d3eb2b2e429
Daniel Roux <roux@epsitec.ch>
2011-01-22 07:12:27 +0000

    - Cresus.Core: Refactoring.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@15324 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionArticlePriceViewController.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionArticlePriceViewController.cs
@@ -100,27 +100,25 @@ namespace Epsitec.Cresus.Core.Controllers.EditionControllers
 				IconUri	        = "Data.ArticlePrice",
 				Title	        = TextFormatter.FormatText ("Prix"),
 				CompactTitle    = TextFormatter.FormatText ("Prix"),
-				CreateEditionUI = this.CreateEditionUIMain,
+				CreateEditionUI = (tile, builder) =>
+				{
+					builder.CreateTextField (tile, 150, "Du", Marshaler.Create (() => this.Entity.BeginDate, x => this.Entity.BeginDate = x));
+					builder.CreateTextField (tile, 150, "Au", Marshaler.Create (() => this.Entity.EndDate, x => this.Entity.EndDate   = x));
+
+					builder.CreateMargin (tile, horizontalSeparator: true);
+
+					builder.CreateTextField (tile, 80, "Quantité minimale", Marshaler.Create (() => this.Entity.MinQuantity, x => this.Entity.MinQuantity = x));
+					builder.CreateTextField (tile, 80, "Quantité maximale", Marshaler.Create (() => this.Entity.MaxQuantity, x => this.Entity.MaxQuantity = x));
+
+					builder.CreateMargin (tile, horizontalSeparator: true);
+
+					//	TODO: gérer le HT/TTC selon this.Entity.ValueIncludesTaxes
+					builder.CreateTextField (tile, 150, "Prix HT", Marshaler.Create (() => this.Entity.Value, x => this.Entity.Value = x));
+					builder.CreateAutoCompleteTextField (tile, 150-UIBuilder.ComboButtonWidth+1, "Monnaie", Marshaler.Create (() => this.Entity.CurrencyCode, x => this.Entity.CurrencyCode = x), Business.Enumerations.GetAllPossibleCurrencyCodes (), x => TextFormatter.FormatText (x.Values[0], "-", x.Values[1]));
+				}
 			};
 
 			data.Add (tileData);
-		}
-
-		private void CreateEditionUIMain(EditionTile tile, UIBuilder builder)
-		{			
-			builder.CreateTextField (tile, 150, "Du", Marshaler.Create (() => this.Entity.BeginDate, x => this.Entity.BeginDate = x));
-			builder.CreateTextField (tile, 150, "Au", Marshaler.Create (() => this.Entity.EndDate,   x => this.Entity.EndDate   = x));
-
-			builder.CreateMargin (tile, horizontalSeparator: true);
-
-			builder.CreateTextField (tile, 80, "Quantité minimale", Marshaler.Create (() => this.Entity.MinQuantity, x => this.Entity.MinQuantity = x));
-			builder.CreateTextField (tile, 80, "Quantité maximale", Marshaler.Create (() => this.Entity.MaxQuantity, x => this.Entity.MaxQuantity = x));
-
-			builder.CreateMargin (tile, horizontalSeparator: true);
-
-			//	TODO: gérer le HT/TTC selon this.Entity.ValueIncludesTaxes
-			builder.CreateTextField (tile, 150, "Prix HT", Marshaler.Create (() => this.Entity.Value, x => this.Entity.Value = x));
-			builder.CreateAutoCompleteTextField (tile, 150-UIBuilder.ComboButtonWidth+1, "Monnaie", Marshaler.Create (() => this.Entity.CurrencyCode, x => this.Entity.CurrencyCode = x), Business.Enumerations.GetAllPossibleCurrencyCodes (), x => TextFormatter.FormatText (x.Values[0], "-", x.Values[1]));
 		}
 
 		private void CreateUIPriceCalculators(TileDataItems data)
