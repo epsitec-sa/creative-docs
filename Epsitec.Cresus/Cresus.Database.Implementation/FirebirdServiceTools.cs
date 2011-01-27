b4e1a0180406024734ef187f9f110608d17fd9a6
Marc Bettex <bettex@epsitec.ch>
2011-01-27 10:04:07 +0000

    - Petite adaptation dans FirebirdServiceTools pour contourner un bug du provider .Net de Firebird.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@15365 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Database.Implementation/FirebirdServiceTools.cs
+++ Epsitec.Cresus/Cresus.Database.Implementation/FirebirdServiceTools.cs
@@ -21,13 +21,19 @@ namespace Epsitec.Cresus.Database.Implementation
 		
 		public void Backup(string path)
 		{
+			// NOTE Here we set the verbose mode to true, as this makes the Execute() method
+			// synchronous. This is a bug in firebird, if the verbose property is set to false, then
+			// the Execute() method becomes asynchronous and we have then no way of knowing when the
+			// backup is finished.
+			// Marc
+			
 			FbBackup backup = new FbBackup ();
 			
 			backup.BackupFiles.Add (new FbBackupFile (path, 2048));
 			
 			backup.ConnectionString = FirebirdAbstraction.MakeConnectionString (fb.DbAccess, fb.GetDbFilePath (), fb.ServerType);
 			backup.Options          = FbBackupFlags.IgnoreLimbo;
-			backup.Verbose          = false;
+			backup.Verbose          = true;
 			backup.ServiceOutput   += FirebirdServiceTools.ServiceOutput;
 			
 			System.Diagnostics.Debug.WriteLine ("Backup: running; saving to " + path);
@@ -44,12 +50,18 @@ namespace Epsitec.Cresus.Database.Implementation
 		
 		public void Restore(string path)
 		{
+			// NOTE Here we set the verbose mode to true, as this makes the Execute() method
+			// synchronous. This is a bug in firebird, if the verbose property is set to false, then
+			// the Execute() method becomes asynchronous and we have then no way of knowing when the
+			// restore is finished.
+			// Marc
+			
 			FbRestore restore = new FbRestore();
 			
 			restore.ConnectionString = FirebirdAbstraction.MakeConnectionString (fb.DbAccess, fb.GetDbFilePath (), fb.ServerType);
 			restore.BackupFiles.Add (new FbBackupFile (path, 2048));
 			
-			restore.Verbose        = false;
+			restore.Verbose        = true;
 			restore.PageSize       = 4096;
 			restore.Options        = FbRestoreFlags.Create | FbRestoreFlags.Replace;
 			restore.ServiceOutput += FirebirdServiceTools.ServiceOutput;
