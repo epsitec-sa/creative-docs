cb6b5d256eaa052be7249d4bb82acc2cf3ab3179
Pierre Arnaud <arnaud@opac.ch>
2011-02-03 05:45:47 +0000

    - Common.Types: corrigé une bulle dans la gestion du module.info qui faisait qu'une ouverture du fichier dans Designer perdait le réglage du 'text mode'. - Common.Types: commentaires.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@15434 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Types/Support/ResourceTextMode.cs
+++ Epsitec.Cresus/Common.Types/Support/ResourceTextMode.cs
@@ -1,18 +1,15 @@
-﻿namespace Epsitec.Common.Support
+﻿//	Copyright © 2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+namespace Epsitec.Common.Support
 {
-
-
+	/// <summary>
+	/// The <c>ResourceTextMode</c> entity defines how the resources will be
+	/// published in the <c>Res.cs</c> source code file.
+	/// </summary>
 	public enum ResourceTextMode
 	{
-
-
 		String,
-
-
 		FormattedText,
-
-
 	}
-
-
 }
