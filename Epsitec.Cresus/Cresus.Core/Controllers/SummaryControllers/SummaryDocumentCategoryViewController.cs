ae128e58f0985e9db0de99e5aa12640ff53e768d
Daniel Roux <roux@epsitec.ch>
2011-02-07 16:07:26 +0000

    - Cresus.Core: Premières tuiles pour accéder réellement aux DocumentCategory et DocumentOptions.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@15477 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Controllers/SummaryControllers/SummaryDocumentCategoryViewController.cs
@@ -0,0 +1,41 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Core.Widgets;
+using Epsitec.Cresus.Core.Controllers;
+using Epsitec.Cresus.Core.Controllers.DataAccessors;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Controllers.SummaryControllers
+{
+	public class SummaryDocumentCategoryViewController : SummaryViewController<Entities.DocumentCategoryEntity>
+	{
+		public SummaryDocumentCategoryViewController(string name, Entities.DocumentCategoryEntity entity)
+			: base (name, entity)
+		{
+		}
+
+
+		protected override void CreateUI()
+		{
+			using (var data = TileContainerController.Setup (this))
+			{
+				data.Add (
+					new TileDataItem
+					{
+						Name				= "DocumentCategory",
+						IconUri				= "Data.DocumentCategory",
+						Title				= TextFormatter.FormatText ("Catégorie"),
+						CompactTitle		= TextFormatter.FormatText ("Catégorie"),
+						TextAccessor		= this.CreateAccessor (x => x.GetSummary ()),
+						CompactTextAccessor = this.CreateAccessor (x => x.GetCompactSummary ()),
+						EntityMarshaler		= this.CreateEntityMarshaler (),
+					});
+			}
+		}
+	}
+}
