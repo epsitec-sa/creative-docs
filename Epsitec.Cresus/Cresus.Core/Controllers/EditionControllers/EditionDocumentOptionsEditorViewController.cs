28e7f74bd346069ad23b97c988f3bb6fa4fd941b
Daniel Roux <roux@epsitec.ch>
2011-02-07 17:21:18 +0000

    - Cresus.Core: EditionDocumentOptionsViewController: Nouvelle tuile. - Cresus.Core: DocumentOptionsEditor: Squelette.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@15479 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionDocumentOptionsEditorViewController.cs
@@ -0,0 +1,60 @@
+//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Drawing;
+using Epsitec.Common.Widgets;
+
+using Epsitec.Cresus.Core;
+using Epsitec.Cresus.Core.Entities;
+using Epsitec.Cresus.Core.Widgets;
+using Epsitec.Cresus.Core.Widgets.Tiles;
+using Epsitec.Cresus.Core.DocumentOptionsEditor;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Controllers.EditionControllers
+{
+	[ControllerSubType (2)]
+	public class EditionDocumentOptionsEditorViewController : EditionViewController<DocumentOptionsEntity>
+	{
+		public EditionDocumentOptionsEditorViewController(string name, DocumentOptionsEntity entity)
+			: base (name, entity)
+		{
+		}
+
+		public override double GetPreferredWidth(int columnIndex, int columnCount)
+		{
+			return base.GetPreferredWidth (columnIndex, columnCount) * 3;
+		}
+
+		protected override void CreateUI()
+		{
+			using (var builder = new UIBuilder (this))
+			{
+				builder.CreateHeaderEditorTile ();
+				builder.CreateEditionTitleTile ("Data.DocumentOptions", "Options pour un document");
+
+				this.CreateUIMain (builder);
+
+				builder.CreateFooterEditorTile ();
+			}
+		}
+
+		private void CreateUIMain(UIBuilder builder)
+		{
+			var tile = builder.CreateEditionTile ();
+
+			var box = new FrameBox
+			{
+				Parent = tile.Container,
+				PreferredHeight = 500,  // TODO: Comment mettre un mode "full height" ?
+				Dock = DockStyle.Top,
+			};
+
+			var editor = new DocumentOptionsEditorController (this.Orchestrator, this.BusinessContext, this.Entity);
+
+			editor.CreateUI (box);
+		}
+	}
+}
---
+++ Epsitec.Cresus/Cresus.Core/DocumentOptionsEditor/DocumentOptionsEditorController.cs
@@ -0,0 +1,81 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using Epsitec.Common.Drawing;
+using Epsitec.Common.Support;
+using Epsitec.Common.Widgets;
+
+using Epsitec.Cresus.Core;
+using Epsitec.Cresus.Core.Business;
+using Epsitec.Cresus.Core.Entities;
+using Epsitec.Cresus.Core.Orchestrators;
+using Epsitec.Cresus.Core.PlugIns;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.DocumentOptionsEditor
+{
+	public sealed class DocumentOptionsEditorController : System.IDisposable
+	{
+		public DocumentOptionsEditorController(DataViewOrchestrator orchestrator, Core.Business.BusinessContext businessContext, DocumentOptionsEntity documentOptionsEntity)
+		{
+			System.Diagnostics.Debug.Assert (orchestrator != null);
+			System.Diagnostics.Debug.Assert (businessContext != null);
+			System.Diagnostics.Debug.Assert (documentOptionsEntity.IsNotNull ());
+
+			this.orchestrator            = orchestrator;
+			this.businessContext         = businessContext;
+			this.documentOptionsEntity   = documentOptionsEntity;
+			
+			this.businessContext.SavingChanges += this.HandleBusinessContextSavingChanges;
+		}
+
+
+
+		public void CreateUI(Widget parent)
+		{
+			var box = new FrameBox
+			{
+				Parent = parent,
+				Dock = DockStyle.Fill,
+				Padding = new Margins (5),
+			};
+
+			this.mainController = new MainController (box, this.businessContext, this.documentOptionsEntity);
+			this.mainController.CreateUI (box);
+		}
+
+		public void NavigateTo(PriceCalculatorEntity entity)
+		{
+			var mainViewController    = this.orchestrator.MainViewController;
+			var browserViewController = mainViewController.BrowserViewController;
+			
+			browserViewController.Select (entity);
+		}
+
+
+		private void HandleBusinessContextSavingChanges(object sender, CancelEventArgs e)
+		{
+			this.mainController.SaveDesign ();
+			this.businessContext.DataContext.SaveChanges ();
+		}
+
+
+		#region IDisposable Members
+
+		public void Dispose()
+		{
+			this.businessContext.SavingChanges -= this.HandleBusinessContextSavingChanges;
+		}
+
+		#endregion
+
+
+		private readonly DataViewOrchestrator				orchestrator;
+		private readonly BusinessContext					businessContext;
+		private readonly DocumentOptionsEntity				documentOptionsEntity;
+
+		private MainController								mainController;
+	}
+}
