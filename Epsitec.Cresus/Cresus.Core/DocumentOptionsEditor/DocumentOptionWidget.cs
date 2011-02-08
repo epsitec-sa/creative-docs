9d52f885108f2fbb593990416c535cc7c7122196
Daniel Roux <roux@epsitec.ch>
2011-02-08 11:23:07 +0000

    - Cresus.Core: Amélioration dans SelectController (sélections multiples, etc.). - Cresus.Core: ValuesController sait construire son interface.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010/Epsitec.Cresus@15484 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/DocumentOptionsEditor/DocumentOptionWidget.cs
@@ -0,0 +1,16 @@
+//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using Epsitec.Cresus.Core.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.DocumentOptionsEditor
+{
+	public enum DocumentOptionWidget
+	{
+		Default,
+		CheckButton,
+		RadioButton,
+	}
+}
