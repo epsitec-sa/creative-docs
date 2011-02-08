c6878008fd00949a110696a6b516da824d7833fa
Daniel Roux <roux@epsitec.ch>
2011-02-08 09:25:56 +0000

    - Cresus.Core: La classe partielle DocumentOptionsEntity a de nouvelles méthodes Get/SetOptions(). Il faudra encore écrire un cache pour GetOptions() ! - Cresus.Core: SelectController est fonctionnel. Déjà !?
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@15483 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/DocumentOptionsEditor/DocumentOption.cs
+++ Epsitec.Cresus/Cresus.Core/DocumentOptionsEditor/DocumentOption.cs
@@ -88,6 +88,11 @@ namespace Epsitec.Cresus.Core.DocumentOptionsEditor
 					}
 				}
 
+				if (strings.Count == 0)
+				{
+					strings.Add (string.Concat ("● ", "Tous"));
+				}
+
 				return string.Join ("<br/>", strings);
 			}
 		}
@@ -97,16 +102,25 @@ namespace Epsitec.Cresus.Core.DocumentOptionsEditor
 		{
 			var list = new List<DocumentOption> ();
 
+			//	Ajoute les options d'impression liées à l'orientation portrait/paysage.
+			list.Add (new DocumentOption ("Orientation du papier :", "Orientation"));
+			list.Add (new DocumentOption ("OrientationVertical",   "Orientation", "Portrait", "true"));
+			list.Add (new DocumentOption ("OrientationHorizontal", "Orientation", "Paysage",  "false"));
+
+			//	Ajoute les options d'impression générales.
+			list.Add (new DocumentOption ("Options générales :", "Global"));
+			list.Add (new DocumentOption ("HeaderLogo", "Global", "Imprime le logo de l'entreprise", "true"));
+			list.Add (new DocumentOption ("Specimen",   "Global", "Incruste la mention SPECIMEN",    "false"));
+
+			list.Add (new DocumentOption ("Aspect des listes :", "TableAspect"));
+			list.Add (new DocumentOption ("LayoutFrameless", "TableAspect", "Espacé, sans encadrements",             "false"));
+			list.Add (new DocumentOption ("LayoutWithLine",  "TableAspect", "Espacé, avec des lignes de séparation", "true"));
+			list.Add (new DocumentOption ("LayoutWithFrame", "TableAspect", "Serré, avec des encadrements",          "false"));
+
 			//	Ajoute les options d'impression liées aux factures.
 			list.Add (new DocumentOption ("Options pour les factures :", "InvoiceOption"));
-			list.Add (new DocumentOption ("HeaderLogo",     "InvoiceOption", "Imprime le logo de l'entreprise",            "true",  Business.DocumentType.Invoice));
 			list.Add (new DocumentOption ("ArticleDelayed", "InvoiceOption", "Imprime les articles livrés ultérieurement", "true",  Business.DocumentType.Invoice));
 			list.Add (new DocumentOption ("ArticleId",      "InvoiceOption", "Imprime les identificateurs d'article",      "false", Business.DocumentType.Invoice));
-
-			list.Add (new DocumentOption ("Aspect de la liste des articles :", "InvoiceTableAspect"));
-			list.Add (new DocumentOption ("LayoutFrameless", "InvoiceTableAspect", "Espacé, sans encadrements",             "false", Business.DocumentType.Invoice));
-			list.Add (new DocumentOption ("LayoutWithLine",  "InvoiceTableAspect", "Espacé, avec des lignes de séparation", "true",  Business.DocumentType.Invoice));
-			list.Add (new DocumentOption ("LayoutWithFrame", "InvoiceTableAspect", "Serré, avec des encadrements",          "false", Business.DocumentType.Invoice));
 
 			list.Add (new DocumentOption ("Ordre des colonnes :", "InvoiceColumnsOrder"));
 			list.Add (new DocumentOption ("ColumnsOrderQD", "InvoiceColumnsOrder", "Quantité, Désignation, Prix", "true", Business.DocumentType.Invoice));
@@ -125,14 +139,13 @@ namespace Epsitec.Cresus.Core.DocumentOptionsEditor
 			list.Add (new DocumentOption ("Mode d'impression du BV :", "InvoiceESRMode"));
 			list.Add (new DocumentOption ("ESRFacsimile", "InvoiceESRMode", "Fac-similé complet du BV", "true", Business.DocumentType.Invoice));
 
-			//	Ajoute les options d'impression liées à l'orientation portrait/paysage.
-			list.Add (new DocumentOption ("Orientation du papier :", "Orientation"));
-			list.Add (new DocumentOption ("OrientationVertical",   "Orientation", "Portrait", "true"));
-			list.Add (new DocumentOption ("OrientationHorizontal", "Orientation", "Paysage",  "false"));
+			list.Add (new DocumentOption ("Signing", null, "Cartouche", "true", Business.DocumentType.OrderBooking, Business.DocumentType.OrderConfirmation, Business.DocumentType.ProductionOrder, Business.DocumentType.ProductionChecklist, Business.DocumentType.ShipmentChecklist, Business.DocumentType.DeliveryNote, Business.DocumentType.Receipt));
 
-			//	Ajoute les options d'impression générales.
-			list.Add (new DocumentOption ("Signing",  null, "Cartouche",                    "true"));
-			list.Add (new DocumentOption ("Specimen", null, "Incruste la mention SPECIMEN", "false"));
+			//	Ajoute les options pour les clients.
+			list.Add (new DocumentOption ("Données du client à inclure :", "Relation"));
+			list.Add (new DocumentOption ("RelationMail",    "Relation", "Adresses",   "true", Business.DocumentType.Summary));
+			list.Add (new DocumentOption ("RelationTelecom", "Relation", "Téléphones", "true", Business.DocumentType.Summary));
+			list.Add (new DocumentOption ("RelationUri",     "Relation", "Emails",     "true", Business.DocumentType.Summary));
 
 			return list;
 		}
