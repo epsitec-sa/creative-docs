cbcfeb27bcddfc0f05e97bec0b3400cad464e55f
Daniel Roux <roux@epsitec.ch>
2011-02-08 18:43:06 +0000

    - Cresus.Core: Mise en place du DocumentPrintingUnitsEditor.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@15491 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/DocumentPrintingUnitsEditor/DocumentOptionsEditorController.cs
@@ -0,0 +1,81 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using Epsitec.Common.Drawing;
+using Epsitec.Common.Support;
+using Epsitec.Common.Widgets;
+
+using Epsitec.Cresus.Core;
+using Epsitec.Cresus.Core.Business;
+using Epsitec.Cresus.Core.Entities;
+using Epsitec.Cresus.Core.Orchestrators;
+using Epsitec.Cresus.Core.PlugIns;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.DocumentPrintingUnitsEditor
+{
+	public sealed class DocumentPrintingUnitsEditorController : System.IDisposable
+	{
+		public DocumentPrintingUnitsEditorController(DataViewOrchestrator orchestrator, Core.Business.BusinessContext businessContext, DocumentPrintingUnitsEntity documentPrintingUnitsEntity)
+		{
+			System.Diagnostics.Debug.Assert (orchestrator != null);
+			System.Diagnostics.Debug.Assert (businessContext != null);
+			System.Diagnostics.Debug.Assert (documentPrintingUnitsEntity.IsNotNull ());
+
+			this.orchestrator                = orchestrator;
+			this.businessContext             = businessContext;
+			this.documentPrintingUnitsEntity = documentPrintingUnitsEntity;
+			
+			this.businessContext.SavingChanges += this.HandleBusinessContextSavingChanges;
+		}
+
+
+
+		public void CreateUI(Widget parent)
+		{
+			var box = new FrameBox
+			{
+				Parent = parent,
+				Dock = DockStyle.Fill,
+				Padding = new Margins (5),
+			};
+
+			this.mainController = new MainController (this.businessContext, this.documentPrintingUnitsEntity);
+			this.mainController.CreateUI (box);
+		}
+
+		public void NavigateTo(PriceCalculatorEntity entity)
+		{
+			var mainViewController    = this.orchestrator.MainViewController;
+			var browserViewController = mainViewController.BrowserViewController;
+			
+			browserViewController.Select (entity);
+		}
+
+
+		private void HandleBusinessContextSavingChanges(object sender, CancelEventArgs e)
+		{
+			this.mainController.SaveDesign ();
+			this.businessContext.DataContext.SaveChanges ();
+		}
+
+
+		#region IDisposable Members
+
+		public void Dispose()
+		{
+			this.businessContext.SavingChanges -= this.HandleBusinessContextSavingChanges;
+		}
+
+		#endregion
+
+
+		private readonly DataViewOrchestrator				orchestrator;
+		private readonly BusinessContext					businessContext;
+		private readonly DocumentPrintingUnitsEntity		documentPrintingUnitsEntity;
+
+		private MainController								mainController;
+	}
+}
