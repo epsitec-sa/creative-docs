7760991d10fd86d830e72f759cc74ec274c8f691
Daniel Roux <roux@epsitec.ch>
2011-02-09 18:50:03 +0000

    - Cresus.Core: Mise en place et début des travaux dans 'Print2', qui remplacera 'Printers'.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@15507 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Print2/PrintEngine.cs
@@ -0,0 +1,64 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using Epsitec.Common.Dialogs;
+using Epsitec.Common.Drawing;
+using Epsitec.Common.Printing;
+using Epsitec.Common.Support;
+using Epsitec.Common.Support.EntityEngine;
+using Epsitec.Common.Support.Extensions;
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Core.Entities;
+
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Epsitec.Cresus.Core.Print2
+{
+	public static class PrintEngine
+	{
+		static PrintEngine()
+		{
+			PrintEngine.RegisterFonts ();
+		}
+
+		public static void Setup()
+		{
+			//	Force execution of static constructor.
+		}
+
+
+		public static bool CanPrint(AbstractEntity entity)
+		{
+			return true;
+		}
+
+
+		public static string Print(CoreData coreData, IEnumerable<AbstractEntity> collection, Dictionary<string, string> options, Dictionary<string, string> printingUnits)
+		{
+			//	Imprime un document et retourne le source xml correspondant, sans aucune interaction.
+			//	Le dictionnaire 'options' contient "nom de l'option" / "valeur".
+			//	Par exemple "HeaderLogo" / "true".
+			//	Le dictionnaire 'printingUnits' contient "type de page" / "unité d'impression".
+			//	Par exemple "ForAllPages" / "Blanc".
+			System.Diagnostics.Debug.Assert (coreData != null);
+			System.Diagnostics.Debug.Assert (collection != null);
+			System.Diagnostics.Debug.Assert (options != null);
+			System.Diagnostics.Debug.Assert (printingUnits != null);
+
+			return null;
+		}
+
+
+		private static void RegisterFonts()
+		{
+			using (var stream = System.Reflection.Assembly.GetExecutingAssembly ().GetManifestResourceStream ("Epsitec.Creus.Core.Resources.OCR_BB.tff"))
+			{
+				Font.RegisterDynamicFont (stream);
+			}
+		}
+	}
+}
