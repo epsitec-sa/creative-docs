fc331d683dde42af21f6253fc724b37fe1c424c7
Pierre Arnaud <arnaud@opac.ch>
2011-02-10 06:11:15 +0000

    - App.CreativeDocs: compilation propre avec fichier RC correctement transformé en .res. - App.CreativeDocs: version 3.1.0.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@15508 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/App.CreativeDocs/compile-rc.bat
+++ Epsitec.Cresus/App.CreativeDocs/compile-rc.bat
@@ -1,18 +1,18 @@
 @echo off
 
-set
+cd %~d0%~p0%
 
 pushd
-call "%VS90COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
+call "%VS100COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
 popd
 
-del /Q "%~1.res"
+if exist "%~n1.res" del /Q "%~n1.res"
 
-rc.exe /r "%~1.rc"
+rc.exe /nologo /r "%~n1.rc"
 
-del /Q "%~1.aps" 2>NUL
+del /Q "%~n1.aps" 2>NUL
 
-move "%~1.RES" "%~1.tmp" 1>NUL
-move "%~1.tmp" "%~1.res" 1>NUL
+move "%~n1.RES" "%~n1.tmp" 1>NUL
+move "%~n1.tmp" "%~n1.res" 1>NUL
 
 echo Compiled resources
