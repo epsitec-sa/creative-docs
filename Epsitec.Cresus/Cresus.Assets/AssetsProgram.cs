3818f7468d0d0f155d9a627b1988faf2390b3242
Pierre Arnaud <arnaud@opac.ch>
2011-02-11 05:49:22 +0000

    - Epsitec.Cresus: ajouté un projet Crésus Assets (= Immobilisations).
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@15509 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Assets/AssetsProgram.cs
@@ -0,0 +1,61 @@
+﻿//	Copyright © 2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Cresus.Core;
+using Epsitec.Cresus.Core.Library;
+
+namespace Epsitec.Cresus.Assets
+{
+	static class AssetsProgram
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[System.STAThread]
+		static void Main()
+		{
+			AssetsProgram.ExecuteCoreProgram ();
+		}
+
+
+		static void ExecuteCoreProgram()
+		{
+			Epsitec.Common.Debug.GeneralExceptionCatcher.Setup ();
+
+			UI.Initialize ();
+
+			//Epsitec.Common.Widgets.Adorners.Factory.SetActive ("LookBlue");
+
+			AssetsProgram.Application = new AssetsApplication ();
+
+			System.Diagnostics.Debug.Assert (AssetsProgram.Application.ResourceManagerPool.PoolName == "Core");
+
+//			AssetsProgram.Application.SetupUI ();
+//			AssetsProgram.Application.SetupDefaultDocument ();
+
+//			splash.NotifyIsRunning ();
+
+//			AssetsProgram.Application.Window.MakeActive ();
+
+//			AssetsUpdate.CheckUpdate ();
+//			AssetsSerial.CheckLicense (AssetsProgram.Application.Window);
+
+//			AssetsProgram.Application.ProcessCommandLine ();
+
+//			AssetsProgram.Application.Window.Show ();
+//			AssetsProgram.Application.Window.Run ();
+
+			UI.ShutDown ();
+
+			AssetsProgram.Application.Dispose ();
+			AssetsProgram.Application = null;
+		}
+
+
+		public static AssetsApplication Application
+		{
+			get;
+			private set;
+		}
+	}
+}
---
+++ Epsitec.Cresus/Cresus.Assets/Resources/Cresus.Assets/SourceCode/Res.cs
@@ -0,0 +1,95 @@
+﻿//  --------------------------------------------------------------------------- 
+//  ATTENTION !
+//  Ce fichier a été généré automatiquement. Ne pas l'éditer manuellement, car 
+//  toute modification sera perdue. 
+//  --------------------------------------------------------------------------- 
+
+namespace Epsitec.Cresus.Assets
+{
+	public static class Res
+	{
+		//	Code mapping for 'Caption' resources
+		
+		
+		
+		//	Code mapping for 'String' resources
+		
+		public static class Strings
+		{
+			//	designer:str/CVA
+			public static global::Epsitec.Common.Types.FormattedText ProductName
+			{
+				get
+				{
+					return global::Epsitec.Cresus.Assets.Res.Strings.GetText (global::Epsitec.Common.Support.Druid.FromFieldId (167772160));
+				}
+			}
+			
+			public static global::Epsitec.Common.Types.FormattedText GetText(params string[] path)
+			{
+				string field = string.Join (".", path);
+				return new global::Epsitec.Common.Types.FormattedText (_stringsBundle[field].AsString);
+			}
+			
+			public static global::System.String GetString(params string[] path)
+			{
+				string field = string.Join (".", path);
+				return _stringsBundle[field].AsString;
+			}
+			
+			#region Internal Support Code
+			
+			private static global::Epsitec.Common.Types.FormattedText GetText(string bundle, params string[] path)
+			{
+				return new global::Epsitec.Common.Types.FormattedText (global::Epsitec.Cresus.Assets.Res.Strings.GetString (bundle, path));
+			}
+			
+			private static global::Epsitec.Common.Types.FormattedText GetText(global::Epsitec.Common.Support.Druid druid)
+			{
+				return new global::Epsitec.Common.Types.FormattedText (global::Epsitec.Cresus.Assets.Res.Strings.GetString (druid));
+			}
+			
+			private static global::System.String GetString(string bundle, params string[] path)
+			{
+				string field = string.Join (".", path);
+				return _stringsBundle[field].AsString;
+			}
+			
+			private static global::System.String GetString(global::Epsitec.Common.Support.Druid druid)
+			{
+				return _stringsBundle[druid].AsString;
+			}
+			
+			private static readonly global::Epsitec.Common.Support.ResourceBundle _stringsBundle = Res._manager.GetBundle ("Strings");
+			
+			#endregion
+		}
+		
+		static Res()
+		{
+			Res._manager = new global::Epsitec.Common.Support.ResourceManager (typeof (Res));
+			Res._manager.DefineDefaultModuleName ("Cresus.Assets");
+		}
+		
+		public static void Initialize()
+		{
+		}
+		
+		public static global::Epsitec.Common.Support.ResourceManager Manager
+		{
+			get
+			{
+				return Res._manager;
+			}
+		}
+		public static int ModuleId
+		{
+			get
+			{
+				return Res._moduleId;
+			}
+		}
+		private static readonly global::Epsitec.Common.Support.ResourceManager _manager;
+		private const int _moduleId = 1004;
+	}
+}
