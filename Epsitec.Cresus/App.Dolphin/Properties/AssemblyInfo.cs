e67e894e866c35eba417e2c062934c3b353258d1
Pierre Arnaud <arnaud@opac.ch>
2011-02-18 10:44:46 +0000

    - Déplacé les applications.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@15563 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/App.Dolphin/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("App.Dolphin")]
-[assembly: AssemblyDescription ("1977 Dolphin Simulator")]
-[assembly: AssemblyCopyright ("Copyright © 2007-2008, Daniel Roux, EPSITEC SA, www.epsitec.ch")]
