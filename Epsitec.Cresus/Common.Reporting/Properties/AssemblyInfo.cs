f6d83db7af06f7c8588c076b2a03e7c891565c2e
Pierre Arnaud <arnaud@opac.ch>
2011-02-18 10:54:34 +0000

    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@15569 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common.Reporting/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿//	Copyright © 2008, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
-//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
-
-using System.Reflection;
-
-[assembly: AssemblyTitle ("Epsitec.Common.Reporting")]
-[assembly: AssemblyDescription ("Reporting Library")]
-[assembly: AssemblyProduct ("Framework")]
-[assembly: AssemblyCopyright ("Copyright © 2008-2010, Pierre Arnaud, EPSITEC SA & OPaC bright ideas, Switzerland")]
