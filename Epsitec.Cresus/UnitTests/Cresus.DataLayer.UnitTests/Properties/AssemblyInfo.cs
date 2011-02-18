2ed723883c4d6e8cc0eacb277bc87eb7c7556af7
Pierre Arnaud <arnaud@opac.ch>
2011-02-18 11:03:30 +0000

    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010@15578 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/UnitTests/Cresus.DataLayer.UnitTests/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle ("Epsitec.Cresus.Core")]
-[assembly: AssemblyDescription ("Crésus Application Core")]
-[assembly: AssemblyProduct ("Crésus")]
-[assembly: AssemblyCopyright ("Copyright © 2007-2010, EPSITEC SA, 1400 Yverdon-les-Bains, Switzerland")]
