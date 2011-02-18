598cdebcec5b1356a8c62ba2fc5f7c380ef0911a
Pierre Arnaud <arnaud@opac.ch>
2011-02-18 11:21:01 +0000

    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.pa-2011-02-18@15583 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Tests.UnitTests/Common.Support.UnitTests/Extensions/UnitTestExceptionThrower.cs
@@ -0,0 +1,120 @@
+﻿using Epsitec.Common.Support.Extensions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Epsitec.Common.Support.UnitTests.Extensions
+{
+
+
+	[TestClass]
+	public class UnitTestExceptionThrower
+	{
+				
+
+		[TestMethod]
+		public void ThrowIfNullTest1()
+		{
+			object element = new object ();
+
+			element.ThrowIfNull ("element");
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentNullException))]
+		public void ThrowIfNullTest2()
+		{
+			object element = null;
+
+			element.ThrowIfNull ("element");
+		}
+		
+
+		[TestMethod]
+		public void ThrowIfNullOrEmptyTest1()
+		{
+			string string1 = "coucou";
+			string string2 = " ";
+			string string3 = "\t";
+			string string4 = "\n";
+			
+			string1.ThrowIfNullOrEmpty (null);
+			string2.ThrowIfNullOrEmpty ("string2");
+			string3.ThrowIfNullOrEmpty ("string3");
+			string4.ThrowIfNullOrEmpty ("string4");
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentException))]
+		public void ThrowIfNullOrEmptyTest2()
+		{
+			string element = null;
+
+			element.ThrowIfNullOrEmpty ("element");
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentException))]
+		public void ThrowIfNullOrEmptyTest3()
+		{
+			string element = "";
+
+			element.ThrowIfNullOrEmpty ("element");
+		}
+
+
+		[TestMethod]
+		public void ThrowIfTest1()
+		{
+			string element = "coucou";
+
+			element.ThrowIf (e => !e.Contains ("cou"), "element must contains 'cou'");
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentException))]
+		public void ThrowIfTest2()
+		{
+			string element = "blabla";
+
+			element.ThrowIf (e => !e.Contains ("cou"), "element must contains 'cou'");
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentNullException))]
+		public void ThrowIfTest3()
+		{
+			string element = "blabla";
+
+			element.ThrowIf (null, "blabla");
+		}
+
+
+		[TestMethod]
+		public void ThrowIfWithoutValueTest1()
+		{
+			int? value = 1;
+
+			value.ThrowIfWithoutValue ("value");
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (System.ArgumentException))]
+		public void ThrowIfWithoutValueTest2()
+		{
+			int? value = null;
+
+			value.ThrowIfWithoutValue ("value");
+		}
+
+
+	}
+
+
+}
---
+++ Epsitec.Cresus/Tests.UnitTests/Common.Support.UnitTests/UnitTestArrayEqualityComparer.cs
@@ -0,0 +1,91 @@
+﻿using Epsitec.Common.Support.Extensions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+
+namespace Epsitec.Common.Support.UnitTests
+{
+
+
+	[TestClass]
+	public class UnitTestArrayEqualityComparer
+	{
+
+
+		[TestMethod]
+		public void TestEquals()
+		{
+			List<double[]> arrays = new List<double[]> ();
+
+			System.Random dice = new System.Random ();
+
+			for (int i = 1; i < 10; i++)
+			{
+				for (int j = 0; j < 100; j++)
+				{
+					arrays.Add (Enumerable.Range (0, i).Select (v => dice.NextDouble ()).Shuffle ().ToArray ());
+				}
+			}
+
+			ArrayEqualityComparer<double> pcec = new ArrayEqualityComparer<double> ();
+
+			foreach (double[] array1 in arrays)
+			{
+				foreach (double[] array2 in arrays)
+				{
+					// Actually, this test is too restrictive, since it could happen that two random
+					// arrays could have the same value, even if the probability is low. We assume in
+					// this test that we are lucky.
+					// Marc
+
+					Assert.AreEqual ((array1 == array2), pcec.Equals (array1, array2));
+				}
+			}
+
+			Assert.IsFalse (pcec.Equals (arrays.First (), null));
+			Assert.IsFalse (pcec.Equals (null, arrays.First ()));
+			Assert.IsFalse (pcec.Equals (null, null));
+		}
+
+
+		[TestMethod]
+		public void TestHashCode()
+		{
+			List<double[]> arrays = new List<double[]> ();
+
+			System.Random dice = new System.Random ();
+
+			for (int i = 1; i < 100; i++)
+			{
+				for (int j = 0; j < 10; j++)
+				{
+					arrays.Add (Enumerable.Range (0, i).Select (v => dice.NextDouble ()).ToArray ());
+				}
+			}
+
+			ArrayEqualityComparer<double> pcec = new ArrayEqualityComparer<double> ();
+
+			foreach (double[] array1 in arrays)
+			{
+				foreach (double[] array2 in arrays)
+				{
+					// Actually, this test is too restrictive, as two different arrays could have
+					// the same hash code. We assume in this test that we are lucky.
+					// Marc
+
+					Assert.AreEqual (pcec.Equals (array1, array2), (pcec.GetHashCode (array1) == pcec.GetHashCode (array2)));
+				}
+			}
+
+			Assert.AreEqual (0, pcec.GetHashCode (null));
+		}
+
+
+	}
+
+
+}
