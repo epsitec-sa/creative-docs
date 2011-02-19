2a1ea9ae64b3ff206a17df5b399486fda25e0cea
Pierre Arnaud <arnaud@opac.ch>
2011-02-19 04:23:41 +0000

    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.pa-2011-02-19@15618 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebServer/Resources/Cresus.WebServer/SourceCode/Res.cs
+++
@@ -1,6 +0,0 @@
-﻿//  --------------------------------------------------------------------------- 
-//  ATTENTION !
-//  Ce fichier a été généré automatiquement. Ne pas l'éditer manuellement, car 
-//  toute modification sera perdue. 
-//  --------------------------------------------------------------------------- 
-
