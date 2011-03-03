3d92c2394accb85a745a646039503b6089e1d882
Pierre Arnaud <arnaud@opac.ch>
2011-03-03 11:41:14 +0000

    - Pictogram fonctiopnne à nouveau (il manquait des ressources).
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@15803 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/App.CresusPictogrammes/Application.cs
+++ Epsitec.Cresus/App.CresusPictogrammes/Application.cs
@@ -1,3 +1,6 @@
+//	Copyright © 2002-2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
 namespace Epsitec.App.Pictogram
 {
 	public class Application
