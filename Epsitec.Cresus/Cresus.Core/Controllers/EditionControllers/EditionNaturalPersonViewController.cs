ff0f0728b9e4f3a40ecf123ead08af5b4dc28da8
Pierre Arnaud <arnaud@opac.ch>
2011-03-04 19:19:21 +0000

    - Core: implémenté l'éditeur de NaturalPersonEntity en utilisant la version unifiée du moteur de Daniel, pour voir comment cela fonctionne.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@15858 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionNaturalPersonViewController.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionNaturalPersonViewController.cs
@@ -26,6 +26,7 @@ namespace Epsitec.Cresus.Core.Controllers.EditionControllers
 		{
 		}
 
+#if false
 		protected override void CreateUI()
 		{
 			using (var builder = new UIBuilder (this))
@@ -95,10 +96,124 @@ namespace Epsitec.Cresus.Core.Controllers.EditionControllers
 		}
 
 
+#else
+		protected override void CreateUI()
+		{
+			using (var data = TileContainerController.Setup (this))
+			{
+				this.CreateUITitle (data);
+				this.CreateUIFirstNameAndLastname (data);
+				this.CreateUIGender (data);
+				this.CreateUIBirthDate (data);
+				this.CreateUIPictures (data);
+			}
+		}
+
+
+		private void CreateUITitle(TileDataItems data)
+		{
+			var tileData = new TileDataItem
+			{
+				Name            = "NaturalPersonTitle",
+				IconUri	        = "Data.NaturalPerson",
+				Title	        = TextFormatter.FormatText ("Personne physique"),
+				CompactTitle    = TextFormatter.FormatText ("Personne physique"),
+				Frameless       = true,
+				CreateEditionUI = (tile, builder) =>
+				{
+					var controller = new SelectionController<PersonTitleEntity> (this.BusinessContext)
+					{
+						ValueGetter         = () => this.Entity.Title,
+						ValueSetter         = x => this.Entity.Title = x,
+						ReferenceController = new ReferenceController (() => this.Entity.Title, creator: this.CreateNewTitle),
+					};
+
+					builder.CreateAutoCompleteTextField (tile, "Titre", controller);
+				}
+			};
+
+			data.Add (tileData);
+		}
+
+		private void CreateUIFirstNameAndLastname(TileDataItems data)
+		{
+			var tileData = new TileDataItem
+			{
+				Name            = "NaturalPersonName",
+				Frameless       = true,
+				CreateEditionUI = (tile, builder) =>
+				{
+					builder.CreateTextField (tile, 0, "Prénom", Marshaler.Create (() => this.Entity.Firstname, x => this.Entity.Firstname = x));
+					builder.CreateTextField (tile, 0, "Nom", Marshaler.Create (() => this.Entity.Lastname, x => this.Entity.Lastname = x));
+					builder.CreateMargin (tile, horizontalSeparator: true);
+
+				}
+			};
+
+			data.Add (tileData);
+		}
+
+		private void CreateUIGender(TileDataItems data)
+		{
+			var tileData = new TileDataItem
+			{
+				Name            = "NaturalPersonGender",
+				Frameless       = true,
+				CreateEditionUI = (tile, builder) =>
+				{
+					var controller = new SelectionController<PersonGenderEntity> (this.BusinessContext)
+					{
+						ValueGetter         = () => this.Entity.Gender,
+						ValueSetter         = x => this.Entity.Gender = x,
+						ReferenceController = new ReferenceController (() => this.Entity.Gender, mode: ViewControllerMode.None),
+					};
+
+					builder.CreateAutoCompleteTextField (tile, "Sexe", controller);
+				}
+			};
+
+			data.Add (tileData);
+		}
+
+		private void CreateUIBirthDate(TileDataItems data)
+		{
+			var tileData = new TileDataItem
+			{
+				Name            = "NaturalPersonBirthDate",
+				Frameless       = true,
+				CreateEditionUI = (tile, builder) =>
+				{
+					builder.CreateTextField (tile, 90, "Date de naissance", Marshaler.Create (() => this.Entity.DateOfBirth, x => this.Entity.DateOfBirth = x));
+				}
+			};
+
+			data.Add (tileData);
+		}
+		private void CreateUIPictures(TileDataItems data)
+		{
+			var tileData = new TileDataItem
+			{
+				Name            = "NaturalPersonPictures",
+				Frameless       = true,
+				CreateEditionUI = (tile, builder) =>
+				{
+					var controller = new SelectionController<ImageEntity> (this.BusinessContext)
+					{
+						CollectionValueGetter    = () => this.Entity.Pictures,
+						ToFormattedTextConverter = x => TextFormatter.FormatText (x.Name).IfNullOrEmptyReplaceWith (CollectionTemplate.DefaultEmptyText),
+					};
+
+					builder.CreateEditionDetailedItemPicker (tile, "Pictures", this.Entity, "Photographies du client", controller, EnumValueCardinality.Any, ViewControllerMode.Summary, 6);
+				}
+			};
+
+			data.Add (tileData);
+		}
 		private NewEntityReference CreateNewTitle(DataContext context)
 		{
 			var title = context.CreateEntityAndRegisterAsEmpty<PersonTitleEntity> ();
 			return title;
 		}
+#endif
 	}
 }
