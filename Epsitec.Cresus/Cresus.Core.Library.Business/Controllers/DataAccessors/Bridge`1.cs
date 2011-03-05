9c58e4abfdae753bafd1bb58a2c0ef53568dc622
Pierre Arnaud <arnaud@opac.ch>
2011-03-05 16:31:24 +0000

    - Bricks: refactoring.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@15867 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core.Library.Business/Controllers/DataAccessors/Bridge`1.cs
+++ Epsitec.Cresus/Cresus.Core.Library.Business/Controllers/DataAccessors/Bridge`1.cs
@@ -17,7 +17,115 @@ using Epsitec.Common.Widgets;
 
 namespace Epsitec.Cresus.Core.Controllers.DataAccessors
 {
-	public class Bridge<T>
+	public abstract class Bridge
+	{
+		protected static void CreateDefaultProperties(Brick brick, System.Type type)
+		{
+			var typeInfo = EntityInfo.GetStructuredType (type) as StructuredType;
+
+			if ((typeInfo == null) ||
+				(typeInfo.Caption == null))
+			{
+				return;
+			}
+
+			var typeName = typeInfo.Caption.Name;
+			var typeIcon = typeInfo.Caption.Icon ?? "Data." + typeName;
+			var labels   = typeInfo.Caption.Labels;
+
+			BrickProperty nameProperty = new BrickProperty (BrickPropertyKey.Name, typeName);
+			BrickProperty iconProperty = new BrickProperty (BrickPropertyKey.Icon, typeIcon);
+
+			Brick.AddProperty (brick, nameProperty);
+			Brick.AddProperty (brick, iconProperty);
+
+			Bridge.CreateLabelProperty (brick, labels, 0, BrickPropertyKey.Title);
+			Bridge.CreateLabelProperty (brick, labels, 1, BrickPropertyKey.TitleCompact);
+		}
+
+		protected static void CreateDefaultTextProperties(Brick brick)
+		{
+			if (!Brick.ContainsProperty (brick, BrickPropertyKey.Text))
+			{
+				Expression<System.Func<AbstractEntity, FormattedText>> expression = x => x.GetSummary ();
+				Brick.AddProperty (brick, new BrickProperty (BrickPropertyKey.Text, expression));
+			}
+
+			if (!Brick.ContainsProperty (brick, BrickPropertyKey.TextCompact))
+			{
+				Expression<System.Func<AbstractEntity, FormattedText>> expression = x => x.GetCompactSummary ();
+				Brick.AddProperty (brick, new BrickProperty (BrickPropertyKey.TextCompact, expression));
+			}
+		}
+
+		protected static void CreateLabelProperty(Brick brick, IList<string> labels, int i, BrickPropertyKey key)
+		{
+			if (i < labels.Count)
+			{
+				BrickProperty property = new BrickProperty (key, labels[i]);
+				Brick.AddProperty (brick, property);
+			}
+		}
+
+		protected void HandleBrickWallBrickAdded(object sender, BrickAddedEventArgs e)
+		{
+			var brick = e.Brick;
+			var type  = e.FieldType;
+
+			Bridge.CreateDefaultProperties (brick, type);
+		}
+
+		protected void HandleBrickWallBrickPropertyAdded(object sender, BrickPropertyAddedEventArgs e)
+		{
+			var brick    = e.Brick;
+			var property = e.Property;
+
+			if (property.Key == BrickPropertyKey.AsType)
+			{
+				var type = property.Brick.GetFieldType ();
+				Bridge.CreateDefaultProperties (brick, type);
+			}
+		}
+
+		protected void ProcessProperty(Brick brick, BrickPropertyKey key, System.Action<bool> setter)
+		{
+			if (Brick.ContainsProperty (brick, key))
+			{
+				setter (true);
+			}
+		}
+
+		protected void ProcessProperty(Brick brick, BrickPropertyKey key, System.Action<string> setter)
+		{
+			var value = Brick.GetProperty (brick, key).StringValue;
+
+			if (value != null)
+			{
+				setter (value);
+			}
+		}
+
+		protected void ProcessTemplateProperty(Brick brick, BrickPropertyKey key, System.Action<object> setter)
+		{
+			var expression = Brick.GetProperty (brick, key).ExpressionValue;
+
+			if (expression == null)
+			{
+				return;
+			}
+
+			var expressionType = expression.GetType ();
+			var function = expressionType.InvokeMember ("Compile", BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, expression, null);
+
+			if (function != null)
+			{
+				setter (function);
+			}
+		}
+
+	}
+
+	public class Bridge<T> : Bridge
 		where T : AbstractEntity, new ()
 	{
 		public Bridge(EntityViewController<T> controller)
@@ -58,7 +166,7 @@ namespace Epsitec.Cresus.Core.Controllers.DataAccessors
 			}
 			else
 			{
-				Bridge<T>.CreateDefaultTextProperties (brick);
+				Bridge.CreateDefaultTextProperties (brick);
 			}
 
 			this.ProcessProperty (brick, BrickPropertyKey.Name, x => item.Name = x);
@@ -111,6 +219,9 @@ namespace Epsitec.Cresus.Core.Controllers.DataAccessors
 
 			return item;
 		}
+
+
+		#region InputProcessor Class
 
 		class InputProcessor
 		{
@@ -238,7 +349,6 @@ namespace Epsitec.Cresus.Core.Controllers.DataAccessors
 								foreach (var action in multiActions)
 								{
 									var subTile = builder.CreateEditionTile (tile);
-//.									subTile.PreferredHeight = 1;
 									action (subTile, builder);
 								}
 							};
@@ -291,77 +401,7 @@ namespace Epsitec.Cresus.Core.Controllers.DataAccessors
 			private TileDataItem item;
 		}
 
-
-
-
-
-		private void HandleBrickWallBrickAdded(object sender, BrickAddedEventArgs e)
-		{
-			var brick = e.Brick;
-			var type  = e.FieldType;
-			
-			Bridge<T>.CreateDefaultProperties (brick, type);
-		}
-
-		private void HandleBrickWallBrickPropertyAdded(object sender, BrickPropertyAddedEventArgs e)
-		{
-			var brick    = e.Brick;
-			var property = e.Property;
-
-			if (property.Key == BrickPropertyKey.AsType)
-			{
-				var type = property.Brick.GetFieldType ();
-				Bridge<T>.CreateDefaultProperties (brick, type);
-			}
-		}
-
-		private static void CreateDefaultProperties(Brick brick, System.Type type)
-		{
-			var typeInfo = EntityInfo.GetStructuredType (type) as StructuredType;
-
-			if ((typeInfo == null) ||
-				(typeInfo.Caption == null))
-			{
-				return;
-			}
-
-			var typeName = typeInfo.Caption.Name;
-			var typeIcon = typeInfo.Caption.Icon ?? "Data." + typeName;
-			var labels   = typeInfo.Caption.Labels;
-
-			BrickProperty nameProperty = new BrickProperty (BrickPropertyKey.Name, typeName);
-			BrickProperty iconProperty = new BrickProperty (BrickPropertyKey.Icon, typeIcon);
-
-			Brick.AddProperty (brick, nameProperty);
-			Brick.AddProperty (brick, iconProperty);
-
-			Bridge<T>.CreateLabelProperty (brick, labels, 0, BrickPropertyKey.Title);
-			Bridge<T>.CreateLabelProperty (brick, labels, 1, BrickPropertyKey.TitleCompact);
-		}
-
-		private static void CreateDefaultTextProperties(Brick brick)
-		{
-			if (!Brick.ContainsProperty (brick, BrickPropertyKey.Text))
-			{
-				Expression<System.Func<AbstractEntity, FormattedText>> expression = x => x.GetSummary ();
-				Brick.AddProperty (brick, new BrickProperty (BrickPropertyKey.Text, expression));
-			}
-
-			if (!Brick.ContainsProperty (brick, BrickPropertyKey.TextCompact))
-			{
-				Expression<System.Func<AbstractEntity, FormattedText>> expression = x => x.GetCompactSummary ();
-				Brick.AddProperty (brick, new BrickProperty (BrickPropertyKey.TextCompact, expression));
-			}
-		}
-		
-		private static void CreateLabelProperty(Brick brick, IList<string> labels, int i, BrickPropertyKey key)
-		{
-			if (i < labels.Count)
-			{
-				BrickProperty property = new BrickProperty (key, labels[i]);
-				Brick.AddProperty (brick, property);
-			}
-		}
+		#endregion
 
 		private void ProcessTemplate(TileDataItems data, TileDataItem item, Brick root, Brick templateBrick)
 		{
@@ -408,24 +448,6 @@ namespace Epsitec.Cresus.Core.Controllers.DataAccessors
 			return accessorFactory.CollectionAccessor;
 		}
 		
-		private void ProcessProperty(Brick brick, BrickPropertyKey key, System.Action<bool> setter)
-		{
-			if (Brick.ContainsProperty (brick, key))
-			{
-				setter (true);
-			}
-		}
-
-		private void ProcessProperty(Brick brick, BrickPropertyKey key, System.Action<string> setter)
-		{
-			var value = Brick.GetProperty (brick, key).StringValue;
-
-			if (value != null)
-			{
-				setter (value);
-			}
-		}
-
 		private void ProcessProperty(Brick brick, BrickPropertyKey key, System.Action<Accessor<FormattedText>> setter)
 		{
 			var formatter = this.ToAccessor (Brick.GetProperty (brick, key));
@@ -433,24 +455,6 @@ namespace Epsitec.Cresus.Core.Controllers.DataAccessors
 			if (formatter != null)
 			{
 				setter (formatter);
-			}
-		}
-
-		private void ProcessTemplateProperty(Brick brick, BrickPropertyKey key, System.Action<object> setter)
-		{
-			var expression = Brick.GetProperty (brick, key).ExpressionValue;
-
-			if (expression == null)
-			{
-				return;
-			}
-
-			var expressionType = expression.GetType ();
-			var function = expressionType.InvokeMember ("Compile", BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, expression, null);
-
-			if (function != null)
-			{
-				setter (function);
 			}
 		}
 
@@ -471,6 +475,4 @@ namespace Epsitec.Cresus.Core.Controllers.DataAccessors
 
 		private readonly EntityViewController<T> controller;
 	}
-
-
 }
