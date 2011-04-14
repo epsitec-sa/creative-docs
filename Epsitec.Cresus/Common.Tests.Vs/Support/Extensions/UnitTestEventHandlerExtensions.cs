1d93c34962ccfb02f0c9d327dd78fcab61a663d1
Marc Bettex <bettex@epsitec.ch>
2011-04-14 13:57:48 +0000

    - Ajout de la classe EventHanderExtensions qui contient une méthode d'extension utile pour lancer un événement selon la manière "officielle" de faire en C# 4. - Ajout de tests pour cette méthode.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16162 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Tests.Vs/Support/Extensions/UnitTestEventHandlerExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Epsitec.Common.Support;
+using Epsitec.Common.Support.Extensions;
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Epsitec.Common.Tests.Vs.Support.Extensions
+{
+
+
+	[TestClass]
+	public sealed class UnitTestEventHandlerExtensions
+	{
+
+
+		[TestMethod]
+		public void TestNullHandler()
+		{
+			EventHandler<EventArgs> eventHandler = null;
+
+			eventHandler.Raise (null, null);
+		}
+
+
+		[TestMethod]
+		public void TestNormalHandler()
+		{
+			int i = 0;
+
+			var sender = new object();
+			var args = new EventArgs();
+
+			EventHandler<EventArgs> handler = (s, e) =>
+			{
+				Assert.AreSame (sender, s);
+				Assert.AreSame (args, e);
+
+				i = i + 1;
+			};
+
+			handler.Raise (sender, args);
+
+			Assert.AreEqual (1, i);
+		}
+
+
+	}
+
+
+}
---
+++ Epsitec.Cresus/Common/Support/Extensions/EventHandlerExtensions.cs
@@ -0,0 +1,54 @@
+﻿namespace Epsitec.Common.Support
+{
+
+
+	/// <summary>
+	/// The <see cref="EventHandlerExtensions"/> class provides extensions methods for instances of
+	/// <see cref="EventHandler{T}"/>.
+	/// </summary>
+	public static class EventHandlerExtensions
+	{
+
+
+		/// <summary>
+		/// Raises an event on <paramref name="eventHandler"/> using the standard C# design
+		/// pattern.
+		/// </summary>
+		/// <remarks>
+		/// This extensions methods is to be used on simple auto generated field like event declared
+		/// by "public event EventHandler{T} handler".
+		/// The field like event ensures that the registration and unregistration are done in a
+		/// consistent way (in C# 4).
+		/// This methods then ensures that the event is not fired if the event handler is null
+		/// because no one has registered to it, or if all listeners have been removed.
+		/// However, there is no guarantee on the thread safety while the event is being raised. In
+		/// particular, a method can be called by the event handler after it has been unregistered
+		/// from that event handler, if event is raised on a thread and the unregistration is done
+		/// on another. The reason for that is that the only way to achieve thread safety in this
+		/// case would be to hold a lock on the event during its execution which would be a deadlock
+		/// prone solution.
+		/// 
+		/// See the following links for more references
+		/// http://blogs.msdn.com/b/cburrows/archive/2010/03/05/events-get-a-little-overhaul-in-c-4-part-i-locks.aspx
+		/// http://blogs.msdn.com/b/cburrows/archive/2010/03/08/events-get-a-little-overhaul-in-c-4-part-ii-semantic-changes-and.aspx
+		/// http://blogs.msdn.com/b/cburrows/archive/2010/03/18/events-get-a-little-overhaul-in-c-4-part-iii-breaking-changes.aspx
+		/// http://blogs.msdn.com/b/cburrows/archive/2010/03/30/events-get-a-little-overhaul-in-c-4-afterward-effective-events.aspx
+		/// http://www.codeproject.com/Articles/37474/Threadsafe-Events.aspx
+		/// </remarks>
+		/// <typeparam name="T">The type of the event data.</typeparam>
+		/// <param name="eventHandler">The event handler to fire.</param>
+		/// <param name="sender">The object considered as the sender of the event.</param>
+		/// <param name="eventArg">The data of the event.</param>
+		public static void Raise<T>(this EventHandler<T> eventHandler, object sender, T eventArg) where T : EventArgs
+		{
+			if (eventHandler != null)
+			{
+				eventHandler (sender, eventArg);
+			}
+		}
+
+
+	}
+
+
+}
