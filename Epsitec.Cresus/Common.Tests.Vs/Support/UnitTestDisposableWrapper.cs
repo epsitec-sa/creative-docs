7bf8298d9265747eb6a43edb4445e22e1b20f69b
Marc Bettex <bettex@epsitec.ch>
2011-04-15 10:13:11 +0000

    - Ajouté la classe DisposableWrapper qui permet de créer des objets IDisposable qui appellent une fonction donnée lors de l'appel à Dispose et qui jettent une exception si Dispose n'est jamais appelée. - Ajouté des tests pour cette classe.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16168 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Tests.Vs/Support/UnitTestDisposableWrapper.cs
@@ -0,0 +1,175 @@
+﻿using Epsitec.Common.Support;
+
+using Epsitec.Common.UnitTesting;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+
+
+namespace Epsitec.Common.Tests.Vs.Support
+{
+
+
+	[TestClass]
+	public class UnitTestDisposableWrapper
+	{
+
+
+		[TestMethod]
+		public void GetArgumentCheck()
+		{
+			ExceptionAssert.Throw<ArgumentNullException>
+			(
+				() => DisposableWrapper.Get (null)
+			);
+		}
+
+
+		[TestMethod]
+		public void SimpleTest()
+		{
+			int nbFinalizerCalls = 0;
+
+			Action finalizer = () => nbFinalizerCalls++;
+
+			Assert.AreEqual (0, nbFinalizerCalls);
+
+			IDisposable disposableWrapper = DisposableWrapper.Get (finalizer);
+
+			Assert.IsNotNull (disposableWrapper);
+
+			Assert.AreEqual (0, nbFinalizerCalls);
+
+			disposableWrapper.Dispose ();
+
+			Assert.AreEqual (1, nbFinalizerCalls);
+
+			disposableWrapper.Dispose ();
+
+			Assert.AreEqual (1, nbFinalizerCalls);
+
+			GC.WaitForPendingFinalizers ();
+		}
+
+
+		[TestMethod]
+		public void NoCallToDisposeTest()
+		{
+			int nbFinalizerCalls = 0;
+
+			Action finalizer = () => nbFinalizerCalls++;
+
+			Assert.AreEqual (0, nbFinalizerCalls);
+
+			IDisposable disposableWrapper = DisposableWrapper.Get (finalizer);
+
+			Assert.IsNotNull (disposableWrapper);
+
+			Assert.AreEqual (0, nbFinalizerCalls);
+
+			GC.WaitForPendingFinalizers ();
+
+			// NOTE Here an exception has probably been thrown somewhere by the garbage collector. I
+			// believe that the dev express unit test runner does handle it so it is normal that it
+			// is normal that it is not thrown here.
+			// Marc
+
+			Assert.AreEqual (0, nbFinalizerCalls);		
+		}
+
+
+		[TestMethod]
+		public void CombineArgumentCheck()
+		{
+			IDisposable disposableWrapper = DisposableWrapper.Get (() => { });
+
+			ExceptionAssert.Throw<ArgumentNullException>
+			(
+				() => DisposableWrapper.Combine (null, disposableWrapper)
+			);
+
+			ExceptionAssert.Throw<ArgumentNullException>
+			(
+				() => DisposableWrapper.Combine (disposableWrapper, null)
+			);
+		}
+
+
+		[TestMethod]
+		public void CombineSimpleTest1()
+		{
+			int nbFinalizerCalls1 = 0;
+			int nbFinalizerCalls2 = 0;
+
+			Action finalizer1 = () => nbFinalizerCalls1++;
+			Action finalizer2 = () => nbFinalizerCalls2++;
+
+			IDisposable disposableWrapper1 = DisposableWrapper.Get (finalizer1);
+			IDisposable disposableWrapper2 = DisposableWrapper.Get (finalizer2);
+
+			Assert.AreEqual (0, nbFinalizerCalls1);
+			Assert.AreEqual (0, nbFinalizerCalls2);
+
+			IDisposable disposableWrapper3 = DisposableWrapper.Combine (disposableWrapper1, disposableWrapper2);
+
+			Assert.AreEqual (0, nbFinalizerCalls1);
+			Assert.AreEqual (0, nbFinalizerCalls2);
+
+			disposableWrapper3.Dispose ();
+
+			Assert.AreEqual (1, nbFinalizerCalls1);
+			Assert.AreEqual (1, nbFinalizerCalls2);
+
+			disposableWrapper3.Dispose ();
+
+			Assert.AreEqual (1, nbFinalizerCalls1);
+			Assert.AreEqual (1, nbFinalizerCalls2);
+
+			GC.WaitForPendingFinalizers ();
+		}
+
+
+		[TestMethod]
+		public void CombineSimpleTest2()
+		{
+			int nbFinalizerCalls1 = 0;
+			int nbFinalizerCalls2 = 0;
+
+			Action finalizer1 = () => nbFinalizerCalls1++;
+			Action finalizer2 = () => nbFinalizerCalls2++;
+
+			IDisposable disposableWrapper1 = DisposableWrapper.Get (finalizer1);
+			IDisposable disposableWrapper2 = DisposableWrapper.Get (finalizer2);
+
+			Assert.AreEqual (0, nbFinalizerCalls1);
+			Assert.AreEqual (0, nbFinalizerCalls2);
+
+			IDisposable disposableWrapper3 = DisposableWrapper.Combine (disposableWrapper1, disposableWrapper2);
+			
+			Assert.AreEqual (0, nbFinalizerCalls1);
+			Assert.AreEqual (0, nbFinalizerCalls2);
+
+			disposableWrapper1.Dispose ();
+
+			Assert.AreEqual (1, nbFinalizerCalls1);
+			Assert.AreEqual (0, nbFinalizerCalls2);
+
+			disposableWrapper3.Dispose ();
+
+			Assert.AreEqual (1, nbFinalizerCalls1);
+			Assert.AreEqual (1, nbFinalizerCalls2);
+
+			disposableWrapper3.Dispose ();
+
+			Assert.AreEqual (1, nbFinalizerCalls1);
+			Assert.AreEqual (1, nbFinalizerCalls2);
+
+			GC.WaitForPendingFinalizers ();
+		}
+
+
+	}
+
+
+}
---
+++ Epsitec.Cresus/Common/Support/DisposableWraper.cs
@@ -0,0 +1,107 @@
+﻿using Epsitec.Common.Support.Extensions;
+
+using System;
+
+
+namespace Epsitec.Common.Support
+{
+
+
+	/// <summary>
+	/// The <see cref="DisposableWrapper"/> class provides an easy way to wrap a call to an
+	/// <see cref="Action"/> into a <see cref="IDisposable"/> object that will ensure that it will
+	/// be called exactly once if the object is disposed and that an
+	/// <see cref="InvalidOperationException"/> will be thrown if the object has not been disposed.
+	/// </summary>
+	public sealed class DisposableWrapper : IDisposable
+	{
+
+
+		private DisposableWrapper(Action action)
+		{
+			this.action = action;
+
+			this.actionDone = false;
+		}
+
+
+		~DisposableWrapper()
+		{
+			if (!this.actionDone)
+			{
+				throw new InvalidOperationException ("Caller forgot to call Dispose");
+			}
+		}
+
+
+		#region IDisposable Members
+
+
+		public void Dispose()
+		{
+			if (!this.actionDone)
+			{
+				this.action ();
+
+				this.actionDone = true;
+			}
+
+			GC.SuppressFinalize (this);
+		}
+
+
+		#endregion
+
+
+		private readonly Action action;
+
+
+		private bool actionDone;
+
+
+		/// <summary>
+		/// Gets an instance of <see cref="IDisposable"/> that will call <paramref name="action"/>
+		/// when disposed for the first time and that will throw an <see cref="InvalidOperationException"/>
+		/// if not disposed.
+		/// </summary>
+		/// <param name="action">The <see cref="Action"/> to execute when disposed.</param>
+		/// <returns>The <see cref="IDisposable"/> object</returns>
+		/// <exception cref="ArgumentNullException">If <paramref name="action"/> is <c>null</c>.</exception>
+		public static IDisposable Get(Action action)
+		{
+			action.ThrowIfNull ("action");
+
+			return new DisposableWrapper (action);
+		}
+
+
+		/// <summary>
+		/// Gets an instance of <see cref="IDisposable"/> that will dispose <paramref name="d1"/>
+		/// and the <paramref name="d2"/> when disposed and that will throw an
+		/// <see cref="InvalidOperationException"/> if not disposed.
+		/// </summary>
+		/// <param name="d1">The first object to dispose.</param>
+		/// <param name="d2">The second object to dispose.</param>
+		/// <returns>The <see cref="IDisposable"/> object</returns>
+		/// <exception cref="ArgumentNullException">If <paramref name="action"/> is <c>null</c>.</exception>
+		public static IDisposable Combine(IDisposable d1, IDisposable d2)
+		{
+			d1.ThrowIfNull ("d1");
+			d2.ThrowIfNull ("d2");
+
+			Action initializer = () => { };
+			
+			Action finalizer = () =>
+			{
+				d1.Dispose ();
+				d2.Dispose ();
+			};
+
+			return DisposableWrapper.Get (finalizer);
+		}
+
+
+	}
+
+
+}
