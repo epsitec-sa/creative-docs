25e339a0a38618ce87d181ac15d67dce70750bd3
Marc Bettex <bettex@epsitec.ch>
2011-04-15 11:50:18 +0000

    - Ajouté la classe InterlockedSafeCounter qui va servire à factoriser pas mal   de code redondant dans AbstractEntity et EntityContext. Cette classe est   potentiellement redondante avec la classe SafeCounter implémentée par Pierre il   y a peu. Mais je risque d'avoir besoin de faire des modifications selon ce que   je vais devoir implémenter pour garantir la thread safety de ces classes. Une   fois que je saurais exactement en quoi consistent ces modifications alors je   déciderai s'il faut fusionner les deux classes ou si elles ont des fonctionnalités   suffisement distinctes pour justifier l'existence des deux. - Ajouté des tests pour InterlockedSafeCounter.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16170 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Tests.Vs/Support/UnitTestInterlockedSafeCounter.cs
@@ -0,0 +1,71 @@
+﻿using Epsitec.Common.Support;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+
+
+namespace Epsitec.Common.Tests.Vs.Support
+{
+
+
+	[TestClass]
+	public sealed class UnitTestInterlockedSafeCounter
+	{
+		
+
+		
+		[TestMethod]
+		public void SimpleTest()
+		{
+			InterlockedSafeCounter counter = new InterlockedSafeCounter ();
+
+			Assert.IsTrue (counter.IsZero);
+
+			using (System.IDisposable disposable = counter.Enter ())
+			{
+				Assert.IsNotNull (disposable);
+
+				Assert.IsFalse (counter.IsZero);
+			}
+
+			Assert.IsTrue (counter.IsZero);
+		}
+
+
+
+		[TestMethod]
+		public void ComplexeTest()
+		{
+			InterlockedSafeCounter counter = new InterlockedSafeCounter ();
+
+			Assert.IsTrue (counter.IsZero);
+
+			List<System.IDisposable> disposables = new List<System.IDisposable>();
+
+			for (int i = 0; i < 10; i++)
+			{
+				var disposable = counter.Enter ();
+
+				Assert.IsNotNull (disposable);
+
+				disposables.Add (disposable);
+
+				Assert.IsFalse (counter.IsZero);
+			}
+
+			for (int i = 9; i >= 0; i--)
+			{
+				Assert.IsFalse (counter.IsZero);
+
+				disposables[i].Dispose ();
+			}
+
+			Assert.IsTrue (counter.IsZero);
+		}
+
+
+	}
+
+
+}
---
+++ Epsitec.Cresus/Common/Support/InterlockedSafeCounter.cs
@@ -0,0 +1,48 @@
+﻿namespace Epsitec.Common.Support
+{
+
+
+	// Note This class might be redundant with SafeCounter. I added it only because I wanted to
+	// extract that logic away from AbstractEntity and EntityContext and that this logic was similar
+	// (but different) from the one in SafeCounter. Because of thread safety stuff that I will do
+	// soon, this logic here might change depending of what and how I will need to make things thread
+	// safe in those classes. When I'll know exactly what I'll need, I'll decide if I'll merge those
+	// two classes together or if I Keep them separate.
+	// Marc
+
+	public sealed class InterlockedSafeCounter
+	{
+
+
+		public InterlockedSafeCounter()
+		{
+			this.value = 0;
+		}
+
+
+		public bool IsZero
+		{
+			get
+			{
+				return this.value == 0;
+			}
+		}
+
+
+		public System.IDisposable Enter ()
+		{
+			System.Threading.Interlocked.Increment (ref this.value);
+
+			System.Action action = () => System.Threading.Interlocked.Decrement (ref this.value);
+
+			return DisposableWrapper.Get (action);
+		}
+
+
+		private int value;
+
+
+	}
+
+
+}
