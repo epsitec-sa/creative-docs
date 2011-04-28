2c9ff5bf1c919be55a310773153179f5995ff874
Pierre Arnaud <arnaud@opac.ch>
2011-04-28 08:44:40 +0000

    - Core: complété l'énumération ArticleQuantityType.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16231 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core.Library.Business/Business/ArticleQuantityType.cs
+++ Epsitec.Cresus/Cresus.Core.Library.Business/Business/ArticleQuantityType.cs
@@ -1,4 +1,4 @@
-//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Copyright © 2010-2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
 using Epsitec.Common.Types;
@@ -8,12 +8,14 @@ namespace Epsitec.Cresus.Core.Business
 	[DesignerVisible]
 	public enum ArticleQuantityType
 	{
-		None		= 0,
+		None				= 0,
 
-		Ordered		= 10,
-		Billed		= 20,
-		Delayed		= 30,
+		Ordered				= 10,
+		Billed				= 20,
+		Delayed				= 30,
+		Shipped				= 40,
+		ShippedPreviously	= 42,
 
-		Information	= 100,
+		Information			= 100,
 	}
 }
