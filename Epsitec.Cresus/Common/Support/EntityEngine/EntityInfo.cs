fda4f91f34de4137962d271b91792a45dbac171f
Pierre Arnaud <arnaud@opac.ch>
2011-06-08 13:21:55 +0000

    - Schémas: les champs qui codent le DRUID d'une entité (par ex. GeneratorDefinition.Entity) ne sont plus de type Default.String, mais de type Data.String.EntityId.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16262 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common/Support/EntityEngine/EntityInfo.cs
+++ Epsitec.Cresus/Common/Support/EntityEngine/EntityInfo.cs
@@ -1,4 +1,4 @@
-//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Copyright © 2010-2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
 using Epsitec.Common.Support;
@@ -75,8 +75,10 @@ namespace Epsitec.Common.Support.EntityEngine
 			{
 				return null;
 			}
-
-			return structuredType.GetField (fieldAttribute.FieldId);
+			else
+			{
+				return structuredType.GetField (fieldAttribute.FieldId);
+			}
 		}
 
 		public static Caption GetFieldCaption(Expression propertyLambdaExpression)
