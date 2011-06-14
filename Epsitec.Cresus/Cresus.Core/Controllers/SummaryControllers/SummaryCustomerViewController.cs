6a55c733a985a11a183bcdefb708b717c32d0ec3
Pierre Arnaud <arnaud@opac.ch>
2011-06-14 05:42:04 +0000

    - Core: ne permet plus de créer des affaires vides.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16288 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core/Controllers/SummaryControllers/SummaryCustomerViewController.cs
+++ Epsitec.Cresus/Cresus.Core/Controllers/SummaryControllers/SummaryCustomerViewController.cs
@@ -1,4 +1,4 @@
-﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+﻿//	Copyright © 2010-2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
 using Epsitec.Common.Types;
@@ -46,6 +46,7 @@ namespace Epsitec.Cresus.Core.Controllers.SummaryControllers
 
 			wall.AddBrick (x => x.Affairs)
 				.Attribute (BrickMode.DefaultToSummarySubview)
+				.Attribute (BrickMode.HideAddButton)
 				.Template ()
 				 .Text (x => x.GetSummary ())
 				 .TextCompact (x => TextFormatter.FormatText (x.IdA))
