df217d481bac0c5aba25d4860f4caa7e325e545e
Daniel Roux <roux@epsitec.ch>
2011-06-22 15:49:47 +0000

    - Cresus.Core: Création des classes partielles manquantes, pour les entités.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16383 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core.Library.Address/Entities/LegalPersonTypeEntity.cs
@@ -0,0 +1,34 @@
+//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
+using Epsitec.Common.Support.EntityEngine;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Entities
+{
+	public partial class LegalPersonTypeEntity
+	{
+		public override FormattedText GetSummary()
+		{
+			return TextFormatter.FormatText (this.Name, "(~", this.ShortName, "~)");
+		}
+
+		public override FormattedText GetCompactSummary()
+		{
+			return TextFormatter.FormatText (this.Name);
+		}
+
+		public override string[] GetEntityKeywords()
+		{
+			return new string[] { this.Name.ToSimpleText () };
+		}
+
+		public override EntityStatus GetEntityStatus ()
+		{
+			return this.Name.GetEntityStatus ();
+		}
+	}
+}
