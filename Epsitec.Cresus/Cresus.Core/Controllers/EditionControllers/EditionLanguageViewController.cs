a356c80077ed65e37fe1a0507bf14a184fbf979b
Daniel Roux <roux@epsitec.ch>
2011-06-22 14:51:55 +0000

    - Cresus.Core: Ajouté tout ce qu'il faut pour pouvoir éditer les langues, comme test. - Cresus.Core: Dessin de quelques icônes.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16377 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionLanguageViewController.cs
@@ -0,0 +1,23 @@
+//	Copyright © 2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Cresus.Core.Entities;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Controllers.EditionControllers
+{
+	public class EditionLanguageViewController : EditionViewController<LanguageEntity>
+	{
+		protected override void CreateBricks(Bricks.BrickWall<LanguageEntity> wall)
+		{
+			wall.AddBrick ()
+				.Input ()
+				  .Field (x => x.Name)
+				  .Field (x => x.Description)
+				  .Field (x => x.IsoLanguageCode)
+				.End ();
+		}
+	}
+}
---
+++ Epsitec.Cresus/Cresus.Core/Controllers/SummaryControllers/SummaryLanguageViewController.cs
@@ -0,0 +1,18 @@
+//	Copyright © 2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Cresus.Core.Entities;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Controllers.SummaryControllers
+{
+	public class SummaryLanguageViewController : SummaryViewController<LanguageEntity>
+	{
+		protected override void CreateBricks(Bricks.BrickWall<LanguageEntity> wall)
+		{
+			wall.AddBrick (x => x);
+		}
+	}
+}
