e193d41751d4c5faf44969c7bd9c236041c48a7f
Pierre Arnaud <arnaud@opac.ch>
2011-06-23 08:52:14 +0000

    - Core: ajouté le code nécessaire à la désérialisation des réglages d'affichage dans l'entité EntityEditionSettings.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16402 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core.Library.UserManagement/Entities/EntityEditionSettingsEntity.cs
@@ -0,0 +1,59 @@
+﻿//	Copyright © 2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Collections.Generic;
+using System.Linq;
+using Epsitec.Cresus.Core.Widgets.Tiles;
+using Epsitec.Cresus.Core.Business;
+
+namespace Epsitec.Cresus.Core.Entities
+{
+	partial class EntityEditionSettingsEntity
+	{
+		/// <summary>
+		/// Gets the display settings. This will deserialize the settings when accessed
+		/// for the first time.
+		/// </summary>
+		[System.Diagnostics.DebuggerBrowsable (System.Diagnostics.DebuggerBrowsableState.Never)]
+		public TileEntityDisplaySettings DisplaySettings
+		{
+			get
+			{
+				this.DeserializeSettingsIfNeeded ();
+				
+				return this.settings;
+			}
+		}
+
+		
+		public void PersistSettings(IBusinessContext context)
+		{
+			if (this.settings != null)
+			{
+				if (this.SerializedSettings.IsNull ())
+				{
+					this.SerializedSettings = context.CreateEntity<XmlBlobEntity> ();
+				}
+
+				this.SerializeSettings ();
+			}
+		}
+
+		
+		private void SerializeSettings()
+		{
+			this.SerializedSettings.XmlData = this.settings.Save ("settings");
+		}
+
+		private void DeserializeSettingsIfNeeded()
+		{
+			if (this.settings == null)
+			{
+				this.settings = TileEntityDisplaySettings.Restore (this.SerializedSettings.XmlData);
+			}
+		}
+
+
+		private TileEntityDisplaySettings settings;
+	}
+}
