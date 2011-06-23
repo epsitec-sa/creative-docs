7140a48188a67a813a912cd2c77aae8cb4cd4423
Daniel Roux <roux@epsitec.ch>
2011-06-23 07:06:01 +0000

    - Cresus.Core: Nouvelles tuiles SummaryStateProvinceCountyViewController et EdtionStateProvinceCountyViewController. - Cresus.Core: Nouvelle classe partielle StateProvinceCountyEntity. - Cresus.Core: Dessiné quelques icônes.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16396 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core.Library.Address/Entities/StateProvinceCountyEntity.cs
@@ -0,0 +1,40 @@
+//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
+using Epsitec.Common.Support.EntityEngine;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Entities
+{
+	public partial class StateProvinceCountyEntity
+	{
+		public override FormattedText GetSummary()
+		{
+			return TextFormatter.FormatText (this.Name, "(~", this.RegionCode, "~)");
+		}
+
+		public override FormattedText GetCompactSummary()
+		{
+			return TextFormatter.FormatText (this.Name);
+		}
+
+		public override string[] GetEntityKeywords()
+		{
+			return new string[] { this.Name.ToSimpleText (), this.RegionCode };
+		}
+
+		public override EntityStatus GetEntityStatus()
+		{
+			using (var a = new EntityStatusAccumulator ())
+			{
+				a.Accumulate (this.Name.GetEntityStatus ());
+				a.Accumulate (this.RegionCode.GetEntityStatus ());
+
+				return a.EntityStatus;
+			}
+		}
+	}
+}
---
+++ Epsitec.Cresus/Cresus.Core/Controllers/EditionControllers/EditionStateProvinceCountyViewController.cs
@@ -0,0 +1,24 @@
+//	Copyright © 2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Cresus.Core.Entities;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Controllers.EditionControllers
+{
+	public class EditionStateProvinceCountyViewController : EditionViewController<StateProvinceCountyEntity>
+	{
+		protected override void CreateBricks(Bricks.BrickWall<StateProvinceCountyEntity> wall)
+		{
+			wall.AddBrick ()
+				.Input ()
+				  .Field (x => x.Name)
+				  .Field (x => x.RegionCode)
+				  .Field (x => x.Country)
+				.End ()
+				;
+		}
+	}
+}
