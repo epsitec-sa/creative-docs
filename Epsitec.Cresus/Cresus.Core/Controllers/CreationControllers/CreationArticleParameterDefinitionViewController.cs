4bd637d2af2ea9b7a268df018d330049945dbd34
Daniel Roux <roux@epsitec.ch>
2011-06-24 07:17:19 +0000

    - Cresus.Core: Nouveau contrôleur CreationArticleParameterDefinitionViewController.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16423 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core/Controllers/CreationControllers/CreationArticleParameterDefinitionViewController.cs
@@ -0,0 +1,76 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
+using Epsitec.Common.Support.EntityEngine;
+using Epsitec.Common.Widgets;
+
+using Epsitec.Cresus.Core.Business;
+using Epsitec.Cresus.Core.Entities;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Controllers.CreationControllers
+{
+	public class CreationArticleParameterDefinitionViewController : CreationViewController<AbstractArticleParameterDefinitionEntity>
+	{
+		protected override void CreateUI()
+		{
+			using (var builder = new UIBuilder (this))
+			{
+				builder.CreatePanelTitleTile ("Data.ArticleParameterDefinition", "Paramètre d'article à créer...");
+
+				this.CreateUINewEnumButton (builder);
+				this.CreateUINewOptionButton (builder);
+				this.CreateUINewNumericButton (builder);
+				this.CreateUINewFreeTextButton (builder);
+				
+				builder.EndPanelTitleTile ();
+			}
+		}
+
+
+		private void CreateUINewEnumButton(UIBuilder builder)
+		{
+			builder.CreateCreationButton<AbstractArticleParameterDefinitionEntity> (this, "Enumération", "Crée un paramètre énuméré", this.SetupEnum);
+		}
+
+		private void CreateUINewOptionButton(UIBuilder builder)
+		{
+			builder.CreateCreationButton<AbstractArticleParameterDefinitionEntity> (this, "Option", "Crée un paramètre optionnel", this.SetupOption);
+		}
+
+		private void CreateUINewNumericButton(UIBuilder builder)
+		{
+			builder.CreateCreationButton<AbstractArticleParameterDefinitionEntity> (this, "Numérique", "Crée un paramètre numérique", this.SetupNumeric);
+		}
+
+		private void CreateUINewFreeTextButton(UIBuilder builder)
+		{
+			builder.CreateCreationButton<AbstractArticleParameterDefinitionEntity> (this, "Texte libre", "Crée un paramètre texte libre", this.SetupFreeText);
+		}
+
+
+		private void SetupEnum(BusinessContext context, AbstractArticleParameterDefinitionEntity param)
+		{
+			//?param = context.CreateEntity<EnumValueArticleParameterDefinitionEntity> ();
+		}
+
+		private void SetupOption(BusinessContext context, AbstractArticleParameterDefinitionEntity param)
+		{
+			//?param = context.CreateEntity<OptionValueArticleParameterDefinitionEntity> ();
+		}
+
+		private void SetupNumeric(BusinessContext context, AbstractArticleParameterDefinitionEntity param)
+		{
+			//?param = context.CreateEntity<NumericValueArticleParameterDefinitionEntity> ();
+		}
+
+		private void SetupFreeText(BusinessContext context, AbstractArticleParameterDefinitionEntity param)
+		{
+			//?param = context.CreateEntity<FreeTextValueArticleParameterDefinitionEntity> ();
+		}
+
+	}
+}
