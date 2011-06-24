11f8a7630766ac58437695e481a243adcfeccba1
Daniel Roux <roux@epsitec.ch>
2011-06-24 13:18:45 +0000

    - Cresus.Core: Nouvelle classe partielle ArticleSupplyEntity. - Cresus.Core: Nouvelle classe partielle ArticleStockLocationEntity.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16433 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core.Library.Business/Entities/ArticleStockLocationEntity.cs
@@ -0,0 +1,44 @@
+//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
+using Epsitec.Common.Support.EntityEngine;
+
+using Epsitec.Cresus.Core.Helpers;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Entities
+{
+	public partial class ArticleStockLocationEntity
+	{
+		public override FormattedText GetSummary()
+		{
+			return TextFormatter.FormatText (this.Name);
+		}
+
+		public override FormattedText GetCompactSummary()
+		{
+			return TextFormatter.FormatText (this.Name);
+		}
+
+
+		public override string[] GetEntityKeywords()
+		{
+			return new string[] { this.Name.ToSimpleText () };
+		}
+
+
+		public override EntityStatus GetEntityStatus()
+		{
+			using (var a = new EntityStatusAccumulator ())
+			{
+				a.Accumulate (this.Name.GetEntityStatus ());
+				a.Accumulate (this.Description.GetEntityStatus ().TreatAsOptional ());
+
+				return a.EntityStatus;
+			}
+		}
+	}
+}
