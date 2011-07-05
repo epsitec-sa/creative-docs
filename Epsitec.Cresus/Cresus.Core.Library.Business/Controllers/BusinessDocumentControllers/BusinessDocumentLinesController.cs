05298eb5e68c408a283c62599d8f2ed4096bd898
Daniel Roux <roux@epsitec.ch>
2011-07-05 15:19:39 +0000

    - Cresus.Core: Refactoring: Extraction du tableau dans une classe séparée (ArticleLinesController).
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16513 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core.Library.Business/Controllers/BusinessDocumentControllers/BusinessDocumentLinesController.cs
+++ Epsitec.Cresus/Cresus.Core.Library.Business/Controllers/BusinessDocumentControllers/BusinessDocumentLinesController.cs
@@ -73,24 +73,8 @@ namespace Epsitec.Cresus.Core.Controllers.BusinessDocumentControllers
 
 			{
 				//	Crée la liste.
-				var tile = new FrameBox
-				{
-					Parent = frame,
-					Dock = DockStyle.Fill,
-					DrawFullFrame = true,
-					TabIndex = tabIndex++,
-				};
-
-				this.table = new CellTable
-				{
-					Parent = tile,
-					StyleH = CellArrayStyles.ScrollNorm | CellArrayStyles.Separator | CellArrayStyles.Header,
-					StyleV = CellArrayStyles.ScrollNorm | CellArrayStyles.Separator | CellArrayStyles.SelectLine,
-					DefHeight = BusinessDocumentLinesController.lineHeight,
-					Margins = new Margins (2),
-					Dock = DockStyle.Fill,
-					TabIndex = tabIndex++,
-				};
+				this.articleLinesController = new ArticleLinesController (this.documentMetadataEntity, this.businessDocumentEntity);
+				this.articleLinesController.CreateUI (frame);
 			}
 
 			{
@@ -237,192 +221,10 @@ namespace Epsitec.Cresus.Core.Controllers.BusinessDocumentControllers
 
 		public void UpdateUI(int? sel = null)
 		{
-			int rows = this.businessDocumentEntity.Lines.Count;
-			int columns = this.ColumnTypes.Count ();
-
-			this.table.SetArraySize (columns, rows);
-
-			int column = 0;
-			foreach (var columnType in this.ColumnTypes)
-			{
-				this.table.SetWidthColumn (column, this.GetColumnWidth (columnType));
-				this.table.SetHeaderTextH (column, this.GetColumnName (columnType));
-
-				column++;
-			}
-
-			if (sel == null)
-			{
-				sel = this.table.SelectedRow;
-			}
-
-			for (int row=0; row<rows; row++)
-			{
-				this.TableFillRow (row);
-				this.TableUpdateRow (row);
-			}
-
-			sel = System.Math.Min (sel.Value, rows-1);
-			if (sel != -1)
-			{
-				this.table.SelectRow (sel.Value, true);
-			}
+			this.articleLinesController.UpdateUI (this.GetCellContent, sel);
 		}
 
-		private void TableFillRow(int row)
-		{
-			//	Peuple une ligne de la table, si nécessaire.
-			int column = 0;
-			foreach (var columnType in this.ColumnTypes)
-			{
-				if (columnType == ColumnType.Group)
-				{
-					// TODO: ...
-				}
-				else
-				{
-					if (this.table[column, row].IsEmpty)
-					{
-						var text = new StaticText
-						{
-							PreferredHeight = BusinessDocumentLinesController.lineHeight,
-							ContentAlignment = this.GetRowColumnContentAlignment (row, columnType),
-							Dock = DockStyle.Fill,
-							Margins = new Margins (4, 4, 0, 0),
-						};
-
-						this.table[column, row].Insert (text);
-					}
-				}
-
-				column++;
-			}
-		}
-
-		private void TableUpdateRow(int row)
-		{
-			//	Met à jour le contenu d'une ligne de la table.
-			int column = 0;
-			foreach (var columnType in this.ColumnTypes)
-			{
-				if (this.table[column, row].Children.Count != 0)
-				{
-					var text = this.table[column, row].Children[0] as StaticText;
-
-					if (text != null)
-					{
-						text.FormattedText = this.GetRowColumnText (row, columnType);
-					}
-				}
-
-				column++;
-			}
-
-			this.table.SelectRow (row, false);
-		}
-
-
-		private IEnumerable<ColumnType> ColumnTypes
-		{
-			//	Retourne les colonnes visibles dans la table, de gauche à droite.
-			get
-			{
-				yield return ColumnType.Group;
-				yield return ColumnType.Id;
-				yield return ColumnType.Description;
-				yield return ColumnType.Quantity;
-				yield return ColumnType.Unit;
-				yield return ColumnType.Type;
-				yield return ColumnType.Price;
-				yield return ColumnType.Total;
-			}
-		}
-
-		private enum ColumnType
-		{
-			Group,
-			Id,
-			Quantity,
-			Unit,
-			Type,
-			Description,
-			Price,
-			Total,
-		}
-
-		private int GetColumnWidth(ColumnType columnType)
-		{
-			switch (columnType)
-			{
-				case ColumnType.Group:
-					return 30;
-
-				case ColumnType.Quantity:
-					return 40;
-
-				case ColumnType.Unit:
-					return 50;
-
-				case ColumnType.Type:
-					return 70;
-
-				case ColumnType.Id:
-					return 50;
-
-				case ColumnType.Description:
-					return 180;
-
-				case ColumnType.Price:
-				case ColumnType.Total:
-					return 70;
-
-				default:
-					return 100;
-			}
-		}
-
-		private string GetColumnName(ColumnType columnType)
-		{
-			switch (columnType)
-			{
-				case ColumnType.Quantity:
-					return "Nb";
-
-				case ColumnType.Unit:
-					return "Unité";
-
-				case ColumnType.Type:
-					return "Type";
-
-				case ColumnType.Id:
-					return "N°";
-
-				case ColumnType.Description:
-					return "Désignation";
-
-				case ColumnType.Price:
-					return "Prix";
-
-				case ColumnType.Total:
-					return "Total";
-
-				default:
-					return null;
-			}
-		}
-
-		private ContentAlignment GetRowColumnContentAlignment(int row, ColumnType columnType)
-		{
-			if (columnType == ColumnType.Quantity ||
-				columnType == ColumnType.Price)
-			{
-				return ContentAlignment.MiddleRight;
-			}
-
-			return ContentAlignment.MiddleLeft;
-		}
-
-		private FormattedText GetRowColumnText(int row, ColumnType columnType)
+		public FormattedText GetCellContent(int row, ColumnType columnType)
 		{
 			var line = this.businessDocumentEntity.Lines[row];
 
@@ -560,6 +362,6 @@ namespace Epsitec.Cresus.Core.Controllers.BusinessDocumentControllers
 		private readonly DocumentMetadataEntity documentMetadataEntity;
 		private readonly BusinessDocumentEntity businessDocumentEntity;
 
-		private CellTable								table;
+		private ArticleLinesController articleLinesController;
 	}
 }
---
+++ Epsitec.Cresus/Cresus.Core.Library.Business/Controllers/BusinessDocumentControllers/ColumnType.cs
@@ -0,0 +1,17 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+namespace Epsitec.Cresus.Core.Controllers.BusinessDocumentControllers
+{
+	public enum ColumnType
+	{
+		Group,
+		Id,
+		Quantity,
+		Unit,
+		Type,
+		Description,
+		Price,
+		Total,
+	}
+}
