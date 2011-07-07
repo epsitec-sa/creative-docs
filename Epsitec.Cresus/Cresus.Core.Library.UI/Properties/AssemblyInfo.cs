43a028ac5496d74f3e73ee6da7ba80c2a99ab00a
Pierre Arnaud <arnaud@opac.ch>
2011-07-07 04:53:29 +0000

    - Ajouté un module Cresus.Core.Library.UI.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16531 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core.Library.UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿//	Copyright © 2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Reflection;
+
+[assembly: AssemblyTitle ("Cresus.Core.Library.UI")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyProduct ("Crésus")]
+[assembly: AssemblyCopyright ("Copyright © 2011, EPSITEC SA, 1400 Yverdon-les-Bains, Switzerland")]
