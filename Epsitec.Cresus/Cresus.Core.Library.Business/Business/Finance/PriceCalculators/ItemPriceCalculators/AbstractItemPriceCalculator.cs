156dbdb9067281a670c0a6cfeb7ca5949a5fc28e
Pierre Arnaud <arnaud@opac.ch>
2011-07-11 12:08:27 +0000

    - Core: début des travaux dans le bas niveau des calculateurs spécifiques.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16558 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core.Library.Business/Business/Finance/PriceCalculators/ItemPriceCalculators/AbstractItemPriceCalculator.cs
+++ Epsitec.Cresus/Cresus.Core.Library.Business/Business/Finance/PriceCalculators/ItemPriceCalculators/AbstractItemPriceCalculator.cs
@@ -1,20 +1,22 @@
-//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Copyright © 2010-2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
-
-using Epsitec.Common.Support.Extensions;
-using Epsitec.Common.Support.EntityEngine;
-using Epsitec.Common.Types;
-using Epsitec.Common.Types.Collections;
-
-using Epsitec.Cresus.Core.Entities;
 
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Epsitec.Cresus.Core.Business.Finance.PriceCalculators.ItemPriceCalculators
 {
+	/// <summary>
+	/// The <c>AbstractItemPriceCalculator</c> class is the base class for all
+	/// specific price calculators.
+	/// </summary>
 	public abstract class AbstractItemPriceCalculator
 	{
+		/// <summary>
+		/// Applies the final price adjustment. This is a relative amount, expressed
+		/// in the active currency. <c>0</c> means no adjustment.
+		/// </summary>
+		/// <param name="adjustment">The adjustment.</param>
 		public abstract void ApplyFinalPriceAdjustment(decimal adjustment);
 	}
 }
