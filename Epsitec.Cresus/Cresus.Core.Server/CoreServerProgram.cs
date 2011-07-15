3c1f0f7edcac3c61a1093c9df0f598294e2bc45a
Jonas Schmidt <jonas@smtp1.infomaniak.ch>
2011-07-15 11:22:02 +0000

    - WebCore: Les panels utilisent maintenant une icone
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@16679 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core.Server/CoreServerProgram.cs
+++ Epsitec.Cresus/Cresus.Core.Server/CoreServerProgram.cs
@@ -24,13 +24,13 @@ namespace Epsitec.Cresus.Core.Server
 		{
 			Epsitec.Common.Document.Engine.Initialize ();
 
-//-			this.ExperimentalProfiling ();
-			this.ExperimentalCode ();
+			//this.ExperimentalProfiling ();
+			CoreServerProgram.ExperimentalCode ();
 
 			var server = new CoreServer ();
 			var session = server.CreateSession ();
 
-			this.ExperimentalEntityManipulations (session);
+			//this.ExperimentalEntityManipulations (session);
 
 			session.DisposeBusinessContext ();
 			server.DeleteSession (session.Id);
@@ -119,29 +119,37 @@ namespace Epsitec.Cresus.Core.Server
 			}
 		}
 
-		private void ExperimentalCode()
+		private static void ExperimentalCode()
 		{
-			BuildControllers (new CustomerEntity (), Controllers.ViewControllerMode.Summary);
-			BuildControllers (new CustomerEntity (), Controllers.ViewControllerMode.Edition);
-			BuildControllers (new MailContactEntity (), Controllers.ViewControllerMode.Summary);
-			BuildControllers (new AffairEntity (), Controllers.ViewControllerMode.Summary);
+			// Recrée un fichier CSS vide
+			System.IO.File.Create (CoreServerProgram.cssFilename).Close ();
+
+			BuildController (new CustomerEntity (), Controllers.ViewControllerMode.Summary);
+			BuildController (new CustomerEntity (), Controllers.ViewControllerMode.Edition);
+			BuildController (new MailContactEntity (), Controllers.ViewControllerMode.Summary);
+			BuildController (new AffairEntity (), Controllers.ViewControllerMode.Summary);
 		}
 
-		private static void BuildControllers(AbstractEntity entity, ViewControllerMode mode)
+		/// <summary>
+		/// Crée un controlleur en fonction d'une entité et d'une mode de la vue
+		/// </summary>
+		/// <param name="entity"></param>
+		/// <param name="mode"></param>
+		private static void BuildController(AbstractEntity entity, ViewControllerMode mode)
 		{
+			// Récupération du brickwall
 			var customerSummaryWall = CoreSession.GetBrickWall (entity, mode);
 			
 			var name = CoreServerProgram.GetControllerName (entity, mode);
 			var path = CoreServerProgram.GetJsFilePath (name);
 
+			// Ouverture du panel principal
 			var jscontent = "Ext.define('";
 			jscontent += name;
 			jscontent += "', {";
 			jscontent += "extend: 'Epsitec.Cresus.Core.Static.SummaryPanel',";
 			jscontent += string.Format ("title: '{0}',", name);
 			jscontent += "items: [";
-
-			// Do something with the bricks
 
 			foreach (var brick in customerSummaryWall.Bricks)
 			{
@@ -150,44 +158,61 @@ namespace Epsitec.Cresus.Core.Server
 				//{
 				//}
 
-				//if (Brick.ContainsProperty (brick, BrickPropertyKey.AsType))
-				//{
-				//    var t = Brick.GetProperty (brick, BrickPropertyKey.AsType);
-				//    var b = t.Brick;
-				//    var a = b.GetType ().GetGenericArguments ();
-				//    CreateDefaultProperties (brick, a.First ());
-				//}
-				//else
-				//{
-				//    CreateDefaultProperties (brick, entity.GetType ());
-				//}
+				// Propriétés par défaut
+				CoreServerProgram.CreateDefaultTextProperties (brick);
 
-				CreateDefaultTextProperties (brick);
-
-				jscontent += CreatePanelContent (brick);
+				// Création du panel pour cette tuile
+				jscontent += CoreServerProgram.CreatePanelContent (brick);
 			}
 
-
+			// Fermeture du panel principal et écriture dans le fichier
 			jscontent += "]";
 			jscontent += "});";
 			System.IO.File.WriteAllText (path, jscontent);
 
 		}
 
+
+
+		/// <summary>
+		/// Retourne le nom d'un controlleur javascript en fonction d'une entité et du mode de la vue
+		/// </summary>
+		/// <param name="entity"></param>
+		/// <param name="mode"></param>
+		/// <returns></returns>
+		private static string GetControllerName(AbstractEntity entity, ViewControllerMode mode)
+		{
+			return string.Format ("Epsitec.Cresus.Core.Controllers.{0}Controllers.{1}", mode, entity.GetType ().Name);
+		}
+
+		/// <summary>
+		/// Obtient le nom de fichier javascript à partir d'un namespace/classname
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
 		private static string GetJsFilePath(string name)
 		{
-			var path = string.Format ("web/js/{0}.js", name.Replace ('.', '/'));
+			var path = string.Format (CoreServerProgram.jsFilename, name.Replace ('.', '/'));
 			CoreServerProgram.EnsureDirectoryStructureExists (path);
 			return path;
 		}
 
+		/// <summary>
+		/// Obtient le nom de fichier image à partir d'un namespace/classname
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
 		private static string GetImageFilePath(string name)
 		{
-			var path = string.Format ("web/images/{0}.png", name.Replace ('.', '/'));
+			var path = string.Format (CoreServerProgram.imagesFilename, name.Replace ('.', '/'));
 			CoreServerProgram.EnsureDirectoryStructureExists (path);
 			return path;
 		}
 
+		/// <summary>
+		/// Vérifie qu'un dossier existe, sinon le crée
+		/// </summary>
+		/// <param name="path"></param>
 		private static void EnsureDirectoryStructureExists(string path)
 		{
 			var dir  = System.IO.Path.GetDirectoryName (path);
@@ -197,39 +222,71 @@ namespace Epsitec.Cresus.Core.Server
 				System.IO.Directory.CreateDirectory (dir);
 			}
 		}
-		private static string CreateIcon(Brick brick)
+
+		/// <summary>
+		/// Crée une image à partir d'une brique.
+		/// Cette image est l'icône représentant la brique.
+		/// </summary>
+		/// <param name="brick">Brick à utiliser pour créer une icône</param>
+		/// <returns>Pair clé/valeur contenant respectivement le nom de l'icône et le nom de fichier créé</returns>
+		private static KeyValuePair<string, string> CreateIcon(Brick brick)
 		{
+			// Pas d'icone pour cette brique
 			if (!Brick.ContainsProperty (brick, BrickPropertyKey.Icon))
 			{
-				return null;
+				return default (KeyValuePair<string, string>);
 			}
 
+			// Récupération du nom de ressource de l'icone
 			var iconRes = Brick.GetProperty (brick, BrickPropertyKey.Icon).StringValue;
 			var iconName = iconRes;
-
 			if (iconName.StartsWith ("manifest:"))
 			{
 				iconName = iconName.Substring (9);
 			}
 
+			// Récupération de l'image depuis les ressources
 			var icon = ImageProvider.Default.GetImage (iconRes, Resources.DefaultManager);
-
 			if (icon == null)
 			{
-				return null;
+				return default (KeyValuePair<string, string>);
 			}
-
 			var bitmap = icon.BitmapImage;
 
+			// Sauvegarde de l'image
 			var bytes = bitmap.Save (ImageFormat.Png);
 			string path = CoreServerProgram.GetImageFilePath (iconName);
 			System.IO.File.WriteAllBytes (path, bytes);
 
-			return path;
+			return new KeyValuePair<string, string> (iconName, path);
 		}
 
+		/// <summary>
+		/// Génère du contenu CSS pour être capable d'appeler une icone depuis le HTML
+		/// </summary>
+		/// <param name="iconName">Nom de l'icone (ressource)</param>
+		/// <param name="path">Nom du fichier dans lequel est enregistrée l'icone</param>
+		/// <returns>Nom de la classe CSS permettant d'utiliser l'icone</returns>
+		private static string CreateCssFromIcon(string iconName, string path)
+		{
+			// CSS n'aime pas les "."
+			var cssClass = iconName.Replace ('.', '-').ToLower ();
+			var css = string.Format (".{0} {{ ", cssClass);
+			css += string.Format ("background-image: url({0}) !important;", path.Replace ("web", ".."));
+			css += "background-size: 16px 16px;";
+			css += "} ";
 
-		private static object CreatePanelContent(Brick brick)
+			System.IO.File.AppendAllText (CoreServerProgram.cssFilename, css);
+
+			return cssClass;
+		}
+
+		/// <summary>
+		/// Crée un panel en fonction d'une brique
+		/// </summary>
+		/// <param name="brick">Brique à utiliser</param>
+		/// <returns>Contenu javascript permettant de créer le panel</returns>
+		private static string CreatePanelContent(Brick brick)
 		{
 			var jscontent = "Ext.create('Epsitec.Cresus.Core.Static.TilePanel', {";
 
@@ -237,34 +294,27 @@ namespace Epsitec.Cresus.Core.Server
 			jscontent += Brick.GetProperty (brick, BrickPropertyKey.Title).StringValue;
 			jscontent += "',";
 
+			// Données de test
 			jscontent += "data: { name: 'Jonas' },";
 
-
-			//jscontent += "html: [";
-
-			//jscontent += "'";
-			//jscontent += Brick.GetProperty (brick, BrickPropertyKey.Text).StringValue;
-			//jscontent += "<br/><b>plop</b>',";
-
-			//jscontent += "],";
-
-			var icon = CreateIcon (brick);
-			if (icon != null)
+			// Création d'une icone si disponible
+			var icon = CoreServerProgram.CreateIcon (brick);
+			if (!icon.Equals (default (KeyValuePair<string, string>)))
 			{
-				jscontent += string.Format ("icon: '{0}',", icon);
+				var iconCls = CoreServerProgram.CreateCssFromIcon (icon.Key, icon.Value);
+				jscontent += string.Format ("iconCls: '{0}',", iconCls);
 			}
 
-
+			// Fermeture du panel
 			jscontent += "}),";
 
 			return jscontent;
 		}
 
-		private static string GetControllerName(AbstractEntity entity, ViewControllerMode mode)
-		{
-			return string.Format ("Epsitec.Cresus.Core.Controllers.{0}Controllers.{1}", mode, entity.GetType ().Name);
-		}
-
+		/// <summary>
+		/// Met des propriétés par défaut dans la brique
+		/// </summary>
+		/// <param name="brick"></param>
 		private static void CreateDefaultTextProperties(Brick brick)
 		{
 			if (!Brick.ContainsProperty (brick, BrickPropertyKey.Text))
@@ -279,6 +329,11 @@ namespace Epsitec.Cresus.Core.Server
 				Brick.AddProperty (brick, new BrickProperty (BrickPropertyKey.TextCompact, expression));
 			}
 		}
+
+		// Nom des fichiers générés
+		private static readonly string cssFilename = "web/css/generated.css";
+		private static readonly string jsFilename = "web/js/{0}.js";
+		private static readonly string imagesFilename = "web/images/{0}.png";
 
 		/*
 		public enum BrickPropertyKey
