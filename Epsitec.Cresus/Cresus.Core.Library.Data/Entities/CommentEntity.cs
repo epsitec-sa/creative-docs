d2dfc623922996121ee3ec25aba46fcfc0c74be1
Pierre Arnaud <arnaud@opac.ch>
2011-08-29 05:21:34 +0000

    - Core: corrigé CommentEntity pour afficher correctement le texte d'un commentaire dans une tuile résumée.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17042 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core.Library.Data/Entities/CommentEntity.cs
+++ Epsitec.Cresus/Cresus.Core.Library.Data/Entities/CommentEntity.cs
@@ -1,4 +1,4 @@
-//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Copyright © 2010-2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
 using Epsitec.Common.Types;
@@ -11,6 +11,11 @@ namespace Epsitec.Cresus.Core.Entities
 {
 	public partial class CommentEntity
 	{
+		public override FormattedText GetSummary()
+		{
+			return TextFormatter.FormatText (string.Join ("<br/>", this.Text.Lines.Take (3).ToArray ()));
+		}
+
 		public override FormattedText GetCompactSummary()
 		{
 			return TextFormatter.FormatText (this.Text.Lines.FirstOrDefault ());
