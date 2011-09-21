3c65b7ad6e49a3ea8c23dcb294219ffdcb09f3be
Pierre Arnaud <arnaud@opac.ch>
2011-09-21 20:20:24 +0000

    - Core: travaux mineurs liés à ISO-3166.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17151 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core.Maintenance/Engine.cs
@@ -0,0 +1,69 @@
+﻿//	Copyright © 2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Core.Business;
+using Epsitec.Cresus.Core.Entities;
+using Epsitec.Cresus.Core.Repositories;
+using Epsitec.Cresus.Core.Server;
+
+using Epsitec.Data.Platform;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Maintenance
+{
+	public sealed class Engine
+	{
+		public void Refresh()
+		{
+			var session = CoreServer.Instance.CreateSession ();
+
+			Engine.ImportCountries (session);
+
+			CoreServer.Instance.DeleteSession (session);
+		}
+
+		public static void ImportCountries(CoreSession session)
+		{
+			var context = session.GetBusinessContext ();
+			var repository = session.CoreData.GetRepository<CountryEntity> (context.DataContext);
+
+			foreach (var alpha2Code in Iso3166.GetAlpha2Codes ())
+			{
+				var country          = Engine.GetCountry (context, repository, alpha2Code);
+				var multilingualName = new MultilingualText ();
+
+				foreach (var languageCode in Engine.GetLanguageCodes ())
+				{
+					multilingualName.SetText (languageCode, Iso3166.GetCountryInformation (alpha2Code, languageCode).Name);
+				}
+
+				country.CountryCode = alpha2Code;
+				country.Name        = multilingualName.GetGlobalText ();
+			}
+
+			context.SaveChanges ();
+		}
+
+
+		private static CountryEntity GetCountry(BusinessContext context, Repository<CountryEntity> repository, string alpha2Code)
+		{
+			var example = repository.CreateExample ();
+
+			example.CountryCode = alpha2Code;
+
+			return repository.GetByExample (example).FirstOrDefault () ?? context.CreateEntity<CountryEntity> ();
+		}
+		
+		private static IEnumerable<string> GetLanguageCodes()
+		{
+			yield return "fr";
+			yield return "de";
+			yield return "en";
+			yield return "it";
+		}
+	}
+}
--- Epsitec.Cresus/Data.Platform/Iso3166.cs
+++ Epsitec.Cresus/Data.Platform/Iso3166.cs
@@ -7,6 +7,11 @@ using System.Net;
 
 namespace Epsitec.Data.Platform
 {
+	/// <summary>
+	/// The <c>Iso3166</c> class retrieves country information as defined by ISO.
+	/// The ISO-3166 standard defines the two-letter codes (such as <c>CH</c> or
+	/// <c>FR</c>).
+	/// </summary>
 	public static class Iso3166
 	{
 		public static IEnumerable<string> GetAlpha2Codes()
@@ -27,6 +32,9 @@ namespace Epsitec.Data.Platform
 
 		public static CountryInformation GetCountryInformation(string code, string language)
 		{
+			//	We are querying the GeoNames server, which provides up to 30'000 replies
+			//	par day with our free account.
+
 			string uri = string.Format ("http://api.geonames.org/countryInfoCSV?lang={0}&country={1}&username=epsitec", language, code);
 			string[] values = Iso3166.DownloadLines (uri).Skip (1).First ().Split ('\t');
 
