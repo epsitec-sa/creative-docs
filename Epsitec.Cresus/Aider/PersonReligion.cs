5bfc224ba541ccb215faa98fea9a2b300f5b6857
Pierre Arnaud <arnaud@opac.ch>
2011-10-03 04:52:37 +0000

    - AIDER: fichiers manquants.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17220 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/PersonReligion.cs
@@ -0,0 +1,17 @@
+//	Copyright © 2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Aider
+{
+	[DesignerVisible]
+	public enum PersonReligion
+	{
+		Unknown = 0,
+		Protestant = 1,
+	}
+}
