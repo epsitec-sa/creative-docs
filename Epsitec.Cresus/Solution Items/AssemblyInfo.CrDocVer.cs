98be7b89794bc9cb17fa491359bb63d157cedf34
Pierre Arnaud <arnaud@opac.ch>
2011-10-12 05:35:06 +0000

    - Version 3.3.6 de CrDoc publiée.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17279 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Solution Items/AssemblyInfo.CrDocVer.cs
+++ Epsitec.Cresus/Solution Items/AssemblyInfo.CrDocVer.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture ("")]
 [assembly: AssemblyConfiguration ("")]
 
-[assembly: AssemblyVersion ("3.3.6.1140")]
-[assembly: AssemblyFileVersion ("3.3.6.1140")]
+[assembly: AssemblyVersion ("3.3.6.1141")]
+[assembly: AssemblyFileVersion ("3.3.6.1141")]
 
 [assembly: ComVisible (false)]
