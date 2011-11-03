a7cace4efb7980ce91ef24513c12b121e6298d85
Pierre Arnaud <arnaud@opac.ch>
2011-11-03 05:50:58 +0000

    - CodeSigning, améliorié la sélectivité du certificat numérique utilisé pour la signature des exécutables. - Documentation, inclus la marche à suivre de Marc pour installer Core/web.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17377 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/External/CodeSigning/sign-opac-single.bat
+++ Epsitec.Cresus/External/CodeSigning/sign-opac-single.bat
@@ -5,4 +5,4 @@ cd %~dp0
 
 echo Signing %1
 
-S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n OPaC /d %2 /t http://timestamp.verisign.com/scripts/timstamp.dll %1
+S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n OPaC /i Verisign /d %2 /t http://timestamp.verisign.com/scripts/timstamp.dll %1
--- Epsitec.Cresus/External/CodeSigning/sign-opac.bat
+++ Epsitec.Cresus/External/CodeSigning/sign-opac.bat
@@ -5,4 +5,4 @@ cd %~dp0
 
 echo Signing %1
 
-for %%f in (%1) do S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n OPaC /d "Creative Docs .NET" /t http://timestamp.verisign.com/scripts/timstamp.dll "%%f"
+for %%f in (%1) do S:\Epsitec.Cresus\External\CodeSigning\signtool.exe sign /a /n OPaC /i Verisign /d "Creative Docs .NET" /t http://timestamp.verisign.com/scripts/timstamp.dll "%%f"
