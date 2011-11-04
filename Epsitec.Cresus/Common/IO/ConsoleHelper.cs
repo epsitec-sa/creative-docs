d9c67d3301867f34dd5ab1bcf65d17fb5a30eb7b
Marc Bettex <bettex@epsitec.ch>
2011-11-04 09:24:25 +0000

    - Ajouté une classe qui permet de créer, détruire et utiliser une console dans une application qui n'en a pas. Typiquement c'est utile pour les "Windows application" dans Visual Studio 2010.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17395 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common/IO/ConsoleHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+using System.Runtime.InteropServices;
+
+
+namespace Epsitec.Common.IO
+{
+
+
+	/// <summary>
+	/// Little helper class that allows windows application to create a console that can be used
+	/// for output and input.
+	/// </summary>
+	/// <remarks>
+	/// Note that a process can have at most one console, so you can't create a console twice and
+	/// trying to do so might be problematic.
+	/// </remarks>
+	public static class ConsoleCreator
+	{
+
+
+		/// <summary>
+		/// Creates a console, executes the given action and deletes the console afterwards. If the
+		/// new console can't be created, an exception will be thrown and the action won't be
+		/// executed.
+		/// </summary>
+		/// <param name="action">The action to execute</param>
+		public static void RunWithConsole(Action action)
+		{
+			bool success = false;
+
+			try
+			{
+				success = ConsoleCreator.CreateConsole ();
+
+				if (!success)
+				{
+					throw new Exception ("The console could not be created.");
+				}
+
+				action ();
+			}
+			finally
+			{
+				if (success)
+				{
+					ConsoleCreator.DeleteConsole ();
+				}
+			}
+		}
+
+
+		/// <summary>
+		/// Creates a console.
+		/// </summary>
+		/// <returns>return true for success and false for failure.</returns>
+		private static bool CreateConsole()
+		{
+			return ConsoleCreator.AllocConsole ();
+		}
+
+
+		/// <summary>
+		/// Deletes a console.
+		/// </summary>
+		/// <returns>return true for success and false for failure.</returns>
+		private static bool DeleteConsole()
+		{
+			return ConsoleCreator.FreeConsole ();
+		}
+
+
+		[DllImport ("kernel32.dll", SetLastError=true)]
+		[return: MarshalAs (UnmanagedType.Bool)]
+		private static extern bool AllocConsole();
+
+
+		[DllImport ("kernel32.dll", SetLastError=true)]
+		[return: MarshalAs (UnmanagedType.Bool)]
+		private static extern bool FreeConsole();
+
+
+	}
+
+
+}
