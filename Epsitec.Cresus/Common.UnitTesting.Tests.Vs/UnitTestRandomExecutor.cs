6743e64ff351d00cc8c7de26867ffb38e55d5a65
Marc Bettex <bettex@epsitec.ch>
2011-11-11 13:05:00 +0000

    - Ajouté RandomExecutor dans Common.UnitTesting pour faciliter les tests qui doivent exécuter des actions de manière aléatoire. - Ajouté des tests pour ces fonctionnalités.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17419 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.UnitTesting.Tests.Vs/UnitTestRandomExecutor.cs
@@ -0,0 +1,91 @@
+﻿using Epsitec.Common.UnitTesting;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+
+namespace Epsitec.Common.UnitTesting.UnitTests.Vs
+{
+
+	
+	[TestClass]
+	public sealed class UnitTestRandomExecutor
+	{
+		
+		
+		[TestMethod]
+		[ExpectedException (typeof (ArgumentException))]
+		public void ArgumentCheck1()
+		{
+			Action[] actions = new Action[] { () => { } };
+
+			RandomExecutor.ExecuteRandomly (0, actions);
+		}
+		
+		
+		[TestMethod]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void ArgumentCheck2()
+		{
+			Action[] actions = null;
+			
+			RandomExecutor.ExecuteRandomly (1, actions);
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (ArgumentException))]
+		public void ArgumentCheck3()
+		{
+			Action[] actions = new Action[0];
+
+			RandomExecutor.ExecuteRandomly (1, actions);
+		}
+
+
+		[TestMethod]
+		[ExpectedException (typeof (ArgumentException))]
+		public void ArgumentCheck4()
+		{
+			Action[] actions = new Action[] { null };
+
+			RandomExecutor.ExecuteRandomly (1, actions);
+		}
+
+
+		[TestMethod]
+		public void SimpleTest()
+		{
+			for (int nbActions = 1; nbActions < 10; nbActions++)
+			{
+				var results = Enumerable.Range (0, nbActions).Select (_ => 0).ToArray ();
+				var actions = Enumerable.Range (0, nbActions).Select<int, Action> (n => () => results[n] = results[n] + 1).ToArray ();
+
+				var nbExecutions = 1000000;
+				var expectedResult = nbExecutions / nbActions;
+				var tolerance = expectedResult / 100;
+				var lowerBound = expectedResult - tolerance;
+				var upperBound = expectedResult + tolerance;
+				
+				RandomExecutor.ExecuteRandomly (nbExecutions, actions);
+
+				for (int j = 0; j < nbActions; j++)
+				{
+					Assert.IsTrue (results[j] > lowerBound);
+					Assert.IsTrue (results[j] < upperBound);
+				}
+
+				Assert.AreEqual (nbExecutions, results.Sum ());
+			}
+		}
+
+
+	}
+
+
+}
---
+++ Epsitec.Cresus/Common.UnitTesting/RandomExecutor.cs
@@ -0,0 +1,45 @@
+﻿using Epsitec.Common.Support.Extensions;
+
+using System;
+
+using System.Linq;
+
+
+namespace Epsitec.Common.UnitTesting
+{
+
+
+	public static class RandomExecutor
+	{
+
+
+		public static void ExecuteRandomly(int nbExecutions, params Action[] actions)
+		{
+			nbExecutions.ThrowIf (i => i < 1, "nbExecutions cannot be smaller than one");
+			actions.ThrowIfNull ("actions");
+			actions.ThrowIf (a => a.Length == 0, "actions must contain at least one element");
+			actions.ThrowIf (a => a.Any (e => e == null), "actions cannot contain null elements");
+
+			var randomSeed = Guid.NewGuid ().GetHashCode ();
+			var dice = new Random (randomSeed);
+
+			for (int i = 0; i < nbExecutions; i++)
+			{
+				var selectionActionIndex = dice.Next (actions.Length);
+				var selectedAction = actions[selectionActionIndex];
+
+				selectedAction ();
+			}
+		}
+
+
+		public static void ExecuteRandomly(this Action[] actions, int nbExecutions)
+		{
+			RandomExecutor.ExecuteRandomly (nbExecutions, actions);
+		}
+
+
+	}
+
+
+}
