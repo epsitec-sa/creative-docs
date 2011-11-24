78eb4f74d8853879730336c8b1e5f5ec03c6596b
Pierre Arnaud <arnaud@opac.ch>
2011-11-24 06:32:09 +0000

    - Core: refactoring du générateur d'ActionItemLayout.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17461 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core.Library.Business/Controllers/ActionControllers/ActionItemGenerator.cs
+++ Epsitec.Cresus/Cresus.Core.Library.Business/Controllers/ActionControllers/ActionItemGenerator.cs
@@ -22,63 +22,95 @@ namespace Epsitec.Cresus.Core.Controllers.ActionControllers
 			
 			this.tileDataItems = new List<TileDataItem> (items);
 			this.tileDataItemLookupTable = new Dictionary<string, TileDataItem> (this.tileDataItems.ToDictionary (x => x.Name));
+			this.layouts = new List<ActionItemLayout> ();
 		}
 
 
 		public IEnumerable<ActionItemLayout> GenerateLayouts()
 		{
+			this.layouts.Clear ();
+
 			foreach (var item in this.tileDataItems)
 			{
-				foreach (var actionItem in this.GenerateActionItems (item))
-				{
-					yield return ActionItemLayout.Create (item, actionItem);
-				}
+				this.GenerateActionItems (item);
 			}
+
+			return this.layouts;
 		}
 
 
-		public IEnumerable<ActionItem> GenerateActionItems(TileDataItem item)
+		private void GenerateActionItems(TileDataItem item)
 		{
-			if ((item.HideAddButton == false) &&
-				(item.AddNewItem != null))
-			{
-				yield return new ActionItem (ActionClasses.Create, item.AddNewItem);
-			}
-
-			if ((item.HideRemoveButton == false) &&
-				(item.DeleteItem != null))
-			{
-				yield return new ActionItem (ActionClasses.Delete, item.DeleteItem);
-			}
+			this.GenerateCreateActionItem (item);
+			this.GenerateDeleteActionItem (item);
 
 			if (item.EntityMarshaler != null)
 			{
-				var entity        = item.EntityMarshaler.GetValue<AbstractEntity> ();
-				var entityType    = entity == null ? item.EntityMarshaler.MarshaledType : entity.GetType ();
-				var entityActions = ActionDispatcher.GetActionInfos (entityType);
-
-				foreach (var actionInfo in entityActions)
-				{
-					var info    = actionInfo;
-					var caption = TextFormatter.GetCurrentCultureCaption (actionInfo.CaptionId);
-
-					yield return new ActionItem (info.ActionClass, () => info.ExecuteAction (entity), caption, weight: info.Weight);
-				}
-
-				foreach (var transition in WorkflowController.GetEnabledTransitions (this.context, entity as IWorkflowHost))
-				{
-					var action  = Epsitec.Cresus.Core.Workflows.WorkflowAction.Parse (transition.Edge.TransitionActions);
-					var source  = action.SourceLines.FirstOrDefault () ?? "";
-					var command = source.Split ('.').Skip (1).FirstOrDefault () ?? "";
-
-					System.Diagnostics.Debug.WriteLine ("Workflow command : " + command);
-				}
+				var entity = item.EntityMarshaler.GetValue<AbstractEntity> ();
+				
+				this.GenerateEntityActionItems (item, entity);
+				this.GenerateWorkflowActionItems (item, entity);
 			}
+		}
+
+		private void GenerateCreateActionItem(TileDataItem item)
+		{
+			if ((item.HideAddButton == false) &&
+					(item.AddNewItem != null))
+			{
+				this.CreateLayout (item, new ActionItem (ActionClasses.Create, item.AddNewItem));
+			}
+		}
+
+		private void GenerateDeleteActionItem(TileDataItem item)
+		{
+			if ((item.HideRemoveButton == false) &&
+					(item.DeleteItem != null))
+			{
+				this.CreateLayout (item, new ActionItem (ActionClasses.Delete, item.DeleteItem));
+			}
+		}
+
+		private void GenerateEntityActionItems(TileDataItem item, AbstractEntity entity)
+		{
+			var entityType    = entity == null ? item.EntityMarshaler.MarshaledType : entity.GetType ();
+			var entityActions = ActionDispatcher.GetActionInfos (entityType);
+
+			foreach (var actionInfo in entityActions)
+			{
+				var info    = actionInfo;
+				var caption = TextFormatter.GetCurrentCultureCaption (actionInfo.CaptionId);
+
+				this.CreateLayout (item, new ActionItem (info.ActionClass, () => info.ExecuteAction (entity), caption, weight: info.Weight));
+			}
+		}
+
+		private void GenerateWorkflowActionItems(TileDataItem item, AbstractEntity entity)
+		{
+			if (item.IsCompact || item.AutoGroup)
+			{
+				return;
+			}
+			
+			foreach (var transition in WorkflowController.GetEnabledTransitions (this.context, entity as IWorkflowHost))
+			{
+				var action  = Epsitec.Cresus.Core.Workflows.WorkflowAction.Parse (transition.Edge.TransitionActions);
+				var source  = action.SourceLines.FirstOrDefault () ?? "";
+				var command = source.Split ('.').Skip (1).FirstOrDefault () ?? "";
+
+				System.Diagnostics.Debug.WriteLine ("Workflow command : " + command);
+			}
+		}
+
+		private void CreateLayout(TileDataItem item, ActionItem actionItem)
+		{
+			this.layouts.Add (ActionItemLayout.Create (item, actionItem));
 		}
 
 
 		private readonly BusinessContext context;
 		private readonly List<TileDataItem> tileDataItems;
 		private readonly Dictionary<string, TileDataItem> tileDataItemLookupTable;
+		private readonly List<ActionItemLayout> layouts;
 	}
 }
