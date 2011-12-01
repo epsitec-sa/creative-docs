3539edb452e5eb24e16ce9a103512d434e98c91e
Daniel Roux <roux@epsitec.ch>
2011-12-01 07:22:26 +0000

    - Fichiers oubliés.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17483 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core.Library.Business/Controllers/ActionViewControllerMode.cs
@@ -0,0 +1,12 @@
+//	Copyright © 2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+namespace Epsitec.Cresus.Core.Controllers
+{
+	public enum ActionViewControllerMode
+	{
+		Hide,
+		Dimmed,
+		Full,
+	}
+}
