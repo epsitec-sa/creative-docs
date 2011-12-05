c7268358c184890bfc5960a6975677dece7252c7
Marc Bettex <bettex@epsitec.ch>
2011-12-05 12:59:21 +0000

    - Corrigé un chemin de fichier que j'ai oublié de mettre à jours lors du renommage de Cresus.Core.WebCore en Cresus.WebCore.Client.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17505 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Server/CoreServerProgram.cs
+++ Epsitec.Cresus/Cresus.WebCore.Server/CoreServerProgram.cs
@@ -82,7 +82,7 @@ namespace Epsitec.Cresus.WebCore.Server
 		private static readonly bool nGinxAutorun = true;
 
 
-		private static readonly DirectoryInfo iconDirectory = new DirectoryInfo ("S:\\Epsitec.Cresus\\Cresus.Core.WebCore\\WebCore\\");
+		private static readonly DirectoryInfo iconDirectory = new DirectoryInfo ("S:\\Epsitec.Cresus\\Cresus.WebCore.Client\\WebCore\\");
 
 
 		private static readonly Uri baseUri = new Uri ("http://localhost:12345/");
