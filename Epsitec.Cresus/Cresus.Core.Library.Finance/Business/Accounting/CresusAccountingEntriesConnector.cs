ce88953afa9734a8277e65867fec288e2fb597ae
Daniel Roux <roux@epsitec.ch>
2011-12-08 17:33:57 +0000

    - Cresus.Core: Améliorations dans la présentation des résultats de la génération des écritures. - Cresus.Core: Possibilité temporaire de simuler des écritures erronées, pour tester la gestion des erreurs.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17520 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core.Library.Finance/Business/Accounting/CresusAccountingEntriesConnector.cs
+++ Epsitec.Cresus/Cresus.Core.Library.Finance/Business/Accounting/CresusAccountingEntriesConnector.cs
@@ -13,11 +13,11 @@ namespace Epsitec.Cresus.Core.Business.Accounting
 {
 	public static class CresusAccountingEntriesConnector
 	{
-		public static int GenerateFiles(CresusChartOfAccounts chart, List<string> écritures, out FormattedText message, int maxLines = int.MaxValue)
+		public static bool GenerateFiles(CresusChartOfAccounts chart, List<string> écritures, out FormattedText message, int maxLines, out int nbEcritures, out int nbErrors)
 		{
 			//	Génère les fichiers ecc/ecf en fonction des réglages de l'entreprise.
-			// TODO: La liste des écritures devra être remplacée par une ligne d'entités de type "écriture" !
-			//	Retourne le nombre d'écriture générées, ou -1 en cas d'erreur.
+			//	TODO: La liste des écritures devra être remplacée par une ligne d'entités de type "écriture" !
+			//	Retourne false en cas d'erreur.
 			var écrituresTest = new List<CresusComptaEcritureMultiple> ();
 
 			foreach (var écriture in écritures)
@@ -27,19 +27,32 @@ namespace Epsitec.Cresus.Core.Business.Accounting
 
 			// TODO: Le numéro 12345789 devra être remplacé par un numéro unique lié au mandat !
 			var journal = new CresusComptaJournal (chart.Path.FullPath, 123456789);
-			int nbEcritures;
-			var errors = journal.GenerateFiles (écrituresTest, chart.BeginDate.ToDateTime (), chart.EndDate.ToDateTime (), out nbEcritures);
+			bool ok = journal.GenerateFiles (écrituresTest, chart.BeginDate.ToDateTime (), chart.EndDate.ToDateTime (), out nbEcritures);
 
 			var builder = new System.Text.StringBuilder ();
 
-			string b = chart.BeginDate.ToDateTime ().ToString ("dd.MM.yyyy");
-			string e = chart.  EndDate.ToDateTime ().ToString ("dd.MM.yyyy");
-			builder.Append (string.Format ("{0} - {1} / {2} :<br/>", b, e, chart.Title));
+			if (!ok)  // erreur ?
+			{
+				builder.Append ("<font color=\"#c90000\">");  // rouge foncé
+			}
+
+			if (chart.BeginDate.Month == 1 &&
+				chart.EndDate.Month == 12 &&
+				chart.BeginDate.Year == chart.EndDate.Year)  // de janvier à décembre de la même année ?
+			{
+				builder.Append (string.Format ("<b>{0} / {1} :</b><br/>", chart.BeginDate.Year.ToString (), chart.Title));
+			}
+			else  // période différente d'une année entière ?
+			{
+				string b = chart.BeginDate.ToDateTime ().ToString ("dd.MM.yyyy");
+				string e = chart.EndDate.ToDateTime ().ToString ("dd.MM.yyyy");
+				builder.Append (string.Format ("<b>{0} — {1} / {2} :</b><br/>", b, e, chart.Title));
+			}
 
 			var messages = journal.Messages;
 			for (int i = 0; i < messages.Count; i++)
 			{
-				if (i < maxLines)
+				if (i < maxLines || messages.Count-maxLines == 1)
 				{
 					builder.Append (string.Concat ("● ", messages[i], "<br/>"));
 				}
@@ -50,11 +63,14 @@ namespace Epsitec.Cresus.Core.Business.Accounting
 				}
 			}
 
-			builder.Append ("<br/>");
+			if (!ok)  // erreur ?
+			{
+				builder.Append ("</font>");
+			}
 
 			message = builder.ToString ();
-
-			return (errors.Count == 0) ? nbEcritures : -1;
+			nbErrors = ok ? 0 : journal.Errors.Count;
+			return ok;
 		}
 	}
 }
--- Epsitec.Cresus/Cresus.Core/CommandHandlers/CoreCommandHandler.cs
+++ Epsitec.Cresus/Cresus.Core/CommandHandlers/CoreCommandHandler.cs
@@ -99,9 +99,6 @@ namespace Epsitec.Cresus.Core.CommandHandlers
 			message += "Cette opération va préparer les écritures en vue de leur comptabilisation<br/>";
 			message += "avec le logiciel Crésus Comptabilité, sous la forme de fichiers ecc/ecf.<br/>";
 			message += "<br/>";
-			message += "Pour l'instant, seules 4 écritures fixes de test (1 en 2010 et 3 en 2011)<br/>";
-			message += "seront exportées.<br/>";
-			message += "<br/>";
 			message += "Voulez-vous exporter les écritures ?";
 
 			var dialog = MessageDialog.CreateYesNo ("Comptabilisation", DialogIcon.Question, message);
@@ -116,47 +113,87 @@ namespace Epsitec.Cresus.Core.CommandHandlers
 			var businessSettings = businessContext.GetCachedBusinessSettings ();
 			var businessFinance = businessSettings.Finance;
 
-			var builder = new System.Text.StringBuilder ();
-			bool ok = true;
-			int total = 0;
-
-			//	On simule 4 écritures.
+			//	On simule quelques écritures.
 			//	TODO: à remplacer par le vrai code dès que cela sera possible !
 			var écritures = new List<string> ();
-			écritures.Add ("01.09.2010/10/1000;2000;Virement 1;123.45");
-			écritures.Add ("01.04.2011/12/1000;...;Virement 4.1;11.00/1030;...;Virement 4.2;22.00/...;2000;Virement 4.3;33.00");
-			écritures.Add ("31.03.2011/11/1000;2000;Virement 2;100.00");
-			écritures.Add ("26.07.2011/13/1000;2000;Virement 3;20.50");
-#if false
-			//	Génère volontairement 10 écritures fausses (sans libellé).
-			for (int i = 0; i < 10; i++)
+			écritures.Add ("12.01.2010/10/1000;2000;Virement 1;123.45");
+			écritures.Add ("04.02.2010/11/1100;3000;Virement 2;0.05");
+			écritures.Add ("01.09.2010/12/1020;2100;Virement 3;10000.00");
+
+			écritures.Add ("31.03.2011/21/1000;2000;Virement 10;100.00");
+			écritures.Add ("01.04.2011/20/1000;...;Virement 11.1;11.00/1030;...;Virement 11.2;22.00/...;2000;Virement 11.3;33.00");
+			écritures.Add ("26.07.2011/22/1100;2100;Virement 12;20.75");
+			écritures.Add ("27.07.2011/23/1010;2010;Virement 12;2.50");
+
+			écritures.Add ("03.01.2012/30/1000;2000;Virement 20;100.00");
+			écritures.Add ("14.01.2012/31/1000;2000;Virement 21;100.00");
+
+			message = "Les écritures générées sont bidons et toujours identiques, juste pour tester les mécanismes (3 en 2010, 4 en 2011 et 2 en 2012).<br/><br/>Voulez-vous simuler des écritures erronées, pour tester la gestion des erreurs ?";
+			dialog = MessageDialog.CreateYesNo ("Comptabilisation", DialogIcon.Question, message);
+			dialog.OpenDialog ();
+			if (dialog.Result == DialogResult.Yes)
 			{
-				écritures.Add (string.Format ("27.07.2011/{0}/1000;2000;;1.00", (i+100).ToString ()));
-			}
+				//	Génère volontairement des écritures fausses en 2010.
+#if false
+				écritures.Add ("02.09.2010/13/...;...;Erreur;1.00");
+				écritures.Add ("03.09.2010/14");
+				écritures.Add ("04.09.2010/15");
 #endif
 
-			var charts = businessFinance.GetAllChartsOfAccounts ().OrderBy (x => x.BeginDate);
+				//	Génère volontairement une écriture multiple non balancée.
+				écritures.Add ("10.11.2011/25/1000;...;Virement 13.1;11.00/1030;...;Virement 13.2;22.00/...;2000;Virement 13.3;33.01");
+
+				//	Génère volontairement 10 écritures fausses (sans libellé).
+				for (int i = 0; i < 10; i++)
+				{
+					écritures.Add (string.Format ("13.11.2011/{0}/1000;2000;;1.00", (i+100).ToString ()));
+				}
+			}
+
+			//	De l'année la plus récente à la plus ancienne.
+			var charts = businessFinance.GetAllChartsOfAccounts ().OrderBy (x => x.BeginDate).Reverse ();
+
+			var builder = new System.Text.StringBuilder ();
+			bool jobOk = true;
+			bool first = true;
+			int total = 0;
+			int maxLines = charts.Count () > 3 ? 3 : 6;
+			int linesCount = 0;
+
 			foreach (Business.Accounting.CresusChartOfAccounts chart in charts)
 			{
-				FormattedText result;
-				int nbEcritures = Business.Accounting.CresusAccountingEntriesConnector.GenerateFiles (chart, écritures, out result, 6);
-
-				if (nbEcritures == -1)  // erreur ?
+				if (!first)
 				{
-					ok = false;
+					builder.Append ("<br/>");
 				}
-				else  // ok ?
+
+				if (linesCount > 12)
+				{
+					maxLines = 1;
+				}
+
+				FormattedText result;
+				int nbEcritures, nbErrors;
+				if (Business.Accounting.CresusAccountingEntriesConnector.GenerateFiles (chart, écritures, out result, maxLines, out nbEcritures, out nbErrors))
 				{
 					total += nbEcritures;
+					linesCount += 2;
+				}
+				else  // erreur ?
+				{
+					jobOk = false;
+					linesCount += 1 + System.Math.Min (nbErrors, maxLines);
 				}
 
 				builder.Append (result);
+
+				first = false;
 			}
 
 			FormattedText summary;
-			if (ok)
+			if (jobOk)
 			{
-				summary = TextFormatter.FormatText ("Les fichiers sont prêts pour la combtabilisation.").ApplyBold ().ApplyFontSize (13.0);
+				summary = TextFormatter.FormatText ("Les fichiers sont prêts pour la combtabilisation.").ApplyBold ().ApplyFontSize (14.0);
 
 				if (total < écritures.Count)
 				{
@@ -166,12 +203,12 @@ namespace Epsitec.Cresus.Core.CommandHandlers
 			}
 			else
 			{
-				summary = TextFormatter.FormatText ("La comptabilisation n'est pas possible, car des erreurs sont survenues.").ApplyBold ().ApplyFontSize (13.0);
+				summary = TextFormatter.FormatText ("La comptabilisation n'est pas possible, car des erreurs sont survenues.").ApplyBold ().ApplyFontSize (14.0);
 			}
 
-			message = FormattedText.Concat (summary, "<br/><br/>", builder.ToString (), " ");
+			message = FormattedText.Concat (summary, "<br/><br/>", builder.ToString (), " ");  // un bug dans TextLayout oblige de terminer par un espace !
 
-			dialog = MessageDialog.CreateOk ("Comptabilisation", ok ? DialogIcon.Question : DialogIcon.Warning, message);
+			dialog = MessageDialog.CreateOk ("Comptabilisation", jobOk ? DialogIcon.Question : DialogIcon.Warning, message);
 			dialog.OpenDialog ();
 		}
 
