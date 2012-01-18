ccb80a796483470af78d58fade1dee81f1b7f5cf
Pierre Arnaud <arnaud@opac.ch>
2012-01-18 05:42:01 +0000

    - Aider: expérimentation avec AiderPerson.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17572 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/Controllers/SummaryControllers/SummaryAiderPersonViewController.cs
+++ Epsitec.Cresus/Aider/Controllers/SummaryControllers/SummaryAiderPersonViewController.cs
@@ -3,6 +3,7 @@
 using Epsitec.Cresus.Bricks;
 
 using Epsitec.Cresus.Core.Controllers.SummaryControllers;
+using Epsitec.Cresus.Core.Bricks;
 
 
 namespace Epsitec.Aider.Controllers.SummaryControllers
@@ -16,15 +17,19 @@ namespace Epsitec.Aider.Controllers.SummaryControllers
 		protected override void CreateBricks(BrickWall<AiderPersonEntity> wall)
 		{
 			wall.AddBrick (x => x);
+			wall.AddBrick (x => x)
+				.Attribute (BrickMode.DefaultToSummarySubView)
+				.Text ("Pour en savoir plus...");	//	Just to check that this is possible
 
 			if (this.Entity.IsGovernmentDefined ())
 			{
 				wall.AddBrick (x => x.eCH_Person.Address);
 			}
 
-			wall.AddBrick (x => x.AdditionalAddress1);
-			// This lines crashes because of a bug in ActionItemGenerator
-			//wall.AddBrick (x => x.AdditionalAddress2);
+			wall.AddBrick (x => x.AdditionalAddress1)
+				.Title ("Adresse supplémentaire 1");
+			wall.AddBrick (x => x.AdditionalAddress2)
+				.Title ("Adresse supplémentaire 2");
 
 			wall.AddBrick (x => x.Household);
 		}
