7bc2eb01d49ab6f86517c3cac2d8317a995f710f
Marc Bettex <bettex@epsitec.ch>
2012-01-30 10:50:05 +0000

    - Ajouté une classe avec des méthodes utilitaires pour les strings.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17658 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common/Support/StringUtils.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace Epsitec.Common.Support
+{
+
+
+	public static class StringUtils
+	{
+
+
+		public static string Join(string separator, params string[] strings)
+		{
+			return string.Join (separator, strings.Where (s => !string.IsNullOrEmpty (s)));
+		}
+
+
+		public static string Join(string separator, params object[] objects)
+		{
+			var strings = objects
+				.Where (o => o != null)
+				.Select (o => o.ToString ())
+				.Where (s => !string.IsNullOrEmpty (s));
+
+			return string.Join (separator, strings);
+		}
+
+
+	}
+
+
+}
