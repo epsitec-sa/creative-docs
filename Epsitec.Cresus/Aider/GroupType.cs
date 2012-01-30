f85a191ce1a524d0f02e4017d1738269731dd48d
Pierre Arnaud <arnaud@opac.ch>
2012-01-30 06:19:03 +0000

    - Aider: encore quelques modifications dans le schéma. Documenté en particulier la relation Group.Root.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17657 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/GroupType.cs
@@ -0,0 +1,21 @@
+//	Copyright © 2012, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Aider
+{
+	[DesignerVisible]
+	public enum GroupType
+	{
+		None = 0,
+
+		Root,
+		Node,
+		NodeAndLeaf,
+		Leaf,
+	}
+}
