ea354bae2d4c765367245c78b210e03861b95b57
Daniel Roux <roux@epsitec.ch>
2012-02-02 14:24:19 +0000

    - Cresus.Compta: BilanController et PPController héritent d'une classe commune DoubleController.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17682 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Compta/Controllers/BilanController.cs
+++ Epsitec.Cresus/Cresus.Compta/Controllers/BilanController.cs
@@ -24,7 +24,7 @@ namespace Epsitec.Cresus.Compta.Controllers
 	/// <summary>
 	/// Ce contrôleur gère le bilan de la comptabilité.
 	/// </summary>
-	public class BilanController : AbstractController
+	public class BilanController : DoubleController
 	{
 		public BilanController(Application app, BusinessContext businessContext, ComptaEntity comptaEntity, MainWindowController mainWindowController)
 			: base (app, businessContext, comptaEntity, mainWindowController)
@@ -42,103 +42,9 @@ namespace Epsitec.Cresus.Compta.Controllers
 			this.UpdateColumnMappers ();
 		}
 
-		protected override void OptionsChanged()
-		{
-			this.UpdateColumnMappers ();
-			this.UpdateArray ();
-
-			base.OptionsChanged ();
-		}
-
 		protected override void UpdateTitle()
 		{
 			this.SetTitle ("Bilan intermédiaire");
-		}
-
-
-		public override bool HasShowSearchPanel
-		{
-			get
-			{
-				return true;
-			}
-		}
-
-		public override bool HasShowFilterPanel
-		{
-			get
-			{
-				return true;
-			}
-		}
-
-		public override bool HasShowOptionsPanel
-		{
-			get
-			{
-				return true;
-			}
-		}
-
-		public override bool HasShowInfoPanel
-		{
-			get
-			{
-				return false;
-			}
-		}
-
-
-		protected override FormattedText GetArrayText(int row, ColumnType columnType)
-		{
-			//	Retourne le texte contenu dans une cellule.
-			var text = this.dataAccessor.GetText (row, columnType);
-			var data = this.dataAccessor.GetReadOnlyData (row) as DoubleData;
-
-			var options = this.dataAccessor.AccessorOptions as DoubleOptions;
-
-			if (columnType == ColumnType.Titre)
-			{
-				for (int i = 0; i < data.Niveau; i++)
-				{
-					text = FormattedText.Concat ("    ", text);
-				}
-			}
-			else if (columnType == ColumnType.Solde)
-			{
-				if (!data.NeverFiltered && options.HideZero && text == "0.00")
-				{
-					text = FormattedText.Empty;
-				}
-			}
-
-			return data.Typo (text);
-		}
-
-
-		protected override IEnumerable<ColumnMapper> InitialColumnMappers
-		{
-			get
-			{
-				yield return new ColumnMapper (ColumnType.Numéro,         0.20, ContentAlignment.MiddleLeft,  "Numéro");
-				yield return new ColumnMapper (ColumnType.Titre,          1.00, ContentAlignment.MiddleLeft,  "Titre du compte");
-				yield return new ColumnMapper (ColumnType.Solde,          0.20, ContentAlignment.MiddleRight, "Montant");
-				yield return new ColumnMapper (ColumnType.SoldeGraphique, 0.20, ContentAlignment.MiddleRight, "", hideForSearch: true);
-				yield return new ColumnMapper (ColumnType.Budget,         0.20, ContentAlignment.MiddleRight, "");
-
-				yield return new ColumnMapper (ColumnType.Date,       0.20, ContentAlignment.MiddleLeft, "Date",       show: false);
-				yield return new ColumnMapper (ColumnType.Profondeur, 0.20, ContentAlignment.MiddleLeft, "Profondeur", show: false);
-			}
-		}
-
-		protected override void UpdateColumnMappers()
-		{
-			var options = this.dataAccessor.AccessorOptions as DoubleOptions;
-
-			this.ShowHideColumn (ColumnType.SoldeGraphique, options.HasGraphics);
-			this.ShowHideColumn (ColumnType.Budget,         options.BudgetEnable && this.optionsController != null);
-
-			this.SetColumnDescription (ColumnType.Budget, options.BudgetColumnDescription);
 		}
 	}
 }
---
+++ Epsitec.Cresus/Cresus.Compta/Controllers/DoubleController.cs
@@ -0,0 +1,129 @@
+﻿//	Copyright © 2010, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using Epsitec.Common.Drawing;
+using Epsitec.Common.Widgets;
+using Epsitec.Common.Types;
+using Epsitec.Common.Types.Converters;
+
+using Epsitec.Cresus.Core;
+using Epsitec.Cresus.Core.Entities;
+using Epsitec.Cresus.Core.Controllers;
+using Epsitec.Cresus.Core.Widgets;
+using Epsitec.Cresus.Core.Library;
+using Epsitec.Cresus.Core.Business;
+
+using Epsitec.Cresus.Compta.Accessors;
+using Epsitec.Cresus.Compta.Entities;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Compta.Controllers
+{
+	/// <summary>
+	/// Ce contrôleur gère les données doubles (bilan avec actif/passif ou PP avec charge/produit) de la comptabilité.
+	/// </summary>
+	public abstract class DoubleController : AbstractController
+	{
+		public DoubleController(Application app, BusinessContext businessContext, ComptaEntity comptaEntity, MainWindowController mainWindowController)
+			: base (app, businessContext, comptaEntity, mainWindowController)
+		{
+		}
+
+
+		public override bool HasShowSearchPanel
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		public override bool HasShowFilterPanel
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		public override bool HasShowOptionsPanel
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		public override bool HasShowInfoPanel
+		{
+			get
+			{
+				return false;
+			}
+		}
+
+
+		protected override void OptionsChanged()
+		{
+			this.UpdateColumnMappers ();
+			this.UpdateArray ();
+
+			base.OptionsChanged ();
+		}
+
+
+		protected override FormattedText GetArrayText(int row, ColumnType columnType)
+		{
+			//	Retourne le texte contenu dans une cellule.
+			var text = this.dataAccessor.GetText (row, columnType);
+			var data = this.dataAccessor.GetReadOnlyData (row) as DoubleData;
+
+			var options = this.dataAccessor.AccessorOptions as DoubleOptions;
+
+			if (columnType == ColumnType.Titre)
+			{
+				for (int i = 0; i < data.Niveau; i++)
+				{
+					text = FormattedText.Concat ("    ", text);
+				}
+			}
+			else if (columnType == ColumnType.Solde)
+			{
+				if (!data.NeverFiltered && options.HideZero && text == "0.00")
+				{
+					text = FormattedText.Empty;
+				}
+			}
+
+			return data.Typo (text);
+		}
+
+
+		protected override IEnumerable<ColumnMapper> InitialColumnMappers
+		{
+			get
+			{
+				yield return new ColumnMapper (ColumnType.Numéro,         0.20, ContentAlignment.MiddleLeft,  "Numéro");
+				yield return new ColumnMapper (ColumnType.Titre,          1.00, ContentAlignment.MiddleLeft,  "Titre du compte");
+				yield return new ColumnMapper (ColumnType.Solde,          0.20, ContentAlignment.MiddleRight, "Montant");
+				yield return new ColumnMapper (ColumnType.SoldeGraphique, 0.20, ContentAlignment.MiddleRight, "", hideForSearch: true);
+				yield return new ColumnMapper (ColumnType.Budget,         0.20, ContentAlignment.MiddleRight, "");
+
+				yield return new ColumnMapper (ColumnType.Date,       0.20, ContentAlignment.MiddleLeft, "Date",       show: false);
+				yield return new ColumnMapper (ColumnType.Profondeur, 0.20, ContentAlignment.MiddleLeft, "Profondeur", show: false);
+			}
+		}
+
+		protected override void UpdateColumnMappers()
+		{
+			var options = this.dataAccessor.AccessorOptions as DoubleOptions;
+
+			this.ShowHideColumn (ColumnType.SoldeGraphique, options.HasGraphics);
+			this.ShowHideColumn (ColumnType.Budget,         options.BudgetEnable && this.optionsController != null);
+
+			this.SetColumnDescription (ColumnType.Budget, options.BudgetColumnDescription);
+		}
+	}
+}
