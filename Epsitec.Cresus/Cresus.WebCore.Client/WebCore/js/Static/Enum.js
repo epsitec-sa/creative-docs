76960c94be82c7c58f74ad6b9b9b2b7c1dee163c
Marc Bettex <bettex@epsitec.ch>
2012-02-15 09:39:17 +0000

    - Supprimé le bouton qui permettait d'éditer une énumeration à droite de sa ComboBox. On ne peut pas éditer une énumération.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@17849 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Client/WebCore/js/Static/Enum.js
+++ Epsitec.Cresus/Cresus.WebCore.Client/WebCore/js/Static/Enum.js
@@ -13,21 +13,8 @@ Ext.define('Epsitec.Cresus.Core.Static.Enum',
       
       var combo = Ext.create('Epsitec.Cresus.Core.Static.EnumComboBox', options);
       
-      var button = Ext.create('Ext.Button',
-          {
-            text : '>',
-            renderTo : Ext.getBody(),
-            handler : function ()
-            {
-              Ext.Msg.alert('Cannot edit this enumeration', 'You cannot directly edit this enumeration. You will need to save the current changes, click the header menu to edit the corresponding enumeration, and come back to this entity to edit it.');
-            },
-            margin : '19 0 0 5'
-          }
-        );
-      
       this.items = this.items || new Array();
       this.items.push(combo);
-      this.items.push(button);
       
       this.callParent();
       
