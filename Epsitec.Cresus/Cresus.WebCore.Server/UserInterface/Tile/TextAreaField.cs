42e1c2d7acc593bdd88db2b42b254cb873b79db2
Marc Bettex <bettex@epsitec.ch>
2012-02-29 15:57:16 +0000

    - Les textes multilignes sont affichés avec un textareafield plutôt qu'un textfield dans webcore.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@18045 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.WebCore.Server/UserInterface/Tile/TextAreaField.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+
+namespace Epsitec.Cresus.WebCore.Server.UserInterface.Tile
+{
+
+
+	internal sealed class TextAreaField : AbstractField
+	{
+
+		public string Value
+		{
+			get;
+			set;
+		}
+
+
+		public override Dictionary<string, object> ToDictionary()
+		{
+			var fieldDictionary = base.ToDictionary ();
+
+			fieldDictionary["xtype"] = "textareafield";
+			fieldDictionary["value"] = this.Value;
+
+			return fieldDictionary;
+		}
+
+
+	}
+
+
+}
--- Epsitec.Cresus/Cresus.WebCore.Server/UserInterface/TileData/TextFieldData.cs
+++ Epsitec.Cresus/Cresus.WebCore.Server/UserInterface/TileData/TextFieldData.cs
@@ -1,5 +1,7 @@
 using Epsitec.Common.Support.EntityEngine;
 
+using Epsitec.Common.Types;
+
 using Epsitec.Cresus.WebCore.Server.UserInterface.PropertyAccessor;
 using Epsitec.Cresus.WebCore.Server.UserInterface.Tile;
 
@@ -11,7 +13,7 @@ using System.Collections.Generic;
 namespace Epsitec.Cresus.WebCore.Server.UserInterface.TileData
 {
 
-	
+
 	internal sealed class TextFieldData : AbstractFieldData
 	{
 
@@ -20,7 +22,32 @@ namespace Epsitec.Cresus.WebCore.Server.UserInterface.TileData
 		{
 			var textPropertyAccessor = (TextPropertyAccessor) this.PropertyAccessor;
 
+			if (StringType.IsMultilineText (textPropertyAccessor.Property.Type))
+			{
+				return this.ToTextAreaField (entity, textPropertyAccessor);
+			}
+			else
+			{
+				return this.ToTextField (entity, textPropertyAccessor);
+			}
+		}
+
+
+		private AbstractField ToTextField(AbstractEntity entity, TextPropertyAccessor textPropertyAccessor)
+		{
 			return new TextField ()
+			{
+				PropertyAccessorId = textPropertyAccessor.Id,
+				Title = this.Title.ToString (),
+				IsReadOnly = this.IsReadOnly,
+				Value = textPropertyAccessor.GetString (entity),
+			};
+		}
+
+
+		private AbstractField ToTextAreaField(AbstractEntity entity, TextPropertyAccessor textPropertyAccessor)
+		{
+			return new TextAreaField ()
 			{
 				PropertyAccessorId = textPropertyAccessor.Id,
 				Title = this.Title.ToString (),
