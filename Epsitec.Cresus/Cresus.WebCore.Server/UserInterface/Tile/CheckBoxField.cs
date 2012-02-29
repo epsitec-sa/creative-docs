34f51fcd09cb12066013c727170de3ce766a9f79
Marc Bettex <bettex@epsitec.ch>
2012-02-29 14:32:52 +0000

    - Oubli au commit précédent.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@18042 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.WebCore.Server/UserInterface/Tile/CheckBoxField.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+
+namespace Epsitec.Cresus.WebCore.Server.UserInterface.Tile
+{
+
+
+	internal sealed class CheckBoxField : AbstractField
+	{
+
+
+		public bool Value
+		{
+			get;
+			set;
+		}
+
+
+		public override Dictionary<string, object> ToDictionary()
+		{
+			var fieldDictionary = base.ToDictionary ();
+
+			fieldDictionary["xtype"] = "checkboxfield";
+			fieldDictionary["checked"] = this.Value;
+			fieldDictionary["inputValue"] = true;
+			fieldDictionary["uncheckedValue"] = false;
+
+			return fieldDictionary;
+		}
+
+		
+	}
+
+
+}
