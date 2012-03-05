089fb1659cee7a26b4628bd753bf49407a30a40d
Marc Bettex <bettex@epsitec.ch>
2012-03-05 11:34:00 +0000

    - Ajouté EervDataImporter qui permet pour le moment d'importer dans la base de donnée les groupes pour les régions et les paroisses.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@18138 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider.Tests.Vs/UnitTestEervDataImporter.cs
@@ -0,0 +1,59 @@
+﻿using Epsitec.Aider.Data;
+using Epsitec.Aider.Data.Ech;
+using Epsitec.Aider.Data.Eerv;
+
+using Epsitec.Common.Widgets;
+
+using Epsitec.Cresus.Core;
+using Epsitec.Cresus.Core.Business;
+using Epsitec.Cresus.Core.Library;
+using Epsitec.Cresus.Core.Library.UI;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+
+using System.IO;
+
+using System.Linq;
+
+
+namespace Aider.Tests.Vs
+{
+
+
+	[TestClass]
+	public class UnitTestEervDataImporter
+	{
+
+
+		[TestMethod]
+		public void Test()
+		{
+			CoreData.ForceDatabaseCreationRequest = true;
+
+			var lines = CoreContext.ReadCoreContextSettingsFile ().ToList ();
+			CoreContext.ParseOptionalSettingsFile (lines);
+			CoreContext.StartAsInteractive ();
+			Services.Initialize ();
+
+			using (var app = new CoreApplication ())
+			{
+				app.SetupApplication ();
+
+				var parishes = ParishAddressRepository.Current.FindAllAddressInformations ().ToList ();
+
+				Func<BusinessContext> businessContextCreator = () => new BusinessContext (app.Data);
+				Action<BusinessContext> businessContextCleaner = b => Application.ExecuteAsyncCallbacks ();
+				
+				EervDataImporter.ImportEervData (businessContextCreator, businessContextCleaner, parishes);
+
+				Services.ShutDown ();
+			}
+		}
+
+
+	}
+
+
+}
---
+++ Epsitec.Cresus/Aider/Data/Eerv/EervDataImporter.cs
@@ -0,0 +1,152 @@
+﻿using Epsitec.Aider.Entities;
+using Epsitec.Aider.Enumerations;
+
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Core.Business;
+
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+using Epsitec.Cresus.DataLayer.Context;
+
+
+namespace Epsitec.Aider.Data.Eerv
+{
+
+
+	internal static class EervDataImporter
+	{
+
+
+		public static void ImportEervData(Func<BusinessContext> businessContextCreator, Action<BusinessContext> businessContextCleaner, IEnumerable<ParishAddressInformation> parishInformations)
+		{
+			var result = EervDataImporter.ImportRegionsAndParishes (businessContextCreator, businessContextCleaner, parishInformations);
+
+			var regionNumbersToEntityKeys = result.Item1;
+			var parishNamesToEntityKeys = result.Item2;
+		}
+
+
+		private static Tuple<Dictionary<int, EntityKey>, Dictionary<string, EntityKey>> ImportRegionsAndParishes(Func<BusinessContext> businessContextCreator, Action<BusinessContext> businessContextCleaner, IEnumerable<ParishAddressInformation> parishInformations)
+		{
+			Dictionary<int, EntityKey> regionNumbersToEntityKeys;
+			Dictionary<string, EntityKey> parishNamesToEntityKeys;
+
+			var regions = EervDataImporter.GetRegions (parishInformations);
+
+			using (var businessContext = businessContextCreator ())
+			{
+				try
+				{
+					var regionGroups = EervDataImporter.CreateRegionGroups (businessContext, regions);
+					var parishGroups = EervDataImporter.CreateParishGroups (businessContext, regionGroups, regions);
+
+					businessContext.SaveChanges ();
+
+					var dataContext = businessContext.DataContext;
+
+					regionNumbersToEntityKeys = regionGroups.ToDictionary (r => r.Key, r => dataContext.GetNormalizedEntityKey (r.Value).Value);
+					parishNamesToEntityKeys = parishGroups.ToDictionary (p=> p.Key, p => dataContext.GetNormalizedEntityKey (p.Value).Value);
+				}
+				finally
+				{
+					businessContextCleaner (businessContext);
+				}
+			}
+
+			return Tuple.Create (regionNumbersToEntityKeys, parishNamesToEntityKeys);
+		}
+
+
+		private static Dictionary<int, Dictionary<string, List<ParishAddressInformation>>> GetRegions(IEnumerable<ParishAddressInformation> parishInformations)
+		{
+			return parishInformations
+				.GroupBy (pi => pi.RegionCode)
+				.ToDictionary (g => g.Key, g => g.GroupBy (pi => pi.ParishName).ToDictionary (g2 => g2.Key, g2 => g2.ToList ()));
+		}
+
+
+		private static Dictionary<int, AiderGroupEntity> CreateRegionGroups(BusinessContext businessContext, Dictionary<int, Dictionary<string, List<ParishAddressInformation>>> regions)
+		{
+			var regionGroupDefinition = EervDataImporter.CreateRegionGroupDefinition (businessContext);
+
+			return regions.Keys.ToDictionary (rc => rc, rc => EervDataImporter.CreateRegionGroup (businessContext, regionGroupDefinition, rc));
+		}
+
+
+		private static Dictionary<string, AiderGroupEntity> CreateParishGroups(BusinessContext businessContext, Dictionary<int, AiderGroupEntity> regionGroups, Dictionary<int, Dictionary<string, List<ParishAddressInformation>>> regions)
+		{
+			var parishGroupDefinitinon = EervDataImporter.CreateParishGroupDefinition (businessContext);
+
+			return regions
+				.Values
+				.SelectMany (p => p.Values.Select (p2 => p2.First ()))
+				.ToDictionary (p => p.ParishName, p => EervDataImporter.CreateParishGroup (businessContext, regionGroups, parishGroupDefinitinon, p));
+		}
+
+
+		private static AiderGroupEntity CreateRegionGroup(BusinessContext businessContext, AiderGroupDefEntity regionGroupDefinition, int regionCode)
+		{
+			var regionGroup = businessContext.CreateEntity<AiderGroupEntity> ();
+
+			regionGroup.Name = "R" + InvariantConverter.ToString (regionCode);
+			regionGroup.GroupDef = regionGroupDefinition;
+
+			return regionGroup;
+		}
+
+
+		private static AiderGroupEntity CreateParishGroup(BusinessContext businessContext, Dictionary<int, AiderGroupEntity> regionGroups, AiderGroupDefEntity parishGroupDefinition, ParishAddressInformation parish)
+		{
+			var parishGroup = businessContext.CreateEntity<AiderGroupEntity> ();
+
+			parishGroup.Name = parish.ParishName;
+			parishGroup.GroupDef = parishGroupDefinition;
+			parishGroup.Root = regionGroups[parish.RegionCode];
+
+			return parishGroup;
+		}
+
+
+		private static AiderGroupDefEntity CreateRegionGroupDefinition(BusinessContext businessContext)
+		{
+			var name = "Définition des groupes régionaux";
+			var category = GroupCategory.SystemDefined;
+			var defType = GroupDefType.Region;
+			var type = GroupType.Root;
+
+			return EervDataImporter.CreateGroupDefinition (businessContext, name, category, type, defType);
+		}
+
+
+		private static AiderGroupDefEntity CreateParishGroupDefinition(BusinessContext businessContext)
+		{
+			var name = "Définition des groupes paroissiaux";
+			var category = GroupCategory.SystemDefined;
+			var defType = GroupDefType.Parish;
+			var type = GroupType.Node;
+
+			return EervDataImporter.CreateGroupDefinition (businessContext, name, category, type, defType);
+		}
+
+
+		private static AiderGroupDefEntity CreateGroupDefinition(BusinessContext businessContext, string name, GroupCategory category, GroupType type, GroupDefType defType)
+		{
+			var groupDefinition = businessContext.CreateEntity<AiderGroupDefEntity> ();
+
+			groupDefinition.Name = name;
+			groupDefinition.Category = category;
+			groupDefinition.DefType = defType;
+			groupDefinition.Type = type;
+
+			return groupDefinition;
+		}
+
+
+	}
+
+
+}
