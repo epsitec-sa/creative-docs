c0b96533f50696b5b8a374e83c096007af30c260
Pierre Arnaud <arnaud@opac.ch>
2012-03-16 06:14:26 +0000

    - Common: début des travaux sur une 'big list' qui permettra de réaliser des scroll list avec des millions d'éléments.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@18238 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common/BigList/ItemState.cs
@@ -0,0 +1,171 @@
+//	Copyright © 2012, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Common.BigList
+{
+	public class ItemState
+	{
+		public ItemState()
+		{
+		}
+
+		public ItemState(ItemState other)
+		{
+			this.Loaded   = other.Loaded;
+			this.Selected = other.Selected;
+			this.Hidden   = other.Hidden;
+			this.Hilite1  = other.Hilite1;
+			this.Hilite2  = other.Hilite2;
+			this.Partial  = other.Partial;
+			this.Height   = other.Height;
+		}
+
+
+		public bool Loaded
+		{
+			get;
+			set;
+		}
+
+		public bool Selected
+		{
+			get;
+			set;
+		}
+
+		public bool Hidden
+		{
+			get;
+			set;
+		}
+
+		public bool Hilite1
+		{
+			get;
+			set;
+		}
+
+		public bool Hilite2
+		{
+			get;
+			set;
+		}
+
+		public bool Partial
+		{
+			get
+			{
+				return this.needsExtraState;
+			}
+			private set
+			{
+				this.needsExtraState = value;
+			}
+		}
+
+		public int Height
+		{
+			get
+			{
+				return this.height;
+			}
+			set
+			{
+				if (value < 0)
+				{
+					throw new System.ArgumentOutOfRangeException ("value", string.Format ("Height may not be negative: {0}", value));
+				}
+				if (value > ItemState.MaxCompactHeight)
+				{
+					this.needsExtraState = true;
+				}
+
+				this.height = value;
+			}
+		}
+
+
+		public ushort ToCompactState()
+		{
+			bool partialFlag = this.ComputePartialFlag ();
+			
+			int compact =
+				(this.Loaded   ? Mask.Loaded   : 0) |
+				(this.Selected ? Mask.Selected : 0) |
+				(this.Hidden   ? Mask.Hidden   : 0) |
+				(this.Hilite1  ? Mask.Hilite1  : 0) |
+				(this.Hilite2  ? Mask.Hilite2  : 0) |
+				(partialFlag   ? Mask.Partial  : 0) |
+				System.Math.Min (this.height, ItemState.MaxCompactHeight);
+
+			return (ushort) compact;
+		}
+
+		public void Apply(ItemState other)
+		{
+			if (this.height == ItemState.MaxCompactHeight)
+			{
+				this.height = other.Height;
+			}
+
+			bool applied = false;
+
+			//	TODO: recopy properties which do not fit into a compact state...
+
+			if (applied)
+			{
+				this.Partial = true;
+			}
+		}
+
+		public static ItemState FromCompactState(ushort state)
+		{
+			return new ItemState ()
+			{
+				Loaded   = (state & Mask.Height)   != 0,
+				Selected = (state & Mask.Selected) != 0,
+				Hidden   = (state & Mask.Hidden)   != 0,
+				Hilite1  = (state & Mask.Hilite1)  != 0,
+				Hilite2  = (state & Mask.Hilite2)  != 0,
+				Partial  = (state & Mask.Partial)  != 0,
+				Height   = (state & Mask.Height)
+			};
+		}
+
+		public bool ComputePartialFlag()
+		{
+			if (this.needsExtraState == false)
+			{
+				return false;
+			}
+			
+			//	TODO: evaluate the extra arguments to see if they still require that the full
+			//	state, including extra information, needs to be stored.
+
+			return this.height > ItemState.MaxCompactHeight;
+		}
+
+		#region Mask Constants
+
+		private static class Mask
+		{
+			public const int Height	  = 0x03ff;
+			public const int Loaded	  = 0x0400;
+			public const int Selected = 0x0800;
+			public const int Hidden   = 0x1000;
+			public const int Hilite1  = 0x2000;
+			public const int Hilite2  = 0x4000;
+			public const int Partial  = 0x8000;
+		}
+
+		#endregion
+
+		public const int MaxCompactHeight		= 1000;
+
+		private int								height;
+		private bool							needsExtraState;
+	}
+}
