2bc22ab78f2b4890be041e16f17495b1d6736237
Marc Bettex <bettex@epsitec.ch>
2012-03-21 10:50:16 +0000

    - Ajouté la class Freezable pour servir de class abstraite à des objets qui peuvent devenir readonly.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@18276 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common/Types/Freezable.cs
@@ -0,0 +1,44 @@
+﻿namespace Epsitec.Common.Types
+{
+
+
+	public abstract class Freezable : IReadOnly
+	{
+
+
+		public Freezable()
+		{
+			this.IsReadOnly = false;
+		}
+
+
+		#region IReadOnly Members
+
+
+		public bool IsReadOnly
+		{
+			get;
+			private set;
+		}
+
+
+		#endregion
+
+
+		public void Freeze()
+		{
+			this.IsReadOnly = true;
+
+			this.HandleFreeze ();
+		}
+
+
+		protected virtual void HandleFreeze()
+		{
+		}
+
+
+	}
+
+
+}
