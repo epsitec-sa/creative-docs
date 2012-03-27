7eacc7570fee0354d775ee0279beb5a6620453a5
Marc Bettex <bettex@epsitec.ch>
2012-03-27 13:19:38 +0000

    - Ajouté la classe JaroWinkler qui permet de calculer la distance de Jaro-Winkler ou la distance de Jaro entre deux strings. - Ajouté des tests pour cette classe.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@18319 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Tests.Vs/Text/UnitTestJaroWinkler.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Epsitec.Common.Text;
+
+
+namespace Epsitec.Common.Tests.Vs.Text
+{
+
+
+	[TestClass]
+	public class UnitTestJaroWinkler
+	{
+		
+		
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.AreEqual (1.0, JaroWinkler.ComputeJaroWinklerDistance (null, null));
+			Assert.AreEqual (1.0, JaroWinkler.ComputeJaroWinklerDistance ("", null));
+			Assert.AreEqual (1.0, JaroWinkler.ComputeJaroWinklerDistance (null, ""));
+			Assert.AreEqual (1.0, JaroWinkler.ComputeJaroWinklerDistance ("", ""));
+			Assert.AreEqual (1.0, JaroWinkler.ComputeJaroWinklerDistance ("abc", "abc"));
+			Assert.AreEqual (1.0, JaroWinkler.ComputeJaroWinklerDistance ("ab", "ab"));
+			Assert.AreEqual (1.0, JaroWinkler.ComputeJaroWinklerDistance ("a", "a"));
+			Assert.AreEqual (1.0, JaroWinkler.ComputeJaroWinklerDistance ("abcd", "abcd"));
+			Assert.AreEqual (0.0, JaroWinkler.ComputeJaroWinklerDistance ("abc", "def"));
+
+
+			this.AreRoughlyEqual (0.840, JaroWinkler.ComputeJaroWinklerDistance ("DWAYNE", "DUANE"));
+			this.AreRoughlyEqual (0.961, JaroWinkler.ComputeJaroWinklerDistance ("MARTHA", "MARHTA"));
+			this.AreRoughlyEqual (0.813, JaroWinkler.ComputeJaroWinklerDistance ("DIXON", "DICKSONX"));
+			this.AreRoughlyEqual (0.982, JaroWinkler.ComputeJaroWinklerDistance ("SHACKLEFORD", "SHACKELFORD"));
+			this.AreRoughlyEqual (0.896, JaroWinkler.ComputeJaroWinklerDistance ("DUNNINGHAM", "CUNNIGHAM"));
+			this.AreRoughlyEqual (0.956, JaroWinkler.ComputeJaroWinklerDistance ("NICHLESON", "NICHULSON"));
+			this.AreRoughlyEqual (0.832, JaroWinkler.ComputeJaroWinklerDistance ("JONES", "JOHNSON"));
+			this.AreRoughlyEqual (0.933, JaroWinkler.ComputeJaroWinklerDistance ("MASSEY", "MASSIE"));
+			this.AreRoughlyEqual (0.922, JaroWinkler.ComputeJaroWinklerDistance ("ABROMS", "ABRAMS"));
+			this.AreRoughlyEqual (0.722, JaroWinkler.ComputeJaroWinklerDistance ("HARDIN", "MARTINEZ"));
+			this.AreRoughlyEqual (0.467, JaroWinkler.ComputeJaroWinklerDistance ("ITMAN", "SMITH"));
+			this.AreRoughlyEqual (0.926, JaroWinkler.ComputeJaroWinklerDistance ("JERALDINE", "GERALDINE"));
+			this.AreRoughlyEqual (0.921, JaroWinkler.ComputeJaroWinklerDistance ("MICHELLE", "MICHAEL"));
+			this.AreRoughlyEqual (0.933, JaroWinkler.ComputeJaroWinklerDistance ("JULIES", "JULIUS"));
+			this.AreRoughlyEqual (0.880, JaroWinkler.ComputeJaroWinklerDistance ("TANYA", "TONYA"));
+			this.AreRoughlyEqual (0.805, JaroWinkler.ComputeJaroWinklerDistance ("SEAN", "SUSAN"));
+			this.AreRoughlyEqual (0.933, JaroWinkler.ComputeJaroWinklerDistance ("JON", "JOHN"));
+		}
+
+
+		public void AreRoughlyEqual(double d1, double d2)
+		{
+			Assert.IsTrue (System.Math.Abs (d1 -d2) < 0.01);
+		}
+
+
+	}
+
+
+}
---
+++ Epsitec.Cresus/Common/Text/JaroWinkler.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+
+namespace Epsitec.Common.Text
+{
+
+
+	public static class JaroWinkler
+	{
+
+
+		public static double ComputeJaroDistance(string s1, string s2)
+		{
+			JaroWinkler.PreProcessArguments (ref s1, ref s2);
+
+			if (s1 == s2)
+			{
+				return JaroWinkler.MaxValue;
+			}
+			else
+			{
+				return JaroWinkler.ComputeJaroDistanceImplementation (s1, s2);
+			}
+		}
+
+
+		private static double ComputeJaroDistanceImplementation(string s1, string s2)
+		{
+			int s1Length = s1.Length;
+			int s2Length = s2.Length;
+
+			int maxDistance = s2Length / 2;
+			int nbMatches = 0;
+			int nbTranspositions = 0;
+
+			int? previousPosition = null;
+
+			for (int i = 0; i < s1Length; i++)
+			{
+				char c = s1[i];
+
+				int jMin = System.Math.Max (0, i - maxDistance);
+				int jMax = System.Math.Min (s2Length, i + maxDistance);
+
+				for (int j = jMin; j < jMax; j++)
+				{
+					if (c == s2[j])
+					{
+						nbMatches++;
+
+						if (previousPosition.HasValue && j < previousPosition)
+						{
+							nbTranspositions++;
+						}
+
+						previousPosition = j;
+
+						break;
+					}
+				}
+			}
+
+			if (nbMatches == 0)
+			{
+				return JaroWinkler.MinValue;
+			}
+			else
+			{
+				double t1 = nbMatches / (double) s1Length;
+				double t2 = nbMatches / (double) s2Length;
+				double t3 = (nbMatches - nbTranspositions) / (double) nbMatches;
+
+				return (t1 + t2 + t3) / 3;
+			}
+		}
+
+
+		public static double ComputeJaroWinklerDistance(string s1, string s2)
+		{
+			var p = JaroWinkler.DefaultScalingFactor;
+			var l = JaroWinkler.DefaultCommonPrefixMaxLength;
+			
+			return JaroWinkler.ComputeJaroWinklerDistance (s1, s2, p, l);
+		}
+
+
+		public static double ComputeJaroWinklerDistance(string s1, string s2, double p, int l)
+		{
+			JaroWinkler.PreProcessArguments(ref s1, ref s2);
+
+			if (s1 == s2)
+			{
+				return JaroWinkler.MaxValue;
+			}
+			else
+			{
+				return JaroWinkler.ComputeJaroWinklerDistanceImplementation (s1, s2, p, l);
+			}
+		}
+
+
+		private static double ComputeJaroWinklerDistanceImplementation(string s1, string s2, double p, int l)
+		{
+			double jaroDistance = JaroWinkler.ComputeJaroDistanceImplementation (s1, s2);
+
+			int s1Length = s1.Length;
+			int commonPrefixLength = 0;
+			int commonPrefixMaxLength = System.Math.Min (l, s1Length);
+
+			while (commonPrefixLength < commonPrefixMaxLength && s1[commonPrefixLength] == s2[commonPrefixLength])
+			{
+				commonPrefixLength++;
+			}
+
+			return jaroDistance + (commonPrefixLength * p * (1 - jaroDistance));
+		}
+
+
+		private static void PreProcessArguments(ref string s1, ref string s2)
+		{
+			if (s1 == null)
+			{
+				s1 = "";
+			}
+
+			if (s2 == null)
+			{
+				s2 = "";
+			}
+
+			if (s1.Length > s2.Length)
+			{
+				string tmp = s2;
+
+				s2 = s1;
+				s1 = tmp;
+			}
+		}
+
+
+		public static readonly double MinValue = 0;
+
+
+		public static readonly double MaxValue = 1;
+		
+		
+		public static readonly double DefaultScalingFactor = 0.1;
+		
+		
+		public static readonly int DefaultCommonPrefixMaxLength = 4;
+		
+
+
+	}
+
+
+}
