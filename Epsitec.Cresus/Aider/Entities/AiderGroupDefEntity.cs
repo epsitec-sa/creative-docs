d714882969845149d74959e25c2e44ade15dfc08
Marc Bettex <bettex@epsitec.ch>
2012-04-20 14:53:46 +0000

    - Ajout dans AiderGroupDefEntity d'une méthode permettant d'instantier une définition de groupe en un group, y compris tous les sous-groupes récursivements.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@18457 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/Entities/AiderGroupDefEntity.cs
+++ Epsitec.Cresus/Aider/Entities/AiderGroupDefEntity.cs
@@ -1,6 +1,8 @@
 //	Copyright © 2012, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
+using Epsitec.Aider.Enumerations;
+
 using Epsitec.Common.Types;
 
 using Epsitec.Cresus.Core;
@@ -28,6 +30,30 @@ namespace Epsitec.Aider.Entities
 		{
 			yield return this.Name;
 		}
+
+		public AiderGroupEntity Instantiate(BusinessContext businessContext, string name)
+		{
+			var group = businessContext.CreateEntity<AiderGroupEntity> ();
+
+			group.Name = name;
+			group.GroupDef = this;
+
+			// TODO Add more stuff to the group, such as root, start date, etc.
+
+			foreach (var subGroupDef in this.Subgroups)
+			{
+				var subGroup = subGroupDef.Instantiate (businessContext, subGroupDef.Name);
+
+				var groupRelationship = businessContext.CreateEntity<AiderGroupRelationshipEntity> ();
+
+				groupRelationship.Group1 = group;
+				groupRelationship.Group2 = subGroup;
+				groupRelationship.Type = GroupRelationshipType.Inclusion;
+			}
+
+			return group;
+		}
+
 
 		public static AiderGroupDefEntity Find(BusinessContext businessContext, string name)
 		{
