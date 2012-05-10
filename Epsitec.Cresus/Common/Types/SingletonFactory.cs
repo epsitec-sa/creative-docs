b76c0ab4c66e469cd784f180f950574a48a7d6d3
Pierre Arnaud <arnaud@opac.ch>
2012-05-10 04:06:14 +0000

    - Common.Types: ajouté SingletonFactory qui permet d'allouer des singletons de manière thread-safe.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@18614 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common/Types/SingletonFactory.cs
@@ -0,0 +1,48 @@
+//	Copyright © 2012, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Common.Types
+{
+	/// <summary>
+	/// The <c>SingletonFactory</c> class is used to retrieve singleton instances of some runtime
+	/// type. An internal cache is used to avoid allocating singletons more than once for a given
+	/// type.
+	/// </summary>
+	public static class SingletonFactory
+	{
+		public static T GetSingleton<T>(System.Type type)
+			where T : class
+		{
+			cacheExclusion.Wait ();
+
+			try
+			{
+				object value;
+				if (SingletonFactory.cache.TryGetValue (type, out value) == false)
+				{
+					value = System.Activator.CreateInstance (type);
+					SingletonFactory.cache[type] = value;
+				}
+
+				return value as T;
+			}
+			finally
+			{
+				cacheExclusion.Release ();
+			}
+		}
+
+		public static T GetSingleton<T>(System.Type genericType, params System.Type[] typeArguments)
+			where T : class
+		{
+			return SingletonFactory.GetSingleton<T> (genericType.MakeGenericType (typeArguments));
+		}
+
+
+		private static System.Threading.SemaphoreSlim	cacheExclusion	= new System.Threading.SemaphoreSlim (1);
+		private static Dictionary<System.Type, object>	cache			= new Dictionary<System.Type, object> ();
+	}
+}
