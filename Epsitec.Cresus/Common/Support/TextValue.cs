f4e3b832e5cf1ed1d46220f71e504a1fb88463f5
Pierre Arnaud <arnaud@opac.ch>
2012-06-14 14:47:52 +0000

    - Common: fichier oublié.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@18894 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common/Support/TextValue.cs
+++ Epsitec.Cresus/Common/Support/TextValue.cs
@@ -1,9 +1,11 @@
 //	Copyright © 2012, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
+using Epsitec.Common.Types;
+using Epsitec.Common.Types.Collections;
+
 using System.Collections.Generic;
 using System.Linq;
-using Epsitec.Common.Types;
 
 namespace Epsitec.Common.Support
 {
