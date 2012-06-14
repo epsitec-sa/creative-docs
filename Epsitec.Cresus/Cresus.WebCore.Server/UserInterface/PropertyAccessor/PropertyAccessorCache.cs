bfad8864525a884e2efcfaf65fc165c52fc2b8c8
Marc Bettex <bettex@epsitec.ch>
2012-06-14 14:55:41 +0000

    - Extrait une bonne partie de la logique de PropertyAccessorCache dans AbstractLambdaCache pour pouvoir la réutiliser par la suite.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@18896 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Server/UserInterface/PropertyAccessor/PropertyAccessorCache.cs
+++ Epsitec.Cresus/Cresus.WebCore.Server/UserInterface/PropertyAccessor/PropertyAccessorCache.cs
@@ -1,71 +1,17 @@
-﻿using Epsitec.Common.Types;
-
-using System.Collections.Generic;
-
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 
 
 namespace Epsitec.Cresus.WebCore.Server.UserInterface.PropertyAccessor
 {
 
 
-	internal sealed class PropertyAccessorCache
+	internal sealed class PropertyAccessorCache : AbstractLambdaCache<AbstractPropertyAccessor>
 	{
 
-
-		public PropertyAccessorCache()
+		protected override AbstractPropertyAccessor Create(LambdaExpression lambda, string id)
 		{
-			this.lambdaToPropertyAccessor = new Dictionary<string, AbstractPropertyAccessor> ();
-			this.idToPropertyAccessor = new Dictionary<string, AbstractPropertyAccessor> ();
+			return AbstractPropertyAccessor.Create (lambda, id);
 		}
-
-
-		public AbstractPropertyAccessor Get(LambdaExpression lambda)
-		{
-			AbstractPropertyAccessor propertyAccessor;
-
-			var lambdaKey = PropertyAccessorCache.GetLambdaKey (lambda);
-
-			var exists = this.lambdaToPropertyAccessor.TryGetValue (lambdaKey, out propertyAccessor);
-
-			if (!exists)
-			{
-				var id = InvariantConverter.ToString (this.lambdaToPropertyAccessor.Count);
-
-				propertyAccessor = AbstractPropertyAccessor.Create (lambda, id);
-
-				this.lambdaToPropertyAccessor[lambdaKey] = propertyAccessor;
-				this.idToPropertyAccessor[id] = propertyAccessor;
-			}
-
-			return propertyAccessor;
-		}
-
-
-		public AbstractPropertyAccessor Get(string id)
-		{
-			AbstractPropertyAccessor propertyAccessor;
-
-			this.idToPropertyAccessor.TryGetValue (id, out propertyAccessor);
-
-			return propertyAccessor;
-		}
-
-
-		private static string GetLambdaKey(LambdaExpression lambda)
-		{
-			var part1 = lambda.ToString ();
-			var part2 = lambda.ReturnType.FullName;
-			var part3 = lambda.Parameters[0].Type.FullName;
-
-			return part1 + part2 + part3;
-		}
-
-
-		private readonly Dictionary<string, AbstractPropertyAccessor> lambdaToPropertyAccessor;
-
-
-		private readonly Dictionary<string, AbstractPropertyAccessor> idToPropertyAccessor;
 
 
 	}
