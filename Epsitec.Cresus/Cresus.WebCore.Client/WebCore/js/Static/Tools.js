ed3e8b4dac3d71ae369e4e187814d49cf7cc8cb5
Marc Bettex <bettex@epsitec.ch>
2012-06-20 13:27:10 +0000

    - Ajouté Tools.js pour contenir des méthodes utilitaires bien pratiques.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@18944 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.WebCore.Client/WebCore/js/Static/Tools.js
@@ -0,0 +1,36 @@
+Ext.define('Epsitec.Cresus.Core.Static.Tools',
+  {
+    alias : 'epsitec.tools',
+    
+    statics :
+    {
+      isUndefined : function(item) {
+        return typeof item === "undefined";
+      },
+      
+      getValueOrDefault : function(value, defaultValue)
+      {
+        return this.isUndefined(value)
+          ? defaultValue
+          : value;
+      },
+      
+      getValueOrNull : function(value)
+      {
+        return this.getValueOrDefault(value, null);
+      },
+      
+      doCallback : function(callback, context, callbackArguments)
+      {
+        if (this.isUndefined(callback) || callback === null)
+        {
+          return;
+        }
+        else
+        {
+          callback.apply(context, callbackArguments);
+        }
+      }
+    }
+  }
+);
