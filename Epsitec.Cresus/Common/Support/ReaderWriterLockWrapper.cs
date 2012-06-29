80cae7fdd3d64a7ce7453644230a87102e88905d
Marc Bettex <bettex@epsitec.ch>
2012-06-29 09:13:45 +0000

    - Ajouté la classe ReaderWriterLockWrapper qui permet de manipuler plus facilement des ReaderWriterLockSlim.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@18956 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common/Support/ReaderWriterLockWrapper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Epsitec.Common.Support
+{
+
+
+	public sealed class ReaderWriterLockWrapper : IDisposable
+	{
+
+
+		public ReaderWriterLockWrapper()
+		{
+			this.rwLock = new ReaderWriterLockSlim ();
+		}
+
+
+		public IDisposable LockRead()
+		{
+			return TimedReaderWriterLock.LockRead (this.rwLock);
+		}
+
+
+		public IDisposable LockWrite()
+		{
+			return TimedReaderWriterLock.LockWrite (this.rwLock);
+		}
+
+
+		#region IDisposable Members
+
+
+		public void Dispose()
+		{
+			this.rwLock.Dispose ();
+		}
+
+
+		#endregion
+	
+
+		private readonly ReaderWriterLockSlim rwLock;
+
+
+	}
+
+
+}
