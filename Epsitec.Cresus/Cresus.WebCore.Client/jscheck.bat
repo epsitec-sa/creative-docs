1793bf281b65d303dec83d25967c58cad0e93f7c
Marc Bettex <bettex@epsitec.ch>
2012-07-20 15:05:57 +0000

    - Modification des scripts d'analyse statique des fichiers javascript pour effectuer une analyse avec jshint en plus de celle faite avec google closure linter.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@18991 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Client/jscheck.bat
+++ Epsitec.Cresus/Cresus.WebCore.Client/jscheck.bat
@@ -1,2 +1,7 @@
-gjslint --nojsdoc --strict WebCore\app.js
-gjslint --nojsdoc --strict -r WebCore\js\Static\
+@ECHO ==================== gjslint ====================
+@gjslint --nojsdoc --strict WebCore\app.js
+@gjslint --nojsdoc --strict -r WebCore\js\Static\
+
+@ECHO ==================== jshint ====================
+@CALL jshint.bat /bitwise:true /curly:true /eqeqeq:true /forin:true /noarg:true /noempty:true /nonew:true /trailing:true /immed:true /latedef:true /newcap:true /regexp:true /undef:true /onecase:true /plusplus:true /global:document:true,window:true,Ext:true,Epsitec:true WebCore\app.js
+@CALL jshint.bat /bitwise:true /curly:true /eqeqeq:true /forin:true /noarg:true /noempty:true /nonew:true /trailing:true /immed:true /latedef:true /newcap:true /regexp:true /undef:true /onecase:true /plusplus:true /global:document:true,window:true,Ext:true,Epsitec:true WebCore\js\Static\
--- Epsitec.Cresus/Cresus.WebCore.Client/jsfix.bat
+++ Epsitec.Cresus/Cresus.WebCore.Client/jsfix.bat
@@ -1,2 +1,3 @@
-fixjsstyle --nojsdoc --strict WebCore\app.js
-fixjsstyle --nojsdoc --strict -r WebCore\js\Static\
+@ECHO ==================== fixjsstype ====================
+@fixjsstyle --nojsdoc --strict WebCore\app.js
+@fixjsstyle --nojsdoc --strict -r WebCore\js\Static\
