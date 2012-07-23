180beeea25cd542976ae4ddd2b1e6ce8b14ac34c
Marc Bettex <bettex@epsitec.ch>
2012-07-23 11:59:04 +0000

    - Amélioré les scripts jschec.bat et jsfix.bat pour qu'ils puissent être lancé depuis n'importe où.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@18998 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Client/jscheck.bat
+++ Epsitec.Cresus/Cresus.WebCore.Client/jscheck.bat
@@ -1,7 +1,7 @@
 @ECHO ==================== gjslint ====================
-@gjslint --nojsdoc --strict WebCore\app.js
-@gjslint --nojsdoc --strict -r WebCore\js\
+@gjslint --nojsdoc --strict %~dp0\WebCore\app.js
+@gjslint --nojsdoc --strict -r %~dp0\WebCore\js\
 
 @ECHO ==================== jshint ====================
-@CALL jshint.bat /bitwise:true /curly:true /eqeqeq:true /forin:true /noarg:true /noempty:true /nonew:true /trailing:true /immed:true /latedef:true /newcap:true /regexp:true /undef:true /onecase:true /plusplus:true /global:document:true,window:true,Ext:true,Epsitec:true WebCore\app.js
-@CALL jshint.bat /bitwise:true /curly:true /eqeqeq:true /forin:true /noarg:true /noempty:true /nonew:true /trailing:true /immed:true /latedef:true /newcap:true /regexp:true /undef:true /onecase:true /plusplus:true /global:document:true,window:true,Ext:true,Epsitec:true WebCore\js\
+@CALL jshint.bat /bitwise:true /curly:true /eqeqeq:true /forin:true /noarg:true /noempty:true /nonew:true /trailing:true /immed:true /latedef:true /newcap:true /regexp:true /undef:true /onecase:true /plusplus:true /global:document:true,window:true,Ext:true,Epsitec:true %~dp0\WebCore\app.js
+@CALL jshint.bat /bitwise:true /curly:true /eqeqeq:true /forin:true /noarg:true /noempty:true /nonew:true /trailing:true /immed:true /latedef:true /newcap:true /regexp:true /undef:true /onecase:true /plusplus:true /global:document:true,window:true,Ext:true,Epsitec:true %~dp0\WebCore\js\
