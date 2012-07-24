4ac0b8a51ef0f06a2b44a8412db540194093a9d8
Marc Bettex <bettex@epsitec.ch>
2012-07-24 12:36:00 +0000

    - Nettoyé EditionPanel pour ne plus utiliser les appels à this.up(...).
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19007 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Client/WebCore/js/EditionTile.js
+++ Epsitec.Cresus/Cresus.WebCore.Client/WebCore/js/EditionTile.js
@@ -16,48 +16,65 @@ Ext.define('Epsitec.cresus.webcore.EditionTile', {
     labelAlign: 'top',
     msgTarget: 'side'
   },
-  buttons: [
-    {
-      text: 'Reset',
-      handler: function() {
-        this.up('form').getForm().reset();
-      }
-    },
-    {
-      text: 'Save',
-      handler: function() {
-        if (this.up('form').getForm().isValid()) {
-          var form = this.up('form');
-          form.setLoading();
-          form.getForm().submit({
-            success: function(form, action) {
-              this.setLoading(false);
-              this.entityPanel.columnManager.refreshColumns(
-                  0, this.entityPanel.columnId - 1
-              );
-            },
-            failure: function(form, action) {
-              this.setLoading(false);
-              try {
-                var config = Ext.decode(action.response.responseText);
-                this.getForm().markInvalid(config.errors);
-              }
-              catch (err) {
-                return;
-              }
-            },
-            scope: form
-          });
-        }
-      }
-    }
-  ],
 
   /* Constructor */
 
   constructor: function(options) {
     options.url = 'proxy/entity/edit/' + options.entityId;
+    this.buttons = this.getButtons();
     this.callParent(arguments);
     return this;
+  },
+
+  getButtons: function() {
+    var resetButton, saveButton;
+
+    resetButton = Ext.create('Ext.button.Button', {
+      text: 'Reset',
+      listeners: {
+        click: this.onResetClick,
+        scope: this
+      }
+    });
+
+    saveButton = Ext.create('Ext.button.Button', {
+      text: 'Save',
+      listeners: {
+        click: this.onSaveClick,
+        scope: this
+      }
+    });
+
+    return [resetButton, saveButton];
+  },
+
+  onResetClick: function() {
+    this.getForm().reset();
+  },
+
+  onSaveClick: function() {
+    var form = this.getForm();
+    if (this.form.isValid()) {
+      this.setLoading();
+      form.submit({
+        success: function(form, action) {
+          this.setLoading(false);
+          this.entityPanel.columnManager.refreshColumns(
+              0, this.entityPanel.columnId - 1
+          );
+        },
+        failure: function(form, action) {
+          this.setLoading(false);
+          try {
+            var config = Ext.decode(action.response.responseText);
+            form.markInvalid(config.errors);
+          }
+          catch (err) {
+            Epsitec.ErrorHandler.handleErrorDefault();
+          }
+        },
+        scope: this
+      });
+    }
   }
 });
