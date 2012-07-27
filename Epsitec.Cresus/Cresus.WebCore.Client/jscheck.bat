8aa619885c60839e9bcd4c01c2ce71149b5109ff
Marc Bettex <bettex@epsitec.ch>
2012-07-27 08:35:26 +0000

    - Il n'y a plus besoin du flag --nomultiprocess sur windows pour google closure linter depuis la version 2.3.6.1.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19028 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Client/jscheck.bat
+++ Epsitec.Cresus/Cresus.WebCore.Client/jscheck.bat
@@ -1,10 +1,6 @@
-@REM The --nomultiprocess flag is rally important here, as it gjslint 2.3.6 will
-@REM spawn gazillions of python.exe processes until your PC requires a shutdown
-@REM without it.
-
 @ECHO ==================== gjslint ====================
-@gjslint --nojsdoc --strict --nomultiprocess %~dp0\WebCore\app.js
-@gjslint --nojsdoc --strict --nomultiprocess -r %~dp0\WebCore\js\
+@gjslint --nojsdoc --strict %~dp0\WebCore\app.js
+@gjslint --nojsdoc --strict -r %~dp0\WebCore\js\
 
 @ECHO ==================== jshint ====================
 @CALL jshint.bat /bitwise:true /curly:true /eqeqeq:true /forin:true /noarg:true /noempty:true /nonew:true /trailing:true /immed:true /latedef:true /newcap:true /regexp:true /undef:true /onecase:true /plusplus:true /global:document:true,window:true,Ext:true,Epsitec:true %~dp0\WebCore\app.js
