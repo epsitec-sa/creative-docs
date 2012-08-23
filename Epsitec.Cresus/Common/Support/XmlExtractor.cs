99c50f86f588ede7d0690191a4c6402b76f217d3
Pierre Arnaud <arnaud@opac.ch>
2012-08-23 05:21:37 +0000

    - Common: commentaires.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19140 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common/Support/XmlExtractor.cs
+++ Epsitec.Cresus/Common/Support/XmlExtractor.cs
@@ -7,8 +7,20 @@ using System.Xml.Linq;
 
 namespace Epsitec.Common.Support
 {
+	/// <summary>
+	/// The <c>XmlExtractor</c> class can be used to extract a piece of XML contained
+	/// within a collection of lines. It recognizes the end of the XML. 
+	/// </summary>
+	/// <remarks>
+	/// Any trailing XML comments will not be taken in account by the extractor; as
+	/// soon as a valid end of XML is reached, the extractor will consider that any
+	/// remaining text is in excess.
+	/// </remarks>
 	public class XmlExtractor
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="XmlExtractor"/> class.
+		/// </summary>
 		public XmlExtractor()
 		{
 			this.buffer = new StringBuilder ();
@@ -16,7 +28,13 @@ namespace Epsitec.Common.Support
 		}
 
 
-		public bool Finished
+		/// <summary>
+		/// Gets a value indicating whether this <see cref="XmlExtractor"/> is finished.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if finished; otherwise, <c>false</c>.
+		/// </value>
+		public bool								Finished
 		{
 			get
 			{
@@ -24,7 +42,15 @@ namespace Epsitec.Common.Support
 			}
 		}
 
-		public bool Started
+		/// <summary>
+		/// Gets a value indicating whether this <see cref="XmlExtractor"/> has started
+		/// extracting XML. This will be the case as soon as an XML comment or an XML
+		/// element has been processed. The XML declaration does not count.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if the extraction started; otherwise, <c>false</c>.
+		/// </value>
+		public bool								Started
 		{
 			get
 			{
@@ -32,7 +58,10 @@ namespace Epsitec.Common.Support
 			}
 		}
 
-		public string ExcessText
+		/// <summary>
+		/// Gets the excess text, after the last piece of XML.
+		/// </summary>
+		public string							ExcessText
 		{
 			get
 			{
@@ -41,6 +70,10 @@ namespace Epsitec.Common.Support
 		}
 
 
+		/// <summary>
+		/// Appends a line, including the <c>\n</c> character.
+		/// </summary>
+		/// <param name="line">The line.</param>
 		public void AppendLine(string line)
 		{
 			this.Append (line);
@@ -61,8 +94,12 @@ namespace Epsitec.Common.Support
 				this.buffer.Append ('\n');
 			}
 		}
-		
-		public void Append(string line)
+
+		/// <summary>
+		/// Appends some text.
+		/// </summary>
+		/// <param name="text">The text.</param>
+		public void Append(string text)
 		{
 			if (this.Finished)
 			{
@@ -75,12 +112,12 @@ namespace Epsitec.Common.Support
 			
 			int length = 0;
 
-			foreach (char c in line)
+			foreach (char c in text)
 			{
 				if ((this.Finished) &&
 					(char.IsWhiteSpace (c) == false))
 				{
-					this.excessText = line.Substring (length);
+					this.excessText = text.Substring (length);
 					break;
 				}
 
@@ -209,6 +246,12 @@ namespace Epsitec.Common.Support
 		}
 
 
+		/// <summary>
+		/// Returns the extracted text.
+		/// </summary>
+		/// <returns>
+		/// The extracted text.
+		/// </returns>
 		public override string ToString()
 		{
 			return this.buffer.ToString ();
