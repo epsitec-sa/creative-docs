f5843f03eaa99223abe676af2053a59a5911fe5f
Pierre Arnaud <arnaud@opac.ch>
2012-08-23 04:54:52 +0000

    - Common: testé et terminé l'implémentation du XmlExtractor. - Tests: ajouté XmlExtractorTest pour vérifier le bon fonctionnement du XmlExtractor.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19138 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Tests/Support/XmlExtractorTest.cs
@@ -0,0 +1,220 @@
+//	Copyright © 2012, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using NUnit.Framework;
+
+using System.Xml.Linq;
+using Epsitec.Common.Support;
+
+namespace Epsitec.Common.Tests.Support
+{
+	[TestFixture]
+	public class XmlExtractorTest
+	{
+		[Test]
+		public void CheckEmpty()
+		{
+			var extractor = new XmlExtractor ();
+
+			Assert.IsFalse (extractor.Finished);
+			Assert.IsFalse (extractor.Started);
+
+			extractor.Append ("");
+
+			Assert.IsFalse (extractor.Finished);
+			Assert.IsFalse (extractor.Started);
+
+			extractor.Append (" \t\r\n  ");
+
+			Assert.IsFalse (extractor.Finished);
+			Assert.IsFalse (extractor.Started);
+
+			Assert.AreEqual (" \t\r\n  ", extractor.ToString ());
+		}
+
+		[Test]
+		public void CheckDeclaration1()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<??>");
+
+			Assert.IsFalse (extractor.Finished);
+			Assert.IsFalse (extractor.Started);
+		}
+
+		[Test]
+		public void CheckDeclaration2()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<?xml");
+			extractor.Append ("foo ?>");
+
+			Assert.IsFalse (extractor.Finished);
+			Assert.IsFalse (extractor.Started);
+		}
+
+		[Test]
+		[ExpectedException (typeof (System.FormatException))]
+		public void CheckDeclarationEx1()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<?>");
+		}
+
+		[Test]
+		[ExpectedException (typeof (System.FormatException))]
+		public void CheckDeclarationEx2()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<? -->");
+		}
+
+		[Test]
+		[ExpectedException (typeof (System.FormatException))]
+		public void CheckDeclarationEx3()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<? >");
+		}
+
+		[Test]
+		[ExpectedException (typeof (System.FormatException))]
+		public void CheckDeclarationEx4()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<? < ?>");
+		}
+
+		[Test]
+		public void CheckElement1()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<foo>");
+
+			Assert.IsTrue (extractor.Started);
+
+			extractor.Append ("</foo>");
+
+			Assert.IsTrue (extractor.Started);
+			Assert.IsTrue (extractor.Finished);
+		}
+
+		[Test]
+		public void CheckElement2()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<foo/>");
+
+			Assert.IsTrue (extractor.Started);
+			Assert.IsTrue (extractor.Finished);
+		}
+
+		[Test]
+		[ExpectedException (typeof (System.FormatException))]
+		public void CheckElementEx1()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("< < > >");
+		}
+
+		[Test]
+		[ExpectedException (typeof (System.FormatException))]
+		public void CheckElementEx2()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append (">");
+		}
+
+		[Test]
+		[ExpectedException (typeof (System.InvalidOperationException))]
+		public void CheckElementEx3()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<foo></foo>");
+			extractor.Append ("<bar></bar>");
+		}
+
+		[Test]
+		public void CheckComment1()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<foo><!-- </foo> < < < -->");
+			extractor.Append ("<bar></bar>");
+			extractor.Append ("</foo>");
+		}
+
+		[Test]
+		public void CheckComment2()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<!-- 1 -->");
+			Assert.IsFalse (extractor.Finished);
+			extractor.Append ("<!-- 2 -->");
+			Assert.IsFalse (extractor.Finished);
+			extractor.Append ("<bar></bar>");
+			Assert.IsTrue (extractor.Finished);
+		}
+
+		[Test]
+		public void CheckComment3()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<!-- 1 -->");
+
+			Assert.IsTrue (extractor.Started);
+			Assert.IsFalse (extractor.Finished);
+		}
+
+		[Test]
+		[ExpectedException (typeof (System.FormatException))]
+		public void CheckExcessEx1()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<!-- 1 --> abc");
+		}
+
+		[Test]
+		[ExpectedException (typeof (System.FormatException))]
+		public void CheckExcessEx2()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("x");
+		}
+
+		[Test]
+		[ExpectedException (typeof (System.FormatException))]
+		public void CheckExcessEx3()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<? xml ?> abc");
+		}
+
+		[Test]
+		public void CheckExcess1()
+		{
+			var extractor = new XmlExtractor ();
+
+			extractor.Append ("<foo>x</foo>  y");
+			
+			Assert.IsTrue (extractor.Finished);
+			Assert.AreEqual ("y", extractor.ExcessText);
+		}
+	}
+}
--- Epsitec.Cresus/Common/Support/XmlExtractor.cs
+++ Epsitec.Cresus/Common/Support/XmlExtractor.cs
@@ -2,6 +2,7 @@
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
 using System.Collections.Generic;
+using System.Text;
 using System.Xml.Linq;
 
 namespace Epsitec.Common.Support
@@ -10,7 +11,8 @@ namespace Epsitec.Common.Support
 	{
 		public XmlExtractor()
 		{
-			this.buffer = new System.Text.StringBuilder ();
+			this.buffer = new StringBuilder ();
+			this.depth = -1;
 		}
 
 
@@ -18,61 +20,192 @@ namespace Epsitec.Common.Support
 		{
 			get
 			{
-				return this.depth == 0;
+				return this.open == 0 && this.depth == 0 && this.elementCount > 0;
 			}
+		}
+
+		public bool Started
+		{
+			get
+			{
+				return this.open > 0 || this.depth > -1;
+			}
+		}
+
+		public string ExcessText
+		{
+			get
+			{
+				return this.excessText;
+			}
+		}
+
+
+		public void AppendLine(string line)
+		{
+			this.Append (line);
+			this.Append ("\n");
 		}
 		
 		public void Append(string line)
 		{
-			char prev = ' ';
+			if (this.Finished)
+			{
+				throw new System.InvalidOperationException ("The XML extractor cannot accept additional lines: it is already in the finished state");
+			}
+
+			char prev0 = ' ';
+			char prev1 = ' ';
+			char prev2 = ' ';
+			
+			int length = 0;
 
 			foreach (char c in line)
 			{
-				if ((this.depth == 0) &&
-					(this.open == 0))
+				if ((this.Finished) &&
+					(char.IsWhiteSpace (c) == false))
 				{
+					this.excessText = line.Substring (length);
 					break;
+				}
+
+				if (this.inComment)
+				{
+					if ((prev1 == '-') &&			//	"...-->"
+						(prev0 == '-') &&
+						(c == '>'))
+					{
+						this.inComment = false;
+						this.open--;
+						this.depth--;
+					}
+
+					goto append;
 				}
 				
 				if (c == '<')
 				{
+					if ((this.inDeclaration) ||
+						(this.open > 0))
+					{
+						throw new System.FormatException ("Invalid XML syntax");
+					}
+					
 					this.open++;
 				}
 				else if (c == '>')
 				{
-					this.open--;
+					if (this.open != 1)
+					{
+						throw new System.FormatException ("Invalid XML syntax");
+					}
 
-					if (prev == '/')			//	"<xxx/>"
-					{							//	     ^
-						this.depth--;
+					this.open--;
+					
+					if (this.inDeclaration)
+					{
+						if (prev0 == '?')		//	"..?>"
+						{
+							if (prev1 == '<')
+							{
+								throw new System.FormatException ("The XML declaration is invalid");
+							}
+
+							this.inDeclaration = false;
+						}
+						else
+						{
+							throw new System.FormatException ("The XML declaration is invalid");
+						}
+					}
+					else
+					{
+						if (prev0 == '/')		//	"<xxx/>"
+						{						//	     ^
+							this.elementCount++;
+							this.depth--;
+						}
 					}
 				}
 				else if (c == '/')
 				{
-					if (prev == '<')			//	"</xxx>"
+					if (prev0 == '<')			//	"</xxx>"
 					{							//	  ^
+						this.elementCount++;
 						this.depth--;
 					}
 				}
-				else if (prev == '<')			//	"<xxx.."
+				else if (prev0 == '<')			//	"<xxx.."
 				{								//	  ^
-					if (this.depth == -1)
-					{
-						this.depth = 0;
+					if (c == '?')
+					{							//	"<?.."
+						if (this.depth > 0)
+						{
+							throw new System.FormatException ("The XML cannot contain a declaration inside of valid the XML tree");
+						}
+						this.inDeclaration = true;
 					}
-					this.depth++;
+					else
+					{
+						if (this.depth == -1)
+						{
+							this.depth = 0;
+						}
+						this.depth++;
+					}
+				}
+				else if ((prev2 == '<') && (prev1 == '!') && (prev0 == '-') && (c == '-'))
+				{
+					this.inComment = true;		//	"<!--..."
+					
+					//	Avoid that part of the "<!--" is used as the start of "-->" by clearing
+					//	the history:
+					
+					prev2 = ' ';
+					prev1 = ' ';
+					prev0 = ' ';
+					
+					this.buffer.Append ('-');
+					length++;
+					
+					continue;
+				}
+				else if (char.IsWhiteSpace (c) == false)
+				{
+					if ((this.open == 0) &&
+						(this.depth < 1))
+					{
+						if ((this.inComment == false) &&
+							(this.inDeclaration == false))
+						{
+							throw new System.FormatException ("The XML data contains text outside of an XML element");
+						}
+					}
 				}
 
-				prev = c;
+			append:
+				prev2 = prev1;
+				prev1 = prev0;
+				prev0 = c;
 
 				this.buffer.Append (c);
+				length++;
 			}
 		}
 
 
+		public override string ToString()
+		{
+			return this.buffer.ToString ();
+		}
 
-		private readonly System.Text.StringBuilder buffer;
-		private int depth;
-		private int open;
+
+		private readonly StringBuilder			buffer;
+		private string							excessText;
+		private int								depth;
+		private int								open;
+		private int								elementCount;
+		private bool							inDeclaration;
+		private bool							inComment;
 	}
 }
