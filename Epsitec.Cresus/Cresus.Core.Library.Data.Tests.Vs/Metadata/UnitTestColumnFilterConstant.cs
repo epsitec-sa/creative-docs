9fa88b47eecd7bfde143dd758384d039f49108f9
Pierre Arnaud <arnaud@opac.ch>
2012-09-03 09:33:53 +0000

    - Tests: ajoutö un test pour ColumnFilterConstant.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19208 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core.Library.Data.Tests.Vs/Metadata/UnitTestColumnFilterConstant.cs
@@ -0,0 +1,68 @@
+﻿//	Copyright © 2012, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Support.Extensions;
+
+using Epsitec.Cresus.Core.Data;
+using Epsitec.Cresus.Core.Library.Data.Tests.Vs.Helpers;
+
+using Epsitec.Cresus.Database;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+
+using System.Linq;
+using Epsitec.Common.Support;
+using Epsitec.Cresus.DataLayer.Context;
+using Epsitec.Common.Types;
+
+
+namespace Epsitec.Cresus.Core.Metadata.Tests.Vs
+{
+	[TestClass]
+	public class UnitTestColumnFilterConstant
+	{
+		[TestMethod]
+		public void CheckFromAndParse()
+		{
+			var entityKey = new EntityKey (Druid.Parse ("[J1AB1]"), new DbKey (new DbId (2)));
+
+			var col1 = ColumnFilterConstant.From (entityKey);
+			var col2 = ColumnFilterConstant.FromEnum (BindingMode.OneTime);
+			var col3 = ColumnFilterConstant.From (10);
+			var col4 = ColumnFilterConstant.From (100.123M);
+			var col5 = ColumnFilterConstant.From ("Hello");
+			var col6 = ColumnFilterConstant.From (new System.DateTime (2012, 9, 3));
+			var col7 = ColumnFilterConstant.From (new Date (2012, 9, 3));
+			var col8 = ColumnFilterConstant.From (new Time (11, 00, 59));
+
+			Assert.AreEqual (ColumnFilterConstantType.EntityKey, col1.Type);
+			Assert.AreEqual (ColumnFilterConstantType.Enumeration, col2.Type);
+			Assert.AreEqual (ColumnFilterConstantType.Integer, col3.Type);
+			Assert.AreEqual (ColumnFilterConstantType.Decimal, col4.Type);
+			Assert.AreEqual (ColumnFilterConstantType.String, col5.Type);
+			Assert.AreEqual (ColumnFilterConstantType.DateTime, col6.Type);
+			Assert.AreEqual (ColumnFilterConstantType.Date, col7.Type);
+			Assert.AreEqual (ColumnFilterConstantType.Time, col8.Type);
+
+			var ser1 = col1.ToString ();
+			var ser2 = col2.ToString ();
+			var ser3 = col3.ToString ();
+			var ser4 = col4.ToString ();
+			var ser5 = col5.ToString ();
+			var ser6 = col6.ToString ();
+			var ser7 = col7.ToString ();
+			var ser8 = col8.ToString ();
+
+			Assert.AreEqual (col1, ColumnFilterConstant.Parse (ser1));
+			Assert.AreEqual (col2, ColumnFilterConstant.Parse (ser2));
+			Assert.AreEqual (col3, ColumnFilterConstant.Parse (ser3));
+			Assert.AreEqual (col4, ColumnFilterConstant.Parse (ser4));
+			Assert.AreEqual (col5, ColumnFilterConstant.Parse (ser5));
+			Assert.AreEqual (col6, ColumnFilterConstant.Parse (ser6));
+			Assert.AreEqual (col7, ColumnFilterConstant.Parse (ser7));
+			Assert.AreEqual (col8, ColumnFilterConstant.Parse (ser8));
+		}
+	}
+}
