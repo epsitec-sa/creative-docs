5318402bcfe904138387e27b639d43e023c4dbda
Pierre Arnaud <arnaud@opac.ch>
2012-10-03 05:55:42 +0000

    - Aider: expérimentation, AiderUserEntity est bien créé à la place de SoftwareUserEntity par la classe Hack.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19300 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/AiderUserSession.cs
+++ Epsitec.Cresus/Aider/AiderUserSession.cs
@@ -1,6 +1,8 @@
 //	Copyright © 2012, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
+using Epsitec.Aider.Entities;
+
 using Epsitec.Cresus.Core;
 using Epsitec.Cresus.Core.Business.UserManagement;
 using Epsitec.Cresus.Core.Entities;
@@ -12,9 +14,43 @@ namespace Epsitec.Aider
 {
 	public sealed class AiderUserSession : UserSession
 	{
-		public AiderUserSession(string sessionId)
-			: base (sessionId)
+		public AiderUserSession(UserManager manager, string sessionId)
+			: base (manager, sessionId)
 		{
 		}
+
+
+		public new AiderUserManager UserManager
+		{
+			get
+			{
+				return base.UserManager as AiderUserManager;
+			}
+		}
+
+		
+		public AiderUserScopeEntity GetActiveScope()
+		{
+			if ((this.activeScope == null) &&
+				(this.UserManager.AuthenticatedUser != null))
+			{
+				this.activeScope = this.ResolveActiveScope ();
+			}
+
+			return this.activeScope;
+		}
+
+		
+		private AiderUserScopeEntity ResolveActiveScope()
+		{
+			var user = this.UserManager.AuthenticatedUser;
+
+			//	TODO: resolve active scope
+
+			return null;
+		}
+
+
+		private AiderUserScopeEntity activeScope;
 	}
 }
