a328f5c5a987920ccedf7b22635e8cf60319dfc0
Pierre Arnaud <arnaud@opac.ch>
2012-10-03 05:37:53 +0000

    - Aider: intégré AiderUserSession qui se substitue à UserSession.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19297 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/AiderUserSession.cs
@@ -0,0 +1,21 @@
+//	Copyright © 2012, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Cresus.Core;
+using Epsitec.Cresus.Core.Business.UserManagement;
+using Epsitec.Cresus.Core.Entities;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Aider
+{
+	public sealed class AiderUserSession : UserSession
+	{
+		public AiderUserSession(string sessionId)
+			: base (sessionId)
+		{
+			System.Console.WriteLine ("Session {0} created", sessionId);
+		}
+	}
+}
