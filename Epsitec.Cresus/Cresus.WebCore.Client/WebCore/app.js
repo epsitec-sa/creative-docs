644ed8e7f7ca768d4f69d25c37b12ab93d0ac0a0
Marc Bettex <bettex@epsitec.ch>
2012-10-24 13:30:33 +0000

    - Ajouté un fix pour un bug de localization connu d'ExtJs. Le fix en question est documenté dans le code.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19390 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Client/WebCore/app.js
+++ Epsitec.Cresus/Cresus.WebCore.Client/WebCore/app.js
@@ -23,10 +23,30 @@ function() {
     tabManager: null,
 
     launch: function() {
+      this.fixLocalizationBug();
       this.showLoginPanel();
     },
 
     /* Additional methods */
+
+    fixLocalizationBug: function() {
+
+      // There is a bug in extjs. The loading texts for Ext.view.AbstractView
+      // and Ext.LoadMask are not localized properly. That's a know bug as of
+      // version 4.1.2 and its id is EXTJSIV-7483. This method implement the
+      // suggested workaround that can be found on the sencha forums at the url
+      // http://www.sencha.com/forum/showthread.php?245783 .
+
+      var loadingText = Ext.view.AbstractView.prototype.msg;
+
+      Ext.override(Ext.view.AbstractView, {
+        loadingText: loadingText
+      });
+
+      Ext.override(Ext.LoadMask, {
+        msg: loadingText
+      });
+    },
 
     showLoginPanel: function() {
       this.loginPanel = Ext.create('Epsitec.LoginPanel', {
