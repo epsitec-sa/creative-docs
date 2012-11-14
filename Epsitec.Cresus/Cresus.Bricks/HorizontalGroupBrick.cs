600a6af3decf0bf83e75ac1aca1740c8225af087
Marc Bettex <bettex@epsitec.ch>
2012-11-14 13:31:41 +0000

    - Simplification dans HorizontalGroupBrick en supprimant une classe inutile dans la hierarchie.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19469 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Bricks/HorizontalGroupBrick.cs
+++ Epsitec.Cresus/Cresus.Bricks/HorizontalGroupBrick.cs
@@ -3,15 +3,55 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace Epsitec.Cresus.Bricks
 {
-	public class HorizontalGroupBrick<TSource, TParent> : InputHorizontalGroupBrick<TSource, HorizontalGroupBrick<TSource, TParent>, TParent>
-				where TParent : Brick
+	public class HorizontalGroupBrick<T, TParent> : Brick
+		where TParent : Brick
 	{
 		public HorizontalGroupBrick(TParent parent)
-			: base (parent)
 		{
+			parent.AddProperty (new BrickProperty (BrickPropertyKey.HorizontalGroup, this));
+
+			this.DefineBrickWall (parent.BrickWall);
+			this.parent = parent;
 		}
+
+		public HorizontalGroupBrick<T, TParent> Field<TResult>(Expression<System.Func<T, TResult>> expression)
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Field, expression));
+			return this;
+		}
+
+		public HorizontalGroupBrick<T, TParent> Width(int value)
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Width, value));
+			return this;
+		}
+
+		public HorizontalGroupBrick<T, TParent> Height(int value)
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Height, value));
+			return this;
+		}
+
+		public HorizontalGroupBrick<T, TParent> ReadOnly()
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.ReadOnly));
+			return this;
+		}
+
+		public TParent End()
+		{
+			return this.parent;
+		}
+
+		public override System.Type GetFieldType()
+		{
+			return typeof (T);
+		}
+
+		readonly TParent parent;
 	}
 }
