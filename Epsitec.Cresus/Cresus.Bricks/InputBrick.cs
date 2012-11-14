479a333136233f28f2d1269123fe3ab4b24b0a0b
Marc Bettex <bettex@epsitec.ch>
2012-11-14 14:26:50 +0000

    - Simplification dans InputBrick en supprimant une classe inutile dans la hierarchie.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19474 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Bricks/InputBrick.cs
+++ Epsitec.Cresus/Cresus.Bricks/InputBrick.cs
@@ -1,17 +1,120 @@
-//	Copyright © 2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Copyright © 2011-2012, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
 
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Dynamic;
 
 namespace Epsitec.Cresus.Bricks
 {
-	public class InputBrick<T, TParent> : InternalInputBrick<T, InputBrick<T, TParent>, TParent>
-			where TParent : Brick
+	public class InputBrick<T, TParent> : Brick
+		where TParent : Brick
 	{
 		public InputBrick(TParent parent)
-			: base (parent)
 		{
+			parent.AddProperty (new BrickProperty (BrickPropertyKey.Input, this));
+
+			this.DefineBrickWall (parent.BrickWall);
+			this.parent = parent;
 		}
+
+		public InputBrick<T, TParent> Title(string value)
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Title, value));
+			return this;
+		}
+
+		public HorizontalGroupBrick<T, InputBrick<T, TParent>> HorizontalGroup(string value)
+		{
+			var group = new HorizontalGroupBrick<T, InputBrick<T, TParent>> (this);
+			group.AddProperty (new BrickProperty (BrickPropertyKey.Title, value));
+			return group;
+		}
+
+		public HorizontalGroupBrick<T, InputBrick<T, TParent>> HorizontalGroup()
+		{
+			return new HorizontalGroupBrick<T, InputBrick<T, TParent>> (this);
+		}
+
+		public InputBrick<T, TParent> Field<TResult>(Expression<System.Func<T, TResult>> expression)
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Field, expression));
+			return this;
+		}
+
+		public InputBrick<T, TParent> Width(int value)
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Width, value));
+			return this;
+		}
+
+		public InputBrick<T, TParent> Height(int value)
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Height, value));
+			return this;
+		}
+
+		public InputBrick<T, TParent> ReadOnly()
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.ReadOnly));
+			return this;
+		}
+
+		public InputBrick<T, TParent> Password()
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Password));
+			return this;
+		}
+
+		public InputBrick<T, TParent> WithSpecialController(int mode = 0)
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.SpecialController, mode));
+			return this;
+		}
+
+		public InputBrick<T, TParent> PickFromCollection(System.Collections.IEnumerable value)
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.FromCollection, value));
+			return this;
+		}
+
+		public InputBrick<T, TParent> Button(FormattedText title, FormattedText description, System.Action action)
+		{
+			dynamic expando = new ExpandoObject ();
+
+			expando.ButtonTitle       = title;
+			expando.ButtonDescription = description;
+			expando.ButtonAction      = action;
+
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Button, expando));
+			return this;
+		}
+
+		public InputBrick<T, TParent> SearchPanel(FormattedText searchTitle, FormattedText actionTitle, System.Action<Epsitec.Common.Support.EntityEngine.AbstractEntity> action)
+		{
+			dynamic expando = new ExpandoObject ();
+
+			expando.SearchTitle  = searchTitle;
+			expando.ButtonTitle  = actionTitle;
+			expando.ButtonAction = action;
+			
+			this.AddProperty (new BrickProperty (BrickPropertyKey.SearchPanel, expando));
+			return this;
+		}
+
+		public TParent End()
+		{
+			return this.parent;
+		}
+
+		public override System.Type GetFieldType()
+		{
+			return typeof (T);
+		}
+
+		readonly TParent parent;
 	}
 }
