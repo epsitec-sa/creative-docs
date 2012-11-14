213b961504141d16638a81f139dcb6f6e04d415a
Marc Bettex <bettex@epsitec.ch>
2012-11-14 14:41:14 +0000

    - Simplification dans SimpleBrick en supprimant une classe inutile dans la hierarchie.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19476 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Bricks/SimpleBrick.cs
+++ Epsitec.Cresus/Cresus.Bricks/SimpleBrick.cs
@@ -3,28 +3,50 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace Epsitec.Cresus.Bricks
 {
-	public class SimpleBrick<T> : InternalSimpleBrick<T, SimpleBrick<T>>
+	public class SimpleBrick<T> : InternalBaseBrick<T, SimpleBrick<T>>
 	{
-		public SimpleBrick()
+		public SimpleBrick<T> Name(string value)
 		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Name, value));
+			return this;
 		}
 
-		private SimpleBrick(Brick parent)
+		public SimpleBrick<T> Include<TResult>(Expression<System.Func<T, TResult>> expression)
 		{
-			parent.AddProperty (new BrickProperty (BrickPropertyKey.OfType, this));
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Include, expression));
+			return this;
+		}
+
+		public SimpleBrick<T> Attribute<TAttribute>(TAttribute attributeValue)
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Attribute, new AttributeValue<TAttribute> (attributeValue)));
+			return this;
 		}
 
 		public SimpleBrick<TOutput> OfType<TOutput>()
 		{
-			var brick = new SimpleBrick<TOutput> (this);
+			var brick = new SimpleBrick<TOutput> ();
+
+			this.AddProperty (new BrickProperty (BrickPropertyKey.OfType, brick));
 
 			brick.InheritResolver (this);
 			brick.DefineBrickWall (this.BrickWall);
 
 			return brick;
+		}
+
+		public TemplateBrick<T, SimpleBrick<T>> Template()
+		{
+			return new TemplateBrick<T, SimpleBrick<T>> (this);
+		}
+
+		public InputBrick<T, SimpleBrick<T>> Input()
+		{
+			return new InputBrick<T, SimpleBrick<T>> (this);
 		}
 	}
 }
