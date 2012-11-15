d5081f5f7f0fa291bb282f2f7070a22f3de796c4
Marc Bettex <bettex@epsitec.ch>
2012-11-15 08:25:11 +0000

    - TemplateBrick hérite de ChildBrick au lieu de Brick<T, TParent>. Cela économise du code redondant entre ChildBrick et TemplateBrick et duplique du code qui était dans Brick<T, TParent>. Mais au final, cela permettra une simplification importante entre Brick<T, TParent> et SimpleBrick.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19482 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Bricks/TemplateBrick.cs
+++ Epsitec.Cresus/Cresus.Bricks/TemplateBrick.cs
@@ -1,27 +1,65 @@
 //	Copyright © 2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
-using System.Collections.Generic;
-using System.Linq;
-
+using System.Linq.Expressions;
 namespace Epsitec.Cresus.Bricks
 {
-	public class TemplateBrick<T, TParent> : Brick<T, TemplateBrick<T, TParent>>
+	public class TemplateBrick<T, TParent> : ChildBrick<T, TParent>
 			where TParent : Brick
 	{
 		public TemplateBrick(TParent parent)
+			: base (parent, BrickPropertyKey.Template)
 		{
-			parent.AddProperty (new BrickProperty (BrickPropertyKey.Template, this));
-
-			this.DefineBrickWall (parent.BrickWall);
-			this.parent = parent;
 		}
 
-		public TParent End()
+		public TemplateBrick<T, TParent> Icon(string value)
 		{
-			return this.parent;
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Icon, value));
+			return this;
 		}
 
-		private readonly TParent parent;
+		public TemplateBrick<T, TParent> Title(Mortar<T> value)
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Title, value));
+			return this;
+		}
+
+		public TemplateBrick<T, TParent> Title<TResult>(Expression<System.Func<T, TResult>> expression)
+		{
+			return this.Title (new Mortar<T, TResult> (expression));
+		}
+
+		public TemplateBrick<T, TParent> TitleCompact(Mortar<T> value)
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.TitleCompact, value));
+			return this;
+		}
+
+		public TemplateBrick<T, TParent> TitleCompact<TResult>(Expression<System.Func<T, TResult>> expression)
+		{
+			return this.TitleCompact (new Mortar<T, TResult> (expression));
+		}
+
+		public TemplateBrick<T, TParent> Text(Mortar<T> value)
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.Text, value));
+			return this;
+		}
+
+		public TemplateBrick<T, TParent> Text<TResult>(Expression<System.Func<T, TResult>> expression)
+		{
+			return this.Text (new Mortar<T, TResult> (expression));
+		}
+
+		public TemplateBrick<T, TParent> TextCompact(Mortar<T> value)
+		{
+			this.AddProperty (new BrickProperty (BrickPropertyKey.TextCompact, value));
+			return this;
+		}
+
+		public TemplateBrick<T, TParent> TextCompact<TResult>(Expression<System.Func<T, TResult>> expression)
+		{
+			return this.TextCompact (new Mortar<T, TResult> (expression));
+		}
 	}
 }
