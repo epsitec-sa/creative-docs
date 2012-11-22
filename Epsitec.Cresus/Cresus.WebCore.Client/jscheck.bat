8848268caca185d192acb1ee26c3bed57387686a
Marc Bettex <bettex@epsitec.ch>
2012-11-22 07:49:29 +0000

    - Correction dans les batchs pour marcher aussi avec des chemins de fichiers qui contiennent des espaces.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19511 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Client/jscheck.bat
+++ Epsitec.Cresus/Cresus.WebCore.Client/jscheck.bat
@@ -1,7 +1,7 @@
 @ECHO ==================== gjslint ====================
-@gjslint --nojsdoc --strict %~dp0\WebCore\app.js
-@gjslint --nojsdoc --strict -r %~dp0\WebCore\js\
+@gjslint --nojsdoc --strict "%~dp0\WebCore\app.js"
+@gjslint --nojsdoc --strict -r "%~dp0\WebCore\js\"
 
 @ECHO ==================== jshint ====================
-@CALL jshint.bat /bitwise:true /curly:true /eqeqeq:true /forin:true /noarg:true /noempty:true /nonew:true /trailing:true /immed:true /latedef:true /newcap:true /regexp:true /undef:true /onecase:true /onevar:true /plusplus:true /global:document:true,window:true,Ext:true,Epsitec:true %~dp0\WebCore\app.js
-@CALL jshint.bat /bitwise:true /curly:true /eqeqeq:true /forin:true /noarg:true /noempty:true /nonew:true /trailing:true /immed:true /latedef:true /newcap:true /regexp:true /undef:true /onecase:true /onevar:true /plusplus:true /global:document:true,window:true,Ext:true,Epsitec:true %~dp0\WebCore\js\
+@CALL jshint.bat /bitwise:true /curly:true /eqeqeq:true /forin:true /noarg:true /noempty:true /nonew:true /trailing:true /immed:true /latedef:true /newcap:true /regexp:true /undef:true /onecase:true /onevar:true /plusplus:true /global:document:true,window:true,Ext:true,Epsitec:true "%~dp0\WebCore\app.js"
+@CALL jshint.bat /bitwise:true /curly:true /eqeqeq:true /forin:true /noarg:true /noempty:true /nonew:true /trailing:true /immed:true /latedef:true /newcap:true /regexp:true /undef:true /onecase:true /onevar:true /plusplus:true /global:document:true,window:true,Ext:true,Epsitec:true "%~dp0\WebCore\js\"
--- Epsitec.Cresus/Cresus.WebCore.Client/jsfix.bat
+++ Epsitec.Cresus/Cresus.WebCore.Client/jsfix.bat
@@ -1,3 +1,3 @@
 @ECHO ==================== fixjsstype ====================
-@fixjsstyle --nojsdoc --strict %~dp0\WebCore\app.js
-@fixjsstyle --nojsdoc --strict -r %~dp0\WebCore\js\
+@fixjsstyle --nojsdoc --strict "%~dp0\WebCore\app.js"
+@fixjsstyle --nojsdoc --strict -r "%~dp0\WebCore\js\"
