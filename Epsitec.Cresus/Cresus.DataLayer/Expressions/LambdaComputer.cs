24ce378244b18960063b0d6a3b1546aad0e80037
Marc Bettex <bettex@epsitec.ch>
2012-12-17 12:49:58 +0000

    - Il manquait la définition du namespace dans le fichier.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19587 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.DataLayer/Expressions/LambdaComputer.cs
+++ Epsitec.Cresus/Cresus.DataLayer/Expressions/LambdaComputer.cs
@@ -5,151 +5,157 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 
 
-public static class LambdaComputer
+namespace Epsitec.Cresus.DataLayer.Expressions
 {
 
 
-	// The code here has been inspired from this blog post :
-	// http://blogs.msdn.com/b/mattwar/archive/2007/08/01/linq-building-an-iqueryable-provider-part-iii.aspx
-
-
-	public static Expression Compute(Expression expression, Func<Expression, bool> isExpressionComputable)
-	{
-		// The basic idea here is first to identify the expressions within the given expression that
-		// can be computed right now (i.e. captured variables, member access, method calls, etc.)
-		// and then to replace them with the result of their execution.
-
-		var finder = new ComputableExpressionFinder (isExpressionComputable);
-		var computableExpressions = finder.Find (expression);
-
-		var computer = new ComputableExpressionComputer (computableExpressions);
-		return computer.Compute (expression);
-	}
-
-
-	private class ComputableExpressionComputer : ExpressionVisitor
+	public static class LambdaComputer
 	{
 
 
-		public ComputableExpressionComputer(ISet<Expression> computableExpressions)
+		// The code here has been inspired from this blog post :
+		// http://blogs.msdn.com/b/mattwar/archive/2007/08/01/linq-building-an-iqueryable-provider-part-iii.aspx
+
+
+		public static Expression Compute(Expression expression, Func<Expression, bool> isExpressionComputable)
 		{
-			this.computableExpressions = computableExpressions;
+			// The basic idea here is first to identify the expressions within the given expression that
+			// can be computed right now (i.e. captured variables, member access, method calls, etc.)
+			// and then to replace them with the result of their execution.
+
+			var finder = new ComputableExpressionFinder (isExpressionComputable);
+			var computableExpressions = finder.Find (expression);
+
+			var computer = new ComputableExpressionComputer (computableExpressions);
+			return computer.Compute (expression);
 		}
 
 
-		public Expression Compute(Expression expression)
+		private class ComputableExpressionComputer : ExpressionVisitor
 		{
-			return this.Visit (expression);
-		}
 
 
-		public override Expression Visit(Expression expression)
-		{
-			return this.computableExpressions.Contains (expression)
-				? this.Evaluate (expression)
-				: base.Visit (expression);
-		}
-
-
-		private Expression Evaluate(Expression expression)
-		{
-			if (expression == null)
+			public ComputableExpressionComputer(ISet<Expression> computableExpressions)
 			{
+				this.computableExpressions = computableExpressions;
+			}
+
+
+			public Expression Compute(Expression expression)
+			{
+				return this.Visit (expression);
+			}
+
+
+			public override Expression Visit(Expression expression)
+			{
+				return this.computableExpressions.Contains (expression)
+					? this.Evaluate (expression)
+					: base.Visit (expression);
+			}
+
+
+			private Expression Evaluate(Expression expression)
+			{
+				if (expression == null)
+				{
+					return expression;
+				}
+				else if (expression.NodeType == ExpressionType.Constant)
+				{
+					return expression;
+				}
+				else
+				{
+					var value = Expression.Lambda (expression).Compile ().DynamicInvoke (null);
+					var type = expression.Type;
+
+					return Expression.Constant (value, type);
+				}
+			}
+
+
+			private readonly ISet<Expression> computableExpressions;
+
+
+		}
+
+
+		private class ComputableExpressionFinder : ExpressionVisitor
+		{
+
+
+			public ComputableExpressionFinder(Func<Expression, bool> isExpressionComputable)
+			{
+				this.isExpressionComputable = isExpressionComputable;
+			}
+
+
+			internal HashSet<Expression> Find(Expression expression)
+			{
+				this.isComputableStack = new Stack<bool> ();
+				this.isComputableStack.Push (true);
+
+				this.computableExpressions = new HashSet<Expression> ();
+
+				this.Visit (expression);
+
+				return this.computableExpressions;
+			}
+
+
+			public override Expression Visit(Expression expression)
+			{
+				if (expression == null)
+				{
+					return expression;
+				}
+
+				// As the ExpressionVisitor class forces us to return an Expression from the Visit
+				// method, we can't return a bool which would have been very useful. That's why we
+				// use a stack of bools to store the values that should have been returned if we
+				// could have done so.
+
+				// We start by assuming that the current expression can be computed if we are said
+				// so by the callback that we are given.
+				this.isComputableStack.Push (this.isExpressionComputable (expression));
+
+				// We walk the tree with the children of the current expression to see if they can
+				// be computed.
+				base.Visit (expression);
+
+				if (!this.isComputableStack.Pop ())
+				{
+					// Either the current expression or at least one of its children cannot be
+					// computed, which implies that the parent expression cannot be computed.
+
+					this.isComputableStack.Pop ();
+					this.isComputableStack.Push (false);
+				}
+				else
+				{
+					// The current expression as well as all its children can be computed, so we add
+					// it to the list of expressions that can be computed.
+
+					this.computableExpressions.Add (expression);
+				}
+
 				return expression;
 			}
-			else if (expression.NodeType == ExpressionType.Constant)
-			{
-				return expression;
-			}
-			else
-			{
-				var value = Expression.Lambda (expression).Compile ().DynamicInvoke (null);
-				var type = expression.Type;
 
-				return Expression.Constant (value, type);
-			}
+
+			private readonly Func<Expression, bool> isExpressionComputable;
+
+
+			private HashSet<Expression> computableExpressions;
+
+
+			private Stack<bool> isComputableStack;
+
+
 		}
-
-
-		private readonly ISet<Expression> computableExpressions;
 
 
 	}
-
-
-	private class ComputableExpressionFinder : ExpressionVisitor
-	{
-
-
-		public ComputableExpressionFinder(Func<Expression, bool> isExpressionComputable)
-		{
-			this.isExpressionComputable = isExpressionComputable;
-		}
-
-		
-		internal HashSet<Expression> Find(Expression expression)
-		{
-			this.isComputableStack = new Stack<bool> ();
-			this.isComputableStack.Push (true);
-
-			this.computableExpressions = new HashSet<Expression> ();
-			
-			this.Visit (expression);
-			
-			return this.computableExpressions;
-		}
-
-
-		public override Expression Visit(Expression expression)
-		{
-			if (expression == null)
-			{
-				return expression;
-			}
-
-			// As the ExpressionVisitor class forces us to return an Expression from the Visit
-			// method, we can't return a bool which would have been very useful. That's why we use
-			// a stack of bools to store the values that should have been returned if we could have
-			// done so.
-
-			// We start by assuming that the current expression can be computed if we are said so by
-			// the callback that we are given.
-			this.isComputableStack.Push (this.isExpressionComputable (expression));
-
-			// We walk the tree with the children of the current expression to see if they can
-			// be computed.
-			base.Visit (expression);
-
-			if (!this.isComputableStack.Pop ())
-			{
-				// Either the current expression or at least one of its children cannot be computed,
-				// which implies that the parent expression cannot be computed.
-
-				this.isComputableStack.Pop ();
-				this.isComputableStack.Push (false);
-			}
-			else
-			{
-				// The current expression as well as all its children can be computed, so we add it
-				// to the list of expressions that can be computed.
-
-				this.computableExpressions.Add (expression);
-			}
-
-			return expression;
-		}
-
-
-		private readonly Func<Expression, bool> isExpressionComputable;
-
-
-		private HashSet<Expression> computableExpressions;
-
-
-		private Stack<bool> isComputableStack;
-
-
-	}
-
 
 }
