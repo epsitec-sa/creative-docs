07858ccd86cfa4832300f3cc57b30a2c045f616c
Pierre Arnaud <arnaud@opac.ch>
2012-12-18 09:40:31 +0000

    - Aider: extrait le code de test des mécanismes PostMatch dans un fichier séparé.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19603 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/AiderProgram.cs
+++ Epsitec.Cresus/Aider/AiderProgram.cs
@@ -24,7 +24,7 @@ namespace Epsitec.Aider
 		public static void Main(string[] args)
 		{
 #if false
-			AiderProgram.TestMatchStreet ();		
+			Tests.TestPostMatch.TestMatchStreet ();		
 			return;
 #endif
 
@@ -47,60 +47,6 @@ namespace Epsitec.Aider
 #endif
 
 			CoreProgram.Main (args);
-		}
-
-		private static void TestMatchStreet()
-		{
-			var streets = SwissPostStreetRepository.Current;
-			var zips    = SwissPostZipRepository.Current;
-
-			var repo = ParishAddressRepository.Current;
-			var name = repo.FindParishName (1400, "Yverdon-les-Bains", SwissPostStreet.NormalizeStreetName ("Fontenay, ch. du"), 6);
-
-			var inputFile = new System.IO.FileInfo (@"S:\Epsitec.Cresus\App.Aider\Samples\eerv-2011-11-29.xml");
-			var persons = EChDataLoader.Load (inputFile);
-
-			var unresolved = new List<string> ();
-			var unresolvedCompact = new HashSet<string> ();
-
-			foreach (var person in persons)
-			{
-				var zip = person.Address.SwissZipCode;
-				var all = SwissPostZipRepository.Current.FindZips (zip, person.Address.Town);
-				var odd = all.Where (x => x.Canton != "VD").ToArray ();
-				var streetName  = person.Address.Street;
-				int houseNumber = SwissPostStreet.NormalizeHouseNumber (person.Address.HouseNumber);
-
-				if ((all.Any () == false) ||
-					(odd.Length > 0))
-				{
-					System.Diagnostics.Debug.WriteLine ("Error: not in VD, {0} {1}, {2} {3}, {4}", person.Adult1.FirstNames, person.Adult1.OfficialName, person.Address.SwissZipCode, person.Address.Town, person.Address.Street);
-				}
-
-				string normalizedStreetName = SwissPostStreet.NormalizeStreetName (streetName);
-
-				if (normalizedStreetName.Length == 0)
-				{
-					System.Diagnostics.Debug.WriteLine (string.Format ("Error: no street for {0} {1}, {2} {3}", person.Adult1.FirstNames, person.Adult1.OfficialName, person.Address.SwissZipCode, person.Address.Town));
-				}
-				if (houseNumber == 0)
-				{
-					System.Diagnostics.Debug.WriteLine (string.Format ("Error: no street number for {0} {1}, {2} {3}, {4}", person.Adult1.FirstNames, person.Adult1.OfficialName, person.Address.SwissZipCode, person.Address.Town, person.Address.Street));
-				}
-
-				name = repo.FindParishName (person.Address.SwissZipCode, person.Address.Town, normalizedStreetName, houseNumber);
-
-				if (name == null)
-				{
-					unresolved.Add (string.Format ("{0}\t{1}\t{2}\t{3}\t{4}", person.Address.SwissZipCode, person.Address.Town, person.Address.Street, person.Address.HouseNumber, person.Adult1.OfficialName.ToUpper () + " " + person.Adult1.FirstNames));
-					unresolvedCompact.Add (string.Format ("{0}\t{1}\t{2}", person.Address.SwissZipCode, person.Address.Town, person.Address.Street));
-				}
-			}
-
-			int count = unresolved.Count;
-
-			System.IO.File.WriteAllLines ("unresolved addresses.txt", unresolved.OrderBy (x => x), System.Text.Encoding.Default);
-			System.IO.File.WriteAllLines ("unresolved addresses (compact).txt", unresolvedCompact.OrderBy (x => x), System.Text.Encoding.Default);
 		}
 
 		private static void TestEChImporter()
---
+++ Epsitec.Cresus/Aider/Tests/TestPostMatch.cs
@@ -0,0 +1,100 @@
+﻿//	Copyright © 2012, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Aider.Data;
+using Epsitec.Aider.Data.ECh;
+
+using Epsitec.Data.Platform;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Aider.Tests
+{
+	internal static class TestPostMatch
+	{
+		public static void TestMatchStreet()
+		{
+			var streets = SwissPostStreetRepository.Current;
+			var zips    = SwissPostZipRepository.Current;
+
+			var repo = ParishAddressRepository.Current;
+			var name = repo.FindParishName (1400, "Yverdon-les-Bains", SwissPostStreet.NormalizeStreetName ("Fontenay, ch. du"), 6);
+
+			var inputFile = new System.IO.FileInfo (@"S:\Epsitec.Cresus\App.Aider\Samples\eerv-2012-12-07.xml");
+			var persons = EChDataLoader.Load (inputFile);
+
+			var unresolved = new List<string> ();
+			var unresolvedCompact = new HashSet<string> ();
+			var fixZipCode = new List<string> ();
+			int personCount = 0;
+
+			foreach (var person in persons)
+			{
+				personCount++;
+
+				var zip = person.Address.SwissZipCode;
+				var all = SwissPostZipRepository.Current.FindZips (zip, person.Address.Town);
+				var odd = all.Where (x => x.Canton != "VD").ToArray ();
+				var streetName  = person.Address.Street;
+				int houseNumber = SwissPostStreet.NormalizeHouseNumber (person.Address.HouseNumber);
+				
+				if ((all.Any () == false) ||
+					(odd.Length > 0))
+				{
+					System.Diagnostics.Debug.WriteLine ("Error: not in VD, {0} {1}, {2} {3}, {4}", person.Adult1.FirstNames, person.Adult1.OfficialName, person.Address.SwissZipCode, person.Address.Town, person.Address.Street);
+				}
+
+				string normalizedStreetName = SwissPostStreet.NormalizeStreetName (streetName);
+
+				if (normalizedStreetName.Length == 0)
+				{
+					System.Diagnostics.Debug.WriteLine (string.Format ("Error: no street for {0} {1}, {2} {3}", person.Adult1.FirstNames, person.Adult1.OfficialName, person.Address.SwissZipCode, person.Address.Town));
+				}
+				if (houseNumber == 0)
+				{
+					System.Diagnostics.Debug.WriteLine (string.Format ("Error: no street number for {0} {1}, {2} {3}, {4}", person.Adult1.FirstNames, person.Adult1.OfficialName, person.Address.SwissZipCode, person.Address.Town, person.Address.Street));
+				}
+
+				var zipCode = person.Address.SwissZipCode;
+
+				name = repo.FindParishName (zipCode, person.Address.Town, normalizedStreetName, houseNumber);
+				
+				if (name == null)
+				{
+					if ((zipCode >= 1000) &&
+						(zipCode <= 1012))
+					{
+						for (int z = 1000; z <= 1012; z++)
+						{
+							name = repo.FindParishName (z, person.Address.Town, normalizedStreetName, houseNumber);
+							
+							if (name != null)
+							{
+								fixZipCode.Add (string.Format ("{0}\t{1}\t{2}", zipCode, person.Address.Street, z));
+								break;
+							}
+						}
+					}
+				}
+
+				if (name == null)
+				{
+					unresolved.Add (string.Format ("{0}\t{1}\t{2}\t{3}\t{4}", person.Address.SwissZipCode, person.Address.Town, person.Address.Street, person.Address.HouseNumber, person.Adult1.OfficialName.ToUpper () + " " + person.Adult1.FirstNames));
+					unresolvedCompact.Add (string.Format ("{0}\t{1}\t{2}", person.Address.SwissZipCode, person.Address.Town, person.Address.Street));
+				}
+
+				if (personCount % 1000 == 0)
+				{
+					System.Diagnostics.Debug.WriteLine ("{0} personnes analysées", personCount);
+				}
+			}
+			
+			int count = unresolved.Count;
+			
+			System.IO.File.WriteAllLines ("unresolved addresses.txt", unresolved.OrderBy (x => x), System.Text.Encoding.Default);
+			System.IO.File.WriteAllLines ("unresolved addresses (compact).txt", unresolvedCompact.OrderBy (x => x).Distinct (), System.Text.Encoding.Default);
+			System.IO.File.WriteAllLines ("incorrect zip codes.txt", fixZipCode.OrderBy (x => x).Distinct (), System.Text.Encoding.Default);
+		}
+	}
+}
