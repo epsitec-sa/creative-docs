40042046262be3041bbace7b7ad05b8786c95059
Marc Bettex <bettex@epsitec.ch>
2012-12-28 16:44:14 +0000

    - Séparé EntityReferenceField en deux classes pour pouvoir réutiliser une partie de la fonctionnalité ailleurs.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19624 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Client/WebCore/js/field/EntityReferenceField.js
+++ Epsitec.Cresus/Cresus.WebCore.Client/WebCore/js/field/EntityReferenceField.js
@@ -1,89 +1,21 @@
 Ext.require([
-  'Epsitec.cresus.webcore.entityList.EntityPicker',
+  'Epsitec.cresus.webcore.entityList.EntityListPicker',
+  'Epsitec.cresus.webcore.field.ReferenceField',
   'Epsitec.cresus.webcore.tools.Callback'
 ],
 function() {
   Ext.define('Epsitec.cresus.webcore.field.EntityReferenceField', {
-    extend: 'Ext.form.TriggerField',
+    extend: 'Epsitec.cresus.webcore.field.ReferenceField',
     alternateClassName: ['Epsitec.EntityReferenceField'],
     alias: 'widget.epsitec.entityreferencefield',
-
-    /* Config */
-
-    trigger1Cls: 'x-form-clear-trigger',
-    trigger2Cls: 'x-form-arrow-trigger',
-    editable: false,
 
     /* Properties */
 
     databaseName: null,
 
-    // This property is supposed to be an object with the following properties:
-    // {
-    //   id: ...,
-    //   summary: ...,
-    // }
-    currentValue: null,
-
-    /* Constructor */
-
-    constructor: function(options) {
-      var newOptions = {
-        onTrigger1Click: this.onClearClick,
-        onTrigger2Click: this.onPickClick
-      };
-      Ext.applyIf(newOptions, options);
-
-      this.callParent([newOptions]);
-      return this;
-    },
-
-    onClearClick: function() {
-      this.setValue(null);
-    },
-
     onPickClick: function() {
-      var callback = Epsitec.Callback.create(this.entityPickerCallback, this);
+      var callback = Epsitec.Callback.create(this.onPickClickCallback, this);
       Epsitec.EntityPicker.showDatabase(this.databaseName, false, callback);
-    },
-
-    entityPickerCallback: function(selectedItems) {
-      this.setValue(selectedItems.length === 1 ? selectedItems[0] : null);
-    },
-
-    setValue: function(value) {
-      // We need to store the whole current value here as we require it in the
-      // rawToValue method because we cannot get the value back from the raw
-      // value only. The rawToValue method is called internally by the setValue
-      // to actualy set the value and it is its return value that is assigned as
-      // the value of the field.
-      this.currentValue = value;
-      this.callParent(arguments);
-    },
-
-    valueToRaw: function(value) {
-      if (value === null || value.summary === null) {
-        return '';
-      }
-      return value.summary;
-    },
-
-    rawToValue: function(object) {
-      var value = this.currentValue;
-      if (value === null || value.summary !== object) {
-        return null;
-      }
-      return value;
-    },
-
-    getSubmitValue: function() {
-      // We need to override the getSubmitValue function as we don't want to
-      // send the whole value to the server, but only part of it.
-      var value = this.getValue();
-      if (value === null || value.id === null) {
-        return '';
-      }
-      return value.id;
     }
   });
 });
