a8b3bc5ad0d2f8e5508c91f1b97906b98f802824
Marc Bettex <bettex@epsitec.ch>
2012-12-31 08:57:28 +0000

    - On utilise le controlleur adéquat pour la sélection des groupes.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19629 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/Controllers/EditionControllers/EditionAiderGroupParticipantViewController.cs
+++ Epsitec.Cresus/Aider/Controllers/EditionControllers/EditionAiderGroupParticipantViewController.cs
@@ -1,6 +1,7 @@
 //	Copyright © 2012, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
+using Epsitec.Aider.Controllers.SpecialFieldControllers;
 using Epsitec.Aider.Entities;
 
 using Epsitec.Cresus.Bricks;
@@ -20,6 +21,7 @@ namespace Epsitec.Aider.Controllers.EditionControllers
 						.Field (x => x.EndDate)
 					.End ()
 					.Field (x => x.Group)
+						.WithSpecialField<AiderGroupSpecialField<AiderGroupParticipantEntity>> ()
 					.Field (x => x.Person)
 				.End ();
 		}
