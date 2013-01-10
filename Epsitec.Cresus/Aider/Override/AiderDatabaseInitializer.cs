edd884898aa23412268d34dd517971f53dfeb1b2
Marc Bettex <bettex@epsitec.ch>
2013-01-10 14:39:47 +0000

    - Ajouté l'importation initiale des scopes et des roles pour aider.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19664 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/Override/AiderDatabaseInitializer.cs
+++ Epsitec.Cresus/Aider/Override/AiderDatabaseInitializer.cs
@@ -1,4 +1,10 @@
-﻿using Epsitec.Cresus.Core.Business;
+﻿using Epsitec.Common.Support.Extensions;
+
+using Epsitec.Aider.Entities;
+
+using Epsitec.Cresus.Core.Business;
+using Epsitec.Cresus.Core.Business.UserManagement;
+using Epsitec.Cresus.Core.Entities;
 using Epsitec.Cresus.Core.Library;
 
 
@@ -19,7 +25,120 @@ namespace Epsitec.Aider.Override
 		public override void Run()
 		{
 			base.Run ();
+
+			this.CreateAiderScopes ();
+			this.CreateAiderRoles ();
+			this.CreateAdministrator ();
 		}
+
+
+		private void CreateAiderScopes()
+		{
+			this.scopeCounty = this.CreateAiderScope
+			(
+				name:"Canton", 
+				path:""
+			);
+
+			this.scopeRegion = this.CreateAiderScope 
+			(
+				name:"Région",
+				path:"<R>."
+			);
+
+			this.scopeParish = this.CreateAiderScope
+			(
+				name:"Paroisse", 
+				path:"<R>.<P>."
+			);
+		}
+
+
+		private AiderUserScopeEntity CreateAiderScope(string name, string path)
+		{
+			var scope = this.BusinessContext.CreateAndRegisterEntity<AiderUserScopeEntity> ();
+
+			scope.Name = name;
+			scope.GroupPath = path;
+
+			return scope;
+		}
+
+
+		private void CreateAiderRoles()
+		{
+			this.roleAdministrator = this.CreateAiderRole
+			(
+				name: "Administrateur",
+				scopes: new AiderUserScopeEntity[] { 
+					this.scopeCounty
+				}
+			);
+
+			this.roleCounty = this.CreateAiderRole
+			(
+				name: "Accès cantonnal",
+				scopes: new AiderUserScopeEntity[] { 
+					this.scopeCounty,
+					this.scopeRegion,
+					this.scopeParish
+				}
+			);
+
+			this.roleRegion = this.CreateAiderRole
+			(
+				name: "Accès régional",
+				scopes: new AiderUserScopeEntity[] { 
+					this.scopeRegion,
+					this.scopeParish
+				}
+			);
+			this.roleParish = this.CreateAiderRole
+			(
+				name: "Accès paroissial",
+				scopes: new AiderUserScopeEntity[] { 
+					this.scopeParish
+				}
+			);
+		}
+
+
+		private AiderUserRoleEntity CreateAiderRole(string name, params AiderUserScopeEntity[] scopes)
+		{
+			var role = this.BusinessContext.CreateAndRegisterEntity<AiderUserRoleEntity> ();
+
+			role.Name = name;
+			role.DefaultScopes.AddRange (scopes);
+
+			return role;
+		}
+
+
+		private void CreateAdministrator()
+		{
+			var admininstrator = (AiderUserEntity) this.CreateUser
+			(
+				displayName: "Administrateur",
+				userLogin: "administrateur",
+				userPassword: "mysuperadministrateurpassword",
+				authentificationMethod: UserAuthenticationMethod.Password,
+				groups: new SoftwareUserGroupEntity[] {
+					this.GroupAdministrator,
+					this.GroupStandard
+				}
+			);
+
+			admininstrator.Role = this.roleAdministrator;
+		}
+
+
+		private AiderUserScopeEntity scopeCounty;
+		private AiderUserScopeEntity scopeRegion;
+		private AiderUserScopeEntity scopeParish;
+		private AiderUserRoleEntity roleAdministrator;
+		private AiderUserRoleEntity roleCounty;
+		private AiderUserRoleEntity roleRegion;
+		private AiderUserRoleEntity roleParish;
 
 
 	}
