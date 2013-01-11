816c858da22388507a66f127203f4f4db559f0b6
Daniel Roux <roux@epsitec.ch>
2013-01-11 09:21:38 +0000

    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19678 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Pdf/Stikers/Stikers.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Epsitec.Common.Types;
+using Epsitec.Common.Pdf.Engine;
+using Epsitec.Common.Drawing;
+
+namespace Epsitec.Common.Pdf.Stikers
+{
+	public class Stikers
+	{
+		public Stikers()
+		{
+		}
+
+		public string GeneratePdf(string path, int count, Func<int, FormattedText> accessor, StikersSetup setup)
+		{
+			this.count = count;
+			this.accessor = accessor;
+			this.setup = setup;
+			this.info = new ExportPdfInfo ();
+
+			this.stikersPerPage = this.StikersPerPage;
+			this.font = Font.GetFont (this.setup.FontFace, this.setup.FontStyle);
+
+			if (this.stikersPerPage < 1)
+			{
+				return "Etiquettes trop grandes par rapport au papier";
+			}
+
+			int pageCount = (this.count + this.stikersPerPage - 1) / this.stikersPerPage;
+
+			var export = new Export (this.info);
+			return export.ExportToFile (path, pageCount, this.Renderer);
+		}
+
+		private void Renderer(Port port, int page)
+		{
+			for (int y = 0; y < this.VerticalStikersCount; y++)
+			{
+				for (int x = 0; x < this.HorizontalStikersCount; x++)
+				{
+					int rankIntoPage = y * this.HorizontalStikersCount + x;
+					int rank = (page-1) * this.stikersPerPage + rankIntoPage;
+
+					if (rank < this.count)
+					{
+						var text = this.accessor (rank);
+
+						var bounds = this.GetStikerBounds (rankIntoPage);
+
+						if (this.setup.PaintFrame)
+						{
+							var path = new Path ();
+							path.AppendRectangle (bounds);
+
+							port.LineWidth = 1.0;  // épaisseur de 0.1mm
+							port.Color = Color.FromBrightness (0.8);  // gris clair
+							port.PaintOutline (path);
+						}
+
+						bounds.Deflate (this.setup.StikerMargins);
+						port.PaintText (bounds.Left, bounds.Bottom, bounds.Size, text, this.font, this.setup.FontSize);
+					}
+				}
+			}
+		}
+
+
+		private Rectangle GetStikerBounds(int rankIntoPage)
+		{
+			int x = rankIntoPage % this.HorizontalStikersCount;
+			int y = rankIntoPage / this.HorizontalStikersCount;
+
+			double ox = this.setup.PageMargins.Left + (this.setup.StikerSize.Width  + this.setup.StikerGap.Width ) * x;
+			double oy = this.setup.PageMargins.Top  + (this.setup.StikerSize.Height + this.setup.StikerGap.Height) * y;
+
+			return new Rectangle (ox, this.info.PageSize.Height - this.setup.StikerSize.Height - oy, this.setup.StikerSize.Width, this.setup.StikerSize.Height);
+		}
+
+		private int StikersPerPage
+		{
+			get
+			{
+				return this.HorizontalStikersCount * this.VerticalStikersCount;
+			}
+		}
+
+		private int HorizontalStikersCount
+		{
+			get
+			{
+				return (int) ((this.info.PageSize.Width - this.setup.PageMargins.Width + this.setup.StikerGap.Width) / (this.setup.StikerSize.Width + this.setup.StikerGap.Width));
+			}
+		}
+
+		private int VerticalStikersCount
+		{
+			get
+			{
+				return (int) ((this.info.PageSize.Height - this.setup.PageMargins.Height + this.setup.StikerGap.Height) / (this.setup.StikerSize.Height + this.setup.StikerGap.Height));
+			}
+		}
+
+
+		private Func<int, FormattedText> accessor;
+		private StikersSetup setup;
+		private ExportPdfInfo info;
+		private int stikersPerPage;
+		private Font font;
+		private int count;
+	}
+}
---
+++ Epsitec.Cresus/Common.Pdf/Stikers/StikersSetup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Epsitec.Common.Drawing;
+
+namespace Epsitec.Common.Pdf.Stikers
+{
+	public class StikersSetup
+	{
+		//	+------------------------------------------
+		//	|     +
+
+		public StikersSetup()
+		{
+			this.PageMargins   = new Margins (100.0);
+			this.StikerSize    = new Size (620.0, 400.0);
+			this.StikerMargins = new Margins (50.0);
+			this.StikerGap     = new Size (10.0, 10.0);
+			this.FontFace      = "Arial";
+			this.FontStyle     = "Regular";
+			this.FontSize      = 30.0;
+			this.PaintFrame    = false;
+		}
+
+		public Margins PageMargins
+		{
+			set;
+			get;
+		}
+
+		public Size StikerSize
+		{
+			set;
+			get;
+		}
+
+		public Margins StikerMargins
+		{
+			set;
+			get;
+		}
+
+		public Size StikerGap
+		{
+			set;
+			get;
+		}
+
+		public string FontFace
+		{
+			set;
+			get;
+		}
+
+		public string FontStyle
+		{
+			set;
+			get;
+		}
+
+		public double FontSize
+		{
+			set;
+			get;
+		}
+
+		public bool PaintFrame
+		{
+			set;
+			get;
+		}
+	}
+}
