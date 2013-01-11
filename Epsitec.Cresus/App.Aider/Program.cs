e9db76c38909b918ea0f969fc959b73998a0ed8e
Marc Bettex <bettex@epsitec.ch>
2013-01-11 08:39:31 +0000

    - Ménage dans les usings.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19676 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/App.Aider/Program.cs
+++ Epsitec.Cresus/App.Aider/Program.cs
@@ -1,12 +1,6 @@
 ﻿//	Copyright © 2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
-using Epsitec.Common.Debug;
-using Epsitec.Cresus.Core.Library;
-
-using System.Collections.Generic;
-using System.Linq;
-
 namespace Epsitec.Aider
 {
 	static class Program
