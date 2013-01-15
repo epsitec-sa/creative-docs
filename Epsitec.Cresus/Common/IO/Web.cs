cd1084c866de818b63df1c24b7f1994b325ee31e
Pierre Arnaud <arnaud@opac.ch>
2013-01-15 09:09:56 +0000

    - Common.IO: ajouté Web.DownloadLines.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@19737 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common/IO/Web.cs
@@ -0,0 +1,25 @@
+﻿//	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace Epsitec.Common.IO
+{
+	public static class Web
+	{
+		public static IEnumerable<string> DownloadLines(string uri, System.Text.Encoding encoding)
+		{
+			string value;
+
+			using (WebClient client = new WebClient ())
+			{
+				byte[] data = client.DownloadData (uri);
+				value = encoding.GetString (data);
+			}
+
+			return value.Split ('\n').Select (x => x.TrimEnd (' ', '\r'));
+		}
+	}
+}
