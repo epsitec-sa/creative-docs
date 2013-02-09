06ac272ccb644a1476a3d837953318ccce20ed06
Marc Bettex <bettex@epsitec.ch>
2013-02-09 14:02:55 +0000

    - Ménage dans les usings.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20039 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/Rules/AiderContactBusinessRules.cs
+++ Epsitec.Cresus/Aider/Rules/AiderContactBusinessRules.cs
@@ -1,12 +1,7 @@
 //	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
-using Epsitec.Aider.Data;
 using Epsitec.Aider.Entities;
-using Epsitec.Aider.Enumerations;
-using Epsitec.Aider.Tools;
-using Epsitec.Common.Support;
-using Epsitec.Common.Types;
 
 using Epsitec.Cresus.Core.Entities;
 using Epsitec.Cresus.Core.Business;
