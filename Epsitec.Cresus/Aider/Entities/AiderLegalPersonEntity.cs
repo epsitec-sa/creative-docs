76100de0db931b57f1847cb1c0ce43a1783e5b46
Marc Bettex <bettex@epsitec.ch>
2013-02-09 14:03:18 +0000

    - Ménage dans les usings.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20040 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/Entities/AiderLegalPersonEntity.cs
+++ Epsitec.Cresus/Aider/Entities/AiderLegalPersonEntity.cs
@@ -1,18 +1,12 @@
 //	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
-using Epsitec.Aider.Controllers;
 using Epsitec.Aider.Enumerations;
-using Epsitec.Aider.Entities.Helpers;
 
-using Epsitec.Common.Support;
 using Epsitec.Common.Support.EntityEngine;
-using Epsitec.Common.Support.Extensions;
 using Epsitec.Common.Types;
 
-using Epsitec.Cresus.Core.Business;
 using Epsitec.Cresus.Core.Entities;
-using Epsitec.Cresus.DataLayer.Context;
 
 using System.Collections.Generic;
 using System.Linq;
