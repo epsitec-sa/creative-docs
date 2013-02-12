6e91192614afb3281eade8e7ca490afe51331c56
Marc Bettex <bettex@epsitec.ch>
2013-02-12 13:17:44 +0000

    - Ajouté une gestion spécifique pour les adresses de LegalPerson depuis les contacts.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20061 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/Controllers/EditionControllers/EditionAiderContactViewController1.cs
+++ Epsitec.Cresus/Aider/Controllers/EditionControllers/EditionAiderContactViewController1.cs
@@ -4,12 +4,12 @@
 using Epsitec.Common.Support;
 
 using Epsitec.Aider.Entities;
+using Epsitec.Aider.Enumerations;
 
 using Epsitec.Cresus.Bricks;
 
 using Epsitec.Cresus.Core.Controllers;
 using Epsitec.Cresus.Core.Controllers.EditionControllers;
-using Epsitec.Cresus.Core.Entities;
 
 namespace Epsitec.Aider.Controllers.EditionControllers
 {
@@ -18,52 +18,52 @@ namespace Epsitec.Aider.Controllers.EditionControllers
 	{
 		protected override void CreateBricks(BrickWall<AiderContactEntity> wall)
 		{
-			var contact = this.Entity;
+			var bricks = wall.AddBrick ();
 
-			if (contact.Household.IsNull ())
+			bricks = this.GetHeader (bricks);
+
+			bricks = bricks
+				.Input ()
+				.Field (x => x.Address.Town)
+				.Field (x => x.Address.AddressLine1)
+				.Field (x => x.Address.StreetUserFriendly)
+				.Field (x => x.Address.HouseNumberAndComplement)
+				.Field (x => x.Address.PostBox)
+			.End ()
+			.Input ()
+				.Field (x => x.Address.Phone1)
+				.Field (x => x.Address.Phone2)
+				.Field (x => x.Address.Mobile)
+				.Field (x => x.Address.Fax)
+				.Field (x => x.Address.Email)
+				.Field (x => x.Address.Web)
+			.End ();
+		}
+
+		private SimpleBrick<AiderContactEntity> GetHeader(SimpleBrick<AiderContactEntity> bricks)
+		{
+			switch (this.Entity.ContactType)
 			{
-				wall.AddBrick ()
-					.Title (Resources.Text ("Adresse alternative"))
-					.Icon ("Data.AiderAddress")
-					.Input ()
-						.Field (x => x.AddressType)
-					.End ()
-					.Input ()
-						.Field (x => x.Address.Town)
-						.Field (x => x.Address.AddressLine1)
-						.Field (x => x.Address.StreetUserFriendly)
-						.Field (x => x.Address.HouseNumberAndComplement)
-						.Field (x => x.Address.PostBox)
-					.End ()
-					.Input ()
-						.Field (x => x.Address.Phone1)
-						.Field (x => x.Address.Phone2)
-						.Field (x => x.Address.Mobile)
-						.Field (x => x.Address.Fax)
-						.Field (x => x.Address.Email)
-						.Field (x => x.Address.Web)
-					.End ();
-			}
-			else
-			{
-				wall.AddBrick ()
-					.Title (Resources.Text ("Adresse du ménage"))
-					.Icon ("Data.AiderAddress")
-					.Input ()
-						.Field (x => x.Address.Town)
-						.Field (x => x.Address.AddressLine1)
-						.Field (x => x.Address.StreetUserFriendly)
-						.Field (x => x.Address.HouseNumberAndComplement)
-						.Field (x => x.Address.PostBox)
-					.End ()
-					.Input ()
-						.Field (x => x.Address.Phone1)
-						.Field (x => x.Address.Phone2)
-						.Field (x => x.Address.Mobile)
-						.Field (x => x.Address.Fax)
-						.Field (x => x.Address.Email)
-						.Field (x => x.Address.Web)
-					.End ();
+				case ContactType.Legal:
+					return bricks
+						.Title (Resources.Text ("Adresse de contact")) // TODO
+						.Icon ("Data.AiderAddress");
+
+				case ContactType.PersonAddress:
+					return bricks
+						.Title (Resources.Text ("Adresse alternative"))
+						.Icon ("Data.AiderAddress")
+						.Input ()
+							.Field (x => x.AddressType)
+						.End ();
+
+				case ContactType.PersonHousehold:
+					return bricks
+						.Title (Resources.Text ("Adresse du ménage"))
+						.Icon ("Data.AiderAddress");
+
+				default:
+					return bricks;
 			}
 		}
 	}
