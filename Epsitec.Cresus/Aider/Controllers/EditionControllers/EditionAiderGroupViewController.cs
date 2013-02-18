774aa0791b25b8a03b8af1b77947ca1171309e31
Marc Bettex <bettex@epsitec.ch>
2013-02-18 10:58:15 +0000

    - Améliorations dans le controlleur des groupes pour ne montrer que les informations nécaissaires et laisser le nom éditable que si le groupe n'a pas été crée à partir d'une définition.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20089 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/Controllers/EditionControllers/EditionAiderGroupViewController.cs
+++ Epsitec.Cresus/Aider/Controllers/EditionControllers/EditionAiderGroupViewController.cs
@@ -5,6 +5,7 @@ using Epsitec.Aider.Entities;
 
 using Epsitec.Cresus.Bricks;
 
+using Epsitec.Cresus.Core.Entities;
 using Epsitec.Cresus.Core.Controllers.EditionControllers;
 
 namespace Epsitec.Aider.Controllers.EditionControllers
@@ -13,33 +14,42 @@ namespace Epsitec.Aider.Controllers.EditionControllers
 	{
 		protected override void CreateBricks(BrickWall<AiderGroupEntity> wall)
 		{
+			if (this.Entity.GroupDef.IsNotNull ())
+			{
+				this.AddBrickWithDefinition (wall);
+			}
+			else
+			{
+				this.AddBrickWithoutDefinition (wall);
+			}
+		}
+
+		private void AddBrickWithDefinition(BrickWall<AiderGroupEntity> wall)
+		{
+			wall.AddBrick ()
+				.Input ()
+					.Field (x => x.GroupDef)
+						.ReadOnly ()
+					.Field (x => x.Name)
+						.ReadOnly ()
+				.End ();
+		}
+
+		private void AddBrickWithoutDefinition(BrickWall<AiderGroupEntity> wall)
+		{
 #if ENABLE_GROUPS
 			wall.AddBrick ()
 				.Input ()
-					.HorizontalGroup ()
-						.Title ("Dates de début et de fin")
-						.Field (x => x.StartDate)
-						.Field (x => x.EndDate)
-					.End ()
-					.Field (x => x.Path).ReadOnly ()
-					.Field (x => x.GroupLevel).ReadOnly ()
-					.Field (x => x.GroupDef)
 					.Field (x => x.Name)
 				.End ();
 #else
 			wall.AddBrick ()
 				.Input ()
-					.HorizontalGroup ()
-						.Title ("Dates de début et de fin")
-						.Field (x => x.StartDate).ReadOnly ()
-						.Field (x => x.EndDate).ReadOnly ()
-					.End ()
-					.Field (x => x.Path).ReadOnly ()
-					.Field (x => x.GroupLevel).ReadOnly ()
-					.Field (x => x.GroupDef).ReadOnly ()
-					.Field (x => x.Name).ReadOnly ()
+					.Field (x => x.Name)
+						.ReadOnly ()
 				.End ();
 #endif
 		}
+
 	}
 }
