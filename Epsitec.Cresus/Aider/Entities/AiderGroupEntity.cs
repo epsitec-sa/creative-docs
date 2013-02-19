79a74e0eece99e91501b651b3f4a58f5e5c764d7
Marc Bettex <bettex@epsitec.ch>
2013-02-19 09:31:42 +0000

    - Cosmétique et déplacement de méthodes au sein de AiderGroupEntity.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20103 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/Entities/AiderGroupEntity.cs
+++ Epsitec.Cresus/Aider/Entities/AiderGroupEntity.cs
@@ -5,7 +5,6 @@ using Epsitec.Aider.Data;
 using Epsitec.Aider.Data.Eerv;
 using Epsitec.Aider.Entities.Helpers;
 
-using Epsitec.Common.Support;
 using Epsitec.Common.Support.Extensions;
 
 using Epsitec.Common.Types;
@@ -22,34 +21,164 @@ using System.Collections.Generic;
 
 using System.Linq;
 
+
 namespace Epsitec.Aider.Entities
 {
+
+
 	public partial class AiderGroupEntity
 	{
+
+
 		public override FormattedText GetSummary()
 		{
 			return TextFormatter.FormatText (this.Name);
 		}
+
 
 		public override FormattedText GetCompactSummary()
 		{
 			return TextFormatter.FormatText (this.Name);
 		}
 
+
 		public override IEnumerable<FormattedText> GetFormattedEntityKeywords()
 		{
 			yield return this.Name;
 		}
 
-		public static AiderGroupEntity Create(BusinessContext businessContext, AiderGroupDefEntity groupDefinition,
-			/**/							  GroupPathInfo info)
+
+		public FormattedText GetParticipantsTitle()
+		{
+			var nbParticipants = this.GetNbParticipants ();
+
+			return TextFormatter.FormatText ("Particpants (", nbParticipants, ")");
+		}
+
+
+		public FormattedText GetParticipantsSummary()
+		{
+			int count = 10;
+
+			var groups = this.GetParticipants (count + 1)
+				.Select (g => g.GetCompactSummary ())
+				.CreateSummarySequence (count, "...");
+
+			return FormattedText.Join (FormattedText.FromSimpleText ("\n"), groups);
+		}
+
+
+		private int GetNbParticipants()
+		{
+			var dataContext = DataContextPool.GetDataContext (this);
+
+			if ((dataContext != null) &&
+				(dataContext.IsPersistent (this)))
+			{
+				var request = AiderGroupParticipantEntity.CreateParticipantRequest (dataContext, this, false, true, true);
+
+				return dataContext.GetCount (request);
+			}
+
+			return 0;
+		}
+
+
+		private IEnumerable<AiderPersonEntity> GetParticipants(int count)
+		{
+			var dataContext = DataContextPool.GetDataContext (this);
+
+			if ((dataContext != null) &&
+				(dataContext.IsPersistent (this)))
+			{
+				var request = AiderGroupParticipantEntity.CreateParticipantRequest (dataContext, this, true, true, true);
+
+				request.Skip = 0;
+				request.Take = count;
+
+				return dataContext.GetByRequest<AiderPersonEntity> (request);
+			}
+
+			return Enumerable.Empty<AiderPersonEntity> ();
+		}
+
+
+		partial void GetSubgroups(ref IList<AiderGroupEntity> value)
+		{
+			if (this.subgroupsList == null)
+			{
+				this.subgroupsList = new AiderGroupSubGroupList (this);
+				;
+			}
+
+			value = this.subgroupsList;
+		}
+
+
+		public IEnumerable<AiderGroupEntity> FindSubgroups(BusinessContext businessContext)
+		{
+			// If we are at the maximum group level, there's no point in looking for sub groups in
+			// the database. We won't find any. So we return directly an empty sequence. Note that
+			// this optimization avoids to make a request with a group path longer than the maximum
+			// group path which Firebird don't like at all. It considers such requests invalid.
+
+			if (this.GroupLevel >= AiderGroupEntity.maxGroupLevel)
+			{
+				return Enumerable.Empty<AiderGroupEntity> ();
+			}
+
+			var dataContext = businessContext.DataContext;
+
+			var example = new AiderGroupEntity ();
+			var request = Request.Create (example);
+
+			var path  = this.Path + AiderGroupIds.SubgroupSqlWildcard;
+			var level = this.GroupLevel + 1;
+
+			request.AddCondition (dataContext, example, x => x.GroupLevel == level && SqlMethods.Like (x.Path, path));
+			request.AddSortClause (ValueField.Create (example, x => x.Name));
+
+			return dataContext.GetByRequest (request);
+		}
+
+
+		public static IList<AiderGroupEntity> FindRootGroups(BusinessContext businessContext)
+		{
+			var example = new AiderGroupEntity ()
+			{
+				GroupLevel = 0,
+			};
+
+			var request = Request.Create (example);
+			request.AddSortClause (ValueField.Create (example, x => x.Name));
+
+			return businessContext.DataContext.GetByRequest (request);
+		}
+
+
+		public IEnumerable<AiderGroupEntity> GetGroupChain(BusinessContext businessContext)
+		{
+			foreach (var path in AiderGroupIds.GetGroupChainPaths (this.Path))
+			{
+				var example = new AiderGroupEntity ()
+				{
+					Path = path
+				};
+
+				yield return businessContext.DataContext.GetByExample (example).Single ();
+			}
+		}
+
+
+		public static AiderGroupEntity Create(BusinessContext businessContext, AiderGroupDefEntity groupDefinition, GroupPathInfo info)
 		{
 			var name = info.Name;
 			var level = info.Level;
 			var path = info.MapPath (groupDefinition);
-			
+
 			return AiderGroupEntity.Create (businessContext, groupDefinition, name, level, path);
 		}
+
 
 		public static AiderGroupEntity Create(BusinessContext businessContext, AiderGroupDefEntity groupDefinition, string name, int level, string path)
 		{
@@ -127,130 +256,13 @@ namespace Epsitec.Aider.Entities
 		}
 
 
-		public IEnumerable<AiderGroupEntity> FindSubgroups(BusinessContext businessContext)
-		{
-			// If we are at the maximum group level, there's no point in looking for sub groups in
-			// the database. We won't find any. So we return directly an empty sequence. Note that
-			// this optimization avoids to make a request with a group path longer than the maximum
-			// group path which Firebird don't like at all. It considers such requests invalid.
-
-			if (this.GroupLevel >= AiderGroupEntity.maxGroupLevel)
-			{
-				return Enumerable.Empty<AiderGroupEntity> ();
-			}
-			
-			var dataContext = businessContext.DataContext;
-
-			var example = new AiderGroupEntity ();
-			var request = Request.Create (example);
-
-			var path  = this.Path + AiderGroupIds.SubgroupSqlWildcard;
-			var level = this.GroupLevel + 1;
-
-			request.AddCondition (dataContext, example, x => x.GroupLevel == level && SqlMethods.Like (x.Path, path));
-			request.AddSortClause (ValueField.Create (example, x => x.Name));
-
-			return dataContext.GetByRequest (request);
-		}
-
-
-		public static IList<AiderGroupEntity> FindRootGroups(BusinessContext businessContext)
-		{
-			var example = new AiderGroupEntity ()
-			{
-				GroupLevel = 0,
-			};
-
-			var request = Request.Create (example);
-			request.AddSortClause (ValueField.Create (example, x => x.Name));
-
-			return businessContext.DataContext.GetByRequest (request);
-		}
-
-
-		public IEnumerable<AiderGroupEntity> GetGroupChain(BusinessContext businessContext)
-		{
-			foreach (var path in AiderGroupIds.GetGroupChainPaths (this.Path))
-			{
-				var example = new AiderGroupEntity ()
-				{
-					Path = path
-				};
-
-				yield return businessContext.DataContext.GetByExample (example).Single ();
-			}
-		}
-
-
-		partial void GetSubgroups(ref IList<AiderGroupEntity> value)
-		{
-			if (this.subgroupsList == null)
-			{
-				this.subgroupsList = new AiderGroupSubGroupList (this);;
-			}
-
-			value = this.subgroupsList;
-		}
-
-
-		public FormattedText GetParticipantsTitle()
-		{
-			var nbParticipants = this.GetNbParticipants ();
-
-			return TextFormatter.FormatText ("Particpants (", nbParticipants, ")");
-		}
-
-
-		public FormattedText GetParticipantsSummary()
-		{
-			int count = 10;
-
-			var groups = this.GetParticipants (count + 1)
-				.Select (g => g.GetCompactSummary ())
-				.CreateSummarySequence (count, "...");
-
-			return FormattedText.Join (FormattedText.FromSimpleText ("\n"), groups);
-		}
-
-
-		private int GetNbParticipants()
-		{
-			var dataContext = DataContextPool.GetDataContext (this);
-
-			if ((dataContext != null) &&
-				(dataContext.IsPersistent (this)))
-			{
-				var request = AiderGroupParticipantEntity.CreateParticipantRequest (dataContext, this, false, true, true);
-
-				return dataContext.GetCount (request);
-			}
-
-			return 0;
-		}
-
-
-		private IEnumerable<AiderPersonEntity> GetParticipants(int count)
-		{
-			var dataContext = DataContextPool.GetDataContext (this);
-
-			if ((dataContext != null) &&
-				(dataContext.IsPersistent (this)))
-			{
-				var request = AiderGroupParticipantEntity.CreateParticipantRequest (dataContext, this, true, true, true);
-
-				request.Skip = 0;
-				request.Take = count;
-
-				return dataContext.GetByRequest<AiderPersonEntity> (request);
-			}
-
-			return Enumerable.Empty<AiderPersonEntity> ();
-		}
-
-
 		private AiderGroupSubGroupList subgroupsList;
+
 
 		private static readonly int maxGroupLevel = 6;
 
+
 	}
+
+
 }
