5b824f1a5bd9a1f965531e69d70fc3da0d704e13
Marc Bettex <bettex@epsitec.ch>
2013-02-26 13:31:38 +0000

    - Corrigé un bug d'affichage introduit à la révision précédente. Il y a un commentaire dans le code qui explique le pourquoi du comment.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20207 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Client/WebCore/js/entityUi/TileColumn.js
+++ Epsitec.Cresus/Cresus.WebCore.Client/WebCore/js/entityUi/TileColumn.js
@@ -39,6 +39,18 @@ function() {
           this
       );
 
+      // This is a hack to work around a bug in extjs. If we have a single item,
+      // it will have the proper size, but if the width of its content is
+      // smaller than the minWidth, its content (toolbar and html) won't be
+      // expanded to fit the width of the column. Therefore, if we have a single
+      // or no item, we change the layout and everything is displayed properly.
+
+      if (newOptions.items.length < 2) {
+        newOptions.layout = {
+          type: 'vbox'
+        };
+      }
+
       this.callParent([newOptions]);
       return this;
     },
