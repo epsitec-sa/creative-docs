bb92b46098abd947012c9dce43ea22c81bb4853b
Marc Bettex <bettex@epsitec.ch>
2013-02-28 13:15:19 +0000

    - Ajouté tous les utilisateurs de tests automatiquement lors du processus d'importation. Par contre il faut manuellement les assigner à la personne à qui ils correspondent.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20228 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/Override/AiderDatabaseInitializer.cs
+++ Epsitec.Cresus/Aider/Override/AiderDatabaseInitializer.cs
@@ -8,6 +8,8 @@ using Epsitec.Cresus.Core.Business.UserManagement;
 using Epsitec.Cresus.Core.Entities;
 using Epsitec.Cresus.Core.Library;
 
+using System.Collections.Generic;
+
 
 namespace Epsitec.Aider.Override
 {
@@ -30,6 +32,7 @@ namespace Epsitec.Aider.Override
 			this.CreateAiderScopes ();
 			this.CreateAiderRoles ();
 			this.CreateAdministrator ();
+			this.CreateAiderTestUsers ();
 		}
 
 
@@ -132,6 +135,54 @@ namespace Epsitec.Aider.Override
 			);
 
 			admininstrator.Role = this.roleAdministrator;
+		}
+
+
+		private void CreateAiderTestUsers()
+		{
+			// Don't forget to assign them to the persons once the db is initialized.
+
+			this.CreateAiderTestUser ("p.arnaud", false);		// Pierre Arnaud
+			this.CreateAiderTestUser ("m.bettex", false);		// Marc Bettex
+			this.CreateAiderTestUser ("b.bolay", false);		// Bernard Bolay
+			this.CreateAiderTestUser ("e.bornand", false);		// Eric Bornand
+			this.CreateAiderTestUser ("j.brand", true);			// Jacques Brand
+			this.CreateAiderTestUser ("g.butticaz", false);		// Geneviève Butticaz
+			this.CreateAiderTestUser ("b.corbaz", false);		// Benjamin Corbaz
+			this.CreateAiderTestUser ("c.cuendet", false);		// Claude Cuendet
+			this.CreateAiderTestUser ("l.dewarrat", false);		// Laurence Dewarrat
+			this.CreateAiderTestUser ("d.fankhauser", false);	// Damaris Fankhauser
+			this.CreateAiderTestUser ("m.genoux", false);		// Michel Genoux
+			this.CreateAiderTestUser ("c.jackson", false);		// Cheryl Jackson
+			this.CreateAiderTestUser ("p.jarne", false);		// Pierrette Jarne
+			this.CreateAiderTestUser ("g.jaton", true);			// Gérard Jaton
+			this.CreateAiderTestUser ("y.knecht", false);		// Yvonne Knecht
+			this.CreateAiderTestUser ("l.pestalozzi", false);	// Lorenzo Pestalozzi
+			this.CreateAiderTestUser ("d.rochat", false);		// Dorothée Rochat (is not in imported persons. Her address is: Avenue du Prieuré 2b, 1009 Pully)
+			this.CreateAiderTestUser ("p.rouge", false);		// Pascal Rouge
+			this.CreateAiderTestUser ("j.sordet", true);		// Jean-Michel Sordet
+			this.CreateAiderTestUser ("j.sotornik", false);		// Jeanette Sotornik
+			this.CreateAiderTestUser ("j.spothelfer", false);	// Jean-Marc Spothelfer
+			this.CreateAiderTestUser ("s.wohlhauser", false);	// Sylvie Wohlhauser
+		}
+
+
+		private void CreateAiderTestUser(string login, bool admin)
+		{
+			var groups = admin
+				? new SoftwareUserGroupEntity[] { this.GroupStandard, this.GroupAdministrator }
+				: new SoftwareUserGroupEntity[] { this.GroupStandard };
+
+			var user = (AiderUserEntity) this.CreateUser
+			(
+				displayName: login,
+				userLogin: login,
+				userPassword: "monsupermotdepasse",
+				authentificationMethod: UserAuthenticationMethod.Password,
+				groups: groups
+			);
+
+			user.Role = this.roleCounty;
 		}
 
 
