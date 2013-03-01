67f872668306247dee9add06d87af7dbdbedb922
Marc Bettex <bettex@epsitec.ch>
2013-03-01 14:24:43 +0000

    - Ajouté une classe stub dans le projet ComptaNG.Client pour pouvoir compiler la solution depuis la ligne de commande, vu que le compilateur en ligne de commande n'aime pas les projet vide et sort une erreur CS2008.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20240 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.ComptaNG.Client/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace Epsitec.Cresus.ComptaNG.Client
+{
+	// The command line compiler doesn't like projects without sour files and signals the CS2008
+	// error. To make it happy, here is a dummy class that must be removed as soon as somebody
+	// implements a real class in this project.
+	class Client
+	{
+	}
+}
