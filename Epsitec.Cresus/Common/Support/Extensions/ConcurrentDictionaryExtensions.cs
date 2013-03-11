71c38a05ab1dfa7e05f1e81afbbb35f8b09c13c0
Pierre Arnaud <arnaud@opac.ch>
2013-03-11 09:03:15 +0000

    - Common: commentaires et ménage.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20279 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common/Support/Extensions/ConcurrentDictionaryExtensions.cs
+++ Epsitec.Cresus/Common/Support/Extensions/ConcurrentDictionaryExtensions.cs
@@ -8,6 +8,14 @@ namespace Epsitec.Common.Support.Extensions
 {
 	public static class ConcurrentDictionaryExtensions
 	{
+		/// <summary>
+		/// Removes the specified key from the concurrent dictionary.
+		/// </summary>
+		/// <typeparam name="TKey">The type of the key.</typeparam>
+		/// <typeparam name="TValue">The type of the value.</typeparam>
+		/// <param name="dict">The concurrent dictionary.</param>
+		/// <param name="key">The key.</param>
+		/// <returns><c>true</c> if the key could be removed; otherwise, <c>false</c>.</returns>
 		public static bool Remove<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dict, TKey key)
 		{
 			TValue value;
