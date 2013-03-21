fc218e8b339ea12225d8b93969a9044eecbd310c
Marc Bettex <bettex@epsitec.ch>
2013-03-21 10:42:48 +0000

    - Mis le test à jour par rapport aux modifications du fichier de données ces derniers temps.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20343 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider.Tests.Vs/UnitTestEervMainDataLoader.cs
+++ Epsitec.Cresus/Aider.Tests.Vs/UnitTestEervMainDataLoader.cs
@@ -21,9 +21,9 @@ namespace Aider.Tests.Vs
 		{
 			var groupDefinitions = EervMainDataLoader.LoadEervGroupDefinitions (this.GroupDefinitionFile).ToList ();
 
-			Assert.AreEqual (657, groupDefinitions.Count);
+			Assert.AreEqual (662, groupDefinitions.Count);
 
-			var g1 = new EervGroupDefinition ("0100000000", "Paramètres transversaux", false)
+			var g1 = new EervGroupDefinition ("0101000000", "Fonctions générales", false)
 			{
 				Parent = null,
 			};
@@ -31,27 +31,27 @@ namespace Aider.Tests.Vs
 
 			var g2 = new EervGroupDefinition ("0102000000", "Personnel EERV", false)
 			{
-				Parent = new EervGroupDefinition ("0100000000", null, false),
+				Parent = null,
 			};
-			this.CheckForEquality (g2, groupDefinitions[89]);
+			this.CheckForEquality (g2, groupDefinitions[88]);
 
-			var g3 = new EervGroupDefinition ("0301010000", "Assemblée régionale", false)
+			var g3 = new EervGroupDefinition ("0301010000", "Assemblée", false)
 			{
 				Parent = new EervGroupDefinition ("0301000000", null, false),
 			};
-			this.CheckForEquality (g3, groupDefinitions[281]);
+			this.CheckForEquality (g3, groupDefinitions[287]);
 
-			var g4 = new EervGroupDefinition ("0303030100", "Catéchumènes 2007-10-12", false)
+			var g4 = new EervGroupDefinition ("0303030100", "Catéchumènes 2007-09-11", false)
 			{
 				Parent = new EervGroupDefinition ("0303030000", null, false),
 			};
-			this.CheckForEquality (g4, groupDefinitions[315]);
+			this.CheckForEquality (g4, groupDefinitions[321]);
 
 			var g5 = new EervGroupDefinition ("0604010203", "Délégués CER", false)
 			{
 				Parent = new EervGroupDefinition ("0604010200", null, false),
 			};
-			this.CheckForEquality (g5, groupDefinitions[591]);
+			this.CheckForEquality (g5, groupDefinitions[596]);
 		}
 
 
