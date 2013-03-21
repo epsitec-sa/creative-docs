dd34872799463f957f7c8e5c9b9df84ef53ef495
Marc Bettex <bettex@epsitec.ch>
2013-03-21 12:28:22 +0000

    - Modifié le script de build pour pouvoir créer à la demande l'application pour le serveur de production ou l'application pour le serveur de test en utilisant l'option -production ou l'option -test sur la ligne de commande.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20348 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/App.Aider/build.bat
+++ Epsitec.Cresus/App.Aider/build.bat
@@ -4,35 +4,54 @@
 @rem anywhere.
 cd /d %~dp0
 
-@rem If the first argument is -nocleanup, skip the svn cleanup
-if "%1"=="-nocleanup" goto skipCleanup
+@rem Here we store whether we must cleanup or not
+set cleanup=1
 
-@rem Ask the user if is really wants to revert everything in its svn repositories
-echo This script will:
-echo - Revert all changes to %~dp0..\..\Epsitec
-echo - Delete all unversionned items in %~dp0..\..\Epsitec
-echo - Delete all ignored items in %~dp0..\..\Epsitec
-echo - Revert all changes to %~dp0..\
-echo - Delete all unversionned items in %~dp0..\
-echo - Delete all ignored items in %~dp0..\
-choice /M "Are you sure that you want to continue"
-if %ERRORLEVEL% neq 1 exit /B
+@rem Here we set the configuration directory where we want to pick the configuration for the build.
+set configurationDirectory=0
+
+@rem Process the command line arguments.
+for %%a in (%*) do (
+    if "%%a"=="-nocleanup" set cleanup=0
+    if "%%a"=="-production" set configurationDirectory=ServerConfigProduction
+    if "%%a"=="-test" set configurationDirectory=ServerConfigTest
+)
+
+@rem Exits if the configuration directory is not set up properly
+if %configurationDirectory%==0 (
+    echo The configuration is not set up properly. Use -production or -test.
+    exit /B
+)
+
+@rem Cleans up if we must do so
+if %cleanup%==1 (
+    @rem Ask the user if is really wants to revert everything in its svn repositories
+    echo This script will:
+    echo - Revert all changes to %~dp0..\..\Epsitec
+    echo - Delete all unversionned items in %~dp0..\..\Epsitec
+    echo - Delete all ignored items in %~dp0..\..\Epsitec
+    echo - Revert all changes to %~dp0..\
+    echo - Delete all unversionned items in %~dp0..\
+    echo - Delete all ignored items in %~dp0..\
+    choice /M "Are you sure that you want to continue"
+    if %ERRORLEVEL% neq 1 exit /B
+
+    @echo on
+
+    @rem Revert all changes to the two repositories.
+    svn revert -R ..\..\Epsitec
+    svn revert -R ..\
+
+    @rem Delete all unversionned files and folders in the two repositories.
+    for /f "usebackq tokens=2*" %%i in (`svn status ..\..\Epsitec ^| findstr /r "^\?"`) do svn delete --force "%%i %%j"
+    for /f "usebackq tokens=2*" %%i in (`svn status ..\ ^| findstr /r "^\?"`) do svn delete --force "%%i %%j"
+
+    @rem Delete all ignored files and folders in the two repositories
+    for /f "usebackq tokens=2*" %%i in (`svn status --no-ignore ..\..\Epsitec ^| findstr /r "^I"`) do svn delete --force "%%i %%j"
+    for /f "usebackq tokens=2*" %%i in (`svn status --no-ignore ..\ ^| findstr /r "^I"`) do svn delete --force "%%i %%j"
+)
 
 @echo on
-
-@rem Revert all changes to the two repositories.
-svn revert -R ..\..\Epsitec
-svn revert -R ..\
-
-@rem Delete all unversionned files and folders in the two repositories.
-for /f "usebackq tokens=2*" %%i in (`svn status ..\..\Epsitec ^| findstr /r "^\?"`) do svn delete --force "%%i %%j"
-for /f "usebackq tokens=2*" %%i in (`svn status ..\ ^| findstr /r "^\?"`) do svn delete --force "%%i %%j"
-
-@rem Delete all ignored files and folders in the two repositories
-for /f "usebackq tokens=2*" %%i in (`svn status --no-ignore ..\..\Epsitec ^| findstr /r "^I"`) do svn delete --force "%%i %%j"
-for /f "usebackq tokens=2*" %%i in (`svn status --no-ignore ..\ ^| findstr /r "^I"`) do svn delete --force "%%i %%j"
-
-:skipCleanup
 
 @rem There are many solutions and configurations involved here. Below is the details about them.
 @rem - Epsitec.Cresus.sln, Release, Mixed Platforms
@@ -81,17 +100,17 @@ xcopy /e ..\Cresus.WebCore.Client\WebCore bin\Build\aider\client
 xcopy /e ..\Cresus.WebCore.Maintenance bin\Build\aider\maintenance
 xcopy /e bin\Release bin\Build\aider\server
 
-@rem Copy the production config file.
-copy Production\app.config bin\Build\aider\server\App.Aider.exe.config
+@rem Copy the app.config file.
+copy %configurationDirectory%\app.config bin\Build\aider\server\App.Aider.exe.config
 
-@rem Copy the production nginx config files.
-copy Production\nginx-maintenance.conf bin\Build\aider\maintenance\Nginx\conf\nginx.conf
-copy Production\nginx-server.conf bin\Build\aider\server\Nginx\conf\nginx.conf
+@rem Copy the nginx config files.
+copy %configurationDirectory%\nginx-maintenance.conf bin\Build\aider\maintenance\Nginx\conf\nginx.conf
+copy %configurationDirectory%\nginx-server.conf bin\Build\aider\server\Nginx\conf\nginx.conf
 
-@rem Copy the production certificate and keys
+@rem Copy the certificate and key
 mkdir bin\Build\aider\server\certificate
-copy Production\certificate.pem bin\Build\aider\server\certificate\certificate.pem
-copy Production\certificate.key bin\Build\aider\server\certificate\certificate.key
+copy %configurationDirectory%\certificate.pem bin\Build\aider\server\certificate\certificate.pem
+copy %configurationDirectory%\certificate.key bin\Build\aider\server\certificate\certificate.key
 
 @rem Zip the build.
 ..\..\Epsitec\dot.net\Epsitec.ZipMe\Epsitec.ZipMe\bin\Release\Epsitec.ZipMe.exe bin\Build\aider.zip bin\Build\aider
