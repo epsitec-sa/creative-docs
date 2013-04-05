642fde117a794b6862e2e89f87690625f84635d3
Marc Bettex <bettex@epsitec.ch>
2013-04-05 15:49:28 +0000

    - Ajouté une classe CsvWriter qui permet d'écrire simplemente des données au format CSV. - Ajouté des tests pour cette classe.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20455 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Tests.Vs/IO/UnitTestCvsWriter.cs
@@ -0,0 +1,158 @@
+﻿using Epsitec.Common.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+
+using System.IO;
+
+using System.Text;
+
+namespace Epsitec.Common.Tests.Vs.IO
+{
+	[TestClass]
+	public class UnitTestCvsWriter
+	{
+		[TestMethod]
+		public void SimpleTest()
+		{
+			var format = new CsvFormat ()
+			{
+				ColumnNames = this.GetHeaders ()
+			};
+			var rows = this.GetRows ();
+
+			var encoding = Encoding.Default;
+			var expected = "a;b;c\n1;2;3\n4;5;6\n7;8;9";
+
+			this.Check (format, rows, encoding, expected);
+		}
+
+		[TestMethod]
+		public void TestFieldSeparator()
+		{
+			var format = new CsvFormat ()
+			{
+				ColumnNames = this.GetHeaders (),
+				FieldSeparator = '-'
+			};
+			var rows = this.GetRows ();
+
+			var encoding = Encoding.Default;
+			var expected = "a-b-c\n1-2-3\n4-5-6\n7-8-9";
+
+			this.Check (format, rows, encoding, expected);
+		}
+
+		[TestMethod]
+		public void TestLineSeparator()
+		{
+			var format = new CsvFormat ()
+			{
+				ColumnNames = this.GetHeaders (),
+				LineSeparator = '-'
+			};
+			var rows = this.GetRows ();
+
+			var encoding = Encoding.Default;
+			var expected = "a;b;c-1;2;3-4;5;6-7;8;9";
+
+			this.Check (format, rows, encoding, expected);
+		}
+
+		[TestMethod]
+		public void TestEncoding()
+		{
+			// The € sign is not at the same place in the greek encoding than in the encoding that
+			// you should get with the Encoding.Default property. This way we check that the csv
+			// writer does not uses it.
+
+			var greekEncoding = Encoding.GetEncoding ("iso-8859-7");
+			var format = new CsvFormat ()
+			{
+				ColumnNames = this.GetHeaders (),
+				Encoding = greekEncoding
+			};
+
+			var rows = new List<IEnumerable<string>> ()
+			{
+				new List<string> () { "€", "€", "€", }
+			};
+
+			var expected = "a;b;c\n€;€;€";
+
+			this.Check (format, rows, greekEncoding, expected);
+		}
+
+		[TestMethod]
+		public void TestQuote()
+		{
+			var format = new CsvFormat ()
+			{
+				ColumnNames = this.GetHeaders ()
+			};
+
+			var rows = new List<IEnumerable<string>> ()
+			{
+				new List<string> () { " a", "a ", "\"", ";", "\n" },
+			};
+
+			var encoding = Encoding.Default;
+			var expected = "a;b;c\n"
+				+ "\" a\"" + ";"
+				+ "\"a \"" + ";"
+				+ "\"\"\"\"" + ";"
+				+ "\";\"" + ";"
+				+ "\"\n\"";
+
+			this.Check (format, rows, encoding, expected);
+		}
+
+		private string[] GetHeaders()
+		{
+			return new string[] { "a", "b", "c" };
+		}
+
+		private IEnumerable<IEnumerable<string>> GetRows()
+		{
+			int nb = 1;
+
+			for (int i = 0; i < 3; i++)
+			{
+				var list = new List<string> ();
+
+				for (int j = 0; j < 3; j++)
+				{
+					list.Add (nb.ToString ());
+
+					nb++;
+				}
+
+				yield return list;
+			}
+		}
+
+		private void Check(CsvFormat format, IEnumerable<IEnumerable<string>> rows, Encoding encoding, string expected)
+		{
+			var actualBytes = this.GetActualBytes (format, rows);
+			var expectedBytes = this.GetExpectedBytes (encoding, expected);
+
+			CollectionAssert.AreEqual (expectedBytes, actualBytes);
+		}
+
+		private byte[] GetActualBytes(CsvFormat format, IEnumerable<IEnumerable<string>> rows)
+		{
+			using (var memoryStream = new MemoryStream ())
+			{
+				CsvWriter.Write (format, rows, memoryStream);
+
+				return memoryStream.ToArray ();
+			}
+		}
+
+		private byte[] GetExpectedBytes(Encoding encoding, string expected)
+		{
+			return encoding.GetBytes (expected);
+		}
+	}
+}
---
+++ Epsitec.Cresus/Common/IO/CsvWriter.cs
@@ -0,0 +1,157 @@
+﻿using Epsitec.Common.Support.Extensions;
+
+using System.Collections.Generic;
+
+using System.IO;
+
+using System.Linq;
+using System;
+
+
+namespace Epsitec.Common.IO
+{
+	/// <summary>
+	/// Helper class that is used to write data as CSV.
+	/// </summary>
+	/// <remarks>
+	/// The definition of the CSV format used here is defined here:
+	/// http://www.creativyst.com/Doc/Articles/CSV/CSV01.htm
+	/// 
+	/// Here is a summary of the rules:
+	/// - Lines are separated by the given instance CsvFormat.LineSeparator char
+	/// - Fields are separated by the given instance CsvFormat.FieldSeparator char
+	/// - The quote char is defined by the instance CsvFormat.QuoteChar char
+	/// - A field must be quoted if
+	///   - it starts with space
+	///   - it ends with space
+	///   - it contains the line separator char
+	///   - it contains the field separator char
+	///   - it contains the quote char
+	/// - An quoted field is enclosed within quote chars and each occurence of the quote char
+	///   within it is escaped with the quote char (the field [a"b] becomes ["a""b"])
+	/// 
+	/// The CsvFormat must contain the header definitions.
+	/// </remarks>
+	public static class CsvWriter
+	{
+		public static void Write(CsvFormat format, IEnumerable<IEnumerable<string>> rows, string outputFile)
+		{
+			using (var outputStream = File.OpenWrite (outputFile))
+			{
+				CsvWriter.Write (format, rows, outputStream);
+			}
+		}
+
+		public static void Write(CsvFormat format, IEnumerable<IEnumerable<string>> rows, Stream outputStream)
+		{
+			var lines = CsvWriter.GetLines (format, rows);
+
+			CsvWriter.WriteLines (format, lines, outputStream);
+		}
+
+		private static IEnumerable<IEnumerable<string>> GetLines(CsvFormat format, IEnumerable<IEnumerable<string>> rows)
+		{
+			var headers = format.ColumnNames.ToEnumerable<IEnumerable<string>> ();
+			var lines = headers.Concat (rows);
+
+			return lines.Select (l => CsvWriter.GetFields (format, l));
+		}
+
+		private static IEnumerable<string> GetFields(CsvFormat format, IEnumerable<string> cells)
+		{
+			return cells.Select (f => CsvWriter.GetField (format, f));
+		}
+
+		private static string GetField(CsvFormat format, string value)
+		{
+			if (string.IsNullOrEmpty (value))
+			{
+				return "";
+			}
+
+			if (CsvWriter.MustQuoteField (format, value))
+			{
+				return CsvWriter.QuoteField (format, value);
+			}
+
+			return value;
+		}
+
+		private static bool MustQuoteField(CsvFormat format, string field)
+		{
+			// If the string contains any special char, it must be quoted.
+			var specialChars = new char[]
+			{
+				format.QuoteChar,
+				format.FieldSeparator,
+				format.LineSeparator
+			};
+
+			var index = field.IndexOfAny (specialChars);
+
+			if (index != -1)
+			{
+				return true;
+			}
+
+			// If the string starts or ends with space, it must be quoted. 
+			if (field[0] == ' ' || field[field.Length - 1] == ' ')
+			{
+				return true;
+			}
+
+			// If none of the above condition holds, the string don't require to be quoted.
+			return false;
+		}
+
+		private static string QuoteField(CsvFormat format, string field)
+		{
+			var oneQuote = "" + format.QuoteChar;
+			var twoQuotes = "" + format.QuoteChar + format.QuoteChar;
+
+			var escapedField = field.Replace (oneQuote, twoQuotes);
+
+			return "" + format.QuoteChar + escapedField + format.QuoteChar;
+		}
+
+		private static void WriteLines(CsvFormat format, IEnumerable<IEnumerable<string>> lines, Stream stream)
+		{
+			// We don't dispose the stream writer because we don't want to dispose the underlying
+			// stream here. Maybe the caller has used a memory stream and intends to read back the
+			// result.
+			var streamWriter = new StreamWriter (stream, format.Encoding);
+
+			var firstLine = true;
+
+			foreach (var line in lines)
+			{
+				if (!firstLine)
+				{
+					streamWriter.Write (format.LineSeparator);
+				}
+				else
+				{
+					firstLine = false;
+				}
+
+				var firstField = true;
+
+				foreach (var field in line)
+				{
+					if (!firstField)
+					{
+						streamWriter.Write (format.FieldSeparator);
+					}
+					else
+					{
+						firstField = false;
+					}
+
+					streamWriter.Write (field);
+				}
+			}
+
+			streamWriter.Flush ();
+		}
+	}
+}
