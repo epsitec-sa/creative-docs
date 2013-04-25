2b8521a3811f9358b884233348f166f5529f943a
Marc Bettex <bettex@epsitec.ch>
2013-04-25 14:06:13 +0000

    - Suppression de code mort.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20585 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/Enumerations/TextGenerator.cs
+++ Epsitec.Cresus/Aider/Enumerations/TextGenerator.cs
@@ -4,40 +4,6 @@ namespace Epsitec.Aider.Enumerations
 {
 	public static class TextGenerator
 	{
-		public static string AsShortText(this PersonSex sex)
-		{
-			switch (sex)
-			{
-				case PersonSex.Female:
-					return "F";
-
-				case PersonSex.Male:
-					return "H";
-
-				case PersonSex.Unknown:
-					return null;
-
-				default:
-					throw new NotImplementedException ();
-			}
-		}
-
-
-		public static string AsShortText(this Language language)
-		{
-			switch (language)
-			{
-				case Language.French:
-					return "Fr";
-
-				case Language.German:
-					return "De";
-
-				default:
-					throw new NotImplementedException ();
-			}
-		}
-
 		public static string GetText(this PersonMrMrs value, bool abbreviated)
 		{
 			return abbreviated
