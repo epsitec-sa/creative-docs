d76db7a92f5a0770ee972e83741800abccd26563
Marc Bettex <bettex@epsitec.ch>
2013-04-29 09:00:59 +0000

    - Ajouté une classe qui permet de créer l'abbréviation d'un prénom d'une personne. Cela fonctionne bien avec les prénoms occidentaux. Je ne garantis pas le fonctionnement avec des noms étrangers ou bizarres.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20595 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Common.Tests.Vs/Text/UnitTestNameProcessor.cs
@@ -0,0 +1,51 @@
+﻿using Epsitec.Common.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+
+using System.Collections.Generic;
+
+
+namespace Epsitec.Common.Tests.Vs.Text
+{
+
+
+	[TestClass]
+	public class UnitTestNameProcessor
+	{
+
+
+		[TestMethod]
+		public void NameProcessorTest()
+		{
+			var samples = new List<Tuple<string, string>>
+			{
+				Tuple.Create ((string) null, (string) null),
+				Tuple.Create ("", ""),
+				Tuple.Create ("Marc", "M."),
+				Tuple.Create ("Marc andré", "M."),
+				Tuple.Create ("Marc-André", "M.-A."),
+				Tuple.Create ("Märc andré", "M."),
+				Tuple.Create ("Märc-André", "M.-A."),
+				Tuple.Create ("Märc-André Albert", "M.-A."),
+				Tuple.Create ("Märc-André-Albert", "M.-A.-A."),
+				Tuple.Create ("M'Ärc", "M'Ä."),
+				Tuple.Create ("G-Rémi", "G-R."),
+				Tuple.Create ("Lou-K", "L.-K"),
+			};
+
+			foreach (var sample in samples)
+			{
+				var actual = NameProcessor.GetAbbreviatedFirstname(sample.Item1);
+				var expected = sample.Item2;
+
+				Assert.AreEqual (expected, actual);
+			}
+		}
+
+
+	}
+
+
+}
---
+++ Epsitec.Cresus/Common/Text/NameProcessor.cs
@@ -0,0 +1,102 @@
+﻿using Epsitec.Common.Support;
+
+using System.Text;
+
+namespace Epsitec.Common.Text
+{
+	public static class NameProcessor
+	{
+		public static string GetAbbreviatedFirstname(string firstname)
+		{
+			if (string.IsNullOrEmpty (firstname))
+			{
+				return firstname;
+			}
+
+			// For the abbreviation, we will keep only the first part of the name, i.e. everything
+			// that comes before the first space in the name.
+
+			var spaceIndex = firstname.IndexOf (' ');
+			if (spaceIndex >= 0)
+			{
+				firstname = firstname.Substring (0, spaceIndex);
+			}
+
+			// Now we want to abbreviate each token and keep the token separators. A token is
+			// defineed as a sequence of letters (with accents). They are separated by separators,
+			// which are everything else (usually '-').
+
+			var result = new StringBuilder ();
+
+			// Here we store the first symbol of the curren token, so that when we reach its end, we
+			// can make the abbreviation.
+			char? firstTokenSymbol = null;
+
+			// Here we store the fact whether the token has one or more sympbols, so that we know
+			// whether or not we must put a dot in the abbreviation.
+			bool singleLetterToken = true;
+
+			for(int i = 0; i < firstname.Length; i++) 
+			{
+				var c = firstname[i];
+
+				bool isPartOfToken = NameProcessor.IsPartOfToken (c);
+				bool isLastSymbol = (i == firstname.Length - 1);
+
+				// Process the current sympbol as part of the token if it is.
+				if (isPartOfToken)
+				{
+					if (firstTokenSymbol.HasValue)
+					{
+						singleLetterToken = false;
+					}
+					else
+					{
+						firstTokenSymbol = c;
+						singleLetterToken = true;
+					}
+				}
+
+				// Finish the token if the current symbol is not part of a token or if it is the
+				// last symbol in the name.
+				if (!isPartOfToken || isLastSymbol)
+				{
+					if (firstTokenSymbol.HasValue)
+					{
+						result.Append (firstTokenSymbol.Value);
+
+						if (!singleLetterToken)
+						{
+							result.Append ('.');
+						}
+
+						firstTokenSymbol = null;
+					}
+				}
+
+				// Adds the current symbol, if it is a token separator.
+				if (!isPartOfToken)
+				{
+					result.Append (c);
+				}
+			}
+
+			return result.ToString ();
+		}
+
+		private static bool IsPartOfToken(char c)
+		{
+			var converted = StringUtils.RemoveDiacritics (c.ToString ());
+
+			for (int i = 0; i < converted.Length; i++)
+			{
+				if (!char.IsLetter (converted[i]))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
