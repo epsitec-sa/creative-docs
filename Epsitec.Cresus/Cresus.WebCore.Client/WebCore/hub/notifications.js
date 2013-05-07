4f08bc427b3d3c286657be5b8463dfebc3467a9a
Samuel Loup <loup@epsitec.ch>
2013-05-07 13:22:28 +0000

    - WebCore.Client : ajout des statics JS et CSS oublié lors du dernier commit
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20691 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.WebCore.Client/WebCore/hub/notifications.js
@@ -0,0 +1,61 @@
+﻿///AIDER NotificationHub
+
+
+var signalRClient = {};
+
+$(function () {
+   
+    function Toastr() {
+
+        this.hub = $.connection.notificationHub;
+
+        //Initialize
+        this.init = function () {
+            signalRClient.connectionId = this.hub.connection.id;
+        }
+
+        //Test Hub
+        this.WarningToastTo = function (connectionId,title,message,datasetId,entityId)
+        {
+            this.hub.server.warningToast(connectionId,title,message,datasetId,entityId);
+
+        }
+
+        //Handlers for our Hub callbacks
+        this.hub.client.StickyWarningNavToast = function (title,msg,datasetId,entityId) {
+
+            //ex. {"id":"[AVA]-1000000054","name":"[LVAHD]","level":"2"}
+            var path = {};
+            path.id = entityId;
+            path.name = datasetId;
+
+            toastr.options = {
+                "debug": false,
+                "positionClass": "toast-bottom-full-width",
+                "onclick": function () {
+                    var app = Epsitec.Cresus.Core.getApplication();
+                    app.showEditableEntity(path);
+                },
+                "fadeIn": 300,
+                "fadeOut": 1000,
+                "timeOut": 0
+            };
+            toastr.warning(msg, title);
+        }
+
+    };
+
+    if (epsitecConfig.featureNotifications)
+    {    
+        $.getScript('signalr/hubs', function () {
+
+            $.connection.hub.logging = false;
+
+            signalRClient.instance = new Toastr();
+            // Start the connection
+            $.connection.hub.start(function () { signalRClient.instance.init(); });
+        });
+    }
+    
+
+});
