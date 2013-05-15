646cac1bda6a2e9e4d96d89b96190e014338e055
Pierre Arnaud <arnaud@opac.ch>
2013-05-15 09:01:46 +0000

    - Core: ajouté un embryon de NotificationManager qui est disponible si la feature "Notifications" est active.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20749 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.Core.Library/Library/NotificationManager.cs
@@ -0,0 +1,31 @@
+//	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Support;
+using Epsitec.Common.Support.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Core.Library
+{
+	public class NotificationManager : CoreAppComponent
+	{
+		public NotificationManager(CoreApp app)
+			: base (app)
+		{
+		}
+
+		#region Factory Class
+
+		private sealed class Factory : Epsitec.Cresus.Core.Factories.DefaultCoreAppComponentFactory<SettingsManager>
+		{
+			public override bool ShouldCreate(CoreApp host)
+			{
+				return CoreContext.HasExperimentalFeature ("Notifications");
+			}
+		}
+
+		#endregion
+	}
+}
+
