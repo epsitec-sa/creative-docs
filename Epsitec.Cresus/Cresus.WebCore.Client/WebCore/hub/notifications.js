0a97d27f54bab7f049bdc205615a47238194d530
Samuel Loup <loup@epsitec.ch>
2013-05-15 12:20:38 +0000

    WebCore : notifications.js, remise en place de l'implémentation du toast simple
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20756 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Client/WebCore/hub/notifications.js
+++ Epsitec.Cresus/Cresus.WebCore.Client/WebCore/hub/notifications.js
@@ -23,8 +23,6 @@ $(function () {
 
         //Handlers for our Hub callbacks
         this.hub.client.StickyWarningNavToast = function (title,msg,datasetId,entityId) {
-
-            //ex. {"id":"[AVA]-1000000054","name":"[LVAHD]","level":"2"}
             var path = {};
             path.id = entityId;
             path.name = datasetId;
@@ -41,6 +39,25 @@ $(function () {
                 "timeOut": 0
             };
             toastr.warning(msg, title);
+        }
+
+        this.hub.client.Toast = function (title, msg, datasetId, entityId) {
+            var path = {};
+            path.id = entityId;
+            path.name = datasetId;
+
+            toastr.options = {
+                "debug": false,
+                "positionClass": "toast-bottom-full-width",
+                "onclick": function () {
+                    var app = Epsitec.Cresus.Core.getApplication();
+                    app.showEditableEntity(path);
+                },
+                "fadeIn": 300,
+                "fadeOut": 1000,
+                "timeOut": 1000
+            };
+            toastr.info(msg, title);
         }
 
     };
