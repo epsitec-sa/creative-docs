c6306464a5fa21103e91e180d3dbbfa4ff42cb71
Marc Bettex <bettex@epsitec.ch>
2013-05-16 08:46:08 +0000

    - Ajouté un controlleur pour l'ajout de refus d'abonnements.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20769 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/Controllers/CreationControllers/BrickCreationAiderSubscriptionRefusalViewController0.cs
@@ -0,0 +1,115 @@
+﻿using Epsitec.Aider.Data.Common;
+
+using Epsitec.Aider.Entities;
+using Epsitec.Aider.Enumerations;
+
+using Epsitec.Cresus.Bricks;
+
+using Epsitec.Cresus.Core.Business;
+
+using Epsitec.Cresus.Core.Controllers;
+using Epsitec.Cresus.Core.Controllers.CreationControllers;
+
+using Epsitec.Cresus.Core.Entities;
+
+using System.Linq;
+
+namespace Epsitec.Aider.Controllers.CreationControllers
+{
+	[ControllerSubType (0)]
+	public sealed class BrickCreationAiderSubscriptionRefusalViewController0 : BrickCreationViewController<AiderSubscriptionRefusalEntity>
+	{
+		protected override void GetForm
+		(
+			ActionBrick<AiderSubscriptionRefusalEntity, SimpleBrick<AiderSubscriptionRefusalEntity>> action
+		)
+		{
+			action
+				.Title ("Créer un nouveau refus")
+				.Text ("Choisissez un destinataire pour le refus de l'abonnement.")
+				.Field<AiderHouseholdEntity> ()
+					.Title ("Destinataire (ménage)")
+				.End ()
+				.Field<AiderContactEntity> ()
+					.Title ("Destinataire (personne morale)")
+					.WithDataset (Res.CommandIds.Base.ShowAiderLegalPersonContact)
+				.End ();
+		}
+
+		public override FunctionExecutor GetExecutor()
+		{
+			return FunctionExecutor.Create
+				<AiderHouseholdEntity, AiderContactEntity, AiderSubscriptionRefusalEntity> (this.Execute);
+		}
+
+		private AiderSubscriptionRefusalEntity Execute
+		(
+			AiderHouseholdEntity household,
+			AiderContactEntity legalPersonContact
+		)
+		{
+			if (household.IsNull () && legalPersonContact.IsNull ())
+			{
+				var message = "Il faut spécifier un destinataire.";
+
+				throw new BusinessRuleException (message);
+			}
+
+			if (household.IsNotNull () && legalPersonContact.IsNotNull ())
+			{
+				var message = "Il faut spécifier qu'un seul destinataire.";
+
+				throw new BusinessRuleException (message);
+			}
+
+			return household.IsNotNull ()
+				? this.Create (household)
+				: this.Create (legalPersonContact);
+		}
+
+		private AiderSubscriptionRefusalEntity Create(AiderHouseholdEntity receiver)
+		{
+			this.CheckRefusalDoesNotExist (receiver);
+
+			return AiderSubscriptionRefusalEntity.Create (this.BusinessContext, receiver);
+		}
+
+		private AiderSubscriptionRefusalEntity Create(AiderContactEntity receiver)
+		{
+			if (receiver.ContactType != ContactType.Legal)
+			{
+				var message = "Le destinataire n'est pas une personne morale.";
+
+				throw new BusinessRuleException (message);
+			}
+
+			this.CheckRefusalDoesNotExist (receiver);
+
+			return AiderSubscriptionRefusalEntity.Create (this.BusinessContext, receiver);
+		}
+
+		private void CheckRefusalDoesNotExist(AiderHouseholdEntity receiver)
+		{
+			var result = AiderSubscriptionRefusalEntity.FindRefusal (this.BusinessContext, receiver);
+
+			this.CheckRefusalDoesNotExist (result);
+		}
+
+		private void CheckRefusalDoesNotExist(AiderContactEntity receiver)
+		{
+			var result = AiderSubscriptionRefusalEntity.FindRefusal (this.BusinessContext, receiver);
+
+			this.CheckRefusalDoesNotExist (result);
+		}
+
+		private void CheckRefusalDoesNotExist(AiderSubscriptionRefusalEntity result)
+		{
+			if (result != null)
+			{
+				var message = "Un refus existe déjà pour ce destinataire.";
+
+				throw new BusinessRuleException (message);
+			}
+		}
+	}
+}
---
+++ Epsitec.Cresus/Aider/Entities/AiderSubscriptionRefusalEntity.cs
@@ -0,0 +1,101 @@
+﻿using Epsitec.Aider.Enumerations;
+
+using Epsitec.Cresus.Core.Business;
+
+using Epsitec.Cresus.DataLayer.Loader;
+
+using System.Linq;
+
+
+namespace Epsitec.Aider.Entities
+{
+
+
+	public partial class AiderSubscriptionRefusalEntity
+	{
+
+
+		public static AiderSubscriptionRefusalEntity Create
+		(
+			BusinessContext businessContext,
+			AiderHouseholdEntity household
+		)
+		{
+			var refusal = businessContext.CreateAndRegisterEntity<AiderSubscriptionRefusalEntity> ();
+
+			refusal.RefusalType = SubscriptionType.Household;
+			refusal.Household = household;
+
+			return refusal;
+		}
+
+
+		public static AiderSubscriptionRefusalEntity Create
+		(
+			BusinessContext businessContext,
+			AiderContactEntity legalPersonContact
+		)
+		{
+			var refusal = businessContext.CreateAndRegisterEntity<AiderSubscriptionRefusalEntity> ();
+
+			refusal.RefusalType = SubscriptionType.LegalPerson;
+			refusal.LegalPersonContact = legalPersonContact;
+
+			return refusal;
+		}
+
+
+		public static AiderSubscriptionRefusalEntity FindRefusal
+		(
+			BusinessContext businessContext,
+			AiderHouseholdEntity household
+		)
+		{
+			var example = new AiderSubscriptionRefusalEntity ()
+			{
+				RefusalType = SubscriptionType.Household,
+				Household = household,
+			};
+
+			return AiderSubscriptionRefusalEntity.FindRefusal (businessContext, example);
+		}
+
+
+		public static AiderSubscriptionRefusalEntity FindRefusal
+		(
+			BusinessContext businessContext,
+			AiderContactEntity legalPersonContact
+		)
+		{
+			var example = new AiderSubscriptionRefusalEntity ()
+			{
+				RefusalType = SubscriptionType.LegalPerson,
+				LegalPersonContact = legalPersonContact,
+			};
+
+			return AiderSubscriptionRefusalEntity.FindRefusal (businessContext, example);
+		}
+
+
+		private static AiderSubscriptionRefusalEntity FindRefusal
+		(
+			BusinessContext businessContext,
+			AiderSubscriptionRefusalEntity example
+		)
+		{
+			var request = new Request ()
+			{
+				RootEntity = example,
+			};
+
+			var dataContext = businessContext.DataContext;
+			var result = dataContext.GetByRequest<AiderSubscriptionRefusalEntity> (request);
+
+			return result.FirstOrDefault ();
+		}
+
+
+	}
+
+
+}
