063bc390c3bbf4d07721870a8e4f5f24214106b2
Marc Bettex <bettex@epsitec.ch>
2013-05-16 10:07:37 +0000

    - Correction d'un problème avec Owin qui polluait la console avec l'output de debug.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20778 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Server/Owin/OwinServer.cs
+++ Epsitec.Cresus/Cresus.WebCore.Server/Owin/OwinServer.cs
@@ -4,6 +4,7 @@ using Epsitec.Cresus.WebCore.Server.Owin.Hubs;
 using Microsoft.Owin.Hosting;
 
 using System;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Epsitec.Cresus.WebCore.Server.Owin
@@ -15,6 +16,8 @@ namespace Epsitec.Cresus.WebCore.Server.Owin
 			this.owin = WebApplication.Start<Startup> (uri.AbsoluteUri);
 			this.hubClient = NotificationClient.Instance;
 
+			OwinServer.CleanTraceListeners ();
+
 			Logger.LogToConsole ("Owin Server started");
 		}
 
@@ -23,6 +26,26 @@ namespace Epsitec.Cresus.WebCore.Server.Owin
 			if (this.owin!=null)
 			{
 				this.owin.Dispose ();
+			}
+		}
+
+		private static void CleanTraceListeners()
+		{
+			// In the dll Microsoft.Owin.Hosting, there is the class KatanaEngine which registers a
+			// TraceListener with the name "KatanaTraceListener" to the global Trace.Listeners list
+			// which is the same list as the Diagnostic.Debug.Listeners list. This means that the
+			// debug output is polluted with stuff that we don't want to see.
+			// With this method, we remove this listener from the global Trace.Listeners list, so
+			// the debug output is clean again.
+
+			var listeners = Trace.Listeners
+				.OfType<TraceListener> ()
+				.Where (t => t.Name == "KatanaTraceListener")
+				.ToList ();
+
+			foreach (var listener in listeners)
+			{
+				Trace.Listeners.Remove (listener);
 			}
 		}
 
