8e75dd92579cfc744e66dfb6a3ab0370c85c0c88
Marc Bettex <bettex@epsitec.ch>
2013-05-17 09:40:30 +0000

    - Extrait de la fonctionnalité dans une nouvelle class pour pouvoir la réutiliser.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20804 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/Data/Eerv/EervDataReader.cs
+++ Epsitec.Cresus/Aider/Data/Eerv/EervDataReader.cs
@@ -1,6 +1,4 @@
-﻿using ClosedXML.Excel;
-
-using Epsitec.Common.Support.Extensions;
+﻿using Epsitec.Aider.Data.Common;
 
 using System.Collections.Generic;
 
@@ -19,8 +17,8 @@ namespace Epsitec.Aider.Data.Eerv
 
 		public static IEnumerable<Dictionary<GroupHeader, string>> ReadGroups(FileInfo groupFile, FileInfo superGroupFile)
 		{
-			var groups = EervDataReader.GetRecords (groupFile, EervDataReader.groupHeaders);
-			var superGroups = EervDataReader.GetRecords (superGroupFile, EervDataReader.superGroupHeaders);
+			var groups = DataReader.GetRecords (groupFile, EervDataReader.groupHeaders);
+			var superGroups = DataReader.GetRecords (superGroupFile, EervDataReader.superGroupHeaders);
 
 			return groups.Concat (superGroups);
 		}
@@ -28,129 +26,25 @@ namespace Epsitec.Aider.Data.Eerv
 
 		public static IEnumerable<Dictionary<ActivityHeader, string>> ReadActivities(FileInfo input)
 		{
-			return EervDataReader.GetRecords (input, EervDataReader.activityHeaders);
+			return DataReader.GetRecords (input, EervDataReader.activityHeaders);
 		}
 
 
 		public static IEnumerable<Dictionary<PersonHeader, string>> ReadPersons(FileInfo input)
 		{
-			return EervDataReader.GetRecords (input, EervDataReader.personHeaders);
+			return DataReader.GetRecords (input, EervDataReader.personHeaders);
 		}
 
 
 		public static IEnumerable<Dictionary<GroupDefinitionHeader, string>> ReadGroupDefinitions(FileInfo input)
 		{
-			return EervDataReader.GetRecords (input, EervDataReader.groupDefinitionHeaders, 3);
+			return DataReader.GetRecords (input, EervDataReader.groupDefinitionHeaders, 3);
 		}
 
 
 		public static IEnumerable<Dictionary<IdHeader, string>> ReadIds(FileInfo input)
 		{
-			return EervDataReader.GetRecords (input, EervDataReader.idHeaders);
-		}
-
-
-		private static IEnumerable<Dictionary<T, string>> GetRecords<T>(FileInfo input, Dictionary<T, string> stringMapping, int nbToSkip = 0)
-		{
-			Dictionary<T, int?> indexMapping = null;
-
-			foreach (var line in EervDataReader.GetLines (input).Skip (nbToSkip))
-			{
-				if (indexMapping == null)
-				{
-					indexMapping = EervDataReader.GetIndexMapping (line, stringMapping);
-				}
-				else
-				{
-					var record = EervDataReader.GetRecord (line, indexMapping);
-
-					var isEmpty = record.All (r => string.IsNullOrWhiteSpace (r.Value));
-
-					if (!isEmpty)
-					{
-						yield return record;
-					}
-				}
-			}
-		}
-
-
-		private static IEnumerable<IList<string>> GetLines(FileInfo input)
-		{
-			var lines = new List<List<string>> ();
-
-			if (input != null)
-			{
-				using (var workbook = new XLWorkbook (input.FullName))
-				{
-					var worksheet = workbook.Worksheets.First ();
-
-					foreach (var row in worksheet.RowsUsed ())
-					{
-						var line = new List<string> ();
-
-						foreach (var cell in row.CellsUsed ())
-						{
-							var columnIndex = cell.Address.ColumnNumber - 1;
-							var value = cell.GetValue<string> ();
-
-							line.InsertAtIndex (columnIndex, value);
-						}
-
-						lines.Add (line);
-					}
-				}
-			}
-
-			return lines;
-		}
-
-
-		private static Dictionary<T, int?> GetIndexMapping<T>(IList<string> headers, Dictionary<T, string> stringMapping)
-		{
-			var indexMapping = new Dictionary<T, int?> ();
-
-			foreach (var item in stringMapping)
-			{
-				var key = item.Key;
-				var index = headers.IndexOf (item.Value);
-
-				indexMapping[key] = index >= 0
-					? index
-					: (int?) null;
-			}
-
-			return indexMapping;
-		}
-
-
-		private static Dictionary<T, string> GetRecord<T>(IList<string> line, Dictionary<T, int?> indexMapping)
-		{
-			var r = new Dictionary<T, string> ();
-
-			foreach (var mapping in indexMapping)
-			{
-				var key = mapping.Key;
-				var index = mapping.Value;
-
-				var value = index.HasValue && line.Count > index.Value
-					? line[index.Value]
-					: null;
-
-				if (value != null)
-				{
-					value = value.Trim ();
-
-					if (string.IsNullOrWhiteSpace (value))
-					{
-						value = null;
-					}
-				}
-
-				r[key] = value;
-			}
-
-			return r;
+			return DataReader.GetRecords (input, EervDataReader.idHeaders);
 		}
 
 
