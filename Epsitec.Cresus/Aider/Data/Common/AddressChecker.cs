b1bca4d73aaacd45dcaf24883022e3effe38aaa7
Marc Bettex <bettex@epsitec.ch>
2013-05-17 14:55:05 +0000

    - Extrait une partie de EervParishDataLoader dans AddressChecker pour pouvoir réutiliser la fonctionnalité ailleurs.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20811 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/Data/Common/AddressChecker.cs
@@ -0,0 +1,144 @@
+﻿using Epsitec.Aider.Tools;
+
+using Epsitec.Common.Support;
+using Epsitec.Common.Types;
+
+using System;
+
+using System.Collections.Generic;
+
+
+namespace Epsitec.Aider.Data.Common
+{
+
+
+	internal sealed class AddressChecker
+	{
+
+
+		public AddressChecker()
+		{
+			this.townCorrections = new Dictionary<Tuple<string, string>, Tuple<string, string>> ();
+			this.streetCorrections = new Dictionary<Tuple<string, string, int?, int, string>, Tuple<string, string, int?, int, string>> ();
+			this.townChecker = new TownChecker ();
+		}
+
+
+		public void FixZipCodeAndTown(ref string zipCode, ref string town)
+		{
+			var result = this.townChecker.Validate (zipCode, town);
+			var newZipCode = result.Item1;
+			var newTown = result.Item2;
+
+			if (newZipCode != zipCode || newTown != town)
+			{
+				var key = Tuple.Create (zipCode, town);
+				var value = Tuple.Create (newZipCode, newTown);
+
+				this.townCorrections[key] = value;
+			}
+
+			zipCode = newZipCode;
+			town = newTown;
+		}
+
+
+		public void FixStreetName
+		(
+			ref string firstAddressLine,
+			ref string streetName,
+			int? houseNumber,
+			ref string zipCode,
+			ref string town
+		)
+		{
+			if (string.IsNullOrEmpty (town) ||  string.IsNullOrWhiteSpace (zipCode))
+			{
+				return;
+			}
+
+			var zipCodeInt = int.Parse (zipCode);
+			var zipCodeAddOn = 0;
+			var zipCodeId = 0;
+
+			var saveFirstAddressLine = firstAddressLine;
+			var saveStreetName = streetName;
+			var saveZipCodeInt = zipCodeInt;
+			var saveTown = town;
+
+			AddressPatchEngine.Current.FixAddress
+			(
+				ref firstAddressLine, ref streetName, houseNumber, ref zipCodeInt,
+				ref zipCodeAddOn, ref zipCodeId, ref town
+			);
+
+			var diff = firstAddressLine != saveFirstAddressLine
+				|| streetName  != saveStreetName
+				|| zipCodeInt != saveZipCodeInt
+				|| town != saveTown;
+
+			if (diff)
+			{
+				var key = Tuple.Create
+				(
+					saveFirstAddressLine, saveStreetName, houseNumber, saveZipCodeInt, saveTown
+				);
+
+				var value = Tuple.Create
+				(
+					firstAddressLine, streetName, houseNumber, zipCodeInt, town
+				);
+
+				this.streetCorrections[key] = value;
+			}
+
+			zipCode = InvariantConverter.ToString (zipCodeInt);
+		}
+
+
+		public void DisplayWarnings()
+		{
+			Console.WriteLine ("=================================================================");
+			Console.WriteLine ("Town corrections");
+			foreach (var townCorrection in this.townCorrections)
+			{
+				var key = townCorrection.Key;
+				var value = townCorrection.Value;
+
+				var message = key.Item1 + "; " + key.Item2
+						+ " => "
+						+ value.Item1 + "; " + value.Item2;
+
+				Console.WriteLine (message);
+			}
+
+			Console.WriteLine ("=================================================================");
+			Console.WriteLine ("Street corrections");
+			foreach (var streetCorrection in this.streetCorrections)
+			{
+				var key = streetCorrection.Key;
+				var value = streetCorrection.Value;
+
+				var message = StringUtils.Join ("; ", key.Item1, key.Item2, key.Item3, key.Item4, key.Item5)
+						+ " => "
+						+ StringUtils.Join ("; ", value.Item1, value.Item2, value.Item3, value.Item4, value.Item5);
+
+				Console.WriteLine (message);
+			}
+		}
+
+
+
+		private readonly Dictionary<Tuple<string, string>, Tuple<string, string>> townCorrections;
+
+
+		private readonly Dictionary<Tuple<string, string, int?, int, string>, Tuple<string, string, int?, int, string>> streetCorrections;
+
+
+		private readonly TownChecker townChecker;
+
+
+	}
+
+
+}
--- Epsitec.Cresus/Aider/Data/Eerv/EervParishDataLoader.cs
+++ Epsitec.Cresus/Aider/Data/Eerv/EervParishDataLoader.cs
@@ -28,9 +28,7 @@ namespace Epsitec.Aider.Data.Eerv
 
 		public EervParishDataLoader()
 		{
-			this.townCorrections = new Dictionary<Tuple<string, string>, Tuple<string, string>> ();
-			this.streetCorrections = new Dictionary<Tuple<string, string, int?, int, string>, Tuple<string, string, int?, int, string>> ();
-			this.townChecker = new TownChecker ();
+			this.addressChecker = new AddressChecker ();
 		}
 
 
@@ -93,7 +91,7 @@ namespace Epsitec.Aider.Data.Eerv
 				}
 			}
 
-			this.DisplayWarnings ();
+			this.addressChecker.DisplayWarnings ();
 		}
 
 
@@ -598,115 +596,11 @@ namespace Epsitec.Aider.Data.Eerv
 			// We check the addresses in Switzerland.
 			if (countryCode == "CH")
 			{
-				this.FixZipCodeAndTown (ref zipCode, ref town);
-				this.FixStreetName (ref firstAddressLine, ref streetName, houseNumber, ref zipCode, ref town);
+				this.addressChecker.FixZipCodeAndTown (ref zipCode, ref town);
+				this.addressChecker.FixStreetName (ref firstAddressLine, ref streetName, houseNumber, ref zipCode, ref town);
 			}
 
 			return new EervAddress (firstAddressLine, streetName, houseNumber, houseNumberComplement, zipCode, town, countryCode);
-		}
-
-
-		private void FixZipCodeAndTown(ref string zipCode, ref string town)
-		{
-			var result = this.townChecker.Validate (zipCode, town);
-			var newZipCode = result.Item1;
-			var newTown = result.Item2;
-
-			if (newZipCode != zipCode || newTown != town)
-			{
-				var key = Tuple.Create (zipCode, town);
-				var value = Tuple.Create (newZipCode, newTown);
-
-				this.townCorrections[key] = value;
-			}
-
-			zipCode = newZipCode;
-			town = newTown;
-		}
-
-
-		private void FixStreetName
-		(
-			ref string firstAddressLine,
-			ref string streetName,
-			int? houseNumber,
-			ref string zipCode,
-			ref string town
-		)
-		{
-			if (string.IsNullOrEmpty (town) ||  string.IsNullOrWhiteSpace (zipCode))
-			{
-				return;
-			}
-
-			var zipCodeInt = int.Parse (zipCode);
-			var zipCodeAddOn = 0;
-			var zipCodeId = 0;
-
-			var saveFirstAddressLine = firstAddressLine;
-			var saveStreetName = streetName;
-			var saveZipCodeInt = zipCodeInt;
-			var saveTown = town;
-
-			AddressPatchEngine.Current.FixAddress
-			(
-				ref firstAddressLine, ref streetName, houseNumber, ref zipCodeInt,
-				ref zipCodeAddOn, ref zipCodeId, ref town
-			);
-
-			var diff = firstAddressLine != saveFirstAddressLine
-						|| streetName  != saveStreetName
-						|| zipCodeInt != saveZipCodeInt
-						|| town != saveTown;
-
-			if (diff)
-			{
-				var key = Tuple.Create
-				(
-					saveFirstAddressLine, saveStreetName, houseNumber, saveZipCodeInt, saveTown
-				);
-
-				var value = Tuple.Create
-				(
-					firstAddressLine, streetName, houseNumber, zipCodeInt, town
-				);
-
-				this.streetCorrections[key] = value;
-			}
-
-			zipCode = InvariantConverter.ToString (zipCodeInt);
-		}
-
-
-		private void DisplayWarnings()
-		{
-			Console.WriteLine ("=================================================================");
-			Console.WriteLine ("Town corrections");
-			foreach (var townCorrection in this.townCorrections)
-			{
-				var key = townCorrection.Key;
-				var value = townCorrection.Value;
-
-				var message = key.Item1 + "; " + key.Item2
-						+ " => "
-						+ value.Item1 + "; " + value.Item2;
-
-				Console.WriteLine (message);
-			}
-
-			Console.WriteLine ("=================================================================");
-			Console.WriteLine ("Street corrections");
-			foreach (var streetCorrection in this.streetCorrections)
-			{
-				var key = streetCorrection.Key;
-				var value = streetCorrection.Value;
-
-				var message = StringUtils.Join ("; ", key.Item1, key.Item2, key.Item3, key.Item4, key.Item5)
-						+ " => "
-						+ StringUtils.Join ("; ", value.Item1, value.Item2, value.Item3, value.Item4, value.Item5);
-
-				Console.WriteLine (message);
-			}
 		}
 
 
@@ -922,13 +816,7 @@ namespace Epsitec.Aider.Data.Eerv
 		}
 
 
-		private readonly Dictionary<Tuple<string, string>, Tuple<string, string>> townCorrections;
-
-
-		private readonly Dictionary<Tuple<string, string, int?, int, string>, Tuple<string, string, int?, int, string>> streetCorrections;
-
-
-		private readonly TownChecker townChecker;
+		private readonly AddressChecker addressChecker;
 
 
 	}
