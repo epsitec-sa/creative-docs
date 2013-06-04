d5856159b328cfbcb917ca1707023b05ccc3ac37
Marc Bettex <bettex@epsitec.ch>
2013-06-04 09:25:44 +0000

    - Simplification du script de build, rendu possible par le nettoyage de la solution des derniers commits.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@20938 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/App.Aider/build.bat
+++ Epsitec.Cresus/App.Aider/build.bat
@@ -53,40 +53,7 @@ if %cleanup%==1 (
 
 @echo on
 
-@rem There are many solutions and configurations involved here. Below is the details about them.
-@rem - Epsitec.Cresus.sln, Release, Mixed Platforms
-@rem   That's the solution and the configuration that we want to build.
-@rem - Epsitec.Cresus.sln, Debug, Mixed Platforms
-@rem   We need the main solution built in debug mode because there is a link to a dll compiled in
-@rem   debug mode in the project App.CresusCore.
-@rem - Epsitec.ShellPE.sln, Release, ANY CPU
-@rem   This solution is referenced by Epsitec.Cresus.sln, Release, Mixed Platforms
-@rem - Epsitec.ShellPE.sln, Debug, ANY CPU
-@rem   This solution is referenced by Epsitec.Cresus.sln, Debug, Mixed Platforms
-@rem - Epsitec.ZipMe.sln, Release, ANY CPU
-@rem   This solution is referenced by Epsitec.ShellPE.sln, Release, ANY CPU
-@rem - Epsitec.ZipMe.sln, Debug, ANY CPU
-@rem   This solution is referenced by Epsitec.ShellPE.sln, Debug, ANY CPU
-@rem - Epsitec.SwissPost.Webservices.sln, Release, ANY CPU
-@rem   This solution is referenced by Epsitec.Cresus.sln, Release, Mixed Platforms
-@rem - Epsitec.SwissPost.Webservices.sln, Debug, ANY CPU
-@rem   This solution is referenced by Epsitec.Cresus.sln, Debug, Mixed Platforms
-
-@rem Build the solutions.
-msbuild /verbosity:minimal /property:Configuration=Debug;Platform="ANY CPU" /target:Build ..\..\Epsitec\dot.net\Epsitec.ZipMe\Epsitec.ZipMe.sln
-if %ERRORLEVEL% neq 0 exit /B 1
-msbuild /verbosity:minimal /property:Configuration=Release;Platform="ANY CPU" /target:Build ..\..\Epsitec\dot.net\Epsitec.ZipMe\Epsitec.ZipMe.sln
-if %ERRORLEVEL% neq 0 exit /B 1
-msbuild /verbosity:minimal /property:Configuration=Debug;Platform="ANY CPU" /target:Build ..\..\Epsitec\dot.net\Epsitec.ShellPE\Epsitec.ShellPE.sln
-if %ERRORLEVEL% neq 0 exit /B 1
-msbuild /verbosity:minimal /property:Configuration=Release;Platform="ANY CPU" /target:Build ..\..\Epsitec\dot.net\Epsitec.ShellPE\Epsitec.ShellPE.sln
-if %ERRORLEVEL% neq 0 exit /B 1
-msbuild /verbosity:minimal /property:Configuration=Debug;Platform="Mixed Platforms" /target:Build ..\..\Epsitec\dot.net\Epsitec.SwissPost\Epsitec.SwissPost.Webservices.sln
-if %ERRORLEVEL% neq 0 exit /B 1
-msbuild /verbosity:minimal /property:Configuration=Release;Platform="Mixed Platforms" /target:Build ..\..\Epsitec\dot.net\Epsitec.SwissPost\Epsitec.SwissPost.Webservices.sln
-if %ERRORLEVEL% neq 0 exit /B 1
-msbuild /verbosity:minimal /property:Configuration=Debug;Platform="x86" /target:Build ..\Epsitec.Cresus.sln
-if %ERRORLEVEL% neq 0 exit /B 1
+@rem Build the solution.
 msbuild /verbosity:minimal /property:Configuration=Release;Platform="x86" /target:Build ..\Epsitec.Cresus.sln
 if %ERRORLEVEL% neq 0 exit /B 1
 
