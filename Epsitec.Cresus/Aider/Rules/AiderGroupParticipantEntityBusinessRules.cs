2efad32f656fa6fb5e2e2d15ef68eb2e44aa2893
Marc Bettex <bettex@epsitec.ch>
2013-06-20 13:03:12 +0000

    - Lorsqu'on bind une AiderGroupParticipantEntity, il faut aussi binder le contact associé, de manière à exécuter ses business rules.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@21042 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/Rules/AiderGroupParticipantEntityBusinessRules.cs
@@ -0,0 +1,22 @@
+﻿using Epsitec.Aider.Entities;
+
+using Epsitec.Cresus.Core.Business;
+using Epsitec.Cresus.Core.Entities;
+
+namespace Epsitec.Aider.Rules
+{
+	[BusinessRule]
+	internal class AiderGroupParticipantEntityBusinessRules : GenericBusinessRule<AiderGroupParticipantEntity>
+	{
+		public override void ApplyBindRule(AiderGroupParticipantEntity entity)
+		{
+			var businessContext = this.GetBusinessContext ();
+
+			if (entity.Contact.IsNotNull ())
+			{
+				businessContext.Register (entity.Contact);
+			}
+		}
+
+	}
+}
