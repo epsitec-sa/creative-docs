dde6f19a6b5f217f8ca200c7992b0c31088d2b46
Marc Bettex <bettex@epsitec.ch>
2013-06-24 09:33:52 +0000

    - L'édition de l'adresse d'une personne morale déclence l'exécution des business rules. - Corrige le bug #2552.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@21049 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/Controllers/EditionControllers/EditionAiderLegalPersonViewController1Address.cs
@@ -0,0 +1,43 @@
+﻿using Epsitec.Aider.Entities;
+
+using Epsitec.Common.Support;
+
+using Epsitec.Cresus.Bricks;
+
+using Epsitec.Cresus.Core.Controllers;
+using Epsitec.Cresus.Core.Controllers.EditionControllers;
+using Epsitec.Cresus.Core.Business.UserManagement;
+
+namespace Epsitec.Aider.Controllers.EditionControllers
+{
+	[ControllerSubType (1)]
+	public sealed class EditionAiderLegalPersonViewController1Address : EditionViewController<AiderLegalPersonEntity>
+	{
+		protected override void CreateBricks(BrickWall<AiderLegalPersonEntity> wall)
+		{
+			var currentUser = UserManager.Current.AuthenticatedUser;
+			var favorites = AiderTownEntity.GetTownFavoritesByUserScope (this.BusinessContext, currentUser as AiderUserEntity);
+
+			wall.AddBrick ()
+				.Title (Resources.Text ("Adresse de base"))
+				.Icon ("Data.AiderAddress")
+				.Input ()
+					.Field (x => x.Address.Town).WithFavorites (favorites)
+					.Field (x => x.Address.AddressLine1)
+					.Field (x => x.Address.StreetHouseNumberAndComplement)
+					.Field (x => x.Address.PostBox)
+				.End ()
+				.Input ()
+					.Field (x => x.Address.Phone1)
+					.Field (x => x.Address.Phone2)
+					.Field (x => x.Address.Mobile)
+					.Field (x => x.Address.Fax)
+					.Field (x => x.Address.Email)
+					.Field (x => x.Address.Web)
+				.End ()
+				.Input ()
+					.Field (x => x.Address.Comment.Text)
+				.End ();
+		}
+	}
+}
--- Epsitec.Cresus/Aider/Controllers/SummaryControllers/SummaryAiderLegalPersonViewController.cs
+++ Epsitec.Cresus/Aider/Controllers/SummaryControllers/SummaryAiderLegalPersonViewController.cs
@@ -1,6 +1,7 @@
 //	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
+using Epsitec.Aider.Controllers.EditionControllers;
 using Epsitec.Aider.Entities;
 
 using Epsitec.Cresus.Core.Bricks;
@@ -17,9 +18,11 @@ namespace Epsitec.Aider.Controllers.SummaryControllers
 		protected override void CreateBricks(Cresus.Bricks.BrickWall<AiderLegalPersonEntity> wall)
 		{
 			wall.AddBrick ();
-			
-			wall.AddBrick (x => x.Address)
-				.Title ("Adresse de base");
+
+			wall.AddBrick ()
+				.Title ("Adresse de base")
+				.Icon ("Data.AiderAddress")
+				.WithSpecialController (typeof (EditionAiderLegalPersonViewController1Address));
 
 			var contacts = this.Entity.Contacts;
 
