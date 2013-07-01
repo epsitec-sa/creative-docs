8f214afae6b61ad6a9014a6c952842e5378fb444
Marc Bettex <bettex@epsitec.ch>
2013-07-01 14:30:26 +0000

    - Ajouté l'export d'étiquettes pour les ménages.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@21094 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/Labels/AiderHouseholdLabelTextFactory0.cs
@@ -0,0 +1,29 @@
+﻿using Epsitec.Aider.Entities;
+using Epsitec.Aider.Enumerations;
+
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Core.Labels;
+
+using System;
+
+
+namespace Epsitec.Aider.Labels
+{
+
+
+	[LabelTextFactoryId (0)]
+	public sealed class AiderHouseholdLabelTextFactory0 : LabelTextFactory<AiderHouseholdEntity>
+	{
+
+
+		public override FormattedText GetLabelText(AiderHouseholdEntity entity)
+		{
+			return entity.GetAddressLabelText ();
+		}
+
+
+	}
+
+
+}
