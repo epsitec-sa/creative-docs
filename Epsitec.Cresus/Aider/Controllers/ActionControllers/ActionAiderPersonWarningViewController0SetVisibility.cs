5da79af662c33377456280a278b5598ba540204f
Samuel Loup <loup@epsitec.ch>
2013-07-15 12:24:20 +0000

    - Aider: Ajout et modification de controlleurs pour les actions sur les warnings
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@21189 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/Controllers/ActionControllers/ActionAiderPersonWarningViewController0SetVisibility.cs
@@ -0,0 +1,65 @@
+//	Copyright © 2012-2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Marc BETTEX, Maintainer: Pierre ARNAUD
+
+using Epsitec.Aider.Entities;
+
+using Epsitec.Common.Support;
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Bricks;
+
+using Epsitec.Cresus.Core.Business;
+using Epsitec.Cresus.Core.Controllers;
+using Epsitec.Cresus.Core.Controllers.ActionControllers;
+using Epsitec.Cresus.Core.Entities;
+
+using System.Collections.Generic;
+using System.Linq;
+using Epsitec.Aider.Enumerations;
+
+namespace Epsitec.Aider.Controllers.ActionControllers
+{
+	[ControllerSubType (0)]
+    public sealed class ActionAiderPersonWarningViewController0SetVisibility : ActionViewController<AiderPersonWarningEntity>
+	{
+		public override FormattedText GetTitle()
+		{
+			return Resources.FormattedText ("Changer la visibilité");
+		}
+
+		public override ActionExecutor GetExecutor()
+		{
+            return ActionExecutor.Create<bool,bool>(this.Execute);
+		}
+
+		private void Execute(bool setInvisible,bool isDecease)
+		{
+            if (isDecease)
+            {
+                this.Entity.Person.Visibility = PersonVisibilityStatus.Deceased;
+                this.Entity.Person.ProcessPersonDeath();
+            }
+            if (setInvisible)
+            {
+                this.Entity.Person.Visibility = PersonVisibilityStatus.Hidden;
+            }
+
+            this.Entity.Person.RemoveWarningInternal(this.Entity);
+		}
+
+        protected override void GetForm(ActionBrick<AiderPersonWarningEntity, SimpleBrick<AiderPersonWarningEntity>> form)
+        {
+            form
+                .Title(this.GetTitle())
+                .Field<bool>()
+                    .Title("Je veux rendre invisible cette personne")
+                    .InitialValue(false)
+                .End()
+                .Field<bool>()
+                    .Title("Cette personne est décédée")
+                    .InitialValue(false)
+                .End()
+            .End();
+        }
+	}
+}
--- Epsitec.Cresus/Aider/Controllers/SummaryControllers/SummaryAiderPersonWarningViewController.cs
+++ Epsitec.Cresus/Aider/Controllers/SummaryControllers/SummaryAiderPersonWarningViewController.cs
@@ -1,13 +1,31 @@
 //	Copyright © 2012, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
+using Epsitec.Aider.Controllers.ActionControllers;
 using Epsitec.Aider.Entities;
-
+using Epsitec.Aider.Enumerations;
+using Epsitec.Cresus.Bricks;
 using Epsitec.Cresus.Core.Controllers.SummaryControllers;
 
 namespace Epsitec.Aider.Controllers.SummaryControllers
 {
 	public sealed class SummaryAiderPersonWarningViewController : SummaryViewController<AiderPersonWarningEntity>
 	{
+        protected override void CreateBricks(BrickWall<AiderPersonWarningEntity> wall)
+        {
+
+            switch (this.Entity.WarningType)
+            {
+                case WarningType.MissingECh: 
+                    wall.AddBrick()
+                        .EnableAction<ActionAiderPersonWarningViewController0SetVisibility>();
+                    break;
+
+                default :
+                    wall.AddBrick();
+                    break;
+            }
+              
+        }
 	}
 }
