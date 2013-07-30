5f1430904aed4c449aa4d8544dd15c4397b6d055
Marc Bettex <bettex@epsitec.ch>
2013-07-30 07:32:50 +0000

    - Changé un commentaire qui n'était pas très claire.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@21264 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Client/WebCore/js/plugins/AiderGroupReader.js
+++ Epsitec.Cresus/Cresus.WebCore.Client/WebCore/js/plugins/AiderGroupReader.js
@@ -2,10 +2,33 @@
 // window.
 
 // We need this subclass in order to override the readRecords method. The
-// problem is that the same setting is used to configure where the tree store
-// should look for children within a node and for nodes within the result of a
-// request. So here we change the location of the nodes within the result of the
-// request to have them at the same location as the children within a node.
+// problem is that the path from the root to the top level nodes should be the
+// same as the path from within a node to its sub nodes. This is a limitation in
+// the json reader of Ext JS.
+// The server gives us back an object like :
+// {
+//   success: ...
+//   content: {
+//     data: {
+//       groups: [{
+//         id: ...,
+//         summary: ...,
+//         groups: [...]
+//       }
+//     }]
+//   }
+// }
+// We transform it in something like :
+// {
+//   success: ...
+//   groups: [{
+//     id: ...,
+//     summary: ...,
+//     groups: [...]
+//   }
+// }]
+// So from the root, we can get to the top level nodes with root.groups and from
+// a node, we can get its sub nodes with node.groups, wich is the same path.
 
 Ext.define('Epsitec.cresus.webcore.plugins.AiderGroupReader', {
   extend: 'Ext.data.reader.Json',
