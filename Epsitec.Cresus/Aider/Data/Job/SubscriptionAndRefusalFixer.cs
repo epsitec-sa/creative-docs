9598343d7a02f4083145f74dbf64272df9729f0a
Marc Bettex <bettex@epsitec.ch>
2013-08-06 14:41:58 +0000

    - Ajouté un job pour supprimer les abonnements et les refus vides, qui ne sont plus liés à un ménage ou à une personne morale.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@21314 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/Data/Job/SubscriptionAndRefusalFixer.cs
@@ -0,0 +1,69 @@
+﻿using Epsitec.Aider.Entities;
+
+using Epsitec.Cresus.Core;
+using Epsitec.Cresus.Core.Business;
+using Epsitec.Cresus.Core.Entities;
+
+using Epsitec.Cresus.DataLayer.Loader;
+
+using System;
+
+
+namespace Epsitec.Aider.Data.Job
+{
+
+	
+	/// <summary>
+	/// This fixer removes the subscriptions and the subscription refusals that are empty, i.e.
+	/// that don't target either an household or a legal person contact. These have appeared
+	/// because for a while, the deletion of an household of of a legal person did not delete the
+	/// subscriptions or subscription refusals.
+	/// </summary>
+	public static class SubscriptionAndRefusalFixer
+	{
+
+
+		public static void FixEmptySubscriptions(CoreData coreData)
+		{
+			using (var businessContext = new BusinessContext (coreData, false))
+			{
+				var example = new AiderSubscriptionEntity ();
+				var request = Request.Create (example);
+				request.AddCondition (businessContext.DataContext, example, e => e.Household == null && e.LegalPersonContact == null);
+
+				var emptySubscriptions = businessContext.DataContext.GetByRequest (request);
+
+				foreach (var subscription in emptySubscriptions)
+				{
+					businessContext.DeleteEntity (subscription);
+				}
+
+				businessContext.SaveChanges (LockingPolicy.ReleaseLock, EntitySaveMode.None);
+			}
+		}
+
+
+		public static void FixEmptyRefusals(CoreData coreData)
+		{
+			using (var businessContext = new BusinessContext (coreData, false))
+			{
+				var example = new AiderSubscriptionRefusalEntity ();
+				var request = Request.Create (example);
+				request.AddCondition (businessContext.DataContext, example, e => e.Household == null && e.LegalPersonContact == null);
+
+				var emptyRefusals = businessContext.DataContext.GetByRequest (request);
+
+				foreach (var refusal in emptyRefusals)
+				{
+					businessContext.DeleteEntity (refusal);
+				}
+
+				businessContext.SaveChanges (LockingPolicy.ReleaseLock, EntitySaveMode.None);
+			}
+		}
+
+
+	}
+
+
+}
