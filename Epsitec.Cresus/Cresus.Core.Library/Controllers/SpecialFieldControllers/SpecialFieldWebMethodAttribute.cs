b95aaf5c57fd105db84d7cc2699ac3ee5c64db01
Marc Bettex <bettex@epsitec.ch>
2013-08-13 09:23:17 +0000

    - Ajouté un commentaire.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@21334 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core.Library/Controllers/SpecialFieldControllers/SpecialFieldWebMethodAttribute.cs
+++ Epsitec.Cresus/Cresus.Core.Library/Controllers/SpecialFieldControllers/SpecialFieldWebMethodAttribute.cs
@@ -2,6 +2,10 @@
 
 namespace Epsitec.Cresus.Core.Controllers.SpecialFieldControllers
 {
+	/// <summary>
+	/// This attribute is used by the special field controller to annotate methods that can be
+	/// called by the client to obtain data used for the field.
+	/// </summary>
 	[AttributeUsage(AttributeTargets.Method)]
 	public sealed class SpecialFieldWebMethodAttribute : Attribute
 	{
