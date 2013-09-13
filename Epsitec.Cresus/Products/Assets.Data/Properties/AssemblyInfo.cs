895a7409e6ae0cdd4c8b700416610379f375325a
Pierre Arnaud <arnaud@opac.ch>
2013-09-13 11:40:16 +0000

    - Solution: ajouté Products.Assets.Data. - Assets: ajouté les ressources (vides) dans chaque projet.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@21462 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Products/Assets.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿//	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Assets.Data")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Assets.Data")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+
+[assembly: Guid("6d2f8d0e-0d64-4513-86c2-530f2dc7a78a")]
