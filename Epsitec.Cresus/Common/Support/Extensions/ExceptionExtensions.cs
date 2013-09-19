b00082c8e2233eb2d65824871a1ebd389f68865b
Pierre Arnaud <arnaud@opac.ch>
2013-09-19 07:16:02 +0000

    - Common: corrigé ExceptionExtensions.GetFullText pour avoir un affichage correct dans tous les cas; supprimé l'affichage redondant d'InnerException et AggregageException.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@21500 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Common/Support/Extensions/ExceptionExtensions.cs
+++ Epsitec.Cresus/Common/Support/Extensions/ExceptionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿//	Copyright © 2012-2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Marc BETTEX, Maintainer: Pierre ARNAUD
 
+using System.Collections.Generic;
 namespace Epsitec.Common.Support.Extensions
 {
 	public static class ExceptionExtensions
@@ -12,31 +13,35 @@ namespace Epsitec.Common.Support.Extensions
 		/// <returns>The full text.</returns>
 		public static string GetFullText(this System.Exception exception)
 		{
+			var set    = new HashSet<System.Exception> ();
 			var buffer = new System.Text.StringBuilder ();
 			var title  = "Exception:";
 
 			for (var e = exception; e != null; e = e.InnerException)
 			{
-				ExceptionExtensions.DumpException (buffer, "", title, e);
+				if (set.Add (e))
+				{
+					ExceptionExtensions.DumpException (buffer, "", title, e, set);
+				}
 				title = "Inner exception:";
 			}
 
 			return buffer.ToString ();
 		}
 
-		private static void DumpException(System.Text.StringBuilder buffer, string prefix, string title, System.Exception exception)
+		private static void DumpException(System.Text.StringBuilder buffer, string prefix, string title, System.Exception exception, HashSet<System.Exception> set)
 		{
 			var format = "{0}\n"
-					   + "  Type: {0}\n"
-					   + "  Message: {1}\n"
-					   + "  Source: {2}\n"
+					   + "  Type: {1}\n"
+					   + "  Message: {2}\n"
+					   + "  Source: {3}\n"
 					   + "  Stack trace:\n"
-					   + "    {3}";
+					   + " {4}";
 
 			var type    = exception.GetType ();
-			var message = exception.Message;
-			var source  = exception.Source;
-			var trace   = exception.StackTrace == null ? "<none>" : exception.StackTrace.Replace ("\n", "\n    ");
+			var message = exception.Message ?? "<none>";
+			var source  = exception.Source ?? "<none>";
+			var trace   = exception.StackTrace == null ? "   <none>" : exception.StackTrace.Replace ("\n", "\n ");
 
 			var exceptionText = prefix + string.Format (format, title, type, message, source, trace).Replace ("\n", "\n" + prefix);
 
@@ -48,7 +53,10 @@ namespace Epsitec.Common.Support.Extensions
 			{
 				foreach (var ex in aggregate.InnerExceptions)
 				{
-					ExceptionExtensions.DumpException (buffer, prefix + "  ", "Aggregate inner exception:", ex);
+					if (set.Add (ex))
+					{
+						ExceptionExtensions.DumpException (buffer, prefix + "  ", "Aggregate inner exception:", ex, set);
+					}
 				}
 			}
 		}
