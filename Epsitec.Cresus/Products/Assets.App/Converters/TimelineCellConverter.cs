9ca9141f2ecedeaeb75cfcd0a716cf30b89e67eb
Pierre Arnaud <arnaud@opac.ch>
2013-09-19 08:47:53 +0000

    - Assets: ébauche de convertisseur d'une représentation métier vers un TimelineCell.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@21513 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Products/Assets.App/Converters/TimelineCellConverter.cs
@@ -0,0 +1,18 @@
+﻿//	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Cresus.Assets.Core.Business;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Assets.App.Converters
+{
+	public sealed class TimelineCellConverter
+	{
+		public Widgets.TimelineCell Convert(TimelineEventCell item)
+		{
+			return new Widgets.TimelineCell (item.Date, Widgets.TimelineCellGlyph.FilledCircle);
+		}
+	}
+}
---
+++ Epsitec.Cresus/Products/Assets.Core/Business/TimelineEventCell.cs
@@ -0,0 +1,25 @@
+﻿//	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.Types;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.Assets.Core.Business
+{
+	public class TimelineEventCell
+	{
+		public TimelineEventCell()
+		{
+
+		}
+
+
+		public Date Date
+		{
+			get;
+			set;
+		}
+	}
+}
