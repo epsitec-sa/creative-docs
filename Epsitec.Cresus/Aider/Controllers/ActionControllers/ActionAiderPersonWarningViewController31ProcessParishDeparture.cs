2bb7d631032195896eb85f83cc4ac10ce02a6782
Pierre Arnaud <arnaud@opac.ch>
2013-09-24 06:33:17 +0000

    - Aider: modifié l'intitulé du bouton.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@21572 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/Controllers/ActionControllers/ActionAiderPersonWarningViewController31ProcessParishDeparture.cs
+++ Epsitec.Cresus/Aider/Controllers/ActionControllers/ActionAiderPersonWarningViewController31ProcessParishDeparture.cs
@@ -25,7 +25,7 @@ namespace Epsitec.Aider.Controllers.ActionControllers
 	{
 		public override FormattedText GetTitle()
 		{
-			return Resources.FormattedText ("Marquer comme lu (tout le ménage)");
+			return Resources.FormattedText ("Marquer comme lu<br/>pour tout le ménage");
 		}
 
 		protected override void Execute()
