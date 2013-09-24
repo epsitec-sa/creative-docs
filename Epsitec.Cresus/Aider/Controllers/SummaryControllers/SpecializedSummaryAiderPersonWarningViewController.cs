064ea071080e1341fa1198a9c7cc8be3977dee3c
Pierre Arnaud <arnaud@opac.ch>
2013-09-24 05:26:49 +0000

    - Aider: ajout d'un premier contrôleur spécifique pour découper plus proprement le monstre qu'est devenu le Summary ViewController de AiderPersonWarning.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@21569 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/Controllers/SummaryControllers/SpecializedSummaryAiderPersonWarningViewController.cs
@@ -0,0 +1,14 @@
+//	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Aider.Entities;
+
+using Epsitec.Cresus.Core.Controllers.SummaryControllers;
+
+namespace Epsitec.Aider.Controllers.SummaryControllers
+{
+	public abstract class SpecializedSummaryAiderPersonWarningViewController : SummaryViewController<AiderPersonWarningEntity>
+	{
+	}
+}
+
