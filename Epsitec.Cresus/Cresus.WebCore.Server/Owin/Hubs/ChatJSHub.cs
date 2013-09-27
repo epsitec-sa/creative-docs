06fafb1bd1b1e93d5611c2c0563a8ac56b1c2be2
Pierre Arnaud <arnaud@opac.ch>
2013-09-27 03:58:16 +0000

    - WebCore: refactoring du ChatJSHub et Gravatar.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@21588 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Server/Owin/Hubs/ChatJSHub.cs
+++ Epsitec.Cresus/Cresus.WebCore.Server/Owin/Hubs/ChatJSHub.cs
@@ -1,66 +1,79 @@
-﻿using System;
-using System.Text;
-using System.Web;
+﻿//	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Samuel LOUP, Maintainer: Samuel LOUP
+
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNet.SignalR;
-using Microsoft.AspNet.SignalR.Hubs;
-using System.Security.Cryptography;
 
 namespace Epsitec.Cresus.WebCore.Server.Owin.Hubs
 {
-    public class ChatJSHub : Hub
-    {
-        public const string ROOM_ID_STUB = "aider-room";
+	public class ChatJSHub : Hub
+	{
+		public static List<ChatUser> Users
+		{
+			get
+			{
+				return ChatJSHub.users;
+			}
+		}
 
-		public static List<ChatUser> Users = new List<ChatUser> ();
-		public static List<ChatMessage> Messages = new List<ChatMessage> ();
-
-
+		public static List<ChatMessage> Messages
+		{
+			get
+			{
+				return ChatJSHub.messages;
+			}
+		}
+		
 		public ChatUser GetUserInfo(string connectionId)
-        {
-			return ChatJSHub.Users.FirstOrDefault (u => u.Id == connectionId);
-        }
+		{
+			lock (ChatJSHub.Users)
+			{
+				return ChatJSHub.Users.FirstOrDefault (u => u.Id == connectionId);
+			}
+		}
 
 		public void UpdateMyInfo(string username,string email)
 		{
-			lock(ChatJSHub.Users)
+			lock (ChatJSHub.Users)
 			{
 				var toUpdate = ChatJSHub.Users.FirstOrDefault (u => u.Id == this.Context.ConnectionId);
 				toUpdate.Name = username;
-				toUpdate.ProfilePictureUrl = GravatarHelper.GetGravatarUrl (GravatarHelper.GetGravatarHash (email), GravatarHelper.Size.s32);
+				toUpdate.ProfilePictureUrl = GravatarHelper.GetGravatarUrl (GravatarHelper.GetGravatarHash (email), GravatarHelper.GravatarSize.s32);
 			}
 			
 			this.BroadcastUsersList();
 		}
 
-        private void BroadcastUsersList()
-        {
+		private void BroadcastUsersList()
+		{
 			this.Clients.All.usersListChanged (ChatJSHub.Users);
-        }
+		}
 
-        public List<ChatMessage> GetMessageHistory(string otherConnectionId)
-        {
+		public List<ChatMessage> GetMessageHistory(string otherConnectionId)
+		{
 			var messages = ChatJSHub.Messages
-                               .Where(
-                                   m =>
-                                   (m.UserTo== this.GetUserInfo(this.Context.ConnectionId) && m.UserFrom == this.GetUserInfo(otherConnectionId)) ||
-                                   (m.UserTo == this.GetUserInfo(otherConnectionId) && m.UserFrom == this.GetUserInfo(this.Context.ConnectionId)))
-                               .OrderByDescending(m => m.Timestamp).Take(30).ToList();
+							   .Where(
+								   m =>
+								   (m.UserTo== this.GetUserInfo(this.Context.ConnectionId) && m.UserFrom == this.GetUserInfo(otherConnectionId)) ||
+								   (m.UserTo == this.GetUserInfo(otherConnectionId) && m.UserFrom == this.GetUserInfo(this.Context.ConnectionId)))
+							   .OrderByDescending(m => m.Timestamp).Take(30).ToList();
 			return messages;
-        }
+		}
 
 		public void SendMessage(string otherConnectionId, string message, string clientGuid)
-        {
+		{
 			var chatMessage = new ChatMessage()
-            {
-                Message = message,
-                UserFrom = this.GetUserInfo(this.Context.ConnectionId),
+			{
+				Message = message,
+				UserFrom = this.GetUserInfo(this.Context.ConnectionId),
 				UserTo = this.GetUserInfo (otherConnectionId),
-                ClientGuid = clientGuid
-            };
+				ClientGuid = clientGuid
+			};
 
 			lock (ChatJSHub.Messages)
 			{
@@ -69,15 +82,15 @@ namespace Epsitec.Cresus.WebCore.Server.Owin.Hubs
 			
 
 			this.Clients.Client (otherConnectionId).sendMessage (chatMessage);
-        }
+		}
 
 		public void SendTypingSignal(string otherConnectionId)
-        {
+		{
 			this.Clients.Client (otherConnectionId).sendTypingSignal (this.GetUserInfo (this.Context.ConnectionId));
-        }
+		}
 
-        public override Task OnConnected()
-        {
+		public override Task OnConnected()
+		{
 			lock (ChatJSHub.Users)
 			{
 				ChatJSHub.Users.Add (new ChatUser ()
@@ -85,150 +98,120 @@ namespace Epsitec.Cresus.WebCore.Server.Owin.Hubs
 					Id = this.Context.ConnectionId,
 					Name = this.Context.ConnectionId,
 					Status = ChatUser.StatusType.Online,
-					ProfilePictureUrl = GravatarHelper.GetGravatarUrl (GravatarHelper.GetGravatarHash (""), GravatarHelper.Size.s32)
+					ProfilePictureUrl = GravatarHelper.GetGravatarUrl (GravatarHelper.GetGravatarHash (""), GravatarHelper.GravatarSize.s32)
 				});
 			}
 	
-            this.BroadcastUsersList();
+			this.BroadcastUsersList();
 
-            return base.OnConnected();
-        }
+			return base.OnConnected();
+		}
 
-        public override Task OnDisconnected()
-        {
+		public override Task OnDisconnected()
+		{
 			lock (ChatJSHub.Users)
 			{
 				ChatJSHub.Users.RemoveAll (u => u.Id == this.Context.ConnectionId);
 			}
-            this.BroadcastUsersList();
-            return base.OnDisconnected();
-        }
+			this.BroadcastUsersList();
+			return base.OnDisconnected();
+		}
 
-        public override Task OnReconnected()
-        {
+		public override Task OnReconnected()
+		{
 			lock (ChatJSHub.Users)
 			{
 				ChatJSHub.Users.RemoveAll (u => u.Id == this.Context.ConnectionId);
 			}
-            this.BroadcastUsersList();
-            return base.OnReconnected();
-        }
-    }
+			this.BroadcastUsersList();
+			return base.OnReconnected();
+		}
 
-    public class ChatMessage
-    {
+		public const string ROOM_ID_STUB = "aider-room";
 
-        public ChatUser UserFrom { get; set; }
+		private static readonly List<ChatUser>		users		= new List<ChatUser> ();
+		private static readonly List<ChatMessage>	messages	= new List<ChatMessage> ();
+	}
 
-        public ChatUser UserTo { get; set; }
+	public class ChatMessage
+	{
 
-        public long Timestamp { get; set; }
+		public ChatUser UserFrom { get; set; }
 
-        public string Message { get; set; }
+		public ChatUser UserTo { get; set; }
 
-        /// <summary>
-        /// Client GUID
-        /// </summary>
-        /// <remarks>
-        /// Every time a message is sent from the client, the client must specify an unique message client GUID. This is
-        /// because when you send a message to the server, the message comes back to the client. This is useful for 2 reasons:
-        /// 1) It allows the client to know that probably the other user received the message
-        /// 2) It allows for different browser windows to be synchronized
-        /// </remarks>
-        public string ClientGuid { get; set; }
-    }
+		public long Timestamp { get; set; }
 
-    public class ChatUser
-    {
-        public enum StatusType
-        {
-            Offline = 0,
-            Online = 1
-        }
+		public string Message { get; set; }
 
-        public ChatUser()
-        {
-            this.Status = StatusType.Offline;
-        }
+		/// <summary>
+		/// Client GUID
+		/// </summary>
+		/// <remarks>
+		/// Every time a message is sent from the client, the client must specify an unique message client GUID. This is
+		/// because when you send a message to the server, the message comes back to the client. This is useful for 2 reasons:
+		/// 1) It allows the client to know that probably the other user received the message
+		/// 2) It allows for different browser windows to be synchronized
+		/// </remarks>
+		public string ClientGuid { get; set; }
+	}
 
-        public string Id { get; set; }
+	public class ChatUser
+	{
+		public enum StatusType
+		{
+			Offline = 0,
+			Online = 1
+		}
 
-        public string Name { get; set; }
+		public ChatUser()
+		{
+			this.Status = StatusType.Offline;
+		}
 
-        public string Url { get; set; }
+		public string Id { get; set; }
 
-        public string ProfilePictureUrl { get; set; }
+		public string Name { get; set; }
 
-        public StatusType Status { get; set; }
-    }
+		public string Url { get; set; }
 
-    public class GravatarHelper
-    {
-        public const string Ampersand = "&";
-        public const string BadgeSymbol = "&#9679;";
+		public string ProfilePictureUrl { get; set; }
 
-        public static String GetGravatarUrl(String gravatarEMailHash, Size size)
-        {
-            string sizeAsString;
-            // this code CAN BE BETTER. I'm jot not feeling like fixing it right now
-            switch (size)
-            {
-                case Size.s16:
-                    sizeAsString = "16";
-                    break;
-                case Size.s24:
-                    sizeAsString = "24";
-                    break;
-                case Size.s32:
-                    sizeAsString = "32";
-                    break;
-                case Size.s64:
-                    sizeAsString = "64";
-                    break;
-                case Size.s128:
-                    sizeAsString = "128";
-                    break;
-                default:
-                    throw new Exception("Size not supported");
-            }
+		public StatusType Status { get; set; }
+	}
 
-            return "https://www.gravatar.com/avatar/" + gravatarEMailHash + "?s=" + sizeAsString + GravatarHelper.Ampersand + "r=PG&d=mm";
-        }
+	public class GravatarHelper
+	{
+		public static string GetGravatarUrl(string gravatarEMailHash, GravatarSize size)
+		{
+			string sizeAsString = new string (size.ToString ().Where (x => char.IsDigit (x)).ToArray ());
+			
+			return "https://www.gravatar.com/avatar/" + gravatarEMailHash + "?s=" + sizeAsString + GravatarHelper.Ampersand + "r=PG&d=mm";
+		}
 
-        // Create an md5 sum string of this string
-        public static string GetGravatarHash(string email)
-        {
-            if (String.IsNullOrEmpty(email))
-                email = "meu@email.com";
+		public static string GetGravatarHash(string email)
+		{
+			var md5Hasher  = System.Security.Cryptography.MD5.Create ();
+			var emailBytes = System.Text.Encoding.Default.GetBytes (email ?? GravatarHelper.DefaultEmail);
+			var emailHash  = md5Hasher.ComputeHash (emailBytes);
 
-            // Create a new instance of the MD5CryptoServiceProvider object.
-            var md5Hasher = MD5.Create();
+			return string.Join ("", emailHash.Select (x => x.ToString ("x2")));
+		}
 
-            // Convert the input string to a byte array and compute the hash.
-            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(email));
+		public const string DefaultEmail = "meu@email.com";
+		public const string Ampersand = "&";
+		public const string BadgeSymbol = "&#9679;";
 
-            // Create a new Stringbuilder to collect the bytes
-            // and create a string.
-            var sBuilder = new StringBuilder();
-
-            // Loop through each byte of the hashed data
-            // and format each one as a hexadecimal string.
-            for (var i = 0; i < data.Length; i++)
-                sBuilder.Append(data[i].ToString("x2"));
-
-            return sBuilder.ToString();  // Return the hexadecimal string.
-        }
-
-        /// <summary>
-        /// Gravatar image size
-        /// </summary>
-        public enum Size
-        {
-            s16,
-            s24,
-            s32,
-            s64,
-            s128
-        }
-    }
+		/// <summary>
+		/// Gravatar image size
+		/// </summary>
+		public enum GravatarSize
+		{
+			s16,
+			s24,
+			s32,
+			s64,
+			s128,
+		}
+	}
 }
