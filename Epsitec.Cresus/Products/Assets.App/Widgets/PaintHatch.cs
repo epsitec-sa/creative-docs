e2e186eecffd6c2bdb2b913b3566a06a4366f4db
Daniel Roux <roux@epsitec.ch>
2013-11-05 07:40:01 +0000

    - Assets: Extraction de l'algorithme de dessin des hachures dans une classe statique.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@22065 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Products/Assets.App/Widgets/PaintHatch.cs
@@ -0,0 +1,58 @@
+﻿//	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using System.Collections.Generic;
+using System.Linq;
+
+using Epsitec.Common.Drawing;
+
+namespace Epsitec.Cresus.Assets.App.Widgets
+{
+	public static class PaintHatch
+	{
+		public static void Paint(Graphics graphics, Rectangle rect)
+		{
+			//	Dessine des hachures à 45 degrés dans un rectangle. On s'arrange pour
+			//	qu'elles soient forcément jointives avec celle du rectangle suivant.
+			double d = rect.Height/3.0;  // distance entre 2 hachures
+
+			for (double y=d; y<=rect.Height; y+=d)
+			{
+				var start = new Point (rect.Left, rect.Top-y);
+				var end = PaintHatch.GetHatchEnd (start, rect);
+				graphics.AddLine (start, end);
+			}
+
+			for (double x=d; x<=rect.Width; x+=d)
+			{
+				var start = new Point (rect.Left+x, rect.Bottom);
+				var end = PaintHatch.GetHatchEnd (start, rect);
+				graphics.AddLine (start, end);
+			}
+
+			graphics.RenderSolid (Color.FromAlphaColor (0.5, ColorManager.GlyphColor));
+		}
+
+		private static Point GetHatchEnd(Point start, Rectangle rect)
+		{
+			//	Calcule le point d'arrivée de la jointure.
+			double x = start.X + rect.Width;
+			double y = start.Y + rect.Width;  // juste, mais trop loin
+
+			//	Effectue un clipping dans le rectangle.
+			if (x > rect.Right)
+			{
+				y -= x - rect.Right + 0.5;
+				x -= x - rect.Right + 0.5;
+			}
+
+			if (y > rect.Top)
+			{
+				x -= y - rect.Top + 0.5;
+				y -= y - rect.Top + 0.5;
+			}
+
+			return new Point (x, y);
+		}
+	}
+}
--- Epsitec.Cresus/Products/Assets.App/Widgets/TimelineRowGlyphs.cs
+++ Epsitec.Cresus/Products/Assets.App/Widgets/TimelineRowGlyphs.cs
@@ -76,7 +76,7 @@ namespace Epsitec.Cresus.Assets.App.Widgets
 
 			if (isLocked)
 			{
-				TimelineRowGlyphs.PaintHash (graphics, rect);
+				PaintHatch.Paint (graphics, rect);
 			}
 		}
 
@@ -84,54 +84,6 @@ namespace Epsitec.Cresus.Assets.App.Widgets
 		{
 			EventGlyph.Paint (graphics, rect, cell.Glyph);
 		}
-
-
-		#region Hash painting
-		private static void PaintHash(Graphics graphics, Rectangle rect)
-		{
-			//	Dessine des hachures à 45 degrés dans un rectangle. On s'arrange pour
-			//	qu'elles soient forcément jointives avec celle du rectangle suivant.
-			double d = rect.Height/3.0;  // distance entre 2 hachures
-
-			for (double y=d; y<=rect.Height; y+=d)
-			{
-				var start = new Point (rect.Left, rect.Top-y);
-				var end = TimelineRowGlyphs.GetHashEnd (start, rect);
-				graphics.AddLine (start, end);
-			}
-
-			for (double x=d; x<=rect.Width; x+=d)
-			{
-				var start = new Point (rect.Left+x, rect.Bottom);
-				var end = TimelineRowGlyphs.GetHashEnd (start, rect);
-				graphics.AddLine (start, end);
-			}
-
-			graphics.RenderSolid (Color.FromAlphaColor (0.5, ColorManager.GlyphColor));
-		}
-
-		private static Point GetHashEnd(Point start, Rectangle rect)
-		{
-			//	Calcule le point d'arrivée de la jointure.
-			double x = start.X + rect.Width;
-			double y = start.Y + rect.Width;  // juste, mais trop loin
-
-			//	Effectue un clipping dans le rectangle.
-			if (x > rect.Right)
-			{
-				y -= x - rect.Right + 0.5;
-				x -= x - rect.Right + 0.5;
-			}
-
-			if (y > rect.Top)
-			{
-				x -= y - rect.Top + 0.5;
-				y -= y - rect.Top + 0.5;
-			}
-
-			return new Point (x, y);
-		}
-		#endregion
 
 
 		private static bool IsSame(TimelineCellGlyph c1, TimelineCellGlyph c2)
