14a0d010bdebe287ac6a8f495e81a03729d34241
Samuel Loup <loup@epsitec.ch>
2013-11-07 07:39:31 +0000

    - Aider: Ajout de l'export en étiquettes pour les publipostages
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@22093 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/Labels/AiderMailingParticipantLabelTextFactory0.cs
@@ -0,0 +1,25 @@
+﻿using Epsitec.Aider.Entities;
+
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Core.Labels;
+
+
+namespace Epsitec.Aider.Labels
+{
+
+
+	[LabelTextFactoryId (0)]
+	public sealed class AiderMailingParticipantLabelTextFactory0 : LabelTextFactory<AiderMailingParticipantEntity>
+	{
+
+
+		public override FormattedText GetLabelText(AiderMailingParticipantEntity entity)
+		{
+			return entity.Contact.GetAddressLabelText ();
+		}
+		
+	}
+
+
+}
---
+++ Epsitec.Cresus/Aider/Labels/AiderMailingParticipantLabelTextFactory1.cs
@@ -0,0 +1,26 @@
+﻿using Epsitec.Aider.Entities;
+
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Core.Labels;
+
+
+namespace Epsitec.Aider.Labels
+{
+
+
+	[LabelTextFactoryId (1)]
+	public sealed class AiderMailingParticipantLabelTextFactory1 : LabelTextFactory<AiderMailingParticipantEntity>
+	{
+
+
+		public override FormattedText GetLabelText(AiderMailingParticipantEntity entity)
+		{
+			return entity.Contact.GetAddressOfParentsLabelText ();
+		}
+
+
+	}
+
+
+}
