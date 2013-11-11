7deeb5f9b09151a82a5429df2a12fc103120e6dd
Daniel Roux <roux@epsitec.ch>
2013-11-11 18:32:35 +0000

    - Assets: Extraction de la logique de calcul de TimelinesArrayController dans TimelinesArrayLogic.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@22156 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Products/Assets.App/Views/TimelinesArrayController.cs
+++ Epsitec.Cresus/Products/Assets.App/Views/TimelinesArrayController.cs
@@ -40,7 +40,8 @@ namespace Epsitec.Cresus.Assets.App.Views
 			var levelNodesGetter = new LevelNodesGetter (primaryNodesGetter, this.accessor, this.baseType);
 			this.nodesGetter = new TreeObjectsNodesGetter (levelNodesGetter);
 
-			this.dataArray = new DataArray ();
+			this.arrayLogic = new TimelinesArrayLogic (this.accessor, this.baseType);
+			this.dataArray = new TimelinesArrayLogic.DataArray ();
 		}
 
 
@@ -879,208 +880,8 @@ namespace Epsitec.Cresus.Assets.App.Views
 			//	qui peut être long. Néanmoins, cela est nécessaire, même si la timeline
 			//	n'affiche qu'un nombre limité de lignes. En effet, il faut allouer toutes
 			//	les colonnes pour lesquelles il existe un événement.
-			this.dataArray.Clear (this.nodesGetter.Count);
-
-			for (int row=0; row<this.nodesGetter.Count; row++)
-			{
-				var node = this.nodesGetter[row];
-				var obj = this.accessor.GetObject (this.baseType, node.Guid);
-
-				var label = ObjectCalculator.GetObjectPropertyString (obj, null, ObjectField.Nom);
-				this.dataArray.RowsLabel.Add (label);
-
-				foreach (var e in obj.Events)
-				{
-					var column = this.dataArray.GetColumn (e.Timestamp);
-					column[row] = this.EventToCell (obj, e);
-				}
-			}
-
-			//	Marque les intervalles bloqués, qui seront hachurés.
-			for (int row=0; row<this.nodesGetter.Count; row++)
-			{
-				var node = this.nodesGetter[row];
-				var obj = this.accessor.GetObject (this.baseType, node.Guid);
-				var lockedIntervals = ObjectCalculator.GetLockedIntervals (obj);
-
-				for (int c=0; c<this.dataArray.ColumnsCount; c++)
-				{
-					var column = this.dataArray.GetColumn (c);
-					if (ObjectCalculator.IsLocked (lockedIntervals, column.Timestamp))
-					{
-						column[row] = new DataCell (column[row].Glyph, true, column[row].Tooltip);
-					}
-				}
-			}
+			this.arrayLogic.Update (this.dataArray, this.nodesGetter);
 		}
-
-		private DataCell EventToCell(DataObject obj, DataEvent e)
-		{
-			var glyph      = TimelineData.TypeToGlyph (e.Type);
-			string tooltip = BusinessLogic.GetTooltip (obj, e.Timestamp, e.Type, 8);
-
-			return new DataCell (glyph, false, tooltip);
-		}
-
-
-		#region Data manager
-		/// <summary>
-		/// Tableau des cellules. Il y a une colonne par Timestamp existant et
-		/// une ligne par objet.
-		/// </summary>
-		private class DataArray
-		{
-			public DataArray()
-			{
-				this.columns = new List<DataColumn> ();
-				this.rowsLabel = new List<string> ();
-			}
-
-			public List<string> RowsLabel
-			{
-				get
-				{
-					return this.rowsLabel;
-				}
-			}
-
-			public int RowsCount
-			{
-				get
-				{
-					return this.rowsCount;
-				}
-			}
-
-			public int ColumnsCount
-			{
-				get
-				{
-					return this.columns.Count;
-				}
-			}
-
-			public DataCell GetCell(int row, int column)
-			{
-				return this.columns[column][row];
-			}
-
-			public void Clear(int rowsCount)
-			{
-				this.rowsCount = rowsCount;
-				this.columns.Clear ();
-			}
-
-			public int FindColumnIndex(Timestamp timestamp)
-			{
-				return this.columns.FindIndex (x => x.Timestamp == timestamp);
-			}
-
-			public IEnumerable<DataColumn> Columns
-			{
-				get
-				{
-					return this.columns;
-				}
-			}
-
-			public DataColumn GetColumn(int column)
-			{
-				if (column >= 0 && column < this.columns.Count)
-				{
-					return this.columns[column];
-				}
-				else
-				{
-					return null;
-				}
-			}
-
-			public DataColumn GetColumn(Timestamp timestamp)
-			{
-				//	Retourne la colonne à utiliser pour un Timestamp donné.
-				//	Si elle n'existe pas, elle est créée.
-				var column = this.columns.Where (x => x.Timestamp == timestamp).FirstOrDefault ();
-
-				if (column == null)
-				{
-					column = new DataColumn (this.rowsCount, timestamp);
-
-					//	Les colonnes sont triées chronologiquement. Il faut donc insérer
-					//	la nouvelle colonne à la bonne place.
-					int i = this.columns.Where (x => x.Timestamp < timestamp).Count ();
-					this.columns.Insert (i, column);
-				}
-
-				return column;
-			}
-
-			private readonly List<DataColumn> columns;
-			private readonly List<string> rowsLabel;
-			private int rowsCount;
-		}
-
-		/// <summary>
-		/// Une colonne pour un Timestamp donné, avec une ligne par objet.
-		/// </summary>
-		private class DataColumn
-		{
-			public DataColumn(int rowsCount, Timestamp timestamp)
-			{
-				this.Timestamp = timestamp;
-
-				this.cells = new DataCell[rowsCount];
-
-				for (int i=0; i<rowsCount; i++)
-				{
-					this.cells[i] = DataCell.Empty;
-				}
-			}
-
-			public readonly Timestamp Timestamp;
-
-			public DataCell this[int index]
-			{
-				get
-				{
-					return this.cells[index];
-				}
-				set
-				{
-					this.cells[index] = value;
-				}
-			}
-
-			private readonly DataCell[] cells;
-		}
-
-		/// <summary>
-		/// Une cellule, correspondant à un événement d'un objet.
-		/// </summary>
-		private class DataCell
-		{
-			public DataCell(TimelineGlyph glyph, bool locked = false, string tooltip = null)
-			{
-				this.Glyph   = glyph;
-				this.Locked  = locked;
-				this.Tooltip = tooltip;
-			}
-
-			public bool IsEmpty
-			{
-				get
-				{
-					return this.Glyph == TimelineGlyph.Empty;
-				}
-			}
-
-			public static DataCell Empty = new DataCell (TimelineGlyph.Empty);
-
-			public readonly TimelineGlyph	Glyph;
-			public readonly bool			Locked;
-			public readonly string			Tooltip;
-		}
-		#endregion
 
 
 		protected void UpdateToolbar()
@@ -1362,20 +1163,21 @@ namespace Epsitec.Cresus.Assets.App.Views
 		private static readonly int lineHeight      = 18;
 		private static readonly int leftColumnWidth = 180;
 
-		private readonly DataAccessor			accessor;
-		private readonly BaseType				baseType;
-		private readonly TreeObjectsNodesGetter	nodesGetter;
-		private readonly DataArray				dataArray;
+		private readonly DataAccessor					accessor;
+		private readonly BaseType						baseType;
+		private readonly TreeObjectsNodesGetter			nodesGetter;
+		private readonly TimelinesArrayLogic			arrayLogic;
+		private readonly TimelinesArrayLogic.DataArray	dataArray;
 
-		private string							title;
-		private TopTitle						topTitle;
-		private TreeTableToolbar				objectsToolbar;
-		private VSplitter						splitter;
-		private TimelinesToolbar				timelinesToolbar;
-		private TreeTableColumnTree				treeColumn;
-		private NavigationTimelineController	controller;
-		private VScroller						scroller;
-		private int								selectedRow;
-		private int								selectedColumn;
+		private string									title;
+		private TopTitle								topTitle;
+		private TreeTableToolbar						objectsToolbar;
+		private VSplitter								splitter;
+		private TimelinesToolbar						timelinesToolbar;
+		private TreeTableColumnTree						treeColumn;
+		private NavigationTimelineController			controller;
+		private VScroller								scroller;
+		private int										selectedRow;
+		private int										selectedColumn;
 	}
 }
---
+++ Epsitec.Cresus/Products/Assets.App/Views/TimelinesArrayLogic.cs
@@ -0,0 +1,235 @@
+﻿//	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using System.Collections.Generic;
+using System.Linq;
+using Epsitec.Cresus.Assets.App.Widgets;
+using Epsitec.Cresus.Assets.Server.SimpleEngine;
+
+namespace Epsitec.Cresus.Assets.App.Views
+{
+	public class TimelinesArrayLogic
+	{
+		public TimelinesArrayLogic(DataAccessor accessor, BaseType baseType)
+		{
+			this.accessor = accessor;
+			this.baseType = baseType;
+		}
+
+
+		public void Update(DataArray dataArray, TreeObjectsNodesGetter nodesGetter)
+		{
+			//	Met à jour this.dataArray en fonction de l'ensemble des événements de
+			//	tous les objets. Cela nécessite d'accéder à l'ensemble des données, ce
+			//	qui peut être long. Néanmoins, cela est nécessaire, même si la timeline
+			//	n'affiche qu'un nombre limité de lignes. En effet, il faut allouer toutes
+			//	les colonnes pour lesquelles il existe un événement.
+			dataArray.Clear (nodesGetter.Count);
+
+			for (int row=0; row<nodesGetter.Count; row++)
+			{
+				var node = nodesGetter[row];
+				var obj = this.accessor.GetObject (this.baseType, node.Guid);
+
+				var label = ObjectCalculator.GetObjectPropertyString (obj, null, ObjectField.Nom);
+				dataArray.RowsLabel.Add (label);
+
+				foreach (var e in obj.Events)
+				{
+					var column = dataArray.GetColumn (e.Timestamp);
+					column[row] = this.EventToCell (obj, e);
+				}
+			}
+
+			//	Marque les intervalles bloqués, qui seront hachurés.
+			for (int row=0; row<nodesGetter.Count; row++)
+			{
+				var node = nodesGetter[row];
+				var obj = this.accessor.GetObject (this.baseType, node.Guid);
+				var lockedIntervals = ObjectCalculator.GetLockedIntervals (obj);
+
+				for (int c=0; c<dataArray.ColumnsCount; c++)
+				{
+					var column = dataArray.GetColumn (c);
+					if (ObjectCalculator.IsLocked (lockedIntervals, column.Timestamp))
+					{
+						column[row] = new DataCell (column[row].Glyph, true, column[row].Tooltip);
+					}
+				}
+			}
+		}
+
+		private DataCell EventToCell(DataObject obj, DataEvent e)
+		{
+			var glyph      = TimelineData.TypeToGlyph (e.Type);
+			string tooltip = BusinessLogic.GetTooltip (obj, e.Timestamp, e.Type, 8);
+
+			return new DataCell (glyph, false, tooltip);
+		}
+
+
+		#region Data manager
+		/// <summary>
+		/// Tableau des cellules. Il y a une colonne par Timestamp existant et
+		/// une ligne par objet.
+		/// </summary>
+		public class DataArray
+		{
+			public DataArray()
+			{
+				this.columns = new List<DataColumn> ();
+				this.rowsLabel = new List<string> ();
+			}
+
+			public List<string> RowsLabel
+			{
+				get
+				{
+					return this.rowsLabel;
+				}
+			}
+
+			public int RowsCount
+			{
+				get
+				{
+					return this.rowsCount;
+				}
+			}
+
+			public int ColumnsCount
+			{
+				get
+				{
+					return this.columns.Count;
+				}
+			}
+
+			public DataCell GetCell(int row, int column)
+			{
+				return this.columns[column][row];
+			}
+
+			public void Clear(int rowsCount)
+			{
+				this.rowsCount = rowsCount;
+				this.columns.Clear ();
+			}
+
+			public int FindColumnIndex(Timestamp timestamp)
+			{
+				return this.columns.FindIndex (x => x.Timestamp == timestamp);
+			}
+
+			public IEnumerable<DataColumn> Columns
+			{
+				get
+				{
+					return this.columns;
+				}
+			}
+
+			public DataColumn GetColumn(int column)
+			{
+				if (column >= 0 && column < this.columns.Count)
+				{
+					return this.columns[column];
+				}
+				else
+				{
+					return null;
+				}
+			}
+
+			public DataColumn GetColumn(Timestamp timestamp)
+			{
+				//	Retourne la colonne à utiliser pour un Timestamp donné.
+				//	Si elle n'existe pas, elle est créée.
+				var column = this.columns.Where (x => x.Timestamp == timestamp).FirstOrDefault ();
+
+				if (column == null)
+				{
+					column = new DataColumn (this.rowsCount, timestamp);
+
+					//	Les colonnes sont triées chronologiquement. Il faut donc insérer
+					//	la nouvelle colonne à la bonne place.
+					int i = this.columns.Where (x => x.Timestamp < timestamp).Count ();
+					this.columns.Insert (i, column);
+				}
+
+				return column;
+			}
+
+			private readonly List<DataColumn> columns;
+			private readonly List<string> rowsLabel;
+			private int rowsCount;
+		}
+
+		/// <summary>
+		/// Une colonne pour un Timestamp donné, avec une ligne par objet.
+		/// </summary>
+		public class DataColumn
+		{
+			public DataColumn(int rowsCount, Timestamp timestamp)
+			{
+				this.Timestamp = timestamp;
+
+				this.cells = new DataCell[rowsCount];
+
+				for (int i=0; i<rowsCount; i++)
+				{
+					this.cells[i] = DataCell.Empty;
+				}
+			}
+
+			public readonly Timestamp Timestamp;
+
+			public DataCell this[int index]
+			{
+				get
+				{
+					return this.cells[index];
+				}
+				set
+				{
+					this.cells[index] = value;
+				}
+			}
+
+			private readonly DataCell[] cells;
+		}
+
+		/// <summary>
+		/// Une cellule, correspondant à un événement d'un objet.
+		/// </summary>
+		public class DataCell
+		{
+			public DataCell(TimelineGlyph glyph, bool locked = false, string tooltip = null)
+			{
+				this.Glyph   = glyph;
+				this.Locked  = locked;
+				this.Tooltip = tooltip;
+			}
+
+			public bool IsEmpty
+			{
+				get
+				{
+					return this.Glyph == TimelineGlyph.Empty;
+				}
+			}
+
+			public static DataCell Empty = new DataCell (TimelineGlyph.Empty);
+
+			public readonly TimelineGlyph	Glyph;
+			public readonly bool			Locked;
+			public readonly string			Tooltip;
+		}
+		#endregion
+
+
+
+		private readonly DataAccessor			accessor;
+		private readonly BaseType				baseType;
+	}
+}
