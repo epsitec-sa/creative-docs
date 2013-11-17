e61895ca511c79b043e5d26ceb0401459ffead20
Pierre Arnaud <arnaud@opac.ch>
2013-11-17 08:35:57 +0000

    - Aider: build.bat copie le résultat du build à la racine du lecteur S:\
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@22235 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/App.Aider/build.bat
+++ Epsitec.Cresus/App.Aider/build.bat
@@ -109,3 +109,5 @@ copy %configurationDirectory%\Aider.Features.crconfig bin\Build\aider\server\Aid
 @rem Zip the build.
 set zipname=aider-%suffix%
 ..\..\Epsitec\dot.net\Epsitec.ZipMe\Epsitec.ZipMe\bin\Release\Epsitec.ZipMe.exe bin\Build\%zipname%.zip bin\Build\aider
+
+copy bin\Build\%zipname%.zip S:\ /Y
