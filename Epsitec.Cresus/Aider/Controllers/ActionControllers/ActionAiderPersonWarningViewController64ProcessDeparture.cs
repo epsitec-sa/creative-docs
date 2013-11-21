c81962e36d4c414b480ae1267a30505cbac0c00c
Pierre Arnaud <arnaud@opac.ch>
2013-11-21 05:16:57 +0000

    - Aider: ajouté une action pour conserver une personne sortie du RCH, totalement inchangée (par ex. pour un enfant dont le dernier parent RCH disparaît).
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@22290 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/Controllers/ActionControllers/ActionAiderPersonWarningViewController64ProcessDeparture.cs
@@ -0,0 +1,38 @@
+//	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Aider.Enumerations;
+using Epsitec.Aider.Entities;
+
+using Epsitec.Common.Support;
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Bricks;
+using Epsitec.Cresus.Core.Business;
+using Epsitec.Cresus.Core.Controllers;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Aider.Controllers.ActionControllers
+{
+	[ControllerSubType (64)]
+	public sealed class ActionAiderPersonWarningViewController64ProcessDeparture : ActionAiderPersonWarningViewControllerInteractive
+	{
+		public override FormattedText GetTitle()
+		{
+			return Resources.FormattedText ("Conserver la personne");
+		}
+
+		public override ActionExecutor GetExecutor()
+		{
+			return ActionExecutor.Create (this.Execute);
+		}
+
+		private void Execute()
+		{
+			this.ClearWarningAndRefreshCaches ();
+			this.ClearWarningAndRefreshCachesForAll (this.Entity.WarningType);
+		}
+	}
+}
