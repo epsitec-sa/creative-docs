b6d61d5bfe1b2a1e0215fcfaa0d0acca4142c37f
Daniel Roux <roux@epsitec.ch>
2013-11-26 18:29:07 +0000

    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@22399 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Products/Assets.Server/BusinessLogic/LogicRange.cs
+++ Epsitec.Cresus/Products/Assets.Server/BusinessLogic/LogicRange.cs
@@ -13,23 +13,24 @@ namespace Epsitec.Cresus.Assets.Server.BusinessLogic
 			//	Retourne la liste des intervalles pour lesquels il faudra générer
 			//	des amortissements. Par exemple:
 			//
-			//	range	01.01.2013 .. 31.12.2013
+			//	range	01.01.2013 -> 31.12.2013
 			//	period	6
-			//	out		01.01.2013 .. 30.06.2013
-			//			01.07.2013 .. 31.12.2013
+			//	out		01.01.2013 -> 30.06.2013
+			//			01.07.2013 -> 31.12.2013
 			//
-			//	range	01.08.2013 .. 01.08.2014
+			//	range	01.08.2013 -> 01.08.2014
 			//	period	3
-			//	out		01.07.2013 .. 31.09.2013
-			//			01.10.2013 .. 31.12.2013
-			//			01.01.2014 .. 31.03.2014
-			//			01.04.2014 .. 30.06.2014
+			//	out		01.07.2013 -> 31.09.2013
+			//			01.10.2013 -> 31.12.2013
+			//			01.01.2014 -> 31.03.2014
+			//			01.04.2014 -> 30.06.2014
 
 			System.Diagnostics.Debug.Assert (period >= 0);
 			var start = new System.DateTime (range.IncludeFrom.Date.Year, 1, 1);
 
 			while (start < range.IncludeTo.Date)
 			{
+				//	Calcule la date de fin inclue.
 				var end = start.AddMonths (period).AddDays (-1);
 
 				if (range.IsInside (end))
@@ -37,6 +38,7 @@ namespace Epsitec.Cresus.Assets.Server.BusinessLogic
 					yield return new DateRange (start, end);
 				}
 
+				//	Avance au début de l'intervalle suivant.
 				start = start.AddMonths (period);
 			}
 		}
