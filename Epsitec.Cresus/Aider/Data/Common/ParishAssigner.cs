a2cd9683734b18a5eb1e3cbf566b1c3c3458d9e0
Pierre Arnaud <arnaud@opac.ch>
2013-12-04 08:07:26 +0000

    - Aider: amélioré la gestion des assignations de paroisses (recycle une participation existante; supprime des doublons au besoin).
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@22443 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Aider/Data/Common/ParishAssigner.cs
+++ Epsitec.Cresus/Aider/Data/Common/ParishAssigner.cs
@@ -50,9 +50,27 @@ namespace Epsitec.Aider.Data.Common
 				return;
 			}
 
-			var noParishGroupParticipation = ParishAssigner.GetNoParishGroupParticipation (person);
+			var participations = ParishAssigner.GetParishGroupParticipations (person);
+			var matchingParticipation = participations.Where (x => x.EndDate == null && x.Group == parishGroup).FirstOrDefault ();
 
-			if (noParishGroupParticipation.IsNull ())
+			if (matchingParticipation != null)
+			{
+				//	We already have a participation for the specified group, which is
+				//	still active (i.e. it has no end date). Make it active. This should
+				//	not happen, but it does as duplicate participations might have been
+				//	created in the past.
+
+				person.ParishGroup = matchingParticipation.Group;
+
+				this.UpdateSubscriptionAndRefreshCache (person);
+
+				return;
+			}
+			
+
+			var noParishGroupParticipations = ParishAssigner.GetNoParishGroupParticipations (person);
+
+			if (noParishGroupParticipations.IsEmpty ())
 			{
 				var participationData = new ParticipationData (person);
 				AiderGroupParticipantEntity.StartParticipation (businessContext, parishGroup, participationData, startDate);
@@ -67,12 +85,25 @@ namespace Epsitec.Aider.Data.Common
 				//	Here we recycle the participation to the no parish group, so we don't delete
 				//	an entity to create a similar one right after.
 
+				var noParishGroupParticipation = noParishGroupParticipations.First ();
+
 				noParishGroupParticipation.Group = parishGroup;
 				person.ParishGroup = parishGroup;
 					
 				System.Diagnostics.Debug.Assert (person.ParishGroupPathCache == parishGroup.Path);
 
 				this.UpdateSubscriptionAndRefreshCache (person);
+			}
+		}
+
+		private void RemoveStaleParishes(AiderPersonEntity person, Date stopDate)
+		{
+			var parishGroup    = person.ParishGroup;
+			var participations = ParishAssigner.GetParishGroupParticipations (person).Where (x => x.Group != parishGroup).ToList ();
+
+			foreach (var participation in participations)
+			{
+				AiderGroupParticipantEntity.StopParticipation (participation, stopDate);
 			}
 		}
 
@@ -214,6 +245,7 @@ namespace Epsitec.Aider.Data.Common
 			var assigner = new ParishAssigner (parishRepository, businessContext);
 
 			assigner.AssignToParish (person, startDate);
+			assigner.RemoveStaleParishes (person, startDate ?? Date.Today);
 		}
 
 
@@ -224,6 +256,7 @@ namespace Epsitec.Aider.Data.Common
 			foreach (var person in persons)
 			{
 				assigner.AssignToParish (person);
+				assigner.RemoveStaleParishes (person, Date.Today);
 			}
 		}
 
@@ -231,35 +264,31 @@ namespace Epsitec.Aider.Data.Common
 		public static void ReassignToParish(ParishAddressRepository parishRepository, BusinessContext businessContext, AiderPersonEntity person, Date? startDate = null)
 		{
 			ParishAssigner.UnassignFromParish (businessContext, person);
-
 			ParishAssigner.AssignToParish (parishRepository, businessContext, person, startDate);
 		}
 
-
 		public static void ReassignToParish(ParishAddressRepository parishRepository, BusinessContext businessContext, IEnumerable<AiderPersonEntity> persons)
 		{
-			foreach (var person in persons)
-			{
-				ParishAssigner.UnassignFromParish (businessContext, person);
-			}
-
+			ParishAssigner.UnassignFromParish (businessContext, persons);
 			ParishAssigner.AssignToParish (parishRepository, businessContext, persons);
 		}
 
-
+		
 		private static void UnassignFromParish(BusinessContext businessContext, AiderPersonEntity person)
 		{
-			var participations = ParishAssigner.GetParishGroupParticipations (person).ToList ();
-
-			var noParishParticipation = ParishAssigner.GetNoParishGroupParticipation (person);
-			if (noParishParticipation.IsNotNull ())
-			{
-				participations.Add (noParishParticipation);
-			}
+			var participations = ParishAssigner.GetParishGroupParticipations (person);
 
 			foreach (var participation in participations)
 			{
 				AiderGroupParticipantEntity.StopParticipation (participation, Date.Today);
+			}
+		}
+
+		private static void UnassignFromParish(BusinessContext businessContext, IEnumerable<AiderPersonEntity> persons)
+		{
+			foreach (var person in persons)
+			{
+				ParishAssigner.UnassignFromParish (businessContext, person);
 			}
 		}
 
@@ -303,17 +332,17 @@ namespace Epsitec.Aider.Data.Common
 
 		public static bool IsInNoParishGroup(AiderPersonEntity person)
 		{
-			return ParishAssigner.GetNoParishGroupParticipation (person) != null;
+			return ParishAssigner.GetNoParishGroupParticipations (person).Any ();
 		}
 
-		public static AiderGroupParticipantEntity GetNoParishGroupParticipation(AiderPersonEntity person)
+		private static IEnumerable<AiderGroupParticipantEntity> GetNoParishGroupParticipations(AiderPersonEntity person)
 		{
-			return person.Groups.FirstOrDefault (g => g.Group.IsNoParish ());
+			return person.Groups.Where (g => g.Group.IsNoParish ());
 		}
 
-		public static IEnumerable<AiderGroupParticipantEntity> GetParishGroupParticipations(AiderPersonEntity person)
+		private static IEnumerable<AiderGroupParticipantEntity> GetParishGroupParticipations(AiderPersonEntity person)
 		{
-			return person.Groups.Where (g => g.Group.IsParish ());
+			return person.Groups.Where (g => g.Group.IsParish () || g.Group.IsNoParish ());
 		}
 
 
