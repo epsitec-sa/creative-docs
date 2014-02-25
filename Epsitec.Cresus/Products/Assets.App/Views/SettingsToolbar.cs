52fe9f030af2c0a492b762491e8c0b110ef0364d
Daniel Roux <roux@epsitec.ch>
2014-02-25 17:46:21 +0000

    - Assets: Extraction de l'initialisation du plan comptable dans DummyAccounts.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@22646 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Products/Assets.App/Views/SettingsToolbar.cs
+++ Epsitec.Cresus/Products/Assets.App/Views/SettingsToolbar.cs
@@ -3,7 +3,6 @@
 
 using System.Collections.Generic;
 using System.Linq;
-using Epsitec.Common.Drawing;
 using Epsitec.Common.Widgets;
 using Epsitec.Cresus.Assets.App.Widgets;
 
---
+++ Epsitec.Cresus/Products/Assets.Server/SimpleEngine/DummyAccounts.cs
@@ -0,0 +1,56 @@
+﻿//	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using System.Collections.Generic;
+using System.Linq;
+using Epsitec.Cresus.Assets.Server.BusinessLogic;
+
+namespace Epsitec.Cresus.Assets.Server.SimpleEngine
+{
+	public static class DummyAccounts
+	{
+		public static void AddAccounts(DataMandat mandat)
+		{
+			var accounts = mandat.GetData (BaseType.Accounts);
+
+			var start  = new Timestamp (new System.DateTime (2000, 1, 1), 0);
+
+			var o0 = new DataObject ();
+			accounts.Add (o0);
+			{
+				var e = new DataEvent (start, EventType.Input);
+				o0.AddEvent (e);
+				e.AddProperty (new DataStringProperty (ObjectField.OneShotNumber, (DummyAccounts.AccountNumber++).ToString ()));
+				e.AddProperty (new DataStringProperty (ObjectField.Name, "Comptes"));
+			}
+
+			///////////////
+
+			var oActif = new DataObject ();
+			accounts.Add (oActif);
+			{
+				var e = new DataEvent (start, EventType.Input);
+				oActif.AddEvent (e);
+				e.AddProperty (new DataStringProperty (ObjectField.OneShotNumber, (DummyAccounts.AccountNumber++).ToString ()));
+				e.AddProperty (new DataGuidProperty (ObjectField.GroupParent, o0.Guid));
+				e.AddProperty (new DataStringProperty (ObjectField.Number, "10"));
+				e.AddProperty (new DataStringProperty (ObjectField.Name, "Actif"));
+			}
+
+			var o1 = new DataObject ();
+			accounts.Add (o1);
+			{
+				var e = new DataEvent (start, EventType.Input);
+				o1.AddEvent (e);
+				e.AddProperty (new DataStringProperty (ObjectField.OneShotNumber, (DummyAccounts.AccountNumber++).ToString ()));
+				e.AddProperty (new DataGuidProperty (ObjectField.GroupParent, oActif.Guid));
+				e.AddProperty (new DataStringProperty (ObjectField.Number, "1000"));
+				e.AddProperty (new DataStringProperty (ObjectField.Name, "Caisse"));
+			}
+
+		}
+
+		private static int AccountNumber = 1;
+	}
+
+}
