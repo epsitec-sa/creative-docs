691f491ef56d8d97a583639e37e55e9b92f19bec
Daniel Roux <roux@epsitec.ch>
2014-02-26 11:38:26 +0000

    - Assets: CategoriesTreeTableFiller affiche les 8 comptes.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@22655 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Products/Assets.Server/DataFillers/CategoriesTreeTableFiller.cs
+++ Epsitec.Cresus/Products/Assets.Server/DataFillers/CategoriesTreeTableFiller.cs
@@ -29,6 +29,15 @@ namespace Epsitec.Cresus.Assets.Server.DataFillers
 				yield return ObjectField.Prorata;
 				yield return ObjectField.Round;
 				yield return ObjectField.ResidualValue;
+
+				yield return ObjectField.Compte1;
+				yield return ObjectField.Compte2;
+				yield return ObjectField.Compte3;
+				yield return ObjectField.Compte4;
+				yield return ObjectField.Compte5;
+				yield return ObjectField.Compte6;
+				yield return ObjectField.Compte7;
+				yield return ObjectField.Compte8;
 			}
 		}
 
@@ -39,13 +48,22 @@ namespace Epsitec.Cresus.Assets.Server.DataFillers
 				var columns = new List<TreeTableColumnDescription> ();
 
 				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String, 180, "Catégorie"));
-				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String,  50, "N°"));
-				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.Rate,    80, "Taux"));
-				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String,  80, "Type"));
+				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String, 50, "N°"));
+				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.Rate, 80, "Taux"));
+				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String, 80, "Type"));
 				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String, 100, "Périodicité"));
 				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String, 100, "Prorata"));
 				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.Amount, 100, "Arrondi"));
 				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.Amount, 120, "Valeur résiduelle"));
+
+				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String, 150, "Comnpte 1"));
+				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String, 150, "Comnpte 2"));
+				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String, 150, "Comnpte 3"));
+				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String, 150, "Comnpte 4"));
+				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String, 150, "Comnpte 5"));
+				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String, 150, "Comnpte 6"));
+				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String, 150, "Comnpte 7"));
+				columns.Add (new TreeTableColumnDescription (TreeTableColumnType.String, 150, "Comnpte 8"));
 
 				return columns.ToArray ();
 			}
@@ -55,7 +73,7 @@ namespace Epsitec.Cresus.Assets.Server.DataFillers
 		{
 			var content = new TreeTableContentItem ();
 
-			for (int i=0; i<8; i++)
+			for (int i=0; i<8+8; i++)
 			{
 				content.Columns.Add (new TreeTableColumnItem ());
 			}
@@ -71,12 +89,12 @@ namespace Epsitec.Cresus.Assets.Server.DataFillers
 				var guid  = node.Guid;
 				var obj   = this.accessor.GetObject (BaseType.Categories, guid);
 
-				var name   = ObjectProperties.GetObjectPropertyString  (obj, this.Timestamp, ObjectField.Name, inputValue: true);
-				var number = ObjectProperties.GetObjectPropertyString  (obj, this.Timestamp, ObjectField.Number);
+				var name   = ObjectProperties.GetObjectPropertyString (obj, this.Timestamp, ObjectField.Name, inputValue: true);
+				var number = ObjectProperties.GetObjectPropertyString (obj, this.Timestamp, ObjectField.Number);
 				var rate   = ObjectProperties.GetObjectPropertyDecimal (obj, this.Timestamp, ObjectField.AmortizationRate);
-				var type   = ObjectProperties.GetObjectPropertyInt     (obj, this.Timestamp, ObjectField.AmortizationType);
-				var period = ObjectProperties.GetObjectPropertyInt     (obj, this.Timestamp, ObjectField.Periodicity);
-				var prorat = ObjectProperties.GetObjectPropertyInt     (obj, this.Timestamp, ObjectField.Prorata);
+				var type   = ObjectProperties.GetObjectPropertyInt (obj, this.Timestamp, ObjectField.AmortizationType);
+				var period = ObjectProperties.GetObjectPropertyInt (obj, this.Timestamp, ObjectField.Periodicity);
+				var prorat = ObjectProperties.GetObjectPropertyInt (obj, this.Timestamp, ObjectField.Prorata);
 				var round  = ObjectProperties.GetObjectPropertyDecimal (obj, this.Timestamp, ObjectField.Round);
 				var resid  = ObjectProperties.GetObjectPropertyDecimal (obj, this.Timestamp, ObjectField.ResidualValue);
 
@@ -86,28 +104,52 @@ namespace Epsitec.Cresus.Assets.Server.DataFillers
 
 				var cellState = (i == selection) ? CellState.Selected : CellState.None;
 
-				var cell0 = new TreeTableCellString  (name,    cellState);
-				var cell1 = new TreeTableCellString  (number,  cellState);
-				var cell2 = new TreeTableCellDecimal (rate,    cellState);
-				var cell3 = new TreeTableCellString  (t,       cellState);
-				var cell4 = new TreeTableCellString  (c,       cellState);
-				var cell5 = new TreeTableCellString  (r,       cellState);
-				var cell6 = new TreeTableCellDecimal (round,   cellState);
-				var cell7 = new TreeTableCellDecimal (resid,   cellState);
+				var cell11 = new TreeTableCellString  (name,   cellState);
+				var cell12 = new TreeTableCellString  (number, cellState);
+				var cell13 = new TreeTableCellDecimal (rate,   cellState);
+				var cell14 = new TreeTableCellString  (t,      cellState);
+				var cell15 = new TreeTableCellString  (c,      cellState);
+				var cell16 = new TreeTableCellString  (r,      cellState);
+				var cell17 = new TreeTableCellDecimal (round,  cellState);
+				var cell18 = new TreeTableCellDecimal (resid,  cellState);
+
+				var cell21 = new TreeTableCellString (this.GetCompte (obj, ObjectField.Compte1), cellState);
+				var cell22 = new TreeTableCellString (this.GetCompte (obj, ObjectField.Compte2), cellState);
+				var cell23 = new TreeTableCellString (this.GetCompte (obj, ObjectField.Compte3), cellState);
+				var cell24 = new TreeTableCellString (this.GetCompte (obj, ObjectField.Compte4), cellState);
+				var cell25 = new TreeTableCellString (this.GetCompte (obj, ObjectField.Compte5), cellState);
+				var cell26 = new TreeTableCellString (this.GetCompte (obj, ObjectField.Compte6), cellState);
+				var cell27 = new TreeTableCellString (this.GetCompte (obj, ObjectField.Compte7), cellState);
+				var cell28 = new TreeTableCellString (this.GetCompte (obj, ObjectField.Compte8), cellState);
 
 				int columnRank = 0;
 
-				content.Columns[columnRank++].AddRow (cell0);
-				content.Columns[columnRank++].AddRow (cell1);
-				content.Columns[columnRank++].AddRow (cell2);
-				content.Columns[columnRank++].AddRow (cell3);
-				content.Columns[columnRank++].AddRow (cell4);
-				content.Columns[columnRank++].AddRow (cell5);
-				content.Columns[columnRank++].AddRow (cell6);
-				content.Columns[columnRank++].AddRow (cell7);
+				content.Columns[columnRank++].AddRow (cell11);
+				content.Columns[columnRank++].AddRow (cell12);
+				content.Columns[columnRank++].AddRow (cell13);
+				content.Columns[columnRank++].AddRow (cell14);
+				content.Columns[columnRank++].AddRow (cell15);
+				content.Columns[columnRank++].AddRow (cell16);
+				content.Columns[columnRank++].AddRow (cell17);
+				content.Columns[columnRank++].AddRow (cell18);
+
+				content.Columns[columnRank++].AddRow (cell21);
+				content.Columns[columnRank++].AddRow (cell22);
+				content.Columns[columnRank++].AddRow (cell23);
+				content.Columns[columnRank++].AddRow (cell24);
+				content.Columns[columnRank++].AddRow (cell25);
+				content.Columns[columnRank++].AddRow (cell26);
+				content.Columns[columnRank++].AddRow (cell27);
+				content.Columns[columnRank++].AddRow (cell28);
 			}
 
 			return content;
+		}
+
+		private string GetCompte(DataObject obj, ObjectField field)
+		{
+			var guid = ObjectProperties.GetObjectPropertyGuid (obj, this.Timestamp, field);
+			return AccountsLogic.GetSummary (this.accessor, guid);
 		}
 	}
 }
