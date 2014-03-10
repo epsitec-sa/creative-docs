d74e36a28ca9d1ee801018b68166dd5f7dd93efd
Pierre Arnaud <arnaud@opac.ch>
2014-03-10 05:55:19 +0000

    - Core: remplacé sealed par static.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@22761 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.Core.Library/Resolvers/EntityPrinterFactoryResolver.cs
+++ Epsitec.Cresus/Cresus.Core.Library/Resolvers/EntityPrinterFactoryResolver.cs
@@ -1,4 +1,4 @@
-﻿//	Copyright © 2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+﻿//	Copyright © 2011-2014, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
 using Epsitec.Common.Support;
@@ -17,7 +17,7 @@ namespace Epsitec.Cresus.Core.Resolvers
 	/// <see cref="IEntityPrinterFactory"/> interface and functionality related to the <see cref="IEntityPrinter"/>
 	/// interface.
 	/// </summary>
-	public sealed class EntityPrinterFactoryResolver
+	public static class EntityPrinterFactoryResolver
 	{
 		public static IEnumerable<IEntityPrinterFactory> Resolve()
 		{
