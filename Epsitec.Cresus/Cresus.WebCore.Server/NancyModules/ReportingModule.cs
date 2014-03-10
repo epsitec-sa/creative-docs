ad49988e2e20e36f50dfe175a5e364bbf97efd5d
Pierre Arnaud <arnaud@opac.ch>
2014-03-10 03:55:15 +0000

    - WebCore: beautifié le code source de ReportingModule.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@22756 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Cresus.WebCore.Server/NancyModules/ReportingModule.cs
+++ Epsitec.Cresus/Cresus.WebCore.Server/NancyModules/ReportingModule.cs
@@ -1,26 +1,32 @@
-﻿using System;
-using System.Linq;
+﻿//	Copyright © 2014, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Samuel LOUP, Maintainer: Samuel LOUP
+
+using Epsitec.Common.Support.EntityEngine;
+using Epsitec.Common.Types;
+
 using Epsitec.Cresus.Core.Business;
 using Epsitec.Cresus.Core.Data;
+using Epsitec.Cresus.Core.Metadata;
 using Epsitec.Cresus.Core.Resolvers;
+
+using Epsitec.Cresus.DataLayer.Context;
+using Epsitec.Cresus.DataLayer.Expressions;
+
 using Epsitec.Cresus.WebCore.Server.Core;
+using Epsitec.Cresus.WebCore.Server.Core.Databases;
 using Epsitec.Cresus.WebCore.Server.Core.Extraction;
 using Epsitec.Cresus.WebCore.Server.Core.IO;
 using Epsitec.Cresus.WebCore.Server.NancyHosting;
+
 using Nancy;
 
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Epsitec.Cresus.WebCore.Server.NancyModules
 {
-
-	using System.Collections.Generic;
-	using Epsitec.Common.Support.EntityEngine;
-	using Epsitec.Common.Types;
-	using Epsitec.Cresus.Core.Metadata;
-	using Epsitec.Cresus.DataLayer.Context;
-	using Epsitec.Cresus.DataLayer.Expressions;
-	using Epsitec.Cresus.WebCore.Server.Core.Databases;
 	using Database = Core.Databases.Database;
+
 	/// <summary>
 	/// Report Builder
 	/// </summary>
@@ -31,20 +37,55 @@ namespace Epsitec.Cresus.WebCore.Server.NancyModules
 		{
 			///TEST REPORT
 			//https://localhost/proxy/reporting/[LVOR13]/[LVOA13]-1000000001/test
-			Get["/{datasetId}/{entityId}/test"] = p =>
-				this.Execute ((wa, b) => this.ProduceTestReport (wa, b, p));
+			Get["/{datasetId}/{entityId}/test"] =
+				p => this.Execute ((wa, b) => this.ProduceTestReport (wa, b, p));
 		}
 
+		
 		private Response ProduceTestReport(WorkerApp workerApp, BusinessContext businessContext, dynamic parameters)
 		{
-			using (EntityExtractor reportExtractor = this.GetReportExtractor(workerApp,businessContext,parameters.datasetId,parameters.entityId))
+			using (EntityExtractor reportExtractor = this.GetReportExtractor (workerApp, businessContext, parameters.datasetId, parameters.entityId))
 			{
 				return ReportingModule.CreateReport (reportExtractor);
 			}
 		}
 
-		internal static Response CreateReport(EntityExtractor extractor)
-		{			
+		private EntityExtractor GetReportExtractor(WorkerApp workerApp, BusinessContext businessContext, string datasetId, string entityId)
+		{
+			var caches = this.CoreServer.Caches;
+
+			var userManager = workerApp.UserManager;
+			var databaseManager = this.CoreServer.DatabaseManager;
+
+
+			System.Func<Database, DataSetAccessor> dataSetAccessorGetter = db =>
+			{
+				return db.GetDataSetAccessor (workerApp.DataSetGetter);
+			};
+
+			string rawDatabaseId = datasetId;
+			var databaseId = DataIO.ParseDruid (rawDatabaseId);
+			var database = databaseManager.GetDatabase (databaseId);
+
+			var entity = EntityIO.ResolveEntity (businessContext, (string) entityId);
+			var context = DataContextPool.GetDataContext (entity);
+			var id = context.GetNormalizedEntityKey (entity).Value.RowKey.Id.Value;
+
+			System.Action<DataContext, DataLayer.Loader.Request, AbstractEntity> customizer = (d, r, e) =>
+			{
+				r.Conditions.Add (ReportingModule.CreateCondition (e, id));
+			};
+
+			return EntityExtractor.Create
+			(
+				businessContext, caches, userManager, databaseManager, dataSetAccessorGetter,
+				databaseId, "", "", customizer
+			);
+		}
+
+		
+		private static Response CreateReport(EntityExtractor extractor)
+		{
 			var itemCount = extractor.Accessor.GetItemCount ();
 
 			EntityWriter writer = ReportingModule.GetEntityWriter (extractor);
@@ -53,39 +94,6 @@ namespace Epsitec.Cresus.WebCore.Server.NancyModules
 			var stream   = writer.GetStream ();
 
 			return CoreResponse.CreateStreamResponse (stream, filename);
-		}
-
-		private EntityExtractor GetReportExtractor(WorkerApp workerApp, BusinessContext businessContext, string datasetId,string entityId)
-		{
-			var caches = this.CoreServer.Caches;
-
-			var userManager = workerApp.UserManager;
-			var databaseManager = this.CoreServer.DatabaseManager;
-			
-
-			Func<Database, DataSetAccessor> dataSetAccessorGetter = db =>
-			{
-				return db.GetDataSetAccessor (workerApp.DataSetGetter);
-			};
-
-			string rawDatabaseId = datasetId;
-			var databaseId = DataIO.ParseDruid (rawDatabaseId);
-			var database = databaseManager.GetDatabase (databaseId);
-			
-			var entity = EntityIO.ResolveEntity (businessContext, (string) entityId);
-			var context = DataContextPool.GetDataContext (entity);
-			var id = context.GetNormalizedEntityKey (entity).Value.RowKey.Id.Value;
-
-			Action<DataContext, DataLayer.Loader.Request, AbstractEntity> customizer = (d, r, e) =>
-			{
-				r.Conditions.Add (ReportingModule.CreateCondition (e, id));
-			};
-
-			return EntityExtractor.Create
-			(
-				businessContext, caches, userManager, databaseManager, dataSetAccessorGetter,
-				databaseId,"","",customizer
-			);
 		}
 
 		private static EntityWriter GetEntityWriter(EntityExtractor extractor)
@@ -99,13 +107,9 @@ namespace Epsitec.Cresus.WebCore.Server.NancyModules
 			return new LetterDocumentWriter (metaData, accessor, layout);
 		}
 
-		private static  DataExpression CreateCondition(AbstractEntity example,long id)
+		private static DataExpression CreateCondition(AbstractEntity example, long id)
 		{
-			return new ValueSetComparison (InternalField.CreateId (example), SetComparator.In, new Constant[] {new Constant (id)});
+			return new ValueSetComparison (InternalField.CreateId (example), SetComparator.In, new Constant[] { new Constant (id) });
 		}
-
-
 	}
-
-
 }
