4f7a037bcf2ce351b0cbd452ccc45efcace76ee6
Pierre Arnaud <arnaud@opac.ch>
2014-03-10 05:54:45 +0000

    - Aider.Processors: premier jet d'un générateur de documents implémenté sous la forme d'un plug-in.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@22760 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider.Processors/Processor.cs
@@ -0,0 +1,17 @@
+﻿//	Copyright © 2014, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+namespace Epsitec.Aider.Processors
+{
+	/// <summary>
+	/// The <c>Processor</c> class is used to setup and link the application which
+	/// is using the containted plug-in classes, or else .NET would not load the
+	/// assembly...
+	/// </summary>
+	public static class Processor
+	{
+		public static void Setup()
+		{
+		}
+	}
+}
---
+++ Epsitec.Cresus/Aider.Processors/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿//	Copyright © 2014, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle ("Aider.Processors")]
+[assembly: AssemblyDescription ("Annuaire de l'EERV, processeurs")]
+[assembly: AssemblyProduct ("AIDER")]
+[assembly: AssemblyCopyright ("Copyright © 2014, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland")]
+
