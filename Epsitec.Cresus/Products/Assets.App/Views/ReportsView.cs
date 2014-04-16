596c182b3e87651705c7a6da299dee28d1e82e9b
Daniel Roux <roux@epsitec.ch>
2014-04-16 06:44:36 +0000

    - Assets: Début des travaux dans ReportsView.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@22947 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Products/Assets.App/Views/ReportsView.cs
+++ Epsitec.Cresus/Products/Assets.App/Views/ReportsView.cs
@@ -3,8 +3,9 @@
 
 using System.Collections.Generic;
 using System.Linq;
-
+using Epsitec.Common.Drawing;
 using Epsitec.Common.Widgets;
+using Epsitec.Cresus.Assets.App.Widgets;
 using Epsitec.Cresus.Assets.Server.SimpleEngine;
 
 namespace Epsitec.Cresus.Assets.App.Views
@@ -19,6 +20,132 @@ namespace Epsitec.Cresus.Assets.App.Views
 		public override void CreateUI(Widget parent)
 		{
 			base.CreateUI (parent);
+
+			var topTitle = new TopTitle
+			{
+				Parent = parent,
+			};
+			topTitle.SetTitle (this.GetViewTitle (ViewType.Reports));
+
+			var controllerFrame = new FrameBox
+			{
+				Parent = parent,
+				Dock   = DockStyle.Fill,
+			};
+
+			var leftFrame = new FrameBox
+			{
+				Parent         = controllerFrame,
+				PreferredWidth = 300,
+				Dock           = DockStyle.Left | DockStyle.Top | DockStyle.Bottom,
+			};
+
+			this.CreateScrollList (leftFrame);
+			this.CreateButtons    (leftFrame);
+
+			this.InitializeScrollList ();
+			this.UpdateButtons ();
 		}
+
+		private void CreateScrollList(Widget parent)
+		{
+			new StaticText
+			{
+				Parent         = parent,
+				Text           = ReportsView.indentPrefix + "Liste des rapports disponibles",
+				Dock           = DockStyle.Top,
+				Margins        = new Margins (0, 0, 0, 10),
+			};
+
+			this.scrollList = new ScrollList
+			{
+				Parent         = parent,
+				Dock           = DockStyle.Fill,
+			};
+
+			this.scrollList.SelectedItemChanged += delegate
+			{
+				this.UpdateButtons ();
+			};
+		}
+
+		private void CreateButtons(Widget parent)
+		{
+			var frame = new FrameBox
+			{
+				Parent              = parent,
+				Dock                = DockStyle.Bottom,
+				ContainerLayoutMode = ContainerLayoutMode.HorizontalFlow,
+				PreferredHeight     = 30,
+				Margins             = new Margins (0, 0, 10, 0),
+			};
+
+			this.showButton = new Button
+			{
+				Parent         = frame,
+				Text           = "Visualiser",
+				ButtonStyle    = ButtonStyle.Icon,
+				Dock           = DockStyle.Fill,
+				Margins        = new Margins (0, 5, 0, 0),
+			};
+
+			this.exportButton = new Button
+			{
+				Parent         = frame,
+				Text           = "Exporter",
+				ButtonStyle    = ButtonStyle.Icon,
+				Dock           = DockStyle.Fill,
+				Margins        = new Margins (5, 0, 0, 0),
+			};
+		}
+
+
+		private void UpdateButtons()
+		{
+			bool enable = this.scrollList.SelectedItemIndex != -1;
+
+			this.showButton  .Enable = enable;
+			this.exportButton.Enable = enable;
+		}
+
+
+		private void InitializeScrollList()
+		{
+			this.scrollList.Items.Clear ();
+
+			foreach (var report in this.Reports)
+			{
+				this.scrollList.Items.Add (report.Id, ReportsView.indentPrefix + report.Name);
+			}
+		}
+
+		private IEnumerable<Report> Reports
+		{
+			get
+			{
+				yield return new Report ("AssetsArray",    "Tableau des immobilisations");
+				yield return new Report ("AssetsList",     "Liste des objets d'immobilisations");
+				yield return new Report ("CateroriesList", "Liste des catégories d'immobilisations");
+			}
+		}
+
+		private struct Report
+		{
+			public Report(string id, string name)
+			{
+				this.Id   = id;
+				this.Name = name;
+			}
+
+			public readonly string Id;
+			public readonly string Name;
+		}
+
+
+		private const string indentPrefix = "  ";
+
+		private ScrollList						scrollList;
+		private Button							showButton;
+		private Button							exportButton;
 	}
 }
