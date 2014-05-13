f9d879e618980889faab7fd8f66b68ceb1c6fd0a
Pierre Arnaud <arnaud@opac.ch>
2014-05-13 14:24:19 +0000

    - Assets: préparation de projets distincts pour permettre à Daniel de basculer une partie du code dans Assets.DataFillers/Assets.NodeGetters.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@23117 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Products/Assets.DataFillers/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿//	Copyright © 2014, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Assets.DataFiller")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Assets.DataFiller")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+
+[assembly: Guid("7da428da-4640-4d1b-b310-e64273b1e335")]
---
+++ Epsitec.Cresus/Products/Assets.NodeGetters/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿//	Copyright © 2014, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Assets.NodeGetters")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct ("Assets.NodeGetters")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+
+[assembly: Guid("fd50898d-65b1-472e-8bec-0bfdeb8bc0a9")]
