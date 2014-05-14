fac21a0c6eb700f54b3c51b1afbf406915bd9095
Pierre Arnaud <arnaud@opac.ch>
2014-05-14 04:26:48 +0000

    - Assets: suite des déplacements et du découpage.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@23128 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Products/Assets.Server.BusinessLogic/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿//	Copyright © 2014, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Assets.Server.BusinessLogic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Assets.Server.BusinessLogic")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+
+[assembly: Guid("b80c0816-79ef-4c41-b3ee-44e9394a9353")]
