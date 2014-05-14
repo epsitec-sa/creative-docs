88619a6a7a8e28e84c8658254925aed8cfa85572
Pierre Arnaud <arnaud@opac.ch>
2014-05-14 04:38:32 +0000

    - Assets: suite des déplacements; extrait le maximum possible de NodeGetters dans un projet séparé.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@23129 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Products/Assets.Server.Engine/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿//	Copyright © 2014, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Assets.Server.Engine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct ("Assets.Server.Engine")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+
+[assembly: Guid("5f86c515-28dd-42f0-bc71-700e6db19eed")]
