ed8d0bf50d4db91efe44a88a9e40a27b19122e35
Pierre Arnaud <arnaud@opac.ch>
2014-06-27 04:46:20 +0000

    - WebCore: fichiers oubliés.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@23341 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Cresus.WebCore.Server/Core/CoreJobStatus.cs
@@ -0,0 +1,31 @@
+//	Copyright © 2014, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Samuel LOUP, Maintainer: Samuel LOUP
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Cresus.WebCore.Server.Core
+{
+	public sealed class CoreJobStatus
+	{
+		private CoreJobStatus(string value)
+		{
+			this.Value = value;
+		}
+
+		
+		public string							Value
+		{
+			get;
+			private set;
+		}
+
+
+		public static readonly CoreJobStatus	Ordered  = new CoreJobStatus ("En attente");
+		public static readonly CoreJobStatus	Waiting  = new CoreJobStatus ("En attente");
+		public static readonly CoreJobStatus	Running  = new CoreJobStatus ("En cours");
+		public static readonly CoreJobStatus	Ended    = new CoreJobStatus ("Terminée");
+		public static readonly CoreJobStatus	Cancelled = new CoreJobStatus ("Annulée");
+	}
+}
+
---
+++ Epsitec.Cresus/Cresus.WebCore.Server/Core/CoreWorkerItem.cs
@@ -0,0 +1,100 @@
+﻿//	Copyright © 2014, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Common.IO;
+using Epsitec.Common.Support;
+using Epsitec.Common.Support.Extensions;
+using Epsitec.Common.Types.Collections.Concurrent;
+
+using Epsitec.Cresus.Core.Business;
+using Epsitec.Cresus.Core.Business.UserManagement;
+
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Epsitec.Cresus.Core.Library;
+
+namespace Epsitec.Cresus.WebCore.Server.Core
+{
+	public abstract class CoreWorkItem
+	{
+		protected CoreWorkItem(string workItemId, string username, string sessionId, dynamic bag)
+		{
+			this.enqueueDateTime = System.DateTime.Now;
+			this.workItemId      = workItemId;
+			this.username        = username;
+			this.sessionId       = sessionId;
+			this.bag             = bag;
+		}
+
+
+		public bool							Cancelled
+		{
+			get;
+			private set;
+		}
+
+		public string						WorkItemId
+		{
+			get
+			{
+				return this.workItemId;
+			}
+		}
+
+		public string						UserName
+		{
+			get
+			{
+				return this.username;
+			}
+		}
+
+		public string						SessionId
+		{
+			get
+			{
+				return this.sessionId;
+			}
+		}
+
+		public System.DateTime				EnqueueDateTime
+		{
+			get
+			{
+				return this.enqueueDateTime;
+			}
+		}
+
+		public dynamic						Bag
+		{
+			get
+			{
+				return this.bag;
+			}
+		}
+
+
+		public void Cancel()
+		{
+			this.Cancelled = true;
+		}
+
+		public abstract void Execute(CoreWorker worker);
+
+		public override string ToString()
+		{
+			return string.Format ("{0}: {1} queued by {2} on session {3}", this.enqueueDateTime, this.workItemId, this.username, this.sessionId);
+		}
+
+		
+		private readonly System.DateTime	enqueueDateTime;
+		private readonly string				workItemId;
+		private readonly string				username;
+		private readonly string				sessionId;
+		private readonly dynamic			bag;
+	}
+
+}
