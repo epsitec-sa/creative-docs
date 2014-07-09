0d7a7b06326d3a4b62a4cc849fb16fc2fdcd7bc2
Daniel Roux <roux@epsitec.ch>
2014-07-09 18:20:51 +0000

    - Assets: Encore des déplacements.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@23433 787ff09c-d2b5-7842-b444-8dd0a418631d

--- Epsitec.Cresus/Products/Assets.App/Views/ToolbarTreeTableControllers/AssetsToolbarTimelineController.cs
+++ Epsitec.Cresus/Products/Assets.App/Views/ToolbarTreeTableControllers/AssetsToolbarTimelineController.cs
@@ -14,7 +14,7 @@ using Epsitec.Cresus.Assets.Data;
 using Epsitec.Cresus.Assets.Server.BusinessLogic;
 using Epsitec.Cresus.Assets.Server.SimpleEngine;
 
-namespace Epsitec.Cresus.Assets.App.Views
+namespace Epsitec.Cresus.Assets.App.Views.ToolbarTreeTableControllers
 {
 	public class AssetsToolbarTimelineController : IDirty
 	{
