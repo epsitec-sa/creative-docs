ba458c824d2c7eaad662af99dbc60fac695c0fa3
Pierre Arnaud <arnaud@opac.ch>
2014-08-02 05:56:52 +0000

    - AIDER: ajouté un fichier manquant (reconstitué, pas sûr que ce soit juste).
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@23526 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/Labels/AiderLegalPersonTextFactory0.cs
@@ -0,0 +1,26 @@
+﻿using Epsitec.Aider.Entities;
+
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Core.Labels;
+
+
+namespace Epsitec.Aider.Labels
+{
+
+
+	[LabelTextFactoryId (0)]
+	public sealed class AiderLegalPersonTextFactory0 : LabelTextFactory<AiderLegalPersonEntity>
+	{
+
+
+		public override FormattedText GetLabelText(AiderLegalPersonEntity entity)
+		{
+			return entity.GetAddressLabelText ();
+		}
+
+
+	}
+
+
+}
