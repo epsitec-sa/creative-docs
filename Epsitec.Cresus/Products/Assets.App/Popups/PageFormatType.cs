14f9e447f72b4321e3f82eeee55d20cb5b71f44b
Daniel Roux <roux@epsitec.ch>
2014-09-03 05:22:11 +0000

    - Assets: Commit de 2 fichiers oubliés.
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@23575 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Products/Assets.App/Popups/PageFormatType.cs
@@ -0,0 +1,21 @@
+//	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using System.Collections.Generic;
+using System.Linq;
+using Epsitec.Common.Types;
+
+namespace Epsitec.Cresus.Assets.App.Popups
+{
+	[DesignerVisible]
+	public enum PageFormatType
+	{
+		Unknown,
+		A2,
+		A3,
+		A4,
+		A5,
+		Letter,
+		Legal,
+	}
+}
---
+++ Epsitec.Cresus/Products/Assets.Server.NodeGetters/FilterNodeGetter.cs
@@ -0,0 +1,119 @@
+﻿//	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Daniel ROUX, Maintainer: Daniel ROUX
+
+using System.Collections.Generic;
+using System.Linq;
+using Epsitec.Cresus.Assets.Data;
+using Epsitec.Cresus.Assets.Server.BusinessLogic;
+using Epsitec.Cresus.Assets.Server.SimpleEngine;
+
+namespace Epsitec.Cresus.Assets.Server.NodeGetters
+{
+	/// <summary>
+	/// Ne retourne que les objets qui appartiennent à un groupe donné.
+	/// Si filterGuid vaut IsEmpty, on retourne tous les objets.
+	/// </summary>
+	public class FilterNodeGetter : INodeGetter<GuidNode>  // outputNodes
+	{
+		public FilterNodeGetter(DataAccessor accessor, INodeGetter<GuidNode> objectNodes)
+		{
+			this.accessor    = accessor;
+			this.objectNodes = objectNodes;
+
+			this.outputNodes = new List<GuidNode> ();
+		}
+
+
+		public void SetParams(Timestamp? timestamp, Guid filterGuid)
+		{
+			this.timestamp  = timestamp;
+			this.filterGuid = filterGuid;
+
+			this.UpdateData ();
+		}
+
+
+		public int Count
+		{
+			get
+			{
+				if (this.filterGuid.IsEmpty)
+				{
+					return this.objectNodes.Count;
+				}
+				else
+				{
+					return this.outputNodes.Count;
+				}
+			}
+		}
+
+		public GuidNode this[int index]
+		{
+			get
+			{
+				if (this.filterGuid.IsEmpty)
+				{
+					if (index >= 0 && index < this.objectNodes.Count)
+					{
+						return this.objectNodes[index];
+					}
+					else
+					{
+						return GuidNode.Empty;
+					}
+				}
+				else
+				{
+					if (index >= 0 && index < this.outputNodes.Count)
+					{
+						return this.outputNodes[index];
+					}
+					else
+					{
+						return GuidNode.Empty;
+					}
+				}
+			}
+		}
+
+
+		private void UpdateData()
+		{
+			this.outputNodes.Clear ();
+
+			if (!this.filterGuid.IsEmpty)
+			{
+				foreach (var objectNode in this.objectNodes.GetNodes ())
+				{
+					var obj = this.accessor.GetObject (BaseType.Assets, objectNode.Guid);
+
+					foreach (var field in DataAccessor.GroupGuidRatioFields)
+					{
+						var gr = ObjectProperties.GetObjectPropertyGuidRatio
+						(
+							obj,
+							this.timestamp,
+							field,
+							inputValue: true
+						);
+
+						if (gr.Guid == this.filterGuid)  // objet faisant partie de ce groupe ?
+						{
+							var node = new GuidNode (obj.Guid);
+							this.outputNodes.Add (node);
+						}
+					}
+				}
+			}
+		}
+
+
+		private readonly DataAccessor			accessor;
+		private readonly INodeGetter<GuidNode>	objectNodes;
+		private readonly List<GuidNode>			outputNodes;
+
+		private Timestamp?						timestamp;
+		private Guid							filterGuid;
+	}
+}
