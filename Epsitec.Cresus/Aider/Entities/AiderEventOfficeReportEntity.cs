b366f0e5b2c8fb8b9675000f906d8a2d0eb4141c
Samuel Loup <loup@epsitec.ch>
2014-12-11 20:49:03 +0000

    - AIDER: Work in progress sur les rapports du registres
    git-svn-id: https://svn.opac.ch/svn/cr/branches/vs2010.1@24090 787ff09c-d2b5-7842-b444-8dd0a418631d

---
+++ Epsitec.Cresus/Aider/Entities/AiderEventOfficeReportEntity.cs
@@ -0,0 +1,40 @@
+﻿//	Copyright © 2014, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Samuel LOUP, Maintainer: Samuel LOUP
+
+using Epsitec.Common.Support;
+using Epsitec.Common.Support.EntityEngine;
+using Epsitec.Common.Support.Extensions;
+using Epsitec.Common.Types;
+
+using Epsitec.Aider.Reporting;
+
+using Epsitec.Cresus.Core.Business;
+using Epsitec.Cresus.Core.Entities;
+
+using Epsitec.Cresus.DataLayer.Context;
+
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Epsitec.Aider.Entities
+{
+	public partial class AiderEventOfficeReportEntity
+	{
+
+		public static AiderEventOfficeReportEntity Create(BusinessContext context, string eventNumber, AiderEventEntity targetEvent, string templateName, IContent content)
+		{
+			var report = context.CreateAndRegisterEntity<AiderEventOfficeReportEntity> ();
+
+			report.Name			= eventNumber;
+			report.DataTemplate = templateName;
+			report.CreationDate	= Date.Today;
+			report.Office		= targetEvent.Office;
+			report.EventNumber  = eventNumber;
+			report.Event        = targetEvent;
+			report.SetContent (content);
+
+			targetEvent.Office.AddDocumentInternal (report);
+			return report;
+		}
+	}
+}
