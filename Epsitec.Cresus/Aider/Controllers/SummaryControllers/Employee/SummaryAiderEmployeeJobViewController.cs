a3e8d608eb9d946f631e02cd66dd2ef1ccb74c66
Pierre Arnaud <arnaud@epsitec.ch>
2015-02-02 20:42:29 +0100

    Ajouter les tuiles pour les listes (postes, utilisateurs AIDER, etc.)

---
+++ Epsitec.Cresus/Aider/Controllers/SummaryControllers/Employee/SummaryAiderEmployeeJobViewController.cs
@@ -0,0 +1,36 @@
+//	Copyright © 2015, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using Epsitec.Aider.Controllers.ActionControllers;
+using Epsitec.Aider.Entities;
+using Epsitec.Cresus.Bricks;
+using Epsitec.Cresus.Core.Controllers;
+using Epsitec.Cresus.Core.Controllers.SummaryControllers;
+using Epsitec.Cresus.Core.Bricks;
+using Epsitec.Aider.Override;
+
+namespace Epsitec.Aider.Controllers.SummaryControllers
+{
+	public sealed class SummaryAiderEmployeeJobViewController : SummaryViewController<AiderEmployeeJobEntity>
+	{
+		protected override void CreateBricks(BrickWall<AiderEmployeeJobEntity> wall)
+		{
+			var user = AiderUserManager.Current.AuthenticatedUser;
+			var canEditEmploye = user.CanEditEmployee () || user.CanEditReferee ();
+
+			wall.AddBrick (x => x.Employee)
+				.Icon ("Data.AiderUser")
+				.Attribute (BrickMode.DefaultToSummarySubView);
+
+			wall.AddBrick ()
+				.Attribute (BrickMode.DefaultToNoSubView).IfFalse (user.CanEditEmployee ());
+
+			wall.AddBrick (x => x.Office)
+				.Icon ("Base.AiderGoup.Parish")
+				.Title ("Gestion associée")
+				.Text (p => p.GetCompactSummary ())
+				.Attribute (BrickMode.DefaultToSummarySubView);
+		}
+	}
+}
+
