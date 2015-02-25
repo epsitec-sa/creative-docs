f59bdf2b1bf5dd75ecc1c7a4a86632a19e28a36e
Pierre Arnaud <arnaud@epsitec.ch>
2015-02-25 07:13:24 +0100

    Assets: extrait les noms des éléments XML dans X.

--- Epsitec.Cresus/Assets.Server.Export/AbstractExport.cs
+++ Epsitec.Cresus/Assets.Server.Export/AbstractExport.cs
@@ -1,4 +1,4 @@
-﻿//	Copyright © 2013, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+﻿//	Copyright © 2013-2015, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Daniel ROUX, Maintainer: Daniel ROUX
 
 using System.Collections.Generic;
