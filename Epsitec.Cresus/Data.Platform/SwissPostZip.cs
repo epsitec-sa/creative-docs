5710c0256b92d358e63f679bcf0414051da978a8
Samuel Loup <samuel.loup@gmail.com>
2015-03-02 16:17:15 +0100

    Data.Platform: smart update pour les zip codes
    Consulte et crée un fichier .meta contenant la date de validité
    des codes zip extrait. Au prochain chargement, le système
    vérifie ce fichier de metadonnée et consulte la date de validité.
    Une mise à jour du fichier Match est effectuée si besoin.

--- Epsitec.Cresus/Data.Platform/SwissPostZip.cs
+++ Epsitec.Cresus/Data.Platform/SwissPostZip.cs
@@ -16,17 +16,70 @@ namespace Epsitec.Data.Platform
 	{
 		public static string GetSwissPostZipCsv()
 		{
-			var matchClient = new MatchWebClient ();
-			var file = matchClient.GetMatchSortFileFromWebsite ();
+			var matchClient     = new MatchWebClient ();
 			var swissPostZipCsv = SwissPostZip.GetMatchZipCsvPath ();
-			MatchSortExtractor.WriteRecordsToFile<SwissPostZipInformation> (file, SwissPostZipInformation.GetMatchRecordId (), swissPostZipCsv);
-			return swissPostZipCsv;
+
+			if (SwissPostZip.MustUpdateOrCreateZipCsv (matchClient))
+			{
+				var file = matchClient.GetMatchSortFile ();
+				var meta = MatchSortLoader.GetFileMetaData (file);
+				SwissPostZip.WriteMatchZipMetaData (meta.Validity);
+				MatchSortExtractor.WriteRecordsToFile<SwissPostZipInformation> (file, SwissPostZipInformation.GetMatchRecordId (), swissPostZipCsv);
+				return swissPostZipCsv;
+			} 
+			else
+			{
+				return swissPostZipCsv;
+			}
+		}
+
+		public static bool MustUpdateOrCreateZipCsv (MatchWebClient client)
+		{
+			var swissPostZipMeta = SwissPostZip.GetMatchZipMetaPath ();
+			if(System.IO.File.Exists (swissPostZipMeta))
+			{
+				var fileMetaData = SwissPostZip.ReadMatchZipMetaData ();
+				/* If validity date is outdated*/
+				int result = System.DateTime.Compare (fileMetaData, System.DateTime.Now);
+				if (result < 0)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			} 
+			else
+			{
+				return true;
+			}
 		}
 
 		private static string GetMatchZipCsvPath()
 		{
 			string path1 = System.Environment.GetFolderPath (System.Environment.SpecialFolder.ApplicationData);
 			return System.IO.Path.Combine (path1, "Epsitec", "swisspostzip.csv");
+		}
+
+		private static string GetMatchZipMetaPath()
+		{
+			string path1 = System.Environment.GetFolderPath (System.Environment.SpecialFolder.ApplicationData);
+			return System.IO.Path.Combine (path1, "Epsitec", "swisspostzip.meta");
+		}
+
+		private static System.DateTime ReadMatchZipMetaData()
+		{
+			var swissPostZipMeta = SwissPostZip.GetMatchZipMetaPath ();
+			string date = System.IO.File.ReadAllText (swissPostZipMeta);
+			return System.Convert.ToDateTime (date);
+		}
+
+		private static void WriteMatchZipMetaData(System.DateTime releaseDate)
+		{
+			var date = releaseDate.ToShortDateString ();
+			var swissPostZipMeta = SwissPostZip.GetMatchZipMetaPath ();
+			System.IO.File.WriteAllText (swissPostZipMeta, date);
 		}
 	}
 }
