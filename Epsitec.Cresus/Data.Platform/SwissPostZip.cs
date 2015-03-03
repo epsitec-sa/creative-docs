da3657fdbcb5be4ce117b1d6384597ee825b40b2
Samuel Loup <loup@epsitec.ch>
2015-03-03 13:02:29 +0100

    Data.Platform: mise à jour
    Suite au changement de responsabilité du client web Match,
    on simplifie ici la génération du fichier local des codes postaux.

--- Epsitec.Cresus/Data.Platform/SwissPostZip.cs
+++ Epsitec.Cresus/Data.Platform/SwissPostZip.cs
@@ -18,42 +18,17 @@ namespace Epsitec.Data.Platform
 		{
 			var matchClient     = new MatchWebClient ();
 			var swissPostZipCsv = SwissPostZip.GetMatchZipCsvPath ();
+			var file            = matchClient.GetMatchSortFile ();
 
-			if (SwissPostZip.MustUpdateOrCreateZipCsv (matchClient))
+			if (matchClient.IsANewRelease || SwissPostZip.MustGenerateMatchZipCsv ())
 			{
-				var file = matchClient.GetMatchSortFile ();
-				var meta = MatchSortLoader.GetFileMetaData (file);
-				SwissPostZip.WriteMatchZipMetaData (meta.Validity);
 				MatchSortExtractor.WriteRecordsToFile<SwissPostZipInformation> (file, SwissPostZipInformation.GetMatchRecordId (), swissPostZipCsv);
 				return swissPostZipCsv;
-			} 
+			}
 			else
 			{
 				return swissPostZipCsv;
-			}
-		}
-
-		public static bool MustUpdateOrCreateZipCsv (MatchWebClient client)
-		{
-			var swissPostZipMeta = SwissPostZip.GetMatchZipMetaPath ();
-			if(System.IO.File.Exists (swissPostZipMeta))
-			{
-				var fileMetaData = SwissPostZip.ReadMatchZipMetaData ();
-				/* If validity date is outdated*/
-				int result = System.DateTime.Compare (fileMetaData, System.DateTime.Now);
-				if (result < 0)
-				{
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-			} 
-			else
-			{
-				return true;
-			}
+			}		
 		}
 
 		private static string GetMatchZipCsvPath()
@@ -62,24 +37,9 @@ namespace Epsitec.Data.Platform
 			return System.IO.Path.Combine (path1, "Epsitec", "swisspostzip.csv");
 		}
 
-		private static string GetMatchZipMetaPath()
+		private static bool MustGenerateMatchZipCsv()
 		{
-			string path1 = System.Environment.GetFolderPath (System.Environment.SpecialFolder.ApplicationData);
-			return System.IO.Path.Combine (path1, "Epsitec", "swisspostzip.meta");
-		}
-
-		private static System.DateTime ReadMatchZipMetaData()
-		{
-			var swissPostZipMeta = SwissPostZip.GetMatchZipMetaPath ();
-			string date = System.IO.File.ReadAllText (swissPostZipMeta);
-			return System.Convert.ToDateTime (date);
-		}
-
-		private static void WriteMatchZipMetaData(System.DateTime releaseDate)
-		{
-			var date = releaseDate.ToShortDateString ();
-			var swissPostZipMeta = SwissPostZip.GetMatchZipMetaPath ();
-			System.IO.File.WriteAllText (swissPostZipMeta, date);
+			return !System.IO.File.Exists (SwissPostZip.GetMatchZipCsvPath ());
 		}
 	}
 }
