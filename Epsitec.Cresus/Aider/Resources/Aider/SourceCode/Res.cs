b97f5c4f9b4e2989e942cfaed3ced815f46fbe9a
Pierre Arnaud <arnaud@epsitec.ch>
2015-03-19 22:17:40 +0100

    Aider: Vie communautaire et cultuelle, v1.4.1512.1.

--- Epsitec.Cresus/Aider/Resources/Aider/SourceCode/Res.cs
+++ Epsitec.Cresus/Aider/Resources/Aider/SourceCode/Res.cs
@@ -1693,6 +1693,14 @@ namespace Epsitec.Aider
 							return global::Epsitec.Aider.Res._manager.GetCaption (new global::Epsitec.Common.Support.Druid (_moduleId, 10, 688));
 						}
 					}
+					//	designer:cap/LVAJL
+					public static global::Epsitec.Common.Types.Caption RegionVCC
+					{
+						get
+						{
+							return global::Epsitec.Aider.Res._manager.GetCaption (new global::Epsitec.Common.Support.Druid (_moduleId, 10, 691));
+						}
+					}
 				}
 				
 				public static class PersonConfession
