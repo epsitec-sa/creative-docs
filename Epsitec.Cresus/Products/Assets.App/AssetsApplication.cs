a64e358cd95d518bc83347f164fae911cf8fb961
Daniel Roux <roux@epsitec.ch>
2015-03-20 09:25:05 +0100

    Assets: Suppression d'un using superflu.

--- Epsitec.Cresus/Products/Assets.App/AssetsApplication.cs
+++ Epsitec.Cresus/Products/Assets.App/AssetsApplication.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Epsitec.Common.Drawing;
 using Epsitec.Common.Widgets;
-using Epsitec.Cresus.Core.Business;
 using Epsitec.Cresus.Core.Library;
 using Epsitec.Cresus.Assets.App.Widgets;
 using Epsitec.Cresus.Assets.Server.SimpleEngine;
