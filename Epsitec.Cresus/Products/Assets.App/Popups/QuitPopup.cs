699cbe2a6eb0c2332128423865b3d0496af57c64
Daniel Roux <roux@epsitec.ch>
2015-03-26 08:43:56 +0100

    Assets: Suppression de l'ancien code basé sur AbstractStackedPopup dans QuitPopup.

--- Epsitec.Cresus/Products/Assets.App/Popups/QuitPopup.cs
+++ Epsitec.Cresus/Products/Assets.App/Popups/QuitPopup.cs
@@ -13,89 +13,6 @@ namespace Epsitec.Cresus.Assets.App.Popups
 	/// <summary>
 	/// Popup posant la question avant de quitter le logiciel.
 	/// </summary>
-# if false
-	public class QuitPopup : AbstractStackedPopup
-	{
-		private QuitPopup(DataAccessor accessor)
-			: base (accessor)
-		{
-			this.title = Res.Strings.Popup.Quit.Title.ToString ();
-
-			var list = new List<StackedControllerDescription> ();
-
-			list.Add (new StackedControllerDescription  // 0
-			{
-				StackedControllerType = StackedControllerType.Label,
-				Width                 = 200,
-				Label                 = Res.Strings.Popup.Quit.Question.ToString (),
-			});
-
-			list.Add (new StackedControllerDescription  // 1
-			{
-				StackedControllerType = StackedControllerType.Radio,
-				MultiLabels           = Res.Strings.Popup.Quit.Radios.ToString (),
-			});
-
-			this.SetDescriptions (list);
-
-			this.defaultAcceptButtonName = Res.Strings.Popup.Quit.MainButton.ToString ();
-		}
-
-
-		private bool							Save
-		{
-			get
-			{
-				var controller = this.GetController (1) as RadioStackedController;
-				System.Diagnostics.Debug.Assert (controller != null);
-				return controller.Value.GetValueOrDefault () == 0;
-			}
-			set
-			{
-				var controller = this.GetController (1) as RadioStackedController;
-				System.Diagnostics.Debug.Assert (controller != null);
-				controller.Value = value ? 0 : 1;
-			}
-		}
-
-
-		protected override void UpdateWidgets(StackedControllerDescription description)
-		{
-			if (string.IsNullOrEmpty (this.accessor.ComputerSettings.MandatFilename))
-			{
-				//	S'il n'y a pas de nom de fichier connu pour le mandat, on ne pourra
-				//	pas enregistrer au moment de quitter le logiciel.
-				this.Save = false;
-				this.SetEnable (1, false);
-			}
-		}
-
-
-		#region Helpers
-		public static void Show(Widget target, DataAccessor accessor, System.Action<bool> action)
-		{
-			if (target != null)
-			{
-				var popup = new QuitPopup (accessor)
-				{
-					Save = true,
-				};
-
-				popup.Create (target, leftOrRight: false);
-
-				popup.ButtonClicked += delegate (object sender, string name)
-				{
-					if (name == "ok")
-					{
-						action (popup.Save);
-					}
-				};
-			}
-		}
-		#endregion
-
-	}
-#else
 	public class QuitPopup : AbstractPopup
 	{
 		private QuitPopup(DataAccessor accessor)
@@ -104,7 +21,7 @@ namespace Epsitec.Cresus.Assets.App.Popups
 		}
 
 
-		protected override Size DialogSize
+		protected override Size					DialogSize
 		{
 			get
 			{
@@ -211,5 +128,4 @@ namespace Epsitec.Cresus.Assets.App.Popups
 
 		private readonly DataAccessor					accessor;
 	}
-#endif
-}
+#
