6ee3d7112be71e69f7335eb6a4d079709b258aa6
Samuel Loup <loup@epsitec.ch>
2015-09-15 06:48:10 +0200

    Aider: fichier source manquant pour le commit précédant

---
+++ Epsitec.Cresus/Aider/Enumerations/MailingGroupMode.cs
@@ -0,0 +1,20 @@
+//	Copyright © 2013-2015, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Samuel LOUP, Maintainer: Samuel LOUP
+
+using Epsitec.Common.Types;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Epsitec.Aider.Enumerations
+{
+	[DesignerVisible]
+	public enum MailingGroupMode
+	{
+
+		ByContact = 0,
+		ByHouseholdUsingDesc = 1,
+		ByHouseholdUsingParticipants = 2,
+	}
+}
+
