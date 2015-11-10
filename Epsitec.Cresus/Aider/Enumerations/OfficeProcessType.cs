141891f5924d66ac1d37e55c76f0359aa1d497e7
Samuel Loup <loup@epsitec.ch>
2015-11-10 05:54:13 +0100

    Aider: ajoute les nouvelles enum.

---
+++ Epsitec.Cresus/Aider/Enumerations/OfficeProcessType.cs
@@ -0,0 +1,16 @@
+//	Copyright © 2014-2015, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Samuel LOUP, Maintainer: Samuel LOUP
+
+using Epsitec.Common.Types;
+
+namespace Epsitec.Aider.Enumerations
+{
+	[DesignerVisible]
+	public enum OfficeProcessType
+	{
+		None = 0,
+
+		PersonsOutputProcess = 1,
+	}
+}
+
