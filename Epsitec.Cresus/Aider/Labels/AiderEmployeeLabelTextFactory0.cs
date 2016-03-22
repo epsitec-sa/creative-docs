fca06f0c450a8b3a6a69be87929c0759ec23c368
Samuel Loup <loup@epsitec.ch>
2016-03-22 10:27:41 +0100

    AIDER: impl. étiquette pour le dataset des ecollaborateurs

---
+++ Epsitec.Cresus/Aider/Labels/AiderEmployeeLabelTextFactory0.cs
@@ -0,0 +1,29 @@
+﻿using Epsitec.Aider.Entities;
+using Epsitec.Aider.Enumerations;
+
+using Epsitec.Common.Types;
+
+using Epsitec.Cresus.Core.Labels;
+
+using System;
+
+
+namespace Epsitec.Aider.Labels
+{
+
+
+	[LabelTextFactoryId (0)]
+	public sealed class AiderEmployeeLabelTextFactory0 : LabelTextFactory<AiderEmployeeEntity>
+	{
+
+
+		public override FormattedText GetLabelText(AiderEmployeeEntity entity)
+		{
+			return entity.PersonContact.GetAddressLabelText (PostalAddressType.Default);
+		}
+
+
+	}
+
+
+}
