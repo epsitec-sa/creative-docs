c376495bf2ce98c77fd222cc2f01ca6af1393f7a
Samuel Loup <loup@epsitec.ch>
2016-05-25 13:36:14 +0200

    WebCoreClient: cosmetics

--- Epsitec.Cresus/Cresus.WebCore.Client/WebCore/app.js
+++ Epsitec.Cresus/Cresus.WebCore.Client/WebCore/app.js
@@ -59,7 +59,7 @@ $.getScript('signalr/hubs', function() {
         // Extend String
         String.prototype.startsWith = function (prefix) {
             return this.indexOf(prefix) === 0;
-        }
+        };
 
         String.prototype.endsWith = function (suffix) {
             return this.match(suffix + "$") == suffix;
