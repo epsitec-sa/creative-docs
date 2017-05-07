021d65fe912a1f17d9d3db8e6e76874fc6087c6c
Pierre Arnaud <arnaud@epsitec.ch>
2017-05-07 08:06:46 +0200

    Aider: ajoute une enum pour l'heuristique de match

---
+++ Epsitec.Cresus/Data.Platform/Heuristics.cs
@@ -0,0 +1,11 @@
+﻿//	Copyright © 2017, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+namespace Epsitec.Data.Platform
+{
+	public enum Heuristics
+	{
+		Default,
+		AcceptPartialRoot
+	}
+}
