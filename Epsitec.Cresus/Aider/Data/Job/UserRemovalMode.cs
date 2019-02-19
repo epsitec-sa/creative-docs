d7b7c3411176b04e8b4d4e04406b996b86cf38ed
Pierre Arnaud <arnaud@epsitec.ch>
2019-02-19 21:42:36 +0100

    Aider: add missing file

---
+++ Epsitec.Cresus/Aider/Data/Job/UserRemovalMode.cs
@@ -0,0 +1,18 @@
+﻿//	Copyright © 2019, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+
+using System.Linq;
+
+namespace Epsitec.Aider.Data.Job
+{
+    [System.Flags]
+    internal enum UserRemovalMode
+    {
+        None            = 0b0000_0000,
+        Empty           = 0b0000_0001,
+        NoContact       = 0b0000_0010,
+        NoEmail         = 0b0000_0100,
+        NotAnEmployee   = 0b0000_1000,
+        BrokenEmployee  = 0b0001_0000
+    }
+}
