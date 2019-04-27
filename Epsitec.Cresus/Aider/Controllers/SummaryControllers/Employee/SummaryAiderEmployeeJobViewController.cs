027f3f52415931cce999691202a3493b7bc0072f
Pierre Arnaud <arnaud@epsitec.ch>
2019-04-27 12:10:45 +0200

    Aider: fix access rights for jobs

--- Epsitec.Cresus/Aider/Controllers/SummaryControllers/Employee/SummaryAiderEmployeeJobViewController.cs
+++ Epsitec.Cresus/Aider/Controllers/SummaryControllers/Employee/SummaryAiderEmployeeJobViewController.cs
@@ -1,38 +1,37 @@
-//	Copyright © 2015, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
+//	Copyright Â© 2015-2019, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
 //	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
 
 using Epsitec.Aider.Controllers.ActionControllers;
 using Epsitec.Aider.Entities;
+using Epsitec.Aider.Override;
 using Epsitec.Cresus.Bricks;
 using Epsitec.Cresus.Core.Controllers;
 using Epsitec.Cresus.Core.Controllers.SummaryControllers;
 using Epsitec.Cresus.Core.Bricks;
-using Epsitec.Aider.Override;
 
 namespace Epsitec.Aider.Controllers.SummaryControllers
 {
-	public sealed class SummaryAiderEmployeeJobViewController : SummaryViewController<AiderEmployeeJobEntity>
-	{
-		protected override void CreateBricks(BrickWall<AiderEmployeeJobEntity> wall)
-		{
-			var user = AiderUserManager.Current.AuthenticatedUser;
-			var canEditEmploye = user.CanEditEmployee () || user.CanEditReferee ();
+    public sealed class SummaryAiderEmployeeJobViewController : SummaryViewController<AiderEmployeeJobEntity>
+    {
+        protected override void CreateBricks(BrickWall<AiderEmployeeJobEntity> wall)
+        {
+            var user = AiderUserManager.Current.AuthenticatedUser;
+            var canEditEmployee = user.CanEditEmployee ();
 
-			wall.AddBrick (x => x.Employee)
-				.Icon ("Data.AiderUser")
-				.Attribute (BrickMode.DefaultToSummarySubView);
+            wall.AddBrick (x => x.Employee)
+                .Icon ("Data.AiderUser")
+                .Attribute (BrickMode.DefaultToSummarySubView);
 
 
-			wall.AddBrick ()
-				.Attribute (BrickMode.DefaultToNoSubView).IfFalse (user.CanEditEmployee ())
-				.EnableActionButton<ActionAiderEmployeeJobViewController01RemoveJob> ();
+            wall.AddBrick ()
+                .Attribute (BrickMode.DefaultToNoSubView).IfFalse (canEditEmployee)
+                .EnableActionButton<ActionAiderEmployeeJobViewController01RemoveJob> ().IfTrue (canEditEmployee);
 
-			wall.AddBrick (x => x.Office)
-				.Icon ("Base.AiderGoup.Parish")
-				.Title ("Gestion associée")
-				.Text (p => p.GetCompactSummary ())
-				.Attribute (BrickMode.DefaultToSummarySubView);
-		}
-	}
+            wall.AddBrick (x => x.Office)
+                .Icon ("Base.AiderGoup.Parish")
+                .Title ("Gestion associÃ©e")
+                .Text (p => p.GetCompactSummary ())
+                .Attribute (BrickMode.DefaultToSummarySubView);
+        }
+    }
 }
-
