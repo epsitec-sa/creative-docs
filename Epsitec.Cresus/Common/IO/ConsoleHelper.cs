158d2d95b41b59f155e1747ffafc5cc8ed3eab38
Pierre Arnaud <arnaud@epsitec.ch>
2020-01-05 14:01:40 +0100

    Aider: update console handler

--- Epsitec.Cresus/Common/IO/ConsoleHelper.cs
+++ Epsitec.Cresus/Common/IO/ConsoleHelper.cs
@@ -9,125 +9,120 @@ namespace Epsitec.Common.IO
 {
 
 
-	/// <summary>
-	/// Little helper class that allows windows application to create a console that can be used
-	/// for output and input.
-	/// </summary>
-	/// <remarks>
-	/// Note that a process can have at most one console, so you can't create a console twice and
-	/// trying to do so might be problematic.
-	/// </remarks>
-	public static class ConsoleCreator
-	{
+    /// <summary>
+    /// Little helper class that allows windows application to create a console that can be used
+    /// for output and input.
+    /// </summary>
+    /// <remarks>
+    /// Note that a process can have at most one console, so you can't create a console twice and
+    /// trying to do so might be problematic.
+    /// </remarks>
+    public static class ConsoleCreator
+    {
         public static void Initialize()
         {
-            ConsoleCreator.CreateConsole ();
+            ConsoleCreator.CreateConsole();
         }
 
-		/// <summary>
-		/// Creates a console, executes the given action and deletes the console afterwards. If the
-		/// new console can't be created, an exception will be thrown and the action won't be
-		/// executed.
-		/// </summary>
-		/// <param name="action">The action to execute.</param>
-		/// <param name="windowWidth">Width of the window (or zero).</param>
-		public static void RunWithConsole(Action action, int windowWidth = 0)
-		{
-			bool success = false;
+        /// <summary>
+        /// Creates a console, executes the given action and deletes the console afterwards. If the
+        /// new console can't be created, an exception will be thrown and the action won't be
+        /// executed.
+        /// </summary>
+        /// <param name="action">The action to execute.</param>
+        /// <param name="windowWidth">Width of the window (or zero).</param>
+        public static void RunWithConsole(Action action, int windowWidth = 0)
+        {
+            bool success = false;
 
-			try
-			{
-				success = ConsoleCreator.CreateConsole ();
+            try
+            {
+                success = ConsoleCreator.CreateConsole ();
 
-				if (!success)
-				{
-					throw new Exception ("The console could not be created.");
-				}
+                if (!success)
+                {
+                    throw new Exception("The console could not be created.");
+                }
 
-				if (windowWidth > 0)
-				{
-                    if (ConsoleCreator.IsOutputRedirected == false)
+                if (windowWidth > 0)
+                {
+                    if (WinConsole.IsConsoleRedirected == false)
                     {
                         try
                         {
-                            System.Console.SetWindowSize (windowWidth, System.Console.WindowHeight);
+                            System.Console.SetWindowSize(windowWidth, System.Console.WindowHeight);
                         }
                         catch (System.ArgumentOutOfRangeException ex)
                         {
                             var find = "maximum window size of ";
                             var message = ex.Message;
-                            var pos = message.IndexOf (find);
+                            var pos = message.IndexOf(find);
 
-                            System.Diagnostics.Trace.WriteLine ($"RunWithConsole: {message}");
+                            System.Diagnostics.Trace.WriteLine($"RunWithConsole: {message}");
 
                             if (pos > 0)
                             {
-                                message = message.Substring (pos + find.Length);
-                                pos = message.IndexOf (' ');
+                                message = message.Substring(pos + find.Length);
+                                pos = message.IndexOf(' ');
                                 if (pos > 0)
                                 {
-                                    var value = int.Parse (message.Substring (0, pos), System.Globalization.CultureInfo.InvariantCulture);
+                                    var value = int.Parse(message.Substring(0, pos), System.Globalization.CultureInfo.InvariantCulture);
                                     windowWidth = value;
-                                    System.Diagnostics.Trace.WriteLine ($"Adjust to {windowWidth}");
-                                    System.Console.SetWindowSize (windowWidth, System.Console.WindowHeight);
+                                    System.Diagnostics.Trace.WriteLine($"Adjust to {windowWidth}");
+                                    System.Console.SetWindowSize(windowWidth, System.Console.WindowHeight);
                                 }
                             }
                         }
-                        System.Console.SetBufferSize (windowWidth, System.Console.BufferHeight);
+                        
+                        System.Console.SetBufferSize(windowWidth, System.Console.BufferHeight);
                     }
-				}
-
-				action ();
-			}
-			finally
-			{
-				if (success)
-				{
-					ConsoleCreator.DeleteConsole ();
-				}
-			}
-		}
-
-
-
-
-		/// <summary>
-		/// Creates a console.
-		/// </summary>
-		/// <returns>return true for success and false for failure.</returns>
-		private static bool CreateConsole()
-		{
-            if (System.Threading.Interlocked.Increment (ref ConsoleCreator.counter) == 1)
-            {
-                ConsoleCreator.consoleAllocResult = ConsoleCreator.AllocConsole ();
-
-                var defaultHandle = new IntPtr (7);
-                var currentHandle = ConsoleCreator.GetStdHandle (ConsoleCreator.StdOutputHandle);
-
-                if (currentHandle != defaultHandle)
-                {
-                    //  Visual Studio has redirected our output, apparently, so we
-                    //  need to restore a valid handle here in order to be able to
-                    //  use SetCursorPosition, etc.
-
-                    ConsoleCreator.SetStdHandle (StdOutputHandle, defaultHandle);
-
-                    System.Diagnostics.Trace.WriteLine ("AllocConsole: restored output to console");
                 }
 
-                var writer = new System.IO.StreamWriter (Console.OpenStandardOutput (), System.Text.Encoding.GetEncoding (850))
+                action();
+            }
+            finally
+            {
+                if (success)
                 {
-                    AutoFlush = true
-                };
+                    ConsoleCreator.DeleteConsole();
+                }
+            }
+        }
 
-                System.Diagnostics.Trace.WriteLine ("Console created");
 
-                Console.SetOut (writer);
+        public static void SetCursorPosition(int x, int y)
+        {
+            if (!ConsoleCreator.disableSetCursor)
+            {
+                System.Console.SetCursorPosition (x, y);
+            }
+        }
 
-                System.Console.WriteLine ($"{System.DateTime.Now.ToShortDateString ()} {System.DateTime.Now.ToLongTimeString ()}");
-                System.Console.SetCursorPosition (0, 0);
-                System.Console.WriteLine ("                   ");
-                System.Console.SetCursorPosition (0, 0);
+
+        /// <summary>
+        /// Creates a console.
+        /// </summary>
+        /// <returns>return true for success and false for failure.</returns>
+        private static bool CreateConsole()
+        {
+            if (System.Threading.Interlocked.Increment(ref ConsoleCreator.counter) == 1)
+            {
+                ConsoleCreator.consoleAllocResult = WinConsole.Initialize (false);
+                
+                if (ConsoleCreator.consoleAllocResult)
+                {
+                    try
+                    {
+                        System.Console.WriteLine ($"{System.DateTime.Now.ToShortDateString ()} {System.DateTime.Now.ToLongTimeString ()}");
+                        System.Console.SetCursorPosition (0, 0);
+                        System.Console.WriteLine ("                   ");
+                        System.Console.SetCursorPosition (0, 0);
+                    }
+                    catch (System.IO.IOException)
+                    {
+                        ConsoleCreator.disableSetCursor = true;
+                    }
+                }
             }
 
             return ConsoleCreator.consoleAllocResult;
@@ -139,49 +134,21 @@ namespace Epsitec.Common.IO
         /// </summary>
         /// <returns>return true for success and false for failure.</returns>
         private static bool DeleteConsole()
-		{
+        {
             if (System.Threading.Interlocked.Decrement (ref ConsoleCreator.counter) == 0)
             {
-                return ConsoleCreator.FreeConsole ();
+                return WinConsole.Free ();
             }
-
-            return true;
-		}
+            else
+            {
+                return true;
+            }
+        }
 
 
         private static int counter;
         private static bool consoleAllocResult;
-
-
-
-        private const uint StdOutputHandle = 0xFFFFFFF5;
-        [DllImport ("kernel32.dll")]
-        private static extern IntPtr GetStdHandle(uint nStdHandle);
-        [DllImport ("kernel32.dll")]
-        private static extern void SetStdHandle(uint nStdHandle, IntPtr handle);
-
-        [DllImport ("kernel32.dll", SetLastError=true)]
-		[return: MarshalAs (UnmanagedType.Bool)]
-		private static extern bool AllocConsole();
-
-        [DllImport ("kernel32.dll", SetLastError=true)]
-		[return: MarshalAs (UnmanagedType.Bool)]
-		private static extern bool FreeConsole();
-        
-        public static bool IsOutputRedirected => FileType.Char != GetFileType (GetStdHandle (StdHandle.Stdout));
-
-        public static bool IsInputRedirected => FileType.Char != GetFileType (GetStdHandle (StdHandle.Stdin));
-
-        public static bool IsErrorRedirected => FileType.Char != GetFileType (GetStdHandle (StdHandle.Stderr));
-
-        // P/Invoke:
-        private enum FileType { Unknown, Disk, Char, Pipe };
-        private enum StdHandle { Stdin = -10, Stdout = -11, Stderr = -12 };
-
-        [DllImport ("kernel32.dll")]
-        private static extern FileType GetFileType(IntPtr hdl);
-
-        [DllImport ("kernel32.dll")]
-        private static extern IntPtr GetStdHandle(StdHandle std);
+        private static bool disableSetCursor;
     }
 }
+
