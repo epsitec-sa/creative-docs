05e7030f4b4b29e5df5992de6fc03b0a14060f85
Baptiste Lambert <lambert@epsitec.ch>
2024-02-15 12:25:41 +0100

    Remove app.manifest and fix warning.

--- App.CreativeDocs/Program.cs
+++ App.CreativeDocs/Program.cs
@@ -1,13 +1,17 @@
 //	Copyright © 2003-2008, EPSITEC SA, 1400 Yverdon-les-Bains, Switzerland
 //	Responsable: Pierre ARNAUD
 
+using System.Windows.Forms;
+
 namespace Epsitec.App.CreativeDocs
 {
-    public class Application
+    public class Program
     {
         [System.STAThread]
         public static void Main()
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Epsitec.Common.Support.Resources.OverrideDefaultTwoLetterISOLanguageName("en");
             Epsitec.Common.DocumentEditor.Application.Start("F");
         }
