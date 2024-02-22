dc2468030e4ab2ae78bcf0e724968cb887962049
Roger Vuistiner <vuistiner@epsitec.ch>
2024-02-22 14:23:27 +0100

    Migrate cresus graph to net8 - first step...
    - 'Common.Graph.Connector' does not compile due to WCF issues.
    - TODO: migrate 'App.CresusGraph' to 'CoreWCF'.

--- Common.Graph/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-//	Copyright © 2003-2011, EPSITEC SA, 1400 Yverdon-les-Bains, Switzerland
-//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
-
-using System.Reflection;
-
-[assembly: AssemblyTitle ("Epsitec.Common.Graph")]
-[assembly: AssemblyDescription ("Charting Framework")]
-[assembly: AssemblyProduct ("Framework")]
-[assembly: AssemblyCopyright ("Copyright © 2009-2011, Pierre Arnaud, EPSITEC SA & OPaC bright ideas, Switzerland")]
