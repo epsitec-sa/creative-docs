dd4d2f047298cc68d9d19c38af3513f7efbfff63
Baptiste Lambert <lambert@epsitec.ch>
2024-06-01 14:54:00 +0200

    Cleanup Image

--- Common/Drawing/Image.cs
+++ Common/Drawing/Image.cs
@@ -8,29 +8,6 @@ namespace Epsitec.Common.Drawing
     /// </summary>
     public abstract class Image : System.IDisposable
     {
-        //public Image(Size size)
-        //{
-        //    this.size = size;
-        //    this.origin = null;
-        //    this.dpiX = 96;
-        //    this.dpiY = 96;
-
-        //    this.uniqueId = System.Threading.Interlocked.Increment(ref Image.uniqueIdSeed);
-        //}
-
-        //public Image(Point? origin, Size size)
-        //    : this(size)
-        //{
-        //    if (origin != null)
-        //    {
-        //        this.origin = origin;
-        //    }
-        //}
-
-        //public string Id {
-        //    //	L'identificateur permet de retrouver l'image lors de la désérialisation du port graphique XmlPort.
-        //    get; set; }
-
         public abstract Size Size { get; }
 
         public double Width
@@ -70,63 +47,8 @@ namespace Epsitec.Common.Drawing
             return null;
         }
 
-        //public virtual Point Origin
-        //{
-        //    //	0 < origin < size: l'origine est dans l'image
-
-        //    get { return this.origin ?? new Point(); }
-        //}
-
-        //public virtual bool IsOriginDefined
-        //{
-        //    get { return this.origin != null; }
-        //}
-
-        //public long UniqueId
-        //{
-        //    get { return this.uniqueId; }
-        //}
-
-        //public double DpiX
-        //{
-        //    get { return this.dpiX; }
-        //}
-
-        //public double DpiY
-        //{
-        //    get { return this.dpiY; }
-        //}
-
-        //public static readonly Image Empty;
-
-        //~Image()
-        //{
-        //    this.Dispose(false);
-        //}
-
         #region IDisposable Members
         public abstract void Dispose();
         #endregion
-
-        //public void AssigneUniqueId(long uniqueId)
-        //{
-        //    if (uniqueId != 0)
-        //    {
-        //        this.uniqueId = uniqueId;
-        //    }
-        //}
-
-        //protected virtual void Dispose(bool disposing) { }
-
-        //public virtual void RemoveFromCache() { }
-
-        //internal double dpiX;
-        //internal double dpiY;
-
-        //protected Size size;
-        //protected Point? origin;
-        //private long uniqueId;
-
-        //private static long uniqueIdSeed = 1;
     }
 }
