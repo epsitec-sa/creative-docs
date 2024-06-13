9a9f95b838119668a8972439315d859a0fa346e3
Baptiste Lambert <lambert@epsitec.ch>
2024-06-13 14:16:58 +0200

    Add serialization interface

---
+++ Common/Support/IXMLSerializable.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Linq;
+
+namespace Epsitec.Common.Support
+{
+    public interface IXMLWritable
+    {
+        public XElement ToXML();
+    }
+
+    public interface IXMLSerializable<T> : IXMLWritable
+    {
+        public static abstract T FromXML(XElement xml);
+    }
+}
