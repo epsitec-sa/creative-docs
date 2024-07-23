00d9ee1fd394f333445fc1d07f696510955c3df5
Baptiste Lambert <lambert@epsitec.ch>
2024-07-23 09:00:11 +0200

    Add license headers

--- Common/Types/Converters/GenericConverter`1.cs
+++ Common/Types/Converters/GenericConverter`1.cs
@@ -1,5 +1,22 @@
-//	Copyright © 2010-2011, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
-//	Author: Pierre ARNAUD, Maintainer: Pierre ARNAUD
+/*
+This file is part of CreativeDocs.
+
+Copyright © 2003-2024, EPSITEC SA, 1400 Yverdon-les-Bains, Switzerland
+
+CreativeDocs is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+CreativeDocs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 
 namespace Epsitec.Common.Types.Converters
 {
